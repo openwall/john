c8d04b2c17b03a1458c9a73ca347110f647c94f0
Solar Designer <solar@openwall.com>
2002-04-11 18:03:14 +0000

    This is 2002.

--- src/options.c
+++ src/options.c
@@ -1,6 +1,6 @@
 /*
  * This file is part of John the Ripper password cracker,
- * Copyright (c) 1996-2000 by Solar Designer
+ * Copyright (c) 1996-2002 by Solar Designer
  */
 
 #include <stdio.h>
@@ -78,7 +78,7 @@ static struct opt_entry opt_list[] = {
 
 #define JOHN_USAGE \
 "John the Ripper password cracker, version " JOHN_VERSION "\n" \
-"Copyright (c) 1996-2001 by " JOHN_COPYRIGHT "\n" \
+"Copyright (c) 1996-2002 by " JOHN_COPYRIGHT "\n" \
 "\n" \
 "Usage: %s [OPTIONS] [PASSWORD-FILES]\n" \
 "-single                   \"single crack\" mode\n" \
