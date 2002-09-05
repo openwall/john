8cf5bfbc89ad9eb4f799fa15e7e874fe5ce80d51
Solar Designer <solar@openwall.com>
2002-09-05 05:56:35 +0000

    Never put dupes in crk_guesses[], that could overflow it and would be inefficient anyway.

--- src/params.h
+++ src/params.h
@@ -15,7 +15,7 @@
 /*
  * John's version number.
  */
-#define JOHN_VERSION			"1.6.31.4"
+#define JOHN_VERSION			"1.6.31.5"
 
 /*
  * Is this a system-wide installation? *BSD ports and Linux distributions
