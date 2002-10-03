63efb2f5098b43ec92fe9ad9dc91e624298e8ab0
Solar Designer <solar@openwall.com>
2002-10-03 04:27:29 +0000

    Never point cfg_name to path_expand()'s result buffer, make a copy.

--- src/params.h
+++ src/params.h
@@ -15,7 +15,7 @@
 /*
  * John's version number.
  */
-#define JOHN_VERSION			"1.6.32"
+#define JOHN_VERSION			"1.6.32.1"
 
 /*
  * Is this a system-wide installation? *BSD ports and Linux distributions
