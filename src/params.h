c1ca4a725d95b21c392b3082cd570f0444a137db
Solar Designer <solar@openwall.com>
2003-01-24 04:02:34 +0000

    Added a 64-bit Solaris SPARC make target (recent gcc only for now).

--- src/params.h
+++ src/params.h
@@ -15,7 +15,7 @@
 /*
  * John's version number.
  */
-#define JOHN_VERSION			"1.6.32.5"
+#define JOHN_VERSION			"1.6.33"
 
 /*
  * Is this a system-wide installation? *BSD ports and Linux distributions
