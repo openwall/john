505155212497a57e8589cd66d19cfce95b462dd3
solar <solar>
2003-10-05 13:32:22 +0000

    Added two make targets for Linux on x86-64, thanks to John Edward Scott.

---
+++ src/x86-64.h
@@ -0,0 +1,47 @@
+/*
+ * This file is part of John the Ripper password cracker,
+ * Copyright (c) 2003 by Solar Designer
+ */
+
+/*
+ * Architecture specific parameters for AMD x86-64.
+ */
+
+#ifndef _JOHN_ARCH_H
+#define _JOHN_ARCH_H
+
+#define ARCH_WORD			long
+#define ARCH_SIZE			8
+#define ARCH_BITS			64
+#define ARCH_BITS_LOG			6
+#define ARCH_BITS_STR			"64"
+#define ARCH_LITTLE_ENDIAN		1
+#define ARCH_INT_GT_32			0
+#define ARCH_ALLOWS_UNALIGNED		1
+#define ARCH_INDEX(x)			((unsigned int)(unsigned char)(x))
+
+#define OS_TIMER			1
+#define OS_FLOCK			1
+
+#define CPU_DETECT			0
+
+#define DES_ASM				0
+#define DES_128K			0
+#define DES_X2				0
+#define DES_MASK			1
+#define DES_SCALE			1
+#define DES_EXTB			1
+#define DES_COPY			0
+#define DES_BS_ASM			0
+#define DES_BS				1
+#define DES_BS_VECTOR			0
+#define DES_BS_EXPAND			1
+
+#define MD5_ASM				0
+#define MD5_X2				1
+#define MD5_IMM				1
+
+#define BF_ASM				0
+#define BF_SCALE			1
+
+#endif
