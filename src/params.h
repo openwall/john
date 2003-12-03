7c7af197628f608ca305bfda2787e948e45c8a84
solar <solar>
2003-12-03 10:23:14 +0000

    1.6.36.8:
    Avoid triggering a MacOS X cpp bug(?) where it would detect and refuse to
    handle "recursive" cpp macros.

--- src/params.h
+++ src/params.h
@@ -15,7 +15,7 @@
 /*
  * John's version number.
  */
-#define JOHN_VERSION			"1.6.36.6"
+#define JOHN_VERSION			"1.6.36.8"
 
 /*
  * Is this a system-wide installation? *BSD ports and Linux distributions
