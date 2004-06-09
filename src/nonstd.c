02f94c8719bafbfd873762994724722b81b46485
Solar Designer <solar@openwall.com>
2004-06-09 04:44:57 +0000

    Updated to versions currently on Matthew's web site which include licensing information.

--- src/nonstd.c
+++ src/nonstd.c
@@ -1,7 +1,10 @@
 /*
  * Generated S-box files.
  *
- * Produced by Matthew Kwan - May 1998
+ * This software may be modified, redistributed, and used for any purpose,
+ * so long as its origin is acknowledged.
+ *
+ * Produced by Matthew Kwan - March 1998
  */
 
 
