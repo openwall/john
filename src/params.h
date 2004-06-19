1895f45ff966e7ec9826189afd0af6d8642ace11
Solar Designer <solar@openwall.com>
2004-06-19 15:22:21 +0000

    1.6.37.2:
    - "N passwords cracked" -> "N password hashes cracked" because there can be
    multiple hashes per password with LM or double-length DES-based crypt(3).
    
    - Updated some documentation files from John 1.6 and included them in doc/

--- src/params.h
+++ src/params.h
@@ -15,7 +15,7 @@
 /*
  * John's version number.
  */
-#define JOHN_VERSION			"1.6.37.1"
+#define JOHN_VERSION			"1.6.37.2"
 
 /*
  * Is this a system-wide installation? *BSD ports and Linux distributions
