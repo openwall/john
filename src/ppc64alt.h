f74fca2dfd7df3a7cb003c7534e3965e55e63e5a
Solar Designer <solar@openwall.com>
2005-02-15 17:00:18 +0000

    Added PowerPC w/ AltiVec support (effective 128-bitness for bitslice DES).
    G5 1.8 GHz:
    
    Benchmarking: Traditional DES [128/128 BS AltiVec]... DONE
    Many salts:     1102K c/s real, 1105K c/s virtual
    Only one salt:  839091 c/s real, 844156 c/s virtual

---
+++ src/ppc64alt.h
@@ -0,0 +1,48 @@
+/*
+ * This file is part of John the Ripper password cracker,
+ * Copyright (c) 1996-2000,2005 by Solar Designer
+ */
+
+/*
+ * Architecture specific parameters for 64-bit PowerPC w/ AltiVec (128-bit).
+ */
+
+#ifndef _JOHN_ARCH_H
+#define _JOHN_ARCH_H
+
+#define ARCH_WORD			long long
+#define ARCH_SIZE			8
+#define ARCH_BITS			64
+#define ARCH_BITS_LOG			6
+#define ARCH_BITS_STR			"64"
+#define ARCH_LITTLE_ENDIAN		0
+#define ARCH_INT_GT_32			0
+#define ARCH_ALLOWS_UNALIGNED		0
+#define ARCH_INDEX(x)			((unsigned int)(unsigned char)(x))
+
+#define OS_TIMER			1
+#define OS_FLOCK			1
+
+#define CPU_DETECT			0
+
+#define DES_ASM				0
+#define DES_128K			0
+#define DES_X2				0
+#define DES_MASK			1
+#define DES_SCALE			0
+#define DES_EXTB			1
+#define DES_COPY			0
+#define DES_BS_ASM			1
+#define DES_BS				1
+#define DES_BS_VECTOR			2
+#define DES_BS_EXPAND			0
+#define DES_BS_ALGORITHM_NAME		"128/128 BS AltiVec"
+
+#define MD5_ASM				0
+#define MD5_X2				1
+#define MD5_IMM				0
+
+#define BF_ASM				0
+#define BF_SCALE			0
+
+#endif
