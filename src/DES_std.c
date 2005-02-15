beb0b6a1d54268243ff694914e8109596748d06d
solar <solar>
2005-02-15 09:00:12 +0000

    Updated copyright years.

--- src/DES_std.c
+++ src/DES_std.c
@@ -1,6 +1,6 @@
 /*
  * This file is part of John the Ripper password cracker,
- * Copyright (c) 1996-2001 by Solar Designer
+ * Copyright (c) 1996-2001,2005 by Solar Designer
  */
 
 #include <string.h>
