db9ae36b382ee87cc02154dcd07dff3b3ffa2672
solar <solar>
2005-04-17 19:25:32 +0000

    Even more bitslice DES set_key*() optimizations:
    Use a separate loop for undoing the old password beyond the new password's
    length (this simplifies the exit check of both loops).
    
    Store byte offsets into K[] rather than bit positions in s1[] (this
    simplifies effective address calculation).
    
    Quickly skip over the first 4 characters if they're unchanged (on archs
    supporting unaligned accesses only).

--- src/DES_bs.c
+++ src/DES_bs.c
@@ -97,15 +97,19 @@ void DES_bs_init(int LM)
 /* Special case: instead of doing an extra check in *_set_key*(), we
  * might overrun into DES_bs_all.B, which is harmless as long as the
  * order of fields is unchanged.  57 is the smallest value to guarantee
- * we'd be past the end of K[] since we start with ofs = -1. */
+ * we'd be past the end of K[] since we start at -1. */
 	DES_bs_all.s1[0] = 57;
 
-/* The same for second bits. */
+/* The same for second bits */
 	for (index = 0; index < 0x100; index++) {
 		bit = DES_bs_all.s1[index];
 		bit += DES_bs_all.s1[index >> bit];
 		DES_bs_all.s2[index] = (bit <= 8) ? bit : 57;
 	}
+
+/* Convert to byte offsets */
+	for (index = 0; index < 0x100; index++)
+		DES_bs_all.s1[index] *= DES_BS_DEPTH >> 3;
 
 	if (LM) {
 		for (c = 0; c < 0x100; c++)
@@ -163,6 +167,8 @@ void DES_bs_clear_keys_LM(void)
 
 void DES_bs_set_key(char *key, int index)
 {
+/* new is NUL-terminated, but not NUL-padded to any length;
+ * old is NUL-padded to 8 characters, but not always NUL-terminated. */
 	register unsigned char *new = (unsigned char *)key;
 	register unsigned char *old = DES_bs_all.keys[index];
 	register DES_bs_vector *k, *kbase;
@@ -173,7 +179,15 @@ void DES_bs_set_key(char *key, int index)
 
 	mask = (ARCH_WORD)1 << index;
 	k = (DES_bs_vector *)&DES_bs_all.K[0] DEPTH - 1;
-	while ((*new || *old) && k < &DES_bs_all.K[55]) {
+#if ARCH_ALLOWS_UNALIGNED
+	if (*(ARCH_WORD_32 *)new == *(ARCH_WORD_32 *)old &&
+	    old[sizeof(ARCH_WORD_32)]) {
+		new += sizeof(ARCH_WORD_32);
+		old += sizeof(ARCH_WORD_32);
+		k += sizeof(ARCH_WORD_32) * 7;
+	}
+#endif
+	while (*new && k < &DES_bs_all.K[55]) {
 		kbase = k;
 		if ((xor = *new ^ *old)) {
 			xor &= 0x7F; /* Note: this might result in xor == 0 */
@@ -181,7 +195,7 @@ void DES_bs_set_key(char *key, int index)
 			do {
 				s1 = DES_bs_all.s1[xor];
 				s2 = DES_bs_all.s2[xor];
-				k[s1] START ^= mask;
+				*(ARCH_WORD *)((char *)k + s1) ^= mask;
 				if (s2 > 8) break; /* Required for xor == 0 */
 				xor >>= s2;
 				k[s2] START ^= mask;
@@ -190,7 +204,7 @@ void DES_bs_set_key(char *key, int index)
 				s1 = DES_bs_all.s1[xor];
 				s2 = DES_bs_all.s2[xor];
 				xor >>= s2;
-				k[s1] START ^= mask;
+				*(ARCH_WORD *)((char *)k + s1) ^= mask;
 				k[s2] START ^= mask;
 				k += s2;
 			} while (xor);
@@ -201,11 +215,36 @@ void DES_bs_set_key(char *key, int index)
 		k = kbase + 7;
 	}
 
+	while (*old && k < &DES_bs_all.K[55]) {
+		kbase = k;
+		xor = *old;
+		*old++ = 0;
+		do {
+			s1 = DES_bs_all.s1[xor];
+			s2 = DES_bs_all.s2[xor];
+			xor >>= s2;
+			*(ARCH_WORD *)((char *)k + s1) ^= mask;
+			k[s2] START ^= mask;
+			k += s2;
+			if (!xor) break;
+			s1 = DES_bs_all.s1[xor];
+			s2 = DES_bs_all.s2[xor];
+			xor >>= s2;
+			*(ARCH_WORD *)((char *)k + s1) ^= mask;
+			k[s2] START ^= mask;
+			k += s2;
+		} while (xor);
+
+		k = kbase + 7;
+	}
+
 	DES_bs_all.keys_changed = 1;
 }
 
 void DES_bs_set_key_LM(char *key, int index)
 {
+/* new is NUL-terminated, but not NUL-padded to any length;
+ * old is NUL-padded to 7 characters and NUL-terminated. */
 	register unsigned char *new = (unsigned char *)key;
 #if !DES_BS_VECTOR && ARCH_BITS >= 64
 	register unsigned char *old = DES_bs_all.E.extras.keys[index];
@@ -221,7 +260,15 @@ void DES_bs_set_key_LM(char *key, int index)
 
 	mask = (ARCH_WORD)1 << index;
 	k = (DES_bs_vector *)&DES_bs_all.K[0] DEPTH - 1;
-	while ((*new || *old) && k < &DES_bs_all.K[55]) {
+#if ARCH_ALLOWS_UNALIGNED
+	if (*(ARCH_WORD_32 *)new == *(ARCH_WORD_32 *)old &&
+	    old[sizeof(ARCH_WORD_32)]) {
+		new += sizeof(ARCH_WORD_32);
+		old += sizeof(ARCH_WORD_32);
+		k += sizeof(ARCH_WORD_32) * 8;
+	}
+#endif
+	while (*new && k < &DES_bs_all.K[55]) {
 		plain = DES_bs_all.E.extras.u[ARCH_INDEX(*new)];
 		kbase = k;
 		if ((xor = plain ^ *old)) {
@@ -230,21 +277,44 @@ void DES_bs_set_key_LM(char *key, int index)
 				s1 = DES_bs_all.s1[xor];
 				s2 = DES_bs_all.s2[xor];
 				xor >>= s2;
-				k[s1] START ^= mask;
+				*(ARCH_WORD *)((char *)k + s1) ^= mask;
 				k[s2] START ^= mask;
 				k += s2;
 				if (!xor) break;
 				s1 = DES_bs_all.s1[xor];
 				s2 = DES_bs_all.s2[xor];
 				xor >>= s2;
-				k[s1] START ^= mask;
+				*(ARCH_WORD *)((char *)k + s1) ^= mask;
 				k[s2] START ^= mask;
 				k += s2;
 			} while (xor);
 		}
 
-		if (*new) new++;
+		new++;
 		old++;
+		k = kbase + 8;
+	}
+
+	while (*old) {
+		kbase = k;
+		xor = *old;
+		*old++ = 0;
+		do {
+			s1 = DES_bs_all.s1[xor];
+			s2 = DES_bs_all.s2[xor];
+			xor >>= s2;
+			*(ARCH_WORD *)((char *)k + s1) ^= mask;
+			k[s2] START ^= mask;
+			k += s2;
+			if (!xor) break;
+			s1 = DES_bs_all.s1[xor];
+			s2 = DES_bs_all.s2[xor];
+			xor >>= s2;
+			*(ARCH_WORD *)((char *)k + s1) ^= mask;
+			k[s2] START ^= mask;
+			k += s2;
+		} while (xor);
+
 		k = kbase + 8;
 	}
 }
--- src/params.h
+++ src/params.h
@@ -15,7 +15,7 @@
 /*
  * John's version number.
  */
-#define JOHN_VERSION			"1.6.37.7"
+#define JOHN_VERSION			"1.6.37.8"
 
 /*
  * Is this a system-wide installation? *BSD ports and Linux distributions
