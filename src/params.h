6ad55b87d5806cafbedf12d34e81d2d70008c980
Solar Designer <solar@openwall.com>
2005-11-12 16:27:13 +0000

    1.6.39.4:
    Corrected the way nouns ending in "z" and "h" (other than those ending in
    "ch" and "sh") are pluralized with the "p" wordlist rules command.

--- src/params.h
+++ src/params.h
@@ -15,7 +15,7 @@
 /*
  * John's version number.
  */
-#define JOHN_VERSION			"1.6.39.3"
+#define JOHN_VERSION			"1.6.39.4"
 
 /*
  * Is this a system-wide installation? *BSD ports and Linux distributions
