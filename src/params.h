151c6d41c2d9660cea10a4fcb4e48c09670d197e
Solar Designer <solar@openwall.com>
2005-12-16 15:55:59 +0000

    1.6.40

--- src/params.h
+++ src/params.h
@@ -15,7 +15,7 @@
 /*
  * John's version number.
  */
-#define JOHN_VERSION			"1.6.39.5"
+#define JOHN_VERSION			"1.6.40"
 
 /*
  * Is this a system-wide installation? *BSD ports and Linux distributions
