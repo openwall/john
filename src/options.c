5cc1252e0bfe863985152b44a8f133f89d81794b
solar <solar>
2008-06-21 15:01:27 +0000

    Updated the copyright years.

--- src/options.c
+++ src/options.c
@@ -1,6 +1,6 @@
 /*
  * This file is part of John the Ripper password cracker,
- * Copyright (c) 1996-2006 by Solar Designer
+ * Copyright (c) 1996-2008 by Solar Designer
  */
 
 #include <stdio.h>
@@ -79,7 +79,7 @@ static struct opt_entry opt_list[] = {
 
 #define JOHN_USAGE \
 "John the Ripper password cracker, version " JOHN_VERSION "\n" \
-"Copyright (c) 1996-2006 by " JOHN_COPYRIGHT "\n" \
+"Copyright (c) 1996-2008 by " JOHN_COPYRIGHT "\n" \
 "Homepage: http://www.openwall.com/john/\n" \
 "\n" \
 "Usage: %s [OPTIONS] [PASSWORD-FILES]\n" \
