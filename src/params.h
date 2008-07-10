a0506cc5aa3c2abde36a00a1425a367cc777d9af
Solar Designer <solar@openwall.com>
2008-07-10 04:15:51 +0000

    1.7.3:
    - Disabled BF_X2 on RISC architectures for now.
    - Documentation updates to refer to LM hashes in a non-confusing way.

--- src/params.h
+++ src/params.h
@@ -15,7 +15,7 @@
 /*
  * John's version number.
  */
-#define JOHN_VERSION			"1.7.2.4"
+#define JOHN_VERSION			"1.7.3"
 
 /*
  * Notes to packagers of John for *BSD "ports", Linux distributions, etc.:
