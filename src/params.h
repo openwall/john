d80b5330bb6f5dac4ef11eaf176e9dd1a4bfdec6
solar <solar>
2010-02-24 18:27:30 +0000

    Set the version to 1.7.5, documented the changes made so far.

--- src/params.h
+++ src/params.h
@@ -15,7 +15,7 @@
 /*
  * John's version number.
  */
-#define JOHN_VERSION			"1.7.4.2"
+#define JOHN_VERSION			"1.7.5"
 
 /*
  * Notes to packagers of John for *BSD "ports", Linux distributions, etc.:
