8e4b908c897be63886fa4d1f46657c8e820bc628
Solar Designer <solar@openwall.com>
2010-06-09 21:58:58 +0000

    Defined several kinds of bitslice DES settings (which may be easily enabled by editing this file) for the use of C compiler intrinsics.
    One of these new sets of settings (for simple use of SSE2) is actually enabled
    by default when compiling with gcc 4.5 or newer.  gcc 4.5.0 happens to produce
    code that runs a little bit faster at least on Core i7 than code from our
    x86-64.S file does.

--- src/x86-64.h
+++ src/x86-64.h
@@ -1,6 +1,6 @@
 /*
  * This file is part of John the Ripper password cracker,
- * Copyright (c) 2003,2006,2008 by Solar Designer
+ * Copyright (c) 2003,2006,2008,2010 by Solar Designer
  */
 
 /*
@@ -32,11 +32,32 @@
 #define DES_SCALE			1
 #define DES_EXTB			1
 #define DES_COPY			0
-#define DES_BS_ASM			1
-#define DES_BS				2
+#if defined(__SSE2__) && \
+    ((__GNUC__ == 4 && __GNUC_MINOR__ >= 5) || __GNUC__ > 4)
+#define DES_BS_ASM			0
+#if 1
 #define DES_BS_VECTOR			2
-#define DES_BS_EXPAND			1
 #define DES_BS_ALGORITHM_NAME		"128/128 BS SSE2-16"
+#elif 0
+#define DES_BS_VECTOR			3
+#define DES_BS_VECTOR_SIZE		4
+#define DES_BS_ALGORITHM_NAME		"128/128 BS SSE2-16 + 64/64 BS MMX"
+#elif 0
+#define DES_BS_NO_MMX
+#define DES_BS_VECTOR			3
+#define DES_BS_VECTOR_SIZE		4
+#define DES_BS_ALGORITHM_NAME		"128/128 BS SSE2-16 + 64/64 BS"
+#else
+#define DES_BS_VECTOR			4
+#define DES_BS_ALGORITHM_NAME		"128/128 BS SSE2-16 + 64/64 BS MMX + 64/64 BS"
+#endif
+#else
+#define DES_BS_ASM			1
+#define DES_BS_VECTOR			2
+#define DES_BS_ALGORITHM_NAME		"128/128 BS SSE2-16"
+#endif
+#define DES_BS				1
+#define DES_BS_EXPAND			1
 
 #define MD5_ASM				0
 #define MD5_X2				1
