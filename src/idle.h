a3f3e4be21984c785ece241fcbd68af635409ad8
solar <solar>
2011-02-27 06:25:58 +0000

    Added idle_requested().  Have cracker.c pass the "format" to idle_init().

--- src/idle.h
+++ src/idle.h
@@ -1,6 +1,6 @@
 /*
  * This file is part of John the Ripper password cracker,
- * Copyright (c) 1996-2001 by Solar Designer
+ * Copyright (c) 1996-2001,2011 by Solar Designer
  */
 
 /*
@@ -10,15 +10,22 @@
 #ifndef _JOHN_IDLE_H
 #define _JOHN_IDLE_H
 
-/*
- * Sets this process to idle priority, if supported and enabled in the
- * configuration file.
- */
-extern void idle_init(void);
+#include "formats.h"
 
 /*
- * If the idle_init() call was unable to set the idle priority, this can
- * still be used to yield a timeslice if there's something else to do.
+ * Returns non-zero if idle priority is requested in the configuration file and
+ * is actually to be enabled for the supplied "format".
+ */
+extern int idle_requested(struct fmt_main *format);
+
+/*
+ * Sets this process to idle priority if requested and supported.
+ */
+extern void idle_init(struct fmt_main *format);
+
+/*
+ * If the idle_init() call was unable to "fully" set the idle priority, yet it
+ * was requested, this will yield a timeslice if there's something else to do.
  */
 extern void idle_yield(void);
 
