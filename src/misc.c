338cf2c734cd99253b77e19aac523751e6f9b42f
solar <solar>
2011-02-27 13:23:23 +0000

    Reimplemented strnzcpy() via strncat().

--- src/misc.c
+++ src/misc.c
@@ -1,6 +1,6 @@
 /*
  * This file is part of John the Ripper password cracker,
- * Copyright (c) 1996-99,2003 by Solar Designer
+ * Copyright (c) 1996-99,2003,2011 by Solar Designer
  */
 
 #include <stdio.h>
@@ -95,13 +95,10 @@ char *strnfcpy(char *dst, char *src, int size)
 
 char *strnzcpy(char *dst, char *src, int size)
 {
-	char *dptr = dst, *sptr = src;
-	int count = size;
-
-	if (count)
-		while (--count)
-			if (!(*dptr++ = *sptr++)) break;
-	*dptr = 0;
+	if (size > 0) {
+		*dst = 0;
+		strncat(dst, src, size - 1);
+	}
 
 	return dst;
 }
