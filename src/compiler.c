57b7d232915cbf694c46aec74cfb361ab9085a77
solar <solar>
2011-05-23 07:56:51 +0000

    Added push_mem_mem_mem (a reasonable optimization based on instruction streams actually executed for real-world tasks).

--- src/compiler.c
+++ src/compiler.c
@@ -110,7 +110,7 @@ struct c_op {
 static struct c_op c_ops[];
 #else
 #ifdef PRINT_INSNS
-static struct c_op c_ops[49];
+static struct c_op c_ops[50];
 #else
 static struct c_op c_ops[38];
 #endif
@@ -358,6 +358,7 @@ static void (*c_op_push_imm_imm)(void);
 static void (*c_op_push_imm_mem)(void);
 static void (*c_op_push_mem_imm)(void);
 static void (*c_op_push_mem_mem)(void);
+static void (*c_op_push_mem_mem_mem)(void);
 
 static void (*c_op_assign)(void);
 static void (*c_op_assign_pop)(void);
@@ -380,6 +381,13 @@ static void (*c_push
 
 		if (c_pass) {
 			(c_code_ptr - 2)->op = last;
+			*c_code_ptr = *value;
+		}
+		c_code_ptr++;
+	} else if (last == c_op_push_mem_mem && op == c_op_push_mem) {
+		last = c_op_push_mem_mem_mem;
+		if (c_pass) {
+			(c_code_ptr - 3)->op = last;
 			*c_code_ptr = *value;
 		}
 		c_code_ptr++;
@@ -915,6 +923,7 @@ static void c_direct(union c_insn *addr)
 		c_op_push_imm_mem = &&op_push_imm_mem;
 		c_op_push_mem_imm = &&op_push_mem_imm;
 		c_op_push_mem_mem = &&op_push_mem_mem;
+		c_op_push_mem_mem_mem = &&op_push_mem_mem_mem;
 
 		c_op_assign = &&op_assign;
 		c_op_assign_pop = &&op_assign_pop;
@@ -991,6 +1000,15 @@ op_push_mem_mem:
 	imm = *((sp + 3)->mem = (pc + 1)->mem);
 	pc += 3;
 	sp += 4;
+	goto *(pc - 1)->op;
+
+op_push_mem_mem_mem:
+	(sp - 2)->imm = imm;
+	sp->imm = *((sp + 1)->mem = pc->mem);
+	(sp + 2)->imm = *((sp + 3)->mem = (pc + 1)->mem);
+	imm = *((sp + 5)->mem = (pc + 2)->mem);
+	pc += 4;
+	sp += 6;
 	goto *(pc - 1)->op;
 
 op_index:
@@ -1236,6 +1254,17 @@ static void c_f_op_push_mem_mem(void)
 	c_sp += 4;
 }
 
+static void c_f_op_push_mem_mem_mem(void)
+{
+	c_sp->imm = *c_pc->mem;
+	(c_sp + 1)->mem = (c_pc++)->mem;
+	(c_sp + 2)->imm = *c_pc->mem;
+	(c_sp + 3)->mem = (c_pc++)->mem;
+	(c_sp + 4)->imm = *c_pc->mem;
+	(c_sp + 5)->mem = (c_pc++)->mem;
+	c_sp += 6;
+}
+
 static void c_op_index(void)
 {
 	c_sp -= 2;
@@ -1456,6 +1485,7 @@ static void (*c_op_push_imm_imm)(void) = c_f_op_push_imm_imm;
 static void (*c_op_push_imm_mem)(void) = c_f_op_push_imm_mem;
 static void (*c_op_push_mem_imm)(void) = c_f_op_push_mem_imm;
 static void (*c_op_push_mem_mem)(void) = c_f_op_push_mem_mem;
+static void (*c_op_push_mem_mem_mem)(void) = c_f_op_push_mem_mem_mem;
 
 static void (*c_op_assign)(void) = c_f_op_assign;
 static void (*c_op_assign_pop)(void) = c_f_op_assign_pop;
@@ -1509,6 +1539,7 @@ static struct c_op c_ops[] = {
 	{0, 0, 0, "push_imm_mem", c_f_op_push_imm_mem},
 	{0, 0, 0, "push_mem_imm", c_f_op_push_mem_imm},
 	{0, 0, 0, "push_mem_mem", c_f_op_push_mem_mem},
+	{0, 0, 0, "push_mem_mem_mem", c_f_op_push_mem_mem_mem},
 	{0, 0, 0, "assign_pop", c_f_op_assign_pop},
 	{-1}
 #else
