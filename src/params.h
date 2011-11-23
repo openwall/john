b94ceba1e48d86dcb4f2053bc17940b4269c717e
Solar Designer <solar@openwall.com>
2011-11-23 06:55:32 +0000

    1.7.8.11

--- src/params.h
+++ src/params.h
@@ -17,7 +17,7 @@
 /*
  * John's version number.
  */
-#define JOHN_VERSION			"1.7.8.10"
+#define JOHN_VERSION			"1.7.8.11"
 
 /*
  * Notes to packagers of John for *BSD "ports", Linux distributions, etc.:
