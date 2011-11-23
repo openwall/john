7894cc8e3052793020db34f4648182a890d8ba7e
solar <solar>
2011-11-23 11:08:46 +0000

    1.7.9

--- src/params.h
+++ src/params.h
@@ -17,7 +17,7 @@
 /*
  * John's version number.
  */
-#define JOHN_VERSION			"1.7.8.11"
+#define JOHN_VERSION			"1.7.9"
 
 /*
  * Notes to packagers of John for *BSD "ports", Linux distributions, etc.:
