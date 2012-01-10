d674b016430d28650c22d19ed2ef906839ca9e8a
magnum <john.magnum@hushmail.com>
2012-01-10 12:59:09 +0100

    HDAA: OMP support

--- src/HDAA_fmt_plug.c
+++ src/HDAA_fmt_plug.c
@@ -2,6 +2,7 @@
  *
  * Written by Romain Raboin - romain.raboin at gmail.com
  *
+ * OMP support added by magnum 2012, no rights reserved
  */
 
 #include <string.h>
@@ -10,7 +11,7 @@
 #define snprintf _snprintf
 #endif
 
-#ifdef	__MMX__
+#ifdef __MMX__
 #include <mmintrin.h>
 #endif
 
@@ -24,7 +25,8 @@
 
 #define FORMAT_LABEL			"hdaa"
 #define FORMAT_NAME			"HTTP Digest access authentication"
-#define ALGORITHM_NAME			"HDAA-MD5"
+
+#define ALGORITHM_NAME			"32/" ARCH_BITS_STR
 
 #define BENCHMARK_COMMENT		""
 #define BENCHMARK_LENGTH		0
@@ -34,10 +36,15 @@
 
 #define BINARY_SIZE			16
 
+#if defined(_OPENMP)
+#include <omp.h>
+#define OMP_SCALE			64
+#endif
+
 #define MIN_KEYS_PER_CRYPT		1
 #define MAX_KEYS_PER_CRYPT		1
 
-#define	SEPARATOR			'$'
+#define SEPARATOR			'$'
 
 #define MAGIC				"$response$"
 #define SIZE_TAB			12
@@ -46,15 +53,14 @@
 
 typedef struct
 {
-	char 	**request;
-	char	h3tmp[HTMP + 1];
-	char	h1tmp[HTMP + 1];
-	size_t	h3tmplen;
+	char	**request;
 	size_t	h1tmplen;
-}      		reqinfo_t;
+	size_t	h3tmplen;
+	char	h1tmp[HTMP];
+	char	h3tmp[HTMP];
+} reqinfo_t;
 
 #define SALT_SIZE			sizeof(reqinfo_t)
-
 
 /*
   digest authentication scheme :
@@ -77,35 +83,37 @@ enum e_req {
 };
 
 /* response:user:realm:method:uri:nonce:nonceCount:ClientNonce:qop */
-static struct fmt_tests hdaa_tests[] = {
+static struct fmt_tests tests[] = {
 	{"$response$679066476e67b5c7c4e88f04be567f8b$user$myrealm$GET$/$8c12bd8f728afe56d45a0ce846b70e5a$00000001$4b61913cec32e2c9$auth", "nocode"},
 	{"$response$faa6cb7d676e5b7c17fcbf966436aa0c$moi$myrealm$GET$/$af32592775d27b1cd06356b3a0db9ddf$00000001$8e1d49754a25aea7$auth", "kikou"},
 	{NULL}
 };
 
-
-static MD5_CTX ctx;
-
 /* used by set_key */
-static char saved_key[PLAINTEXT_LENGTH + 1];
+static char (*saved_plain)[PLAINTEXT_LENGTH + 1];
 
 /* store the ciphertext for value currently being tested */
-static unsigned char crypt_key[BINARY_SIZE + 1];
+static unsigned char (*crypt_key)[BINARY_SIZE + 1];
 
 /* Store information about the request ()*/
 static reqinfo_t *rinfo = NULL;
 
-/* Store the hash convertion (binary to ascii)*/
-#ifdef __MMX__
-static __m64 conv[4 + 1];
-#else
-static uint32_t conv[(CIPHERTEXT_LENGTH / 4) + 1];
-#endif
-
-static int 	hdaa_valid(char *ciphertext, struct fmt_main *pFmt)
+static void init(struct fmt_main *pFmt)
 {
-	int	nb = 0;
-	int	i;
+#if defined (_OPENMP)
+	int omp_t = omp_get_max_threads();
+	pFmt->params.min_keys_per_crypt = omp_t;
+	omp_t *= OMP_SCALE;
+	pFmt->params.max_keys_per_crypt = omp_t;
+#endif
+	crypt_key = mem_calloc_tiny(sizeof(*crypt_key) * pFmt->params.max_keys_per_crypt, MEM_ALIGN_WORD);
+	saved_plain = mem_calloc_tiny(sizeof(*saved_plain) * pFmt->params.max_keys_per_crypt, MEM_ALIGN_WORD);
+}
+
+static int valid(char *ciphertext, struct fmt_main *pFmt)
+{
+	int nb = 0;
+	int i;
 
 	if (strncmp(ciphertext, MAGIC, strlen(MAGIC)) != 0)
 		return 0;
@@ -119,27 +127,37 @@ static int 	hdaa_valid(char *ciphertext, struct fmt_main *pFmt)
 	return 0;
 }
 
-static void	hdaa_set_salt(void *salt)
+static void set_salt(void *salt)
 {
 	rinfo = salt;
 }
 
-static void	hdaa_set_key(char *key, int index)
+static void set_key(char *key, int index)
 {
-	strnzcpy(saved_key, key, PLAINTEXT_LENGTH + 1);
+	strnzcpy(saved_plain[index], key, PLAINTEXT_LENGTH + 1);
 }
 
-static char	*hdaa_get_key(int index)
+static char *get_key(int index)
 {
-	return saved_key;
+	return saved_plain[index];
 }
 
-static int	hdaa_cmp_all(void *binary, int index)
+static int cmp_all(void *binary, int count)
 {
-	return !(memcmp((char *)binary, (char *)crypt_key, BINARY_SIZE));
+	int index;
+
+	for (index = 0; index < count; index++)
+		if (!(memcmp(binary, crypt_key[index], BINARY_SIZE)))
+			return 1;
+	return 0;
 }
 
-static int	hdaa_cmp_exact(char *source, int count)
+static int cmp_one(void *binary, int index)
+{
+	return !(memcmp(binary, crypt_key[index], BINARY_SIZE));
+}
+
+static int cmp_exact(char *source, int count)
 {
 	return 1;
 }
@@ -149,16 +167,16 @@ static int	hdaa_cmp_exact(char *source, int count)
 
 #ifdef __MMX__
 
-static void	bin2ascii(__m64 src[2])
+static void bin2ascii(__m64 *conv, __m64 *src)
 {
-	unsigned int	i = 0;
+	unsigned int i = 0;
 
 	while (i != 4) {
-		__m64	l;
-		__m64	r;
-		__m64	t;
-		__m64	u;
-		__m64	v;
+		__m64 l;
+		__m64 r;
+		__m64 t;
+		__m64 u;
+		__m64 v;
 
 		/* 32 bits to 64 bits */
 		t = _mm_set1_pi32(0x0f0f0f0f);
@@ -198,11 +216,11 @@ static void	bin2ascii(__m64 src[2])
 
 #else
 
-static void		bin2ascii(unsigned char *src)
+static void bin2ascii(uint32_t *conv, unsigned char *src)
 {
-	unsigned int	i;
-	unsigned int	j = 0;
-	uint32_t	t = 0;
+	unsigned int i;
+	unsigned int j = 0;
+	uint32_t t = 0;
 
 	for (i = 0; i < BINARY_SIZE; i += 2) {
 #if (ARCH_LITTLE_ENDIAN == 0)
@@ -230,40 +248,60 @@ static void		bin2ascii(unsigned char *src)
 
 #endif /* MMX */
 
-static void		hdaa_crypt_all(int count)
+static void crypt_all(int count)
 {
-	int		len;
-	char	*h1tmp, *h3tmp;
-	size_t	tmp;
-#ifdef __MMX__
-	__m64		h1[2];
+	int index = 0;
+#ifdef _OPENMP
+#pragma omp parallel for
+	for (index = 0; index < count; index++)
+#endif
+	{
+		MD5_CTX ctx;
+		int len;
+#ifdef _OPENMP
+		char h3tmp[HTMP];
+		char h1tmp[HTMP];
 #else
-	static unsigned char *h1;
-	if (!h1) h1 = mem_alloc_tiny(BINARY_SIZE, MEM_ALIGN_WORD);
+		char *h3tmp;
+		char *h1tmp;
+#endif
+		size_t tmp;
+#ifdef __MMX__
+		__m64 h1[BINARY_SIZE / sizeof(__m64)];
+		__m64 conv[CIPHERTEXT_LENGTH / sizeof(__m64) + 1];
+#else
+		unsigned int h1[BINARY_SIZE / sizeof(unsigned int)];
+		uint32_t conv[(CIPHERTEXT_LENGTH / sizeof(uint32_t)) + 1];
 #endif
 
-	h3tmp = rinfo->h3tmp;
-	h1tmp = rinfo->h1tmp;
-	tmp = rinfo->h1tmplen;
-	len = strlen(saved_key);
-	memcpy(&h1tmp[tmp], saved_key, len + 1);
+		tmp = rinfo->h1tmplen;
+		len = strlen(saved_plain[index]);
+#ifdef _OPENMP
+		memcpy(h1tmp, rinfo->h1tmp, tmp);
+		memcpy(h3tmp + CIPHERTEXT_LENGTH, rinfo->h3tmp + CIPHERTEXT_LENGTH, rinfo->h3tmplen - CIPHERTEXT_LENGTH);
+#else
+		h3tmp = rinfo->h3tmp;
+		h1tmp = rinfo->h1tmp;
+#endif
+		memcpy(&h1tmp[tmp], saved_plain[index], len);
 
-	MD5_Init(&ctx);
-	MD5_Update(&ctx, h1tmp, len + tmp);
-	MD5_Final((unsigned char*)h1, &ctx);
-	bin2ascii(h1);
+		MD5_Init(&ctx);
+		MD5_Update(&ctx, h1tmp, len + tmp);
+		MD5_Final((unsigned char*)h1, &ctx);
+		bin2ascii(conv, h1);
 
-	memcpy(h3tmp, conv, CIPHERTEXT_LENGTH);
-	MD5_Init(&ctx);
-	MD5_Update(&ctx, h3tmp, rinfo->h3tmplen);
-	MD5_Final(crypt_key, &ctx);
+		memcpy(h3tmp, conv, CIPHERTEXT_LENGTH);
+		MD5_Init(&ctx);
+		MD5_Update(&ctx, h3tmp, rinfo->h3tmplen);
+		MD5_Final(crypt_key[index], &ctx);
+	}
 }
 
-static char		*mystrndup(const char *s, size_t n)
+static char *mystrndup(const char *s, size_t n)
 {
-	size_t	tmp;
-	size_t	size;
-	char	*ret;
+	size_t tmp;
+	size_t size;
+	char *ret;
 
 	for (tmp = 0; s[tmp] != 0 && tmp <= n; tmp++);
 	size = n;
@@ -276,32 +314,33 @@ static char		*mystrndup(const char *s, size_t n)
 	return ret;
 }
 
-static size_t		reqlen(char *str)
+static size_t reqlen(char *str)
 {
-	size_t	len;
+	size_t len;
 
 	for (len = 0; str[len] != 0 && str[len] != SEPARATOR; len++);
 	return len;
 }
 
-static void			*hdaa_salt(char *ciphertext)
+static void *salt(char *ciphertext)
 {
-
-	int		nb;
-	int		i;
-	char		**request;
-	char		*str;
-	reqinfo_t	*r;
+	int nb;
+	int i;
+	char **request;
+	char *str;
+	reqinfo_t *r;
 #ifdef __MMX__
-	__m64		h2[2];
+	__m64 h2[BINARY_SIZE / sizeof(__m64)];
+	__m64 conv[CIPHERTEXT_LENGTH / sizeof(__m64) + 1];
 #else
-	static unsigned char	*h2;
-	if (!h2) h2 = mem_alloc_tiny(BINARY_SIZE, MEM_ALIGN_WORD);
+	unsigned int h2[BINARY_SIZE / sizeof(unsigned int)];
+	uint32_t conv[(CIPHERTEXT_LENGTH / sizeof(uint32_t)) + 1];
 #endif
+	MD5_CTX ctx;
+
 	/* parse the password string */
-	request = mem_alloc_tiny(sizeof(char *) * SIZE_TAB,MEM_ALIGN_WORD);
-	r = mem_alloc_tiny(sizeof(*r),MEM_ALIGN_WORD);
-	memset(r, 0, sizeof(*r));
+	request = mem_alloc_tiny(sizeof(char*) * SIZE_TAB, MEM_ALIGN_WORD);
+	r = mem_calloc_tiny(sizeof(*r), MEM_ALIGN_WORD);
 	for (nb = 0, i = 1; ciphertext[i] != 0; i++) {
 		if (ciphertext[i] == SEPARATOR) {
 			i++;
@@ -315,10 +354,10 @@ static void			*hdaa_salt(char *ciphertext)
 	sprintf(str, "%s:%s", request[R_METHOD], request[R_URI]);
 	MD5_Init(&ctx);
 	MD5_Update(&ctx, str, strlen(str));
-	MD5_Final((unsigned char *)h2, &ctx);
+	MD5_Final((unsigned char*)h2, &ctx);
 
-	memset(conv, 0, sizeof(conv));
-	bin2ascii(h2);
+	memset(conv, 0, CIPHERTEXT_LENGTH + 1);
+	bin2ascii(conv, h2);
 	MEM_FREE(str);
 
 	/* create a part of h1 (h1tmp = request:realm:)*/
@@ -326,29 +365,45 @@ static void			*hdaa_salt(char *ciphertext)
 
 	/* create a part of h3 (h3tmp = nonce:noncecount:clientnonce:qop:h2)*/
 	snprintf(&r->h3tmp[CIPHERTEXT_LENGTH], HTMP - CIPHERTEXT_LENGTH, ":%s:%s:%s:%s:%s",
-		request[R_NONCE], request[R_NONCECOUNT], request[R_CLIENTNONCE],
-		request[R_QOP], (char*)conv);
+	         request[R_NONCE], request[R_NONCECOUNT], request[R_CLIENTNONCE],
+	         request[R_QOP], (char*)conv);
+
 	r->request = request;
 	r->h1tmplen = strlen(r->h1tmp);
 	r->h3tmplen = strlen(&r->h3tmp[CIPHERTEXT_LENGTH]) + CIPHERTEXT_LENGTH;
+
 	return r;
 }
 
-/* convert response in binary form */
-static void		*hdaa_binary(char *ciphertext)
+/* convert response to binary form */
+static void *binary(char *ciphertext)
 {
-	static char	*realcipher;
-	int		i;
-
-	if (!realcipher) realcipher = mem_alloc_tiny(BINARY_SIZE, MEM_ALIGN_WORD);
+	static unsigned int realcipher[BINARY_SIZE / sizeof(int)];
+	int i;
 
 	ciphertext += 10;
 	for (i = 0; i < BINARY_SIZE; i++) {
-		realcipher[i] = atoi16[ARCH_INDEX(ciphertext[i * 2])] * 16 +
+		((unsigned char*)realcipher)[i] = atoi16[ARCH_INDEX(ciphertext[i * 2])] * 16 +
 			atoi16[ARCH_INDEX(ciphertext[i * 2 + 1])];
 	}
-	return (void *) realcipher;
+	return (void*) realcipher;
 }
+
+static int binary_hash_0(void *binary) { return *(ARCH_WORD_32 *)binary & 0xf; }
+static int binary_hash_1(void *binary) { return *(ARCH_WORD_32 *)binary & 0xff; }
+static int binary_hash_2(void *binary) { return *(ARCH_WORD_32 *)binary & 0xfff; }
+static int binary_hash_3(void *binary) { return *(ARCH_WORD_32 *)binary & 0xffff; }
+static int binary_hash_4(void *binary) { return *(ARCH_WORD_32 *)binary & 0xfffff; }
+static int binary_hash_5(void *binary) { return *(ARCH_WORD_32 *)binary & 0xffffff; }
+static int binary_hash_6(void *binary) { return *(ARCH_WORD_32 *)binary & 0x7ffffff; }
+
+static int get_hash_0(int index) { return *(ARCH_WORD_32*)&crypt_key[index] & 0xf; }
+static int get_hash_1(int index) { return *(ARCH_WORD_32*)&crypt_key[index] & 0xff; }
+static int get_hash_2(int index) { return *(ARCH_WORD_32*)&crypt_key[index] & 0xfff; }
+static int get_hash_3(int index) { return *(ARCH_WORD_32*)&crypt_key[index] & 0xffff; }
+static int get_hash_4(int index) { return *(ARCH_WORD_32*)&crypt_key[index] & 0xfffff; }
+static int get_hash_5(int index) { return *(ARCH_WORD_32*)&crypt_key[index] & 0xffffff; }
+static int get_hash_6(int index) { return *(ARCH_WORD_32*)&crypt_key[index] & 0x7ffffff; }
 
 struct fmt_main fmt_HDAA = {
 	{
@@ -362,37 +417,41 @@ struct fmt_main fmt_HDAA = {
 		SALT_SIZE,
 		MIN_KEYS_PER_CRYPT,
 		MAX_KEYS_PER_CRYPT,
-		FMT_CASE | FMT_8_BIT,
-		hdaa_tests
+		FMT_OMP | FMT_CASE | FMT_8_BIT,
+		tests
 	}, {
-		fmt_default_init,
+		init,
 		fmt_default_prepare,
-		hdaa_valid,
+		valid,
 		fmt_default_split,
-		hdaa_binary,
-		hdaa_salt,
+		binary,
+		salt,
 		{
-			fmt_default_binary_hash,
-			fmt_default_binary_hash,
-			fmt_default_binary_hash,
-			fmt_default_binary_hash,
-			fmt_default_binary_hash
+			binary_hash_0,
+			binary_hash_1,
+			binary_hash_2,
+			binary_hash_3,
+			binary_hash_4,
+			binary_hash_5,
+			binary_hash_6
 		},
 		fmt_default_salt_hash,
-		hdaa_set_salt,
-		hdaa_set_key,
-		hdaa_get_key,
+		set_salt,
+		set_key,
+		get_key,
 		fmt_default_clear_keys,
-		hdaa_crypt_all,
+		crypt_all,
 		{
-			fmt_default_get_hash,
-			fmt_default_get_hash,
-			fmt_default_get_hash,
-			fmt_default_get_hash,
-			fmt_default_get_hash
+			get_hash_0,
+			get_hash_1,
+			get_hash_2,
+			get_hash_3,
+			get_hash_4,
+			get_hash_5,
+			get_hash_6
 		},
-		hdaa_cmp_all,
-		hdaa_cmp_all,
-		hdaa_cmp_exact
+		cmp_all,
+		cmp_one,
+		cmp_exact
 	}
 };
