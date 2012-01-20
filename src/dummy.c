04cb1a945b14e2bf52b082d59be9cc22500b877a
Solar Designer <solar@openwall.com>
2012-01-20 23:56:41 +0000

    Use the MAYBE_INLINE macro.

--- src/dummy.c
+++ src/dummy.c
@@ -88,14 +88,7 @@ static char *decode(char *ciphertext)
 	return out;
 }
 
-#ifdef __GNUC__
-#if __GNUC__ > 3 || (__GNUC__ == 3 && __GNUC_MINOR__ >= 1)
-__attribute__((always_inline))
-#else
-__inline__
-#endif
-#endif
-static ARCH_WORD_32 string_hash(char *s)
+static MAYBE_INLINE ARCH_WORD_32 string_hash(char *s)
 {
 	ARCH_WORD_32 hash, extra;
 	char *p;
