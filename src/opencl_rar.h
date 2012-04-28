8092506c1a1a6e2d519d269b4256ab5f21af336b
magnum <john.magnum@hushmail.com>
2012-04-28 14:27:46 +0200

    Include a vectorized kernel in same file as non-vectorized. It is not used yet. Add an opencl-rar.h file.

---
+++ src/opencl_rar.h
@@ -0,0 +1,18 @@
+#ifndef _OPENCL_RAR_H 
+#define _OPENCL_RAR_H
+
+/* NOTE if you alter this file, touch rar_fmt.c too, or make clean */
+
+/* Note that enabling debugging also enables
+   lots of self tests that skews benchmarks */
+//#define DEBUG
+
+#define PLAINTEXT_LENGTH	16
+#define FIXED_LEN		4
+
+#define LWS_CONFIG		"rar_LWS"
+#define KPC_CONFIG		"rar_KPC"
+
+#define ROUNDS			0x40000
+
+#endif /* _OPENCL_RAR_H */
