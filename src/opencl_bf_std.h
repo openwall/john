f4ceb7e189e637462feb78849ebc04c8a4ee3fa7
Sayantan <std2048@gmail.com>
2012-05-29 23:16:55 +0530

    blowfish-opencl:performance update,7970 bug fix

--- src/opencl_bf_std.h
+++ src/opencl_bf_std.h
@@ -3,6 +3,25 @@
  * and it is hereby released to the general public under the following terms:
  * Redistribution and use in source and binary forms, with or without modification, are permitted.
  */
+
+/*NOTE: Changing the following parameters to improve performance for different GPUs. WAVEFRONT_SIZE , NUM_CHANNELS and OFFSET here must be same as that in 'bf_kernel.cl'. 
+ *      Threrefore if the above three parameters are changed here,it must be also changed in the kernel file.
+ *  
+ * WORK_GROUP_SIZE : Increase or decrease in multiples of 64
+ * WAVEFRONT_SIZE  : For Nvidia GPUs it may be also called warp size.
+ *                   Wavfront size is 64 for AMD GPUs.
+ *                   Warp size is 32 for Nvidia GPUs.
+ *                   Set it to integral multiple of 32 for Nv GPUs and integral multiple of 64 for AMD GPUs.
+ *                   Size of 64 works well for both AMD and Nv GPUs.
+ * NUM_CHANNELS    : Number of memory channels available for the device. eg  for GTX570 set it 10 +/- 1
+ *                                                                           for HD4890 set it  8 +/- 1
+ *									     for hd7970 set it 12 +/- 1										    
+ * 	             Increasing or decreasing this parameter by 1 may improve performance.
+ * MULTIPLIER      : Use this to increase BF_N		
+ * OFFSET          : This is to fine tune performance.Set it between 0 to WAVEFRONT_SIZE inclusive 0.
+ * 
+ */
+
 #ifndef _OPENCL_BF_STD_H
 #define _OPENCL_BF_STD_H
 
@@ -26,7 +45,19 @@ typedef struct {
  */
 typedef BF_word BF_binary[6];
 
-#define BF_N				1024
+#define WORK_GROUP_SIZE                  64
+
+#define WAVEFRONT_SIZE                   64
+
+#define NUM_CHANNELS                     13
+
+#define OFFSET                           0
+
+#define MULTIPLIER                       4
+
+#define CHANNEL_INTERLEAVE              WAVEFRONT_SIZE*NUM_CHANNELS
+
+#define BF_N				CHANNEL_INTERLEAVE*MULTIPLIER
 
 #define MAX_DEVICES_PER_PLATFORM         8
 
