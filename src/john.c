86766cd55bf998119161a47cdb1fe9331cbaaf8c
Frank Dittrich <frank_dittrich@hotmail.com>
2012-06-23 13:26:15 +0200

    --list=format-details: add number of test cases to output
    Furthermore, clanup unnecessary code for --list=format-details
    (which had been copied from the --list=formats code)
    
    Added --list=formats and --list=format-details description to
    doc/OPTIONS

--- src/john.c
+++ src/john.c
@@ -774,23 +774,27 @@ static void john_init(char *name, int argc, char **argv)
 
 		i = 0;
 		format = fmt_list;
-		while ((format = format->next))
-			i++;
-
-		i = 0;
-		format = fmt_list;
 		do {
+			int ntests;
+			struct fmt_tests *current;
+			ntests = 0;
+
+			if(format->params.tests) {
+				while (format->params.tests[ntests++].ciphertext);
+				ntests--;
+			}
 			/*
 			 * FIXME: Are other parameters more important?
 			 *        Should I use hexadecimal output
 			 *        for the FMT_flags?
 			 */
-			printf("%s\t%d\t%d\t%d\t%d\t%s\t%s\n",
+			printf("%s\t%d\t%d\t%d\t%d\t%d\t%s\t%s\n",
 			       format->params.label,
 			       format->params.plaintext_length,
 			       format->params.min_keys_per_crypt,
 			       format->params.max_keys_per_crypt,
 			       format->params.flags,
+			       ntests,
 			       format->params.algorithm_name,
 			       format->params.format_name);
 		} while ((format = format->next));
