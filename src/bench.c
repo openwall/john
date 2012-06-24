openwall/john

commit aab646b42afe93ca909ba1a99bc0e2a1b027a79d

Use first two salts when benchmarking instead of top one.

src/bench.c
@@ -155,12 +155,14 @@ char *benchmark_format(struct fmt_main *format, int salts,
 			prepared = format->methods.prepare(current->flds, format);
 			ciphertext = format->methods.split(prepared, 0);
 			salt = format->methods.salt(ciphertext);
+			++current;
 		}
 		else
 			salt = two_salts[0];
 
 		memcpy(two_salts[index], salt, format->params.salt_size);
 	}
+	format->methods.set_salt(two_salts[0]);
 
 	if (format->params.benchmark_length > 0) {
 		cond = (salts == 1) ? 1 : -1;

commit 94751b830c2a83b873cfc4fbdd8f2f4be60b1e48

Fix breakage caused by commit aab646b42afe93ca909b

src/bench.c
@@ -150,12 +150,13 @@ char *benchmark_format(struct fmt_main *format, int salts,
 		two_salts[index] = mem_alloc(format->params.salt_size);
 
 		if ((ciphertext = format->params.tests[index].ciphertext)) {
+			struct fmt_tests *current =
+			    &format->params.tests[index];
 			char *prepared;
 			current->flds[1] = current->ciphertext;
 			prepared = format->methods.prepare(current->flds, format);
 			ciphertext = format->methods.split(prepared, 0);
 			salt = format->methods.salt(ciphertext);
-			++current;
 		}
 		else
 			salt = two_salts[0];
@@ -427,16 +428,19 @@ int benchmark_all(void)
 #endif /* _OPENMP */
 #endif /* HAVE_MPI */
 		switch (format->params.benchmark_length) {
+		case 0:
+		case -1000:
+			if (format->params.tests[1].ciphertext) {
+				msg_m = "Many salts";
+				msg_1 = "Only one salt";
+				break;
+			}
+			/* fall through */
+
 		case -1:
 		case -1001:
 			msg_m = "Raw";
 			msg_1 = NULL;
-			break;
-
-		case 0:
-		case -1000:
-			msg_m = "Many salts";
-			msg_1 = "Only one salt";
 			break;
 
 		default: