d76dfddadc1f9baa2a9a0308debad797d0155ceb
Frank Dittrich <frank_dittrich@hotmail.com>
2012-06-26 15:21:31 +0200

    Enhanced compiler version info for --list=build-info

--- src/john.c
+++ src/john.c
@@ -631,12 +631,18 @@ static void john_init(char *name, int argc, char **argv)
 			printf("CHARSET_MAX: %d (0x%02x)\n", CHARSET_MAX,
 			       CHARSET_MAX);
 			printf("CHARSET_LENGTH: %d\n", CHARSET_LENGTH);
+#ifdef __VERSION__
+		printf("Compiler version: %s\n", __VERSION__);
+#endif
 #ifdef __GNUC__
 			printf("gcc version: %d.%d.%d\n", __GNUC__,
 			       __GNUC_MINOR__, __GNUC_PATCHLEVEL__);
 #endif
 #ifdef __ICC
 			printf("icc version: %d\n", __ICC);
+#endif
+#ifdef __clang_version__
+			printf("clang version: %s\n", __clang_version__);
 #endif
 			exit(0);
 		}
