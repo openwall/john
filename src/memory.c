5b1056935ca5112b1091c434088db888e0e4179b
JimF <jfoug@cox.net>
2012-07-08 16:32:19 +0200

    JtR-bleed-align-macro-fix-1.diff

--- src/memory.c
+++ src/memory.c
@@ -226,9 +226,7 @@ void alter_endianity_w(void *_x, unsigned int count) {
 	}
 #else
 	unsigned char *cpX, c;
-	long l;
-	l = (long)x;
-	if ( (l & (sizeof(ARCH_WORD_32)-1)) == 0) {
+	if (isaligned(x,sizeof(ARCH_WORD_32))) {
 		// we are in alignment.
 		while (++i < count) {
 			x[i] = JOHNSWAP(x[i]);
