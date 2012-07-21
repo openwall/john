c1f2c82031c84b26d9574b4c3e93f0624bec6da0
Dhiru Kholia <dhiru.kholia@gmail.com>
2012-07-21 20:29:16 +0530

    Update FORMAT_NAME in ODF format

--- src/odf_fmt_plug.c
+++ src/odf_fmt_plug.c
@@ -27,7 +27,7 @@
 #endif
 
 #define FORMAT_LABEL		"odf"
-#define FORMAT_NAME		"ODF SHA-1 Blowfish"
+#define FORMAT_NAME		"ODF SHA-1 Blowfish / SHA-256 AES"
 #define ALGORITHM_NAME		"32/" ARCH_BITS_STR
 #define BENCHMARK_COMMENT	""
 #define BENCHMARK_LENGTH	-1
