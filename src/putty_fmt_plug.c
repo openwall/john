c55bf8e27b0acb470fe3d72ebbea08cd34a90064
Dhiru Kholia <dhiru.kholia@gmail.com>
2012-08-07 22:20:29 +0530

    Use our faster HMAC-SHA1 implementation

--- src/putty_fmt_plug.c
+++ src/putty_fmt_plug.c
@@ -17,7 +17,8 @@
 #include <openssl/aes.h>
 #include <openssl/sha.h>
 #include <openssl/evp.h>
-#include <openssl/hmac.h>
+//#include <openssl/hmac.h>
+#include "gladman_hmac.h"
 #include <openssl/aes.h>
 #ifdef _OPENMP
 #include <omp.h>
@@ -168,102 +169,97 @@ static void SHA_Simple(void *p, int len, unsigned char *output)
 
 static int LAME_ssh2_load_userkey(char *passphrase)
 {
-        int passlen = strlen(passphrase);
+	int passlen = strlen(passphrase);
 	unsigned char out[cur_salt->private_blob_len];
 	AES_KEY akey;
 	unsigned char iv[32];
 
-        /* Decrypt the private blob. */
-        if (cur_salt->cipher) {
-                unsigned char key[40];
-                SHA_CTX s;
-                if (cur_salt->private_blob_len % cur_salt->cipherblk)
-                        goto error;
+	/* Decrypt the private blob. */
+	if (cur_salt->cipher) {
+		unsigned char key[40];
+		SHA_CTX s;
+		if (cur_salt->private_blob_len % cur_salt->cipherblk)
+			goto error;
 
-                SHA1_Init(&s);
-                SHA1_Update(&s, (void*)"\0\0\0\0", 4);
-                SHA1_Update(&s, passphrase, passlen);
-                SHA1_Final(key + 0, &s);
-                SHA1_Init(&s);
-                SHA1_Update(&s, (void*)"\0\0\0\1", 4);
-                SHA1_Update(&s, passphrase, passlen);
-                SHA1_Final(key + 20, &s);
-                memset(iv, 0, 32);
-                memset(&akey, 0, sizeof(AES_KEY));
-                if(AES_set_decrypt_key(key, 256, &akey) < 0) {
-                        fprintf(stderr, "AES_set_derypt_key failed!\n");
-                }
-                AES_cbc_encrypt(cur_salt->private_blob, out , cur_salt->private_blob_len, &akey, iv, AES_DECRYPT);
-        }
-
-        /* Verify the MAC. */
-        {
-                char realmac[41];
-                unsigned char binary[20];
-                unsigned char *macdata;
-                int maclen;
-                int free_macdata;
+		SHA1_Init(&s);
+		SHA1_Update(&s, (void*)"\0\0\0\0", 4);
+		SHA1_Update(&s, passphrase, passlen);
+		SHA1_Final(key + 0, &s);
+		SHA1_Init(&s);
+		SHA1_Update(&s, (void*)"\0\0\0\1", 4);
+		SHA1_Update(&s, passphrase, passlen);
+		SHA1_Final(key + 20, &s);
+		memset(iv, 0, 32);
+		memset(&akey, 0, sizeof(AES_KEY));
+		if(AES_set_decrypt_key(key, 256, &akey) < 0) {
+			fprintf(stderr, "AES_set_derypt_key failed!\n");
+		}
+		AES_cbc_encrypt(cur_salt->private_blob, out , cur_salt->private_blob_len, &akey, iv, AES_DECRYPT);
+	}
+	/* Verify the MAC. */
+	{
+		char realmac[41];
+		unsigned char binary[20];
+		unsigned char *macdata;
+		int maclen;
+		int free_macdata;
 		int i;
-
-                if (cur_salt->old_fmt) {
-                        /* MAC (or hash) only covers the private blob. */
-                        macdata = out;
-                        maclen = cur_salt->private_blob_len;
-                        free_macdata = 0;
-                } else {
-                        unsigned char *p;
-                        int namelen = strlen(cur_salt->alg);
-                        int enclen = strlen(cur_salt->encryption);
-                        int commlen = strlen(cur_salt->comment);
-                        maclen = (4 + namelen +
-                                4 + enclen +
-                                4 + commlen +
-                                4 + cur_salt->public_blob_len +
-                                4 + cur_salt->private_blob_len);
-                        macdata = (unsigned char*)malloc(maclen);
-                        p = macdata;
+		if (cur_salt->old_fmt) {
+			/* MAC (or hash) only covers the private blob. */
+			macdata = out;
+			maclen = cur_salt->private_blob_len;
+			free_macdata = 0;
+		} else {
+			unsigned char *p;
+			int namelen = strlen(cur_salt->alg);
+			int enclen = strlen(cur_salt->encryption);
+			int commlen = strlen(cur_salt->comment);
+			maclen = (4 + namelen +
+					4 + enclen +
+					4 + commlen +
+					4 + cur_salt->public_blob_len +
+					4 + cur_salt->private_blob_len);
+			macdata = (unsigned char*)malloc(maclen);
+			p = macdata;
 #define DO_STR(s,len) PUT_32BIT(p,(len));memcpy(p+4,(s),(len));p+=4+(len)
-                        DO_STR(cur_salt->alg, namelen);
-                        DO_STR(cur_salt->encryption, enclen);
-                        DO_STR(cur_salt->comment, commlen);
-                        DO_STR(cur_salt->public_blob, cur_salt->public_blob_len);
-                        DO_STR(out, cur_salt->private_blob_len);
-                        free_macdata = 1;
-                }
+			DO_STR(cur_salt->alg, namelen);
+			DO_STR(cur_salt->encryption, enclen);
+			DO_STR(cur_salt->comment, commlen);
+			DO_STR(cur_salt->public_blob, cur_salt->public_blob_len);
+			DO_STR(out, cur_salt->private_blob_len);
+			free_macdata = 1;
+		}
+		if (cur_salt->is_mac) {
+			SHA_CTX s;
+			unsigned char mackey[20];
+			unsigned int length = 20;
+			// HMAC_CTX ctx;
+			char header[] = "putty-private-key-file-mac-key";
+			SHA1_Init(&s);
+			SHA1_Update(&s, header, sizeof(header)-1);
+			if (cur_salt->cipher && passphrase)
+				SHA_Update(&s, passphrase, passlen);
+			SHA1_Final(mackey, &s);
+			hmac_sha1(mackey, 20, macdata, maclen, binary, length);
+			/* HMAC_Init(&ctx, mackey, 20, EVP_sha1());
+			 * HMAC_Update(&ctx, macdata, maclen);
+			 * HMAC_Final(&ctx, binary, &length);
+			 * HMAC_CTX_cleanup(&ctx); */
+		} else {
+			SHA_Simple(macdata, maclen, binary);
+		}
+		if (free_macdata) {
+			free(macdata);
+		}
+		for (i = 0; i < 20; i++)
+			sprintf(realmac + 2 * i, "%02x", binary[i]);
 
-                if (cur_salt->is_mac) {
-                        SHA_CTX s;
-                        unsigned char mackey[20];
-                        unsigned int length = 20;
-                        HMAC_CTX ctx;
-                        char header[] = "putty-private-key-file-mac-key";
-
-                        SHA1_Init(&s);
-                        SHA1_Update(&s, header, sizeof(header)-1);
-                        if (cur_salt->cipher && passphrase)
-                                SHA_Update(&s, passphrase, passlen);
-                        SHA1_Final(mackey, &s);
-
-                        HMAC_Init(&ctx, mackey, 20, EVP_sha1());
-                        HMAC_Update(&ctx, macdata, maclen);
-                        HMAC_Final(&ctx, binary, &length);
-                        HMAC_CTX_cleanup(&ctx);
-                } else {
-                        SHA_Simple(macdata, maclen, binary);
-                }
-
-                if (free_macdata) {
-                        free(macdata);
-                }
-                for (i = 0; i < 20; i++)
-                        sprintf(realmac + 2 * i, "%02x", binary[i]);
-
-                if (strcmp(cur_salt->mac, realmac) == 0)
+		if (strcmp(cur_salt->mac, realmac) == 0)
 			return 1;
 	}
 
 error:
-        return 0;
+	return 0;
 }
 
 static void crypt_all(int count)
