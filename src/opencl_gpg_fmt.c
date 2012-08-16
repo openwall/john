b7ee29d71b2c758662e23ec71e6438f452839b6d
magnum <john.magnum@hushmail.com>
2012-08-16 17:25:08 +0200

    opencl_gpg lacked forward-compatible format struct

--- src/opencl_gpg_fmt.c
+++ src/opencl_gpg_fmt.c
@@ -498,7 +498,13 @@ struct fmt_main fmt_opencl_gpg = {
 		BENCHMARK_LENGTH,
 		PLAINTEXT_LENGTH,
 		BINARY_SIZE,
+#if FMT_MAIN_VERSION > 9
+		DEFAULT_ALIGN,
+#endif
 		SALT_SIZE,
+#if FMT_MAIN_VERSION > 9
+		DEFAULT_ALIGN,
+#endif
 		MIN_KEYS_PER_CRYPT,
 		MAX_KEYS_PER_CRYPT,
 		FMT_CASE | FMT_8_BIT | FMT_OMP | FMT_NOT_EXACT,
@@ -510,6 +516,9 @@ struct fmt_main fmt_opencl_gpg = {
 		fmt_default_split,
 		fmt_default_binary,
 		get_salt,
+#if FMT_MAIN_VERSION > 9
+		fmt_default_source,
+#endif
 		{
 			fmt_default_binary_hash
 		},
