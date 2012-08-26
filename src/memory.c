openwall/john

commit 8f3e4522e115c0ab5bdbf0eb4550eaf87f2af02b

RAR format: Use mem_alloc() and MEM_FREE().

src/unrar.c
@@ -2,8 +2,9 @@
  *  Extract RAR archives
  *
  * Modified for JtR, (c) magnum 2012. This code use a memory buffer instead
- * of a file handle. It does not store the inflated data, it just CRC's it.
- * Support for older RAR versions was stripped. Autoconf stuff was removed.
+ * of a file handle, and decrypts while reading. It does not store inflated
+ * data, it just CRC's it. Support for older RAR versions was stripped.
+ * Autoconf stuff was removed.
  *
  *  Copyright (C) 2005-2006 trog@uncon.org
  *
@@ -564,10 +565,8 @@ static int read_end_of_block(const unsigned char **fd, unpack_data_t *unpack_dat
 
 void rar_init_filters(unpack_data_t *unpack_data)
 {
-	if (unpack_data->old_filter_lengths) {
-		rar_free(unpack_data->old_filter_lengths);
-		unpack_data->old_filter_lengths = NULL;
-	}
+	MEM_FREE(unpack_data->old_filter_lengths);
+	unpack_data->old_filter_lengths = NULL;
 	unpack_data->old_filter_lengths_size = 0;
 	unpack_data->last_filter = 0;
 
@@ -709,7 +708,7 @@ static int add_vm_code(unpack_data_t *unpack_data, unsigned int first_byte,
 			rar_dbgmsg("ERROR: vm_codesize=0x%x buf_size=0x%x\n", vm_codesize, rarvm_input.buf_size);
 			return 0;
 		}
-		vm_code = (unsigned char *) rar_malloc(vm_codesize);
+		vm_code = (unsigned char *) mem_alloc(vm_codesize);
 		if(!vm_code) {
 		    rar_dbgmsg("unrar: add_vm_code: rar_malloc failed for vm_code\n");
 		    return 0;
@@ -720,10 +719,10 @@ static int add_vm_code(unpack_data_t *unpack_data, unsigned int first_byte,
 		}
 		if(!rarvm_prepare(&unpack_data->rarvm_data, &rarvm_input, &vm_code[0], vm_codesize, &filter->prg)) {
 		    rar_dbgmsg("unrar: add_vm_code: rarvm_prepare failed\n");
-		    rar_free(vm_code);
+		    MEM_FREE(vm_code);
 		    return 0;
 		}
-		rar_free(vm_code);
+		MEM_FREE(vm_code);
 	}
 	stack_filter->prg.alt_cmd = &filter->prg.cmd.array[0];
 	stack_filter->prg.cmd_count = filter->prg.cmd_count;
@@ -731,7 +730,7 @@ static int add_vm_code(unpack_data_t *unpack_data, unsigned int first_byte,
 	static_size = filter->prg.static_size;
 	if (static_size > 0 && static_size < VM_GLOBALMEMSIZE) {
 		// read statically defined data contained in DB commands
-		stack_filter->prg.static_data = rar_malloc(static_size);
+		stack_filter->prg.static_data = mem_alloc(static_size);
 		if(!stack_filter->prg.static_data) {
 		    rar_dbgmsg("unrar: add_vm_code: rar_malloc failed for stack_filter->prg.static_data\n");
 		    return 0;
@@ -740,8 +739,8 @@ static int add_vm_code(unpack_data_t *unpack_data, unsigned int first_byte,
 	}
 
 	if (stack_filter->prg.global_size < VM_FIXEDGLOBALSIZE) {
-		rar_free(stack_filter->prg.global_data);
-		stack_filter->prg.global_data = rar_malloc(VM_FIXEDGLOBALSIZE);
+		MEM_FREE(stack_filter->prg.global_data);
+		stack_filter->prg.global_data = mem_alloc(VM_FIXEDGLOBALSIZE);
 		if(!stack_filter->prg.global_data) {
 		    rar_dbgmsg("unrar: add_vm_code: rar_malloc failed for stack_filter->prg.global_data\n");
 		    return 0;
@@ -807,7 +806,7 @@ static int read_vm_code(unpack_data_t *unpack_data, const unsigned char **fd)
 		length = rar_getbits(unpack_data);
 		rar_addbits(unpack_data, 16);
 	}
-	vmcode = (unsigned char *) rar_malloc(length + 2);
+	vmcode = (unsigned char *) mem_alloc(length + 2);
 	rar_dbgmsg("VM code length: %d\n", length);
 	if (!vmcode) {
 		return 0;
@@ -817,14 +816,14 @@ static int read_vm_code(unpack_data_t *unpack_data, const unsigned char **fd)
 		// But if we read all bytes except the last, one byte is enough.
 		if (unpack_data->in_addr >= unpack_data->read_top-1 &&
 				!rar_unp_read_buf(fd, unpack_data) && i<length-1) {
-			rar_free(vmcode);
+			MEM_FREE(vmcode);
 			return 0;
 		}
 		vmcode[i] = rar_getbits(unpack_data) >> 8;
 		rar_addbits(unpack_data, 8);
 	}
 	retval = add_vm_code(unpack_data, first_byte, vmcode, length);
-	rar_free(vmcode);
+	MEM_FREE(vmcode);
 	return retval;
 }
 
@@ -856,7 +855,7 @@ static int read_vm_code_PPM(unpack_data_t *unpack_data, const unsigned char **fd
 		}
 		length = b1*256 + b2;
 	}
-	vmcode = (unsigned char *) rar_malloc(length + 2);
+	vmcode = (unsigned char *) mem_alloc(length + 2);
 	rar_dbgmsg("VM PPM code length: %d\n", length);
 	if (!vmcode) {
 		return 0;
@@ -864,13 +863,13 @@ static int read_vm_code_PPM(unpack_data_t *unpack_data, const unsigned char **fd
 	for (i=0 ; i < length ; i++) {
 		ch = ppm_decode_char(&unpack_data->ppm_data, fd, unpack_data);
 		if (ch == -1) {
-			rar_free(vmcode);
+			MEM_FREE(vmcode);
 			return 0;
 		}
 		vmcode[i] = ch;
 	}
 	retval = add_vm_code(unpack_data, first_byte, vmcode, length);
-	rar_free(vmcode);
+	MEM_FREE(vmcode);
 	return retval;
 }
 

src/unrarfilter.c
@@ -2,8 +2,9 @@
  *  Extract RAR archives
  *
  * Modified for JtR, (c) magnum 2012. This code use a memory buffer instead
- * of a file handle. It does not store the inflated data, it just CRC's it.
- * Support for older RAR versions was stripped. Autoconf stuff was removed.
+ * of a file handle, and decrypts while reading. It does not store inflated
+ * data, it just CRC's it. Support for older RAR versions was stripped.
+ * Autoconf stuff was removed.
  *
  *  Copyright (C) 2005 trog@uncon.org
  *
@@ -40,9 +41,7 @@ void rar_filter_array_reset(rar_filter_array_t *filter_a)
 	for (i=0 ; i < filter_a->num_items ; i++) {
 		rar_filter_delete(filter_a->array[i]);
 	}
-	if (filter_a->array) {
-		rar_free(filter_a->array);
-	}
+	MEM_FREE(filter_a->array);
 	filter_a->array = NULL;
 	filter_a->num_items = 0;
 }
@@ -64,7 +63,7 @@ struct UnpackFilter *rar_filter_new(void)
 {
 	struct UnpackFilter *filter;
 
-	filter = (struct UnpackFilter *) rar_malloc(sizeof(struct UnpackFilter));
+	filter = (struct UnpackFilter *) mem_alloc(sizeof(struct UnpackFilter));
 	if (!filter) {
 		return NULL;
 	}
@@ -87,12 +86,8 @@ void rar_filter_delete(struct UnpackFilter *filter)
 	if (!filter) {
 		return;
 	}
-	if (filter->prg.global_data) {
-		rar_free(filter->prg.global_data);
-	}
-	if (filter->prg.static_data) {
-		rar_free(filter->prg.static_data);
-	}
+	MEM_FREE(filter->prg.global_data);
+	MEM_FREE(filter->prg.static_data);
 	rar_cmd_array_reset(&filter->prg.cmd);
-	rar_free(filter);
+	MEM_FREE(filter);
 }

src/unrarhlp.c
@@ -1,7 +1,8 @@
 /*
  * Modified for JtR, (c) magnum 2012. This code use a memory buffer instead
- * of a file handle. It does not store the inflated data, it just CRC's it.
- * Support for older RAR versions was stripped. Autoconf stuff was removed.
+ * of a file handle, and decrypts while reading. It does not store inflated
+ * data, it just CRC's it. Support for older RAR versions was stripped.
+ * Autoconf stuff was removed.
  *
  *  Copyright (C) 2007 Sourcefire, Inc.
  *
@@ -27,26 +28,6 @@ static void rar_dbgmsg(const char* fmt,...){}
 
 #define RAR_MAX_ALLOCATION 184549376
 
-void *rar_malloc(size_t size)
-{
-	void *alloc;
-
-
-    if(!size || size > RAR_MAX_ALLOCATION) {
-	rar_dbgmsg("UNRAR: rar_malloc(): Attempt to allocate %lu bytes. Please report to http://bugs.clamav.net\n", size);
-	return NULL;
-    }
-
-    alloc = malloc(size);
-
-    rar_dbgmsg("%s: allocated %zu bytes at %p\n", __func__, size, alloc);
-
-    if(!alloc) {
-	    fprintf(stderr, "UNRAR: rar_malloc(): Can't allocate memory (%zu bytes).\n", size);
-	return NULL;
-    } else return alloc;
-}
-
 void *rar_realloc2(void *ptr, size_t size)
 {
 	void *alloc;
@@ -60,21 +41,11 @@ void *rar_realloc2(void *ptr, size_t size)
 
     if(!alloc) {
 	fprintf(stderr, "UNRAR: rar_realloc2(): Can't allocate memory (%zu bytes).\n", size);
-	if(ptr)
-	    rar_free(ptr);
+	MEM_FREE(ptr);
 	return NULL;
     }
 
     rar_dbgmsg("%s: reallocated %p to %zu bytes at %p\n", __func__, ptr, size, alloc);
 
     return alloc;
-}
-
-void rar_free(void *ptr) {
-	if (ptr != NULL) {
-		free(ptr);
-
-		rar_dbgmsg("%s: freed %p\n", __func__, ptr);
-		ptr = NULL;
-	}
 }

src/unrarhlp.h
@@ -1,7 +1,8 @@
 /*
  * Modified for JtR, (c) magnum 2012. This code use a memory buffer instead
- * of a file handle. It does not store the inflated data, it just CRC's it.
- * Support for older RAR versions was stripped. Autoconf stuff was removed.
+ * of a file handle, and decrypts while reading. It does not store inflated
+ * data, it just CRC's it. Support for older RAR versions was stripped.
+ * Autoconf stuff was removed.
  *
  *  Copyright (C) 2007 Sourcefire, Inc.
  *
@@ -18,11 +19,10 @@
 #define __UNRARHLP_H
 
 #include "arch.h"
+#include "memory.h"
 
 //#define RAR_HIGH_DEBUG
 
-void *rar_malloc(size_t size);
 void *rar_realloc2(void *ptr, size_t size);
-void rar_free(void *ptr);
 
 #endif /* __UNRARHLP_H */

src/unrarppm.c
@@ -2,8 +2,9 @@
  *  Extract RAR archives
  *
  * Modified for JtR, (c) magnum 2012. This code use a memory buffer instead
- * of a file handle. It does not store the inflated data, it just CRC's it.
- * Support for older RAR versions was stripped. Autoconf stuff was removed.
+ * of a file handle, and decrypts while reading. It does not store inflated
+ * data, it just CRC's it. Support for older RAR versions was stripped.
+ * Autoconf stuff was removed.
  *
  *  Copyright (C) 2005-2006 trog@uncon.org
  *
@@ -101,7 +102,7 @@ static void sub_allocator_stop_sub_allocator(sub_allocator_t *sub_alloc)
 {
 	if (sub_alloc->sub_allocator_size) {
 		sub_alloc->sub_allocator_size = 0;
-		rar_free(sub_alloc->heap_start);
+		MEM_FREE(sub_alloc->heap_start);
 	}
 }
 
@@ -123,7 +124,7 @@ static int sub_allocator_start_sub_allocator(sub_allocator_t *sub_alloc, int sa_
 	/* Allow for aligned access requirements */
 	alloc_size += UNIT_SIZE;
 #endif
-	if ((sub_alloc->heap_start = (unsigned char *) rar_malloc(alloc_size)) == NULL) {
+	if ((sub_alloc->heap_start = (unsigned char *) mem_alloc(alloc_size)) == NULL) {
 		rar_dbgmsg("sub_alloc start failed\n");
 		return 0;
 	}

src/unrarvm.c
@@ -2,8 +2,9 @@
  *  Extract RAR archives
  *
  * Modified for JtR, (c) magnum 2012. This code use a memory buffer instead
- * of a file handle. It does not store the inflated data, it just CRC's it.
- * Support for older RAR versions was stripped. Autoconf stuff was removed.
+ * of a file handle, and decrypts while reading. It does not store inflated
+ * data, it just CRC's it. Support for older RAR versions was stripped.
+ * Autoconf stuff was removed.
  *
  *  Copyright (C) 2005-2006 trog@uncon.org
  *
@@ -195,7 +196,7 @@ unsigned int rar_crc(unsigned int start_crc, void *addr, unsigned int size)
 
 int rarvm_init(rarvm_data_t *rarvm_data)
 {
-	rarvm_data->mem = (unsigned char *) rar_malloc(RARVM_MEMSIZE+4);
+	rarvm_data->mem = (unsigned char *) mem_alloc(RARVM_MEMSIZE+4);
 	if (!rarvm_data->mem) {
 		return 0;
 	}
@@ -205,7 +206,7 @@ int rarvm_init(rarvm_data_t *rarvm_data)
 void rarvm_free(rarvm_data_t *rarvm_data)
 {
 	if (rarvm_data && rarvm_data->mem) {
-		rar_free(rarvm_data->mem);
+		MEM_FREE(rarvm_data->mem);
 		rarvm_data->mem = NULL;
 	}
 }
@@ -920,7 +921,7 @@ int rarvm_execute(rarvm_data_t *rarvm_data, struct rarvm_prepared_program *prg)
 	prg->filtered_data_size = new_size;
 
 	if (prg->global_data) {
-		rar_free(prg->global_data);
+		MEM_FREE(prg->global_data);
 		prg->global_data = NULL;
 		prg->global_size = 0;
 	}
@@ -1082,7 +1083,7 @@ int rarvm_prepare(rarvm_data_t *rarvm_data, rarvm_input_t *rarvm_input, unsigned
 		if (data_flag & 0x8000) {
 			int data_size = rarvm_read_data(rarvm_input)+1;
 			rar_dbgmsg("data_size=%d\n", data_size);
-			prg->static_data = rar_malloc(data_size);
+			prg->static_data = mem_alloc(data_size);
 			if(!prg->static_data) {
 			    rar_dbgmsg("unrar: rarvm_prepare: rar_malloc failed for prg->static_data\n");
 			    return 0;

src/unrar.h
@@ -2,8 +2,9 @@
  * Extract RAR archives
  *
  * Modified for JtR, (c) magnum 2012. This code use a memory buffer instead
- * of a file handle. It does not store the inflated data, it just CRC's it.
- * Support for older RAR versions was stripped. Autoconf stuff was removed.
+ * of a file handle, and decrypts while reading. It does not store inflated
+ * data, it just CRC's it. Support for older RAR versions was stripped.
+ * Autoconf stuff was removed.
  *
  * Copyright (C) 2005-2006 trog@uncon.org
  *

src/unrarcmd.c
@@ -2,8 +2,9 @@
  *  Extract RAR archives
  *
  * Modified for JtR, (c) magnum 2012. This code use a memory buffer instead
- * of a file handle. It does not store the inflated data, it just CRC's it.
- * Support for older RAR versions was stripped. Autoconf stuff was removed.
+ * of a file handle, and decrypts while reading. It does not store inflated
+ * data, it just CRC's it. Support for older RAR versions was stripped.
+ * Autoconf stuff was removed.
  *
  *  Copyright (C) 2005 trog@uncon.org
  *
@@ -36,9 +37,7 @@ void rar_cmd_array_reset(rar_cmd_array_t *cmd_a)
 	if (!cmd_a) {
 		return;
 	}
-	if (cmd_a->array) {
-		rar_free(cmd_a->array);
-	}
+	MEM_FREE(cmd_a->array);
 	cmd_a->array = NULL;
 	cmd_a->num_items = 0;
 }

commit d02cdafd8a989ed306db3bca861aa12e3df5f135

memory.c: print some clues if alloc() fails. RAR: handle expected bogus alloc sizes (bug introduced in 8f3e452)

src/memory.c
@@ -52,7 +52,7 @@ void *mem_alloc(size_t size)
 	if (!size) return NULL;
 
 	if (!(res = malloc(size))) {
-		fprintf(stderr, "malloc: %s\n", strerror(ENOMEM));
+		fprintf(stderr, "%s: %s trying to allocate %zd bytes\n", __func__, strerror(ENOMEM), size);
 		error();
 	}
 

src/unrar.c
@@ -723,7 +723,7 @@ static int add_vm_code(unpack_data_t *unpack_data, unsigned int first_byte,
 			rar_dbgmsg("ERROR: vm_codesize=0x%x buf_size=0x%x\n", vm_codesize, rarvm_input.buf_size);
 			return 0;
 		}
-		vm_code = (unsigned char *) mem_alloc(vm_codesize);
+		vm_code = (unsigned char *) rar_malloc(vm_codesize);
 		if(!vm_code) {
 		    rar_dbgmsg("unrar: add_vm_code: rar_malloc failed for vm_code\n");
 		    return 0;
@@ -745,7 +745,7 @@ static int add_vm_code(unpack_data_t *unpack_data, unsigned int first_byte,
 	static_size = filter->prg.static_size;
 	if (static_size > 0 && static_size < VM_GLOBALMEMSIZE) {
 		// read statically defined data contained in DB commands
-		stack_filter->prg.static_data = mem_alloc(static_size);
+		stack_filter->prg.static_data = rar_malloc(static_size);
 		if(!stack_filter->prg.static_data) {
 		    rar_dbgmsg("unrar: add_vm_code: rar_malloc failed for stack_filter->prg.static_data\n");
 		    return 0;
@@ -755,7 +755,7 @@ static int add_vm_code(unpack_data_t *unpack_data, unsigned int first_byte,
 
 	if (stack_filter->prg.global_size < VM_FIXEDGLOBALSIZE) {
 		MEM_FREE(stack_filter->prg.global_data);
-		stack_filter->prg.global_data = mem_alloc(VM_FIXEDGLOBALSIZE);
+		stack_filter->prg.global_data = rar_malloc(VM_FIXEDGLOBALSIZE);
 		if(!stack_filter->prg.global_data) {
 		    rar_dbgmsg("unrar: add_vm_code: rar_malloc failed for stack_filter->prg.global_data\n");
 		    return 0;
@@ -821,7 +821,7 @@ static int read_vm_code(unpack_data_t *unpack_data, const unsigned char **fd)
 		length = rar_getbits(unpack_data);
 		rar_addbits(unpack_data, 16);
 	}
-	vmcode = (unsigned char *) mem_alloc(length + 2);
+	vmcode = (unsigned char *) rar_malloc(length + 2);
 	rar_dbgmsg("VM code length: %d\n", length);
 	if (!vmcode) {
 		return 0;
@@ -870,7 +870,7 @@ static int read_vm_code_PPM(unpack_data_t *unpack_data, const unsigned char **fd
 		}
 		length = b1*256 + b2;
 	}
-	vmcode = (unsigned char *) mem_alloc(length + 2);
+	vmcode = (unsigned char *) rar_malloc(length + 2);
 	rar_dbgmsg("VM PPM code length: %d\n", length);
 	if (!vmcode) {
 		return 0;

src/unrarfilter.c
@@ -63,7 +63,7 @@ struct UnpackFilter *rar_filter_new(void)
 {
 	struct UnpackFilter *filter;
 
-	filter = (struct UnpackFilter *) mem_alloc(sizeof(struct UnpackFilter));
+	filter = (struct UnpackFilter *) rar_malloc(sizeof(struct UnpackFilter));
 	if (!filter) {
 		return NULL;
 	}

src/unrarhlp.c
@@ -28,7 +28,20 @@ static void rar_dbgmsg(const char* fmt,...){}
 
 #define RAR_MAX_ALLOCATION 184549376
 
-void *rar_realloc2(void *ptr, size_t size)
+inline void *rar_malloc(size_t size)
+{
+	if(!size || size > (size_t)RAR_MAX_ALLOCATION) {
+#ifdef DEBUG
+		fprintf(stderr, "UNRAR: rar_malloc(): Attempt to allocate %lu bytes.\n", size);
+#endif
+		return NULL;
+	}
+	rar_dbgmsg("%s() allocating %zd bytes\n", __func__, size);
+
+	return mem_alloc(size);
+}
+
+inline void *rar_realloc2(void *ptr, size_t size)
 {
 	void *alloc;
 

src/unrarhlp.h
@@ -23,6 +23,7 @@
 
 //#define RAR_HIGH_DEBUG
 
-void *rar_realloc2(void *ptr, size_t size);
+inline void *rar_malloc(size_t size);
+inline void *rar_realloc2(void *ptr, size_t size);
 
 #endif /* __UNRARHLP_H */

src/unrarppm.c
@@ -124,7 +124,7 @@ static int sub_allocator_start_sub_allocator(sub_allocator_t *sub_alloc, int sa_
 	/* Allow for aligned access requirements */
 	alloc_size += UNIT_SIZE;
 #endif
-	if ((sub_alloc->heap_start = (unsigned char *) mem_alloc(alloc_size)) == NULL) {
+	if ((sub_alloc->heap_start = (unsigned char *) rar_malloc(alloc_size)) == NULL) {
 		rar_dbgmsg("sub_alloc start failed\n");
 		return 0;
 	}

src/unrarvm.c
@@ -196,7 +196,7 @@ unsigned int rar_crc(unsigned int start_crc, void *addr, unsigned int size)
 
 int rarvm_init(rarvm_data_t *rarvm_data)
 {
-	rarvm_data->mem = (unsigned char *) mem_alloc(RARVM_MEMSIZE+4);
+	rarvm_data->mem = (unsigned char *) rar_malloc(RARVM_MEMSIZE+4);
 	if (!rarvm_data->mem) {
 		return 0;
 	}
@@ -1083,7 +1083,7 @@ int rarvm_prepare(rarvm_data_t *rarvm_data, rarvm_input_t *rarvm_input, unsigned
 		if (data_flag & 0x8000) {
 			int data_size = rarvm_read_data(rarvm_input)+1;
 			rar_dbgmsg("data_size=%d\n", data_size);
-			prg->static_data = mem_alloc(data_size);
+			prg->static_data = rar_malloc(data_size);
 			if(!prg->static_data) {
 			    rar_dbgmsg("unrar: rarvm_prepare: rar_malloc failed for prg->static_data\n");
 			    return 0;