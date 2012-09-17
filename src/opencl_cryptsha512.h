cebd59b3740662a882826e1eca15c258978902fd
Claudio André <claudio.andre@correios.net.br>
2012-09-17 10:34:44 -0300

    Remove copied code from everywhere. A new .h file has all necessary code.

--- src/opencl_cryptsha512.h
+++ src/opencl_cryptsha512.h
@@ -14,29 +14,7 @@
 #ifndef _CRYPTSHA512_H
 #define _CRYPTSHA512_H
 
-//Copied from common-opencl.h
-#define UNKNOWN                 0
-#define CPU                     1
-#define GPU                     2
-#define ACCELERATOR             4
-#define AMD                     64
-#define NVIDIA                  128
-#define INTEL                   256
-#define AMD_GCN                 1024
-#define AMD_VLIW4               2048
-#define AMD_VLIW5               4096
-#define NO_BYTE_ADDRESSABLE     8192
-
-#define cpu(n)                  ((n & CPU) == (CPU))
-#define gpu(n)                  ((n & GPU) == (GPU))
-#define gpu_amd(n)              ((n & AMD) && gpu(n))
-#define gpu_nvidia(n)           ((n & NVIDIA) && gpu(n))
-#define gpu_intel(n)            ((n & INTEL) && gpu(n))
-#define cpu_amd(n)              ((n & AMD) && cpu(n))
-#define amd_gcn(n)              ((n & AMD_GCN) && gpu_amd(n))
-#define amd_vliw4(n)            ((n & AMD_VLIW4) && gpu_amd(n))
-#define amd_vliw5(n)            ((n & AMD_VLIW5) && gpu_amd(n))
-#define no_byte_addressable(n)  (n & NO_BYTE_ADDRESSABLE)
+#include "opencl_device_info.h"
 
 //Type names definition.
 #define uint8_t  unsigned char
---
+++ src/opencl_device_info.h
@@ -0,0 +1,42 @@
+/*
+ * Developed by Claudio André <claudio.andre at correios.net.br> in 2012
+ *
+ * More information at http://openwall.info/wiki/john/OpenCL-RAWSHA-512
+ *
+ * Copyright (c) 2012 Claudio André <claudio.andre at correios.net.br>
+ * This program comes with ABSOLUTELY NO WARRANTY; express or implied.
+ *
+ * This is free software, and you are welcome to redistribute it
+ * under certain conditions; as expressed here
+ * http://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+#ifndef OPENCL_DEVICE_INFO_H
+#define	OPENCL_DEVICE_INFO_H
+
+//Copied from common-opencl.h
+#define UNKNOWN                 0
+#define CPU                     1
+#define GPU                     2
+#define ACCELERATOR             4
+#define AMD                     64
+#define NVIDIA                  128
+#define INTEL                   256
+#define AMD_GCN                 1024
+#define AMD_VLIW4               2048
+#define AMD_VLIW5               4096
+#define NO_BYTE_ADDRESSABLE     8192
+
+#define cpu(n)                  ((n & CPU) == (CPU))
+#define gpu(n)                  ((n & GPU) == (GPU))
+#define gpu_amd(n)              ((n & AMD) && gpu(n))
+#define gpu_nvidia(n)           ((n & NVIDIA) && gpu(n))
+#define gpu_intel(n)            ((n & INTEL) && gpu(n))
+#define cpu_amd(n)              ((n & AMD) && cpu(n))
+#define amd_gcn(n)              ((n & AMD_GCN) && gpu_amd(n))
+#define amd_vliw4(n)            ((n & AMD_VLIW4) && gpu_amd(n))
+#define amd_vliw5(n)            ((n & AMD_VLIW5) && gpu_amd(n))
+#define no_byte_addressable(n)  (n & NO_BYTE_ADDRESSABLE)
+
+#endif	/* OPENCL_DEVICE_INFO_H */
+
