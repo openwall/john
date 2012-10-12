99ccd6c0cdbbebe22b59ec37dcabf45882f20284
ukasz <lukas.odzioba@gmail.com>
2012-10-12 10:15:04 +0200

    Fixed valid in pwsafe formats

--- src/pwsafe_fmt_plug.c
+++ src/pwsafe_fmt_plug.c
@@ -44,7 +44,7 @@ static struct fmt_tests pwsafe_tests[] = {
 
 
 static char (*saved_key)[PLAINTEXT_LENGTH + 1];
-static ARCH_WORD_32 (*crypt_out)[BINARY_SIZE / sizeof(ARCH_WORD_32)];
+static ARCH_WORD_32(*crypt_out)[BINARY_SIZE / sizeof(ARCH_WORD_32)];
 
 static struct custom_salt {
 	int version;
@@ -61,13 +61,39 @@ static void init(struct fmt_main *self)
 	self->params.max_keys_per_crypt *= omp_t;
 #endif
 	saved_key = mem_calloc_tiny(sizeof(*saved_key) *
-			self->params.max_keys_per_crypt, MEM_ALIGN_NONE);
-	crypt_out = mem_calloc_tiny(sizeof(*crypt_out) * self->params.max_keys_per_crypt, MEM_ALIGN_WORD);
+	    self->params.max_keys_per_crypt, MEM_ALIGN_NONE);
+	crypt_out =
+	    mem_calloc_tiny(sizeof(*crypt_out) *
+	    self->params.max_keys_per_crypt, MEM_ALIGN_WORD);
 }
 
 static int valid(char *ciphertext, struct fmt_main *self)
 {
-	return !strncmp(ciphertext, "$pwsafe$", 8);
+	// format $pwsafe$version*salt*iterations*hash
+	char *p;
+	char *ctcopy = strdup(ciphertext);
+	char *keeptr = ctcopy;
+	if (strncmp(ciphertext, "$pwsafe$", 8) != 0)
+		return 0;
+	ctcopy += 9;		/* skip over "$pwsafe$*" */
+	if ((p = strtok(ctcopy, "*")) == NULL)	/* version */
+		return 0;
+	if (atoi(p) == 0)
+		return 0;
+	if ((p = strtok(NULL, "*")) == NULL)	/* salt */
+		return 0;
+	if (strlen(p) < 64)
+		return 0;
+	if ((p = strtok(NULL, "*")) == NULL)	/* iterations */
+		return 0;
+	if (atoi(p) == 0)
+		return 0;
+	if ((p = strtok(NULL, "*")) == NULL)	/* hash */
+		return 0;
+	if (strlen(p) != 64)
+		return 0;
+	MEM_FREE(keeptr);
+	return 1;
 }
 
 static void *get_salt(char *ciphertext)
@@ -77,13 +103,13 @@ static void *get_salt(char *ciphertext)
 	char *p;
 	int i;
 	static struct custom_salt cs;
-	ctcopy += 9;	/* skip over "$pwsafe$*" */
+	ctcopy += 9;		/* skip over "$pwsafe$*" */
 	p = strtok(ctcopy, "*");
 	cs.version = atoi(p);
 	p = strtok(NULL, "*");
 	for (i = 0; i < 32; i++)
 		cs.salt[i] = atoi16[ARCH_INDEX(p[i * 2])] * 16
-			+ atoi16[ARCH_INDEX(p[i * 2 + 1])];
+		    + atoi16[ARCH_INDEX(p[i * 2 + 1])];
 	p = strtok(NULL, "*");
 	cs.iterations = (unsigned int)atoi(p);
 	MEM_FREE(keeptr);
@@ -102,29 +128,82 @@ static void *get_binary(char *ciphertext)
 	p = strrchr(ciphertext, '*') + 1;
 	for (i = 0; i < BINARY_SIZE; i++) {
 		out[i] =
-		    (atoi16[ARCH_INDEX(*p)] << 4) |
-		    atoi16[ARCH_INDEX(p[1])];
+		    (atoi16[ARCH_INDEX(*p)] << 4) | atoi16[ARCH_INDEX(p[1])];
 		p += 2;
 	}
 
 	return out;
 }
 
-static int binary_hash_0(void *binary) { return *(ARCH_WORD_32 *)binary & 0xf; }
-static int binary_hash_1(void *binary) { return *(ARCH_WORD_32 *)binary & 0xff; }
-static int binary_hash_2(void *binary) { return *(ARCH_WORD_32 *)binary & 0xfff; }
-static int binary_hash_3(void *binary) { return *(ARCH_WORD_32 *)binary & 0xffff; }
-static int binary_hash_4(void *binary) { return *(ARCH_WORD_32 *)binary & 0xfffff; }
-static int binary_hash_5(void *binary) { return *(ARCH_WORD_32 *)binary & 0xffffff; }
-static int binary_hash_6(void *binary) { return *(ARCH_WORD_32 *)binary & 0x7ffffff; }
+static int binary_hash_0(void *binary)
+{
+	return *(ARCH_WORD_32 *) binary & 0xf;
+}
 
-static int get_hash_0(int index) { return crypt_out[index][0] & 0xf; }
-static int get_hash_1(int index) { return crypt_out[index][0] & 0xff; }
-static int get_hash_2(int index) { return crypt_out[index][0] & 0xfff; }
-static int get_hash_3(int index) { return crypt_out[index][0] & 0xffff; }
-static int get_hash_4(int index) { return crypt_out[index][0] & 0xfffff; }
-static int get_hash_5(int index) { return crypt_out[index][0] & 0xffffff; }
-static int get_hash_6(int index) { return crypt_out[index][0] & 0x7ffffff; }
+static int binary_hash_1(void *binary)
+{
+	return *(ARCH_WORD_32 *) binary & 0xff;
+}
+
+static int binary_hash_2(void *binary)
+{
+	return *(ARCH_WORD_32 *) binary & 0xfff;
+}
+
+static int binary_hash_3(void *binary)
+{
+	return *(ARCH_WORD_32 *) binary & 0xffff;
+}
+
+static int binary_hash_4(void *binary)
+{
+	return *(ARCH_WORD_32 *) binary & 0xfffff;
+}
+
+static int binary_hash_5(void *binary)
+{
+	return *(ARCH_WORD_32 *) binary & 0xffffff;
+}
+
+static int binary_hash_6(void *binary)
+{
+	return *(ARCH_WORD_32 *) binary & 0x7ffffff;
+}
+
+static int get_hash_0(int index)
+{
+	return crypt_out[index][0] & 0xf;
+}
+
+static int get_hash_1(int index)
+{
+	return crypt_out[index][0] & 0xff;
+}
+
+static int get_hash_2(int index)
+{
+	return crypt_out[index][0] & 0xfff;
+}
+
+static int get_hash_3(int index)
+{
+	return crypt_out[index][0] & 0xffff;
+}
+
+static int get_hash_4(int index)
+{
+	return crypt_out[index][0] & 0xfffff;
+}
+
+static int get_hash_5(int index)
+{
+	return crypt_out[index][0] & 0xffffff;
+}
+
+static int get_hash_6(int index)
+{
+	return crypt_out[index][0] & 0x7ffffff;
+}
 
 static void set_salt(void *salt)
 {
@@ -142,13 +221,15 @@ static void crypt_all(int count)
 		SHA256_CTX ctx;
 		int i;
 		SHA256_Init(&ctx);
-		SHA256_Update(&ctx, saved_key[index], strlen(saved_key[index]));
+		SHA256_Update(&ctx, saved_key[index],
+		    strlen(saved_key[index]));
 		SHA256_Update(&ctx, cur_salt->salt, 32);
-		SHA256_Final((unsigned char*)crypt_out[index], &ctx);
-		for(i = 0; i <= cur_salt->iterations; i++)  {
+		SHA256_Final((unsigned char *)crypt_out[index], &ctx);
+		for (i = 0; i <= cur_salt->iterations; i++) {
 			SHA256_Init(&ctx);
-			SHA256_Update(&ctx, (unsigned char*)crypt_out[index], 32);
-			SHA256_Final((unsigned char*)crypt_out[index], &ctx);
+			SHA256_Update(&ctx, (unsigned char *)crypt_out[index],
+			    32);
+			SHA256_Final((unsigned char *)crypt_out[index], &ctx);
 		}
 	}
 }
@@ -190,51 +271,47 @@ static char *get_key(int index)
 
 struct fmt_main pwsafe_fmt = {
 	{
-		FORMAT_LABEL,
-		FORMAT_NAME,
-		ALGORITHM_NAME,
-		BENCHMARK_COMMENT,
-		BENCHMARK_LENGTH,
-		PLAINTEXT_LENGTH,
-		BINARY_SIZE,
-		SALT_SIZE,
-		MIN_KEYS_PER_CRYPT,
-		MAX_KEYS_PER_CRYPT,
-		FMT_CASE | FMT_8_BIT | FMT_OMP,
-		pwsafe_tests
-	}, {
-		init,
-		fmt_default_prepare,
-		valid,
-		fmt_default_split,
-		get_binary,
-		get_salt,
-		{
-			binary_hash_0,
-			binary_hash_1,
-			binary_hash_2,
-			binary_hash_3,
-			binary_hash_4,
-			binary_hash_5,
-			binary_hash_6
-		},
-		fmt_default_salt_hash,
-		set_salt,
-		pwsafe_set_key,
-		get_key,
-		fmt_default_clear_keys,
-		crypt_all,
-		{
-			get_hash_0,
-			get_hash_1,
-			get_hash_2,
-			get_hash_3,
-			get_hash_4,
-			get_hash_5,
-			get_hash_6
-		},
-		cmp_all,
-		cmp_one,
-		cmp_exact
-	}
+		    FORMAT_LABEL,
+		    FORMAT_NAME,
+		    ALGORITHM_NAME,
+		    BENCHMARK_COMMENT,
+		    BENCHMARK_LENGTH,
+		    PLAINTEXT_LENGTH,
+		    BINARY_SIZE,
+		    SALT_SIZE,
+		    MIN_KEYS_PER_CRYPT,
+		    MAX_KEYS_PER_CRYPT,
+		    FMT_CASE | FMT_8_BIT | FMT_OMP,
+	    pwsafe_tests}, {
+		    init,
+		    fmt_default_prepare,
+		    valid,
+		    fmt_default_split,
+		    get_binary,
+		    get_salt,
+		    {
+				binary_hash_0,
+				binary_hash_1,
+				binary_hash_2,
+				binary_hash_3,
+				binary_hash_4,
+				binary_hash_5,
+			binary_hash_6},
+		    fmt_default_salt_hash,
+		    set_salt,
+		    pwsafe_set_key,
+		    get_key,
+		    fmt_default_clear_keys,
+		    crypt_all,
+		    {
+				get_hash_0,
+				get_hash_1,
+				get_hash_2,
+				get_hash_3,
+				get_hash_4,
+				get_hash_5,
+			get_hash_6},
+		    cmp_all,
+		    cmp_one,
+	    cmp_exact}
 };
