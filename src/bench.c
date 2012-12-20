openwall/john

commit ef133e46bfa91303c65fff464cd07855831eed8b

Quick-response to "events" (like key presses) for my loop-kernel OpenCL formats. I'll let Claudio and Sayantan decide for themselves whether to use it or not. Note that the clFinish() is needed within the loop for this to work correctly.

src/common-opencl.c
@@ -1,11 +1,15 @@
 /* Common OpenCL functions go in this file */
 
-#include "common-opencl.h"
 #include <assert.h>
 #include <string.h>
 #include <ctype.h>
 #include <sys/stat.h>
 #include <time.h>
+
+#include "common-opencl.h"
+#include "signals.h"
+#include "recovery.h"
+#include "status.h"
 
 #define LOG_SIZE 1024*16
 
@@ -13,6 +17,31 @@ static char opencl_log[LOG_SIZE];
 static char *kernel_source;
 static int kernel_loaded;
 static size_t program_size;
+
+void opencl_process_event(void)
+{
+#if !OS_TIMER
+	sig_timer_emu_tick();
+#endif
+	if (event_pending) {
+		if (event_save) {
+			event_save = 0;
+			rec_save();
+		}
+
+		if (event_status) {
+			event_status = 0;
+			status_print();
+		}
+
+		if (event_ticksafety) {
+			event_ticksafety = 0;
+			status_ticks_overflow_safety();
+		}
+
+		event_pending = event_abort;
+	}
+}
 
 void advance_cursor()
 {

src/common-opencl.h
@@ -89,4 +89,7 @@ void listOpenCLdevices();
 
 void opencl_find_gpu(int *dev_id, int *platform_id);
 
+/* Call this to check for keypress etc. within kernel loops */
+void opencl_process_event(void);
+
 #endif

src/opencl_krb5pa-sha1_fmt.c
@@ -279,7 +279,7 @@ static void find_best_gws(int do_benchmark, struct fmt_main *self)
 	unsigned int SHAspeed, bestSHAspeed = 0;
 	int optimal_gws = local_work_size;
 	const int sha1perkey = 2 * ITERATIONS * 2 + 6;
-	unsigned long long int MaxRunTime = cpu(device_info[ocl_gpu_id]) ? 1000000000ULL : 5000000000ULL;
+	unsigned long long int MaxRunTime = cpu(device_info[ocl_gpu_id]) ? 1000000000ULL : 10000000000ULL;
 
 	if (do_benchmark) {
 		fprintf(stderr, "Calculating best keys per crypt (GWS) for LWS=%zd and max. %llu s duration.\n\n", local_work_size, MaxRunTime / 1000000000UL);
@@ -774,13 +774,19 @@ static void crypt_all(int count)
 	/// Run kernel
 	HANDLE_CLERROR(clEnqueueNDRangeKernel(queue[ocl_gpu_id], pbkdf2_init, 1, NULL, &scalar_gws, &local_work_size, 0, NULL, firstEvent), "Run initial kernel");
 
-	for (i = 0; i < ITERATIONS / HASH_LOOPS; i++)
+	for (i = 0; i < ITERATIONS / HASH_LOOPS; i++) {
 		HANDLE_CLERROR(clEnqueueNDRangeKernel(queue[ocl_gpu_id], pbkdf2_loop, 1, NULL, &global_work_size, &local_work_size, 0, NULL, NULL), "Run loop kernel");
+		HANDLE_CLERROR(clFinish(queue[ocl_gpu_id]), "Error running loop kernel");
+		opencl_process_event();
+	}
 
 	HANDLE_CLERROR(clEnqueueNDRangeKernel(queue[ocl_gpu_id], pbkdf2_pass2, 1, NULL, &scalar_gws, &local_work_size, 0, NULL, NULL), "Run intermediate kernel");
 
-	for (i = 0; i < ITERATIONS / HASH_LOOPS; i++)
+	for (i = 0; i < ITERATIONS / HASH_LOOPS; i++) {
 		HANDLE_CLERROR(clEnqueueNDRangeKernel(queue[ocl_gpu_id], pbkdf2_loop, 1, NULL, &global_work_size, &local_work_size, 0, NULL, NULL), "Run loop kernel (2nd pass)");
+		HANDLE_CLERROR(clFinish(queue[ocl_gpu_id]), "Error running loop kernel");
+		opencl_process_event();
+	}
 
 	HANDLE_CLERROR(clEnqueueNDRangeKernel(queue[ocl_gpu_id], pbkdf2_final, 1, NULL, &scalar_gws, &local_work_size, 0, NULL, lastEvent), "Run final kernel (SHA1)");
 	HANDLE_CLERROR(clFinish(queue[ocl_gpu_id]), "Failed running final kernel");

src/opencl_office2007_fmt.c
@@ -340,7 +340,7 @@ static void find_best_gws(int do_benchmark, struct fmt_main *self)
 	unsigned int SHAspeed, bestSHAspeed = 0, max_gws;
 	int optimal_gws = local_work_size;
 	const int sha1perkey = 50004;
-	unsigned long long int MaxRunTime = 5000000000ULL;
+	unsigned long long int MaxRunTime = cpu(device_info[ocl_gpu_id]) ? 1000000000ULL : 10000000000ULL;
 
 	max_gws = get_max_mem_alloc_size(ocl_gpu_id) / (UNICODE_LENGTH * VF);
 
@@ -490,7 +490,7 @@ static int valid(char *ciphertext, struct fmt_main *self)
 	return !strncmp(ciphertext, "$office$*2007", 13);
 }
 
-static int PasswordVerifier(unsigned char *key)
+static inline int PasswordVerifier(unsigned char *key)
 {
 	unsigned char decryptedVerifier[16];
 	AES_KEY akey;
@@ -533,8 +533,11 @@ static void crypt_all(int count)
 
 	HANDLE_CLERROR(clEnqueueNDRangeKernel(queue[ocl_gpu_id], GenerateSHA1pwhash, 1, NULL, &scalar_gws, &local_work_size, 0, NULL, firstEvent), "failed in clEnqueueNDRangeKernel");
 
-	for (index = 0; index < 50000 / HASH_LOOPS; index++)
+	for (index = 0; index < 50000 / HASH_LOOPS; index++) {
 		HANDLE_CLERROR(clEnqueueNDRangeKernel(queue[ocl_gpu_id], crypt_kernel, 1, NULL, &global_work_size, &local_work_size, 0, NULL, NULL), "failed in clEnqueueNDRangeKernel");
+		HANDLE_CLERROR(clFinish(queue[ocl_gpu_id]), "Error running loop kernel");
+		opencl_process_event();
+	}
 
 	HANDLE_CLERROR(clEnqueueNDRangeKernel(queue[ocl_gpu_id], Generate2007key, 1, NULL, &global_work_size, &local_work_size, 0, NULL, lastEvent), "failed in clEnqueueNDRangeKernel");
 

src/opencl_office2010_fmt.c
@@ -341,7 +341,7 @@ static void find_best_gws(int do_benchmark, struct fmt_main *self)
 	unsigned int SHAspeed, bestSHAspeed = 0, max_gws;
 	int optimal_gws = local_work_size;
 	int sha1perkey;
-	unsigned long long int MaxRunTime = 5000000000ULL;
+	unsigned long long int MaxRunTime = cpu(device_info[ocl_gpu_id]) ? 1000000000ULL : 10000000000ULL;
 
 	max_gws = get_max_mem_alloc_size(ocl_gpu_id) / (UNICODE_LENGTH * VF);
 
@@ -525,8 +525,11 @@ static void crypt_all(int count)
 
 	HANDLE_CLERROR(clEnqueueNDRangeKernel(queue[ocl_gpu_id], GenerateSHA1pwhash, 1, NULL, &scalar_gws, &local_work_size, 0, NULL, firstEvent), "failed in clEnqueueNDRangeKernel");
 
-	for (index = 0; index < spincount / HASH_LOOPS; index++)
+	for (index = 0; index < spincount / HASH_LOOPS; index++) {
 		HANDLE_CLERROR(clEnqueueNDRangeKernel(queue[ocl_gpu_id], crypt_kernel, 1, NULL, &global_work_size, &local_work_size, 0, NULL, NULL), "failed in clEnqueueNDRangeKernel");
+		HANDLE_CLERROR(clFinish(queue[ocl_gpu_id]), "Error running loop kernel");
+		opencl_process_event();
+	}
 
 	HANDLE_CLERROR(clEnqueueNDRangeKernel(queue[ocl_gpu_id], Generate2010key, 1, NULL, &global_work_size, &local_work_size, 0, NULL, lastEvent), "failed in clEnqueueNDRangeKernel");
 

src/opencl_office2013_fmt.c
@@ -343,7 +343,7 @@ static void find_best_gws(int do_benchmark, struct fmt_main *self)
 	unsigned int SHAspeed, bestSHAspeed = 0, max_gws;
 	int optimal_gws = local_work_size;
 	int sha512perkey;
-	unsigned long long int MaxRunTime = 5000000000ULL;
+	unsigned long long int MaxRunTime = cpu(device_info[ocl_gpu_id]) ? 1000000000ULL : 10000000000ULL;
 
 	max_gws = get_max_mem_alloc_size(ocl_gpu_id) / (UNICODE_LENGTH * VF);
 
@@ -520,8 +520,11 @@ static void crypt_all(int count)
 
 	HANDLE_CLERROR(clEnqueueNDRangeKernel(queue[ocl_gpu_id], GenerateSHA512pwhash, 1, NULL, &scalar_gws, &local_work_size, 0, NULL, firstEvent), "failed in clEnqueueNDRangeKernel");
 
-	for (index = 0; index < spincount / HASH_LOOPS; index++)
+	for (index = 0; index < spincount / HASH_LOOPS; index++) {
 		HANDLE_CLERROR(clEnqueueNDRangeKernel(queue[ocl_gpu_id], crypt_kernel, 1, NULL, &global_work_size, &local_work_size, 0, NULL, NULL), "failed in clEnqueueNDRangeKernel");
+		HANDLE_CLERROR(clFinish(queue[ocl_gpu_id]), "Error running loop kernel");
+		opencl_process_event();
+	}
 
 	HANDLE_CLERROR(clEnqueueNDRangeKernel(queue[ocl_gpu_id], Generate2013key, 1, NULL, &global_work_size, &local_work_size, 0, NULL, lastEvent), "failed in clEnqueueNDRangeKernel");
 

src/opencl_wpapsk_fmt.c
@@ -269,7 +269,7 @@ static void find_best_gws(int do_benchmark, struct fmt_main *self)
 	unsigned int SHAspeed, bestSHAspeed = 0;
 	int optimal_gws = local_work_size;
 	const int sha1perkey = 2 * ITERATIONS * 2 + 6 + 10;
-	unsigned long long int MaxRunTime = cpu(device_info[ocl_gpu_id]) ? 1000000000ULL : 5000000000ULL;
+	unsigned long long int MaxRunTime = cpu(device_info[ocl_gpu_id]) ? 1000000000ULL : 10000000000ULL;
 
 	if (do_benchmark) {
 		fprintf(stderr, "Calculating best keys per crypt (GWS) for LWS=%zd and max. %llu s duration.\n\n", local_work_size, MaxRunTime / 1000000000UL);
@@ -409,13 +409,19 @@ static void crypt_all(int count)
 	/// Run kernel
 	HANDLE_CLERROR(clEnqueueNDRangeKernel(queue[ocl_gpu_id], wpapsk_init, 1, NULL, &scalar_gws, &local_work_size, 0, NULL, firstEvent), "Run initial kernel");
 
-	for (i = 0; i < ITERATIONS / HASH_LOOPS; i++)
+	for (i = 0; i < ITERATIONS / HASH_LOOPS; i++) {
 		HANDLE_CLERROR(clEnqueueNDRangeKernel(queue[ocl_gpu_id], wpapsk_loop, 1, NULL, &global_work_size, &local_work_size, 0, NULL, NULL), "Run loop kernel");
+		HANDLE_CLERROR(clFinish(queue[ocl_gpu_id]), "Error running loop kernel");
+		opencl_process_event();
+	}
 
 	HANDLE_CLERROR(clEnqueueNDRangeKernel(queue[ocl_gpu_id], wpapsk_pass2, 1, NULL, &scalar_gws, &local_work_size, 0, NULL, NULL), "Run intermediate kernel");
 
-	for (i = 0; i < ITERATIONS / HASH_LOOPS; i++)
+	for (i = 0; i < ITERATIONS / HASH_LOOPS; i++) {
 		HANDLE_CLERROR(clEnqueueNDRangeKernel(queue[ocl_gpu_id], wpapsk_loop, 1, NULL, &global_work_size, &local_work_size, 0, NULL, NULL), "Run loop kernel (2nd pass)");
+		HANDLE_CLERROR(clFinish(queue[ocl_gpu_id]), "Error running loop kernel");
+		opencl_process_event();
+	}
 
 	if (hccap.keyver == 1)
 		HANDLE_CLERROR(clEnqueueNDRangeKernel(queue[ocl_gpu_id], wpapsk_final_md5, 1, NULL, &scalar_gws, &local_work_size, 0, NULL, lastEvent), "Run final kernel (MD5)");

src/rar_fmt.c
@@ -587,7 +587,7 @@ static void find_best_gws(int do_benchmark, struct fmt_main *self)
 	unsigned int SHAspeed, bestSHAspeed = 0, max_gws;
 	int optimal_gws = local_work_size;
 	const int sha1perkey = (strlen(self->params.tests[0].plaintext) * 2 + 8 + 3) * 0x40000 / 64 + 16;
-	unsigned long long int MaxRunTime = 5000000000ULL;
+	unsigned long long int MaxRunTime = cpu(device_info[ocl_gpu_id]) ? 1000000000 : 10000000000ULL;
 
 	max_gws = get_max_mem_alloc_size(ocl_gpu_id) / (UNICODE_LENGTH + 8);
 
@@ -896,8 +896,11 @@ static void crypt_all(int count)
 		HANDLE_CLERROR(clEnqueueNDRangeKernel(queue[ocl_gpu_id], RarInit, 1, NULL, &global_work_size, &local_work_size, 0, NULL, firstEvent), "failed in clEnqueueNDRangeKernel");
 		for (k = 0; k < 16; k++) {
 			HANDLE_CLERROR(clEnqueueNDRangeKernel(queue[ocl_gpu_id], RarGetIV, 1, NULL, &global_work_size, &local_work_size, 0, NULL, NULL), "failed in clEnqueueNDRangeKernel");
-			for (j = 0; j < 256 / HASH_LOOPS; j++)
+			for (j = 0; j < 256 / HASH_LOOPS; j++) {
 				HANDLE_CLERROR(clEnqueueNDRangeKernel(queue[ocl_gpu_id], crypt_kernel, 1, NULL, &global_work_size, &local_work_size, 0, NULL, NULL), "failed in clEnqueueNDRangeKernel");
+				HANDLE_CLERROR(clFinish(queue[ocl_gpu_id]), "Error running loop kernel");
+				opencl_process_event();
+			}
 		}
 		HANDLE_CLERROR(clEnqueueNDRangeKernel(queue[ocl_gpu_id], RarFinal, 1, NULL, &global_work_size, &local_work_size, 0, NULL, lastEvent), "failed in clEnqueueNDRangeKernel");
 		// read back aes key & iv

commit 3040fc9656f9e792b3587ae22f9382d2a0fb3bfc

Fix a bug introduced in ef133e4 where a system with OS_TIMER=0 would run into problems when the formats called sig_timer_emu_tick() during benchmark. To fix this, I made bench_running a global variable. Maybe there are better ways. This patch also fixes a minor bug in erasing the "spinning wheel".

src/bench.c
@@ -72,7 +72,7 @@ void clk_tck_init(void)
 
 unsigned int benchmark_time = BENCHMARK_TIME;
 
-static volatile int bench_running;
+volatile int bench_running;
 
 static void bench_handle_timer(int signum)
 {
@@ -256,10 +256,6 @@ char *benchmark_format(struct fmt_main *format, int salts,
 	end_virtual += buf.tms_cutime + buf.tms_cstime;
 	if (end_virtual == start_virtual) end_virtual++;
 	results->virtual = end_virtual - start_virtual;
-#endif
-#ifdef CL_VERSION_1_0
-	/* This erases the 'spinning wheel' cursor from self-test */
-	fprintf(stderr, " \b");
 #endif
 
 	results->real = end_real - start_real;

src/common-opencl.c
@@ -18,28 +18,33 @@ static char *kernel_source;
 static int kernel_loaded;
 static size_t program_size;
 
+extern volatile int bench_running;
+
 void opencl_process_event(void)
 {
+	if (!bench_running) {
 #if !OS_TIMER
-	sig_timer_emu_tick();
+		sig_timer_emu_tick();
 #endif
-	if (event_pending) {
-		if (event_save) {
-			event_save = 0;
-			rec_save();
-		}
+		if (event_pending) {
 
-		if (event_status) {
-			event_status = 0;
-			status_print();
-		}
+			event_pending = event_abort;
 
-		if (event_ticksafety) {
-			event_ticksafety = 0;
-			status_ticks_overflow_safety();
-		}
+			if (event_save) {
+				event_save = 0;
+				rec_save();
+			}
 
-		event_pending = event_abort;
+			if (event_status) {
+				event_status = 0;
+				status_print();
+			}
+
+			if (event_ticksafety) {
+				event_ticksafety = 0;
+				status_ticks_overflow_safety();
+			}
+		}
 	}
 }
 

src/cracker.c
@@ -66,6 +66,11 @@ void crk_init(struct db_main *db, void (*fix_state)(void),
 		error();
 	}
 
+#ifdef CL_VERSION_1_0
+	/* This erases the 'spinning wheel' cursor from self-test */
+	fprintf(stderr, " \b");
+#endif
+
 	crk_db = db;
 	memcpy(&crk_params, &db->format->params, sizeof(struct fmt_params));
 	memcpy(&crk_methods, &db->format->methods, sizeof(struct fmt_methods));