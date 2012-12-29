23f45613277c602440820451e742c207d5da8c8b
magnum <john.magnum@hushmail.com>
2012-12-29 13:17:05 +0100

    common-opencl.c: No need to call include_source() with a cached binary.

--- src/common-opencl.c
+++ src/common-opencl.c
@@ -232,8 +232,7 @@ static void build_kernel_from_binary(int dev_id)
                                        (const unsigned char**)srcptr, NULL, &ret_code );
 	HANDLE_CLERROR(ret_code, "Error while creating program");
 
-	build_code = clBuildProgram(program[dev_id], 0, NULL,
-		include_source("$JOHN/kernels", dev_id, NULL), NULL, NULL);
+	build_code = clBuildProgram(program[dev_id], 0, NULL, NULL, NULL, NULL);
 
 	HANDLE_CLERROR(clGetProgramBuildInfo(program[dev_id], devices[dev_id],
 		CL_PROGRAM_BUILD_LOG, sizeof(opencl_log), (void *) opencl_log,
