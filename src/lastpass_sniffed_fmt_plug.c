ac82ff5c14da242e00ee025ad1597be8ceb8d692
Dhiru Kholia <dhiru@openwall.com>
2013-01-05 00:24:50 +0530

    Add support for cracking LastPass's offline database

--- src/lastpass_sniffed_fmt_plug.c
+++ src/lastpass_sniffed_fmt_plug.c
@@ -68,7 +68,7 @@ static struct custom_salt {
 	unsigned char length;
 } *cur_salt;
 
-void pbkdf2(unsigned char *K, int KL, unsigned char *S, int SL, int R, ARCH_WORD_32 *dgst);
+void pbkdf2_sha256(unsigned char *K, int KL, unsigned char *S, int SL, int R, ARCH_WORD_32 *dgst);
 
 static void init(struct fmt_main *self)
 {
@@ -159,7 +159,7 @@ static void crypt_all(int count)
 		unsigned char iv[16] = { 0 };
 		AES_KEY akey;
 		// PKCS5_PBKDF2_HMAC(saved_key[index], strlen(saved_key[index]), (unsigned char*)cur_salt->username, strlen((char*)cur_salt->username), cur_salt->iterations, EVP_sha256(), 32, key);
-		pbkdf2((unsigned char*)saved_key[index], strlen(saved_key[index]), (unsigned char*)cur_salt->username, strlen((char*)cur_salt->username), cur_salt->iterations, key);
+		pbkdf2_sha256((unsigned char*)saved_key[index], strlen(saved_key[index]), (unsigned char*)cur_salt->username, strlen((char*)cur_salt->username), cur_salt->iterations, key);
 
 		if(AES_set_decrypt_key((const unsigned char *)key, 256, &akey) < 0) {
 			fprintf(stderr, "AES_set_derypt_key failed in crypt!\n");
