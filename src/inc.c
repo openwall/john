openwall/john

commit e82b54e1cba582833f1f46e0a6213cd45d51356c

Here is a patch that removes any plug dependencies.

src/inc.c
@@ -430,9 +430,11 @@ void do_incremental_crack(struct db_main *db, char *mode)
 	if (!mode) {
 		if (db->format == &fmt_LM)
 			mode = "LanMan";
-		else if (db->format == &fmt_NETLM)
+		//else if (db->format == &fmt_NETLM) // This format is a *_plug.c, so decouple from using any linked data
+		else if (!strcmp(db->format->params.label, "netlm"))
 			mode = "LanMan";
-		else if (db->format == &fmt_NETHALFLM)
+		//else if (db->format == &fmt_NETHALFLM) // This format is a *_plug.c, so decouple from using any linked data
+		else if (!strcmp(db->format->params.label, "nethalflm"))
 			mode = "LanMan";
 		else
 			mode = "All";

src/Makefile
@@ -131,6 +131,8 @@ JOHN_OBJS = \
 	rawSHA1_ng_fmt.o \
 	plugin.o \
 	dummy.o \
+	NT_fmt.o \
+	gost.o \
 	batch.o bench.o charset.o common.o compiler.o config.o cracker.o \
 	crc32.o external.o formats.o getopt.o idle.o inc.o john.o list.o \
 	loader.o logger.o math.o memory.o misc.o options.o params.o path.o \
@@ -1818,6 +1820,9 @@ miscnl.o: misc.c
 
 bench-t.o: bench.c
 	$(CC) $(CFLAGS) $(OPT_NORMAL) -D_JOHN_BENCH_TMP bench.c -o bench-t.o
+
+*_fmt_plug.c: Makefile john.c
+	@
 
 fmt_externs.h: $(PLUGFORMATS_SRCS) Makefile *_fmt_plug.c
 	LC_ALL=C $(SED) -n 's/^\(struct fmt_main [^ ]*\) =.*/extern \1;/p' *_fmt_plug.c | LC_ALL=C $(SORT) > fmt_externs.h

src/NT_fmt.c
@@ -315,7 +315,7 @@ static int valid(char *ciphertext, struct fmt_main *self)
         if (!*pos && pos - ciphertext == CIPHERTEXT_LENGTH)
 		return 1;
         else
-        	return 0;
+		return 0;
 
 }
 
@@ -353,8 +353,8 @@ static void *get_binary(char *ciphertext)
 	ciphertext+=4;
 	for (; i<4; i++)
 	{
- 		temp  = (atoi16[ARCH_INDEX(ciphertext[i*8+0])])<<4;
- 		temp |= (atoi16[ARCH_INDEX(ciphertext[i*8+1])]);
+		temp  = (atoi16[ARCH_INDEX(ciphertext[i*8+0])])<<4;
+		temp |= (atoi16[ARCH_INDEX(ciphertext[i*8+1])]);
 
 		temp |= (atoi16[ARCH_INDEX(ciphertext[i*8+2])])<<12;
 		temp |= (atoi16[ARCH_INDEX(ciphertext[i*8+3])])<<8;

src/blake2.h
@@ -19,6 +19,7 @@
 
 #if defined(_MSC_VER)
 #define ALIGN(x) __declspec(align(x))
+#define inline _inline
 #else
 #define ALIGN(x) __attribute__((aligned(x)))
 #endif

src/cracker.c
@@ -113,6 +113,27 @@ static void crk_remove_salt(struct db_salt *salt)
 	*current = salt->next;
 }
 
+/* this utility function is used by cracker.c AND loader.c.  Since media-wiki has a variable width salt, of which
+   in regen_lost_salts mode, we only handle 0 to 99999 as salts, we built a function that will assign the salt from
+   one buffer into another */
+void mediawiki_fix_salt(char *Buf, char *source_to_fix, char *salt_rec, int max_salt_len) {
+	char *cp = source_to_fix;
+	char *cp2 = salt_rec;
+	int i = 0;
+
+	strncpy(Buf, cp, 11+32+1);
+	Buf += (11+32+1);
+	cp += (11+32+1);
+	cp2 += 6;
+	while (++i < max_salt_len && *cp2 != '-') {
+		*Buf++ = *cp2++;
+		++cp;
+	}
+	++cp;
+	*Buf++ = *cp2++;
+	*Buf = 0;
+}
+
 /*
  * Updates the database after a password has been cracked.
  */

src/john.c
@@ -97,6 +97,7 @@ extern int CPU_detect(void);
 
 extern struct fmt_main fmt_DES, fmt_BSDI, fmt_MD5, fmt_BF;
 extern struct fmt_main fmt_AFS, fmt_LM;
+extern struct fmt_main fmt_NT;
 #ifdef HAVE_CRYPT
 extern struct fmt_main fmt_crypt;
 #endif
@@ -244,6 +245,7 @@ static void john_register_all(void)
 	john_register_one(&fmt_BF);
 	john_register_one(&fmt_AFS);
 	john_register_one(&fmt_LM);
+	john_register_one(&fmt_NT);
 
 	for (i = 0; i < cnt; ++i)
 		john_register_one(&(selfs[i]));

src/mediawiki_fmt_plug.c
@@ -81,27 +81,6 @@ static struct fmt_main *pDynamic_9;
 static void mediawiki_init(struct fmt_main *self);
 static void get_ptr();
 
-/* this utility function is used by cracker.c AND loader.c.  Since media-wiki has a variable width salt, of which
-   in regen_lost_salts mode, we only handle 0 to 99999 as salts, we built a function that will assign the salt from
-   one buffer into another */
-void mediawiki_fix_salt(char *Buf, char *source_to_fix, char *salt_rec, int max_salt_len) {
-	char *cp = source_to_fix;
-	char *cp2 = salt_rec;
-	int i = 0;
-
-	strncpy(Buf, cp, 11+32+1);
-	Buf += (11+32+1);
-	cp += (11+32+1);
-	cp2 += 6;
-	while (++i < max_salt_len && *cp2 != '-') {
-		*Buf++ = *cp2++;
-		++cp;
-	}
-	++cp;
-	*Buf++ = *cp2++;
-	*Buf = 0;
-}
-
 /* this function converts a 'native' mediawiki signature string into a Dynamic_9 syntax string */
 static char *Convert(char *Buf, char *ciphertext)
 {

commit 2505c1d8c1483c3591fce14df3cc1787ea973cc7

Fix a bug introduced with e82b54e. When running --stdout, we have a limited format struct, and params.label is NULL.

src/inc.c
@@ -430,11 +430,11 @@ void do_incremental_crack(struct db_main *db, char *mode)
 	if (!mode) {
 		if (db->format == &fmt_LM)
 			mode = "LanMan";
-		//else if (db->format == &fmt_NETLM) // This format is a *_plug.c, so decouple from using any linked data
-		else if (!strcmp(db->format->params.label, "netlm"))
+		else if (db->format->params.label &&
+		         !strcmp(db->format->params.label, "netlm"))
 			mode = "LanMan";
-		//else if (db->format == &fmt_NETHALFLM) // This format is a *_plug.c, so decouple from using any linked data
-		else if (!strcmp(db->format->params.label, "nethalflm"))
+		else if (db->format->params.label &&
+		         !strcmp(db->format->params.label, "nethalflm"))
 			mode = "LanMan";
 		else
 			mode = "All";