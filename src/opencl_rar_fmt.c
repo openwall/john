openwall/john

commit 216cc5789484c44c4fc6f4180c95606cfc34025d

Get rid of Clang warnings for rar2john

src/rar2john.c
@@ -123,13 +123,15 @@ static void process_file(const char *archive_name)
 	size_t bestsize = 0;
 	char best[LINE_BUFFER_SIZE] = "";
 	char *base_aname;
+	char *pp;
 	uint16_t archive_header_head_flags, file_header_head_flags;
 
-	base_aname = basename(strdup(archive_name));
+	pp = strdup(archive_name);
+	base_aname = basename(pp);
 
 	if (!(fp = fopen(archive_name, "rb"))) {
 		fprintf(stderr, "! %s: %s\n", archive_name, strerror(errno));
-		return;
+		goto err;
 	}
 	/* marker block */
 	memset(marker_block, 0, 7);
@@ -137,8 +139,7 @@ static void process_file(const char *archive_name)
 	assert(count == 1);
 	if (memcmp(marker_block, "\x52\x61\x72\x21\x1a\x07\x00", 7)) {
 		fprintf(stderr, "! %s: Not a RAR file\n", archive_name);
-		fclose(fp);
-		return;
+		goto err;
 	}
 
 	/* archive header block */
@@ -200,7 +201,8 @@ next_file_header:
 		size_t file_header_pack_size, file_header_unp_size;
 		int EXT_TIME_SIZE;
 		uint16_t file_header_head_size, file_name_size;
-		unsigned char file_name[256], SALT[8], FILE_CRC[4];
+		unsigned char file_name[256], FILE_CRC[4];
+		unsigned SALT[8] = { 0 };
 		char rejbuf[32];
 		long pos;
 
@@ -387,6 +389,13 @@ BailOut:
 			fprintf(stderr, "Did not find a valid encrypted candidate in %s\n", base_aname);
 	}
 	fclose(fp);
+
+err:
+	if (fp)
+		fclose(fp);
+	if (pp)
+		free(pp);
+
 }
 
 int rar2john(int argc, char **argv)

commit 687cab9f25ec956a582543692cfe5a9a944acd6f

Fix a hideous bug in rar2john introduced by in 216cc57. A variable was declared as just "unsigned" (taken as int) instead of "unsigned char" which lead to incorrect output. Not sure why the compiler did not warn.

src/opencl_rar_fmt.c
@@ -685,13 +685,13 @@ static void init(struct fmt_main *self)
 	if (!local_work_size) {
 		if (getenv("LWS")) {
 			/* LWS was explicitly set to 0 */
-			int temp = global_work_size;
+			int tmp = global_work_size;
 			local_work_size = maxsize;
 			global_work_size = global_work_size ? global_work_size : 4 * maxsize;
 			create_clobj(global_work_size, self);
 			opencl_find_best_workgroup_limit(self, maxsize);
 			release_clobj();
-			global_work_size = temp;
+			global_work_size = tmp;
 		} else {
 			if (cpu(device_info[ocl_gpu_id])) {
 				if (get_platform_vendor_id(platform_id) == DEV_INTEL)
@@ -785,7 +785,7 @@ static int valid(char *ciphertext, struct fmt_main *self)
 		return 1;
 	} else {
 		int inlined;
-		size_t plen, ulen;
+		long long plen, ulen;
 
 		if (hexlen(ptr) != 8) /* 4 bytes of CRC */
 			goto error;
@@ -812,17 +812,14 @@ static int valid(char *ciphertext, struct fmt_main *self)
 		} else {
 			FILE *fp;
 			char *archive_name;
-			size_t pos;
 
 			archive_name = ptr;
-			if (!(ptr = strtok(NULL, "*")))
-				goto error;
-			if ((pos = atoll(ptr)) < 0)
-				goto error;
 			if (!(fp = fopen(archive_name, "rb"))) {
-				fprintf(stderr, "! %s: %s\n", archive_name, strerror(errno));
+				fprintf(stderr, "! %s: %s, skipping.\n", archive_name, strerror(errno));
 				goto error;
 			}
+			/* We could go on and actually try seeking to pos
+			   but this is enough for now */
 			fclose(fp);
 		}
 	}
@@ -830,6 +827,13 @@ static int valid(char *ciphertext, struct fmt_main *self)
 	return 1;
 
 error:
+#ifdef DEBUG
+	{
+		char buf[68];
+		strnzcpy(buf, ciphertext, sizeof(buf));
+		fprintf(stderr, "rejecting %s\n", buf);
+	}
+#endif
 	MEM_FREE(keeptr);
 	return 0;
 }

src/rar2john.c
@@ -199,10 +199,10 @@ next_file_header:
 		printf(":%d::::%s\n", type, archive_name);
 	} else {
 		size_t file_header_pack_size, file_header_unp_size;
-		int EXT_TIME_SIZE;
+		int ext_time_size;
 		uint16_t file_header_head_size, file_name_size;
-		unsigned char file_name[256], FILE_CRC[4];
-		unsigned SALT[8] = { 0 };
+		unsigned char file_name[256], file_crc[4];
+		unsigned char salt[8] = { 0 };
 		char rejbuf[32];
 		long pos;
 
@@ -222,7 +222,7 @@ next_file_header:
 		        file_header_unp_size);
 #endif
 		/* calculate EXT_TIME size */
-		EXT_TIME_SIZE = file_header_head_size - 32;
+		ext_time_size = file_header_head_size - 32;
 
 		if (file_header_head_flags & 0x100) {
 #ifdef DEBUG
@@ -230,7 +230,7 @@ next_file_header:
 #endif
 			count = fread(rejbuf, 4, 1, fp);
 			assert(count == 1);
-			EXT_TIME_SIZE -= 4;
+			ext_time_size -= 4;
 		}
 		if (file_header_head_flags & 0x100) {
 #ifdef DEBUG
@@ -238,7 +238,7 @@ next_file_header:
 #endif
 			count = fread(rejbuf, 4, 1, fp);
 			assert(count == 1);
-			EXT_TIME_SIZE -= 4;
+			ext_time_size -= 4;
 		}
 		/* file name processing */
 		file_name_size =
@@ -249,7 +249,7 @@ next_file_header:
 		memset(file_name, 0, sizeof(file_name));
 		count = fread(file_name, file_name_size, 1, fp);
 		assert(count == 1);
-		EXT_TIME_SIZE -= file_name_size;
+		ext_time_size -= file_name_size;
 
 		/* If this flag is set, file_name contains some weird
 		   wide char encoding that need to be decoded to UTF16
@@ -276,10 +276,10 @@ next_file_header:
 		} else
 			fprintf(stderr, "file name: %s\n", file_name);
 
-		/* SALT processing */
+		/* salt processing */
 		if (file_header_head_flags & 0x400) {
-			EXT_TIME_SIZE -= 8;
-			count = fread(SALT, 8, 1, fp);
+			ext_time_size -= 8;
+			count = fread(salt, 8, 1, fp);
 			assert(count == 1);
 		}
 
@@ -287,9 +287,9 @@ next_file_header:
 		if (file_header_head_flags & 0x1000) {
 #ifdef DEBUG
 			fprintf(stderr, "! EXT_TIME present with size %d\n",
-			    EXT_TIME_SIZE);
+			    ext_time_size);
 #endif
-			count = fread(rejbuf, EXT_TIME_SIZE, 1, fp);
+			count = fread(rejbuf, ext_time_size, 1, fp);
 			assert(count == 1);
 		}
 
@@ -329,13 +329,14 @@ next_file_header:
 
 		/* process encrypted data of size "file_header_pack_size" */
 		sprintf(best, "%s:$RAR3$*%d*", base_aname, type);
-		for (i = 0; i < 8; i++) { /* encode SALT */
-			sprintf(&best[strlen(best)], "%c%c", itoa16[ARCH_INDEX(SALT[i] >> 4)], itoa16[ARCH_INDEX(SALT[i] & 0x0f)]);
+		for (i = 0; i < 8; i++) { /* encode salt */
+			sprintf(&best[strlen(best)], "%c%c", itoa16[ARCH_INDEX(salt[i] >> 4)], itoa16[ARCH_INDEX(salt[i] & 0x0f)]);
 		}
+		fprintf(stderr, "salt: '%s'\n", best);
 		sprintf(&best[strlen(best)], "*");
-		memcpy(FILE_CRC, file_header_block + 16, 4);
-		for (i = 0; i < 4; i++) { /* encode FILE_CRC */
-			sprintf(&best[strlen(best)], "%c%c", itoa16[ARCH_INDEX(FILE_CRC[i] >> 4)], itoa16[ARCH_INDEX(FILE_CRC[i] & 0x0f)]);
+		memcpy(file_crc, file_header_block + 16, 4);
+		for (i = 0; i < 4; i++) { /* encode file_crc */
+			sprintf(&best[strlen(best)], "%c%c", itoa16[ARCH_INDEX(file_crc[i] >> 4)], itoa16[ARCH_INDEX(file_crc[i] & 0x0f)]);
 		}
 		/* Minimal version needed to unpack this file */
 		fprintf(stderr, "! UNP_VER is %0.1f\n", (float)file_header_block[24] / 10.);

src/rar_fmt.c
@@ -410,7 +410,7 @@ static int valid(char *ciphertext, struct fmt_main *self)
 		return 1;
 	} else {
 		int inlined;
-		size_t plen, ulen;
+		long long plen, ulen;
 
 		if (hexlen(ptr) != 8) /* 4 bytes of CRC */
 			goto error;
@@ -437,17 +437,14 @@ static int valid(char *ciphertext, struct fmt_main *self)
 		} else {
 			FILE *fp;
 			char *archive_name;
-			size_t pos;
 
 			archive_name = ptr;
-			if (!(ptr = strtok(NULL, "*")))
-				goto error;
-			if ((pos = atoll(ptr)) < 0)
-				goto error;
 			if (!(fp = fopen(archive_name, "rb"))) {
-				fprintf(stderr, "! %s: %s\n", archive_name, strerror(errno));
+				fprintf(stderr, "! %s: %s, skipping.\n", archive_name, strerror(errno));
 				goto error;
 			}
+			/* We could go on and actually try seeking to pos
+			   but this is enough for now */
 			fclose(fp);
 		}
 	}
@@ -455,6 +452,13 @@ static int valid(char *ciphertext, struct fmt_main *self)
 	return 1;
 
 error:
+#ifdef DEBUG
+	{
+		char buf[68];
+		strnzcpy(buf, ciphertext, sizeof(buf));
+		fprintf(stderr, "rejecting %s\n", buf);
+	}
+#endif
 	MEM_FREE(keeptr);
 	return 0;
 }