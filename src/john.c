openwall/john

commit e14ac00ae6e1eb7f18208df8a150958c4d2a064d

WPAPSK formats has a minimum password length of 8. This patch adds code to john.c that enforces this. We may want to add min_length in the format struct later on, and remove this hack.

src/john.c
@@ -720,6 +720,27 @@ static void john_run(void)
 					fprintf(stderr, "Note: Running both MPI and OMP (see doc/README.mpi)\n");
 		}
 #endif
+
+		/* WPA-PSK has a min-length of 8. Until the format struct can
+		   hold this information, we need this hack here. */
+		if (!strncmp(database.format->params.label, "wpapsk", 6) &&
+		    options.force_minlength < 8) {
+			options.force_minlength = 8;
+			fprintf(stderr, "Note: minimum length forced to 8\n");
+
+			/* Now we need to re-check this */
+			if (options.force_maxlength &&
+			    options.force_maxlength < options.force_minlength) {
+#ifdef HAVE_MPI
+				if (mpi_id == 0)
+#endif
+					fprintf(stderr, "Invalid option: "
+					        "--max-length smaller than "
+					        "minimum length for format\n");
+				error();
+			}
+		}
+
 		if (options.flags & FLG_SINGLE_CHK)
 			do_single_crack(&database);
 		else

commit 03ad5f5d7f67e5b115c52c325d4f8bdb19091068

Fix a bug when using --stdout, introduced in e14ac00.

src/john.c
@@ -723,7 +723,8 @@ static void john_run(void)
 
 		/* WPA-PSK has a min-length of 8. Until the format struct can
 		   hold this information, we need this hack here. */
-		if (!strncmp(database.format->params.label, "wpapsk", 6) &&
+		if (database.format->params.label &&
+		    !strncmp(database.format->params.label, "wpapsk", 6) &&
 		    options.force_minlength < 8) {
 			options.force_minlength = 8;
 			fprintf(stderr, "Note: minimum length forced to 8\n");