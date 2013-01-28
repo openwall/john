6307bf13b61c05cab2126d96cf0beac114dc7094
Dhiru Kholia <dhiru@openwall.com>
2013-01-29 01:42:17 +0530

    Integrate dmg2john with john

--- src/john.c
+++ src/john.c
@@ -204,6 +204,7 @@ extern int keyring2john(int argc, char **argv);
 extern int rar2john(int argc, char **argv);
 extern int racf2john(int argc, char **argv);
 extern int pwsafe2john(int argc, char **argv);
+extern int dmg2john(int argc, char **argv);
 #endif
 extern int zip2john(int argc, char **argv);
 
@@ -917,6 +918,11 @@ int main(int argc, char **argv)
 	if (!strcmp(name, "pwsafe2john")) {
 		CPU_detect_or_fallback(argv, 0);
 		return pwsafe2john(argc, argv);
+	}
+
+	if (!strcmp(name, "dmg2john")) {
+		CPU_detect_or_fallback(argv, 0);
+		return dmg2john(argc, argv);
 	}
 #endif
 
