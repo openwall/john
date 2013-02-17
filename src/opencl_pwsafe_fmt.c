971d862f8bdecb93987f91a4d018b421059ce91c
bwall <nightstrike9809@gmail.com>
2013-02-17 22:45:12 +0100

    Increased the plaintext size to 87 in pwsafe-opencl

--- src/opencl_pwsafe_fmt.c
+++ src/opencl_pwsafe_fmt.c
@@ -31,7 +31,7 @@
 #define ALGORITHM_NAME          "OpenCL"
 #define BENCHMARK_COMMENT       ""
 #define BENCHMARK_LENGTH        0
-#define PLAINTEXT_LENGTH        23
+#define PLAINTEXT_LENGTH        87
 #define BINARY_SIZE             0
 #define KERNEL_INIT_NAME	"pwsafe_init"
 #define KERNEL_RUN_NAME   	"pwsafe_iter"
@@ -66,6 +66,8 @@ static struct fmt_tests pwsafe_tests[] = {
 	{"$pwsafe$*3*fefc1172093344c9d5577b25f5b4b6e5d2942c94f9fc24c21733e28ae6527521*2048*88cbaf7d8668c1a98263f5dce7cb39c3304c49a3e0d76a7ea475dc02ab2f97a7", "12345678"},
 	{"$pwsafe$*3*581cd1135b9b993ccb0f6b01c1fcfacd799c69960496c96286f94fe1400c1b25*2048*4ab3c2d3af251e94eb2f753fdf30fb9da074bec6bac0fa9d9d152b95fc5795c6", "openwall"},
 	{"$pwsafe$*3*34ba0066d0fc594c126b60b9db98b6024e1cf585901b81b5b005ce386f173d4c*2048*cc86f1a5d930ff19b3602770a86586b5d9dea7bb657012aca875aa2a7dc71dc0", "12345678901234567890123"},
+	{"$pwsafe$*3*a42431191707895fb8d1121a3a6e255e33892d8eecb50fc616adab6185b5affb*2048*0f71d12df2b7c5394ae90771f6475a7ad0437007a8eeb5d9b58e35d8fd57c827", "123456789012345678901234567"},
+	{"$pwsafe$*3*c380dee0dbb536f5454f78603b020be76b33e294e9c2a0e047f43b9c61669fc8*2048*e88ed54a85e419d555be219d200563ae3ba864e24442826f412867fc0403917d", "this is an 87 character password to test the max bound of pwsafe-opencl................"},
 	{NULL}
 };
 
@@ -74,7 +76,7 @@ cl_kernel finish_kernel;
 
 //Also acts as the hash state
 typedef struct {
-	uint8_t v[32];
+	uint8_t v[87];
 	uint32_t length;
 } pwsafe_pass;
 
@@ -332,7 +334,7 @@ static void set_salt(void *salt)
 static int crypt_all_benchmark(int *pcount, struct db_salt *salt)
 {
 	int count = *pcount;
-	int i = 0;
+	int i;
 
 	global_work_size = (count + local_work_size - 1) / local_work_size * local_work_size;
 
