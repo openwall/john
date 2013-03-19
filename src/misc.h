88d488fe4816ebc9dc28211c5a66b63b28afe9aa
JimF <jfoug@cox.net>
2013-03-19 20:30:53 +0100

    basename portability 'fix'.

--- src/misc.h
+++ src/misc.h
@@ -66,6 +66,45 @@ extern int strnzcpyn(char *dst, const char *src, int size);
 extern char *strnzcat(char *dst, const char *src, int size);
 
 /*
+ * Portable basename() function.  DO NOT USE basename().  Use this
+ * proper working equivelent.  The _r version is thread safe. In the
+ * _r version, pass in a buffer that is at least strlen(name)+1 bytes
+ * long, however, PATH_BUFFER_SIZE+1 can also be used.
+ *
+ *  here is what defined:
+ *    if name is null, or points to a null string (0 byte), then a '.' is returned.
+ *    if name is all / chars (or \ chars), then a single / (or \) is returned.
+ *    DOS drive letters are ignored.
+ *    / or \ chars are properly handled.
+ *    Trailing / (or \) are removed, IF there was real path data in there.
+ *
+ *  here are some examples:
+ *    jtr_basename("/user/lib")      == lib
+ *    jtr_basename("/user/")         == user
+ *    jtr_basename("/")              == /
+ *    jtr_basename("//")             == /
+ *    jtr_basename("///")            == /
+ *    jtr_basename("//user//lib//")  == lib
+ *    jtr_basename("c:\\txt.doc")    == txt.doc
+ *    jtr_basename("c:txt.doc")      == txt.doc
+ *    jtr_basename("c:b/c\\txt.doc/")== txt.doc
+ *    jtr_basename("c:\\txt.doc\\")  == txt.doc
+ *    jtr_basename("c:")             == .
+ *    jtr_basename("")               == .
+ *    jtr_basename(NULL)             == .
+ *    jtr_basename("\\user\\lib")    == lib
+ *    jtr_basename("\\user\\")       == user
+ *    jtr_basename("\\")             == \
+ *    jtr_basename("\\\\")           == \
+ *    jtr_basename("one")            == one
+ */
+extern char *jtr_basename(const char *name);
+extern char *jtr_basename_r(const char *name, char *buf);
+#undef basename
+#define basename(a) jtr_basename(a)
+
+
+/*
  * Converts a string to lowercase.
  */
 #ifndef _MSC_VER
