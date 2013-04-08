b06e9924124f1c82fd435c951419db15bdbdf536
magnum <john.magnum@hushmail.com>
2013-04-08 20:06:31 +0200

    PDF format still used the Bad Idea[tm] version of valid().

--- src/pdf_fmt_plug.c
+++ src/pdf_fmt_plug.c
@@ -97,11 +97,14 @@ static void init(struct fmt_main *self)
 
 static int valid(char *ciphertext, struct fmt_main *self)
 {
-	char *ctcopy = strdup(ciphertext);
-	char *keeptr = ctcopy;
+	char *ctcopy, *keeptr;
 	char *p;
+
 	if (strncmp(ciphertext,  "$pdf$", 5) != 0)
-		goto err;
+		return 0;
+
+	ctcopy = strdup(ciphertext);
+	keeptr = ctcopy;
 	ctcopy += 5;
 	if ((p = strtok(ctcopy, "*")) == NULL)	/* V */
 		goto err;
