79c871dc5aa630f0a228b183fb1c403c96a38f6d
JimF <jfoug@cox.net>
2013-04-09 19:21:24 +0200

    Move alloc/free out of parallel region in putty format

--- src/putty_fmt_plug.c
+++ src/putty_fmt_plug.c
@@ -250,14 +250,13 @@ static int LAME_ssh2_load_userkey(char *passphrase)
 		char realmac[41];
 		unsigned char binary[20];
 		unsigned char *macdata;
+		unsigned char macdata_ar[4*5+sizeof(cur_salt->alg)+sizeof(cur_salt->encryption)+sizeof(cur_salt->comment)+sizeof(cur_salt->public_blob_len)+sizeof(cur_salt->private_blob_len)+1];
 		int maclen;
-		int free_macdata;
 		int i;
 		if (cur_salt->old_fmt) {
 			/* MAC (or hash) only covers the private blob. */
 			macdata = out;
 			maclen = cur_salt->private_blob_len;
-			free_macdata = 0;
 		} else {
 			unsigned char *p;
 			int namelen = strlen(cur_salt->alg);
@@ -268,15 +267,14 @@ static int LAME_ssh2_load_userkey(char *passphrase)
 					4 + commlen +
 					4 + cur_salt->public_blob_len +
 					4 + cur_salt->private_blob_len);
-			macdata = (unsigned char*)mem_alloc(maclen);
-			p = macdata;
+			p = macdata_ar;
 #define DO_STR(s,len) PUT_32BIT(p,(len));memcpy(p+4,(s),(len));p+=4+(len)
 			DO_STR(cur_salt->alg, namelen);
 			DO_STR(cur_salt->encryption, enclen);
 			DO_STR(cur_salt->comment, commlen);
 			DO_STR(cur_salt->public_blob, cur_salt->public_blob_len);
 			DO_STR(out, cur_salt->private_blob_len);
-			free_macdata = 1;
+			macdata = macdata_ar;
 		}
 		if (cur_salt->is_mac) {
 			SHA_CTX s;
@@ -297,8 +295,6 @@ static int LAME_ssh2_load_userkey(char *passphrase)
 		} else {
 			SHA_Simple(macdata, maclen, binary);
 		}
-		if (free_macdata)
-			MEM_FREE(macdata);
 		for (i = 0; i < 20; i++)
 			sprintf(realmac + 2 * i, "%02x", binary[i]);
 
