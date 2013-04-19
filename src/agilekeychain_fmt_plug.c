openwall/john

commit 07a3e05c3032673e078b509dbebf98c7c08bf167

Even more optimizations (courtesy of atom)

src/agilekeychain_fmt_plug.c
@@ -154,19 +154,19 @@ static void set_salt(void *salt)
 
 static int akcdecrypt(unsigned char *derived_key, unsigned char *data)
 {
-	unsigned char key[16];
-	unsigned char iv[16] = { 0 };
+	unsigned char *iv = data + CTLEN - 32;
 	unsigned char out[CTLEN];
 	int pad, n, i, key_size;
 	AES_KEY akey;
-	memcpy(key, derived_key, 16);
-	memset(out, 0, sizeof(out));
-	memset(&akey, 0, sizeof(AES_KEY));
 
-	if(AES_set_decrypt_key(key, 128, &akey) < 0) {
+	// memcpy(key, derived_key, 16);
+	// memset(out, 0, sizeof(out));
+	// memset(&akey, 0, sizeof(AES_KEY));
+
+	if(AES_set_decrypt_key(derived_key, 128, &akey) < 0) {
 		fprintf(stderr, "AES_set_derypt_key failed in crypt!\n");
 	}
-	AES_cbc_encrypt(data + CTLEN - 32, out + CTLEN - 32, 32, &akey, iv, AES_DECRYPT);
+	AES_cbc_encrypt(data + CTLEN - 16, out + CTLEN - 16, 16, &akey, iv, AES_DECRYPT);
 
 	// now check padding
 	pad = out[CTLEN - 1];

src/o5logon_fmt_plug.c
@@ -150,7 +150,7 @@ static int crypt_all(int *pcount, struct db_salt *salt)
 	{
 		unsigned char key[24];
 		unsigned char pt[48];
-		unsigned char iv[16];
+		unsigned char *iv = cur_salt->ct + 16;
 		AES_KEY akey;
 		SHA_CTX ctx;
 
@@ -161,7 +161,7 @@ static int crypt_all(int *pcount, struct db_salt *salt)
 		SHA1_Final(key, &ctx);
 
 		AES_set_decrypt_key(key, 192, &akey);
-		AES_cbc_encrypt(cur_salt->ct + 16, pt + 16, 32, &akey, iv, AES_DECRYPT);
+		AES_cbc_encrypt(cur_salt->ct + 32, pt + 32, 16, &akey, iv, AES_DECRYPT);
 		if (!memcmp(pt + 40, "\x08\x08\x08\x08\x08\x08\x08\x08", 8))
 			any_cracked = cracked[index] = 1;
 	}

commit 2f866f438db7d79576f59ef55474ce3f23b4c8a4

Don't be too greedy. Fix failures caused by 07a3e05.

src/agilekeychain_fmt_plug.c
@@ -156,11 +156,11 @@ static void set_salt(void *salt)
 
 static int akcdecrypt(unsigned char *derived_key, unsigned char *data)
 {
-	unsigned char *iv = data + CTLEN - 32;
 	unsigned char out[CTLEN];
 	int pad, n, i, key_size;
 	AES_KEY akey;
-
+	unsigned char iv[16];
+	memcpy(iv, data + CTLEN - 32, 16);
 	// memcpy(key, derived_key, 16);
 	// memset(out, 0, sizeof(out));
 	// memset(&akey, 0, sizeof(AES_KEY));

src/o5logon_fmt_plug.c
@@ -149,11 +149,13 @@ static void crypt_all(int count)
 	{
 		unsigned char key[24];
 		unsigned char pt[48];
-		unsigned char *iv = cur_salt->ct + 16;
+		unsigned char iv[16];
 		AES_KEY akey;
 		SHA_CTX ctx;
 
 		memset(&key[20], 0, 4);
+		memcpy(iv, cur_salt->ct + 16, 16);
+
 		SHA1_Init(&ctx);
 		SHA1_Update(&ctx, saved_key[index], strlen(saved_key[index]));
 		SHA1_Update(&ctx, cur_salt->salt, 10);