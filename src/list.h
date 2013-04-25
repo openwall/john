e5b072bac04cfe846bd559c3a1bd39a34c30082e
Solar Designer <solar@openwall.com>
2013-04-25 02:56:37 +0000

    Optimized the charset file generation algorithms and code ("--make-charset"). On a certain test case, this has improved the time to generate a charset file from 55.5 to 19.5 seconds while generating the exact same file.

--- src/list.h
+++ src/list.h
@@ -1,6 +1,6 @@
 /*
  * This file is part of John the Ripper password cracker,
- * Copyright (c) 1996-98 by Solar Designer
+ * Copyright (c) 1996-98,2013 by Solar Designer
  */
 
 /*
@@ -40,19 +40,26 @@ extern void list_init(struct list_main **list);
 extern void list_add(struct list_main *list, char *data);
 
 /*
+ * Adds a previously allocated entry to the list.
+ */
+extern void list_add_link(struct list_main *list, struct list_entry *entry);
+
+/*
  * Adds multiple entries to the list from a comma-separated string.
  */
 extern void list_add_multi(struct list_main *list, char *data);
 
 /*
- * Adds an entry to the list checking for dupes. This is slow, and should
+ * Adds an entry to the list checking for dupes.  This is slow, and should
  * only be used on tiny lists.
  */
 extern void list_add_unique(struct list_main *list, char *data);
 
+#if 0
 /*
  * Deletes the entry following prev from the list.
  */
 extern void list_del_next(struct list_main *list, struct list_entry *prev);
+#endif
 
 #endif
