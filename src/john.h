6a2e044a13500565f0b9731536bbace81ec89129
solar <solar>
2013-04-30 13:57:24 +0000

    John's global variables exported from john.c to other source files (currently this is just john_main_process).

---
+++ src/john.h
@@ -0,0 +1,19 @@
+/*
+ * This file is part of John the Ripper password cracker,
+ * Copyright (c) 2013 by Solar Designer
+ */
+
+/*
+ * John's global variables exported from john.c to other source files.
+ */
+
+#ifndef _JOHN_JOHN_H
+#define _JOHN_JOHN_H
+
+/*
+ * Are we the main process?  (The only process or the parent of a group of
+ * child processes.)
+ */
+extern int john_main_process;
+
+#endif
