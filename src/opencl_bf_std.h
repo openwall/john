6dad03c25f8040bc1028719f95b068e9f065ea05
Sayantan Datta <std2048@gmail.com>
2013-05-18 21:11:32 +0530

    bf-opencl:code cleanup

--- src/opencl_bf_std.h
+++ src/opencl_bf_std.h
@@ -16,15 +16,15 @@ typedef unsigned int BF_word ;
  * Binary salt type, also keeps the number of rounds and hash sub-type.
  */
 typedef struct {
-	BF_word salt[4];
-	unsigned char rounds;
-	char subtype;
+	BF_word salt[4] ;
+	unsigned char rounds ;
+	char subtype ;
 } BF_salt ;
 
 /*
  * Binary ciphertext type.
  */
-typedef BF_word BF_binary[6];
+typedef BF_word BF_binary[6] ;
 
 /*NOTE: If you change the WORK_GROUP_SIZE here it must also be changed in bf_kernel.cl*/
 /*
@@ -42,7 +42,7 @@ typedef BF_word BF_binary[6];
 #define NUM_CHANNELS                    1   
 #define WAVEFRONT_SIZE                  1
 #define CHANNEL_INTERLEAVE              (WAVEFRONT_SIZE*NUM_CHANNELS)
-#define MULTIPLIER                      4096
+#define MULTIPLIER                      1024
 #define BF_N				(CHANNEL_INTERLEAVE*MULTIPLIER)
 #define MAX_DEVICES_PER_PLATFORM        8
 #define GWS_CONFIG		        "bf_GWS"
@@ -55,43 +55,43 @@ extern BF_binary *opencl_BF_out ;
 /*
  * ASCII to binary conversion table, for use in BF_fmt.valid().
  */
-extern unsigned char opencl_BF_atoi64[0x80];
+extern unsigned char opencl_BF_atoi64[0x80] ;
 
 /*
  * Sets a key for BF_std_crypt().
  */
-extern void opencl_BF_std_set_key(char *key, int index, int sign_extension_bug);
+extern void opencl_BF_std_set_key(char *key, int index, int sign_extension_bug) ;
 
 /*
  * Main hashing routine, sets first two words of BF_out
  * (or all words in an OpenMP-enabled build).
  */
-extern void opencl_BF_std_crypt(BF_salt *salt, int n);
+extern void opencl_BF_std_crypt(BF_salt *salt, int n) ;
 
 /*
  * Calculates the rest of BF_out, for exact comparison.
  */
-extern void opencl_BF_std_crypt_exact(int index);
+extern void opencl_BF_std_crypt_exact(int index) ;
 
 /*
  * Returns the salt for BF_std_crypt().
  */
-extern void *opencl_BF_std_get_salt(char *ciphertext);
+extern void *opencl_BF_std_get_salt(char *ciphertext) ;
 
 /*
  * Converts an ASCII ciphertext to binary.
  */
-extern void *opencl_BF_std_get_binary(char *ciphertext);
+extern void *opencl_BF_std_get_binary(char *ciphertext) ;
 
 /*
  * Select a device: BF_select_device(platform_id,device_id)
  */
 
-extern void BF_select_device(struct fmt_main*);
+extern void BF_select_device(struct fmt_main*) ;
 
 /*
  * Clear all GPU Buffers
  */
-extern void BF_clear_buffer(void);
+extern void BF_clear_buffer(void) ;
 
 #endif
