5e0786b8d7c337c6f11094123eec43d5e93004a3
Dhiru Kholia <dhiru@openwall.com>
2013-05-18 14:21:26 +0530

    Add support for cracking Snefru hashes

---
+++ src/snefru.h
@@ -0,0 +1,40 @@
+/* snefru.h */
+#ifndef SNEFRU_H
+#define SNEFRU_H
+#include "stdint.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Snefru-128 processses message by blocks of 48 bytes, */
+/* and Snefru-256 uses blocks of 32 bytes */
+
+/* here we declare the maximal block size */
+#define snefru_block_size 48
+
+#define snefru128_hash_length 16
+#define snefru256_hash_length 32
+
+/* algorithm context */
+typedef struct snefru_ctx
+{
+	unsigned hash[8];         /* algorithm 512-bit hashing state */
+	unsigned char buffer[48]; /* 384-bit message block */
+	uint64_t length;          /* processed message length */
+	unsigned index;           /* index in the buffer of the last byte stored */
+	unsigned digest_length;   /* length of the algorithm digest in bytes */
+} snefru_ctx;
+
+/* hash functions */
+
+void rhash_snefru128_init(snefru_ctx *ctx);
+void rhash_snefru256_init(snefru_ctx *ctx);
+void rhash_snefru_update(snefru_ctx *ctx, const unsigned char *data, size_t size);
+void rhash_snefru_final(snefru_ctx *ctx, unsigned char* result);
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif /* __cplusplus */
+
+#endif /* SNEFRU_H */
---
+++ src/snefru_fmt_plug.c
@@ -0,0 +1,327 @@
+/* Snefru cracker patch for JtR. Hacked together during May of 2013 by Dhiru
+ * Kholia <dhiru at openwall.com>.
+ *
+ * This software is Copyright (c) 2013 Dhiru Kholia <dhiru at openwall.com> and
+ * it is hereby released to the general public under the following terms:
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted.
+ */
+
+#include <string.h>
+#include "arch.h"
+#include "snefru.h"
+#include "misc.h"
+#include "common.h"
+#include "formats.h"
+#include "params.h"
+#include "options.h"
+#ifdef _OPENMP
+static int omp_t = 1;
+#include <omp.h>
+#define OMP_SCALE               1 // FIXME
+#endif
+
+#define FORMAT_TAG		"$snefru$"
+#define TAG_LENGTH		8
+#define ALGORITHM_NAME		"32/" ARCH_BITS_STR
+#define BENCHMARK_COMMENT	""
+#define BENCHMARK_LENGTH	-1
+#define PLAINTEXT_LENGTH	125
+#define BINARY_SIZE		32
+#define CMP_SIZE		16
+#define SALT_SIZE		0
+#define MIN_KEYS_PER_CRYPT	1
+#define MAX_KEYS_PER_CRYPT	1
+
+static struct fmt_tests snefru_128_tests[] = {
+	{"$snefru$53b8a9b1c9ed00174d88d705fb7bae30", "mystrongpassword"},
+	{NULL}
+};
+
+static struct fmt_tests snefru_256_tests[] = {
+	{"$snefru$4170e04e900e6221562ceb5ff6ea27fa9b9b0d9587add44a4379a02619c5a106", "mystrongpassword"},
+	{NULL}
+};
+
+static char (*saved_key)[PLAINTEXT_LENGTH + 1];
+static ARCH_WORD_32 (*crypt_out)[BINARY_SIZE / sizeof(ARCH_WORD_32)];
+
+static void init(struct fmt_main *self)
+{
+#ifdef _OPENMP
+	omp_t = omp_get_max_threads();
+	self->params.min_keys_per_crypt *= omp_t;
+	omp_t *= OMP_SCALE;
+	self->params.max_keys_per_crypt *= omp_t;
+#endif
+	saved_key = mem_calloc_tiny(sizeof(*saved_key) *
+			self->params.max_keys_per_crypt, MEM_ALIGN_WORD);
+	crypt_out = mem_calloc_tiny(sizeof(*crypt_out) * self->params.max_keys_per_crypt, MEM_ALIGN_WORD);
+}
+
+// XXX fix me
+static int valid(char *ciphertext, struct fmt_main *self)
+{
+	char *p;
+
+	p = ciphertext;
+
+	if (!strncmp(p, FORMAT_TAG, TAG_LENGTH))
+		p += TAG_LENGTH;
+	if (strlen(p) != 32 && strlen(p) != 64)
+		return 0;
+
+	return 1;
+}
+
+static void *get_binary_256(char *ciphertext)
+{
+	static union {
+		unsigned char c[32];
+		ARCH_WORD dummy;
+	} buf;
+	unsigned char *out = buf.c;
+	char *p;
+	int i;
+
+	if (!strncmp(ciphertext, FORMAT_TAG, TAG_LENGTH))
+		p = strrchr(ciphertext, '$') + 1;
+	else
+		p = ciphertext;
+	for (i = 0; i < 32; i++) {
+		out[i] =
+		    (atoi16[ARCH_INDEX(*p)] << 4) |
+		    atoi16[ARCH_INDEX(p[1])];
+		p += 2;
+	}
+
+	return out;
+}
+
+static void *get_binary_128(char *ciphertext)
+{
+	static union {
+		unsigned char c[16];
+		ARCH_WORD dummy;
+	} buf;
+	unsigned char *out = buf.c;
+	char *p;
+	int i;
+
+	if (!strncmp(ciphertext, FORMAT_TAG, TAG_LENGTH))
+		p = strrchr(ciphertext, '$') + 1;
+	else
+		p = ciphertext;
+	for (i = 0; i < 16; i++) {
+		out[i] =
+		    (atoi16[ARCH_INDEX(*p)] << 4) |
+		    atoi16[ARCH_INDEX(p[1])];
+		p += 2;
+	}
+
+	return out;
+}
+
+static int binary_hash_0(void *binary) { return *(ARCH_WORD_32 *)binary & 0xf; }
+static int binary_hash_1(void *binary) { return *(ARCH_WORD_32 *)binary & 0xff; }
+static int binary_hash_2(void *binary) { return *(ARCH_WORD_32 *)binary & 0xfff; }
+static int binary_hash_3(void *binary) { return *(ARCH_WORD_32 *)binary & 0xffff; }
+static int binary_hash_4(void *binary) { return *(ARCH_WORD_32 *)binary & 0xfffff; }
+static int binary_hash_5(void *binary) { return *(ARCH_WORD_32 *)binary & 0xffffff; }
+static int binary_hash_6(void *binary) { return *(ARCH_WORD_32 *)binary & 0x7ffffff; }
+
+static int get_hash_0(int index) { return crypt_out[index][0] & 0xf; }
+static int get_hash_1(int index) { return crypt_out[index][0] & 0xff; }
+static int get_hash_2(int index) { return crypt_out[index][0] & 0xfff; }
+static int get_hash_3(int index) { return crypt_out[index][0] & 0xffff; }
+static int get_hash_4(int index) { return crypt_out[index][0] & 0xfffff; }
+static int get_hash_5(int index) { return crypt_out[index][0] & 0xffffff; }
+static int get_hash_6(int index) { return crypt_out[index][0] & 0x7ffffff; }
+
+static int crypt_256(int *pcount, struct db_salt *salt)
+{
+	int count = *pcount;
+	int index = 0;
+
+#ifdef _OPENMP
+#pragma omp parallel for
+	for (index = 0; index < count; index++)
+#endif
+	{
+		snefru_ctx ctx;;
+
+		rhash_snefru256_init(&ctx);
+		rhash_snefru_update(&ctx, (unsigned char*)saved_key[index], strlen(saved_key[index]));
+		rhash_snefru_final(&ctx, (unsigned char*)crypt_out[index]);
+	}
+	return count;
+}
+
+static int crypt_128(int *pcount, struct db_salt *salt)
+{
+	int count = *pcount;
+	int index = 0;
+
+#ifdef _OPENMP
+#pragma omp parallel for
+	for (index = 0; index < count; index++)
+#endif
+	{
+		snefru_ctx ctx;;
+
+		rhash_snefru128_init(&ctx);
+		rhash_snefru_update(&ctx, (unsigned char*)saved_key[index], strlen(saved_key[index]));
+		rhash_snefru_final(&ctx, (unsigned char*)crypt_out[index]);
+
+	}
+	return count;
+}
+
+static int cmp_all(void *binary, int count)
+{
+	int index = 0;
+#ifdef _OPENMP
+	for (; index < count; index++)
+#endif
+		if (!memcmp(binary, crypt_out[index], CMP_SIZE))
+			return 1;
+	return 0;
+}
+
+static int cmp_one(void *binary, int index)
+{
+	return !memcmp(binary, crypt_out[index], CMP_SIZE);
+}
+
+static int cmp_exact(char *source, int index)
+{
+	return 1;
+}
+
+static void snefru_set_key(char *key, int index)
+{
+	int saved_key_length = strlen(key);
+	if (saved_key_length > PLAINTEXT_LENGTH)
+		saved_key_length = PLAINTEXT_LENGTH;
+	memcpy(saved_key[index], key, saved_key_length);
+	saved_key[index][saved_key_length] = 0;
+}
+
+static char *get_key(int index)
+{
+	return saved_key[index];
+}
+
+struct fmt_main fmt_snefru_256 = {
+	{
+		"snefru-256",
+		"Snefru 256",
+		ALGORITHM_NAME,
+		BENCHMARK_COMMENT,
+		BENCHMARK_LENGTH,
+		PLAINTEXT_LENGTH,
+		BINARY_SIZE,
+		DEFAULT_ALIGN,
+		SALT_SIZE,
+		DEFAULT_ALIGN,
+		MIN_KEYS_PER_CRYPT,
+		MAX_KEYS_PER_CRYPT,
+		FMT_CASE | FMT_8_BIT | FMT_OMP,
+		snefru_256_tests
+	}, {
+		init,
+		fmt_default_done,
+		fmt_default_reset,
+		fmt_default_prepare,
+		valid,
+		fmt_default_split,
+		get_binary_256,
+		fmt_default_salt,
+		fmt_default_source,
+		{
+			binary_hash_0,
+			binary_hash_1,
+			binary_hash_2,
+			binary_hash_3,
+			binary_hash_4,
+			binary_hash_5,
+			binary_hash_6
+		},
+		fmt_default_salt_hash,
+		fmt_default_set_salt,
+		snefru_set_key,
+		get_key,
+		fmt_default_clear_keys,
+		crypt_256,
+		{
+			get_hash_0,
+			get_hash_1,
+			get_hash_2,
+			get_hash_3,
+			get_hash_4,
+			get_hash_5,
+			get_hash_6
+		},
+		cmp_all,
+		cmp_one,
+		cmp_exact
+	}
+};
+
+
+struct fmt_main fmt_snefru_128 = {
+	{
+		"snefru-128",
+		"Snefru 128",
+		ALGORITHM_NAME,
+		BENCHMARK_COMMENT,
+		BENCHMARK_LENGTH,
+		PLAINTEXT_LENGTH,
+		BINARY_SIZE,
+		DEFAULT_ALIGN,
+		SALT_SIZE,
+		DEFAULT_ALIGN,
+		MIN_KEYS_PER_CRYPT,
+		MAX_KEYS_PER_CRYPT,
+		FMT_CASE | FMT_8_BIT | FMT_OMP,
+		snefru_128_tests
+	}, {
+		init,
+		fmt_default_done,
+		fmt_default_reset,
+		fmt_default_prepare,
+		valid,
+		fmt_default_split,
+		get_binary_128,
+		fmt_default_salt,
+		fmt_default_source,
+		{
+			binary_hash_0,
+			binary_hash_1,
+			binary_hash_2,
+			binary_hash_3,
+			binary_hash_4,
+			binary_hash_5,
+			binary_hash_6
+		},
+		fmt_default_salt_hash,
+		fmt_default_set_salt,
+		snefru_set_key,
+		get_key,
+		fmt_default_clear_keys,
+		crypt_128,
+		{
+			get_hash_0,
+			get_hash_1,
+			get_hash_2,
+			get_hash_3,
+			get_hash_4,
+			get_hash_5,
+			get_hash_6
+		},
+		cmp_all,
+		cmp_one,
+		cmp_exact
+	}
+};
