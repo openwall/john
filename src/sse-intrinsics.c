c8d361e7bbd9d02c5ee3d3722f191c75f9db6a06
magnum <john.magnum@hushmail.com>
2013-05-22 00:04:53 +0200

    Add ULL to large numbers in SHA512 code, muting loads of complaints when building 32-bit.

--- src/sse-intrinsics.c
+++ src/sse-intrinsics.c
@@ -1789,111 +1789,111 @@ void SSESHA512body(__m128i* data, unsigned int *out, ARCH_WORD_32 *reload_state,
 	} else {
 		if (SSEi_flags & SSEi_CRYPT_SHA384) {
 			/* SHA-384 IV */
-			a = _mm_set1_epi64x (0xcbbb9d5dc1059ed8);
-			b = _mm_set1_epi64x (0x629a292a367cd507);
-			c = _mm_set1_epi64x (0x9159015a3070dd17);
-			d = _mm_set1_epi64x (0x152fecd8f70e5939);
-			e = _mm_set1_epi64x (0x67332667ffc00b31);
-			f = _mm_set1_epi64x (0x8eb44a8768581511);
-			g = _mm_set1_epi64x (0xdb0c2e0d64f98fa7);
-			h = _mm_set1_epi64x (0x47b5481dbefa4fa4);
+			a = _mm_set1_epi64x (0xcbbb9d5dc1059ed8ULL);
+			b = _mm_set1_epi64x (0x629a292a367cd507ULL);
+			c = _mm_set1_epi64x (0x9159015a3070dd17ULL);
+			d = _mm_set1_epi64x (0x152fecd8f70e5939ULL);
+			e = _mm_set1_epi64x (0x67332667ffc00b31ULL);
+			f = _mm_set1_epi64x (0x8eb44a8768581511ULL);
+			g = _mm_set1_epi64x (0xdb0c2e0d64f98fa7ULL);
+			h = _mm_set1_epi64x (0x47b5481dbefa4fa4ULL);
 		} else {
 			// SHA-512 IV */
-			a = _mm_set1_epi64x (0x6a09e667f3bcc908);
-			b = _mm_set1_epi64x (0xbb67ae8584caa73b);
-			c = _mm_set1_epi64x (0x3c6ef372fe94f82b);
-			d = _mm_set1_epi64x (0xa54ff53a5f1d36f1);
-			e = _mm_set1_epi64x (0x510e527fade682d1);
-			f = _mm_set1_epi64x (0x9b05688c2b3e6c1f);
-			g = _mm_set1_epi64x (0x1f83d9abfb41bd6b);
-			h = _mm_set1_epi64x (0x5be0cd19137e2179);
+			a = _mm_set1_epi64x (0x6a09e667f3bcc908ULL);
+			b = _mm_set1_epi64x (0xbb67ae8584caa73bULL);
+			c = _mm_set1_epi64x (0x3c6ef372fe94f82bULL);
+			d = _mm_set1_epi64x (0xa54ff53a5f1d36f1ULL);
+			e = _mm_set1_epi64x (0x510e527fade682d1ULL);
+			f = _mm_set1_epi64x (0x9b05688c2b3e6c1fULL);
+			g = _mm_set1_epi64x (0x1f83d9abfb41bd6bULL);
+			h = _mm_set1_epi64x (0x5be0cd19137e2179ULL);
 		}
 	}
 
-	SHA512_STEP(a, b, c, d, e, f, g, h,  0, 0x428a2f98d728ae22);
-	SHA512_STEP(h, a, b, c, d, e, f, g,  1, 0x7137449123ef65cd);
-	SHA512_STEP(g, h, a, b, c, d, e, f,  2, 0xb5c0fbcfec4d3b2f);
-	SHA512_STEP(f, g, h, a, b, c, d, e,  3, 0xe9b5dba58189dbbc);
-	SHA512_STEP(e, f, g, h, a, b, c, d,  4, 0x3956c25bf348b538);
-	SHA512_STEP(d, e, f, g, h, a, b, c,  5, 0x59f111f1b605d019);
-	SHA512_STEP(c, d, e, f, g, h, a, b,  6, 0x923f82a4af194f9b);
-	SHA512_STEP(b, c, d, e, f, g, h, a,  7, 0xab1c5ed5da6d8118);
-	SHA512_STEP(a, b, c, d, e, f, g, h,  8, 0xd807aa98a3030242);
-	SHA512_STEP(h, a, b, c, d, e, f, g,  9, 0x12835b0145706fbe);
-	SHA512_STEP(g, h, a, b, c, d, e, f, 10, 0x243185be4ee4b28c);
-	SHA512_STEP(f, g, h, a, b, c, d, e, 11, 0x550c7dc3d5ffb4e2);
-	SHA512_STEP(e, f, g, h, a, b, c, d, 12, 0x72be5d74f27b896f);
-	SHA512_STEP(d, e, f, g, h, a, b, c, 13, 0x80deb1fe3b1696b1);
-	SHA512_STEP(c, d, e, f, g, h, a, b, 14, 0x9bdc06a725c71235);
-	SHA512_STEP(b, c, d, e, f, g, h, a, 15, 0xc19bf174cf692694);
+	SHA512_STEP(a, b, c, d, e, f, g, h,  0, 0x428a2f98d728ae22ULL);
+	SHA512_STEP(h, a, b, c, d, e, f, g,  1, 0x7137449123ef65cdULL);
+	SHA512_STEP(g, h, a, b, c, d, e, f,  2, 0xb5c0fbcfec4d3b2fULL);
+	SHA512_STEP(f, g, h, a, b, c, d, e,  3, 0xe9b5dba58189dbbcULL);
+	SHA512_STEP(e, f, g, h, a, b, c, d,  4, 0x3956c25bf348b538ULL);
+	SHA512_STEP(d, e, f, g, h, a, b, c,  5, 0x59f111f1b605d019ULL);
+	SHA512_STEP(c, d, e, f, g, h, a, b,  6, 0x923f82a4af194f9bULL);
+	SHA512_STEP(b, c, d, e, f, g, h, a,  7, 0xab1c5ed5da6d8118ULL);
+	SHA512_STEP(a, b, c, d, e, f, g, h,  8, 0xd807aa98a3030242ULL);
+	SHA512_STEP(h, a, b, c, d, e, f, g,  9, 0x12835b0145706fbeULL);
+	SHA512_STEP(g, h, a, b, c, d, e, f, 10, 0x243185be4ee4b28cULL);
+	SHA512_STEP(f, g, h, a, b, c, d, e, 11, 0x550c7dc3d5ffb4e2ULL);
+	SHA512_STEP(e, f, g, h, a, b, c, d, 12, 0x72be5d74f27b896fULL);
+	SHA512_STEP(d, e, f, g, h, a, b, c, 13, 0x80deb1fe3b1696b1ULL);
+	SHA512_STEP(c, d, e, f, g, h, a, b, 14, 0x9bdc06a725c71235ULL);
+	SHA512_STEP(b, c, d, e, f, g, h, a, 15, 0xc19bf174cf692694ULL);
 
-	SHA512_STEP(a, b, c, d, e, f, g, h, 16, 0xe49b69c19ef14ad2);
-	SHA512_STEP(h, a, b, c, d, e, f, g, 17, 0xefbe4786384f25e3);
-	SHA512_STEP(g, h, a, b, c, d, e, f, 18, 0x0fc19dc68b8cd5b5);
-	SHA512_STEP(f, g, h, a, b, c, d, e, 19, 0x240ca1cc77ac9c65);
-	SHA512_STEP(e, f, g, h, a, b, c, d, 20, 0x2de92c6f592b0275);
-	SHA512_STEP(d, e, f, g, h, a, b, c, 21, 0x4a7484aa6ea6e483);
-	SHA512_STEP(c, d, e, f, g, h, a, b, 22, 0x5cb0a9dcbd41fbd4);
-	SHA512_STEP(b, c, d, e, f, g, h, a, 23, 0x76f988da831153b5);
-	SHA512_STEP(a, b, c, d, e, f, g, h, 24, 0x983e5152ee66dfab);
-	SHA512_STEP(h, a, b, c, d, e, f, g, 25, 0xa831c66d2db43210);
-	SHA512_STEP(g, h, a, b, c, d, e, f, 26, 0xb00327c898fb213f);
-	SHA512_STEP(f, g, h, a, b, c, d, e, 27, 0xbf597fc7beef0ee4);
-	SHA512_STEP(e, f, g, h, a, b, c, d, 28, 0xc6e00bf33da88fc2);
-	SHA512_STEP(d, e, f, g, h, a, b, c, 29, 0xd5a79147930aa725);
-	SHA512_STEP(c, d, e, f, g, h, a, b, 30, 0x06ca6351e003826f);
-	SHA512_STEP(b, c, d, e, f, g, h, a, 31, 0x142929670a0e6e70);
+	SHA512_STEP(a, b, c, d, e, f, g, h, 16, 0xe49b69c19ef14ad2ULL);
+	SHA512_STEP(h, a, b, c, d, e, f, g, 17, 0xefbe4786384f25e3ULL);
+	SHA512_STEP(g, h, a, b, c, d, e, f, 18, 0x0fc19dc68b8cd5b5ULL);
+	SHA512_STEP(f, g, h, a, b, c, d, e, 19, 0x240ca1cc77ac9c65ULL);
+	SHA512_STEP(e, f, g, h, a, b, c, d, 20, 0x2de92c6f592b0275ULL);
+	SHA512_STEP(d, e, f, g, h, a, b, c, 21, 0x4a7484aa6ea6e483ULL);
+	SHA512_STEP(c, d, e, f, g, h, a, b, 22, 0x5cb0a9dcbd41fbd4ULL);
+	SHA512_STEP(b, c, d, e, f, g, h, a, 23, 0x76f988da831153b5ULL);
+	SHA512_STEP(a, b, c, d, e, f, g, h, 24, 0x983e5152ee66dfabULL);
+	SHA512_STEP(h, a, b, c, d, e, f, g, 25, 0xa831c66d2db43210ULL);
+	SHA512_STEP(g, h, a, b, c, d, e, f, 26, 0xb00327c898fb213fULL);
+	SHA512_STEP(f, g, h, a, b, c, d, e, 27, 0xbf597fc7beef0ee4ULL);
+	SHA512_STEP(e, f, g, h, a, b, c, d, 28, 0xc6e00bf33da88fc2ULL);
+	SHA512_STEP(d, e, f, g, h, a, b, c, 29, 0xd5a79147930aa725ULL);
+	SHA512_STEP(c, d, e, f, g, h, a, b, 30, 0x06ca6351e003826fULL);
+	SHA512_STEP(b, c, d, e, f, g, h, a, 31, 0x142929670a0e6e70ULL);
 
-	SHA512_STEP(a, b, c, d, e, f, g, h, 32, 0x27b70a8546d22ffc);
-	SHA512_STEP(h, a, b, c, d, e, f, g, 33, 0x2e1b21385c26c926);
-	SHA512_STEP(g, h, a, b, c, d, e, f, 34, 0x4d2c6dfc5ac42aed);
-	SHA512_STEP(f, g, h, a, b, c, d, e, 35, 0x53380d139d95b3df);
-	SHA512_STEP(e, f, g, h, a, b, c, d, 36, 0x650a73548baf63de);
-	SHA512_STEP(d, e, f, g, h, a, b, c, 37, 0x766a0abb3c77b2a8);
-	SHA512_STEP(c, d, e, f, g, h, a, b, 38, 0x81c2c92e47edaee6);
-	SHA512_STEP(b, c, d, e, f, g, h, a, 39, 0x92722c851482353b);
-	SHA512_STEP(a, b, c, d, e, f, g, h, 40, 0xa2bfe8a14cf10364);
-	SHA512_STEP(h, a, b, c, d, e, f, g, 41, 0xa81a664bbc423001);
-	SHA512_STEP(g, h, a, b, c, d, e, f, 42, 0xc24b8b70d0f89791);
-	SHA512_STEP(f, g, h, a, b, c, d, e, 43, 0xc76c51a30654be30);
-	SHA512_STEP(e, f, g, h, a, b, c, d, 44, 0xd192e819d6ef5218);
-	SHA512_STEP(d, e, f, g, h, a, b, c, 45, 0xd69906245565a910);
-	SHA512_STEP(c, d, e, f, g, h, a, b, 46, 0xf40e35855771202a);
-	SHA512_STEP(b, c, d, e, f, g, h, a, 47, 0x106aa07032bbd1b8);
+	SHA512_STEP(a, b, c, d, e, f, g, h, 32, 0x27b70a8546d22ffcULL);
+	SHA512_STEP(h, a, b, c, d, e, f, g, 33, 0x2e1b21385c26c926ULL);
+	SHA512_STEP(g, h, a, b, c, d, e, f, 34, 0x4d2c6dfc5ac42aedULL);
+	SHA512_STEP(f, g, h, a, b, c, d, e, 35, 0x53380d139d95b3dfULL);
+	SHA512_STEP(e, f, g, h, a, b, c, d, 36, 0x650a73548baf63deULL);
+	SHA512_STEP(d, e, f, g, h, a, b, c, 37, 0x766a0abb3c77b2a8ULL);
+	SHA512_STEP(c, d, e, f, g, h, a, b, 38, 0x81c2c92e47edaee6ULL);
+	SHA512_STEP(b, c, d, e, f, g, h, a, 39, 0x92722c851482353bULL);
+	SHA512_STEP(a, b, c, d, e, f, g, h, 40, 0xa2bfe8a14cf10364ULL);
+	SHA512_STEP(h, a, b, c, d, e, f, g, 41, 0xa81a664bbc423001ULL);
+	SHA512_STEP(g, h, a, b, c, d, e, f, 42, 0xc24b8b70d0f89791ULL);
+	SHA512_STEP(f, g, h, a, b, c, d, e, 43, 0xc76c51a30654be30ULL);
+	SHA512_STEP(e, f, g, h, a, b, c, d, 44, 0xd192e819d6ef5218ULL);
+	SHA512_STEP(d, e, f, g, h, a, b, c, 45, 0xd69906245565a910ULL);
+	SHA512_STEP(c, d, e, f, g, h, a, b, 46, 0xf40e35855771202aULL);
+	SHA512_STEP(b, c, d, e, f, g, h, a, 47, 0x106aa07032bbd1b8ULL);
 
-	SHA512_STEP(a, b, c, d, e, f, g, h, 48, 0x19a4c116b8d2d0c8);
-	SHA512_STEP(h, a, b, c, d, e, f, g, 49, 0x1e376c085141ab53);
-	SHA512_STEP(g, h, a, b, c, d, e, f, 50, 0x2748774cdf8eeb99);
-	SHA512_STEP(f, g, h, a, b, c, d, e, 51, 0x34b0bcb5e19b48a8);
-	SHA512_STEP(e, f, g, h, a, b, c, d, 52, 0x391c0cb3c5c95a63);
-	SHA512_STEP(d, e, f, g, h, a, b, c, 53, 0x4ed8aa4ae3418acb);
-	SHA512_STEP(c, d, e, f, g, h, a, b, 54, 0x5b9cca4f7763e373);
-	SHA512_STEP(b, c, d, e, f, g, h, a, 55, 0x682e6ff3d6b2b8a3);
-	SHA512_STEP(a, b, c, d, e, f, g, h, 56, 0x748f82ee5defb2fc);
-	SHA512_STEP(h, a, b, c, d, e, f, g, 57, 0x78a5636f43172f60);
-	SHA512_STEP(g, h, a, b, c, d, e, f, 58, 0x84c87814a1f0ab72);
-	SHA512_STEP(f, g, h, a, b, c, d, e, 59, 0x8cc702081a6439ec);
-	SHA512_STEP(e, f, g, h, a, b, c, d, 60, 0x90befffa23631e28);
-	SHA512_STEP(d, e, f, g, h, a, b, c, 61, 0xa4506cebde82bde9);
-	SHA512_STEP(c, d, e, f, g, h, a, b, 62, 0xbef9a3f7b2c67915);
-	SHA512_STEP(b, c, d, e, f, g, h, a, 63, 0xc67178f2e372532b);
+	SHA512_STEP(a, b, c, d, e, f, g, h, 48, 0x19a4c116b8d2d0c8ULL);
+	SHA512_STEP(h, a, b, c, d, e, f, g, 49, 0x1e376c085141ab53ULL);
+	SHA512_STEP(g, h, a, b, c, d, e, f, 50, 0x2748774cdf8eeb99ULL);
+	SHA512_STEP(f, g, h, a, b, c, d, e, 51, 0x34b0bcb5e19b48a8ULL);
+	SHA512_STEP(e, f, g, h, a, b, c, d, 52, 0x391c0cb3c5c95a63ULL);
+	SHA512_STEP(d, e, f, g, h, a, b, c, 53, 0x4ed8aa4ae3418acbULL);
+	SHA512_STEP(c, d, e, f, g, h, a, b, 54, 0x5b9cca4f7763e373ULL);
+	SHA512_STEP(b, c, d, e, f, g, h, a, 55, 0x682e6ff3d6b2b8a3ULL);
+	SHA512_STEP(a, b, c, d, e, f, g, h, 56, 0x748f82ee5defb2fcULL);
+	SHA512_STEP(h, a, b, c, d, e, f, g, 57, 0x78a5636f43172f60ULL);
+	SHA512_STEP(g, h, a, b, c, d, e, f, 58, 0x84c87814a1f0ab72ULL);
+	SHA512_STEP(f, g, h, a, b, c, d, e, 59, 0x8cc702081a6439ecULL);
+	SHA512_STEP(e, f, g, h, a, b, c, d, 60, 0x90befffa23631e28ULL);
+	SHA512_STEP(d, e, f, g, h, a, b, c, 61, 0xa4506cebde82bde9ULL);
+	SHA512_STEP(c, d, e, f, g, h, a, b, 62, 0xbef9a3f7b2c67915ULL);
+	SHA512_STEP(b, c, d, e, f, g, h, a, 63, 0xc67178f2e372532bULL);
 
-	SHA512_STEP(a, b, c, d, e, f, g, h, 64, 0xca273eceea26619c);
-	SHA512_STEP(h, a, b, c, d, e, f, g, 65, 0xd186b8c721c0c207);
-	SHA512_STEP(g, h, a, b, c, d, e, f, 66, 0xeada7dd6cde0eb1e);
-	SHA512_STEP(f, g, h, a, b, c, d, e, 67, 0xf57d4f7fee6ed178);
-	SHA512_STEP(e, f, g, h, a, b, c, d, 68, 0x06f067aa72176fba);
-	SHA512_STEP(d, e, f, g, h, a, b, c, 69, 0x0a637dc5a2c898a6);
-	SHA512_STEP(c, d, e, f, g, h, a, b, 70, 0x113f9804bef90dae);
-	SHA512_STEP(b, c, d, e, f, g, h, a, 71, 0x1b710b35131c471b);
-	SHA512_STEP(a, b, c, d, e, f, g, h, 72, 0x28db77f523047d84);
-	SHA512_STEP(h, a, b, c, d, e, f, g, 73, 0x32caab7b40c72493);
-	SHA512_STEP(g, h, a, b, c, d, e, f, 74, 0x3c9ebe0a15c9bebc);
-	SHA512_STEP(f, g, h, a, b, c, d, e, 75, 0x431d67c49c100d4c);
-	SHA512_STEP(e, f, g, h, a, b, c, d, 76, 0x4cc5d4becb3e42b6);
-	SHA512_STEP(d, e, f, g, h, a, b, c, 77, 0x597f299cfc657e2a);
-	SHA512_STEP(c, d, e, f, g, h, a, b, 78, 0x5fcb6fab3ad6faec);
-	SHA512_STEP(b, c, d, e, f, g, h, a, 79, 0x6c44198c4a475817);
+	SHA512_STEP(a, b, c, d, e, f, g, h, 64, 0xca273eceea26619cULL);
+	SHA512_STEP(h, a, b, c, d, e, f, g, 65, 0xd186b8c721c0c207ULL);
+	SHA512_STEP(g, h, a, b, c, d, e, f, 66, 0xeada7dd6cde0eb1eULL);
+	SHA512_STEP(f, g, h, a, b, c, d, e, 67, 0xf57d4f7fee6ed178ULL);
+	SHA512_STEP(e, f, g, h, a, b, c, d, 68, 0x06f067aa72176fbaULL);
+	SHA512_STEP(d, e, f, g, h, a, b, c, 69, 0x0a637dc5a2c898a6ULL);
+	SHA512_STEP(c, d, e, f, g, h, a, b, 70, 0x113f9804bef90daeULL);
+	SHA512_STEP(b, c, d, e, f, g, h, a, 71, 0x1b710b35131c471bULL);
+	SHA512_STEP(a, b, c, d, e, f, g, h, 72, 0x28db77f523047d84ULL);
+	SHA512_STEP(h, a, b, c, d, e, f, g, 73, 0x32caab7b40c72493ULL);
+	SHA512_STEP(g, h, a, b, c, d, e, f, 74, 0x3c9ebe0a15c9bebcULL);
+	SHA512_STEP(f, g, h, a, b, c, d, e, 75, 0x431d67c49c100d4cULL);
+	SHA512_STEP(e, f, g, h, a, b, c, d, 76, 0x4cc5d4becb3e42b6ULL);
+	SHA512_STEP(d, e, f, g, h, a, b, c, 77, 0x597f299cfc657e2aULL);
+	SHA512_STEP(c, d, e, f, g, h, a, b, 78, 0x5fcb6fab3ad6faecULL);
+	SHA512_STEP(b, c, d, e, f, g, h, a, 79, 0x6c44198c4a475817ULL);
 
 	if (SSEi_flags & SSEi_RELOAD) {
 		if ((SSEi_flags & SSEi_RELOAD_INP_FMT)==SSEi_RELOAD_INP_FMT)
@@ -1929,24 +1929,24 @@ void SSESHA512body(__m128i* data, unsigned int *out, ARCH_WORD_32 *reload_state,
 	} else if ((SSEi_flags & SSEi_SKIP_FINAL_ADD) == 0) {
 		if (SSEi_flags & SSEi_CRYPT_SHA384) {
 			/* SHA-384 IV */
-			a = _mm_add_epi64 (a, _mm_set1_epi64x (0xcbbb9d5dc1059ed8));
-			b = _mm_add_epi64 (b, _mm_set1_epi64x (0x629a292a367cd507));
-			c = _mm_add_epi64 (c, _mm_set1_epi64x (0x9159015a3070dd17));
-			d = _mm_add_epi64 (d, _mm_set1_epi64x (0x152fecd8f70e5939));
-			e = _mm_add_epi64 (e, _mm_set1_epi64x (0x67332667ffc00b31));
-			f = _mm_add_epi64 (f, _mm_set1_epi64x (0x8eb44a8768581511));
-			g = _mm_add_epi64 (g, _mm_set1_epi64x (0xdb0c2e0d64f98fa7));
-			h = _mm_add_epi64 (h, _mm_set1_epi64x (0x47b5481dbefa4fa4));
+			a = _mm_add_epi64 (a, _mm_set1_epi64x (0xcbbb9d5dc1059ed8ULL));
+			b = _mm_add_epi64 (b, _mm_set1_epi64x (0x629a292a367cd507ULL));
+			c = _mm_add_epi64 (c, _mm_set1_epi64x (0x9159015a3070dd17ULL));
+			d = _mm_add_epi64 (d, _mm_set1_epi64x (0x152fecd8f70e5939ULL));
+			e = _mm_add_epi64 (e, _mm_set1_epi64x (0x67332667ffc00b31ULL));
+			f = _mm_add_epi64 (f, _mm_set1_epi64x (0x8eb44a8768581511ULL));
+			g = _mm_add_epi64 (g, _mm_set1_epi64x (0xdb0c2e0d64f98fa7ULL));
+			h = _mm_add_epi64 (h, _mm_set1_epi64x (0x47b5481dbefa4fa4ULL));
 		} else {
 			/* SHA-512 IV */
-			a = _mm_add_epi64 (a, _mm_set1_epi64x (0x6a09e667f3bcc908));
-			b = _mm_add_epi64 (b, _mm_set1_epi64x (0xbb67ae8584caa73b));
-			c = _mm_add_epi64 (c, _mm_set1_epi64x (0x3c6ef372fe94f82b));
-			d = _mm_add_epi64 (d, _mm_set1_epi64x (0xa54ff53a5f1d36f1));
-			e = _mm_add_epi64 (e, _mm_set1_epi64x (0x510e527fade682d1));
-			f = _mm_add_epi64 (f, _mm_set1_epi64x (0x9b05688c2b3e6c1f));
-			g = _mm_add_epi64 (g, _mm_set1_epi64x (0x1f83d9abfb41bd6b));
-			h = _mm_add_epi64 (h, _mm_set1_epi64x (0x5be0cd19137e2179));
+			a = _mm_add_epi64 (a, _mm_set1_epi64x (0x6a09e667f3bcc908ULL));
+			b = _mm_add_epi64 (b, _mm_set1_epi64x (0xbb67ae8584caa73bULL));
+			c = _mm_add_epi64 (c, _mm_set1_epi64x (0x3c6ef372fe94f82bULL));
+			d = _mm_add_epi64 (d, _mm_set1_epi64x (0xa54ff53a5f1d36f1ULL));
+			e = _mm_add_epi64 (e, _mm_set1_epi64x (0x510e527fade682d1ULL));
+			f = _mm_add_epi64 (f, _mm_set1_epi64x (0x9b05688c2b3e6c1fULL));
+			g = _mm_add_epi64 (g, _mm_set1_epi64x (0x1f83d9abfb41bd6bULL));
+			h = _mm_add_epi64 (h, _mm_set1_epi64x (0x5be0cd19137e2179ULL));
 		}
 	}
 
