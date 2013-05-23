98bbfc4a3dbf86168f195114d975388d4d16f171
Dhiru Kholia <dhiru@openwall.com>
2013-05-23 11:52:01 +0530

    Add support for SHA3-256 hashing function

---
+++ src/rawKeccak_256_fmt_plug.c
@@ -0,0 +1,306 @@
+/* Keccak-256 cracker patch for JtR. Hacked together during May of 2013
+ * by Dhiru Kholia <dhiru.kholia at gmail.com>.
+ *
+ * Usage: john --format:raw-keccak-256 <hash file>
+ *
+ * This file is part of John the Ripper password cracker,
+ * Copyright (c) 2012 by Solar Designer
+ *
+ * based on rawMD4_fmt.c code, with trivial changes by groszek.
+ */
+
+#include <string.h>
+
+#include "arch.h"
+#include "params.h"
+#include "common.h"
+#include "formats.h"
+#include "options.h"
+#include "KeccakF-1600-interface.h"
+#include "KeccakNISTInterface.h"
+
+#ifdef _OPENMP
+#define OMP_SCALE			2048
+#include <omp.h>
+#endif
+
+#define FORMAT_TAG		"$keccak256$"
+#define TAG_LENGTH		11
+
+#define FORMAT_LABEL		"raw-keccak-256"
+#define FORMAT_NAME		"Keccak 256"
+#if defined(__AVX__)
+#define ALGORITHM_NAME			"AVX"
+#elif defined(__XOP__)
+#define ALGORITHM_NAME			"XOP"
+#elif defined(__SSE4_1__)
+#define ALGORITHM_NAME			"SSE4.1"
+#elif defined(__SSSE3__)
+#define ALGORITHM_NAME			"SSSE3"
+#elif defined(__SSE2__)
+#define ALGORITHM_NAME			"SSE2"
+#else
+#define ALGORITHM_NAME			"32/" ARCH_BITS_STR
+#endif
+
+#define BENCHMARK_COMMENT		""
+#define BENCHMARK_LENGTH		-1
+
+#define PLAINTEXT_LENGTH		125
+#define CIPHERTEXT_LENGTH		64
+
+#define BINARY_SIZE			32
+#define SALT_SIZE			0
+
+#define MIN_KEYS_PER_CRYPT		1
+#define MAX_KEYS_PER_CRYPT		1
+
+static struct fmt_tests tests[] = {
+	{"$keccak256$4e03657aea45a94fc7d47ba826c8d667c0d1e6e33a64a036ec44f58fa12d6c45", "abc"},
+	{NULL}
+};
+
+static int (*saved_key_length);
+static char (*saved_key)[PLAINTEXT_LENGTH + 1];
+static ARCH_WORD_32 (*crypt_out)
+    [(BINARY_SIZE + sizeof(ARCH_WORD_32) - 1) / sizeof(ARCH_WORD_32)];
+
+static void init(struct fmt_main *self)
+{
+#ifdef _OPENMP
+	int omp_t;
+
+	omp_t = omp_get_max_threads();
+	self->params.min_keys_per_crypt = omp_t * MIN_KEYS_PER_CRYPT;
+	omp_t *= OMP_SCALE;
+	self->params.max_keys_per_crypt = omp_t * MAX_KEYS_PER_CRYPT;
+#endif
+	saved_key_length = mem_calloc_tiny(sizeof(*saved_key_length) * self->params.max_keys_per_crypt, MEM_ALIGN_WORD);
+	saved_key = mem_calloc_tiny(sizeof(*saved_key) * self->params.max_keys_per_crypt, MEM_ALIGN_WORD);
+	crypt_out = mem_calloc_tiny(sizeof(*crypt_out) * self->params.max_keys_per_crypt, MEM_ALIGN_WORD);
+	KeccakInitialize();
+}
+
+static int valid(char *ciphertext, struct fmt_main *self)
+{
+	char *p, *q;
+
+	p = ciphertext;
+	if (!strncmp(p, FORMAT_TAG, TAG_LENGTH))
+		p += TAG_LENGTH;
+
+	q = p;
+	while (atoi16[ARCH_INDEX(*q)] != 0x7F)
+		q++;
+	return !*q && q - p == CIPHERTEXT_LENGTH;
+}
+
+static char *split(char *ciphertext, int index, struct fmt_main *pFmt)
+{
+	static char out[TAG_LENGTH + CIPHERTEXT_LENGTH + 1];
+
+	if (!strncmp(ciphertext, FORMAT_TAG, TAG_LENGTH))
+		return ciphertext;
+
+	memcpy(out, FORMAT_TAG, TAG_LENGTH);
+	memcpy(out + TAG_LENGTH, ciphertext, CIPHERTEXT_LENGTH + 1);
+	strlwr(out + TAG_LENGTH);
+	return out;
+}
+
+static void *get_binary(char *ciphertext)
+{
+	static unsigned char *out;
+	char *p;
+	int i;
+
+	if (!out) out = mem_alloc_tiny(BINARY_SIZE, MEM_ALIGN_WORD);
+
+	p = ciphertext + TAG_LENGTH;
+	for (i = 0; i < BINARY_SIZE; i++) {
+		out[i] =
+		    (atoi16[ARCH_INDEX(*p)] << 4) |
+		    atoi16[ARCH_INDEX(p[1])];
+		p += 2;
+	}
+
+	return out;
+}
+
+static int binary_hash_0(void *binary)
+{
+	return *(ARCH_WORD_32 *)binary & 0xF;
+}
+
+static int binary_hash_1(void *binary)
+{
+	return *(ARCH_WORD_32 *)binary & 0xFF;
+}
+
+static int binary_hash_2(void *binary)
+{
+	return *(ARCH_WORD_32 *)binary & 0xFFF;
+}
+
+static int binary_hash_3(void *binary)
+{
+	return *(ARCH_WORD_32 *)binary & 0xFFFF;
+}
+
+static int binary_hash_4(void *binary)
+{
+	return *(ARCH_WORD_32 *)binary & 0xFFFFF;
+}
+
+static int binary_hash_5(void *binary)
+{
+	return *(ARCH_WORD_32 *)binary & 0xFFFFFF;
+}
+
+static int binary_hash_6(void *binary)
+{
+	return *(ARCH_WORD_32 *)binary & 0x7FFFFFF;
+}
+
+static int get_hash_0(int index)
+{
+	return crypt_out[index][0] & 0xF;
+}
+
+static int get_hash_1(int index)
+{
+	return crypt_out[index][0] & 0xFF;
+}
+
+static int get_hash_2(int index)
+{
+	return crypt_out[index][0] & 0xFFF;
+}
+
+static int get_hash_3(int index)
+{
+	return crypt_out[index][0] & 0xFFFF;
+}
+
+static int get_hash_4(int index)
+{
+	return crypt_out[index][0] & 0xFFFFF;
+}
+
+static int get_hash_5(int index)
+{
+	return crypt_out[index][0] & 0xFFFFFF;
+}
+
+static int get_hash_6(int index)
+{
+	return crypt_out[index][0] & 0x7FFFFFF;
+}
+
+static void set_key(char *key, int index)
+{
+	int len = strlen(key);
+	saved_key_length[index] = len;
+	if (len > PLAINTEXT_LENGTH)
+		len = saved_key_length[index] = PLAINTEXT_LENGTH;
+	memcpy(saved_key[index], key, len);
+}
+
+static char *get_key(int index)
+{
+	saved_key[index][saved_key_length[index]] = 0;
+	return saved_key[index];
+}
+
+static int crypt_all(int *pcount, struct db_salt *salt)
+{
+	int count = *pcount;
+	int index = 0;
+#ifdef _OPENMP
+#pragma omp parallel for
+#endif
+	for (index = 0; index < count; index++)
+	{
+		Hash(256, (BitSequence *)saved_key[index], saved_key_length[index] * 8, (BitSequence *)crypt_out[index]);
+	}
+	return count;
+}
+
+static int cmp_all(void *binary, int count)
+{
+	int index = 0;
+	for (; index < count; index++)
+		if (!memcmp(binary, crypt_out[index], BINARY_SIZE))
+			return 1;
+	return 0;
+}
+
+static int cmp_one(void *binary, int index)
+{
+	return !memcmp(binary, crypt_out[index], BINARY_SIZE);
+}
+
+static int cmp_exact(char *source, int index)
+{
+	return 1;
+}
+
+struct fmt_main fmt_rawKeccak_256 = {
+	{
+		FORMAT_LABEL,
+		FORMAT_NAME,
+		ALGORITHM_NAME,
+		BENCHMARK_COMMENT,
+		BENCHMARK_LENGTH,
+		PLAINTEXT_LENGTH,
+		BINARY_SIZE,
+#if FMT_MAIN_VERSION > 9
+                DEFAULT_ALIGN,
+#endif
+		SALT_SIZE,
+#if FMT_MAIN_VERSION > 9
+                DEFAULT_ALIGN,
+#endif
+		MIN_KEYS_PER_CRYPT,
+		MAX_KEYS_PER_CRYPT,
+		FMT_CASE | FMT_8_BIT | FMT_OMP,
+		tests
+	}, {
+		init,
+		fmt_default_done,
+		fmt_default_reset,
+		fmt_default_prepare,
+		valid,
+		split,
+		get_binary,
+		fmt_default_salt,
+		fmt_default_source,
+		{
+			binary_hash_0,
+			binary_hash_1,
+			binary_hash_2,
+			binary_hash_3,
+			binary_hash_4,
+			binary_hash_5,
+			binary_hash_6
+		},
+		fmt_default_salt_hash,
+		fmt_default_set_salt,
+		set_key,
+		get_key,
+		fmt_default_clear_keys,
+		crypt_all,
+		{
+			get_hash_0,
+			get_hash_1,
+			get_hash_2,
+			get_hash_3,
+			get_hash_4,
+			get_hash_5,
+			get_hash_6
+		},
+		cmp_all,
+		cmp_one,
+		cmp_exact
+	}
+};
