6f388c93cb4a74e52830114336e5d41f9ab050a9
Solar Designer <solar@openwall.com>
2013-05-30 00:54:19 +0000

    1.7.9.15: - In incremental mode charset file generation, revised and tuned the estimated cracks calculation based on actual testing. - Revised the pre-defined incremental modes, as well as external mode filters that are used to generate .chr files. - Added makechr, a script to (re-)generate the .chr files. - Updated the documentation. - Relaxed the license for many source files to cut-down BSD. - Relaxed the license for John the Ripper as a whole from GPLv2 (exact version) to GPLv2 or newer with optional OpenSSL and unRAR exceptions.

--- src/params.h
+++ src/params.h
@@ -22,7 +22,7 @@
 /*
  * John's version number.
  */
-#define JOHN_VERSION			"1.7.9.14"
+#define JOHN_VERSION			"1.7.9.15"
 
 /*
  * Notes to packagers of John for *BSD "ports", Linux distributions, etc.:
