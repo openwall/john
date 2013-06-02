26a7a044238a9182f8f654ba06431445127d095f
Solar Designer <solar@openwall.com>
2013-06-02 23:51:30 +0000

    Added linux-arm32le make target

---
+++ src/arm32le.h
@@ -0,0 +1,50 @@
+/*
+ * This file is part of John the Ripper password cracker,
+ * Copyright (c) 2013 by Solar Designer
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted.
+ *
+ * There's ABSOLUTELY NO WARRANTY, express or implied.
+ */
+
+/*
+ * Architecture specific parameters for 32-bit ARM in little-endian mode.
+ */
+
+#ifndef _JOHN_ARCH_H
+#define _JOHN_ARCH_H
+
+#define ARCH_WORD			long
+#define ARCH_SIZE			4
+#define ARCH_BITS			32
+#define ARCH_BITS_LOG			5
+#define ARCH_BITS_STR			"32"
+#define ARCH_LITTLE_ENDIAN		1
+#define ARCH_INT_GT_32			0
+#define ARCH_ALLOWS_UNALIGNED		0
+#define ARCH_INDEX(x)			((unsigned int)(unsigned char)(x))
+
+#define CPU_DETECT			0
+
+#define DES_ASM				0
+#define DES_128K			0
+#define DES_X2				0
+#define DES_MASK			0
+#define DES_SCALE			1
+#define DES_EXTB			0
+#define DES_COPY			1
+#define DES_BS_ASM			0
+#define DES_BS				1
+#define DES_BS_VECTOR			0
+#define DES_BS_EXPAND			1
+
+#define MD5_ASM				0
+#define MD5_X2				1
+#define MD5_IMM				0
+
+#define BF_ASM				0
+#define BF_SCALE			1
+#define BF_X2				0
+
+#endif
