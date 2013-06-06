c09389359f56d8b6b3de1cf055a8907c84e0845d
Dhiru Kholia <dhiru@openwall.com>
2013-06-07 01:19:09 +0530

    get rid of DEFAULT_ALIGN from tiger format

--- src/tiger_fmt_plug.c
+++ src/tiger_fmt_plug.c
@@ -32,6 +32,9 @@ static int omp_t = 1;
 #define PLAINTEXT_LENGTH	125
 #define BINARY_SIZE		24
 #define SALT_SIZE		0
+#define BINARY_ALIGN		4
+#define SALT_ALIGN		1
+
 #define MIN_KEYS_PER_CRYPT	1
 #define MAX_KEYS_PER_CRYPT	1
 
@@ -175,9 +178,9 @@ struct fmt_main fmt_tiger = {
 		BENCHMARK_LENGTH,
 		PLAINTEXT_LENGTH,
 		BINARY_SIZE,
-		DEFAULT_ALIGN,
+		BINARY_ALIGN,
 		SALT_SIZE,
-		DEFAULT_ALIGN,
+		SALT_ALIGN,
 		MIN_KEYS_PER_CRYPT,
 		MAX_KEYS_PER_CRYPT,
 		FMT_CASE | FMT_8_BIT | FMT_OMP,
