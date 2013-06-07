ad79b5478c64e1d56b1de6f9b437074737e762c6
Sergey V <sftp.mtuci@gmail.com>
2013-06-08 03:29:32 +0400

    make blockchain-opencl non-plug
    This fixes building on systems without OpenCL (with non-opencl make
    targets used).
    
    Compile error was:
    $ make linux-x86-64i
    ...
    gcc -c -Wall -Wdeclaration-after-statement -O2
    -fomit-frame-pointer -I/usr/local/include -I/usr/include/nss
    -I/usr/include/nspr -DHAVE_NSS -DHAVE_GMP -DHAVE_CRYPT -DHAVE_DL
    -DUSING_ICC_S_FILE -funroll-loops opencl_blockchain_fmt_plug.c
    In file included from opencl_blockchain_fmt_plug.c:29:0:
    common-opencl.h:22:19: fatal error: CL/cl.h: No such file or directory
    compilation terminated.
    make[1]: *** [opencl_blockchain_fmt_plug.o] Error 1
    make[1]: Leaving directory `/home/src/john/src'
    make: *** [linux-x86-64i] Error 2

--- src/john.c
+++ src/john.c
@@ -191,6 +191,7 @@ extern struct fmt_main fmt_opencl_wpapsk;
 extern struct fmt_main fmt_opencl_xsha512;
 extern struct fmt_main fmt_opencl_xsha512_ng;
 extern struct fmt_main fmt_opencl_zip;
+extern struct fmt_main fmt_opencl_blockchain;
 #endif
 #ifdef HAVE_CUDA
 extern struct fmt_main fmt_cuda_cryptmd5;
@@ -398,6 +399,7 @@ static void john_register_all(void)
 		john_register_one(&fmt_opencl_xsha512);
 		john_register_one(&fmt_opencl_xsha512_ng);
 		john_register_one(&fmt_opencl_zip);
+		john_register_one(&fmt_opencl_blockchain);
 		/* The following two need to be last until they are fixed
 		   for new --device handling */
 		john_register_one(&fmt_opencl_bf);
