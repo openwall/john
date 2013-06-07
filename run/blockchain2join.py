b6295b424df02c6f0f522a7c9312a09ffc13d1db
Dhiru Kholia <dhiru@openwall.com>
2013-06-07 20:23:38 +0530

    blockchain format actually works now ;)

--- run/blockchain2join.py
+++ run/blockchain2join.py
@@ -1,24 +1,37 @@
 #!/usr/bin/env python
 
-import json
-import hashlib
 import sys
 import base64
+import binascii
+from optparse import OptionParser
 
 if __name__ == '__main__':
 
-    args = sys.argv
+    parser = OptionParser(usage="%prog [blockchain wallet files]")
+
+    parser.add_option("--json", dest="json", action="store_true",
+            default=False, help="is input in base64 format?")
+
+    (options, args) = parser.parse_args()
 
     if len(args) < 1:
-        print >> sys.stderr, "Usage: %s [blockchain wallet files]" % sys.argv[0]
+        parser.print_help()
         sys.exit(-1)
 
-    for i in range(1, len(args)):
-        filename = args[i]
-        with open(filename, "rb") as f:
-            data = f.read()
-            ddata = base64.decodestring(data)
-            print "%s:$blockchain$%s$%s" % (filename,
-                    len(ddata), ddata.encode("hex"))
+    if options.json:
+        for i in range(0, len(args)):
+            filename = args[i]
+            with open(filename, "rb") as f:
+                data = f.read()
+                ddata = base64.decodestring(data)
+                print "%s:$blockchain$%s$%s" % (filename,
+                        len(ddata), binascii.hexlify(ddata))
+    else:
+        for i in range(0, len(args)):
+            filename = args[i]
+            with open(filename, "rb") as f:
+                data = f.read()
+                print "%s:$blockchain$%s$%s" % (filename,
+                        len(data), binascii.hexlify(data))
 
 
