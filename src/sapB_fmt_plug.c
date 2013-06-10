30116247a96cdda74bbb4fb1322efb3a01284b9f
magnum <john.magnum@hushmail.com>
2013-06-10 23:16:31 +0200

    Even more use of fmt_default_binary_hash_N().

--- src/sapB_fmt_plug.c
+++ src/sapB_fmt_plug.c
@@ -629,14 +629,6 @@ static char *split(char *ciphertext, int index, struct fmt_main *self)
 	return out;
 }
 
-static int binary_hash_0(void *binary) { return *(ARCH_WORD_32*)binary & 0xF; }
-static int binary_hash_1(void *binary) { return *(ARCH_WORD_32*)binary & 0xFF; }
-static int binary_hash_2(void *binary) { return *(ARCH_WORD_32*)binary & 0xFFF; }
-static int binary_hash_3(void *binary) { return *(ARCH_WORD_32*)binary & 0xFFFF; }
-static int binary_hash_4(void *binary) { return *(ARCH_WORD_32*)binary & 0xFFFFF; }
-static int binary_hash_5(void *binary) { return *(ARCH_WORD_32*)binary & 0xFFFFFF; }
-static int binary_hash_6(void *binary) { return *(ARCH_WORD_32*)binary & 0x7FFFFFF; }
-
 #ifdef MMX_COEF
 #define HASH_OFFSET (index&(MMX_COEF-1))+(index/MMX_COEF)*MMX_COEF*4
 static int get_hash_0(int index) { return ((ARCH_WORD_32 *)crypt_key)[HASH_OFFSET] & 0xf; }
@@ -699,13 +691,13 @@ struct fmt_main fmt_sapB = {
 		get_salt,
 		fmt_default_source,
 		{
-			binary_hash_0,
-			binary_hash_1,
-			binary_hash_2,
-			binary_hash_3,
-			binary_hash_4,
-			binary_hash_5,
-			binary_hash_6
+			fmt_default_binary_hash_0,
+			fmt_default_binary_hash_1,
+			fmt_default_binary_hash_2,
+			fmt_default_binary_hash_3,
+			fmt_default_binary_hash_4,
+			fmt_default_binary_hash_5,
+			fmt_default_binary_hash_6
 		},
 		salt_hash,
 		set_salt,
