dd20c3e63c02df9742c4e3c9b16cbfa6c90f17ff
magnum <john.magnum@hushmail.com>
2013-08-19 15:34:46 +0200

    Add missing FMT_OMP flag to ssha512 format.

--- src/ssha512_fmt_plug.c
+++ src/ssha512_fmt_plug.c
@@ -221,7 +221,7 @@ struct fmt_main fmt_saltedsha2 = {
 		SALT_ALIGN,
 		MIN_KEYS_PER_CRYPT,
 		MAX_KEYS_PER_CRYPT,
-		FMT_CASE | FMT_8_BIT,
+		FMT_CASE | FMT_8_BIT | FMT_OMP,
 		tests
 	}, {
 		init,
