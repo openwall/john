cc2ba69cd8e001039bf42258ec3d0d449765c939
magnum <john.magnum@hushmail.com>
2013-08-20 01:03:58 +0200

    Fix a bug where -DDEBUG would cause an infinite loop in mem_alloc_tiny() because MEM_ALLOC_SIZE is set to zero (Jumbo-specific) and we ended up never breaking out of the while loop. Thanks to bytebieger for noticing the problem and patiently convince me.

--- src/memory.c
+++ src/memory.c
@@ -87,6 +87,17 @@ void *mem_alloc_tiny(size_t size, size_t align)
 	size_t mask;
 	char *p;
 
+#ifdef DEBUG
+	/*
+	 * We may be called with size zero, for example from ldr_load_pw_line()
+	 * that calls mem_alloc_copy() with format->params.salt_size as size.
+	 * This causes problems with -DDEBUG without this fix because we never
+	 * get out of the while loop when MEM_ALLOC_SIZE is zero too.
+	 */
+	if (size == 0)
+		return NULL;
+#endif
+
 #if ARCH_ALLOWS_UNALIGNED
 	if (mem_saving_level > 2 && align < MEM_ALIGN_SIMD)
 		align = MEM_ALIGN_NONE;
