openwall/john

commit 42912b391f1ee5d15c41598e1cd3d9f219e0fe22

increased PW length of mscash2 from 27 to 125 and username length from 22 to 128

src/mscash2_fmt_plug.c
@@ -39,6 +39,9 @@
  *	  to the .S code were simply to make it work at all and the same optimizations were placed there.
  *	- the OMP code was removed during initial re-write, and was properly re-incorporated by magnum.
  *
+ *  In June 2013, salt length (Username) increased from 22 to 128, and max password length increased
+ *     from 27 to 125 bytes (unicode bytes, so 250 ?)
+ *
  * This module is based on:
  *     - the MSCASH patch for john written by Alain Espinosa <alainesp at gmail.com> in 2007
  *     - RFC 1320 - The MD4 Message-Digest Algorithm
@@ -88,7 +91,9 @@ static struct fmt_tests tests[] = {
 	{"$DCC2$10240#TEST2#c6758e5be7fc943d00b97972a8a97620", "test2" },    // salt is lowercased before hashing
 	{"$DCC2$10240#test3#360e51304a2d383ea33467ab0b639cc4", "test3" },
 	{"$DCC2$10240#test4#6f79ee93518306f071c47185998566ae", "test4" },
-
+	
+	// max length user name 128 bytes, and max length password, 125 bytes
+	{"$DCC2$10240#12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678#5ba26de44bd3a369f43a1c72fba76d45", "12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345"},
 	{NULL}
 };
 
@@ -98,12 +103,12 @@ static struct fmt_tests tests[] = {
 #define BENCHMARK_COMMENT		""
 #define BENCHMARK_LENGTH		-1
 
-#define PLAINTEXT_LENGTH		27
-#define MAX_CIPHERTEXT_LENGTH		(6 + 5 + 22*3 + 2 + 32) // x3 because salt may be UTF-8 in input  // changed to $DCC2$num#salt#hash  WARNING, only handles num of 5 digits!!
+#define PLAINTEXT_LENGTH		125
+#define MAX_CIPHERTEXT_LENGTH		(6 + 5 + 128*3 + 2 + 32) // x3 because salt may be UTF-8 in input  // changed to $DCC2$num#salt#hash  WARNING, only handles num of 5 digits!!
 
 #define BINARY_SIZE			16
 #define BINARY_ALIGN			4
-#define SALT_SIZE			(11*4+4)
+#define SALT_SIZE			(64*4+4)
 #define SALT_ALIGN			2
 
 #define ALGORITHM_NAME			SHA1_ALGORITHM_NAME
@@ -206,8 +211,8 @@ int mscash2_valid(char *ciphertext, int max_salt_length, const char *format_labe
 {
 	unsigned int i;
 	unsigned int l;
-	char insalt[3*22+1];
-	UTF16 realsalt[23];
+	char insalt[3*128+1];
+	UTF16 realsalt[129];
 	int saltlen;
 
 	if (strncmp(ciphertext, "$DCC2$10240#", 12))
@@ -225,7 +230,7 @@ int mscash2_valid(char *ciphertext, int max_salt_length, const char *format_labe
 		if (atoi16[ARCH_INDEX(ciphertext[i])] == 0x7F)
 			return 0;
 
-	// This is tricky: Max supported salt length is 22 characters of Unicode
+	// This is tricky: Max supported salt length is 128 characters of Unicode
 	i = 6;
 	while (ciphertext[i] && ciphertext[i] != '#') ++i;
 	++i;
@@ -250,7 +255,7 @@ int mscash2_valid(char *ciphertext, int max_salt_length, const char *format_labe
 
 static int valid(char *ciphertext, struct fmt_main *self)
 {
-	return mscash2_valid(ciphertext, 22, FORMAT_LABEL, self);
+	return mscash2_valid(ciphertext, 128, FORMAT_LABEL, self);
 }
 
 char *mscash2_prepare(char *split_fields[10], struct fmt_main *self)
@@ -298,8 +303,8 @@ static void set_salt(void *salt) {
 static void *get_salt(char *_ciphertext)
 {
 	unsigned char *ciphertext = (unsigned char *)_ciphertext;
-	static UTF16 out[24+1];
-	unsigned char input[22*3+1];
+	static UTF16 out[130+1];
+	unsigned char input[128*3+1];
 	int utf16len, md4_size;
 
 	memset(out, 0, sizeof(out));
@@ -315,7 +320,7 @@ static void *get_salt(char *_ciphertext)
 	}
 	input[md4_size] = 0;
 
-	utf16len = enc_to_utf16(&out[2], 22, input, md4_size);
+	utf16len = enc_to_utf16(&out[2], 128, input, md4_size);
 	if (utf16len < 0)
 		utf16len = strlen16(&out[2]);
 	out[0] = utf16len << 1;

commit d8637e769fd621d055e99fb577111315b8e30c33

Fix UTF-8 bug in mscash2 introduced in 42912b39.

src/mscash2_fmt_plug.c
@@ -133,7 +133,6 @@ static unsigned char (*sse_crypt2);
 #define MIN_KEYS_PER_CRYPT		1
 #define MAX_KEYS_PER_CRYPT		MS_NUM_KEYS
 
-#define PLAIN_KEY_LEN			(3*PLAINTEXT_LENGTH+1)
 #define U16_KEY_LEN			(2*PLAINTEXT_LENGTH)
 #define HASH_LEN			(16+48)
 
@@ -154,7 +153,7 @@ static void init(struct fmt_main *self)
 	self->params.max_keys_per_crypt = omp_t * MS_NUM_KEYS;
 #endif
 
-	key = mem_calloc_tiny(sizeof(*key)*PLAIN_KEY_LEN*self->params.max_keys_per_crypt, MEM_ALIGN_NONE);
+	key = mem_calloc_tiny(sizeof(*key)*(PLAINTEXT_LENGTH + 1)*self->params.max_keys_per_crypt, MEM_ALIGN_NONE);
 	md4hash = mem_calloc_tiny(sizeof(*md4hash)*HASH_LEN*self->params.max_keys_per_crypt, MEM_ALIGN_NONE);
 	crypt_out = mem_calloc_tiny(sizeof(*crypt_out)*4*self->params.max_keys_per_crypt, MEM_ALIGN_WORD);
 #if defined (MMX_COEF)
@@ -177,7 +176,8 @@ static void init(struct fmt_main *self)
 
 	if (options.utf8) {
 		// UTF8 may be up to three bytes per character
-		self->params.plaintext_length = PLAIN_KEY_LEN - 1;
+		// but core max. is 125 anyway
+		//self->params.plaintext_length = MIN(125, 3*PLAINTEXT_LENGTH);
 		tests[1].plaintext = "\xc3\xbc";         // German u-umlaut in UTF-8
 		tests[1].ciphertext = "$DCC2$10240#joe#bdb80f2c4656a8b8591bd27d39064a54";
 		tests[2].plaintext = "\xe2\x82\xac\xe2\x82\xac"; // 2 x Euro signs
@@ -487,14 +487,14 @@ static int cmp_exact(char *source, int index)
 
 static void set_key(char *_key, int index)
 {
-	strnzcpy ((char*)&key[index*PLAIN_KEY_LEN], _key, PLAIN_KEY_LEN);
+	strnzcpy ((char*)&key[index*(PLAINTEXT_LENGTH + 1)], _key, (PLAINTEXT_LENGTH + 1));
 	new_key = 1;
 }
 
 
 static char *get_key(int index)
 {
-	return (char*)&key[index*PLAIN_KEY_LEN];
+	return (char*)&key[index*(PLAINTEXT_LENGTH + 1)];
 }
 
 
@@ -685,9 +685,9 @@ static int crypt_all(int *pcount, struct db_salt *salt)
 			int utf16len;
 			UTF16 pass_unicode[PLAINTEXT_LENGTH+1];
 			MD4_CTX ctx;
-			utf16len = enc_to_utf16(pass_unicode, PLAINTEXT_LENGTH, &key[PLAIN_KEY_LEN*i], strlen((char*)&key[PLAIN_KEY_LEN*i]));
+			utf16len = enc_to_utf16(pass_unicode, PLAINTEXT_LENGTH, &key[(PLAINTEXT_LENGTH + 1)*i], strlen((char*)&key[(PLAINTEXT_LENGTH + 1)*i]));
 			if (utf16len <= 0) {
-				key[PLAIN_KEY_LEN*i-utf16len] = 0;
+				key[(PLAINTEXT_LENGTH + 1)*i-utf16len] = 0;
 				if (utf16len != 0)
 					utf16len = strlen16(pass_unicode);
 			}