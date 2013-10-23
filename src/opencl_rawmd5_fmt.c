ff0459188819784069daa8b6a846e543c0e02a77
magnum <john.magnum@hushmail.com>
2013-10-23 18:29:47 +0200

    OpenCL raw MD4/MD5 formats bugfix for overtaxing key index.

--- src/opencl_rawmd5_fmt.c
+++ src/opencl_rawmd5_fmt.c
@@ -208,8 +208,8 @@ static void init(struct fmt_main *self)
 	}
 	// Current key_idx can only hold 26 bits of offset so
 	// we can't reliably use a GWS higher than 4.7M or so.
-	if (global_work_size > (1 << 26) * 4 / PLAINTEXT_LENGTH) {
-		global_work_size = (1 << 26) * 4 / PLAINTEXT_LENGTH;
+	if (global_work_size > (1 << 26) * 4 / BUFSIZE) {
+		global_work_size = (1 << 26) * 4 / BUFSIZE;
 		global_work_size = global_work_size / local_work_size *
 			local_work_size;
 	}
