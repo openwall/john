openwall/john

commit e3289d31e5c850a96fb3fa012f7f2d478741127b

mscash2-opencl: improvedpreprocess kernel.

src/common_opencl_pbkdf2.c
@@ -13,7 +13,7 @@
 #include "options.h"
 
 typedef struct {
-	cl_kernel	krnl[3];
+	cl_kernel	krnl[4];
 	size_t		lws;
 	gpu_mem_buffer	gpu_buffer;
 	long double 	exec_time_inv;
@@ -50,9 +50,10 @@ void clean_all_buffer() {
 
 	for (i = 0; i < active_dev_ctr; i++) {
 		clean_gpu_buffer(&globalObj[ocl_device_list[i]].gpu_buffer);
-		HANDLE_CLERROR(clReleaseKernel(globalObj[ocl_device_list[i]].krnl[0]), "Error releasing kernel pbkdf2_preprocess");
-		HANDLE_CLERROR(clReleaseKernel(globalObj[ocl_device_list[i]].krnl[1]), "Error releasing kernel pbkdf2_iter");
-		HANDLE_CLERROR(clReleaseKernel(globalObj[ocl_device_list[i]].krnl[2]), "Error releasing kernel pbkdf2_postprocess");
+		HANDLE_CLERROR(clReleaseKernel(globalObj[ocl_device_list[i]].krnl[0]), "Error releasing kernel pbkdf2_preprocess_short");
+		HANDLE_CLERROR(clReleaseKernel(globalObj[ocl_device_list[i]].krnl[1]), "Error releasing kernel pbkdf2_preprocess_long");
+		HANDLE_CLERROR(clReleaseKernel(globalObj[ocl_device_list[i]].krnl[2]), "Error releasing kernel pbkdf2_iter");
+		HANDLE_CLERROR(clReleaseKernel(globalObj[ocl_device_list[i]].krnl[3]), "Error releasing kernel pbkdf2_postprocess");
 	 }
 }
 
@@ -164,17 +165,22 @@ size_t 	select_device(int jtrUniqDevNo, struct fmt_main *fmt) {
 
 	opencl_init("$JOHN/kernels/pbkdf2_kernel.cl", jtrUniqDevNo, NULL);
 
-	globalObj[jtrUniqDevNo].krnl[0] = clCreateKernel(program[jtrUniqDevNo], "pbkdf2_preprocess", &err);
+	globalObj[jtrUniqDevNo].krnl[0] = clCreateKernel(program[jtrUniqDevNo], "pbkdf2_preprocess_short", &err);
 	if (err) {
-		fprintf(stderr, "Create Kernel pbkdf2_preprocess FAILED\n");
+		fprintf(stderr, "Create Kernel pbkdf2_preprocess_short FAILED\n");
 		return 0;
 	}
-	globalObj[jtrUniqDevNo].krnl[1] = clCreateKernel(program[jtrUniqDevNo], "pbkdf2_iter", &err);
+	globalObj[jtrUniqDevNo].krnl[1] = clCreateKernel(program[jtrUniqDevNo], "pbkdf2_preprocess_long", &err);
+	if (err) {
+		fprintf(stderr, "Create Kernel pbkdf2_preprocess_long FAILED\n");
+		return 0;
+	}
+	globalObj[jtrUniqDevNo].krnl[2] = clCreateKernel(program[jtrUniqDevNo], "pbkdf2_iter", &err);
 	if (err) {
 		fprintf(stderr, "Create Kernel pbkdf2_iter FAILED\n");
 		return 0;
 	}
-	globalObj[jtrUniqDevNo].krnl[2] = clCreateKernel(program[jtrUniqDevNo], "pbkdf2_postprocess", &err);
+	globalObj[jtrUniqDevNo].krnl[3] = clCreateKernel(program[jtrUniqDevNo], "pbkdf2_postprocess", &err);
 	if (err) {
 		fprintf(stderr, "Create Kernel pbkdf2_postprocess FAILED\n");
 		return 0;
@@ -207,11 +213,13 @@ size_t 	select_device(int jtrUniqDevNo, struct fmt_main *fmt) {
 
 	HANDLE_CLERROR(clSetKernelArg(globalObj[jtrUniqDevNo].krnl[0], 0, sizeof(cl_mem), &globalObj[jtrUniqDevNo].gpu_buffer.pass_gpu), "Set Kernel 0 Arg 0 :FAILED");
 	HANDLE_CLERROR(clSetKernelArg(globalObj[jtrUniqDevNo].krnl[0], 1, sizeof(cl_mem), &globalObj[jtrUniqDevNo].gpu_buffer.salt_gpu), "Set Kernel 0 Arg 1 :FAILED");
-	HANDLE_CLERROR(clSetKernelArg(globalObj[jtrUniqDevNo].krnl[0], 4, sizeof(cl_mem), &globalObj[jtrUniqDevNo].gpu_buffer.temp_buf_gpu), "Set Kernel 0 Arg 4 :FAILED");
-	HANDLE_CLERROR(clSetKernelArg(globalObj[jtrUniqDevNo].krnl[0], 5, sizeof(cl_mem), &globalObj[jtrUniqDevNo].gpu_buffer.hmac_sha1_gpu), "Set Kernel 0 Arg 4 :FAILED");
-	HANDLE_CLERROR(clSetKernelArg(globalObj[jtrUniqDevNo].krnl[1], 0, sizeof(cl_mem), &globalObj[jtrUniqDevNo].gpu_buffer.temp_buf_gpu), "Set Kernel 1 Arg 0 :FAILED");
+	HANDLE_CLERROR(clSetKernelArg(globalObj[jtrUniqDevNo].krnl[0], 3, sizeof(cl_mem), &globalObj[jtrUniqDevNo].gpu_buffer.temp_buf_gpu), "Set Kernel 0 Arg 3 :FAILED");
+	HANDLE_CLERROR(clSetKernelArg(globalObj[jtrUniqDevNo].krnl[1], 0, sizeof(cl_mem), &globalObj[jtrUniqDevNo].gpu_buffer.pass_gpu), "Set Kernel 1 Arg 0 :FAILED");
+	HANDLE_CLERROR(clSetKernelArg(globalObj[jtrUniqDevNo].krnl[1], 1, sizeof(cl_mem), &globalObj[jtrUniqDevNo].gpu_buffer.temp_buf_gpu), "Set Kernel 1 Arg 1 :FAILED");
+	HANDLE_CLERROR(clSetKernelArg(globalObj[jtrUniqDevNo].krnl[1], 2, sizeof(cl_mem), &globalObj[jtrUniqDevNo].gpu_buffer.hmac_sha1_gpu), "Set Kernel 1 Arg 2 :FAILED");
 	HANDLE_CLERROR(clSetKernelArg(globalObj[jtrUniqDevNo].krnl[2], 0, sizeof(cl_mem), &globalObj[jtrUniqDevNo].gpu_buffer.temp_buf_gpu), "Set Kernel 2 Arg 0 :FAILED");
-	HANDLE_CLERROR(clSetKernelArg(globalObj[jtrUniqDevNo].krnl[2], 1, sizeof(cl_mem), &globalObj[jtrUniqDevNo].gpu_buffer.hash_out_gpu), "Set Kernel 2 Arg 1 :FAILED");
+	HANDLE_CLERROR(clSetKernelArg(globalObj[jtrUniqDevNo].krnl[3], 0, sizeof(cl_mem), &globalObj[jtrUniqDevNo].gpu_buffer.temp_buf_gpu), "Set Kernel 3 Arg 0 :FAILED");
+	HANDLE_CLERROR(clSetKernelArg(globalObj[jtrUniqDevNo].krnl[3], 1, sizeof(cl_mem), &globalObj[jtrUniqDevNo].gpu_buffer.hash_out_gpu), "Set Kernel 3 Arg 1 :FAILED");
 
 	if (!local_work_size)
 		find_best_workgroup(jtrUniqDevNo);
@@ -227,6 +235,9 @@ size_t 	select_device(int jtrUniqDevNo, struct fmt_main *fmt) {
 		if (maxsize2 > maxsize)
 			maxsize = maxsize2;
 		HANDLE_CLERROR(clGetKernelWorkGroupInfo(globalObj[jtrUniqDevNo].krnl[2], devices[jtrUniqDevNo], CL_KERNEL_WORK_GROUP_SIZE, sizeof(maxsize2), &maxsize2, NULL), "Error querying max LWS");
+		if (maxsize2 > maxsize)
+			maxsize = maxsize2;
+		HANDLE_CLERROR(clGetKernelWorkGroupInfo(globalObj[jtrUniqDevNo].krnl[3], devices[jtrUniqDevNo], CL_KERNEL_WORK_GROUP_SIZE, sizeof(maxsize2), &maxsize2, NULL), "Error querying max LWS");
 		if (maxsize2 > maxsize)
 			maxsize = maxsize2;
 
@@ -405,13 +416,15 @@ static gpu_mem_buffer exec_pbkdf2(cl_uint *pass_api, cl_uint *salt_api, cl_uint 
 	HANDLE_CLERROR(clEnqueueWriteBuffer(cmdq, globalObj[jtrUniqDevNo].gpu_buffer.pass_gpu, CL_FALSE, 0, 4 * num * sizeof(cl_uint), pass_api, 0, NULL, NULL ), "Copy data to gpu");
 	if(saltlen_api > 22)
 		HANDLE_CLERROR(clEnqueueWriteBuffer(cmdq, globalObj[jtrUniqDevNo].gpu_buffer.hmac_sha1_gpu, CL_FALSE, 0, 5 * num * sizeof(cl_uint), hmac_sha1_api, 0, NULL, NULL ), "Copy data to gpu");
-
+	else
+	      HANDLE_CLERROR(clSetKernelArg(globalObj[jtrUniqDevNo].krnl[0], 2, sizeof(cl_uint), &saltlen_api), "Set Kernel 0 Arg 2 :FAILED");
+	
 	HANDLE_CLERROR(clEnqueueWriteBuffer(cmdq, globalObj[jtrUniqDevNo].gpu_buffer.salt_gpu, CL_FALSE, 0, (MAX_SALT_LENGTH / 2 + 1) * sizeof(cl_uint), salt_api, 0, NULL, NULL ), "Copy data to gpu");
 
-	HANDLE_CLERROR(clSetKernelArg(globalObj[jtrUniqDevNo].krnl[0], 2, sizeof(cl_uint), &saltlen_api), "Set Kernel 0 Arg 2 :FAILED");
-	HANDLE_CLERROR(clSetKernelArg(globalObj[jtrUniqDevNo].krnl[0], 3, sizeof(cl_uint), &num), "Set Kernel 0 Arg 3 :FAILED");
-
-	err = clEnqueueNDRangeKernel(cmdq, globalObj[jtrUniqDevNo].krnl[0], 1, NULL, &N, &M, 0, NULL, &evnt);
+	if(saltlen_api < 22)
+		err = clEnqueueNDRangeKernel(cmdq, globalObj[jtrUniqDevNo].krnl[0], 1, NULL, &N, &M, 0, NULL, &evnt);
+	else
+		err = clEnqueueNDRangeKernel(cmdq, globalObj[jtrUniqDevNo].krnl[1], 1, NULL, &N, &M, 0, NULL, &evnt);
 
 	if (err) {
 		if (PROFILE)
@@ -438,9 +451,9 @@ static gpu_mem_buffer exec_pbkdf2(cl_uint *pass_api, cl_uint *salt_api, cl_uint 
 		if (i == (10240 - itrCntKrnl))
 			--itrCntKrnl;
 
-		HANDLE_CLERROR(clSetKernelArg(globalObj[jtrUniqDevNo].krnl[1], 1, sizeof(cl_uint), &itrCntKrnl), "Set Kernel 1 Arg 1 :FAILED");
+		HANDLE_CLERROR(clSetKernelArg(globalObj[jtrUniqDevNo].krnl[2], 1, sizeof(cl_uint), &itrCntKrnl), "Set Kernel 1 Arg 1 :FAILED");
 
-		err = clEnqueueNDRangeKernel(cmdq, globalObj[jtrUniqDevNo].krnl[1], 1, NULL, &N, &M, 0, NULL, &evnt);
+		err = clEnqueueNDRangeKernel(cmdq, globalObj[jtrUniqDevNo].krnl[2], 1, NULL, &N, &M, 0, NULL, &evnt);
 
 		if (err) {
 			if (PROFILE)
@@ -467,7 +480,7 @@ static gpu_mem_buffer exec_pbkdf2(cl_uint *pass_api, cl_uint *salt_api, cl_uint 
 
 	}
 
-	err = clEnqueueNDRangeKernel(cmdq, globalObj[jtrUniqDevNo].krnl[2], 1, NULL, &N, &M, 0, NULL, &events[event_ctr]);
+	err = clEnqueueNDRangeKernel(cmdq, globalObj[jtrUniqDevNo].krnl[3], 1, NULL, &N, &M, 0, NULL, &events[event_ctr]);
 
 	if (err) {
 		if (PROFILE)

src/opencl/pbkdf2_kernel.cl
@@ -549,118 +549,88 @@ inline void hmac_sha1(__private uint *istate, __private uint *ostate, __private 
 	buf[4] = A[4];
 }
 
-__kernel
-void pbkdf2_preprocess(	const __global unsigned int *pass_global,
-			const __global unsigned int *salt,
-			int usrlen,
-			uint num_keys,
-			__global temp_buf *tmp,
-			__global unsigned int *hmac_sha1_out) {
-	int lid = get_local_id(0) ;
-	int id = get_global_id(0) ;
+#define init_preprocess() 					\
+	int lid = get_local_id(0) ;				\
+	int id = get_global_id(0) ;				\
+								\
+	int i, j ;						\
+								\
+	unsigned int pass[4] ;					\
+								\
+	unsigned int istate[5], ostate[5];			\
+								\
+	unsigned int ipad[16] = { IPAD, IPAD, IPAD, IPAD,	\
+				  IPAD, IPAD, IPAD, IPAD,	\
+				  IPAD, IPAD, IPAD, IPAD,	\
+				  IPAD, IPAD, IPAD, IPAD } ;	\
+	unsigned int opad[16] = { OPAD, OPAD, OPAD, OPAD,	\
+				  OPAD, OPAD, OPAD, OPAD,	\
+				  OPAD, OPAD, OPAD, OPAD,	\
+				  OPAD, OPAD, OPAD, OPAD } ;	\
+								\
+	i = 4 * id ;						\
+        pass[0] = pass_global[i++] ;				\
+        pass[1] = pass_global[i++] ;				\
+        pass[2] = pass_global[i++] ;				\
+	pass[3] = pass_global[i] ;				\
+								\
+	for (j = 0; j < 4; j++) {				\
+		ipad[j] = ipad[j] ^ pass[j] ;			\
+		opad[j] = opad[j] ^ pass[j] ;			\
+	}							\
+								\
+	sha1_pad(ipad, istate) ;				\
+	sha1_pad(opad, ostate) ;
 
-	int i, j ;
-
-	unsigned int pass[4] ;
-
-	unsigned int istate[5], ostate[5];
 #define IPAD	0x36363636
 #define OPAD 	0x5C5C5C5C
 
-	unsigned int ipad[16] = { IPAD, IPAD, IPAD, IPAD,
-				  IPAD, IPAD, IPAD, IPAD,
-				  IPAD, IPAD, IPAD, IPAD,
-				  IPAD, IPAD, IPAD, IPAD } ;
-	unsigned int opad[16] = { OPAD, OPAD, OPAD, OPAD,
-				  OPAD, OPAD, OPAD, OPAD,
-				  OPAD, OPAD, OPAD, OPAD,
-				  OPAD, OPAD, OPAD, OPAD } ;
-#undef IPAD
-#undef OPAD
+__kernel
+void pbkdf2_preprocess_short(	const __global unsigned int *pass_global,
+				const __global unsigned int *salt,
+				int usrlen,
+				__global temp_buf *tmp	) {
 
-	i = 4 * id ;
-        pass[0] = pass_global[i++] ;
-        pass[1] = pass_global[i++] ;
-        pass[2] = pass_global[i++] ;
-	pass[3] = pass_global[i] ;
+	init_preprocess();
 
-	for (j = 0; j < 4; j++) {
-		ipad[j] = ipad[j] ^ pass[j] ;
-		opad[j] = opad[j] ^ pass[j] ;
-	}
-
-	sha1_pad(ipad, istate) ;
-	sha1_pad(opad, ostate) ;
-
-	if ((usrlen <= 22)) {
-
-		__local unsigned int salt_local[40] ;
-		if (lid == 0)
+	__local unsigned int salt_local[40] ;
+	if (lid == 0)
 		for (i = 0; i <= usrlen / 2; ++i)
 			salt_local[i] = salt[i] ;
-		barrier(CLK_LOCAL_MEM_FENCE) ;
+	barrier(CLK_LOCAL_MEM_FENCE) ;
 
-		unsigned int buf[16] = { 0 } ;
+	unsigned int buf[16] = { 0 } ;
 
-		if ((usrlen & 1)) {
-			for (i = 0; i <= usrlen >> 1; i++)
-				buf[i] = salt_local[i] ;
+	if (usrlen & 1) {
+		for (i = 0; i <= usrlen >> 1; i++)
+			buf[i] = salt_local[i] ;
 			buf[(usrlen >> 1) + 1] = 0x01 << 8 ;
-		}
-		else {
-			for (i = 0; i < usrlen >> 1; i++)
-				buf[i] = salt_local[i] ;
-			buf[usrlen >> 1] = 0x01 << 24 ;
-		}
-
-		if ((usrlen & 1))
-			buf[(usrlen >> 1) + 1] = 0x80 << 16 | buf[(usrlen >> 1) + 1] ;
-		else
-			buf[(usrlen >> 1) + 1] = 0x80 | buf[(usrlen >> 1) + 1] ;
-
-		PUT_WORD_32_BE((64 + (usrlen << 1) + 4) << 3, buf, 15) ;
-
-		hmac_sha1(istate, ostate, buf) ;
-
-		tmp[id].out[0] = buf[0] ;
-		tmp[id].out[1] = buf[1] ;
-		tmp[id].out[2] = buf[2] ;
-		tmp[id].out[3] = buf[3] ;
-
-		tmp[id].buf[0] = buf[0];
-		tmp[id].buf[1] = buf[1] ;
-		tmp[id].buf[2] = buf[2] ;
-		tmp[id].buf[3] = buf[3] ;
-		tmp[id].buf[4] = buf[4] ;
 	}
-
 	else {
-		unsigned int out[5];
-
-		i = id * 5;
-		out[0] = hmac_sha1_out[i++] ;
-		out[1] = hmac_sha1_out[i++] ;
-		out[2] = hmac_sha1_out[i++] ;
-		out[3] = hmac_sha1_out[i++] ;
-		out[4] = hmac_sha1_out[i] ;
-
-		GET_WORD_32_BE(out[0], out, 0);
-		GET_WORD_32_BE(out[1], out, 1);
-		GET_WORD_32_BE(out[2], out, 2);
-		GET_WORD_32_BE(out[3], out, 3);
-		GET_WORD_32_BE(out[4], out, 4);
-
-		tmp[id].buf[0] = out[0];
-		tmp[id].buf[1] = out[1];
-		tmp[id].buf[2] = out[2];
-		tmp[id].buf[3] = out[3];
-		tmp[id].buf[4] = out[4];
-
-		tmp[id].out[0] = out[0];
-		tmp[id].out[1] = out[1];
-		tmp[id].out[2] = out[2];
-		tmp[id].out[3] = out[3];
+		for (i = 0; i < usrlen >> 1; i++)
+			buf[i] = salt_local[i] ;
+		buf[usrlen >> 1] = 0x01 << 24 ;
 	}
+
+	if (usrlen & 1)
+		buf[(usrlen >> 1) + 1] = 0x80 << 16 | buf[(usrlen >> 1) + 1] ;
+	else
+		buf[(usrlen >> 1) + 1] = 0x80 | buf[(usrlen >> 1) + 1] ;
+
+	PUT_WORD_32_BE((64 + (usrlen << 1) + 4) << 3, buf, 15) ;
+
+	hmac_sha1(istate, ostate, buf) ;
+
+	tmp[id].out[0] = buf[0] ;
+	tmp[id].out[1] = buf[1] ;
+	tmp[id].out[2] = buf[2] ;
+	tmp[id].out[3] = buf[3] ;
+
+	tmp[id].buf[0] = buf[0];
+	tmp[id].buf[1] = buf[1] ;
+	tmp[id].buf[2] = buf[2] ;
+	tmp[id].buf[3] = buf[3] ;
+	tmp[id].buf[4] = buf[4] ;
 
 	for (i = 0; i< 5; i++) {
 		tmp[id].istate[i] = istate[i] ;
@@ -669,6 +639,48 @@ void pbkdf2_preprocess(	const __global unsigned int *pass_global,
 
 }
 
+__kernel
+void pbkdf2_preprocess_long(	const __global unsigned int *pass_global,
+				__global temp_buf *tmp,
+				__global unsigned int *hmac_sha1_out) {
+
+	init_preprocess();
+
+	unsigned int out[5];
+
+	i = id * 5;
+	out[0] = hmac_sha1_out[i++] ;
+	out[1] = hmac_sha1_out[i++] ;
+	out[2] = hmac_sha1_out[i++] ;
+	out[3] = hmac_sha1_out[i++] ;
+	out[4] = hmac_sha1_out[i] ;
+
+	GET_WORD_32_BE(out[0], out, 0);
+	GET_WORD_32_BE(out[1], out, 1);
+	GET_WORD_32_BE(out[2], out, 2);
+	GET_WORD_32_BE(out[3], out, 3);
+	GET_WORD_32_BE(out[4], out, 4);
+
+	tmp[id].buf[0] = out[0];
+	tmp[id].buf[1] = out[1];
+	tmp[id].buf[2] = out[2];
+	tmp[id].buf[3] = out[3];
+	tmp[id].buf[4] = out[4];
+
+	tmp[id].out[0] = out[0];
+	tmp[id].out[1] = out[1];
+	tmp[id].out[2] = out[2];
+	tmp[id].out[3] = out[3];
+
+	for (i = 0; i< 5; i++) {
+		tmp[id].istate[i] = istate[i] ;
+		tmp[id].ostate[i] = ostate[i] ;
+	}
+
+}
+
+#undef IPAD
+#undef OPAD
 
 __kernel
 void pbkdf2_iter(__global temp_buf *tmp,

commit 42f7a9d3384c0eb03dc9eec4f62cc4e24e663911

Fix a really nasty bug in mscash2-opencl introduced in e3289d3 which made all salt lengths of exactly length 22 result in false negatives. Test vector added to prevent this in the future.

src/common_opencl_pbkdf2.c
@@ -474,7 +474,7 @@ static gpu_mem_buffer exec_pbkdf2(cl_uint *pass_api, cl_uint *salt_api, cl_uint 
 
 	HANDLE_CLERROR(clEnqueueWriteBuffer(cmdq, globalObj[jtrUniqDevNo].gpu_buffer.salt_gpu, CL_FALSE, 0, (MAX_SALT_LENGTH / 2 + 1) * sizeof(cl_uint), salt_api, 0, NULL, NULL ), "Copy data to gpu");
 
-	if(saltlen_api < 22)
+	if(saltlen_api < 23)
 		err = clEnqueueNDRangeKernel(cmdq, globalObj[jtrUniqDevNo].krnl[0], 1, NULL, &N, &M, 0, NULL, &evnt);
 	else
 		err = clEnqueueNDRangeKernel(cmdq, globalObj[jtrUniqDevNo].krnl[1], 1, NULL, &N, &M, 0, NULL, &evnt);

src/mscash2_fmt_plug.c
@@ -94,7 +94,9 @@ static struct fmt_tests tests[] = {
 
 	// max length user name 128 bytes, and max length password, 125 bytes
 	{"$DCC2$10240#12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678#5ba26de44bd3a369f43a1c72fba76d45", "12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345"},
-// Non-standard iterations count
+	// Critical length salt
+	{"$DCC2$twentytwoXX_characters#c22936e38aac84474d9a4821b196ef5c", "password"},
+	// Non-standard iterations count
 	{"$DCC2$10000#Twelve_chars#54236c670e185043c8016006c001e982", "magnum"},
 	{NULL}
 };

src/opencl_mscash2_fmt.c
@@ -69,7 +69,9 @@ static struct fmt_tests tests[] = {
 	{"$DCC2$10240#nineteen_characters#cda4cef92db4398ce648a8fed8dc6853", "12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345"},
 	//MAX length salt with MAX length password
 	{"$DCC2$10240#12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678#5ba26de44bd3a369f43a1c72fba76d45", "12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345"},
-// Non-standard iterations count
+	// Critical length salt
+	{"$DCC2$twentytwoXX_characters#c22936e38aac84474d9a4821b196ef5c", "password"},
+	// Non-standard iterations count
 	{"$DCC2$10000#Twelve_chars#54236c670e185043c8016006c001e982", "magnum"},
 	{NULL}
 } ;