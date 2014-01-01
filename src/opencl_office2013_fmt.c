675be6c70f88c120b16d3cf7c8af783015dc00e5
magnum <john.magnum@hushmail.com>
2014-01-01 18:56:05 +0100

    Fix a typo in previous merge

--- src/opencl_office2013_fmt.c
+++ src/opencl_office2013_fmt.c
@@ -34,7 +34,7 @@
 #define HASH_LOOPS		64 /* Lower figure gives less X hogging */
 
 #define FORMAT_LABEL		"office2013-opencl"
-#define FORMAT_NAME		"MA Office 2013"
+#define FORMAT_NAME		"MS Office 2013"
 #define ALGORITHM_NAME		"SHA512 AES OpenCL"
 #define BENCHMARK_COMMENT	" (100,000 iterations)"
 #define BENCHMARK_LENGTH	-1
