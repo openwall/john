openwall/john

commit ffc781245f605ccb3949cd3eece64735958a0e6c

Minor improvement of PBKDF2-HMAC-SHA512 format's valid()

src/pbkdf2-hmac-sha512_fmt_plug.c
@@ -131,7 +131,10 @@ static int valid(char *ciphertext, struct fmt_main *self)
 	keeptr = ctcopy;
 	if (!(ptr = strtok(ctcopy, ".")))
 		goto error;
-	if (!atoi(ptr))
+	if (strlen(ptr) >= 10)
+		goto error;
+	len = atoi(ptr);
+	if (len < 0 || len >= 2147483647) // FIXME: atoi() undefined behavior
 		goto error;
 	if (!(ptr = strtok(NULL, ".")))
 		goto error;

commit 9e5805ad8aefaa17a2d0976e223035c0cf36e473

Fix warning caused by ffc7812

src/pbkdf2-hmac-sha512_fmt_plug.c
@@ -134,7 +134,7 @@ static int valid(char *ciphertext, struct fmt_main *self)
 	if (strlen(ptr) >= 10)
 		goto error;
 	len = atoi(ptr);
-	if (len < 0 || len >= INT_MAX) // FIXME: atoi() undefined behavior
+	if (len >= UINT_MAX) // FIXME: atoi() undefined behavior
 		goto error;
 	if (!(ptr = strtok(NULL, ".")))
 		goto error;