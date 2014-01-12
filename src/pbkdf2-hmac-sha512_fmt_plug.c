9aaa81da60dba0c617963a13741116658c9733bb
magnum <john.magnum@hushmail.com>
2014-01-12 16:39:16 +0100

    Change description for pbkdf2-hmac-sha512 from "OS X 10.8" to "OS X 10.8+"

--- src/pbkdf2-hmac-sha512_fmt_plug.c
+++ src/pbkdf2-hmac-sha512_fmt_plug.c
@@ -32,7 +32,7 @@
 #define FORMAT_TAG              "$pbkdf2-hmac-sha512$"
 #define FORMAT_TAG2             "$ml$"
 #define FORMAT_TAG3             "grub.pbkdf2.sha512."
-#define FORMAT_NAME             "GRUB2 / OS X 10.8"
+#define FORMAT_NAME             "GRUB2 / OS X 10.8+"
 
 #ifdef MMX_COEF_SHA512
 #define ALGORITHM_NAME		"PBKDF2-SHA512 " SHA512_ALGORITHM_NAME
