ce5966f24ca0053e653e4474bb0954cdfc6a44a1
magnum <john.magnum@hushmail.com>
2014-02-25 15:19:13 +0100

    hccap2john: Add same extra output that was recently added to wpapcap2john (BSSID's, filename etc) and handle multiple files better (don't abort on error, print filename among warnings). Closes #521. Also, support multiple entries in each file. This was a weird limitation but it might have been due to Hashcat legacy behavior (it initially only supported one per file). Anyways it's lifted now.

--- src/hccap2john.c
+++ src/hccap2john.c
@@ -1,12 +1,16 @@
 /*
-* This software is Copyright (c) 2012 Lukas Odzioba <ukasz at openwall dot net>
-* and it is hereby released to the general public under the following terms:
-* Redistribution and use in source and binary forms, with or without modification, are permitted.
-*
-* hccap2john processes input hccap files into a format suitable for use with JtR.
-* hccap format was introduced by oclHashcat-plus, and it is described here: http://hashcat.net/wiki/hccap
-* hccap format => $WPAPSK$essid#base64 encoded hccap_t
-*/
+ * This software is Copyright (c) 2012 Lukas Odzioba <ukasz at openwall dot net>
+ * and Copyright (c) 2012-2014 magnum
+ * and it is hereby released to the general public under the following terms:
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted.
+ *
+ * hccap2john processes hccap files into a format suitable for use with JtR.
+ * hccap format was introduced by oclHashcat-plus, and it is described here:
+ * http://hashcat.net/wiki/hccap
+ *
+ * hccap format => $WPAPSK$essid#base64 encoded hccap_t
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
@@ -20,18 +24,18 @@
 #include <assert.h>
 #include "common.h"
 
-#define HCCAP_SIZE		392
+#define HCCAP_SIZE		sizeof(hccap_t)
 typedef struct
 {
-  char          essid[36];
-  unsigned char mac1[6];
-  unsigned char mac2[6];
-  unsigned char nonce1[32];
-  unsigned char nonce2[32];
-  unsigned char eapol[256];
-  int           eapol_size;
-  int           keyver;
-  unsigned char keymic[16];
+	char          essid[36];
+	unsigned char mac1[6];
+	unsigned char mac2[6];
+	unsigned char nonce1[32];
+	unsigned char nonce2[32];
+	unsigned char eapol[256];
+	int           eapol_size;
+	int           keyver;
+	unsigned char keymic[16];
 } hccap_t;
 
 static void code_block(unsigned char *in, unsigned char b)
@@ -45,58 +49,91 @@ static void code_block(unsigned char *in, unsigned char b)
 		putchar(itoa64[((in[1] & 0x0f) << 2)]);
 }
 
-static void print_hccap(hccap_t * cap)
+void to_dashed(char ssid[18], unsigned char *p)
+{
+	sprintf(ssid, "%02x-%02x-%02x-%02x-%02x-%02x",p[0],p[1],p[2],p[3],p[4],p[5]);
+}
+
+void to_compact(char ssid[18], unsigned char *p)
+{
+	sprintf(ssid, "%02x%02x%02x%02x%02x%02x",p[0],p[1],p[2],p[3],p[4],p[5]);
+}
+
+static void print_hccap(hccap_t * cap, const char *filename)
 {
 	int i;
-	unsigned char *w = (unsigned char *) cap;
+	unsigned char *w = (unsigned char *)cap;
+	char sta_mac[18], ap_mac[18], gecos[13];
+	char *base;
+
+	to_compact(gecos, cap->mac1);
+	to_dashed(ap_mac, cap->mac1);
+	to_dashed(sta_mac, cap->mac2);
+
+	if ((base = strrchr(filename, '/')))
+		filename = ++base;
+
 	printf("%s:$WPAPSK$%s#", cap->essid, cap->essid);
 	for (i = 36; i + 3 < sizeof(hccap_t); i += 3)
 		code_block(&w[i], 1);
 	code_block(&w[i], 0);
-	puts("");
+	printf(":%s:%s:%s::WPA", sta_mac, ap_mac, gecos);
+	if (cap->keyver > 1)
+		printf("%d", cap->keyver);
+	printf(":%s\n", filename);
 }
 
-static void process_file(const char *filename)
+static int process_file(const char *filename)
 {
 	hccap_t hccap;
 	FILE *f;
 	struct stat sb;
+	size_t size;
 
 	f = fopen(filename, "r");
 	if (stat(filename, &sb) == -1) {
-		perror("stat");
-		exit(EXIT_FAILURE);
+		perror(filename);
+		fprintf(stderr, "\n");
+		return 0;
 	}
-	if (sb.st_size != sizeof(hccap)) {
-		puts("file has wrong size");
-		exit(EXIT_FAILURE);
+	if (sb.st_size % sizeof(hccap)) {
+		fprintf(stderr, "%s: file has wrong size\n\n", filename);
+		return 0;
 	}
-	errno = 0;
-	if (fread(&hccap, sizeof(hccap), 1, f) != 1) {
-		if (ferror(f) && errno)
-			perror("fread");
-		else
-			puts("file read error");
-		exit(EXIT_FAILURE);
-	}
+
+	size = sb.st_size;
+
+	do {
+		errno = 0;
+		if (fread(&hccap, sizeof(hccap), 1, f) != 1) {
+			if (ferror(f) && errno) {
+				perror(filename);
+				fprintf(stderr, "\n");
+			} else
+				fprintf(stderr, "%s: file read error\n\n", filename);
+			return 0;
+		}
+		print_hccap(&hccap, filename);
+		fprintf(stderr, "\n");
+		size -= sizeof(hccap);
+	} while (size);
+
 	fclose(f);
 
-	print_hccap(&hccap);
+	return 1;
 }
 
 int hccap2john(int argc, char **argv)
 {
-	int i;
-
-	assert(sizeof(hccap_t) == HCCAP_SIZE);
+	int i, ret = 0;
 
 	if (argc < 2) {
-		fprintf(stderr, "Usage: hccap2john [hccap format binary files]\n");
+		fprintf(stderr, "Usage: %s <hccap file[s]>\n", argv[0]);
 		return 1;
 	}
 
 	for (i = 1; i < argc; i++)
-		process_file(argv[i]);
+		ret |= process_file(argv[i]);
 
-	return 0;
+	return ret;
 }
