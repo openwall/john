039e25f3aff6c788752f146a45aca20cccc3870a
Claudio André <claudio.andre@correios.net.br>
2014-03-05 17:55:16 -0300

    Move auto tune functions to its own source file.

--- src/opencl_sha2_common.c
+++ src/opencl_sha2_common.c
@@ -9,60 +9,6 @@
  * http://www.gnu.org/licenses/gpl-2.0.html
  */
 
-#include "common-opencl.h"
-#include "opencl_sha2_common.h"
-
-/* Allow the developer to select configurable step size for gws. */
-int common_get_next_gws_size(size_t num, int step, int startup,
-                             int default_value) {
-	if (startup) {
-		if (step == 0)
-			return GET_MULTIPLE(default_value, local_work_size);
-		else
-			return GET_MULTIPLE(step, local_work_size);
-	}
-
-	if (step < 1)
-		return num * 2;
-
-	return num + step;
-}
-
-/* Can be used to select a 'good' default lws size */
-size_t common_get_task_max_work_group_size(int use_local_memory,
-                                           int local_memory_size,
-                                           cl_kernel crypt_kernel)
-{
-
-	size_t max_available;
-
-	if (use_local_memory)
-		max_available = get_local_memory_size(gpu_id) /
-			(local_memory_size);
-	else
-		max_available = get_device_max_lws(gpu_id);
-
-	if (max_available > get_kernel_max_lws(gpu_id, crypt_kernel))
-		return get_kernel_max_lws(gpu_id, crypt_kernel);
-
-	return max_available;
-}
-
-/* Can be used to select a 'good' default gws size */
-size_t common_get_task_max_size(int multiplier, int keys_per_core_cpu,
-                                int keys_per_core_gpu, cl_kernel crypt_kernel)
-{
-	size_t max_available;
-
-	max_available = get_max_compute_units(gpu_id);
-
-	if (cpu(device_info[gpu_id]))
-		return max_available * keys_per_core_cpu;
-	else
-		return max_available * multiplier * keys_per_core_gpu *
-			get_kernel_max_lws(gpu_id, crypt_kernel);
-}
-
 /*
  * Public domain hash function by DJ Bernstein
  * We are hashing almost the entire struct
@@ -77,33 +23,4 @@ int common_salt_hash(void * salt, int salt_size, int salt_hash_size)
 		hash = ((hash << 5) + hash) ^ s[i];
 
 	return hash & (salt_hash_size - 1);
-}
-
-/* --
-   This function could be used to calculated the best local
-   group size for the given format
-   -- */
-void common_find_best_lws(size_t group_size_limit,
-                          int sequential_id, cl_kernel crypt_kernel)
-{
-	//Call the default function.
-	opencl_find_best_lws(group_size_limit, sequential_id, crypt_kernel);
-}
-
-/* --
-   This function could be used to calculated the best num
-   of keys per crypt for the given format
-   -- */
-void common_find_best_gws(int sequential_id, unsigned int rounds, int step,
-                          unsigned long long int max_run_time)
-{
-	char *tmp_value;
-
-	if ((tmp_value = getenv("STEP")))
-		step = atoi(tmp_value);
-
-	step = GET_MULTIPLE(step, local_work_size);
-
-	//Call the default function.
-	opencl_find_best_gws(step, max_run_time, sequential_id, rounds);
 }
