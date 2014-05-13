7c3d6faaaf359d060d58e1d4f73c8305c42080a5
magnum <john.magnum@hushmail.com>
2014-05-13 20:49:11 +0200

    Add autoconf_arch.h. Sooner or later we'll not need any other one (which means we could drop it altogether and write configure output to it instead).

---
+++ src/autoconf_arch.h
@@ -0,0 +1,43 @@
+/*
+ * This file is Copyright (C) 2014 magnum,
+ * and is hereby released to the general public under the following terms:
+ * Redistribution and use in source and binary forms, with or without
+ * modifications, are permitted.
+ */
+
+/*
+ * Architecture specific parameters for autoconf target.
+ */
+
+#ifndef _JOHN_ARCH_H
+#define _JOHN_ARCH_H
+
+#include "autoconfig.h"
+
+/* Do we need an autoconf test for this? */
+#define ARCH_INDEX(x)			((unsigned int)(unsigned char)(x))
+
+/* We could do an autoconf test for this */
+#define CPU_DETECT			0
+
+#define DES_ASM				0
+#define DES_128K			0
+#define DES_X2				0
+#define DES_MASK			0
+#define DES_SCALE			1
+#define DES_EXTB			0
+#define DES_COPY			0
+#define DES_BS_ASM			0
+#define DES_BS				1
+#define DES_BS_VECTOR			0
+#define DES_BS_EXPAND			1
+
+#define MD5_ASM				0
+#define MD5_X2				1
+#define MD5_IMM				1
+
+#define BF_ASM				0
+#define BF_SCALE			0
+#define BF_X2				1
+
+#endif
