openwall/john

commit f9711be829c019ca0befe4d8330981843b99c851

Commented out old CUDA logic in Makefile.in.  Magnum, please validate we can remove this, and then remove it

src/Makefile.in
@@ -42,18 +42,21 @@ infodir = $(prefix)/info
 libdir = $(prefix)/lib/gnudl
 mandir = $(prefix)/man/man1
 
-# CUDA settings. Instead of "-arch sm_10" you could use sm_20 or sm_30 or
-# -gencode arch=compute_10,code=sm_10 -gencode arch=compute_20,code=sm_21
-# but that leads to problems on GTX Titan.
-# You could add "-gencode arch=compute_30,code=sm_30" too but that just
-# leads to regressions for Kepler cards, when it works at all.
-NVCC_FLAGS = -c -Xptxas -v -fmad=true -arch sm_10
-CUDAPATH = /usr/local/cuda
-NVCC = PATH=$(PATH):$(CUDAPATH)/bin/ nvcc
-CCBIN_OSX = /usr/bin/llvm-gcc-4.2
-
-# -DHAVE_SKEY
-# -lskey
+########
+# This section commented out, until Magnum can validate we do not need it.  I am
+# not going to scrap it (yet), but I think configure should handle ALL of this,
+# I am just not 100% sure it is all done right now.
+########
+#   # CUDA settings. Instead of "-arch sm_10" you could use sm_20 or sm_30 or
+#   # -gencode arch=compute_10,code=sm_10 -gencode arch=compute_20,code=sm_21
+#   # but that leads to problems on GTX Titan.
+#   # You could add "-gencode arch=compute_30,code=sm_30" too but that just
+#   # leads to regressions for Kepler cards, when it works at all.
+#   NVCC_FLAGS = -c -Xptxas -v -fmad=true -arch sm_10
+#   CUDAPATH = /usr/local/cuda
+#   NVCC = PATH=$(PATH):$(CUDAPATH)/bin/ nvcc
+#   CCBIN_OSX = /usr/bin/llvm-gcc-4.2
+########
 
 # @CPU_BITS@ left out
 CPPFLAGS += @CPPFLAGS@

commit 5fdd91fdf42765d8c4b2bd386aa2cac12259d65f

Fix CUDA after f9711be broke it.

src/Makefile.in
@@ -42,25 +42,16 @@ infodir = $(prefix)/info
 libdir = $(prefix)/lib/gnudl
 mandir = $(prefix)/man/man1
 
-########
-# This section commented out, until Magnum can validate we do not need it.  I am
-# not going to scrap it (yet), but I think configure should handle ALL of this,
-# I am just not 100% sure it is all done right now.
-########
-#   # CUDA settings. Instead of "-arch sm_10" you could use sm_20 or sm_30 or
-#   # -gencode arch=compute_10,code=sm_10 -gencode arch=compute_20,code=sm_21
-#   # but that leads to problems on GTX Titan.
-#   # You could add "-gencode arch=compute_30,code=sm_30" too but that just
-#   # leads to regressions for Kepler cards, when it works at all.
-#   NVCC_FLAGS = -c -Xptxas -v -fmad=true -arch sm_10
-#   CUDAPATH = /usr/local/cuda
-#   NVCC = PATH=$(PATH):$(CUDAPATH)/bin/ nvcc
-#   CCBIN_OSX = /usr/bin/llvm-gcc-4.2
-########
+# CUDA settings. Instead of "-arch sm_10" you could use sm_20 or sm_30 or
+# -gencode arch=compute_10,code=sm_10 -gencode arch=compute_20,code=sm_21
+# but that leads to problems on GTX Titan.
+# You could add "-gencode arch=compute_30,code=sm_30" too but that just
+# leads to regressions for Kepler cards, when it works at all.
+NVCC_FLAGS = -c -Xptxas -v -fmad=true -arch sm_10
 
 # @CPU_BITS@ left out
 CPPFLAGS += @CPPFLAGS@
-CFLAGSX = -DAC_BUILT $(CPPFLAGS) -c @CFLAGS@ @CFLAGS_EXTRA@ @HAVE_NSS@ @NSS_CFLAGS@ @OPENSSL_CFLAGS@ @OPENMP_CFLAGS@ @HAVE_MPI@ $(RELEASE_BLD)
+CFLAGSX = -DAC_BUILT $(CPPFLAGS) -c @CFLAGS@ @CFLAGS_EXTRA@ @HAVE_CUDA@ @HAVE_NSS@ @NSS_CFLAGS@ @OPENSSL_CFLAGS@ @OPENMP_CFLAGS@ @HAVE_MPI@ $(RELEASE_BLD)
 # CFLAGS for use on the main john.c file only
 CFLAGS_MAIN = @CC_MAIN_CPU@ $(CFLAGSX)
 CFLAGS = @CC_CPU@ $(CFLAGSX)
@@ -197,18 +188,12 @@ endif
 
 WITH_CUDA=@HAVE_CUDA@
 ifdef WITH_CUDA
-CFLAGS += -I$(CUDAPATH)/include -DHAVE_CUDA
-CFLAGS_MAIN += -I$(CUDAPATH)/include -DHAVE_CUDA
-ifneq ($(wildcard $(CUDAPATH)/lib),)
-LDFLAGS += -L$(CUDAPATH)/lib
-endif
-ifneq ($(wildcard $(CUDAPATH)/lib@CPU_BIT_STR@),)
-LDFLAGS += -L$(CUDAPATH)/lib@CPU_BIT_STR@
-endif
-JOHN_OBJS += $(CUDA_OBJS)
+NVCC = PATH=$(PATH):%NVIDIA_CUDA%/bin/ nvcc
+CCBIN_OSX = /usr/bin/llvm-gcc-4.2
 ifneq ($(wildcard $(CCBIN_OSX)),)
 NVCC_FLAGS += @CPU_BITS@ -ccbin=$(CCBIN_OSX)
 endif
+JOHN_OBJS += $(CUDA_OBJS)
 endif
 
 WITH_OPENCL=@CL_LIBS@

src/configure
@@ -654,6 +654,7 @@ HAVE_NSS
 HAVE_PCAP
 OPENSSL_LIBS
 OPENSSL_CFLAGS
+NVIDIA_CUDA
 EGREP
 PKG_CONFIG_LIBDIR
 PKG_CONFIG_PATH
@@ -8838,16 +8839,20 @@ CPPFLAGS+="$ADD_CFLAGS"
 $as_echo_n "checking additional paths for CUDA... " >&6; }
   ADD_LDFLAGS=""
   ADD_CFLAGS=""
-if test -z "$NVIDIA_CUDA"; then
-   NVIDIA_CUDA=/usr/local/cuda
+if test -n "$NVIDIA_CUDA"; then
+   CUDAPATH="$NVIDIA_CUDA"
+else
+   CUDAPATH=/usr/local/cuda
 fi
-if test -d "$NVIDIA_CUDA/include"; then
-   ADD_CFLAGS+=" -I$NVIDIA_CUDA/include"
+NVIDIA_CUDA="$CUDAPATH"
+
+if test -d "$CUDAPATH/include"; then
+   ADD_CFLAGS+=" -I$CUDAPATH/include"
 fi
-if test $CPU_BIT_STR = 64 -a -d "$NVIDIA_CUDA/lib64"; then
-   ADD_LDFLAGS+=" -L$NVIDIA_CUDA/lib64"
-elif test -d "$NVIDIA_CUDA/lib"; then
-   ADD_LDFLAGS+=" -L$NVIDIA_CUDA/lib"
+if test $CPU_BIT_STR = 64 -a -d "$CUDAPATH/lib64"; then
+   ADD_LDFLAGS+=" -L$CUDAPATH/lib64"
+elif test -d "$CUDAPATH/lib"; then
+   ADD_LDFLAGS+=" -L$CUDAPATH/lib"
 fi
 case "x${ADD_CFLAGS}x${ADD_LDFLAGS}" in
      "xx") cond_and="no" ;;
@@ -11166,7 +11171,7 @@ fi
 { $as_echo "$as_me:${as_lineno-$LINENO}: result: $ac_cv_lib_cudart_cudaGetDeviceCount" >&5
 $as_echo "$ac_cv_lib_cudart_cudaGetDeviceCount" >&6; }
 if test "x$ac_cv_lib_cudart_cudaGetDeviceCount" = xyes; then :
-  using_cuda=yes HAVE_CUDA=1
+  using_cuda=yes HAVE_CUDA=-DHAVE_CUDA
  LIBS+=" -lcudart"
 fi
 

src/configure.ac
@@ -129,7 +129,7 @@ AC_MSG_CHECKING([arg check macro with $CC])
 AC_JTR_FLAG_CHECK([-fFOO_BAR_SWITCH_NOT_VALID], 0)
 AC_JTR_FLAG_CHECK([-mFOO_BAR_SWITCH_NOT_VALID], 0)
 AC_JTR_FLAG_CHECK([-m666], 0)
-AS_IF([test "x${CFLAGS_EX}" = x], 
+AS_IF([test "x${CFLAGS_EX}" = x],
    [AC_MSG_RESULT([yes])]
    JTR_FLAG_CHECK_WORKS=yes
    ,[AC_MSG_RESULT([no])])
@@ -421,7 +421,7 @@ AX_OPENCL(C)
 # Check for CUDA, including headers.
 using_cuda=no
 if test "x$enable_cuda" != xno; then
-   AC_CHECK_HEADER([cuda.h],[AC_CHECK_LIB([cudart],[cudaGetDeviceCount],using_cuda=yes AC_SUBST(HAVE_CUDA,1) [LIBS+=" -lcudart"])])
+   AC_CHECK_HEADER([cuda.h],[AC_CHECK_LIB([cudart],[cudaGetDeviceCount],using_cuda=yes AC_SUBST([HAVE_CUDA],[-DHAVE_CUDA]) [LIBS+=" -lcudart"])])
    if test "x$using_cuda" != xyes -a "x$enable_cuda" = xyes; then
       AC_MSG_FAILURE([CUDA not found])
    fi

src/m4/ax_jtr_utility_macros.m4
@@ -64,16 +64,19 @@ AC_DEFUN([AC_JTR_SET_CUDA_INCLUDES],
   AC_MSG_CHECKING([additional paths for CUDA])
   ADD_LDFLAGS=""
   ADD_CFLAGS=""
-if test -z "$NVIDIA_CUDA"; then
-   NVIDIA_CUDA=/usr/local/cuda
+if test -n "$NVIDIA_CUDA"; then
+   CUDAPATH="$NVIDIA_CUDA"
+else
+   CUDAPATH=/usr/local/cuda
 fi
-if test -d "$NVIDIA_CUDA/include"; then
-   ADD_CFLAGS+=" -I$NVIDIA_CUDA/include"
+AC_SUBST([NVIDIA_CUDA],["$CUDAPATH"])
+if test -d "$CUDAPATH/include"; then
+   ADD_CFLAGS+=" -I$CUDAPATH/include"
 fi
-if test $CPU_BIT_STR = 64 -a -d "$NVIDIA_CUDA/lib64"; then
-   ADD_LDFLAGS+=" -L$NVIDIA_CUDA/lib64"
-elif test -d "$NVIDIA_CUDA/lib"; then
-   ADD_LDFLAGS+=" -L$NVIDIA_CUDA/lib"
+if test $CPU_BIT_STR = 64 -a -d "$CUDAPATH/lib64"; then
+   ADD_LDFLAGS+=" -L$CUDAPATH/lib64"
+elif test -d "$CUDAPATH/lib"; then
+   ADD_LDFLAGS+=" -L$CUDAPATH/lib"
 fi
 case "x${ADD_CFLAGS}x${ADD_LDFLAGS}" in
      "xx") cond_and="no" ;;