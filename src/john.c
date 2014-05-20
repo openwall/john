openwall/john

commit 246f945534a21b849a879bfa0597ec0b5ec24a37

Bugfix for forked resume.

src/john.c
@@ -878,30 +878,30 @@ static void john_load_conf(void)
 		}
 	}
 
-	if (options.loader.activepot == NULL) {
+	if (pers_opts.activepot == NULL) {
 		if (options.secure)
-			options.loader.activepot = str_alloc_copy(SEC_POT_NAME);
+			pers_opts.activepot = str_alloc_copy(SEC_POT_NAME);
 		else
-			options.loader.activepot = str_alloc_copy(POT_NAME);
+			pers_opts.activepot = str_alloc_copy(POT_NAME);
 	}
 
-	if (options.activewordlistrules == NULL)
-		if (!(options.activewordlistrules =
+	if (pers_opts.activewordlistrules == NULL)
+		if (!(pers_opts.activewordlistrules =
 		      cfg_get_param(SECTION_OPTIONS, NULL,
 		                    "BatchModeWordlistRules")))
-			options.activewordlistrules =
+			pers_opts.activewordlistrules =
 				str_alloc_copy(SUBSECTION_WORDLIST);
 
-	if (options.activesinglerules == NULL)
-		if (!(options.activesinglerules =
+	if (pers_opts.activesinglerules == NULL)
+		if (!(pers_opts.activesinglerules =
 		      cfg_get_param(SECTION_OPTIONS, NULL,
 		                    "SingleRules")))
-			options.activesinglerules =
+			pers_opts.activesinglerules =
 				str_alloc_copy(SUBSECTION_SINGLE);
 
 	if ((options.flags & FLG_LOOPBACK_CHK) &&
 	    !(options.flags & FLG_RULES)) {
-		if ((options.activewordlistrules =
+		if ((pers_opts.activewordlistrules =
 		     cfg_get_param(SECTION_OPTIONS, NULL,
 		                   "LoopbackRules")))
 			options.flags |= FLG_RULES;
@@ -909,7 +909,7 @@ static void john_load_conf(void)
 
 	if ((options.flags & FLG_WORDLIST_CHK) &&
 	    !(options.flags & FLG_RULES)) {
-		if ((options.activewordlistrules =
+		if ((pers_opts.activewordlistrules =
 		     cfg_get_param(SECTION_OPTIONS, NULL,
 		                   "WordlistRules")))
 			options.flags |= FLG_RULES;
@@ -1074,7 +1074,7 @@ static void john_load(void)
 		ldr_init_database(&database, &options.loader);
 
 		if (options.flags & FLG_PASSWD) {
-			ldr_show_pot_file(&database, options.loader.activepot);
+			ldr_show_pot_file(&database, pers_opts.activepot);
 
 			database.options->flags |= DB_PLAINTEXTS;
 			if ((current = options.passwd->head))
@@ -1083,7 +1083,7 @@ static void john_load(void)
 			} while ((current = current->next));
 		} else {
 			database.options->flags |= DB_PLAINTEXTS;
-			ldr_show_pot_file(&database, options.loader.activepot);
+			ldr_show_pot_file(&database, pers_opts.activepot);
 		}
 
 		return;
@@ -1114,7 +1114,7 @@ static void john_load(void)
 			options.loader.flags |= DB_CRACKED;
 			ldr_init_database(&database, &options.loader);
 
-			ldr_show_pot_file(&database, options.loader.activepot);
+			ldr_show_pot_file(&database, pers_opts.activepot);
 
 			if ((current = options.passwd->head))
 			do {
@@ -1178,7 +1178,7 @@ static void john_load(void)
 		}
 
 		total = database.password_count;
-		ldr_load_pot_file(&database, options.loader.activepot);
+		ldr_load_pot_file(&database, pers_opts.activepot);
 		ldr_fix_database(&database);
 
 		if (!database.password_count) {
@@ -1226,9 +1226,9 @@ static void john_load(void)
 		options.loader.flags |= DB_CRACKED;
 		ldr_init_database(&loop_db, &options.loader);
 
-		loop_db.options->activepot = options.wordlist ?
-			options.wordlist : options.loader.activepot;
-		ldr_show_pot_file(&loop_db, loop_db.options->activepot);
+		pers_opts.activepot = options.wordlist ?
+			options.wordlist : pers_opts.activepot;
+		ldr_show_pot_file(&loop_db, pers_opts.activepot);
 
 		loop_db.options->flags |= DB_PLAINTEXTS;
 
@@ -1461,7 +1461,7 @@ static void john_run(void)
 				error();
 			}
 			database.format->methods.reset(&database);
-			log_init(LOG_NAME, options.loader.activepot,
+			log_init(LOG_NAME, pers_opts.activepot,
 			         options.session);
 			status_init(NULL, 1);
 			if (john_main_process) {

src/cracker.c
@@ -475,7 +475,7 @@ int crk_reload_pot(void)
 		return 0;
 
 	if ((pot_fd =
-	     open(path_expand(options.loader.activepot), O_RDONLY)) == -1) {
+	     open(path_expand(pers_opts.activepot), O_RDONLY)) == -1) {
 		if (errno != ENOENT)
 			perror("open potfile");
 		return 0;
@@ -483,10 +483,10 @@ int crk_reload_pot(void)
 
 #if OS_FLOCK
 	if (flock(pot_fd, LOCK_SH) == -1)
-		pexit("flock: %s", options.loader.activepot);
+		pexit("flock: %s", pers_opts.activepot);
 #endif
 	if (!(pot_file = fdopen(pot_fd, "rb")))
-		pexit("fdopen: %s", options.loader.activepot);
+		pexit("fdopen: %s", pers_opts.activepot);
 
 	if (crk_pot_pos && (fseek(pot_file, crk_pot_pos, SEEK_SET) == -1)) {
 		perror("fseek");

src/loader.c
@@ -127,7 +127,7 @@ static void read_file(struct db_main *db, char *name, int flags,
 		process_line(db, line);
 		check_abort(0);
 	}
-	if (name == options.loader.activepot)
+	if (name == pers_opts.activepot)
 		crk_pot_pos = (long int)ftell(file);
 
 	if (ferror(file)) pexit("fgets");

src/loader.h
@@ -192,9 +192,6 @@ struct db_options {
 	unsigned int max_cost[FMT_TUNABLE_COSTS];
 #endif
 
-/* Pot file used (default is $JOHN/john.pot) */
-	char *activepot;
-
 /* if --show=left is used, john dumps the non-cracked hashes */
 	int showuncracked;
 

src/options.c
@@ -66,7 +66,7 @@ static char *costs_str;
 static struct opt_entry opt_list[] = {
 	{"", FLG_PASSWD, 0, 0, 0, OPT_FMT_ADD_LIST, &options.passwd},
 	{"single", FLG_SINGLE_SET, FLG_CRACKING_CHK, 0, 0,
-		OPT_FMT_STR_ALLOC, &options.activesinglerules},
+		OPT_FMT_STR_ALLOC, &pers_opts.activesinglerules},
 	{"wordlist", FLG_WORDLIST_SET, FLG_CRACKING_CHK,
 		0, 0, OPT_FMT_STR_ALLOC, &options.wordlist},
 	{"loopback", FLG_LOOPBACK_SET, FLG_CRACKING_CHK,
@@ -88,7 +88,7 @@ static struct opt_entry opt_list[] = {
 	{"pipe", FLG_PIPE_SET, FLG_CRACKING_CHK},
 #endif
 	{"rules", FLG_RULES, FLG_RULES, FLG_WORDLIST_CHK, FLG_STDIN_CHK,
-		OPT_FMT_STR_ALLOC, &options.activewordlistrules},
+		OPT_FMT_STR_ALLOC, &pers_opts.activewordlistrules},
 	{"incremental", FLG_INC_SET, FLG_CRACKING_CHK,
 		0, 0, OPT_FMT_STR_ALLOC, &options.charset},
 	{"mask", FLG_MASK_SET, FLG_CRACKING_CHK,
@@ -143,7 +143,7 @@ static struct opt_entry opt_list[] = {
 		"%u", &options.fork},
 #endif
 	{"pot", FLG_POT, FLG_POT, 0, OPT_REQ_PARAM,
-	    OPT_FMT_STR_ALLOC, &options.loader.activepot},
+	    OPT_FMT_STR_ALLOC, &pers_opts.activepot},
 	{"format", FLG_FORMAT, FLG_FORMAT,
 		0, FLG_STDOUT | OPT_REQ_PARAM,
 		OPT_FMT_STR_ALLOC, &options.format},

src/options.h
@@ -227,12 +227,6 @@ struct options_main {
    is done. */
 	unsigned int max_fix_state_delay;
 
-/* the wordlist rules section (default if none entered is Wordlist) */
-	char *activewordlistrules;
-
-/* the 'single' rules section (default if none entered is Single) */
-	char *activesinglerules;
-
 /* This is a 'special' flag.  It causes john to add 'extra' code to search for
  * some salted types, when we have only the hashes.  The only type supported is
  * PHPS (at this time.).  So PHPS will set this to a 1. OTherwise it will
@@ -335,6 +329,15 @@ struct pers_opts {
 
 /* Show/log/report UTF-8. Default is no conversion. */
 	int report_utf8;
+
+/* Pot file used (default is $JOHN/john.pot) */
+	char *activepot;
+
+/* the wordlist rules section (default if none entered is Wordlist) */
+	char *activewordlistrules;
+
+/* the 'single' rules section (default if none entered is Single) */
+	char *activesinglerules;
 };
 
 extern struct pers_opts pers_opts;

src/recovery.c
@@ -218,10 +218,10 @@ void rec_save(void)
 			fprintf(rec_file, "%s=%s\n", *opt, options.wordlist);
 		else if (!strcmp(*opt, "--rules"))
 			fprintf(rec_file, "%s=%s\n", *opt,
-			        options.activewordlistrules);
+			        pers_opts.activewordlistrules);
 		else if (!strcmp(*opt, "--single"))
 			fprintf(rec_file, "%s=%s\n", *opt,
-			        options.activesinglerules);
+			        pers_opts.activesinglerules);
 		else if (!strcmp(*opt, "--incremental"))
 			fprintf(rec_file, "%s=%s\n", *opt,
 			        options.charset);

src/single.c
@@ -115,12 +115,12 @@ static void single_init(void)
 	while (key_count > 0xffff / length + 1)
 		key_count >>= 1;
 
-	if (rpp_init(rule_ctx, options.activesinglerules)) {
+	if (rpp_init(rule_ctx, pers_opts.activesinglerules)) {
 		log_event("! No \"%s\" mode rules found",
-		          options.activesinglerules);
+		          pers_opts.activesinglerules);
 		if (john_main_process)
 			fprintf(stderr, "No \"%s\" mode rules found in %s\n",
-			        options.activesinglerules, cfg_name);
+			        pers_opts.activesinglerules, cfg_name);
 		error();
 	}
 

commit 15c60e268100d55d1d80dad4876d89a7eeee5af5

246f945 introduced a bad bug when -loop is used with a file name other than the active pot file.

src/john.c
@@ -1228,6 +1228,7 @@ static void john_load(void)
 	    database.format != &fmt_LM) {
 		struct db_main loop_db;
 		struct fmt_main *save_list = fmt_list;
+		char *save_pot = pers_opts.activepot;
 
 		fmt_list = &fmt_LM;
 
@@ -1256,6 +1257,7 @@ static void john_load(void)
 		}
 		database.plaintexts = loop_db.plaintexts;
 		options.loader.flags &= ~DB_CRACKED;
+		pers_opts.activepot = save_pot;
 		fmt_list = save_list;
 	}
 