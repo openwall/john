openwall/john

commit 9aa03202fd8ebb32bc01625c02be577d16d66613

Sync Raw-MD5 format tests (OpenCL / CPU / dynamic)

src/rawMD5_fmt_plug.c
@@ -59,11 +59,16 @@ john_register_one(&fmt_rawMD5);
 #define TAG_LENGTH				(sizeof(FORMAT_TAG) - 1)
 
 static struct fmt_tests tests[] = {
-	{"5a105e8b9d40e1329780d62ea2265d8a","test1"},
-	{FORMAT_TAG "5a105e8b9d40e1329780d62ea2265d8a","test1"},
-	{FORMAT_TAG "378e2c4a07968da2eca692320136433d","thatsworking"},
-	{FORMAT_TAG "8ad8757baa8564dc136c1e07507f4a98","test3"},
+	{"5a105e8b9d40e1329780d62ea2265d8a", "test1"},
+	{FORMAT_TAG "5a105e8b9d40e1329780d62ea2265d8a", "test1"},
+	{"098f6bcd4621d373cade4e832627b4f6", "test"},
+	{FORMAT_TAG "378e2c4a07968da2eca692320136433d", "thatsworking"},
+	{FORMAT_TAG "8ad8757baa8564dc136c1e07507f4a98", "test3"},
 	{"d41d8cd98f00b204e9800998ecf8427e", ""},
+#ifdef DEBUG
+	{FORMAT_TAG "c9ccf168914a1bcfc3229f1948e67da0","1234567890123456789012345678901234567890123456789012345"},
+	{FORMAT_TAG "57edf4a22be3c955ac49da2e2107b67a","12345678901234567890123456789012345678901234567890123456789012345678901234567890"},
+#endif
 	{NULL}
 };
 

src/dynamic_preloads.c
@@ -218,11 +218,12 @@ static DYNAMIC_primitive_funcp _Funcs_0[] =
 static struct fmt_tests _Preloads_0[] =
 {
 	{"$dynamic_0$5a105e8b9d40e1329780d62ea2265d8a","test1"},
+	{"$dynamic_0$098f6bcd4621d373cade4e832627b4f6","test"},
 	{"$dynamic_0$378e2c4a07968da2eca692320136433d","thatsworking"},
 	{"$dynamic_0$8ad8757baa8564dc136c1e07507f4a98","test3"},
 	// These next 2 do slow down self tests, so (by as much as 20%), so they are commented out, but can be uncommented
 	// to validate that these max length hashes DO not cause the format to fail to work.  They MUST be able to be
-	// successfully processed with no errors.  But can be commented out later, to keep same STest speeds.
+	// successfully processed with no errors.  But can be commented out later, to keep same test speeds.
 	// to turn on these 'max length' tests, just rebuild with -DDEBUG
 #ifdef DEBUG
 	{"$dynamic_0$c9ccf168914a1bcfc3229f1948e67da0","1234567890123456789012345678901234567890123456789012345"},

src/opencl_rawmd5_fmt_plug.c
@@ -75,10 +75,18 @@ static int crypt_all(int *pcount, struct db_salt *_salt);
 #include "opencl_autotune.h"
 
 static struct fmt_tests tests[] = {
+	{"5a105e8b9d40e1329780d62ea2265d8a", "test1"},
+	{FORMAT_TAG "5a105e8b9d40e1329780d62ea2265d8a", "test1"},
 	{"098f6bcd4621d373cade4e832627b4f6", "test"},
-	{FORMAT_TAG "378e2c4a07968da2eca692320136433d","thatsworking"},
-	{FORMAT_TAG "8ad8757baa8564dc136c1e07507f4a98","test3"},
+	{FORMAT_TAG "378e2c4a07968da2eca692320136433d", "thatsworking"},
+	{FORMAT_TAG "8ad8757baa8564dc136c1e07507f4a98", "test3"},
 	{"d41d8cd98f00b204e9800998ecf8427e", ""},
+#ifdef DEBUG
+	{FORMAT_TAG "c9ccf168914a1bcfc3229f1948e67da0","1234567890123456789012345678901234567890123456789012345"},
+#if PLAINTEXT_LENGTH >= 80
+	{FORMAT_TAG "57edf4a22be3c955ac49da2e2107b67a","12345678901234567890123456789012345678901234567890123456789012345678901234567890"},
+#endif
+#endif
 	{NULL}
 };
 

commit 73568a4c065c0913aaf74afe073e23f4f7ec9e8a

Raw-MD5: fix debug build breakage caused by 9aa03202

src/rawMD5_fmt_plug.c
@@ -67,7 +67,9 @@ static struct fmt_tests tests[] = {
 	{"d41d8cd98f00b204e9800998ecf8427e", ""},
 #ifdef DEBUG
 	{FORMAT_TAG "c9ccf168914a1bcfc3229f1948e67da0","1234567890123456789012345678901234567890123456789012345"},
+#if PLAINTEXT_LENGTH >= 80
 	{FORMAT_TAG "57edf4a22be3c955ac49da2e2107b67a","12345678901234567890123456789012345678901234567890123456789012345678901234567890"},
+#endif
 #endif
 	{NULL}
 };