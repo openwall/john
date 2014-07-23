openwall/john

commit cc4650b16ef0e9704a2d5ec93f0d3ba94402830f

Workaround for a thin dynamic problem resulting from recent self test fixes.

src/bench.c
@@ -432,6 +432,11 @@ int benchmark_all(void)
 			}
 		}
 #endif
+
+		/* Kludge for thin dynamics */
+		if (format->params.flags && FMT_DYNAMIC)
+			fmt_init(format);
+
 #ifdef _OPENMP
 		// MPIOMPmutex may have capped the number of threads
 		ompt = omp_get_max_threads();

src/formats.c
@@ -145,15 +145,6 @@ static char *fmt_self_test_body(struct fmt_main *format,
 #endif
 	int ml;
 
-	/*
-	 * Kludge for thin dynamic formats (split is NULL before init).
-	 * This is a bug in dynamic - it should be fixed and this kludge
-	 * should be dropped. The split() function can be called BEFORE
-	 * init(), by definition.
-	 */
-	if (!format->methods.split)
-		fmt_init(format);
-
 	// validate that there are no NULL function pointers
 	if (format->methods.prepare == NULL)    return "method prepare NULL";
 	if (format->methods.valid == NULL)      return "method valid NULL";

commit c256ebc1584d195362a0ec389c3d92b0438d4d3c

Fix a bug "use of logical '&&' with constant" in bench .c
This had been introduced with commit cc4650b1-
Looks like init() had been called for all formats, not just for dynamic formats.

clang warned about this:
bench.c:437:28: warning: use of logical '&&' with constant operand [-Wconstant-logical-operand]
                if (format->params.flags && FMT_DYNAMIC)
                                         ^  ~~~~~~~~~~~
bench.c:437:28: note: use '&' for a bitwise operation
                if (format->params.flags && FMT_DYNAMIC)
                                         ^~
                                         &
bench.c:437:28: note: remove constant to silence this warning
                if (format->params.flags && FMT_DYNAMIC)
                                        ~^~~~~~~~~~~~~~
1 warning generated.

Also, adjusted the comment, while at it (added a FIXME).

src/bench.c
@@ -433,8 +433,8 @@ int benchmark_all(void)
 		}
 #endif
 
-		/* Kludge for thin dynamics */
-		if (format->params.flags && FMT_DYNAMIC)
+		/* FIXME: Kludge for thin dynamics */
+		if (format->params.flags & FMT_DYNAMIC)
 			fmt_init(format);
 
 #ifdef _OPENMP