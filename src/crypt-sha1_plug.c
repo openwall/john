a96e5470d0e43f3f20fc872709188d9a1624589e
JimF <jfoug@cox.net>
2014-08-27 10:49:38 -0500

    Removed Gladman HMAC code, choosing a faster saved state model, like we do in PBKDF2

--- src/crypt-sha1_plug.c
+++ src/crypt-sha1_plug.c
@@ -49,7 +49,11 @@
 #include <time.h>
 
 #include "jumbo.h"
+//#define USE_NAIVE_GLADMAN_CODE
+#ifdef USE_NAIVE_GLADMAN_CODE
 #include "gladman_hmac.h"
+#endif
+#include "memdbg.h"
 
 /*
  * The default iterations - should take >0s on a fast CPU
@@ -64,6 +68,73 @@
 #ifndef CRYPT_SHA1_SALT_LENGTH
 #define CRYPT_SHA1_SALT_LENGTH 64
 #endif
+
+/* 
+ * this is the function from mscash2 john wiki (http://openwall.info/wiki/john/MSCash2_simple_code)
+ * tweaked just a touch for differences between PBKDF2 and PBKDF1 (lack of xor accum of digest)
+ * and it also contains logic to handle passwords > 64 bytes.
+ */
+static void pbkdf1(
+		const unsigned char key[], size_t key_len,
+		unsigned char salt[],size_t salt_len,
+		unsigned int rounds,
+		unsigned char digest[])
+{
+	SHA_CTX ctx1, ctx2, tmp_ctx1, tmp_ctx2;
+	unsigned char ipad[SHA_CBLOCK+1], opad[SHA_CBLOCK+1], tmp_hash[SHA_DIGEST_LENGTH];
+	unsigned i, j;
+	const unsigned char *keyp = key;
+ 
+	memset(ipad, 0x36, sizeof(ipad));
+	memset(opad, 0x5C, sizeof(opad));
+ 
+	// if key_len is > 64, then we hash and use the hash as the key.
+	if (key_len > SHA_CBLOCK) {
+		SHA1_Init(&ctx1);
+		SHA1_Update(&ctx1,key,key_len);
+		SHA1_Final(tmp_hash,&ctx1);
+		keyp = tmp_hash;
+		key_len = SHA_DIGEST_LENGTH;
+	}
+
+	for(i = 0;i < key_len;i++) {
+		ipad[i] ^= keyp[i];
+		opad[i] ^= keyp[i];
+	}
+ 
+	SHA1_Init(&ctx1);
+	SHA1_Init(&ctx2);
+ 
+	SHA1_Update(&ctx1,ipad,SHA_CBLOCK);
+	SHA1_Update(&ctx2,opad,SHA_CBLOCK);
+ 
+	memcpy(&tmp_ctx1,&ctx1,sizeof(SHA_CTX));
+	memcpy(&tmp_ctx2,&ctx2,sizeof(SHA_CTX));
+ 
+	SHA1_Update(&ctx1,salt,salt_len);
+	SHA1_Final(tmp_hash,&ctx1);
+ 
+	SHA1_Update(&ctx2,tmp_hash,SHA_DIGEST_LENGTH);
+	SHA1_Final(tmp_hash,&ctx2);
+ 
+	memcpy(digest,tmp_hash,SHA_DIGEST_LENGTH);
+ 
+	for(i = 1;i < rounds;i++)
+	{
+		memcpy(&ctx1,&tmp_ctx1,sizeof(SHA_CTX));
+		memcpy(&ctx2,&tmp_ctx2,sizeof(SHA_CTX));
+ 
+		SHA1_Update(&ctx1,tmp_hash,SHA_DIGEST_LENGTH);
+		SHA1_Final(tmp_hash,&ctx1);
+ 
+		SHA1_Update(&ctx2,tmp_hash,SHA_DIGEST_LENGTH);
+		SHA1_Final(tmp_hash,&ctx2);
+ 
+		//for(j = 0;j<5;j++)
+		//	((unsigned int*)digest)[j] ^= ((unsigned int*)tmp_hash)[j];
+	}
+	memcpy(digest,tmp_hash,SHA_DIGEST_LENGTH);
+}
 
 /*
  * UNIX password using hmac_sha1
@@ -128,12 +199,15 @@ void internal_crypt_sha1(const char *pw, const char *salt, unsigned char *inout)
 	// Prime the pump with <salt><magic><iterations>
 	dl = snprintf((char*)inout, 32, "%.*s%s%u", sl, salt, magic, iterations);
 
+#ifdef USE_NAIVE_GLADMAN_CODE
 	// Then hmac using <pw> as key, and repeat...
 	hmac_sha1((unsigned char*)pw, pl, inout, dl, hmac_buf, SHA1_SIZE);
 	for (i = 1; i < iterations; i++) {
 		hmac_sha1((unsigned char*)pw, pl, hmac_buf, SHA1_SIZE, hmac_buf, SHA1_SIZE);
 	}
-
 	// Now output
 	memcpy(inout, hmac_buf, SHA1_SIZE);
+#else
+	pbkdf1((const unsigned char*)pw, pl, inout,dl, iterations, inout);
+#endif
 }
