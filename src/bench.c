openwall/john

commit 7ec88f81a3826006e667ff8b2972ec89d87612fa

Add a --stress-test[=TIME] option that is just like --test except it loops the test(s) forever.

src/bench.c
@@ -394,9 +394,10 @@ int benchmark_all(void)
 	}
 #endif
 
+AGAIN:
 	total = failed = 0;
 #ifdef DEBUG
-	if (!benchmark_time)
+	if (benchmark_time)
 	puts("NOTE: This is a debug build, figures might be lower than normal");
 #endif
 #ifndef BENCH_BUILD
@@ -604,6 +605,9 @@ next:
 	else if (total > 1 && !event_abort)
 		if (john_main_process)
 			printf("All %u formats passed self-tests!\n", total);
+
+	if (options.flags & FLG_LOOPTEST && !event_abort)
+		goto AGAIN;
 
 	return failed || event_abort;
 }

src/options.c
@@ -219,6 +219,9 @@ static struct opt_entry opt_list[] = {
 
 #endif
 	{"keep-guessing", FLG_KEEP_GUESSING, FLG_KEEP_GUESSING},
+	{"stress-test", FLG_LOOPTEST | FLG_TEST_SET, FLG_TEST_CHK,
+		0, ~FLG_TEST_SET & ~FLG_FORMAT & ~FLG_SAVEMEM & ~FLG_DYNFMT &
+		~OPT_REQ_PARAM & ~FLG_NOLOG, "%u", &benchmark_time},
 	{NULL}
 };
 
@@ -404,6 +407,7 @@ void opt_print_hidden_usage(void)
 	puts("--reject-printable        reject printable binaries");
 	puts("--verbosity=N             change verbosity (1-5, default 3)");
 	puts("--skip-self-tests         skip self tests");
+	puts("--stress-test[=TIME]      loop self tests forever");
 	puts("--input-encoding=NAME     input encoding (alias for --encoding)");
 	puts("--internal-encoding=NAME  encoding used in rules/masks (see doc/ENCODING)");
 	puts("--target-encoding=NAME    output encoding (used by format, see doc/ENCODING)");

src/options.h
@@ -152,6 +152,9 @@
 #define FLG_VERBOSITY			0x0000800000000000ULL
 /* Sets FMT_NOT_EXACT, searching for cleartext collisions */
 #define FLG_KEEP_GUESSING		0x0001000000000000ULL
+/* Loops self-test forever */
+#define FLG_LOOPTEST			0x0002000000000000ULL
+
 
 /*
  * Structure with option flags and all the parameters.

commit 8f45264b34f81f655040922411fd0d6550b0e4fe

Fix generic build (-f Makefile.legacy) breakage which was caused by commit 7ec88f81a3826006e667ff8b2972ec89d87612fa.

src/bench.c
@@ -394,7 +394,9 @@ int benchmark_all(void)
 	}
 #endif
 
+#ifndef BENCH_BUILD
 AGAIN:
+#endif
 	total = failed = 0;
 #ifdef DEBUG
 	if (benchmark_time)
@@ -606,8 +608,10 @@ next:
 		if (john_main_process)
 			printf("All %u formats passed self-tests!\n", total);
 
+#ifndef BENCH_BUILD
 	if (options.flags & FLG_LOOPTEST && !event_abort)
 		goto AGAIN;
+#endif
 
 	return failed || event_abort;
 }