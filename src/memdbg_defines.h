899f6531f77db0e5ef4034063ded7c20ebb33a20
magnum <john.magnum@hushmail.com>
2014-11-02 12:34:12 +0100

    Add a 'memdbg' build target which can be used without editing anything.

--- src/memdbg_defines.h
+++ src/memdbg_defines.h
@@ -4,14 +4,18 @@
 #undef MEMDBG_ON
 #undef MEMDBG_EXTRA_CHECKS
 
-/* comment out the next line, to FULLY turn off memory debugging from this module, or uncomment to turn debugging on. */
-/*#define MEMDBG_ON*/
+#if defined MEMDBG_BUILD
+/* turn debugging on. */
+#define MEMDBG_ON
 
-/* If this is uncommented (and MEMDBG_ON is also uncommented), then the memory checking will be much more through,
- * but memory will not be freed, and runtime will slow, possibly noticeably.  However, it is much more in-depth,
- * finding things like usage of freed pointers.  Some functions like
+/*
+ * If this is uncommented (and MEMDBG_ON is also uncommented), then the memory
+ * checking will be much more through, but memory will not be freed, and
+ * runtime will slow, possibly noticeably.  However, it is much more in-depth,
+ * finding things like usage of freed pointers.
  */
-/*#define MEMDBG_EXTRA_CHECKS*/
+#define MEMDBG_EXTRA_CHECKS
+#endif
 
 #if defined (JTR_RELEASE_BUILD)
 #undef MEMDBG_ON
