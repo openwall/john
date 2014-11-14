openwall/john

commit 01c44fdb27f3d8fd297ce29cced0e24a7ab0a351

Support SHA-1 hashes in BFD authentication

src/net_sha1_fmt_plug.c
@@ -38,14 +38,14 @@ john_register_one(&fmt_netsha1);
 #include "memdbg.h"
 
 #define FORMAT_LABEL            "net-sha1"
-#define FORMAT_NAME             "\"Keyed SHA1\" RIPv2, OSPF, BGP, SNMPv2"
+#define FORMAT_NAME             "\"Keyed SHA1\" BFD"
 #define FORMAT_TAG              "$netsha1$"
 #define TAG_LENGTH              (sizeof(FORMAT_TAG) - 1)
 #define ALGORITHM_NAME          "SHA1 32/" ARCH_BITS_STR
 #define BENCHMARK_COMMENT       ""
 #define BENCHMARK_LENGTH        0
 
-#define PLAINTEXT_LENGTH        16
+#define PLAINTEXT_LENGTH        20  // get this right ;)
 #define BINARY_SIZE             20
 #define BINARY_ALIGN            sizeof(ARCH_WORD_32)
 #define SALT_SIZE               sizeof(struct custom_salt)
@@ -55,10 +55,9 @@ john_register_one(&fmt_netsha1);
 #define HEXCHARS                "0123456789abcdef"
 
 static struct fmt_tests tests[] = {
-/* Mockup test vectors. Both these lines should be deleted once we get some
-   real test vectors */
-	{           "02020000ffff0003002c01145267d48d000000000000000000020000ac100100ffffff000000000000000001ffff0001$7236e720eaebe9eb3954c53db5cc19224a6cfeb3", "quagga"},
-	{FORMAT_TAG "02020000ffff0003002c01145267d48d000000000000000000020000ac100100ffffff000000000000000001ffff0001$71893ac93cb5511937d4a7189d390d14d9e013fd", "magnum"},
+	/* Real hashes from Cisco routers ;) */
+	{"$netsha1$20440a340000000100000000000f4240000f424000000000051c010000000001$709d3307304d790f58bf0a3cefd783b438408996", "password12345"},
+	{"$netsha1$20440a340000000100000000000f4240000f424000000000051c010000000002$94bce4d9084199508669b39f044064082a093de3", "password12345"},
 	{NULL}
 };
 
@@ -141,14 +140,13 @@ static void *get_salt(char *ciphertext)
 		cs.salt[i] = (atoi16[ARCH_INDEX(ciphertext[2 * i])] << 4) |
 			atoi16[ARCH_INDEX(ciphertext[2 * i + 1])];
 
-	if (len < 230) {
+	if (0 && len < 230) { // XXX disable "dynamic" since I can't get it to work!
 		// return our memset buffer (putting the dyna salt pointer into it).
 		// This keeps teh 'pre-cleaned salt() warning from hitting this format)
 		//return pDynamicFmt->methods.salt(Convert(Conv_Buf, orig_ct));
 		memcpy((char*)(&cs), pDynamicFmt->methods.salt(Convert(Conv_Buf, orig_ct)), pDynamicFmt->params.salt_size);
 		dyna_salt_seen=1;
 		return &cs;
-		
 	}
 	cs.magic = MAGIC;
 	cs.length = len;

run/bfd2john.py
@@ -38,13 +38,19 @@ def pcap_parser(fname):
                 continue
 
             authentication_type = ord(data[24])
+            salt = data[0:32].encode("hex")
 
-            if authentication_type == 2:  # Keyed MD5
+            if authentication_type == 2 or authentication_type == 3:  # Keyed MD5, Meticulous Keyed MD5
                 h = data[-16:].encode("hex")  # MD5 hash
-                salt = data[0:32].encode("hex")
+                # password needs to be padded to length 16 (password + ''.join(['\x00' * (16 - len(password))])),
+                # "netmd5" format automagically handles this ;)
                 sys.stdout.write("$netmd5$%s$%s\n" % (salt, h))
+            elif authentication_type == 4 or authentication_type == 5:  # Keyed SHA1, Meticulous Keyed SHA1
+                # http://tools.ietf.org/html/rfc5880
+                # password needs to be padded to length 20 (password + ''.join(['\x00' * (20 - len(password))])),
+                h = data[-20:].encode("hex")  # SHA1 hash
+                sys.stdout.write("$netsha1$%s$%s\n" % (salt, h))
             else:
-                # http://tools.ietf.org/html/rfc5880 (TODO)
                 assert 0
 
     f.close()

commit 7e4483098fe46544bc89a193b66793650ad87e65

Fixed broken dyna40 as required by 01c44fd

run/pass_gen.pl
@@ -2234,6 +2234,13 @@ sub pad16 { # used by pad16($p)  This will null pad a string to 16 bytes long
 	}
 	return $p;
 }
+sub pad20 { # used by pad20($p)  This will null pad a string to 20 bytes long
+	my $p = $_[0];
+	while (length($p) < 20) {
+		$p .= "\0";
+	}
+	return $p;
+}
 # used by pad_md64($p)  This will null pad a string to 64 bytes long, appends the 0x80 after current length, and puts length
 # 'bits' (i.e. length << 3) in proper place for md5 processing.  HSRP format uses this.
 sub pad_md64 {
@@ -2423,7 +2430,7 @@ sub dynamic_compile {
 			$dynamic_args==37 && do {$fmt='sha1($u.$p),usrname=lc';		last SWITCH; };
 			$dynamic_args==38 && do {$fmt='sha1($s.sha1($s.sha1($p))),saltlen=20';	last SWITCH; };
 			$dynamic_args==39 && do {$fmt='md5($s.pad16($p)),saltlen=60';	last SWITCH; };
-			$dynamic_args==40 && do {$fmt='sha1($s.pad16($p)),saltlen=60';	last SWITCH; };
+			$dynamic_args==40 && do {$fmt='sha1($s.pad20($p)),saltlen=60';	last SWITCH; };
 			$dynamic_args==50 && do {$fmt='sha224($p)';					last SWITCH; };
 			$dynamic_args==51 && do {$fmt='sha224($s.$p),saltlen=6';	last SWITCH; };
 			$dynamic_args==52 && do {$fmt='sha224($p.$s)';				last SWITCH; };
@@ -2644,6 +2651,7 @@ sub do_dynamic_GetToken {
 	if (substr($exprStr, 0,13) eq "ripemd320_raw") { push(@gen_toks, "frip320r"); return substr($exprStr,13); }
 	if (substr($exprStr, 0,12) eq "haval256_raw")  { push(@gen_toks, "fhavr"); return substr($exprStr,12); }
 	if (substr($exprStr, 0,5)  eq "pad16")         { push(@gen_toks, "fpad16"); return substr($exprStr,5); }
+	if (substr($exprStr, 0,5)  eq "pad20")         { push(@gen_toks, "fpad20"); return substr($exprStr,5); }
 	if (substr($exprStr, 0,7)  eq "padmd64")       { push(@gen_toks, "fpadmd64"); return substr($exprStr,7); }
 
 	$gen_lastTokIsFunc=1;
@@ -3218,4 +3226,5 @@ sub dynamic_fhave  { require Digest::Haval256; $h = pop @gen_Stack; $h = haval25
 sub dynamic_fhavu  { require Digest::Haval256; $h = pop @gen_Stack; $h = haval256_hex(encode("UTF-16LE",$h)); $gen_Stack[@gen_Stack-1] .= $h; return $h; }
 sub dynamic_fhavr  { require Digest::Haval256; $h = pop @gen_Stack; $h = haval256($h); $gen_Stack[@gen_Stack-1] .= $h; return $h; }
 sub dynamic_fpad16 { $h = pop @gen_Stack; $h = pad16($h); $gen_Stack[@gen_Stack-1] .= $h; return $h; }
+sub dynamic_fpad20 { $h = pop @gen_Stack; $h = pad20($h); $gen_Stack[@gen_Stack-1] .= $h; return $h; }
 sub dynamic_fpadmd64 { $h = pop @gen_Stack; $h = pad_md64($h); $gen_Stack[@gen_Stack-1] .= $h; return $h; }

src/dynamic.h
@@ -189,6 +189,7 @@ extern void DynamicFunc__clean_input_kwik(DYNA_OMP_PARAMS);
 extern void DynamicFunc__clean_input_full(DYNA_OMP_PARAMS);
 extern void DynamicFunc__append_keys(DYNA_OMP_PARAMS);
 extern void DynamicFunc__append_keys_pad16(DYNA_OMP_PARAMS);
+extern void DynamicFunc__append_keys_pad20(DYNA_OMP_PARAMS);
 extern void DynamicFunc__crypt_md5(DYNA_OMP_PARAMS);
 extern void DynamicFunc__crypt_md4(DYNA_OMP_PARAMS);
 extern void DynamicFunc__append_from_last_output_as_base16(DYNA_OMP_PARAMS);

src/dynamic_fmt.c
@@ -3542,6 +3542,48 @@ void DynamicFunc__append_keys_pad16(DYNA_OMP_PARAMS)
 		total_len_X86[j] += 16;
 	}
 }
+void DynamicFunc__append_keys_pad20(DYNA_OMP_PARAMS)
+{
+	unsigned j;
+	unsigned til;
+#ifdef _OPENMP
+	til = last;
+	j = first;
+#else
+	j = 0;
+	til = m_count;
+#endif
+#ifdef MMX_COEF
+	if (dynamic_use_sse==1) {
+		for (; j < til; ++j) {
+			unsigned idx = (j>>(MMX_COEF>>1));
+			unsigned idx_mod = j&(MMX_COEF-1);
+			unsigned bf_ptr = (total_len[idx] >> ((32/MMX_COEF)*idx_mod)) & 0xFF;
+			saved_key[j][saved_key_len[j]] = 0; // so strncpy 'works'
+			if (saved_key_len[j] < 20) {
+				char buf[21];
+				strncpy(buf, saved_key[j], 21);
+				total_len[idx] += (20 << ((32/MMX_COEF)*idx_mod));
+				__SSE_append_string_to_input(input_buf[idx].c,idx_mod,(unsigned char*)buf,20,bf_ptr,1);
+			} else {
+				total_len[idx] += (saved_key_len[j] << ((32/MMX_COEF)*idx_mod));
+				__SSE_append_string_to_input(input_buf[idx].c,idx_mod,(unsigned char*)saved_key[j],saved_key_len[j],bf_ptr,1);
+			}
+		}
+		return;
+	}
+#endif
+	for (; j < til; ++j) {
+		saved_key[j][saved_key_len[j]] = 0;  // so strncpy 'works'
+#if MD5_X2
+		if (j&1)
+			strncpy(&(input_buf_X86[j>>MD5_X2].x2.b2[total_len_X86[j]]), saved_key[j], 21);
+		else
+#endif
+		strncpy(&(input_buf_X86[j>>MD5_X2].x1.b[total_len_X86[j]]), saved_key[j], 21);
+		total_len_X86[j] += 20;
+	}
+}
 
 /**************************************************************
  * DYNAMIC primitive helper function

src/dynamic_parser.c
@@ -93,6 +93,7 @@ static Dynamic_Predicate_t Dynamic_Predicate[] =  {
 	{ "DynamicFunc__clean_input_full", DynamicFunc__clean_input_full },
 	{ "DynamicFunc__append_keys", DynamicFunc__append_keys },
 	{ "DynamicFunc__append_keys_pad16", DynamicFunc__append_keys_pad16 },
+	{ "DynamicFunc__append_keys_pad20", DynamicFunc__append_keys_pad20 },
 	{ "DynamicFunc__crypt", DynamicFunc__crypt_md5 },  // legacy name.  Now the function is explicit to md5, but we still handle deprecated format
 	{ "DynamicFunc__crypt_md5", DynamicFunc__crypt_md5 },
 	{ "DynamicFunc__crypt_md4", DynamicFunc__crypt_md4 },

src/dynamic_preloads.c
@@ -1206,15 +1206,21 @@ static DYNAMIC_primitive_funcp _Funcs_40[] =
 	//MGF_FLAT_BUFFERS
 	DynamicFunc__clean_input,
 	DynamicFunc__append_salt,
-	DynamicFunc__append_keys_pad16,
+	DynamicFunc__append_keys_pad20,
 	DynamicFunc__SHA1_crypt_input1_to_output1_FINAL,
 	NULL
 };
 static struct fmt_tests _Preloads_40[] =
 {
-	{"$dynamic_40$7236e720eaebe9eb3954c53db5cc19224a6cfeb3$HEX$02020000ffff0003002c01145267d48d000000000000000000020000ac100100ffffff000000000000000001ffff0001","quagga"},
-	{"$dynamic_40$71893ac93cb5511937d4a7189d390d14d9e013fd$HEX$02020000ffff0003002c01145267d48d000000000000000000020000ac100100ffffff000000000000000001ffff0001","magnum"},
-	{"$dynamic_40$0d9442df7d8e9cee94911365018900e230d200e6$12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890","aaaaaaaaaaaaaaaa"},
+		/* Real hashes from Cisco routers ;) */
+//	{"$netsha1$20440a340000000100000000000f4240000f424000000000051c010000000001$709d3307304d790f58bf0a3cefd783b438408996", "password12345"},
+//	{"$netsha1$20440a340000000100000000000f4240000f424000000000051c010000000002$94bce4d9084199508669b39f044064082a093de3", "password12345"},
+	{"$dynamic_40$94bce4d9084199508669b39f044064082a093de3$HEX$20440a340000000100000000000f4240000f424000000000051c010000000002","password12345"},
+	{"$dynamic_40$709d3307304d790f58bf0a3cefd783b438408996$HEX$20440a340000000100000000000f4240000f424000000000051c010000000001","password12345"},
+
+	//{"$dynamic_40$7236e720eaebe9eb3954c53db5cc19224a6cfeb3$HEX$02020000ffff0003002c01145267d48d000000000000000000020000ac100100ffffff000000000000000001ffff0001","quagga"},
+	//{"$dynamic_40$71893ac93cb5511937d4a7189d390d14d9e013fd$HEX$02020000ffff0003002c01145267d48d000000000000000000020000ac100100ffffff000000000000000001ffff0001","magnum"},
+	//{"$dynamic_40$0d9442df7d8e9cee94911365018900e230d200e6$12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890","aaaaaaaaaaaaaaaa"},
 	{NULL}
 };
 

src/net_sha1_fmt_plug.c
@@ -140,7 +140,7 @@ static void *get_salt(char *ciphertext)
 		cs.salt[i] = (atoi16[ARCH_INDEX(ciphertext[2 * i])] << 4) |
 			atoi16[ARCH_INDEX(ciphertext[2 * i + 1])];
 
-	if (0 && len < 230) { // XXX disable "dynamic" since I can't get it to work!
+	if (len < 230) {
 		// return our memset buffer (putting the dyna salt pointer into it).
 		// This keeps teh 'pre-cleaned salt() warning from hitting this format)
 		//return pDynamicFmt->methods.salt(Convert(Conv_Buf, orig_ct));