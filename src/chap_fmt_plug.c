openwall/john

commit f5366a2b55771be01bfb8cefda9feb3087208503

DEFAULT_ALIGN.  Worked on cleaning up some of these (issue #317)

src/chap_fmt_plug.c
@@ -44,6 +44,8 @@ static int omp_t = 1;
 #define BENCHMARK_LENGTH	-1
 #define PLAINTEXT_LENGTH	32
 #define BINARY_SIZE		16
+#define BINARY_ALIGN		1
+#define SALT_ALIGN			sizeof(int)
 #define SALT_SIZE		sizeof(struct custom_salt)
 #define MIN_KEYS_PER_CRYPT	1
 #define MAX_KEYS_PER_CRYPT	1
@@ -215,9 +217,9 @@ struct fmt_main fmt_chap = {
 		BENCHMARK_LENGTH,
 		PLAINTEXT_LENGTH,
 		BINARY_SIZE,
-		DEFAULT_ALIGN,
+		BINARY_ALIGN,
 		SALT_SIZE,
-		DEFAULT_ALIGN,
+		SALT_ALIGN,
 		MIN_KEYS_PER_CRYPT,
 		MAX_KEYS_PER_CRYPT,
 		FMT_CASE | FMT_8_BIT | FMT_OMP,

src/agilekeychain_fmt_plug.c
@@ -45,6 +45,8 @@ john_register_one(&fmt_agile_keychain);
 #define BENCHMARK_COMMENT	""
 #define BENCHMARK_LENGTH	-1
 #define BINARY_SIZE		0
+#define BINARY_ALIGN		1
+#define SALT_ALIGN		sizeof(int)
 #define PLAINTEXT_LENGTH	125
 #define SALT_SIZE		sizeof(struct custom_salt)
 #ifdef MMX_COEF
@@ -304,9 +306,9 @@ struct fmt_main fmt_agile_keychain = {
 		BENCHMARK_LENGTH,
 		PLAINTEXT_LENGTH,
 		BINARY_SIZE,
-		DEFAULT_ALIGN,
+		BINARY_ALIGN,
 		SALT_SIZE,
-		DEFAULT_ALIGN,
+		SALT_ALIGN,
 		MIN_KEYS_PER_CRYPT,
 		MAX_KEYS_PER_CRYPT,
 		FMT_CASE | FMT_8_BIT | FMT_OMP | FMT_NOT_EXACT,

src/androidfde_fmt_plug.c
@@ -71,6 +71,8 @@ static int omp_t = 1;
 #define PLAINTEXT_LENGTH    64
 #define BENCHMARK_LENGTH    -1
 #define BINARY_SIZE         0
+#define BINARY_ALIGN		1
+#define SALT_ALIGN			sizeof(int)
 #define SALT_SIZE           sizeof(struct custom_salt)
 #ifdef MMX_COEF
 #define MIN_KEYS_PER_CRYPT  SSE_GROUP_SZ_SHA1
@@ -330,9 +332,9 @@ struct fmt_main fmt_fde = {
 		BENCHMARK_LENGTH,
 		PLAINTEXT_LENGTH,
 		BINARY_SIZE,
-		DEFAULT_ALIGN,
+		BINARY_ALIGN,
 		SALT_SIZE,
-		DEFAULT_ALIGN,
+		SALT_ALIGN,
 		MIN_KEYS_PER_CRYPT,
 		MAX_KEYS_PER_CRYPT,
 		FMT_CASE | FMT_8_BIT | FMT_OMP,

src/bitcoin_fmt_plug.c
@@ -61,6 +61,8 @@ static int omp_t = 1;
 #define BENCHMARK_LENGTH	-1
 #define PLAINTEXT_LENGTH	64
 #define BINARY_SIZE		0
+#define BINARY_ALIGN		1
+#define SALT_ALIGN			sizeof(int)
 #define SALT_SIZE		sizeof(struct custom_salt)
 #ifdef MMX_COEF_SHA512
 #define MIN_KEYS_PER_CRYPT	MMX_COEF_SHA512
@@ -418,13 +420,9 @@ struct fmt_main fmt_bitcoin = {
 		BENCHMARK_LENGTH,
 		PLAINTEXT_LENGTH,
 		BINARY_SIZE,
-#if FMT_MAIN_VERSION > 9
-                DEFAULT_ALIGN,
-#endif
+		BINARY_ALIGN,
 		SALT_SIZE,
-#if FMT_MAIN_VERSION > 9
-                DEFAULT_ALIGN,
-#endif
+		SALT_ALIGN,
 		MIN_KEYS_PER_CRYPT,
 		MAX_KEYS_PER_CRYPT,
 		FMT_CASE | FMT_8_BIT | FMT_OMP,

src/cuda_cryptsha256_fmt_plug.c
@@ -42,6 +42,9 @@ john_register_one(&fmt_cuda_cryptsha256);
 
 #define SALT_SIZE		(3+7+9+16)
 
+#define BINARY_ALIGN		4
+#define SALT_ALIGN			sizeof(uint32_t)
+
 #define MIN_KEYS_PER_CRYPT	THREADS
 #define MAX_KEYS_PER_CRYPT	KEYS_PER_CRYPT
 
@@ -350,9 +353,9 @@ struct fmt_main fmt_cuda_cryptsha256 = {
 		BENCHMARK_LENGTH,
 		PLAINTEXT_LENGTH,
 		BINARY_SIZE,
-		DEFAULT_ALIGN,
+		BINARY_ALIGN,
 		SALT_SIZE,
-		DEFAULT_ALIGN,
+		SALT_ALIGN,
 		MIN_KEYS_PER_CRYPT,
 		MAX_KEYS_PER_CRYPT,
 		FMT_CASE | FMT_8_BIT,

src/cuda_cryptsha512_fmt_plug.c
@@ -34,6 +34,8 @@ john_register_one(&fmt_cuda_cryptsha512);
 #define BINARY_SIZE		(3+16+86)
 #define MD5_DIGEST_LENGTH 	16
 
+#define BINARY_ALIGN		8
+#define SALT_ALIGN			sizeof(uint32_t)
 
 #define SALT_SIZE		(3+7+9+16)
 
@@ -334,9 +336,9 @@ struct fmt_main fmt_cuda_cryptsha512 = {
 		BENCHMARK_LENGTH,
 		PLAINTEXT_LENGTH,
 		BINARY_SIZE,
-		DEFAULT_ALIGN,
+		BINARY_ALIGN,
 		SALT_SIZE,
-		DEFAULT_ALIGN,
+		SALT_ALIGN,
 		MIN_KEYS_PER_CRYPT,
 		MAX_KEYS_PER_CRYPT,
 		FMT_CASE | FMT_8_BIT,

src/cuda_mscash2_fmt_plug.c
@@ -30,6 +30,9 @@ john_register_one(&fmt_cuda_mscash2);
 #define ALGORITHM_NAME		"PBKDF2-SHA1 CUDA"
 #define MAX_SALT_LENGTH		19
 
+#define BINARY_ALIGN		sizeof(uint32_t)
+#define SALT_ALIGN			sizeof(uint32_t)
+
 #define BENCHMARK_COMMENT	""
 #define BENCHMARK_LENGTH	-1
 //#define _MSCASH2_DEBUG
@@ -260,9 +263,9 @@ struct fmt_main fmt_cuda_mscash2 = {
 		BENCHMARK_LENGTH,
 		PLAINTEXT_LENGTH,
 		BINARY_SIZE,
-		DEFAULT_ALIGN,
+		BINARY_ALIGN,
 		SALT_SIZE,
-		DEFAULT_ALIGN,
+		SALT_ALIGN,
 		MIN_KEYS_PER_CRYPT,
 		MAX_KEYS_PER_CRYPT,
 		FMT_CASE | FMT_8_BIT | FMT_SPLIT_UNIFIES_CASE | FMT_UNICODE | FMT_UTF8,

src/cuda_mscash_fmt_plug.c
@@ -31,6 +31,9 @@ john_register_one(&fmt_cuda_mscash);
 #define BENCHMARK_COMMENT	""
 #define BENCHMARK_LENGTH	0
 
+#define BINARY_ALIGN		sizeof(uint32_t)
+#define SALT_ALIGN			sizeof(uint32_t)
+
 static mscash_password *inbuffer;
 static mscash_hash *outbuffer;
 static mscash_salt currentsalt;
@@ -256,9 +259,9 @@ struct fmt_main fmt_cuda_mscash = {
 		BENCHMARK_LENGTH,
 		PLAINTEXT_LENGTH,
 		BINARY_SIZE,
-		DEFAULT_ALIGN,
+		BINARY_ALIGN,
 		SALT_SIZE,
-		DEFAULT_ALIGN,
+		SALT_ALIGN,
 		MIN_KEYS_PER_CRYPT,
 		MAX_KEYS_PER_CRYPT,
 		FMT_CASE | FMT_8_BIT | FMT_SPLIT_UNIFIES_CASE | FMT_UNICODE | FMT_UTF8,

commit 2603301665a16e029225d79b75b4f56b63a48685

fixed BE core caused by f5366a2.  NOTE all fmt_default_binary_hash_x requires this alignment

src/chap_fmt_plug.c
@@ -44,7 +44,7 @@ static int omp_t = 1;
 #define BENCHMARK_LENGTH	-1
 #define PLAINTEXT_LENGTH	32
 #define BINARY_SIZE		16
-#define BINARY_ALIGN		1
+#define BINARY_ALIGN		sizeof(ARCH_WORD_32)
 #define SALT_ALIGN			sizeof(int)
 #define SALT_SIZE		sizeof(struct custom_salt)
 #define MIN_KEYS_PER_CRYPT	1