371eface6fb92c1518d11b5fc421aa040cb88eae
Dhiru Kholia <dhiru@spotify.com>
2014-12-07 07:13:25 +0100

    Handle custom salt in ecryptfs2john.py [1/2]

--- run/ecryptfs2john.py
+++ run/ecryptfs2john.py
@@ -1,25 +1,50 @@
 #!/usr/bin/env python
 
+# Helper script for cracking eCryptfs.
+#
+# Refer to "ecryptfs-utils_104.orig.tar.gz" in case of doubt.
+#
+# This software is Copyright (c) 2014, DhiruKholia <dhiru.kholia at gmail.com>
+# and it is hereby released to the general public under the following terms:
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted.
+
 import sys
 
-def process_file(filename):
+
+def process(filename, ecryptfsrc):
+    salt = None
+    if ecryptfsrc:  # extract variable salt from this file
+        with open(ecryptfsrc, "r") as f:
+            for line in f:
+                if line.startswith("salt="):
+                    _, salt = line.strip().split("=")
+                    salt = salt[0:16]  # fixed size salt
+
     with open(filename, "r") as f:
         h = f.read(16)
         if len(h) != 16:
             return
 
-        # we don't use basename() because we need to distinguish clearly
-        # between different files all named wrapped-passphrase
-        sys.stdout.write("%s:$ecryptfs$0$%s\n" % \
-                (filename, h))
+    # we don't use basename() because we need to distinguish clearly
+    # between different files, which are all named "wrapped-passphrase"
+    if not salt:
+        sys.stdout.write("%s:$ecryptfs$0$%s\n" % (filename, h))
+    else:
+        sys.stdout.write("%s:$ecryptfs$0$%s$1$%s\n" % (filename, h, salt))  # $1$ indicates variable salt
+
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
-        sys.stderr.write("Usage: %s <wrapped-passphrase file(s)>\n" % \
-                sys.argv[0])
-        sys.stderr.write("\nExample: %s ~/.ecryptfs/wrapped-passphrase\n" % \
-                sys.argv[0])
+        sys.stderr.write("Usage: %s <wrapped-passphrase> [.ecryptfsrc]\n" % sys.argv[0])
+        sys.stderr.write("\nExample: %s ~/.ecryptfs/wrapped-passphrase" % sys.argv[0])
+        sys.stderr.write("\nExample: %s ~/.ecryptfs/wrapped-passphrase ~/.ecryptfsrc\n" % sys.argv[0])
         sys.exit(-1)
 
-    for i in range(1, len(sys.argv)):
-        process_file(sys.argv[i])
+    try:
+        ecryptfsrc = sys.argv[2]
+    except IndexError:
+        ecryptfsrc = None
+
+    process(sys.argv[1], ecryptfsrc)
