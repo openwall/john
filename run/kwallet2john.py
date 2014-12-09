c5c9ecc57a49d6610e6c90ca4440fe61d7cc9875
Dhiru Kholia <dhiru@spotify.com>
2014-12-09 07:39:59 +0100

    Clean up kwallet2john.py (PEP8 fixes + a nasty logic fix)

--- run/kwallet2john.py
+++ run/kwallet2john.py
@@ -11,13 +11,13 @@ import os
 import struct
 from binascii import hexlify
 
-KWMAGIC ="KWALLET\n\r\0\r\n"
+KWMAGIC = "KWALLET\n\r\0\r\n"
 KWMAGIC_LEN = 12
 KWALLET_VERSION_MAJOR = 0
 KWALLET_VERSION_MINOR = 0
 KWALLET_CIPHER_BLOWFISH_CBC = 0
 KWALLET_CIPHER_3DES_CBC = 1
-KWALLET_HASH_SHA1 =  0
+KWALLET_HASH_SHA1 = 0
 KWALLET_HASH_MD5 = 1
 N = 128
 
@@ -32,11 +32,11 @@ def process_file(filename):
         sys.stderr.write("%s\n" % str(e))
         return
 
-    # XXX TOCTOU
+    # TOCTOU but who cares, right? ;)
     size = os.stat(filename).st_size
     buf = fd.read(KWMAGIC_LEN)
 
-    if buf != KWMAGIC :
+    if buf != KWMAGIC:
         sys.stderr.write("%s : Not a KDE KWallet file!\n" % filename)
         return
     offset += KWMAGIC_LEN
@@ -45,40 +45,34 @@ def process_file(filename):
 
     # First byte is major version, second byte is minor version
     if buf[0] != KWALLET_VERSION_MAJOR:
-       sys.stderr.write("%s : Unknown version!\n" % filename)
-       return
+        sys.stderr.write("%s : Unknown version!\n" % filename)
+        return
     if buf[1] != KWALLET_VERSION_MINOR:
-       sys.stderr.write("%s : Unknown version!\n" % filename)
-       return
+        sys.stderr.write("%s : Unknown version!\n" % filename)
+        return
     if buf[2] != KWALLET_CIPHER_BLOWFISH_CBC:
-       sys.stderr.write("%s : Unsupported cipher\n" % filename)
-       return
+        sys.stderr.write("%s : Unsupported cipher\n" % filename)
+        return
     if buf[3] != KWALLET_HASH_SHA1:
-       sys.stderr.write("%s : Unsupported hash\n" % filename)
-       return
+        sys.stderr.write("%s : Unsupported hash\n" % filename)
+        return
 
     # Read in the hashes
-    buf= fd.read(4)
+    buf = fd.read(4)
     n = struct.unpack("> I", buf)[0]
-
-    # Use "print" when in doubt!
     if n > 0xffff:
         sys.stderr.write("%s : sanity check failed!\n" % filename)
         sys.exit(6)
-
     offset += 4
-
-    for i in range (0, n):
+    for i in range(0, n):
         buf = fd.read(16)
-        count = len(buf)
         offset += 16
-        # read 4 bytes more
-        buf = fd.read(4)
+        buf = fd.read(4)  # read 4 bytes more
         fsz = struct.unpack("> I", buf)[0]
         offset += 4
-        for j in range (0, fsz):
-            count = len(fd.read(16))
-        offset += 16
+        for j in range(0, fsz):
+            fd.read(16)
+            offset += 16
 
     # Read in the rest of the file
     encrypted_size = size - offset
@@ -86,11 +80,10 @@ def process_file(filename):
     encrypted_size = len(encrypted)
 
     if encrypted_size % 8 != 0:
-        sys.stderr.write(stderr, "%s : invalid file structure!\n", filename)
+        sys.stderr.write("%s : invalid file structure!\n", filename)
         sys.exit(7)
 
-    sys.stdout.write("%s:$kwallet$%ld$%s" % (os.path.basename(filename),
-        encrypted_size, hexlify(encrypted)))
+    sys.stdout.write("%s:$kwallet$%ld$%s" % (os.path.basename(filename), encrypted_size, hexlify(encrypted)))
 
     sys.stdout.write(":::::%s\n" % filename)
 
