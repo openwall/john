openwall/john

commit 229fb7623b877016e9ff53f4788b66b8ce6ce32f

django: fix #893

src/django_fmt_plug.c
@@ -141,11 +141,11 @@ static int valid(char *ciphertext, struct fmt_main *self)
 		goto err;
 	if ((p = strtok(NULL, "$")) == NULL)	/* salt */
 		goto err;
-	if (base64_valid_length(p,e_b64_mime,flg_Base64_MIME_TRAIL_EQ) > SALT_SIZE)
+	if (strlen(p)  > SALT_SIZE)
 		goto err;
 	if ((p = strtok(NULL, "")) == NULL)	/* hash */
 		goto err;
-	if (base64_valid_length(p,e_b64_mime,flg_Base64_MIME_TRAIL_EQ) > HASH_LENGTH-1)  {
+	if (strlen(p) != base64_valid_length(p,e_b64_mime,flg_Base64_MIME_TRAIL_EQ) || strlen(p) > HASH_LENGTH-1)  {
 		goto err;
 	}
 	MEM_FREE(keeptr);

commit fbee8431598dad39b0a925b0a0878b672bef3aa5

django:  #229fb76 caused validation problems

src/django_fmt_plug.c
@@ -145,7 +145,7 @@ static int valid(char *ciphertext, struct fmt_main *self)
 		goto err;
 	if ((p = strtok(NULL, "")) == NULL)	/* hash */
 		goto err;
-	if (strlen(p) != base64_valid_length(p,e_b64_mime,flg_Base64_MIME_TRAIL_EQ) || strlen(p) > HASH_LENGTH-1)  {
+	if (strlen(p)-1 != base64_valid_length(p,e_b64_mime,flg_Base64_MIME_TRAIL_EQ) || strlen(p)-1 > HASH_LENGTH-1)  {
 		goto err;
 	}
 	MEM_FREE(keeptr);