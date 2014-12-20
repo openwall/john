648fb1a6449384b6934b708cfd7af3a242a94c46
jfoug <jfoug@cox.net>
2014-12-20 11:40:03 -0600

    sapH: added cost and did some small speedups reusing buffers better.

--- src/sapH_fmt_plug.c
+++ src/sapH_fmt_plug.c
@@ -3,6 +3,18 @@
  * Copyright (c) 2014 JimF, and it is hereby released
  * to the general public under the following terms:  Redistribution and use in
  * source and binary forms, with or without modification, are permitted.
+ *
+ * The internals of this algorithm were found on the HashCat forum, and
+ * implemented here, whether, it is right or wrong. A link to that post is:
+ * http://hashcat.net/forum/thread-3804.html
+ * There are some things which are unclear, BUT which have been coded as listed
+ * within that post. Things such as the signatures themselves are somewhat
+ * unclear, and do not follow patterns well. The sha1 signature is lower case
+ * and does not contain the 1. The other signatures are upper case. This code
+ * was implemented in the exact manner as described on the forum, and will be
+ * used as such, until we find out that it is right or wrong (i.e. we get sample
+ * hashs from a REAL system in the other formats). If things are not correct,
+ * getting this format corrected will be trivial.
  */
 
 #if FMT_EXTERNS_H
@@ -49,13 +61,14 @@ john_register_one(&fmt_sapH);
 #include "sse-intrinsics.h"
 
 #define FORMAT_LABEL            "saph"
-#define FORMAT_NAME             "SAP CODVN H (HCODE)"
+#define FORMAT_NAME             "SAP CODVN H (PWDSALTEDHASH)"
 
-#define ALGORITHM_NAME          "SHA1/SHA256/SHA384/SHA512"
+//#define ALGORITHM_NAME          "SHA-1/256/384/512 " SHA1_ALGORITHM_NAME
+#define ALGORITHM_NAME          "SHA-1/256/384/512"
 
 #include "memdbg.h"
 
-#define BENCHMARK_COMMENT		""
+#define BENCHMARK_COMMENT		" (sha1, 1024)"
 #define BENCHMARK_LENGTH		0
 
 #define SALT_LENGTH             16  /* the max used sized salt */
@@ -69,7 +82,7 @@ john_register_one(&fmt_sapH);
 #define SHA384_BINARY_SIZE      48
 #define SHA512_BINARY_SIZE      64
 #define BINARY_ALIGN            4
-#define SALT_SIZE               sizeof(struct saltstruct)
+#define SALT_SIZE               sizeof(struct sapH_salt)
 #define SALT_ALIGN              4
 
 #ifdef MMX_COEF
@@ -82,9 +95,14 @@ john_register_one(&fmt_sapH);
 #endif
 
 static struct fmt_tests tests[] = {
+	/* first 2 hashes are 'default' 1024 iteration with 12 bytes salt so */
+	/* timings reflect that, and benchmark comment set to (sha1, 1024) */
+	{"{x-issha, 1024}hmiyJ2a/Z+HRpjQ37Osz+rYax9UxMjM0NTY3ODkwYWI=","OpenWall"},
+	{"{x-issha, 1024}fRLe9EvN/Le81BDEDZR5SEC0O6BhYmNkZWZnaHVrYWw=","JohnTheRipper"},
 	{"{x-issha, 1024}L1PHSP1vOwdYh0ASjswI69fQQQhzQXFlWmxnaFA5","booboo"},
 	{"{x-issha, 1024}dCjaHQ47/WeSwsoSYDR/8puLby5T","booboo"},	/* 1 byte salt */
 	{"{x-issha, 1024}+q+WSxWXJt7SjV5VJEymEKPUbn1FQWM=","HYulafeE!3"},
+	{"{x-issha, 6666}7qNFlIR+ZQUpe2DtSBvpvzU5VlBzcG1DVGxvOEFQODI=","dif_iterations"},
 
 	{"{x-isSHA256, 3000}UqMnsr5BYN+uornWC7yhGa/Wj0u5tshX19mDUQSlgih6OTFoZjRpMQ==","booboo"},
 	{"{x-isSHA256, 3000}ydi0JlyU6lX5305Qk/Q3uLBbIFjWuTyGo3tPBZDcGFd6NkFvV1gza3RkNg==","GottaGoWhereNeeded"},
@@ -102,12 +120,12 @@ static struct fmt_tests tests[] = {
 static char (*saved_plain)[PLAINTEXT_LENGTH + 1];
 static ARCH_WORD_32 (*crypt_key)[BINARY_SIZE/sizeof(ARCH_WORD_32)];
 
-static struct saltstruct {
+static struct sapH_salt {
 	int slen;	/* actual length of salt ( 1 to 16 bytes) */
 	int type;	/* 1, 256, 384 or 512 for sha1, sha256, sha384 or sha512 */
 	unsigned iter;   /* from 1 to 2^32 rounds */
 	unsigned char s[SALT_LENGTH];
-} *cur_salt;
+} *sapH_cur_salt;
 
 static void init(struct fmt_main *self)
 {
@@ -177,7 +195,7 @@ err:
 
 static void set_salt(void *salt)
 {
-	cur_salt = (struct saltstruct*)salt;
+	sapH_cur_salt = (struct sapH_salt*)salt;
 }
 
 static void set_key(char *key, int index)
@@ -215,19 +233,20 @@ static void crypt_all_1(int count) {
 #endif
 	for (idx = 0; idx < count; ++idx) {
 		int i, len = strlen(saved_plain[idx]);
-		unsigned char tmp[SHA1_BINARY_SIZE];
+		unsigned char tmp[PLAINTEXT_LENGTH+SHA1_BINARY_SIZE], *cp=&tmp[len];
 		SHA_CTX ctx;
 		SHA1_Init(&ctx);
 		SHA1_Update(&ctx, saved_plain[idx], len);
-		SHA1_Update(&ctx, cur_salt->s, cur_salt->slen);
-		SHA1_Final(tmp, &ctx);
-		for (i = 1; i < cur_salt->iter; ++i) {
+		SHA1_Update(&ctx, sapH_cur_salt->s, sapH_cur_salt->slen);
+		strcpy((char*)tmp, saved_plain[idx]);
+		len += SHA1_BINARY_SIZE;
+		SHA1_Final(cp, &ctx);
+		for (i = 1; i < sapH_cur_salt->iter; ++i) {
 			SHA1_Init(&ctx);
-			SHA1_Update(&ctx, saved_plain[idx], len);
-			SHA1_Update(&ctx, tmp, SHA1_BINARY_SIZE);
-			SHA1_Final(tmp, &ctx);
+			SHA1_Update(&ctx, tmp, len);
+			SHA1_Final(cp, &ctx);
 		}
-		memcpy(crypt_key[idx], tmp, BINARY_SIZE);
+		memcpy(crypt_key[idx], cp, BINARY_SIZE);
 	}
 }
 static void crypt_all_256(int count) {
@@ -237,19 +256,20 @@ static void crypt_all_256(int count) {
 #endif
 	for (idx = 0; idx < count; ++idx) {
 		int i, len = strlen(saved_plain[idx]);
-		unsigned char tmp[SHA256_BINARY_SIZE];
+		unsigned char tmp[PLAINTEXT_LENGTH+SHA256_BINARY_SIZE], *cp=&tmp[len];
 		SHA256_CTX ctx;
 		SHA256_Init(&ctx);
 		SHA256_Update(&ctx, saved_plain[idx], len);
-		SHA256_Update(&ctx, cur_salt->s, cur_salt->slen);
-		SHA256_Final(tmp, &ctx);
-		for (i = 1; i < cur_salt->iter; ++i) {
+		SHA256_Update(&ctx, sapH_cur_salt->s, sapH_cur_salt->slen);
+		strcpy((char*)tmp, saved_plain[idx]);
+		len += SHA256_BINARY_SIZE;
+		SHA256_Final(cp, &ctx);
+		for (i = 1; i < sapH_cur_salt->iter; ++i) {
 			SHA256_Init(&ctx);
-			SHA256_Update(&ctx, saved_plain[idx], len);
-			SHA256_Update(&ctx, tmp, SHA256_BINARY_SIZE);
-			SHA256_Final(tmp, &ctx);
+			SHA256_Update(&ctx, tmp, len);
+			SHA256_Final(cp, &ctx);
 		}
-		memcpy(crypt_key[idx], tmp, BINARY_SIZE);
+		memcpy(crypt_key[idx], cp, BINARY_SIZE);
 	}
 }
 static void crypt_all_384(int count) {
@@ -259,19 +279,20 @@ static void crypt_all_384(int count) {
 #endif
 	for (idx = 0; idx < count; ++idx) {
 		int i, len = strlen(saved_plain[idx]);
-		unsigned char tmp[SHA384_BINARY_SIZE];
+		unsigned char tmp[PLAINTEXT_LENGTH+SHA384_BINARY_SIZE], *cp=&tmp[len];
 		SHA512_CTX ctx;
 		SHA384_Init(&ctx);
 		SHA384_Update(&ctx, saved_plain[idx], len);
-		SHA384_Update(&ctx, cur_salt->s, cur_salt->slen);
-		SHA384_Final(tmp, &ctx);
-		for (i = 1; i < cur_salt->iter; ++i) {
+		SHA384_Update(&ctx, sapH_cur_salt->s, sapH_cur_salt->slen);
+		strcpy((char*)tmp, saved_plain[idx]);
+		len += SHA384_BINARY_SIZE;
+		SHA384_Final(cp, &ctx);
+		for (i = 1; i < sapH_cur_salt->iter; ++i) {
 			SHA384_Init(&ctx);
-			SHA384_Update(&ctx, saved_plain[idx], len);
-			SHA384_Update(&ctx, tmp, SHA384_BINARY_SIZE);
-			SHA384_Final(tmp, &ctx);
+			SHA384_Update(&ctx, tmp, len);
+			SHA384_Final(cp, &ctx);
 		}
-		memcpy(crypt_key[idx], tmp, BINARY_SIZE);
+		memcpy(crypt_key[idx], cp, BINARY_SIZE);
 	}
 }
 static void crypt_all_512(int count) {
@@ -281,19 +302,20 @@ static void crypt_all_512(int count) {
 #endif
 	for (idx = 0; idx < count; ++idx) {
 		int i, len = strlen(saved_plain[idx]);
-		unsigned char tmp[SHA512_BINARY_SIZE];
+		unsigned char tmp[PLAINTEXT_LENGTH+SHA512_BINARY_SIZE], *cp=&tmp[len];
 		SHA512_CTX ctx;
 		SHA512_Init(&ctx);
 		SHA512_Update(&ctx, saved_plain[idx], len);
-		SHA512_Update(&ctx, cur_salt->s, cur_salt->slen);
-		SHA512_Final(tmp, &ctx);
-		for (i = 1; i < cur_salt->iter; ++i) {
+		SHA512_Update(&ctx, sapH_cur_salt->s, sapH_cur_salt->slen);
+		strcpy((char*)tmp, saved_plain[idx]);
+		len += SHA512_BINARY_SIZE;
+		SHA512_Final(cp, &ctx);
+		for (i = 1; i < sapH_cur_salt->iter; ++i) {
 			SHA512_Init(&ctx);
-			SHA512_Update(&ctx, saved_plain[idx], len);
-			SHA512_Update(&ctx, tmp, SHA512_BINARY_SIZE);
-			SHA512_Final(tmp, &ctx);
+			SHA512_Update(&ctx, tmp, len);
+			SHA512_Final(cp, &ctx);
 		}
-		memcpy(crypt_key[idx], tmp, BINARY_SIZE);
+		memcpy(crypt_key[idx], cp, BINARY_SIZE);
 	}
 }
 
@@ -303,11 +325,11 @@ static int crypt_all(int *pcount, struct db_salt *salt)
 	 * split logic into 4 separate functions, to make the logic more
 	 * simplistic, when we start adding OMP + SIMD code
 	 */
-	switch(cur_salt->type) {
+	switch(sapH_cur_salt->type) {
 		case 1: crypt_all_1(*pcount); break;
-		case 256: crypt_all_256(*pcount); break;
-		case 384: crypt_all_384(*pcount); break;
-		case 512: crypt_all_512(*pcount); break;
+		case 2: crypt_all_256(*pcount); break;
+		case 3: crypt_all_384(*pcount); break;
+		case 4: crypt_all_512(*pcount); break;
 	}
 	return *pcount;
 }
@@ -338,7 +360,7 @@ static void *binary(char *ciphertext)
 
 static void *get_salt(char *ciphertext)
 {
-	static struct saltstruct s;
+	static struct sapH_salt s;
 	char *cp = ciphertext;
 	unsigned char tmp[MAX_BINARY_SIZE+SALT_LENGTH+4];
 	int total_len, hash_len;
@@ -346,9 +368,9 @@ static void *get_salt(char *ciphertext)
 	memset(&s, 0, sizeof(s));
 	cp += 5; /* skip the {x-is */
 	if (!strncasecmp(cp, "sha, ", 5)) { s.type = 1; cp += 5; hash_len = SHA1_BINARY_SIZE; }
-	else if (!strncasecmp(cp, "sha256, ", 8)) { s.type = 256; cp += 8; hash_len = SHA256_BINARY_SIZE; }
-	else if (!strncasecmp(cp, "sha384, ", 8)) { s.type = 384; cp += 8; hash_len = SHA384_BINARY_SIZE; }
-	else if (!strncasecmp(cp, "sha512, ", 8)) { s.type = 512; cp += 8; hash_len = SHA512_BINARY_SIZE; }
+	else if (!strncasecmp(cp, "sha256, ", 8)) { s.type = 2; cp += 8; hash_len = SHA256_BINARY_SIZE; }
+	else if (!strncasecmp(cp, "sha384, ", 8)) { s.type = 3; cp += 8; hash_len = SHA384_BINARY_SIZE; }
+	else if (!strncasecmp(cp, "sha512, ", 8)) { s.type = 4; cp += 8; hash_len = SHA512_BINARY_SIZE; }
 	else { fprintf(stderr, "error, bad signature in sap-H format!\n"); exit(-1); }
 	sscanf (cp, "%u", &s.iter);
 	while (*cp != '}') ++cp;
@@ -380,12 +402,28 @@ static int salt_hash(void *salt)
 	unsigned int hash = 5381;
 	unsigned int i;
 
-	for (i = 0; i < sizeof(struct saltstruct); i++)
+	for (i = 0; i < sizeof(struct sapH_salt); i++)
 		hash = ((hash << 5) + hash) ^ cp[i];
 
 	return hash & (SALT_HASH_SIZE - 1);
 }
 
+#if FMT_MAIN_VERSION > 11
+static unsigned int sapH_type(void *salt)
+{
+	struct sapH_salt *my_salt;
+
+	my_salt = (struct sapH_salt *)salt;
+	return my_salt->type;
+}
+static unsigned int iteration_count(void *salt)
+{
+	struct sapH_salt *my_salt;
+
+	my_salt = (struct sapH_salt *)salt;
+	return my_salt->iter;
+}
+#endif
 struct fmt_main fmt_sapH = {
 	{
 		FORMAT_LABEL,
@@ -402,7 +440,10 @@ struct fmt_main fmt_sapH = {
 		MAX_KEYS_PER_CRYPT,
 		FMT_OMP | FMT_8_BIT,
 #if FMT_MAIN_VERSION > 11
-		{ NULL },
+		{
+			"hash type [1:sha1 2:SHA256 3:SHA384 4:SHA512]",
+			"iteration count",
+		},
 #endif
 		tests
 	}, {
@@ -415,7 +456,10 @@ struct fmt_main fmt_sapH = {
 		binary,
 		get_salt,
 #if FMT_MAIN_VERSION > 11
-		{ NULL },
+		{
+			sapH_type,
+			iteration_count,
+		},
 #endif
 		fmt_default_source,
 		{
