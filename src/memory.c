149ee9b0f82e89c19a3f6b9d511f544961b1bb1b
magnum <john.magnum@hushmail.com>
2014-12-23 02:33:08 +0100

    Fix indentation and widescreen comments in memory.c

--- src/memory.c
+++ src/memory.c
@@ -216,12 +216,14 @@ char *str_alloc_copy_func(char *src
 #endif
 }
 
+/* if we stipulate that align must be 1<<x (i.e. base 2), %align can be
+   replaced with &(align-1) */
 void *mem_align(void *stack_ptr, int align) {
-    char *cp_align = (char*)stack_ptr;
+	char *cp_align = (char*)stack_ptr;
 
-    cp_align += (align-1);
-    cp_align -= (size_t)cp_align % align; // if we stipulate that align must be 1<<x (i.e. base 2), %align can be replaced with &(align-1)
-    return (void*)cp_align;
+	cp_align += (align-1);
+	cp_align -= (size_t)cp_align % align;
+	return (void*)cp_align;
 }
 
 void dump_text(void *in, int len)
