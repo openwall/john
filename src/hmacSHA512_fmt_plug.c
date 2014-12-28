a67718deebdbe9f835026028d74aac1df6324c8e
jfoug <jfoug@cox.net>
2014-12-28 11:29:35 -0600

    hmacSHA2: OMP scaling

--- src/hmacSHA512_fmt_plug.c
+++ src/hmacSHA512_fmt_plug.c
@@ -21,7 +21,7 @@ john_register_one(&fmt_hmacSHA512);
 #ifdef _OPENMP
 static int omp_t = 1;
 #include <omp.h>
-#define OMP_SCALE               64
+#define OMP_SCALE               512 // scaled K8-dual HT
 #endif
 #include "memdbg.h"
 
