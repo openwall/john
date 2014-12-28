2221cbbc7e5cacc1d5f25c5ba709869e519680fb
jfoug <jfoug@cox.net>
2014-12-28 12:55:58 -0600

    mysqlna: OMP scaling

--- src/mysql_netauth_fmt_plug.c
+++ src/mysql_netauth_fmt_plug.c
@@ -25,7 +25,7 @@ john_register_one(&fmt_mysqlna);
 #ifdef _OPENMP
 static int omp_t = 1;
 #include <omp.h>
-#define OMP_SCALE               64
+#define OMP_SCALE               4096 // tuned K8-dual HT
 #endif
 #include "memdbg.h"
 
