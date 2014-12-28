352419650cee505363e29e54aa66cd7aaf22d88c
jfoug <jfoug@cox.net>
2014-12-28 13:29:27 -0600

    postgres: OMP scaling

--- src/postgres_fmt_plug.c
+++ src/postgres_fmt_plug.c
@@ -24,7 +24,7 @@ john_register_one(&fmt_postgres);
 #ifdef _OPENMP
 static int omp_t = 1;
 #include <omp.h>
-#define OMP_SCALE               64
+#define OMP_SCALE               8192 // scaled on K8-dual HT
 #endif
 
 #include "md5.h"
