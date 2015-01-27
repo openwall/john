50262affd1a7b4e2fc0a58a3fee6e74fefac5304
jfoug <jfoug@cox.net>
2015-01-27 11:50:15 -0600

    scrypt: corrected pointer alias issues causing scrypt to fail on MIPS64.  #1032

--- src/escrypt/crypto_scrypt.h
+++ src/escrypt/crypto_scrypt.h
@@ -51,6 +51,11 @@ typedef struct {
 	size_t size;
 } escrypt_region_t;
 
+typedef union {
+	uint64_t d[8];
+	uint32_t w[16];
+} escrypt_block_t;
+
 typedef escrypt_region_t escrypt_local_t;
 
 extern int escrypt_init_local(escrypt_local_t * __local);
