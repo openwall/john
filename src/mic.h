933d59965831bedba9f909af280217bee6edf374
Solar Designer <solar@openwall.com>
2015-03-05 16:24:03 +0000

    Added linux-mic make target for Intel MIC (first generation Xeon Phi, aka Knights Corner), along with use of 512-bit SIMD intrinsics for bitslice DES.

---
+++ src/mic.h
@@ -0,0 +1,51 @@
+/*
+ * This file is part of John the Ripper password cracker,
+ * Copyright (c) 2013 by Solar Designer
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted.
+ *
+ * There's ABSOLUTELY NO WARRANTY, express or implied.
+ */
+
+/*
+ * Architecture specific parameters for Intel MIC.
+ */
+
+#ifndef _JOHN_ARCH_H
+#define _JOHN_ARCH_H
+
+#define ARCH_WORD			long
+#define ARCH_SIZE			8
+#define ARCH_BITS			64
+#define ARCH_BITS_LOG			6
+#define ARCH_BITS_STR			"64"
+#define ARCH_LITTLE_ENDIAN		1
+#define ARCH_INT_GT_32			0
+#define ARCH_ALLOWS_UNALIGNED		0
+#define ARCH_INDEX(x)			((unsigned int)(unsigned char)(x))
+
+#define CPU_DETECT			0
+
+#define DES_ASM				0
+#define DES_128K			0
+#define DES_X2				0
+#define DES_MASK			1
+#define DES_SCALE			1
+#define DES_EXTB			1
+#define DES_COPY			0
+#define DES_BS_ASM			0
+#define DES_BS				1
+#define DES_BS_VECTOR			8
+#define DES_BS_ALGORITHM_NAME		"512/512"
+#define DES_BS_EXPAND			1
+
+#define MD5_ASM				0
+#define MD5_X2				1
+#define MD5_IMM				1
+
+#define BF_ASM				0
+#define BF_SCALE			1
+#define BF_X2				1
+
+#endif
