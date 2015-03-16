788a9e65bd22932b63932b58469b686f847cb1fc
jfoug <jfoug@cox.net>
2015-03-16 17:01:56 -0500

    dynamic: redone, to use the aligment versions in memory.c, prior 2 patches were incomplete

--- src/dynamic_fmt.c
+++ src/dynamic_fmt.c
@@ -721,9 +721,9 @@ static void init(struct fmt_main *pFmt)
 #endif
 #ifdef SIMD_COEF_32
 	input_buf = mem_calloc_align(1,MMX_INP_BUF_SZ, MEM_ALIGN_SIMD, &SIMD_ptr1);
-	input_buf2 = mem_calloc(1,MMX_INP_BUF2_SZ, MEM_ALIGN_SIMD, &SIMD_ptr1);
-	crypt_key = mem_calloc(1,MMX_CRYPT_KEY_SZ, MEM_ALIGN_SIMD, &SIMD_ptr1);
-	crypt_key2 = mem_calloc(1,MMX_CRYPT_KEY2_SZ, MEM_ALIGN_SIMD, &SIMD_ptr1);
+	input_buf2 = mem_calloc_align(1,MMX_INP_BUF2_SZ, MEM_ALIGN_SIMD, &SIMD_ptr1);
+	crypt_key = mem_calloc_align(1,MMX_CRYPT_KEY_SZ, MEM_ALIGN_SIMD, &SIMD_ptr1);
+	crypt_key2 = mem_calloc_align(1,MMX_CRYPT_KEY2_SZ, MEM_ALIGN_SIMD, &SIMD_ptr1);
 	total_len  = mem_calloc(1, MMX_TOT_LEN_SZ);
 	total_len2 = mem_calloc(1, MMX_TOT_LEN2_SZ);
 #endif
