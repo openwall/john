openwall/john

commit 70f3b95aa74199a74b97bc8e2c10351267e410ae

strip:  Fix to work with strtokm

src/strip_fmt_plug.c
@@ -100,7 +100,7 @@ static int valid(char *ciphertext, struct fmt_main *self)
 		return 0;
 	ctcopy = strdup(ciphertext);
 	keeptr = ctcopy;
-	ctcopy += 7;
+	ctcopy += 7+1;	/* skip over "$strip$" and first '*' */
 	if ((p = strtok(ctcopy, "*")) == NULL)	/* salt + data */
 		goto err;
 	if (strlen(p) != 2048)
@@ -123,7 +123,7 @@ static void *get_salt(char *ciphertext)
 	char *p;
 	int i;
 	static struct custom_salt cs;
-	ctcopy += 7;	/* skip over "$strip$" */
+	ctcopy += 7+1;	/* skip over "$strip$" and first '*' */
 	p = strtok(ctcopy, "*");
 	for (i = 0; i < 16; i++)
 			cs.salt[i] = atoi16[ARCH_INDEX(p[i * 2])] * 16

commit d29864029fb121fce04a4514ed49e35fb83c482e

Strip format, fix an ASan regression from 70f3b95a (I guess)

src/strip_fmt_plug.c
@@ -96,7 +96,7 @@ static int valid(char *ciphertext, struct fmt_main *self)
 	char *ctcopy;
 	char *keeptr;
 	char *p;
-	if (strncmp(ciphertext, "$strip$", 7))
+	if (strncmp(ciphertext, "$strip$*", 8))
 		return 0;
 	ctcopy = strdup(ciphertext);
 	keeptr = ctcopy;