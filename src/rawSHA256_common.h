openwall/john

commit b9e13c3889a6ed385cb57c9c84530dfcbd248d35

Mask mode implementation in raw-sha256-opencl.

src/rawSHA256_common.h
@@ -62,8 +62,10 @@ static int valid(char *ciphertext, struct fmt_main *self)
 /* Convert Cisco hashes to hex ones, so .pot entries are compatible */
 static char *prepare(char *split_fields[10], struct fmt_main *self)
 {
-	static char out[HEX_TAG_LEN + HEX_CIPHERTEXT_LENGTH + 1];
+	static char * out;
 	char *o, *p = split_fields[1];
+
+	if (!out) out = mem_alloc_tiny(HEX_TAG_LEN + HEX_CIPHERTEXT_LENGTH + 1, MEM_ALIGN_WORD);
 
 	if (!valid_cisco(p))
 		return p;
@@ -108,7 +110,9 @@ static char *prepare(char *split_fields[10], struct fmt_main *self)
 /* ------- Split ------- */
 static char *split(char *ciphertext, int index, struct fmt_main *self)
 {
-	static char out[HEX_TAG_LEN + HEX_CIPHERTEXT_LENGTH + 1];
+	static char * out;
+
+	if (!out) out = mem_alloc_tiny(HEX_TAG_LEN + HEX_CIPHERTEXT_LENGTH + 1, MEM_ALIGN_WORD);
 
 	if (!strncmp(ciphertext, HEX_TAG, HEX_TAG_LEN))
 		ciphertext += HEX_TAG_LEN;

src/common-gpu.c
@@ -7,7 +7,7 @@
  * Copyright (c) 2010-2012 Samuele Giovanni Tonon <samu at linuxasylum dot net>
  * Copyright (c) 2010-2013 Lukas Odzioba <ukasz@openwall.net>
  * Copyright (c) 2010-2013 magnum
- * Copyright (c) 2012-2013 Claudio André <claudioandre.br at gmail.com>
+ * Copyright (c) 2012-2015 Claudio André <claudioandre.br at gmail.com>
  * and is hereby released to the general public under the following terms:
  *    Redistribution and use in source and binary forms, with or without
  *    modifications, are permitted.

src/common-opencl.c
@@ -7,7 +7,7 @@
  * Copyright (c) 2010-2012 Samuele Giovanni Tonon <samu at linuxasylum dot net>
  * Copyright (c) 2010-2013 Lukas Odzioba <ukasz@openwall.net>
  * Copyright (c) 2010-2013 magnum
- * Copyright (c) 2012-2013 Claudio André <claudioandre.br at gmail.com>
+ * Copyright (c) 2012-2015 Claudio André <claudioandre.br at gmail.com>
  * and is hereby released to the general public under the following terms:
  *    Redistribution and use in source and binary forms, with or without
  *    modifications, are permitted.

src/opencl/sha256_kernel.cl
@@ -12,6 +12,7 @@
  */
 
 #include "opencl_rawsha256.h"
+#include "opencl_mask_extras.h"
 
 inline void _memcpy(               uint32_t * dest,
                     __global const uint32_t * src,
@@ -21,7 +22,7 @@ inline void _memcpy(               uint32_t * dest,
         *dest++ = *src++;
 }
 
-inline uint32_t sha256_block(uint32_t * buffer, int total) {
+inline void sha256_block(uint32_t * buffer, int total, uint32_t * H) {
     uint32_t a = H0;
     uint32_t b = H1;
     uint32_t c = H2;
@@ -38,24 +39,77 @@ inline uint32_t sha256_block(uint32_t * buffer, int total) {
         w[i] = SWAP32(buffer[i]);
     w[15] = (uint32_t) (total * 8);
 
-    /* Do the job, up to 61 iterations. */
-    SHA256_SHORT()
+    /* Do the job. */
+    #pragma unroll
+    for (int i = 0; i < 16; i++) {
+	t = k[i] + w[i] + h + Sigma1(e) + Ch(e, f, g);
 
-    /* Return partial hash value. */
-    return d;
+	h = g;
+	g = f;
+	f = e;
+	e = d + t;
+	t = t + Maj(a, b, c) + Sigma0(a);
+	d = c;
+	c = b;
+	b = a;
+	a = t;
+    }
+
+    #pragma unroll
+    for (int i = 16; i < 64; i++) {
+	w[i & 15] = sigma1(w[(i - 2) & 15]) + sigma0(w[(i - 15) & 15]) + w[(i - 16) & 15] + w[(i - 7) & 15];
+	t = k[i] + w[i & 15] + h + Sigma1(e) + Ch(e, f, g);
+
+	h = g;
+	g = f;
+	f = e;
+	e = d + t;
+	t = t + Maj(a, b, c) + Sigma0(a);
+	d = c;
+	c = b;
+	b = a;
+	a = t;
+    }
+
+    /* Put checksum in context given as argument. */
+    H[0] = SWAP32(a + H0);
+    H[1] = SWAP32(b + H1);
+    H[2] = SWAP32(c + H2);
+    H[3] = SWAP32(d + H3);
+    H[4] = SWAP32(e + H4);
+    H[5] = SWAP32(f + H5);
+    H[6] = SWAP32(g + H6);
+    H[7] = SWAP32(h + H7);
 }
 
 __kernel
-void kernel_crypt(__global   const uint32_t  * keys_buffer,
-                  __global   const uint32_t  * index,
-                  __global   uint32_t        * out_buffer) {
+void kernel_crypt(
+	     __global const uint32_t * keys_buffer,
+             __global const uint32_t * index,		//keys offset and length
+	     __global const uint32_t * int_key_loc,	//the position of the mask to apply
+	     __global const uint32_t * int_keys,	//mask to be applied
+			    uint32_t candidates_number,	//the number of candidates by mask mode
+			    uint32_t num_loaded_hashes,	//number of password hashes transfered
+	     __global const uint32_t * loaded_hashes,	//buffer of password hashes transfered
+    volatile __global       uint32_t * hash_id,		//information about how recover the cracked password
+    volatile __global       uint32_t * bitmap) {
 
     //Compute buffers (on CPU and NVIDIA, better private)
     int		    total;
     uint32_t	    w[16];
+    uint32_t	    H[8];
 
     //Get the task to be done
     size_t gid = get_global_id(0);
+
+    //Clean bitmap and result buffer
+    if (!gid) {
+	hash_id[0] = 0;
+
+	for (int i = 0; i < (num_loaded_hashes - 1)/32 + 1; i++)
+	    bitmap[i] = 0;
+    }
+    barrier(CLK_GLOBAL_MEM_FENCE);
 
     //Get position and length of informed key.
     uint32_t base = index[gid];
@@ -66,43 +120,25 @@ void kernel_crypt(__global   const uint32_t  * keys_buffer,
 
     //Clear the buffer.
     w[0] = 0;
-    w[1] = 0;
-    w[2] = 0;
-    w[3] = 0;
-    w[4] = 0;
-    w[5] = 0;
-    w[6] = 0;
-    w[7] = 0;
-    w[8] = 0;
-    w[9] = 0;
-    w[10] = 0;
-    w[11] = 0;
-    w[12] = 0;
-    w[13] = 0;
-    w[14] = 0;
+    w[1] = 0; w[2] = 0; w[3] = 0;  w[4] = 0;  w[5] = 0;  w[6] = 0;  w[7] = 0;
+    w[8] = 0; w[9] = 0; w[10] = 0; w[11] = 0; w[12] = 0; w[13] = 0; w[14] = 0;
 
     //Get password.
     _memcpy(w, keys_buffer, total);
 
     //Prepare buffer.
-    APPEND_SINGLE(w, 0x80UL, total);
-    CLEAR_BUFFER_32_SINGLE(w, total + 1);
+    CLEAR_BUFFER_32_SINGLE(w, total);
+    APPEND_SINGLE(w, 0x80U, total);
 
-    /* Run the collected hash value through SHA512. Return parcial results */
-    out_buffer[gid] = sha256_block(w, total);
-}
+    //Handle the candidates (candidates_number) to be produced.
+    for (int i = 0; i < candidates_number; i++) {
 
-__kernel
-void kernel_cmp(__global   uint32_t        * partial_hash,
-                __constant uint32_t        * partial_binary,
-                __global   int             * result) {
+	//Mask Mode: keys generation/finalization.
+	MASK_KEYS_GENERATION
 
-    //Get the task to be done
-    size_t gid = get_global_id(0);
+	/* Run the collected hash value through SHA256. */
+	sha256_block(w, total, H);
 
-    //Compare with partial computed hash.
-    if (*partial_binary == partial_hash[gid]) {
-        //Barrier point. FIX IT
-        *result = 1;
+	compare(i, num_loaded_hashes, loaded_hashes, hash_id, H, bitmap);
     }
 }

src/opencl_mask_extras.h
@@ -1,0 +1,77 @@
+/*
+ * This file is part of John the Ripper password cracker.
+ *
+ * Common OpenCL functions go in this file.
+ *
+ * This software is
+ * Copyright (c) 2014 by Sayantan Datta
+ * Copyright (c) 2012-2015 Claudio André <claudioandre.br at gmail.com>
+ * and is hereby released to the general public under the following terms:
+ *    Redistribution and use in source and binary forms, with or without
+ *    modifications, are permitted.
+ */
+
+#ifndef _JOHN_OPENCL_MASK_H
+#define _JOHN_OPENCL_MASK_H
+
+#define MASK_FMT_INT_PLHDR 		4
+
+#ifdef _OPENCL_COMPILER
+//To keep Sayantan license, some code was moved to this file.
+
+inline void compare(uint32_t iter,		//which candidates_number is this one
+		    uint32_t num_loaded_hashes,	//number of password hashes transfered
+     __global const uint32_t * loaded_hashes,   //buffer of password hashes transfered
+  volatile __global uint32_t * hash_id,		//information about how recover the cracked password
+		    uint32_t * hash,		//the hash calculated by this kernel
+  volatile __global uint32_t * bitmap) {
+
+    uint32_t found, j;
+
+    for (j = 0; j < num_loaded_hashes; j++) {
+	//It is not really better to handle only part of binary (hash[0]) on GPU.
+	found = (loaded_hashes[HASH_PARTS * j] == hash[0]);
+
+	if (found) {
+	    	found =
+		    (loaded_hashes[HASH_PARTS * j + 1] == hash[1]) &&
+		    (loaded_hashes[HASH_PARTS * j + 2] == hash[2]) &&
+		    (loaded_hashes[HASH_PARTS * j + 3] == hash[3]) &&
+		    (loaded_hashes[HASH_PARTS * j + 4] == hash[4]) &&
+		    (loaded_hashes[HASH_PARTS * j + 5] == hash[5]) &&
+		    (loaded_hashes[HASH_PARTS * j + 6] == hash[6]) &&
+		    (loaded_hashes[HASH_PARTS * j + 7] == hash[7]);
+
+	    if (found) {
+		/* Prevent duplicate keys from cracking same hash */
+		if (!(atomic_or(&bitmap[j/32], (1U << (j % 32))) &
+			(1U << (j % 32)))) {
+		    found = atomic_inc(&hash_id[0]);
+
+		    hash_id[1 + 3 * found] = get_global_id(0);
+		    hash_id[2 + 3 * found] = iter;
+		    hash_id[3 + 3 * found] = j;
+		}
+	    }
+	}
+    }
+}
+
+#define	MASK_KEYS_GENERATION \
+	if (candidates_number > 1) {								\
+		uint32_t ikl = int_key_loc[gid];						\
+		PUTCHAR(w, (ikl & 0xff), (int_keys[i] & 0xff));					\
+												\
+		if ((ikl & 0xff00) != 0x8000)							\
+		    PUTCHAR(w, ((ikl & 0xff00) >> 8), ((int_keys[i] & 0xff00) >> 8));		\
+												\
+		if ((ikl & 0xff0000) != 0x800000)						\
+		    PUTCHAR(w, ((ikl & 0xff0000) >> 16), ((int_keys[i] & 0xff0000) >> 16));	\
+												\
+		if ((ikl & 0xff000000) != 0x80000000)						\
+		    PUTCHAR(w, ((ikl & 0xff000000) >> 24), ((int_keys[i] & 0xff000000) >> 24));	\
+	}
+
+#endif
+
+#endif

src/opencl_rawsha256.h
@@ -18,13 +18,14 @@
 #include "opencl_sha256.h"
 
 //Constants.
-#define RAW_PLAINTEXT_LENGTH    55
+#define RAW_PLAINTEXT_LENGTH    55	/* 55 characters + 0x80 */
 #define CISCO_PLAINTEXT_LENGTH  26      /* 25 characters + 0x80 */
+#define PLAINTEXT_LENGTH	RAW_PLAINTEXT_LENGTH
 
 #define BUFFER_SIZE             56      /* RAW_PLAINTEXT_LENGTH multiple of 4 */
 #define CIPHERTEXT_LENGTH       64
-#define BINARY_SIZE             4
-#define FULL_BINARY_SIZE        32
+#define BINARY_SIZE             32
+#define HASH_PARTS		BINARY_SIZE / 4
 #define BINARY_ALIGN            4
 #define SALT_SIZE               0
 #define SALT_ALIGN              1

src/opencl_rawsha512_fmt_plug.c
@@ -310,6 +310,12 @@ static int salt_hash(void * salt) {
 }
 
 /* ------- Key functions ------- */
+static void reset(struct db_main *db) {
+	offset = 0;
+	offset_idx = 0;
+	key_idx = 0;
+}
+
 static void clear_keys(void) {
 	offset = 0;
 	offset_idx = 0;
@@ -317,12 +323,9 @@ static void clear_keys(void) {
 }
 
 static void set_key(char * _key, int index) {
-	int len = 0;
 
-	const uint32_t * key = (uint32_t *) _key;
-
-	while (*(_key++))
-		len++;
+	const ARCH_WORD_32 * key = (ARCH_WORD_32 *) _key;
+	int len = strlen(_key);
 
 	saved_idx[index] = (key_idx << 6) | len;
 
@@ -331,7 +334,7 @@ static void set_key(char * _key, int index) {
 		len -= 4;
 	}
 
-	if (len)
+	if (len > 0)
 		plaintext[key_idx++] = *key;
 
 	//Batch transfers to GPU.
@@ -355,9 +358,11 @@ static void set_key(char * _key, int index) {
 }
 
 static char * get_key(int index) {
-	static char ret[PLAINTEXT_LENGTH + 1];
+	static char * ret;
 	int len = saved_idx[index] & 63;
 	char * key = (char *) &plaintext[saved_idx[index] >> 6];
+
+	if (!ret) ret = mem_alloc_tiny(PLAINTEXT_LENGTH + 1, MEM_ALIGN_WORD);
 
 	memcpy(ret, key, PLAINTEXT_LENGTH);
 	ret[len] = '\0';
@@ -424,7 +429,9 @@ static int valid(char * ciphertext, struct fmt_main * self) {
 
 static char *split(char *ciphertext, int index, struct fmt_main *pFmt) {
 
-	static char out[8 + CIPHERTEXT_LENGTH_RAW + 1];
+	static char * out;
+
+	if (!out) out = mem_alloc_tiny(8 + CIPHERTEXT_LENGTH_RAW + 1, MEM_ALIGN_WORD);
 
 	if (!strncmp(ciphertext, "$SHA512$", 8))
 		ciphertext += 8;
@@ -449,7 +456,9 @@ static int valid_x(char * ciphertext, struct fmt_main * self) {
 }
 
 static char *split_x(char *ciphertext, int index, struct fmt_main *pFmt) {
-	static char out[8 + CIPHERTEXT_LENGTH_X + 1];
+	static char * out;
+
+	if (!out) out = mem_alloc_tiny(8 + CIPHERTEXT_LENGTH_X + 1, MEM_ALIGN_WORD);
 
 	if (!strncmp(ciphertext, "$LION$", 6))
 		return ciphertext;
@@ -514,10 +523,6 @@ static int crypt_all(int *pcount, struct db_salt *_salt) {
 
 	gws = GET_MULTIPLE_OR_BIGGER(count, local_work_size);
 
-	/* Self-test cludge */
-	if (offset > key_idx || offset > gws)
-		offset = 0;
-
 	//Send data to device.
 	if (new_keys && key_idx > offset)
 		HANDLE_CLERROR(clEnqueueWriteBuffer(queue[gpu_id], pass_buffer, CL_FALSE,
@@ -562,9 +567,6 @@ static int cmp_all(void * binary, int count) {
 	//Send data to device.
 	HANDLE_CLERROR(clEnqueueWriteBuffer(queue[gpu_id], p_binary_buffer, CL_FALSE, 0,
 			sizeof(uint32_t), &partial_binary, 0, NULL, NULL),
-			"failed in clEnqueueWriteBuffer p_binary_buffer");
-	HANDLE_CLERROR(clEnqueueWriteBuffer(queue[gpu_id], result_buffer, CL_FALSE, 0,
-			sizeof(int), &hash_found, 0, NULL, NULL),
 			"failed in clEnqueueWriteBuffer p_binary_buffer");
 
 	//Enqueue the kernel
@@ -686,7 +688,7 @@ struct fmt_main fmt_opencl_rawsha512 = {
 	}, {
 		init,
 		done,
-		fmt_default_reset,
+		reset,
 		fmt_default_prepare,
 		valid,
 		split,
@@ -750,7 +752,7 @@ struct fmt_main fmt_opencl_xsha512 = {
 	}, {
 		init_x,
 		done,
-		fmt_default_reset,
+		reset,
 		fmt_default_prepare,
 		valid_x,
 		split_x,

commit 763bc9eb148b4fcc29a3a9ef69fc7d1faa171664

Fix a bug introduced in b9e13c38, closing #1139.

src/rawSHA256_common.h
@@ -65,7 +65,8 @@ static char *prepare(char *split_fields[10], struct fmt_main *self)
 	static char * out;
 	char *o, *p = split_fields[1];
 
-	if (!out) out = mem_alloc_tiny(HEX_TAG_LEN + HEX_CIPHERTEXT_LENGTH + 1, MEM_ALIGN_WORD);
+	if (!out) out = mem_calloc_tiny(HEX_TAG_LEN + HEX_CIPHERTEXT_LENGTH + 1,
+	                                MEM_ALIGN_WORD);
 
 	if (!valid_cisco(p))
 		return p;
@@ -112,7 +113,8 @@ static char *split(char *ciphertext, int index, struct fmt_main *self)
 {
 	static char * out;
 
-	if (!out) out = mem_alloc_tiny(HEX_TAG_LEN + HEX_CIPHERTEXT_LENGTH + 1, MEM_ALIGN_WORD);
+	if (!out) out = mem_calloc_tiny(HEX_TAG_LEN + HEX_CIPHERTEXT_LENGTH + 1,
+	                                MEM_ALIGN_WORD);
 
 	if (!strncmp(ciphertext, HEX_TAG, HEX_TAG_LEN))
 		ciphertext += HEX_TAG_LEN;