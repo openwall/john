openwall/john

commit 2e33cfd620b526a287c646e1047970770a242b37

Change calloc_tiny to calloc/free in 14 formats. See #1110.

src/MD5_fmt.c
@@ -103,14 +103,18 @@ static void init(struct fmt_main *self)
 	self->params.max_keys_per_crypt = MD5_std_max_kpc;
 #endif
 
-	saved_key = mem_calloc_tiny(
-	    sizeof(*saved_key) * self->params.max_keys_per_crypt,
-	    MEM_ALIGN_CACHE);
+	saved_key = mem_calloc_align(self->params.max_keys_per_crypt,
+	                             sizeof(*saved_key), MEM_ALIGN_CACHE);
 #ifdef MD5_SSE_PARA
-	sout = mem_calloc_tiny(sizeof(*sout) *
-	                       self->params.max_keys_per_crypt *
-	                       BINARY_SIZE, sizeof(MD5_word));
+	sout = mem_calloc(self->params.max_keys_per_crypt,
+	                  sizeof(*sout) * BINARY_SIZE);
 #endif
+}
+
+static void done(void)
+{
+	MEM_FREE(sout);
+	MEM_FREE(saved_key);
 }
 
 static int get_hash_0(int index)
@@ -363,7 +367,7 @@ struct fmt_main fmt_MD5 = {
 		tests
 	}, {
 		init,
-		fmt_default_done,
+		done,
 		fmt_default_reset,
 		fmt_default_prepare,
 		cryptmd5_common_valid,

src/7z_fmt_plug.c
@@ -90,11 +90,17 @@ static void init(struct fmt_main *self)
 	omp_t *= OMP_SCALE;
 	self->params.max_keys_per_crypt *= omp_t;
 #endif
-	saved_key = mem_calloc_tiny(sizeof(*saved_key) *
-			self->params.max_keys_per_crypt, MEM_ALIGN_WORD);
-	cracked = mem_calloc_tiny(sizeof(*cracked) *
-			self->params.max_keys_per_crypt, MEM_ALIGN_WORD);
+	saved_key = mem_calloc(self->params.max_keys_per_crypt,
+	                       sizeof(*saved_key));
+	cracked   = mem_calloc(self->params.max_keys_per_crypt,
+	                       sizeof(*cracked));
 	CRC32_Init(&crc);
+}
+
+static void done(void)
+{
+	MEM_FREE(cracked);
+	MEM_FREE(saved_key);
 }
 
 static int valid(char *ciphertext, struct fmt_main *self)
@@ -430,7 +436,7 @@ struct fmt_main fmt_sevenzip = {
 		sevenzip_tests
 	}, {
 		init,
-		fmt_default_done,
+		done,
 		fmt_default_reset,
 		fmt_default_prepare,
 		valid,

src/BFEgg_fmt_plug.c
@@ -94,7 +94,7 @@ static int valid(char *ciphertext, struct fmt_main *self) {
 }
 
 void init(struct fmt_main *self) {
-    const char *pos;
+	const char *pos;
 
 #ifdef _OPENMP
 	omp_t = omp_get_max_threads();
@@ -102,13 +102,20 @@ void init(struct fmt_main *self) {
 	omp_t *= OMP_SCALE;
 	self->params.max_keys_per_crypt *= omp_t;
 #endif
-	saved_key = mem_calloc_tiny(sizeof(*saved_key) *
-			self->params.max_keys_per_crypt, MEM_ALIGN_WORD);
-	crypt_out = mem_calloc_tiny(sizeof(*crypt_out) * self->params.max_keys_per_crypt, MEM_ALIGN_WORD);
+	saved_key = mem_calloc(self->params.max_keys_per_crypt,
+	                       sizeof(*saved_key));
+	crypt_out = mem_calloc(self->params.max_keys_per_crypt,
+	                       sizeof(*crypt_out));
 
-    memset(_atoi64, 0x7F, sizeof(_atoi64));
-    for (pos = _itoa64; pos <= &_itoa64[63]; pos++)
-        _atoi64[ARCH_INDEX(*pos)] = pos - _itoa64;
+	memset(_atoi64, 0x7F, sizeof(_atoi64));
+	for (pos = _itoa64; pos <= &_itoa64[63]; pos++)
+		_atoi64[ARCH_INDEX(*pos)] = pos - _itoa64;
+}
+
+static void done(void)
+{
+	MEM_FREE(crypt_out);
+	MEM_FREE(saved_key);
 }
 
 /* The base64 is flawed - we just mimic flaws from the original code */
@@ -222,7 +229,7 @@ struct fmt_main fmt_BFEgg = {
     tests
   }, {
     init,
-    fmt_default_done,
+    done,
     fmt_default_reset,
     fmt_default_prepare,
     valid,

src/DMD5_fmt_plug.c
@@ -119,8 +119,16 @@ static void init(struct fmt_main *self)
 	omp_t *= OMP_SCALE;
 	self->params.max_keys_per_crypt *= omp_t;
 #endif
-	saved_key = mem_calloc_tiny(sizeof(*saved_key) * self->params.max_keys_per_crypt, MEM_ALIGN_SIMD);
-	crypt_key = mem_calloc_tiny(sizeof(*crypt_key) * self->params.max_keys_per_crypt, MEM_ALIGN_WORD);
+	saved_key = mem_calloc(self->params.max_keys_per_crypt,
+	                       SHA_BUF_SIZ * 4);
+	crypt_key = mem_calloc(self->params.max_keys_per_crypt,
+	                       BINARY_SIZE);
+}
+
+static void done(void)
+{
+	MEM_FREE(crypt_key);
+	MEM_FREE(saved_key);
 }
 
 static int valid(char *ciphertext, struct fmt_main *self)
@@ -409,7 +417,7 @@ struct fmt_main fmt_DMD5 = {
 	},
 	{
 		init,
-		fmt_default_done,
+		done,
 		fmt_default_reset,
 		fmt_default_prepare,
 		valid,

src/DOMINOSEC_fmt_plug.c
@@ -167,12 +167,20 @@ static void init(struct fmt_main *self)
 	omp_t *= OMP_SCALE;
 	self->params.max_keys_per_crypt *= omp_t;
 #endif
-	saved_key = mem_calloc_tiny(sizeof(*saved_key) *
-			self->params.max_keys_per_crypt, MEM_ALIGN_WORD);
-	crypt_out = mem_calloc_tiny(sizeof(*crypt_out) * self->params.max_keys_per_crypt, MEM_ALIGN_WORD);
-	digest34 = mem_calloc_tiny(sizeof(*digest34) *
-			self->params.max_keys_per_crypt, MEM_ALIGN_WORD);
+	saved_key = mem_calloc(self->params.max_keys_per_crypt,
+	                       sizeof(*saved_key));
+	crypt_out = mem_calloc(self->params.max_keys_per_crypt,
+	                       sizeof(*crypt_out));
+	digest34  = mem_calloc(self->params.max_keys_per_crypt,
+	                       sizeof(*digest34));
 	keys_changed = salt_changed = 0;
+}
+
+static void done(void)
+{
+	MEM_FREE(digest34);
+	MEM_FREE(crypt_out);
+	MEM_FREE(saved_key);
 }
 
 static struct {
@@ -746,7 +754,7 @@ struct fmt_main fmt_DOMINOSEC = {
 	},
 	{
 		init,
-		fmt_default_done,
+		done,
 		fmt_default_reset,
 		fmt_default_prepare,
 		valid,

src/HDAA_fmt_plug.c
@@ -149,14 +149,36 @@ static void init(struct fmt_main *self)
 #endif
 #ifdef SIMD_COEF_32
 	for (i = 0; i < LIMBS; i++)
-		saved_key[i] = mem_calloc_tiny(64 * self->params.max_keys_per_crypt, MEM_ALIGN_SIMD);
-	interm_key = mem_calloc_tiny(16 * self->params.max_keys_per_crypt, MEM_ALIGN_SIMD);
-	crypt_key = mem_calloc_tiny(16 * self->params.max_keys_per_crypt, MEM_ALIGN_SIMD);
+		saved_key[i] = mem_calloc_align(self->params.max_keys_per_crypt,
+		                                64, MEM_ALIGN_SIMD);
+	interm_key = mem_calloc_align(self->params.max_keys_per_crypt,
+	                              16, MEM_ALIGN_SIMD);
+	crypt_key = mem_calloc_align(self->params.max_keys_per_crypt,
+	                             16, MEM_ALIGN_SIMD);
 #else
-	crypt_key = mem_calloc_tiny(sizeof(*crypt_key) * self->params.max_keys_per_crypt, MEM_ALIGN_WORD);
-	saved_len = mem_calloc_tiny(sizeof(*saved_len) * self->params.max_keys_per_crypt, MEM_ALIGN_WORD);
+	saved_len = mem_calloc(self->params.max_keys_per_crypt,
+	                       sizeof(*saved_len));
+	crypt_key = mem_calloc(self->params.max_keys_per_crypt,
+	                       sizeof(*crypt_key));
 #endif
-	saved_plain = mem_calloc_tiny(sizeof(*saved_plain) * self->params.max_keys_per_crypt, MEM_ALIGN_WORD);
+	saved_plain = mem_calloc(self->params.max_keys_per_crypt,
+	                         sizeof(*saved_plain));
+}
+
+static void done(void)
+{
+#ifdef SIMD_COEF_32
+	int i;
+#endif
+	MEM_FREE(saved_plain);
+	MEM_FREE(crypt_key);
+#ifdef SIMD_COEF_32
+	MEM_FREE(interm_key);
+	for (i = 0; i < LIMBS; i++)
+		MEM_FREE(saved_key[i]);
+#else
+	MEM_FREE(saved_len);
+#endif
 }
 
 static int valid(char *ciphertext, struct fmt_main *self)
@@ -697,7 +719,7 @@ struct fmt_main fmt_HDAA = {
 		tests
 	}, {
 		init,
-		fmt_default_done,
+		done,
 		fmt_default_reset,
 		fmt_default_prepare,
 		valid,

src/IPB2_fmt_plug.c
@@ -150,19 +150,37 @@ static void init(struct fmt_main *self)
 	self->params.max_keys_per_crypt = (omp_t*NBKEYS);
 #endif
 #if SIMD_COEF_32
-	key_buf = mem_calloc_tiny(64 * self->params.max_keys_per_crypt, MEM_ALIGN_SIMD);
-	crypt_key = mem_calloc_tiny(BINARY_SIZE * self->params.max_keys_per_crypt, MEM_ALIGN_SIMD);
-	saved_key = mem_calloc_tiny(64 * self->params.max_keys_per_crypt, MEM_ALIGN_SIMD);
-	empty_key = mem_calloc_tiny(64 * NBKEYS, MEM_ALIGN_SIMD);
+	key_buf   = mem_calloc_align(self->params.max_keys_per_crypt,
+	                             64, MEM_ALIGN_SIMD);
+	empty_key = mem_calloc_align(64 * NBKEYS,
+	                             sizeof(empty_key), MEM_ALIGN_SIMD);
 	for (i = 0; i < NBKEYS; ++i) {
 		empty_key[GETPOS(0, i)] = 0x80;
 		((unsigned int*)empty_key)[14*SIMD_COEF_32 + (i&3) + (i>>2)*16*SIMD_COEF_32] = (2 * MD5_HEX_SIZE)<<3;
 	}
+	crypt_key = mem_calloc_align(self->params.max_keys_per_crypt,
+	                             BINARY_SIZE, MEM_ALIGN_SIMD);
+	saved_key = mem_calloc_align(self->params.max_keys_per_crypt,
+	                             64, MEM_ALIGN_SIMD);
 #else
-	crypt_key = mem_calloc_tiny(sizeof(*crypt_key) * self->params.max_keys_per_crypt, MEM_ALIGN_WORD);
-	saved_key = mem_calloc_tiny(sizeof(*saved_key) * self->params.max_keys_per_crypt, MEM_ALIGN_WORD);
+	crypt_key = mem_calloc(self->params.max_keys_per_crypt,
+	                       sizeof(*crypt_key));
+	saved_key = mem_calloc(self->params.max_keys_per_crypt,
+	                       sizeof(*saved_key));
 #endif
-	saved_plain = mem_calloc_tiny(sizeof(*saved_plain) * self->params.max_keys_per_crypt, MEM_ALIGN_WORD);
+	saved_plain = mem_calloc(self->params.max_keys_per_crypt,
+	                         sizeof(*saved_plain));
+}
+
+static void done(void)
+{
+	MEM_FREE(saved_plain);
+	MEM_FREE(saved_key);
+	MEM_FREE(crypt_key);
+#if SIMD_COEF_32
+	MEM_FREE(empty_key);
+	MEM_FREE(key_buf);
+#endif
 }
 
 static int valid(char *ciphertext, struct fmt_main *self)
@@ -475,7 +493,7 @@ struct fmt_main fmt_IPB2 = {
 	},
 	{
 		init,
-		fmt_default_done,
+		done,
 		fmt_default_reset,
 		fmt_default_prepare,
 		valid,

src/KRB5_fmt_plug.c
@@ -52,7 +52,7 @@ john_register_one(&fmt_KRB5);
 #include "memdbg.h"
 
 
-// defines                                                  // {{{
+// defines
 #define MAGIC_PREFIX        "$krb5$"
 #define MAX_REALM_LEN       64
 #define TGT_SIZE            228
@@ -74,37 +74,33 @@ john_register_one(&fmt_KRB5);
 
 /* This string is a bit too short - might give false positives */
 #define KRBTGT              "krbtgt"
-// }}}
 
 
 /**
- * structure to hold the self tests                             // {{{
+ * structure to hold the self tests
  */
 static struct fmt_tests    fmt_tests[] = {
     {"$krb5$oskov$ACM.UIUC.EDU$4730d7249765615d6f3652321c4fb76d09fb9cd06faeb0c31b8737f9fdfcde4bd4259c31cb1dff25df39173b09abdff08373302d99ac09802a290915243d9f0ea0313fdedc7f8d1fae0d9df8f0ee6233818d317f03a72c2e77b480b2bc50d1ca14fba85133ea00e472c50dbc825291e2853bd60a969ddb69dae35b604b34ea2c2265a4ffc72e9fb811da17c7f2887ccb17e2f87cd1f6c28a9afc0c083a9356a9ee2a28d2e4a01fc7ea90cc8836b8e25650c3a1409b811d0bad42a59aa418143291d42d7b1e6cb5b1876a4cc758d721323a762e943f774630385c9faa68df6f3a94422f97", "p4ssW0rd"},
     {"$krb5$oskov$ACM.UIUC.EDU$6cba0316d38e31ba028f87394792baade516afdfd8c5a964b6a7677adbad7815d778b297beb238394aa97a4d495adb7c9b7298ba7c2a2062fb6c9a4297f12f83755060f4f58a1ea4c7026df585cdfa02372ad619ab1a4ec617ad23e76d6e37e36268d9aa0abcf83f11fa8092b4328c5e6c577f7ec6f1c1684d9c99a309eee1f5bd764c4158a2cf311cded8794b2de83131c3dc51303d5300e563a2b7a230eac67e85b4593e561bf6b88c77b82c729e7ba7f3d2f99b8dc85b07873e40335aff4647833a87681ee557fbd1ffa1a458a5673d1bd3c1587eceeabaebf4e44c24d9a8ac8c1d89", "Nask0Oskov"},
     {NULL}
 };
-// }}}
 
 /**
  * struct to save the salt into
  */
-struct salt {                                                       // {{{
+struct salt {
     char    realm[MAX_REALM_LEN];
     char    user[MAX_USER_LEN];
     char    tgt_ebin[TGT_SIZE];
     char    passwd[MAX_PASS_LEN];
 };
 
-// }}}
 
-struct key {                                                        // {{{
+struct key {
     char    passwd[MAX_PASS_LEN];
     char    key[MAX_PASS_LEN];
     DES_key_schedule sched[3];
 };
-// }}}
 
 static struct salt *psalt = NULL;
 static struct key skey;
@@ -120,7 +116,7 @@ static krb5_key _krb5key;
 static krb5_key *krb5key = &_krb5key;
 
 /**
- * hex2bin           // {{{
+ * hex2bin
  */
 static char *hex2bin(char *src, unsigned char *dst, int outsize) {
     char *p, *pe;
@@ -145,10 +141,9 @@ static char *hex2bin(char *src, unsigned char *dst, int outsize) {
     }
     return p;
 }
-// }}}
 
 /**
- * krb5_decrypt_compare                                             // {{{
+ * krb5_decrypt_compare
  *
  */
 static int decrypt_compare() {
@@ -181,10 +176,9 @@ static int decrypt_compare() {
             }
     return 0;
 }
-// }}}
 
 /**
- * void * krb5_salt                                                 // {{{
+ * void * krb5_salt
  *
  */
 static void * salt(char *ciphertext) {
@@ -223,10 +217,9 @@ static void * salt(char *ciphertext) {
 
     return &salt;
 }
-// }}}
 
 /**
- * int valid                                                   // {{{
+ * int valid
  *
  */
 static int valid(char *ciphertext, struct fmt_main *self) {
@@ -236,19 +229,17 @@ static int valid(char *ciphertext, struct fmt_main *self) {
 
     return salt(ciphertext) ? 1 : 0;
 }
-// }}}
 
 /**
- * void set_salt                                               // {{{
+ * void set_salt
  *
  */
 static void set_salt(void *salt) {
     psalt = (struct salt *) salt;
 }
-// }}}
 
 /**
- * void krb5_set_key                                                // {{{
+ * void krb5_set_key
  *
  */
 static void krb5_set_key(char *key, int index) {
@@ -258,38 +249,34 @@ static void krb5_set_key(char *key, int index) {
     strnzcpy(skey.passwd, key, sizeof(skey.passwd));
 
 }
-// }}}
 
 /**
- * char * get_key                                              // {{{
+ * char * get_key
  *
  */
 static char * get_key(int index) {
     return skey.passwd;
 }
-// }}}
 
 /**
- * void crypt_all                                              // {{{
+ * void crypt_all
  *
  */
 static int crypt_all(int *pcount, struct db_salt *salt)
 {
 	return *pcount;
 }
-// }}}
 
 /**
- * int cmp_all                                                 // {{{
+ * int cmp_all
  *
  */
 static int cmp_all(void *binary, int count) {
     return decrypt_compare();
 }
-// }}}
 
 /**
- * int cmp_one                                                 // {{{
+ * int cmp_one
  *
  */
 static int cmp_one(void *binary, int count) {
@@ -297,19 +284,17 @@ static int cmp_one(void *binary, int count) {
     return decrypt_compare();
 
 }
-// }}}
 
 /**
- * int cmp_exact                                               // {{{
+ * int cmp_exact
  *
  */
 static int cmp_exact(char *source, int index) {
     return 1;
 }
-// }}}
 
 /**
- * void init                                                   // {{{
+ * void init
  *
  */
 static void init(struct fmt_main *self) {
@@ -318,16 +303,21 @@ static void init(struct fmt_main *self) {
     memset(&skey, 0x00, sizeof(skey));
     memset(krb5key, 0x00, sizeof(krb5_key));
 
-    krb5key->key = (char *) mem_alloc_tiny(DES3_KEY_SIZE, MEM_ALIGN_NONE);
-    krb5key->schedule = (char *) mem_alloc_tiny(DES3_KEY_SCHED_SIZE, MEM_ALIGN_WORD);
+    krb5key->key = (char *) mem_alloc(DES3_KEY_SIZE);
+    krb5key->schedule = (char *) mem_alloc(DES3_KEY_SCHED_SIZE);
     memset(krb5key->key, 0x00, DES3_KEY_SIZE);
     memset(krb5key->schedule, 0x00, DES3_KEY_SCHED_SIZE);
 
 }
-// }}}
+
+static void done(void)
+{
+	MEM_FREE(krb5key->schedule);
+	MEM_FREE(krb5key->key);
+}
 
 /**
- * fmt_main struct with KRB5 values                                     // {{{
+ * fmt_main struct with KRB5 values
  */
 struct fmt_main fmt_KRB5 = {
 	{
@@ -351,7 +341,7 @@ struct fmt_main fmt_KRB5 = {
 		fmt_tests
 	}, {
 		init,
-		fmt_default_done,
+		done,
 		fmt_default_reset,
 		fmt_default_prepare,
 		valid,
@@ -388,6 +378,5 @@ struct fmt_main fmt_KRB5 = {
 		cmp_exact
 	}
 };
-// }}}
 
 #endif /* plugin stanza */

commit 458579439524d3766976e37e4ae30b19b40cbc93

Fix a bug in MD5 introduced in 2e33cfd. Closes #1142.

src/MD5_fmt.c
@@ -113,7 +113,9 @@ static void init(struct fmt_main *self)
 
 static void done(void)
 {
+#ifdef MD5_SSE_PARA
 	MEM_FREE(sout);
+#endif
 	MEM_FREE(saved_key);
 }
 