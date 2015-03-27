openwall/john

commit f6af7d71df255c87fafb4cb9a7d46886e9f7369a

dynamic: calls dynamic_FIX_SALT_TO_HEX if hash is dyna on --show=left fixes #986

src/loader.c
@@ -1083,6 +1083,11 @@ static void ldr_show_left(struct db_main *db, struct db_password *pw)
 {
 	char uid_sep[2] = { 0 };
 	char *uid_out = "";
+	char *pw_source = pw->source;
+	/* Note for salted dynamic, we 'may' need to fix up the salts to
+	 * make them properly usable. */
+	if (!strncmp(pw_source, "$dynamic_", 9))
+		pw_source = dynamic_FIX_SALT_TO_HEX(pw_source);
 	if (options.show_uid_on_crack && pw->uid && *pw->uid) {
 		uid_sep[0] = db->options->field_sep_char;
 		uid_out = pw->uid;
@@ -1094,11 +1099,11 @@ static void ldr_show_left(struct db_main *db, struct db_password *pw)
 		cp_to_utf8_r(pw->login, utf8login,
 		             PLAINTEXT_BUFFER_SIZE);
 		printf("%s%c%s%s%s\n", utf8login, db->options->field_sep_char,
-		       db->format->methods.source(pw->source, pw->binary),
+		       db->format->methods.source(pw_source, pw->binary),
 			   uid_sep, uid_out);
 	} else
 		printf("%s%c%s%s%s\n", pw->login, db->options->field_sep_char,
-		       db->format->methods.source(pw->source, pw->binary),
+		       db->format->methods.source(pw_source, pw->binary),
 			   uid_sep, uid_out);
 }
 

commit c0e334a1f4ac3230a55bf3c471fc4c821994eaa6

Fix a bad bug introduced by f6af7d7: --show=left was broken for all formats but dynamic. Closes #1130.

src/loader.c
@@ -1083,7 +1083,8 @@ static void ldr_show_left(struct db_main *db, struct db_password *pw)
 {
 	char uid_sep[2] = { 0 };
 	char *uid_out = "";
-	char *pw_source = pw->source;
+	char *pw_source = db->format->methods.source(pw->source, pw->binary);
+
 	/* Note for salted dynamic, we 'may' need to fix up the salts to
 	 * make them properly usable. */
 	if (!strncmp(pw_source, "$dynamic_", 9))
@@ -1099,12 +1100,10 @@ static void ldr_show_left(struct db_main *db, struct db_password *pw)
 		cp_to_utf8_r(pw->login, utf8login,
 		             PLAINTEXT_BUFFER_SIZE);
 		printf("%s%c%s%s%s\n", utf8login, db->options->field_sep_char,
-		       db->format->methods.source(pw_source, pw->binary),
-			   uid_sep, uid_out);
+		       pw_source, uid_sep, uid_out);
 	} else
 		printf("%s%c%s%s%s\n", pw->login, db->options->field_sep_char,
-		       db->format->methods.source(pw_source, pw->binary),
-			   uid_sep, uid_out);
+		       pw_source, uid_sep, uid_out);
 }
 
 /*