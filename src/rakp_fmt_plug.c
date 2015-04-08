c42a628d5dd1c4231a6a89d6be6a856e6862c0f2
magnum <john.magnum@hushmail.com>
2015-04-08 10:37:24 +0200

    Fix hardcoded vector widths in RAKP.

--- src/rakp_fmt_plug.c
+++ src/rakp_fmt_plug.c
@@ -130,7 +130,7 @@ static void init(struct fmt_main *self)
 	                             sizeof(*prep_opad), MEM_ALIGN_SIMD);
 	for (i = 0; i < self->params.max_keys_per_crypt; ++i) {
 		crypt_key[GETPOS(BINARY_SIZE, i)] = 0x80;
-		((unsigned int*)crypt_key)[15 * SIMD_COEF_32 + (i & 3) + (i >> 2) * SHA_BUF_SIZ * SIMD_COEF_32] = (BINARY_SIZE + 64) << 3;
+		((unsigned int*)crypt_key)[15 * SIMD_COEF_32 + (i&(SIMD_COEF_32-1)) + i/SIMD_COEF_32 * SHA_BUF_SIZ * SIMD_COEF_32] = (BINARY_SIZE + 64) << 3;
 	}
 	clear_keys();
 #else
@@ -315,8 +315,8 @@ static int cmp_one(void *binary, int index)
 #ifdef SIMD_COEF_32
 	int i;
 	for(i = 0; i < (BINARY_SIZE/4); i++)
-		// NOTE crypt_key is in input format (4*SHA_BUF_SIZ*SIMD_COEF_32)
-		if (((ARCH_WORD_32*)binary)[i] != ((ARCH_WORD_32*)crypt_key)[i * SIMD_COEF_32 + (index & 3) + (index >> 2) * SHA_BUF_SIZ * SIMD_COEF_32])
+		// NOTE crypt_key is in input format (4 * SHA_BUF_SIZ * SIMD_COEF_32)
+		if (((ARCH_WORD_32*)binary)[i] != ((ARCH_WORD_32*)crypt_key)[i * SIMD_COEF_32 + (index&(SIMD_COEF_32-1)) + index/SIMD_COEF_32 * SHA_BUF_SIZ * SIMD_COEF_32])
 			return 0;
 	return 1;
 #else
@@ -437,7 +437,7 @@ static void *get_salt(char *ciphertext)
 			cur_salt[j>>6][GETPOS(j & 63, i)] = 0;
 
 	for (i = 0; i < SHA1_N; ++i)
-		((unsigned int*)cur_salt[1])[15 * SIMD_COEF_32 + (i & 3) + (i >> 2) * SHA_BUF_SIZ * SIMD_COEF_32] = (len + 64) << 3;
+		((unsigned int*)cur_salt[1])[15 * SIMD_COEF_32 + (i&(SIMD_COEF_32-1)) + i/SIMD_COEF_32 * SHA_BUF_SIZ * SIMD_COEF_32] = (len + 64) << 3;
 	return &cur_salt;
 #else
 	cur_salt.length = len;
