openwall/john

commit eaaf00625c2af421736b1e69dfaad3939cd4fb0f

Fix incorrect function declarations for cmp_all() and cmp_one()

src/oracle_fmt_plug.c
@@ -341,11 +341,18 @@ static int get_hash_4(int index) { return crypt_key[0] & 0xfffff; }
 static int get_hash_5(int index) { return crypt_key[0] & 0xffffff; }
 static int get_hash_6(int index) { return crypt_key[0] & 0x7ffffff; }
 
-static int cmp_all(void *binary, int index) {
+static int cmp_all(void *binary, int count)
+{
 	return !memcmp(binary, crypt_key, sizeof(crypt_key));
 }
 
-static int cmp_exact(char *source, int count) {
+static int cmp_one(void *binary, int index)
+{
+	return 1;
+}
+
+static int cmp_exact(char *source, int index)
+{
 	return 1;
 }
 
@@ -408,7 +415,7 @@ struct fmt_main fmt_oracle = {
 			get_hash_6
 		},
 		cmp_all,
-		cmp_all,
+		cmp_one,
 		cmp_exact
 	}
 };

src/BFEgg_fmt_plug.c
@@ -175,7 +175,8 @@ static int cmp_one(void *binary, int index)
 	return !memcmp(binary, crypt_out[index], BINARY_SIZE);
 }
 
-static int cmp_exact(char *source, int index) {
+static int cmp_exact(char *source, int index)
+{
   return 1;
 }
 

src/HDAA_fmt_plug.c
@@ -282,7 +282,7 @@ static int cmp_one(void *binary, int index)
 #endif
 }
 
-static int cmp_exact(char *source, int count)
+static int cmp_exact(char *source, int index)
 {
 	return 1;
 }

src/KRB5_fmt_plug.c
@@ -289,7 +289,8 @@ static int cmp_one(void *binary, int count) {
  * int cmp_exact
  *
  */
-static int cmp_exact(char *source, int index) {
+static int cmp_exact(char *source, int index)
+{
     return 1;
 }
 

src/PO_fmt_plug.c
@@ -144,10 +144,14 @@ static char *get_key(int index)
 	return saved_key;
 }
 
-static int cmp_all(void *binary, int index)
+static int cmp_all(void *binary, int count)
 {
-	/* also used for cmp_one */
 	return *(MD5_word *)binary == MD5_out[0];
+}
+
+static int cmp_one(void *binary, int index)
+{
+	return 1;
 }
 
 static int cmp_exact(char *source, int index)
@@ -259,7 +263,7 @@ struct fmt_main fmt_PO = {
 			get_hash_6
 		},
 		cmp_all,
-		cmp_all,
+		cmp_one,
 		cmp_exact
 	}
 };

src/SKEY_fmt_plug.c
@@ -261,7 +261,7 @@ skey_cmp_one(void *binary, int count)
 }
 
 static int
-skey_cmp_exact(char *source, int count)
+skey_cmp_exact(char *source, int index)
 {
 	return 1;
 }

src/cuda_cryptsha256_fmt_plug.c
@@ -207,7 +207,7 @@ static int cmp_one(void *binary, int index)
 	return 1;
 }
 
-static int cmp_exact(char *source, int count)
+static int cmp_exact(char *source, int index)
 {
 	return 1;
 }

src/cuda_cryptsha512_fmt_plug.c
@@ -202,7 +202,7 @@ static int cmp_one(void *binary, int index)
 	return 1;
 }
 
-static int cmp_exact(char *source, int count)
+static int cmp_exact(char *source, int index)
 {
 	return 1;
 }

commit 8c58013e7ebdbe0b539108b659ac503c8d6b01e2

Fix oracle format breakage caused by commit eaaf00625c2af421736b1e69dfaad3939cd4fb0f cmp_one cannot be a dummy function (returning 1) if cmp_exact is a dummy.

src/oracle_fmt_plug.c
@@ -346,11 +346,6 @@ static int cmp_all(void *binary, int count)
 	return !memcmp(binary, crypt_key, sizeof(crypt_key));
 }
 
-static int cmp_one(void *binary, int index)
-{
-	return 1;
-}
-
 static int cmp_exact(char *source, int index)
 {
 	return 1;
@@ -415,7 +410,7 @@ struct fmt_main fmt_oracle = {
 			get_hash_6
 		},
 		cmp_all,
-		cmp_one,
+		cmp_all,
 		cmp_exact
 	}
 };