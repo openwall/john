openwall/john

commit 03404dfd88c6fcc8aa8fa7dd31b72bbc060cc200

Report glibc version with --list=build-info

src/listconf.c
@@ -49,6 +49,10 @@
 #endif
 #endif
 
+#if __GNUC__
+#include <gnu/libc-version.h>
+#endif
+
 #include "regex.h"
 
 #ifdef NO_JOHN_BLD
@@ -191,6 +195,12 @@ static void listconf_list_build_info(void)
 #endif
 #if defined(__clang_version__) && !__INTEL_COMPILER
 	printf("clang version: %s\n", __clang_version__);
+#endif
+#ifdef __GLIBC_MINOR__
+#ifdef __GNUC__
+	printf("GNU libc version: %d.%d (loaded: %s)\n",
+	       __GLIBC__, __GLIBC_MINOR__, gnu_get_libc_version());
+#endif
 #endif
 #if HAVE_CUDA
 	printf("CUDA library version: %s\n",get_cuda_header_version());

commit 3b54b18d64d7b6a30545e8c733e3476d73292000

03404dfd broke for any system using gcc but not glibc. Just comment it out for now.

src/listconf.c
@@ -53,9 +53,11 @@
 #endif
 #endif
 
+/* fails when building on gcc on non-GNU systems (eg. OSX)
 #if __GNUC__
 #include <gnu/libc-version.h>
 #endif
+*/
 
 #include "regex.h"
 