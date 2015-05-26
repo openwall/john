openwall/john

commit b96ed88fc50090a513d5037b703f031dbeaa3e59

SHA2 interleaving fixes. More to come.

src/mssql12_fmt_plug.c
@@ -73,8 +73,8 @@ static int omp_t = 1;
 #define SALT_ALIGN              4
 
 #ifdef SIMD_COEF_64
-#define MIN_KEYS_PER_CRYPT      SIMD_COEF_64
-#define MAX_KEYS_PER_CRYPT      SIMD_COEF_64
+#define MIN_KEYS_PER_CRYPT      (SIMD_COEF_64*SIMD_PARA_SHA512)
+#define MAX_KEYS_PER_CRYPT      (SIMD_COEF_64*SIMD_PARA_SHA512)
 #else
 #define MIN_KEYS_PER_CRYPT      1
 #define MAX_KEYS_PER_CRYPT      1
@@ -103,7 +103,7 @@ static struct fmt_tests tests[] = {
 static unsigned char cursalt[SALT_SIZE];
 #ifdef SIMD_COEF_64
 static ARCH_WORD_64 (*saved_key)[SHA512_BUF_SIZ];
-static ARCH_WORD_64 (*crypt_out)[8*SIMD_COEF_64];
+static ARCH_WORD_64 (*crypt_out)[8*MAX_KEYS_PER_CRYPT];
 static int max_keys;
 static int new_keys;
 #else
@@ -167,9 +167,7 @@ static void init(struct fmt_main *self)
 	saved_key = mem_calloc_align(self->params.max_keys_per_crypt,
 	                             sizeof(*saved_key),
 	                             MEM_ALIGN_SIMD);
-	crypt_out = mem_calloc_align(self->params.max_keys_per_crypt /
-	                             SIMD_COEF_64,
-	                             sizeof(*crypt_out), MEM_ALIGN_SIMD);
+	crypt_out = mem_calloc_align(omp_t, sizeof(*crypt_out), MEM_ALIGN_SIMD);
 	max_keys = self->params.max_keys_per_crypt;
 #else
 	saved_key = mem_calloc(self->params.max_keys_per_crypt,
@@ -318,23 +316,18 @@ static int crypt_all(int *pcount, struct db_salt *salt)
 {
 	const int count = *pcount;
 	unsigned int index = 0;
-#ifdef SIMD_COEF_64
-	const int inc = SIMD_COEF_64;
-#else
-	const int inc = 1;
-#endif
 
 #ifdef _OPENMP
 #pragma omp parallel for
 #endif
 #if defined(_OPENMP) || PLAINTEXT_LENGTH > 1
-	for (index = 0; index < count; index += inc)
+	for (index = 0; index < count; index += MAX_KEYS_PER_CRYPT)
 #endif
 	{
 #ifdef SIMD_COEF_64
 		if (new_keys) {
 			int i;
-			for (i = 0; i < SIMD_COEF_64; i++) {
+			for (i = 0; i < MAX_KEYS_PER_CRYPT; i++) {
 				ARCH_WORD_64 *keybuffer = saved_key[index + i];
 				unsigned char *wucp = (unsigned char*)keybuffer;
 				int j, len = (keybuffer[15] >> 3) - SALT_SIZE;
@@ -346,7 +339,7 @@ static int crypt_all(int *pcount, struct db_salt *salt)
 				wucp[len + 4] = 0x80;
 			}
 		}
-		SSESHA512body(&saved_key[index], crypt_out[index/SIMD_COEF_64], NULL, SSEi_FLAT_IN);
+		SSESHA512body(&saved_key[index], crypt_out[index/MAX_KEYS_PER_CRYPT], NULL, SSEi_FLAT_IN);
 #else
 		SHA512_CTX ctx;
 		memcpy(saved_key[index]+saved_len[index], cursalt, SALT_SIZE);

src/rawSHA224_fmt_plug.c
@@ -65,8 +65,8 @@ john_register_one(&fmt_rawSHA224);
 #define SALT_ALIGN				1
 
 #ifdef SIMD_COEF_32
-#define MIN_KEYS_PER_CRYPT		SIMD_COEF_32
-#define MAX_KEYS_PER_CRYPT      SIMD_COEF_32
+#define MIN_KEYS_PER_CRYPT		(SIMD_COEF_32*SIMD_PARA_SHA256)
+#define MAX_KEYS_PER_CRYPT      (SIMD_COEF_32*SIMD_PARA_SHA256)
 #else
 #define MIN_KEYS_PER_CRYPT		1
 #define MAX_KEYS_PER_CRYPT		1
@@ -82,8 +82,8 @@ static struct fmt_tests tests[] = {
 
 #ifdef SIMD_COEF_32
 #define GETPOS(i, index)		( (index&(SIMD_COEF_32-1))*4 + ((i)&(0xffffffff-3))*SIMD_COEF_32 + (3-((i)&3)) + (unsigned int)index/SIMD_COEF_32*SHA256_BUF_SIZ*SIMD_COEF_32*4 )
-static uint32_t (*saved_key)[SHA256_BUF_SIZ*SIMD_COEF_32];
-static uint32_t (*crypt_out)[8*SIMD_COEF_32];
+static uint32_t (*saved_key)[SHA256_BUF_SIZ*MAX_KEYS_PER_CRYPT];
+static uint32_t (*crypt_out)[8*MAX_KEYS_PER_CRYPT];
 #else
 static int (*saved_len);
 static char (*saved_key)[PLAINTEXT_LENGTH + 1];
@@ -109,12 +109,8 @@ static void init(struct fmt_main *self)
 	crypt_out = mem_calloc(self->params.max_keys_per_crypt,
 	                       sizeof(*crypt_out));
 #else
-	saved_key = mem_calloc_align(self->params.max_keys_per_crypt /
-	                             SIMD_COEF_32,
-	                             sizeof(*saved_key), MEM_ALIGN_SIMD);
-	crypt_out = mem_calloc_align(self->params.max_keys_per_crypt /
-	                             SIMD_COEF_32,
-	                             sizeof(*crypt_out), MEM_ALIGN_SIMD);
+	saved_key = mem_calloc_align(omp_t, sizeof(*saved_key), MEM_ALIGN_SIMD);
+	crypt_out = mem_calloc_align(omp_t, sizeof(*crypt_out), MEM_ALIGN_SIMD);
 #endif
 }
 
@@ -270,14 +266,8 @@ static int crypt_all(int *pcount, struct db_salt *salt)
 	int index = 0;
 
 #ifdef _OPENMP
-#ifdef SIMD_COEF_32
-	int inc = SIMD_COEF_32;
-#else
-	int inc = 1;
-#endif
-
 #pragma omp parallel for
-	for (index = 0; index < count; index += inc)
+	for (index = 0; index < count; index += MAX_KEYS_PER_CRYPT)
 #endif
 	{
 #ifdef SIMD_COEF_32

src/XSHA512_fmt_plug.c
@@ -41,8 +41,8 @@ john_register_one(&fmt_XSHA512);
 #define SALT_ALIGN			sizeof(ARCH_WORD_32)
 
 #ifdef SIMD_COEF_64
-#define MIN_KEYS_PER_CRYPT      SIMD_COEF_64
-#define MAX_KEYS_PER_CRYPT      SIMD_COEF_64
+#define MIN_KEYS_PER_CRYPT      (SIMD_COEF_64*SIMD_PARA_SHA512)
+#define MAX_KEYS_PER_CRYPT      (SIMD_COEF_64*SIMD_PARA_SHA512)
 #else
 #define MIN_KEYS_PER_CRYPT		1
 #define MAX_KEYS_PER_CRYPT		0x100

src/aix_ssha_fmt_plug.c
@@ -71,23 +71,8 @@ static int omp_t = 1;
 #define MAX_SALT_SIZE		24
 #define SALT_SIZE		sizeof(struct custom_salt)
 #define SALT_ALIGN		4
-#ifdef SIMD_COEF_32
-// since we have a 'common' crypt_all() function, find 'max' of sha1/sha256/sha512, and that is the block size
-// crypt_all handles looping 'within' each OMP thread (or within the single thread if non OMP).
-#if SSE_GROUP_SZ_SHA1 > SSE_GROUP_SZ_SHA256 && SSE_GROUP_SZ_SHA1 > SSE_GROUP_SZ_SHA512
-#define MIN_KEYS_PER_CRYPT	SSE_GROUP_SZ_SHA1
-#define MAX_KEYS_PER_CRYPT	SSE_GROUP_SZ_SHA1
-#elif SSE_GROUP_SZ_SHA512 > SSE_GROUP_SZ_SHA256
-#define MIN_KEYS_PER_CRYPT	SSE_GROUP_SZ_SHA512
-#define MAX_KEYS_PER_CRYPT	SSE_GROUP_SZ_SHA512
-#else
-#define MIN_KEYS_PER_CRYPT	SSE_GROUP_SZ_SHA256
-#define MAX_KEYS_PER_CRYPT	SSE_GROUP_SZ_SHA256
-#endif
-#else
 #define MIN_KEYS_PER_CRYPT	1
 #define MAX_KEYS_PER_CRYPT	1
-#endif
 #define BASE64_ALPHABET	  \
 	"./0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 
@@ -123,6 +108,7 @@ static struct fmt_tests aixssha_tests512[] = {
 
 static char (*saved_key)[PLAINTEXT_LENGTH + 1];
 static ARCH_WORD_32 (*crypt_out)[BINARY_SIZE / sizeof(ARCH_WORD_32)];
+static unsigned int key_batch;
 
 static struct custom_salt {
 	int iterations;
@@ -132,6 +118,7 @@ static struct custom_salt {
 
 static void init(struct fmt_main *self)
 {
+	key_batch = self->params.max_keys_per_crypt;
 #ifdef _OPENMP
 	omp_t = omp_get_max_threads();
 	self->params.min_keys_per_crypt *= omp_t;
@@ -293,10 +280,10 @@ static int crypt_all(int *pcount, struct db_salt *salt)
 #ifdef _OPENMP
 #pragma omp parallel for
 #endif
-	for (index = 0; index < count; index += MAX_KEYS_PER_CRYPT)
+	for (index = 0; index < count; index += key_batch)
 	{
 		int j = index;
-		while (j < index + MAX_KEYS_PER_CRYPT) {
+		while (j < index + key_batch) {
 			if (cur_salt->type == 1) {
 #ifdef SSE_GROUP_SZ_SHA1
 				int lens[SSE_GROUP_SZ_SHA1], i;
@@ -428,8 +415,13 @@ struct fmt_main fmt_aixssha1 = {
 		BINARY_ALIGN,
 		SALT_SIZE,
 		SALT_ALIGN,
+#ifdef SIMD_COEF_32
+		SSE_GROUP_SZ_SHA1,
+		SSE_GROUP_SZ_SHA1,
+#else
 		MIN_KEYS_PER_CRYPT,
 		MAX_KEYS_PER_CRYPT,
+#endif
 		FMT_CASE | FMT_8_BIT | FMT_OMP,
 #if FMT_MAIN_VERSION > 11
 		{
@@ -496,8 +488,13 @@ struct fmt_main fmt_aixssha256 = {
 		BINARY_ALIGN,
 		SALT_SIZE,
 		SALT_ALIGN,
+#ifdef SIMD_COEF_32
+		SSE_GROUP_SZ_SHA256,
+		SSE_GROUP_SZ_SHA256,
+#else
 		MIN_KEYS_PER_CRYPT,
 		MAX_KEYS_PER_CRYPT,
+#endif
 		FMT_CASE | FMT_8_BIT | FMT_OMP,
 #if FMT_MAIN_VERSION > 11
 		{
@@ -564,8 +561,13 @@ struct fmt_main fmt_aixssha512 = {
 		BINARY_ALIGN,
 		SALT_SIZE,
 		SALT_ALIGN,
+#ifdef SIMD_COEF_64
+		SSE_GROUP_SZ_SHA512,
+		SSE_GROUP_SZ_SHA512,
+#else
 		MIN_KEYS_PER_CRYPT,
 		MAX_KEYS_PER_CRYPT,
+#endif
 		FMT_CASE | FMT_8_BIT | FMT_OMP,
 #if FMT_MAIN_VERSION > 11
 		{

src/bitcoin_fmt_plug.c
@@ -65,8 +65,8 @@ static int omp_t = 1;
 #define SALT_ALIGN			sizeof(int)
 #define SALT_SIZE		sizeof(struct custom_salt)
 #ifdef SIMD_COEF_64
-#define MIN_KEYS_PER_CRYPT	SIMD_COEF_64
-#define MAX_KEYS_PER_CRYPT	SIMD_COEF_64
+#define MIN_KEYS_PER_CRYPT	(SIMD_COEF_64*SIMD_PARA_SHA512)
+#define MAX_KEYS_PER_CRYPT	(SIMD_COEF_64*SIMD_PARA_SHA512)
 #else
 #define MIN_KEYS_PER_CRYPT	1
 #define MAX_KEYS_PER_CRYPT	1
@@ -261,7 +261,7 @@ static int crypt_all(int *pcount, struct db_salt *salt)
 		// the above alignment was crashing on OMP build on some 32 bit linux (compiler bug?? not aligning).
 		// so the alignment was done using raw buffer, and aligning at runtime to get 16 byte alignment.
 		// that works, and should cause no noticeable overhead differences.
-		char unaligned_buf[SIMD_COEF_64*SHA512_BUF_SIZ*sizeof(ARCH_WORD_64)+MEM_ALIGN_SIMD];
+		char unaligned_buf[MAX_KEYS_PER_CRYPT*SHA512_BUF_SIZ*sizeof(ARCH_WORD_64)+MEM_ALIGN_SIMD];
 		ARCH_WORD_64 *key_iv = (ARCH_WORD_64*)mem_align(unaligned_buf, MEM_ALIGN_SIMD);
 		JTR_ALIGN(8)  unsigned char hash1[SHA512_DIGEST_LENGTH];            // 512 bits
 		int index2;
@@ -278,17 +278,17 @@ static int crypt_all(int *pcount, struct db_salt *salt)
 			// out the rest of the buffer, putting 512 (#bits) at the end.  Once this part of the buffer is set up, we never
 			// touch it again, for the rest of the crypt.  We simply overwrite the first half of this buffer, over and over
 			// again, with BE results of the prior hash.
-			key_iv[ SHA512_DIGEST_LENGTH/sizeof(ARCH_WORD_64) * SIMD_COEF_64 + index2 ] = 0x8000000000000000ULL;
-			for (i = (SHA512_DIGEST_LENGTH/sizeof(ARCH_WORD_64)+1) * SIMD_COEF_64 + index2; i < 15*SIMD_COEF_64; i += SIMD_COEF_64)
-				key_iv[i] = 0;
-			key_iv[15*SIMD_COEF_64 + index2] = (SHA512_DIGEST_LENGTH << 3);
+			key_iv[ SHA512_DIGEST_LENGTH/sizeof(ARCH_WORD_64) * SIMD_COEF_64 + (index2&(SIMD_COEF_64-1)) + index2/SIMD_COEF_64*SHA512_BUF_SIZ*SIMD_COEF_64 ] = 0x8000000000000000ULL;
+			for (i = SHA512_DIGEST_LENGTH/sizeof(ARCH_WORD_64)+1; i < 15; i++)
+				key_iv[i*SIMD_COEF_64 + (index2&(SIMD_COEF_64-1)) + index2/SIMD_COEF_64*SHA512_BUF_SIZ*SIMD_COEF_64] = 0;
+			key_iv[15*SIMD_COEF_64 + (index2&(SIMD_COEF_64-1)) + index2/SIMD_COEF_64*SHA512_BUF_SIZ*SIMD_COEF_64] = (SHA512_DIGEST_LENGTH << 3);
 
 			// Now copy and convert hash1 from flat into SIMD_COEF_64 buffers.
 			for (i = 0; i < SHA512_DIGEST_LENGTH/sizeof(ARCH_WORD_64); ++i) {
 #if COMMON_DIGEST_FOR_OPENSSL
-				key_iv[SIMD_COEF_64*i + index2] = sha_ctx.hash[i];  // this is in BE format
+				key_iv[SIMD_COEF_64*i + (index2&(SIMD_COEF_64-1)) + index2/SIMD_COEF_64*SHA512_BUF_SIZ*SIMD_COEF_64] = sha_ctx.hash[i];  // this is in BE format
 #else
-				key_iv[SIMD_COEF_64*i + index2] = sha_ctx.h[i];
+				key_iv[SIMD_COEF_64*i + (index2&(SIMD_COEF_64-1)) + index2/SIMD_COEF_64*SHA512_BUF_SIZ*SIMD_COEF_64] = sha_ctx.h[i];
 #endif
 			}
 		}
@@ -306,9 +306,9 @@ static int crypt_all(int *pcount, struct db_salt *salt)
 
 			// Copy and convert from SIMD_COEF_64 buffers back into flat buffers
 			for (i = 0; i < sizeof(key)/sizeof(ARCH_WORD_64); i++)  // the derived key
-				((ARCH_WORD_64 *)key)[i] = key_iv[SIMD_COEF_64*i + index2];
+				((ARCH_WORD_64 *)key)[i] = key_iv[SIMD_COEF_64*i + (index2&(SIMD_COEF_64-1)) + index2/SIMD_COEF_64*SHA512_BUF_SIZ*SIMD_COEF_64];
 			for (i = 0; i < sizeof(iv)/sizeof(ARCH_WORD_64); i++)   // the derived iv
-				((ARCH_WORD_64 *)iv)[i]  = key_iv[SIMD_COEF_64*(sizeof(key)/sizeof(ARCH_WORD_64) + i) + index2];
+				((ARCH_WORD_64 *)iv)[i]  = key_iv[SIMD_COEF_64*(sizeof(key)/sizeof(ARCH_WORD_64) + i) + (index2&(SIMD_COEF_64-1)) + index2/SIMD_COEF_64*SHA512_BUF_SIZ*SIMD_COEF_64];
 
 			/* NOTE: write our code instead of using following high-level OpenSSL functions */
 			EVP_CIPHER_CTX_init(&ctx);

src/blackberry_ES10_fmt_plug.c
@@ -64,8 +64,8 @@ john_register_one(&fmt_blackberry1);
 #define SALT_SIZE		sizeof(struct custom_salt)
 #define SALT_ALIGN		4
 #ifdef SIMD_COEF_64
-#define MIN_KEYS_PER_CRYPT	SIMD_COEF_64
-#define MAX_KEYS_PER_CRYPT	SIMD_COEF_64
+#define MIN_KEYS_PER_CRYPT	(SIMD_COEF_64*SIMD_PARA_SHA512)
+#define MAX_KEYS_PER_CRYPT	(SIMD_COEF_64*SIMD_PARA_SHA512)
 #else
 #define MIN_KEYS_PER_CRYPT	1
 #define MAX_KEYS_PER_CRYPT	1

src/cryptsha256_fmt_plug.c
@@ -151,8 +151,8 @@ john_register_one(&fmt_cryptsha256);
 #define SALT_SIZE				sizeof(struct saltstruct)
 
 #ifdef SIMD_COEF_32
-#define MIN_KEYS_PER_CRYPT		SIMD_COEF_32
-#define MAX_KEYS_PER_CRYPT		SIMD_COEF_32
+#define MIN_KEYS_PER_CRYPT		(SIMD_COEF_32*SIMD_PARA_SHA256)
+#define MAX_KEYS_PER_CRYPT		(SIMD_COEF_32*SIMD_PARA_SHA256)
 #else
 #define MIN_KEYS_PER_CRYPT		1
 #define MAX_KEYS_PER_CRYPT		1
@@ -161,11 +161,7 @@ john_register_one(&fmt_cryptsha256);
 #define __CRYPTSHA256_CREATE_PROPER_TESTS_ARRAY__
 #include "cryptsha256_common.h"
 
-#ifndef SIMD_COEF_32
-#define BLKS 1
-#else
-#define BLKS SIMD_COEF_32
-#endif
+#define BLKS MAX_KEYS_PER_CRYPT
 
 /* This structure is 'pre-loaded' with the keyspace of all possible crypts which  */
 /* will be performed WITHIN the inner loop.  There are 8 possible buffers that    */
@@ -595,7 +591,7 @@ static int crypt_all(int *pcount, struct db_salt *salt)
 
 #ifdef SIMD_COEF_32
 	// group based upon size splits.
-	MixOrder = mem_calloc((count+6*SIMD_COEF_32), sizeof(int));
+	MixOrder = mem_calloc((count+6*MAX_KEYS_PER_CRYPT), sizeof(int));
 	{
 		static const int lens[17][6] = {
 			{0,12,24,38,39,40},  //  0 byte salt (down to 2 slots now, but probably NOT valid.)
@@ -660,7 +656,7 @@ static int crypt_all(int *pcount, struct db_salt *salt)
 		cryptloopstruct *crypt_struct;
 #ifdef SIMD_COEF_32
 		//JTR_ALIGN(MEM_ALIGN_SIMD) ARCH_WORD_32 sse_out[64];
-		char tmp_sse_out[8*SIMD_COEF_32*4+MEM_ALIGN_SIMD];
+		char tmp_sse_out[8*MAX_KEYS_PER_CRYPT*4+MEM_ALIGN_SIMD];
 		ARCH_WORD_32 *sse_out;
 		sse_out = (ARCH_WORD_32 *)mem_align(tmp_sse_out, MEM_ALIGN_SIMD);
 #endif
@@ -765,10 +761,10 @@ static int crypt_all(int *pcount, struct db_salt *salt)
 				break;
 			{
 				int j, k;
-				for (k = 0; k < SIMD_COEF_32; ++k) {
+				for (k = 0; k < MAX_KEYS_PER_CRYPT; ++k) {
 					ARCH_WORD_32 *o = (ARCH_WORD_32 *)crypt_struct->cptr[k][idx];
 					for (j = 0; j < 8; ++j)
-						*o++ = JOHNSWAP(sse_out[(j*SIMD_COEF_32)+k]);
+						*o++ = JOHNSWAP(sse_out[(j*SIMD_COEF_32)+(k&(SIMD_COEF_32-1))+k/SIMD_COEF_32*8*SIMD_COEF_32]);
 				}
 			}
 			if (++idx == 42)
@@ -776,10 +772,10 @@ static int crypt_all(int *pcount, struct db_salt *salt)
 		}
 		{
 			int j, k;
-			for (k = 0; k < SIMD_COEF_32; ++k) {
+			for (k = 0; k < MAX_KEYS_PER_CRYPT; ++k) {
 				ARCH_WORD_32 *o = (ARCH_WORD_32 *)crypt_out[MixOrder[index+k]];
 				for (j = 0; j < 8; ++j)
-					*o++ = JOHNSWAP(sse_out[(j*SIMD_COEF_32)+k]);
+					*o++ = JOHNSWAP(sse_out[(j*SIMD_COEF_32)+(k&(SIMD_COEF_32-1))+k/SIMD_COEF_32*8*SIMD_COEF_32]);
 			}
 		}
 #else

src/drupal7_fmt_plug.c
@@ -55,8 +55,8 @@ john_register_one(&fmt_drupal7);
 #define SALT_ALIGN			4
 
 #ifdef SIMD_COEF_64
-#define MIN_KEYS_PER_CRYPT      SIMD_COEF_64
-#define MAX_KEYS_PER_CRYPT      SIMD_COEF_64
+#define MIN_KEYS_PER_CRYPT      (SIMD_COEF_64*SIMD_PARA_SHA512)
+#define MAX_KEYS_PER_CRYPT      (SIMD_COEF_64*SIMD_PARA_SHA512)
 #define GETPOS(i, index)        ( (index&(SIMD_COEF_64-1))*8 + ((i)&(0xffffffff-7))*SIMD_COEF_64 + (7-((i)&7)) + (unsigned int)index/SIMD_COEF_64*SHA512_BUF_SIZ*SIMD_COEF_64*8 )
 #else
 #define MIN_KEYS_PER_CRYPT		1
@@ -193,7 +193,7 @@ static int crypt_all(int *pcount, struct db_salt *salt)
 			for (j = 0; j < len; ++j)
 				keys[GETPOS(j+8, i)] = EncKey[index+i][j];
 			keys[GETPOS(j+8, i)] = 0x80;
-			keys64[15*SIMD_COEF_64+i] = (len+8) << 3;
+			keys64[15*SIMD_COEF_64+(i&(SIMD_COEF_64-1))+i/SIMD_COEF_64*SHA512_BUF_SIZ*SIMD_COEF_64] = (len+8) << 3;
 		}
 		SSESHA512body(keys, keys64, NULL, SSEi_MIXED_IN);
 		for (i = 0; i < MAX_KEYS_PER_CRYPT; ++i) {
@@ -201,7 +201,7 @@ static int crypt_all(int *pcount, struct db_salt *salt)
 			for (j = 0; j < len; ++j)
 				keys[GETPOS(j+64, i)] = EncKey[index+i][j];
 			keys[GETPOS(j+64, i)] = 0x80;
-			keys64[15*SIMD_COEF_64+i] = (len+64) << 3;
+			keys64[15*SIMD_COEF_64+(i&(SIMD_COEF_64-1))+i/SIMD_COEF_64*SHA512_BUF_SIZ*SIMD_COEF_64] = (len+64) << 3;
 		}
 		do {
 			SSESHA512body(keys, keys64, NULL, SSEi_MIXED_IN);
@@ -210,8 +210,7 @@ static int crypt_all(int *pcount, struct db_salt *salt)
 		for (i = 0; i < MAX_KEYS_PER_CRYPT; ++i) {
 			crypt = (ARCH_WORD_64*)crypt_key[index+i];
 			for (j = 0; j < 8; ++j)
-				crypt[j] = JOHNSWAP64(keys64[j*SIMD_COEF_64]);
-			++keys64;
+				crypt[j] = JOHNSWAP64(keys64[j*SIMD_COEF_64+(i&(SIMD_COEF_64-1))+i/SIMD_COEF_64*SHA512_BUF_SIZ*SIMD_COEF_64]);
 		}
 #else
 		SHA512_CTX ctx;

commit 7e8050b5e4cea3ffd1170c34dca1511c79bbc8ac

Fix non-OpenMP build problems introduced with b96ed88f.

src/mssql12_fmt_plug.c
@@ -48,7 +48,6 @@ john_register_one(&fmt_mssql12);
 #include "sse-intrinsics.h"
 #include "memdbg.h"
 #ifdef _OPENMP
-static int omp_t = 1;
 #include <omp.h>
 #ifdef SIMD_COEF_64
 #define OMP_SCALE               2048
@@ -79,6 +78,8 @@ static int omp_t = 1;
 #define MIN_KEYS_PER_CRYPT      1
 #define MAX_KEYS_PER_CRYPT      1
 #endif
+
+static int omp_t = MAX_KEYS_PER_CRYPT;
 
 #undef MIN
 #define MIN(a, b)               (((a) > (b)) ? (b) : (a))

src/rawSHA224_fmt_plug.c
@@ -72,6 +72,8 @@ john_register_one(&fmt_rawSHA224);
 #define MAX_KEYS_PER_CRYPT		1
 #endif
 
+static int omp_t = MAX_KEYS_PER_CRYPT;
+
 static struct fmt_tests tests[] = {
 	{"d63dc919e201d7bc4c825630d2cf25fdc93d4b2f0d46706d29038d01", "password"},
 	{"$SHA224$d63dc919e201d7bc4c825630d2cf25fdc93d4b2f0d46706d29038d01", "password"},
@@ -94,8 +96,6 @@ static ARCH_WORD_32 (*crypt_out)
 static void init(struct fmt_main *self)
 {
 #ifdef _OPENMP
-	int omp_t;
-
 	omp_t = omp_get_max_threads();
 	self->params.min_keys_per_crypt *= omp_t;
 	omp_t *= OMP_SCALE;