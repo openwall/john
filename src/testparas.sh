176f001ee9a00382771653ec3e10e86d90e005c2
magnum <john.magnum@hushmail.com>
2015-05-28 09:10:34 +0200

    Add a testparas.sh script. See #1297.

---
+++ src/testparas.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+for i in 1 2 3 4 5; do
+	CPPFLAGS="-DMD4_SSE_PARA=$i -DMD5_SSE_PARA=$i -DSHA1_SSE_PARA=$i -DSIMD_PARA_SHA256=$i -DSIMD_PARA_SHA512=$i"
+	./configure CPPFLAGS="$CPPFLAGS" --disable-cuda --disable-opencl --disable-openmp >/dev/null || break
+	make -s clean || break
+	make -sj4 || break
+	echo
+	echo -n "\n===== Speeds for ${i}x interleaving: ====="
+	for j in nt md5crypt pbkdf2-hmac-sha1 pbkdf2-hmac-sha256 pbkdf2-hmac-sha512; do
+		../run/john -test -form:$j
+	done
+done
