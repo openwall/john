openwall/john

commit 87fab5b86bbbc846225a3654011632014f49b221

XSHA512:  fixed failures in non SSE builds

src/XSHA512_fmt_plug.c
@@ -45,7 +45,7 @@ john_register_one(&fmt_XSHA512);
 #define MAX_KEYS_PER_CRYPT      (SIMD_COEF_64*SIMD_PARA_SHA512)
 #else
 #define MIN_KEYS_PER_CRYPT		1
-#define MAX_KEYS_PER_CRYPT		0x100
+#define MAX_KEYS_PER_CRYPT		1
 #endif
 
 #if ARCH_BITS >= 64 || defined(__SSE2__)

commit 8c53b0d561084fad90a152ac27f46a711b1be30c

Fix a minor OMP_SCALE regression from 87fab5b.

src/XSHA512_fmt_plug.c
@@ -25,7 +25,7 @@ john_register_one(&fmt_XSHA512);
 #ifdef SIMD_COEF_64
 #define OMP_SCALE               4096
 #else
-#define OMP_SCALE               64
+#define OMP_SCALE               8192
 #endif
 #endif
 