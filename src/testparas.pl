8ee68431b7612fa96b1464ade5044dc7193a4174
magnum <john.magnum@hushmail.com>
2015-06-04 02:48:08 +0200

    Add non-OMP figures in testparas.pl

--- src/testparas.pl
+++ src/testparas.pl
@@ -7,50 +7,64 @@ use strict;
 my $compiler = `gcc -v 2>&1 | tail -1` or die;
 print $compiler;
 
-my ($i, $j);
+my ($i, $j, $k);
 my %speed;
 my %best;
 
 foreach $i (1..5)
 {
 	my $CPPFLAGS="-DSIMD_PARA_MD4=$i -DSIMD_PARA_MD5=$i -DSIMD_PARA_SHA1=$i -DSIMD_PARA_SHA256=$i -DSIMD_PARA_SHA512=$i -DOMP_SCALE=1";
-	print `./configure CPPFLAGS="$CPPFLAGS" --disable-cuda --disable-opencl >/dev/null` or die;
+	print `./configure CPPFLAGS="$CPPFLAGS" --disable-cuda --disable-opencl --enable-openmp-for-fast-formats >/dev/null` or die;
 	print `make -s clean` or die;
 	print `make -sj4` or die;
 
 	print "\n===== Speeds for ${i}x interleaving: =====\n";
 	foreach $j (qw(nt md5crypt pbkdf2-hmac-sha1 pbkdf2-hmac-sha256 pbkdf2-hmac-sha512))
 	{
-		my $out = `../run/john -test -form:$j` or die;
+		my $out = `OMP_NUM_THREADS=1 ../run/john -test -form:$j` or die;
 		print $out;
 		$out =~ s/.*^Raw:\t(\d+K?).*/$1/ms;
 		$speed{$j}{$i} = $out;
 		my $sp = $out;
 		$sp =~ s/K/000/;
 		if (!defined $best{$j}{"speed"} || ($best{$j}{"speed"} < $sp))
-		   {
+		{
 			$best{$j}{"speed"} = $sp;
 			$best{$j}{"para"} = $i;
+		}
+		$out = `../run/john -test -form:$j` or die;
+		print $out;
+		$out =~ s/.*^Raw:\t(\d+K?).*/$1/ms;
+		$speed{$j."-omp"}{$i} = $out;
+		$sp = $out;
+		$sp =~ s/K/000/;
+		if (!defined $best{$j."-omp"}{"speed"} || ($best{$j."-omp"}{"speed"} < $sp))
+		{
+			$best{$j."-omp"}{"speed"} = $sp;
+			$best{$j."-omp"}{"para"} = $i;
 		}
 	}
 }
 
 print "\n", $compiler, "\n";
-printf "%-18s |  %6d  |  %6d  |  %6d  |  %6d  |  %6d  |\n", "hash\\para", 1, 2, 3, 4, 5;
-print "-------------------|----------|----------|----------|----------|----------|\n";
+printf "%-22s |  %6d  |  %6d  |  %6d  |  %6d  |  %6d  |\n", "hash\\para", 1, 2, 3, 4, 5;
+print "-----------------------|----------|----------|----------|----------|----------|\n";
 foreach $j (qw(nt md5crypt pbkdf2-hmac-sha1 pbkdf2-hmac-sha256 pbkdf2-hmac-sha512))
 {
-	printf "%-18s |", $j;
-	foreach $i (1..5)
+	foreach $k ("", "-omp")
 	{
-		if ($best{$j}{"para"} == $i)
+		printf "%-22s |", $j.$k;
+		foreach $i (1..5)
 		{
-			$speed{$j}{$i} = "**".$speed{$j}{$i}."**";
-			printf "%10s|", $speed{$j}{$i};
-		} else {
-			printf "  %6s  |", $speed{$j}{$i};
+			if ($best{$j.$k}{"para"} == $i)
+			{
+				$speed{$j.$k}{$i} = "**".$speed{$j.$k}{$i}."**";
+				printf "%10s|", $speed{$j.$k}{$i};
+			} else {
+				printf "  %6s  |", $speed{$j.$k}{$i};
+			}
 		}
+		print "\n";
 	}
-	print "\n";
 }
 print "\n";
