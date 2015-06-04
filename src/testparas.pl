a42a4ef31e669235461027dbc1e858e3586acbd7
magnum <john.magnum@hushmail.com>
2015-06-04 14:26:14 +0200

    Improve testparas.pl for quicker run and real non-OMP figures. This is very hacky but does the job.

--- src/testparas.pl
+++ src/testparas.pl
@@ -8,30 +8,27 @@ my $compiler = `gcc -v 2>&1 | tail -1` or die;
 print $compiler;
 
 my ($i, $j, $k);
+my ($out, $sp);
 my %speed;
 my %best;
 
+print "This will take a while.\n";
+print "Initial configure...\n";
+print `./configure --disable-cuda --disable-opencl --enable-openmp-for-fast-formats >/dev/null` or die;
+print `make -s clean` or die;
+print "Initial build...\n";
+
 foreach $i (1..5)
 {
-	my $CPPFLAGS="-DSIMD_PARA_MD4=$i -DSIMD_PARA_MD5=$i -DSIMD_PARA_SHA1=$i -DSIMD_PARA_SHA256=$i -DSIMD_PARA_SHA512=$i -DOMP_SCALE=1";
-	print `./configure CPPFLAGS="$CPPFLAGS" --disable-cuda --disable-opencl --enable-openmp-for-fast-formats >/dev/null` or die;
-	print `make -s clean` or die;
-	print `make -sj4` or die;
+	print `rm -f sse-intrinsics.o nt2_fmt_plug.o MD5_*o pbkdf2*fmt*o dynamic*o ../run/john` or die;
+	my $CPPFLAGS="-DSIMD_PARA_MD4=$i -DSIMD_PARA_MD5=$i -DSIMD_PARA_SHA1=$i -DSIMD_PARA_SHA256=$i -DSIMD_PARA_SHA512=$i";
+	print `make -sj4 CPPFLAGS="$CPPFLAGS" nt2_fmt_plug.o` or die;
+	$CPPFLAGS="-DSIMD_PARA_MD4=$i -DSIMD_PARA_MD5=$i -DSIMD_PARA_SHA1=$i -DSIMD_PARA_SHA256=$i -DSIMD_PARA_SHA512=$i -DOMP_SCALE=1";
+	print `make -sj4 CPPFLAGS="$CPPFLAGS"` or die;
 
-	print "\n===== Speeds for ${i}x interleaving: =====\n";
+	print "\n== Speeds for ${i}x interleaving (OMP_SCALE 1 except NT): ==\n";
 	foreach $j (qw(nt md5crypt pbkdf2-hmac-sha1 pbkdf2-hmac-sha256 pbkdf2-hmac-sha512))
 	{
-		my $out = `OMP_NUM_THREADS=1 ../run/john -test -form:$j` or die;
-		print $out;
-		$out =~ s/.*^Raw:\t(\d+K?).*/$1/ms;
-		$speed{$j}{$i} = $out;
-		my $sp = $out;
-		$sp =~ s/K/000/;
-		if (!defined $best{$j}{"speed"} || ($best{$j}{"speed"} < $sp))
-		{
-			$best{$j}{"speed"} = $sp;
-			$best{$j}{"para"} = $i;
-		}
 		$out = `../run/john -test -form:$j` or die;
 		print $out;
 		$out =~ s/.*^Raw:\t(\d+K?).*/$1/ms;
@@ -42,6 +39,24 @@ foreach $i (1..5)
 		{
 			$best{$j."-omp"}{"speed"} = $sp;
 			$best{$j."-omp"}{"para"} = $i;
+		}
+	}
+	print `rm nt2_fmt_plug.o MD5_*o pbkdf2*fmt*o dynamic_fmt.o ../run/john` or die;
+	$CPPFLAGS="-DSIMD_PARA_MD4=$i -DSIMD_PARA_MD5=$i -DSIMD_PARA_SHA1=$i -DSIMD_PARA_SHA256=$i -DSIMD_PARA_SHA512=$i -U_OPENMP";
+	print `make -sj4 CPPFLAGS="$CPPFLAGS"` or die;
+	print "\n===== Speeds for ${i}x interleaving (no OMP): =====\n";
+	foreach $j (qw(nt md5crypt pbkdf2-hmac-sha1 pbkdf2-hmac-sha256 pbkdf2-hmac-sha512))
+	{
+		$out = `../run/john -test -form:$j` or die;
+		print $out;
+		$out =~ s/.*^Raw:\t(\d+K?).*/$1/ms;
+		$speed{$j}{$i} = $out;
+		$sp = $out;
+		$sp =~ s/K/000/;
+		if (!defined $best{$j}{"speed"} || ($best{$j}{"speed"} < $sp))
+		{
+			$best{$j}{"speed"} = $sp;
+			$best{$j}{"para"} = $i;
 		}
 	}
 }
