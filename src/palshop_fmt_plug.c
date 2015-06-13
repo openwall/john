13c6b4466087afb15ca3c2c17304d277c8dd1367
Dhiru Kholia <dhiru.kholia@gmail.com>
2015-06-14 00:53:01 +0200

    InsidePro inspired formats

---
+++ src/palshop_fmt_plug.c
@@ -0,0 +1,292 @@
+/* Collection of miscellaneous formats, inspired from InsidePro!
+ *
+ * Copyright (c) 2015 Dhiru Kholia <dhiru.kholia@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#if FMT_EXTERNS_H
+extern struct fmt_main fmt_palshop;
+#elif FMT_REGISTERS_H
+john_register_one(&fmt_palshop);
+#else
+
+#include "arch.h"
+#include "sha.h"
+#include "md5.h"
+
+#include <string.h>
+#include <assert.h>
+#include <errno.h>
+#include "misc.h"
+#include "common.h"
+#include "formats.h"
+#include "params.h"
+#include "options.h"
+#ifdef _OPENMP
+#include <omp.h>
+#ifndef OMP_SCALE
+#define OMP_SCALE               8 // tuned on core i7
+#endif
+#endif
+#include "memdbg.h"
+
+#define FORMAT_LABEL		"Palshop"
+#define FORMAT_NAME		"MD5(Palshop)"
+#define ALGORITHM_NAME		"MD5 + SHA1 32/" ARCH_BITS_STR
+#define BENCHMARK_COMMENT	""
+#define BENCHMARK_LENGTH	-1
+#define PLAINTEXT_LENGTH	125
+#define BINARY_SIZE		20  /* 20 chracters of "m2" */
+#define SALT_SIZE		0
+#define BINARY_ALIGN		sizeof(ARCH_WORD_32)
+#define SALT_ALIGN		sizeof(int)
+#define MIN_KEYS_PER_CRYPT	1
+#define MAX_KEYS_PER_CRYPT	64
+#define FORMAT_TAG              "$palshop$"
+#define TAG_LENGTH              (sizeof(FORMAT_TAG) - 1)
+
+
+static struct fmt_tests palshop_tests[] = {
+	{"$palshop$68b11ee90ed17ef14aa0f51af494c2c63ad7d281a9888cb593e", "123"},
+	{NULL}
+};
+
+static char (*saved_key)[PLAINTEXT_LENGTH + 1];
+static ARCH_WORD_32 (*crypt_out)[BINARY_SIZE / sizeof(ARCH_WORD_32)];
+
+static inline void hex_encode(unsigned char *str, int len, unsigned char *out)
+{
+	int i;
+	for (i = 0; i < len; ++i) {
+		out[0] = itoa16[str[i]>>4];
+		out[1] = itoa16[str[i]&0xF];
+		out += 2;
+	}
+}
+
+static void init(struct fmt_main *self)
+{
+#ifdef _OPENMP
+	static int omp_t = 1;
+	omp_t = omp_get_max_threads();
+	self->params.min_keys_per_crypt *= omp_t;
+	omp_t *= OMP_SCALE;
+	self->params.max_keys_per_crypt *= omp_t;
+#endif
+	saved_key = mem_calloc(self->params.max_keys_per_crypt,
+			sizeof(*saved_key));
+	crypt_out = mem_calloc(self->params.max_keys_per_crypt,
+			sizeof(*crypt_out));
+}
+
+static void done(void)
+{
+	MEM_FREE(crypt_out);
+	MEM_FREE(saved_key);
+}
+
+static int valid(char *ciphertext, struct fmt_main *self)
+{
+	char *p = ciphertext;
+	if (!strncmp(ciphertext, FORMAT_TAG, TAG_LENGTH))
+		p = ciphertext + TAG_LENGTH;
+
+	if(!p)
+		return 0;
+	if (!ishex(p))
+		return 0;
+
+	if (strlen(p) != 51)
+		return 0;
+
+	return 1;
+}
+
+static void *get_binary(char *ciphertext)
+{
+	static union {
+		unsigned char c[BINARY_SIZE+1];
+		ARCH_WORD dummy;
+	} buf;
+	unsigned char *out = buf.c;
+	char *p = ciphertext;
+
+	if (!strncmp(ciphertext, FORMAT_TAG, TAG_LENGTH))
+		p = ciphertext + TAG_LENGTH;
+
+	memcpy(out, p, BINARY_SIZE);
+
+	return out;
+}
+
+static int get_hash_0(int index) { return crypt_out[index][0] & 0xf; }
+static int get_hash_1(int index) { return crypt_out[index][0] & 0xff; }
+static int get_hash_2(int index) { return crypt_out[index][0] & 0xfff; }
+static int get_hash_3(int index) { return crypt_out[index][0] & 0xffff; }
+static int get_hash_4(int index) { return crypt_out[index][0] & 0xfffff; }
+static int get_hash_5(int index) { return crypt_out[index][0] & 0xffffff; }
+static int get_hash_6(int index) { return crypt_out[index][0] & 0x7ffffff; }
+
+static int crypt_all(int *pcount, struct db_salt *salt)
+{
+	const int count = *pcount;
+	int index = 0;
+#ifdef _OPENMP
+#pragma omp parallel for
+#endif
+	for (index = 0; index < count; index++)
+	{
+		unsigned char m1[32];
+		unsigned char s1[40];
+		unsigned char m2[32];
+		// unsigned char s2[40];
+		unsigned char data[51 + 1] = {0};
+		unsigned char buffer[20];
+		MD5_CTX mctx;
+		SHA_CTX sctx;
+
+		// m1 = md5($p)
+		MD5_Init(&mctx);
+		MD5_Update(&mctx, saved_key[index], strlen(saved_key[index]));
+		MD5_Final(buffer, &mctx);
+		hex_encode(buffer, 16, m1);
+
+		// s1 = sha1($p)
+		SHA1_Init(&sctx);
+		SHA1_Update(&sctx, saved_key[index], strlen(saved_key[index]));
+		SHA1_Final((unsigned char*)crypt_out[index], &sctx);
+		hex_encode((unsigned char*)crypt_out[index], 20, s1);
+
+		// data = m1[11:] + s1[:29] + m1[0:1]  // 51 bytes!
+		memcpy(data     , m1 + 11, 32 - 11);
+		memcpy(data + 21, s1     , 29     );
+		data[50] = m1[0];
+
+		// m2 = md5(data)
+		MD5_Init(&mctx);
+		MD5_Update(&mctx, data, 51);
+		MD5_Final(buffer, &mctx);
+		hex_encode(buffer, 16, m2);
+
+		// s2 = sha1(data)
+		// SHA1_Init(&sctx);
+		// SHA1_Update(&sctx, data, 51);
+		// SHA1_Final((unsigned char*)crypt_out[index], &sctx);
+		// hex_encode((unsigned char*)crypt_out[index], 20, s1);
+
+		// hash =  m2[11:] + s2[:29] + m2[0], but starting 20 bytes should be enough!
+		memcpy((unsigned char*)crypt_out[index], m2 + 11, 20);
+	}
+	return count;
+}
+
+static int cmp_all(void *binary, int count)
+{
+	int index = 0;
+	for (; index < count; index++)
+		if (*((ARCH_WORD_32*)binary) == crypt_out[index][0])
+			return 1;
+	return 0;
+}
+
+static int cmp_one(void *binary, int index)
+{
+	return !memcmp(binary, crypt_out[index], BINARY_SIZE);
+}
+
+static int cmp_exact(char *source, int index)
+{
+	return 1;
+}
+
+static void palshop_set_key(char *key, int index)
+{
+	int saved_len = strlen(key);
+	if (saved_len > PLAINTEXT_LENGTH)
+		saved_len = PLAINTEXT_LENGTH;
+	memcpy(saved_key[index], key, saved_len);
+	saved_key[index][saved_len] = 0;
+}
+
+static char *get_key(int index)
+{
+	return saved_key[index];
+}
+
+struct fmt_main fmt_palshop = {
+	{
+		FORMAT_LABEL,
+		FORMAT_NAME,
+		ALGORITHM_NAME,
+		BENCHMARK_COMMENT,
+		BENCHMARK_LENGTH,
+		0,
+		PLAINTEXT_LENGTH,
+		BINARY_SIZE,
+		BINARY_ALIGN,
+		SALT_SIZE,
+		SALT_ALIGN,
+		MIN_KEYS_PER_CRYPT,
+		MAX_KEYS_PER_CRYPT,
+		FMT_CASE | FMT_8_BIT | FMT_OMP,
+#if FMT_MAIN_VERSION > 11
+		{ NULL },
+#endif
+		palshop_tests
+	}, {
+		init,
+		done,
+		fmt_default_reset,
+		fmt_default_prepare,
+		valid,
+		fmt_default_split,
+		get_binary,
+		fmt_default_salt,
+#if FMT_MAIN_VERSION > 11
+		{ NULL },
+#endif
+		fmt_default_source,
+		{
+			fmt_default_binary_hash_0,
+			fmt_default_binary_hash_1,
+			fmt_default_binary_hash_2,
+			fmt_default_binary_hash_3,
+			fmt_default_binary_hash_4,
+			fmt_default_binary_hash_5,
+			fmt_default_binary_hash_6
+		},
+		fmt_default_salt_hash,
+		NULL,
+		fmt_default_set_salt,
+		palshop_set_key,
+		get_key,
+		fmt_default_clear_keys,
+		crypt_all,
+		{
+			get_hash_0,
+			get_hash_1,
+			get_hash_2,
+			get_hash_3,
+			get_hash_4,
+			get_hash_5,
+			get_hash_6
+		},
+		cmp_all,
+		cmp_one,
+		cmp_exact
+	}
+};
+
+#endif /* plugin stanza */
---
+++ src/ventrilo_fmt_plug.c
@@ -0,0 +1,385 @@
+/*
+
+Ventrilo password hashing algorithm 0.1
+by Luigi Auriemma
+e-mail: aluigi@autistici.org
+web:    aluigi.org
+
+This algorithm is the method used by the chat program Ventrilo
+(http://www.ventrilo.com) for calculating the password hash used from
+version 2.3.0.
+
+Copyright 2004,2005,2006 Luigi Auriemma
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+
+http://www.gnu.org/licenses/gpl.txt
+
+*/
+
+#if FMT_EXTERNS_H
+extern struct fmt_main fmt_ventrilo;
+#elif FMT_REGISTERS_H
+john_register_one(&fmt_ventrilo);
+#else
+
+#include "arch.h"
+#include "sha.h"
+#include "md5.h"
+
+#include <string.h>
+#include <assert.h>
+#include <errno.h>
+#include "misc.h"
+#include "common.h"
+#include "formats.h"
+#include "params.h"
+#include "options.h"
+#ifdef _OPENMP
+#include <omp.h>
+#ifndef OMP_SCALE
+#define OMP_SCALE               8 // tuned on core i7
+#endif
+#endif
+#include "memdbg.h"
+
+#define FORMAT_LABEL		"ventrilo"
+#define FORMAT_NAME		""
+#define ALGORITHM_NAME		"Some CRC"
+#define BENCHMARK_COMMENT	""
+#define BENCHMARK_LENGTH	-1
+#define PLAINTEXT_LENGTH	125
+#define BINARY_SIZE		32
+#define SALT_SIZE		0
+#define BINARY_ALIGN		sizeof(ARCH_WORD_32)
+#define SALT_ALIGN		sizeof(int)
+#define MIN_KEYS_PER_CRYPT	1
+#define MAX_KEYS_PER_CRYPT	64
+#define FORMAT_TAG              "$ventrilo$"
+#define TAG_LENGTH              (sizeof(FORMAT_TAG) - 1)
+
+static struct fmt_tests ventrilo_tests[] = {
+	{"$ventrilo$427d82b83add3aa92c16e99736918555a729e862ddb0421d9adbbb3cd6a19c51", "ABC"},
+	{NULL}
+};
+
+static char (*saved_key)[PLAINTEXT_LENGTH + 1];
+static ARCH_WORD_32 (*crypt_out)[BINARY_SIZE / sizeof(ARCH_WORD_32)];
+
+const static unsigned int crc_table[] = {
+        0x00000000, 0x77073096, 0xee0e612c, 0x990951ba,
+        0x076dc419, 0x706af48f, 0xe963a535, 0x9e6495a3,
+        0x0edb8832, 0x79dcb8a4, 0xe0d5e91e, 0x97d2d988,
+        0x09b64c2b, 0x7eb17cbd, 0xe7b82d07, 0x90bf1d91,
+        0x1db71064, 0x6ab020f2, 0xf3b97148, 0x84be41de,
+        0x1adad47d, 0x6ddde4eb, 0xf4d4b551, 0x83d385c7,
+        0x136c9856, 0x646ba8c0, 0xfd62f97a, 0x8a65c9ec,
+        0x14015c4f, 0x63066cd9, 0xfa0f3d63, 0x8d080df5,
+        0x3b6e20c8, 0x4c69105e, 0xd56041e4, 0xa2677172,
+        0x3c03e4d1, 0x4b04d447, 0xd20d85fd, 0xa50ab56b,
+        0x35b5a8fa, 0x42b2986c, 0xdbbbc9d6, 0xacbcf940,
+        0x32d86ce3, 0x45df5c75, 0xdcd60dcf, 0xabd13d59,
+        0x26d930ac, 0x51de003a, 0xc8d75180, 0xbfd06116,
+        0x21b4f4b5, 0x56b3c423, 0xcfba9599, 0xb8bda50f,
+        0x2802b89e, 0x5f058808, 0xc60cd9b2, 0xb10be924,
+        0x2f6f7c87, 0x58684c11, 0xc1611dab, 0xb6662d3d,
+        0x76dc4190, 0x01db7106, 0x98d220bc, 0xefd5102a,
+        0x71b18589, 0x06b6b51f, 0x9fbfe4a5, 0xe8b8d433,
+        0x7807c9a2, 0x0f00f934, 0x9609a88e, 0xe10e9818,
+        0x7f6a0dbb, 0x086d3d2d, 0x91646c97, 0xe6635c01,
+        0x6b6b51f4, 0x1c6c6162, 0x856530d8, 0xf262004e,
+        0x6c0695ed, 0x1b01a57b, 0x8208f4c1, 0xf50fc457,
+        0x65b0d9c6, 0x12b7e950, 0x8bbeb8ea, 0xfcb9887c,
+        0x62dd1ddf, 0x15da2d49, 0x8cd37cf3, 0xfbd44c65,
+        0x4db26158, 0x3ab551ce, 0xa3bc0074, 0xd4bb30e2,
+        0x4adfa541, 0x3dd895d7, 0xa4d1c46d, 0xd3d6f4fb,
+        0x4369e96a, 0x346ed9fc, 0xad678846, 0xda60b8d0,
+        0x44042d73, 0x33031de5, 0xaa0a4c5f, 0xdd0d7cc9,
+        0x5005713c, 0x270241aa, 0xbe0b1010, 0xc90c2086,
+        0x5768b525, 0x206f85b3, 0xb966d409, 0xce61e49f,
+        0x5edef90e, 0x29d9c998, 0xb0d09822, 0xc7d7a8b4,
+        0x59b33d17, 0x2eb40d81, 0xb7bd5c3b, 0xc0ba6cad,
+        0xedb88320, 0x9abfb3b6, 0x03b6e20c, 0x74b1d29a,
+        0xead54739, 0x9dd277af, 0x04db2615, 0x73dc1683,
+        0xe3630b12, 0x94643b84, 0x0d6d6a3e, 0x7a6a5aa8,
+        0xe40ecf0b, 0x9309ff9d, 0x0a00ae27, 0x7d079eb1,
+        0xf00f9344, 0x8708a3d2, 0x1e01f268, 0x6906c2fe,
+        0xf762575d, 0x806567cb, 0x196c3671, 0x6e6b06e7,
+        0xfed41b76, 0x89d32be0, 0x10da7a5a, 0x67dd4acc,
+        0xf9b9df6f, 0x8ebeeff9, 0x17b7be43, 0x60b08ed5,
+        0xd6d6a3e8, 0xa1d1937e, 0x38d8c2c4, 0x4fdff252,
+        0xd1bb67f1, 0xa6bc5767, 0x3fb506dd, 0x48b2364b,
+        0xd80d2bda, 0xaf0a1b4c, 0x36034af6, 0x41047a60,
+        0xdf60efc3, 0xa867df55, 0x316e8eef, 0x4669be79,
+        0xcb61b38c, 0xbc66831a, 0x256fd2a0, 0x5268e236,
+        0xcc0c7795, 0xbb0b4703, 0x220216b9, 0x5505262f,
+        0xc5ba3bbe, 0xb2bd0b28, 0x2bb45a92, 0x5cb36a04,
+        0xc2d7ffa7, 0xb5d0cf31, 0x2cd99e8b, 0x5bdeae1d,
+        0x9b64c2b0, 0xec63f226, 0x756aa39c, 0x026d930a,
+        0x9c0906a9, 0xeb0e363f, 0x72076785, 0x05005713,
+        0x95bf4a82, 0xe2b87a14, 0x7bb12bae, 0x0cb61b38,
+        0x92d28e9b, 0xe5d5be0d, 0x7cdcefb7, 0x0bdbdf21,
+        0x86d3d2d4, 0xf1d4e242, 0x68ddb3f8, 0x1fda836e,
+        0x81be16cd, 0xf6b9265b, 0x6fb077e1, 0x18b74777,
+        0x88085ae6, 0xff0f6a70, 0x66063bca, 0x11010b5c,
+        0x8f659eff, 0xf862ae69, 0x616bffd3, 0x166ccf45,
+        0xa00ae278, 0xd70dd2ee, 0x4e048354, 0x3903b3c2,
+        0xa7672661, 0xd06016f7, 0x4969474d, 0x3e6e77db,
+        0xaed16a4a, 0xd9d65adc, 0x40df0b66, 0x37d83bf0,
+        0xa9bcae53, 0xdebb9ec5, 0x47b2cf7f, 0x30b5ffe9,
+        0xbdbdf21c, 0xcabac28a, 0x53b39330, 0x24b4a3a6,
+        0xbad03605, 0xcdd70693, 0x54de5729, 0x23d967bf,
+        0xb3667a2e, 0xc4614ab8, 0x5d681b02, 0x2a6f2b94,
+        0xb40bbe37, 0xc30c8ea1, 0x5a05df1b, 0x2d02ef8d
+};
+
+#if ARCH_LITTLE_ENDIAN
+#define john_htonl(x) ((((x)>>24) & 0xffL) | (((x)>>8) & 0xff00L) | \
+                (((x)<<8) & 0xff0000L) | (((x)<<24) & 0xff000000L))
+#else
+#define john_htonl(x) (x)
+#endif
+
+
+void ventrilo_pwd_hash(unsigned char *pwd, unsigned char *out) {
+	unsigned int    crc;
+	int             i,
+			j,
+			edi,
+			cnt,
+			len,
+			chr;
+
+	union {
+		unsigned char c[4 + 1];
+		ARCH_WORD dummy;
+	} buf;
+	unsigned char *tmp = buf.c;
+	unsigned char *lptr;
+
+	len = strlen((const char*)pwd);
+	buf.dummy = 0;
+	lptr = out;
+
+	edi = 1;
+	cnt = len;
+	for(i = 0; i < 32; i++) {
+		if (i < len) {
+			chr = *pwd++;
+		} else {
+			chr = ((tmp[(cnt + 1) & 3] + *lptr) - 0x3f) & 0x7f;
+			lptr++;
+		}
+
+		out[i] = chr;
+		crc = 0;
+		for (j = 0; j < edi; j++) {
+			crc = crc_table[out[j] ^ (crc & 0xff)] ^ (crc >> 8);
+		}
+		*(unsigned int *)tmp = john_htonl(crc);
+
+		cnt += chr;
+		if (crc) {
+			while (!tmp[cnt & 3]) cnt++;
+		}
+
+		out[i] += tmp[cnt & 3];
+		cnt++;
+		edi++;
+	}
+}
+
+static void init(struct fmt_main *self)
+{
+#ifdef _OPENMP
+	static int omp_t = 1;
+	omp_t = omp_get_max_threads();
+	self->params.min_keys_per_crypt *= omp_t;
+	omp_t *= OMP_SCALE;
+	self->params.max_keys_per_crypt *= omp_t;
+#endif
+	saved_key = mem_calloc(self->params.max_keys_per_crypt,
+			sizeof(*saved_key));
+	crypt_out = mem_calloc(self->params.max_keys_per_crypt,
+			sizeof(*crypt_out));
+}
+
+static void done(void)
+{
+	MEM_FREE(crypt_out);
+	MEM_FREE(saved_key);
+}
+
+static int valid(char *ciphertext, struct fmt_main *self)
+{
+	char *p = ciphertext;
+	if (!strncmp(ciphertext, FORMAT_TAG, TAG_LENGTH))
+		p = ciphertext + TAG_LENGTH;
+
+	if(!p)
+		return 0;
+	if (!ishex(p))
+		return 0;
+
+	if (strlen(p) != BINARY_SIZE * 2)
+		return 0;
+
+	return 1;
+}
+
+static void *get_binary(char *ciphertext)
+{
+	static union {
+		unsigned char c[BINARY_SIZE+1];
+		ARCH_WORD dummy;
+	} buf;
+	unsigned char *out = buf.c;
+	char *p = ciphertext;
+	int i;
+
+	if (!strncmp(ciphertext, FORMAT_TAG, TAG_LENGTH))
+		p = ciphertext + TAG_LENGTH;
+
+	for (i = 0; i < BINARY_SIZE; i++) {
+		out[i] =
+			(atoi16[ARCH_INDEX(*p)] << 4) |
+			atoi16[ARCH_INDEX(p[1])];
+		p += 2;
+	}
+
+	return out;
+}
+
+static int get_hash_0(int index) { return crypt_out[index][0] & 0xf; }
+static int get_hash_1(int index) { return crypt_out[index][0] & 0xff; }
+static int get_hash_2(int index) { return crypt_out[index][0] & 0xfff; }
+static int get_hash_3(int index) { return crypt_out[index][0] & 0xffff; }
+static int get_hash_4(int index) { return crypt_out[index][0] & 0xfffff; }
+static int get_hash_5(int index) { return crypt_out[index][0] & 0xffffff; }
+static int get_hash_6(int index) { return crypt_out[index][0] & 0x7ffffff; }
+
+static int crypt_all(int *pcount, struct db_salt *salt)
+{
+	const int count = *pcount;
+	int index = 0;
+#ifdef _OPENMP
+#pragma omp parallel for
+#endif
+	for (index = 0; index < count; index++)
+	{
+		ventrilo_pwd_hash((unsigned char*)saved_key[index], (unsigned char*)crypt_out[index]);
+
+	}
+	return count;
+}
+
+static int cmp_all(void *binary, int count)
+{
+	int index = 0;
+	for (; index < count; index++)
+		if (*((ARCH_WORD_32*)binary) == crypt_out[index][0])
+			return 1;
+	return 0;
+}
+
+static int cmp_one(void *binary, int index)
+{
+	return !memcmp(binary, crypt_out[index], BINARY_SIZE);
+}
+
+static int cmp_exact(char *source, int index)
+{
+	return 1;
+}
+
+static void ventrilo_set_key(char *key, int index)
+{
+	int saved_len = strlen(key);
+	if (saved_len > PLAINTEXT_LENGTH)
+		saved_len = PLAINTEXT_LENGTH;
+	memcpy(saved_key[index], key, saved_len);
+	saved_key[index][saved_len] = 0;
+}
+
+static char *get_key(int index)
+{
+	return saved_key[index];
+}
+
+struct fmt_main fmt_ventrilo = {
+	{
+		FORMAT_LABEL,
+		FORMAT_NAME,
+		ALGORITHM_NAME,
+		BENCHMARK_COMMENT,
+		BENCHMARK_LENGTH,
+		0,
+		PLAINTEXT_LENGTH,
+		BINARY_SIZE,
+		BINARY_ALIGN,
+		SALT_SIZE,
+		SALT_ALIGN,
+		MIN_KEYS_PER_CRYPT,
+		MAX_KEYS_PER_CRYPT,
+		FMT_CASE | FMT_8_BIT | FMT_OMP,
+#if FMT_MAIN_VERSION > 11
+		{ NULL },
+#endif
+		ventrilo_tests
+	}, {
+		init,
+		done,
+		fmt_default_reset,
+		fmt_default_prepare,
+		valid,
+		fmt_default_split,
+		get_binary,
+		fmt_default_salt,
+#if FMT_MAIN_VERSION > 11
+		{ NULL },
+#endif
+		fmt_default_source,
+		{
+			fmt_default_binary_hash_0,
+			fmt_default_binary_hash_1,
+			fmt_default_binary_hash_2,
+			fmt_default_binary_hash_3,
+			fmt_default_binary_hash_4,
+			fmt_default_binary_hash_5,
+			fmt_default_binary_hash_6
+		},
+		fmt_default_salt_hash,
+		NULL,
+		fmt_default_set_salt,
+		ventrilo_set_key,
+		get_key,
+		fmt_default_clear_keys,
+		crypt_all,
+		{
+			get_hash_0,
+			get_hash_1,
+			get_hash_2,
+			get_hash_3,
+			get_hash_4,
+			get_hash_5,
+			get_hash_6
+		},
+		cmp_all,
+		cmp_one,
+		cmp_exact
+	}
+};
+
+#endif /* plugin stanza */
