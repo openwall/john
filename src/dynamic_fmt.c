658f9eb46d0044b3b26c69527c57cf4e9c76f6dd
jfoug <jfoug@cox.net>
2015-07-12 08:26:02 -0500

    dynamic: fix salt crash problem. Fix #1545

--- src/dynamic_fmt.c
+++ src/dynamic_fmt.c
@@ -2046,7 +2046,7 @@ static unsigned int salt_external_to_internal_convert(unsigned char *extern_salt
 	// the any order has 1 exception of salt_data MUST be first.  So if we get $$2salt2, then we know there is no salt-1 value.
 	unsigned char *salt2=0, *userid=0, *Flds[10];
 	int i, nsalt2=0, nuserid=0, nFlds[10]={0,0,0,0,0,0,0,0,0,0};
-	unsigned char len = strlen((char*)extern_salt), bit;
+	unsigned int len = strlen((char*)extern_salt), bit;
 	unsigned int bit_array=0;
 	unsigned int the_real_len = 6;  // 2 bytes base-8 length, and 4 bytes base-8 bitmap.
 
