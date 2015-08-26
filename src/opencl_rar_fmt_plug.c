d40cdc5fbd29518300c1a71f73cf3b489a377763
magnum <john.magnum@hushmail.com>
2015-08-27 01:46:55 +0200

    RAR-opencl: Autotune fix just for good measure. I saw no problems.

--- src/opencl_rar_fmt_plug.c
+++ src/opencl_rar_fmt_plug.c
@@ -332,7 +332,7 @@ static int crypt_all(int *pcount, struct db_salt *salt)
 	size_t *lws = local_work_size ? &local_work_size : NULL;
 	size_t gws = ((count + (local_work_size - 1)) / local_work_size) * local_work_size;
 
-	if (new_keys) {
+	if (ocl_autotune_running || new_keys) {
 		BENCH_CLERROR(clEnqueueWriteBuffer(queue[gpu_id], cl_saved_key, CL_FALSE, 0, UNICODE_LENGTH * gws, saved_key, 0, NULL, multi_profilingEvent[0]), "failed in clEnqueueWriteBuffer saved_key");
 		BENCH_CLERROR(clEnqueueWriteBuffer(queue[gpu_id], cl_saved_len, CL_FALSE, 0, sizeof(int) * gws, saved_len, 0, NULL, multi_profilingEvent[1]), "failed in clEnqueueWriteBuffer saved_len");
 		new_keys = 0;
