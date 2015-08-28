5ca823a35a888cf99a1581827b9e71a649911697
jfoug <jfoug@cox.net>
2015-08-28 16:19:29 -0500

    dynamic: lowercase for -list=subformats.  #1717

--- src/dynamic_utils.c
+++ src/dynamic_utils.c
@@ -57,11 +57,12 @@ void dynamic_DISPLAY_ALL_FORMATS()
 	for (i = 0; i < 1000; ++i)
 	{
 		char *sz = dynamic_PRELOAD_SIGNATURE(i);
-		char Type[14], *cp;
+		char Type[14], *cp, lc_type[512];
 		if (!sz)
 			break;
-		strncpy(Type, sz, sizeof(Type));
-		Type[13] = 0;
+		strnzcpy(Type, sz, sizeof(Type));
+		strnzcpy(lc_type, sz, sizeof(lc_type));
+		strlwr(lc_type);
 		cp = strchr(Type, ':');
 		if (cp) *cp = 0;
 #if !defined (DEBUG) && !defined (UNICODE_NO_OPTIONS)
@@ -69,7 +70,7 @@ void dynamic_DISPLAY_ALL_FORMATS()
 		    (!options.format || strcasecmp(options.format, "dynamic-all")))
 			continue;
 #endif
-		printf ("Format = %s%s  type = %s\n", Type, strlen(Type)<10?" ":"", sz);
+		printf ("Format = %s%s  type = %s\n", Type, strlen(Type)<10?" ":"", lc_type);
 	}
 
 	// The config has not been loaded, so we have to load it now, if we want to 'check'
@@ -87,7 +88,12 @@ void dynamic_DISPLAY_ALL_FORMATS()
 		if (sz &&
 		    // dynamic_IS_PARSER_VALID(i)) // this would include "reserved" formats
 		    dynamic_IS_VALID(i, 0) == 1) // skip "reserved" formats & disabled
-			printf ("UserFormat = dynamic_%d  type = %s\n", i, sz);
+		{
+			char lc_type[512];
+			strnzcpy(lc_type, sz, sizeof(lc_type));
+			strlwr(lc_type);
+			printf ("UserFormat = dynamic_%d  type = %s\n", i, lc_type);
+		}
 	}
 }
 
