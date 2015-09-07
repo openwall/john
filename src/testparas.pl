8f5029f4145ba4535d0019aae83deb2880e1332b
magnum <john.magnum@hushmail.com>
2015-09-07 22:06:13 +0200

    testparas.pl: Finish with printing a complete suggested command line for ./configure. Also add a second optional parameter for number of cores (for make -j option).

--- src/testparas.pl
+++ src/testparas.pl
@@ -2,7 +2,7 @@
 #
 # Final output is a table in GitHub Markdown format
 #
-# Usage: ./testparas.pl [seconds]
+# Usage: ./testparas.pl [seconds] [num cpus (for -j)]
 #
 # ../run/john will run with -test=seconds or just -test depending
 # upon whether the seconds parameter is provided or not.
@@ -10,21 +10,32 @@
 use strict;
 
 my $compiler = `gcc -v 2>&1 | tail -1` or die;
-print $compiler;
-
+my $MAX_PARA = 5;
 my ($i, $j, $k);
 my ($out, $sp);
 my %speed;
 my %best;
-my $test="first";
+my $test="-test";
 my $john_build;
+my $default_para = "";
 my $gomp_stuff = "";
+my $num_cpus = "-j4";
+
+if (defined($ARGV[0]) && $ARGV[0] > 0) { $test="-test=$ARGV[0]"; }
+if (defined($ARGV[1]) && $ARGV[1] > 0) { $num_cpus = $ARGV[1]; }
 
 print "This will take a while.\n";
 print "Initial configure...\n";
 print `./configure --disable-cuda --disable-opencl >/dev/null` or die;
-print `make -s clean` or die;
 print "Initial build...\n";
+print `make -s clean && make -sj${num_cpus}` or die;
+system ("../run/john >JohnUsage.Scr 2>&1");
+open(FILE, "<JohnUsage.Scr") or die $!;
+my @johnUsageScreen = <FILE>;
+close(FILE);
+unlink("JohnUsage.Scr");
+$john_build = $johnUsageScreen[0];
+$default_para = "Default paras: " . `../run/john --list=build-info | grep interl`;
 
 if (defined $ENV{OMP_NUM_THREADS}) {
 	$gomp_stuff .= "OMP_NUM_THREADS=" . $ENV{OMP_NUM_THREADS} . " ";
@@ -33,21 +44,13 @@ if (defined $ENV{GOMP_CPU_AFFINITY}) {
 	$gomp_stuff .= "GOMP_CPU_AFFINITY=" . $ENV{GOMP_CPU_AFFINITY};
 }
 
-foreach $i (1..5)
+print "Here we go...\n";
+
+foreach $i (1..$MAX_PARA)
 {
 	print `rm -f simd-intrinsics.o pbkdf2*fmt*o dynamic*o ../run/john` or die;
 	my $CPPFLAGS="-DSIMD_PARA_MD4=$i -DSIMD_PARA_MD5=$i -DSIMD_PARA_SHA1=$i -DSIMD_PARA_SHA256=$i -DSIMD_PARA_SHA512=$i -DOMP_SCALE=1";
-	print `make -sj4 CPPFLAGS="$CPPFLAGS"` or die;
-	if ($test eq "first") {
-		system ("../run/john >JohnUsage.Scr 2>&1");
-		open(FILE, "<JohnUsage.Scr") or die $!;
-		my @johnUsageScreen = <FILE>;
-		close(FILE);
-		unlink("JohnUsage.Scr");
-		$john_build = $johnUsageScreen[0];
-		$test="-test";
-		if (defined($ARGV[0]) && $ARGV[0] ne "") { $test="-test=$ARGV[0]"; }
-	}
+	print `make -sj${num_cpus} CPPFLAGS="$CPPFLAGS"` or die;
 	print "\n== Speeds for ${i}x interleaving (OMP_SCALE 1): ==\n";
 	foreach $j (qw(md4 md5 sha1 sha256 sha512))
 	{
@@ -65,7 +68,7 @@ foreach $i (1..5)
 	}
 	print `rm pbkdf2*fmt*o ../run/john` or die;
 	$CPPFLAGS="-DSIMD_PARA_MD4=$i -DSIMD_PARA_MD5=$i -DSIMD_PARA_SHA1=$i -DSIMD_PARA_SHA256=$i -DSIMD_PARA_SHA512=$i -U_OPENMP";
-	print `make -sj4 CPPFLAGS="$CPPFLAGS"` or die;
+	print `make -sj${num_cpus} CPPFLAGS="$CPPFLAGS"` or die;
 	print "\n===== Speeds for ${i}x interleaving (no OMP): =====\n";
 	foreach $j (qw(md4 md5 sha1 sha256 sha512))
 	{
@@ -82,9 +85,11 @@ foreach $i (1..5)
 		}
 	}
 }
+print `make -s clean`;
 
 print "\n$compiler";
-print "$john_build";
+print $john_build;
+print $default_para;
 if ($test ne "-test") {
 	print "running john with \'$test\' for each test\n";
 }
@@ -98,7 +103,7 @@ foreach $j (qw(md4 md5 sha1 sha256 sha512))
 	foreach $k ("", "-omp")
 	{
 		printf "%-10s |", $j.$k;
-		foreach $i (1..5)
+		foreach $i (1..$MAX_PARA)
 		{
 			if ($best{$j.$k}{"para"} == $i)
 			{
@@ -112,3 +117,10 @@ foreach $j (qw(md4 md5 sha1 sha256 sha512))
 	}
 }
 print "\n";
+print "Suggested command line for configure:\n";
+print "./configure CPPFLAGS=\"";
+foreach $j (qw(md4 md5 sha1 sha256 sha512)) {
+	print "-DSIMD_".uc($j)."_PARA=".$best{$j."-omp"}{"para"};
+	print " " unless $j eq "sha512";
+}
+print "\"\n";
