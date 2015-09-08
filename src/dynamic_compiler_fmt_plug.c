1428511cd49c4639a143b1615de8d399f7c65363
jfoug <jfoug@cox.net>
2015-09-08 11:12:56 -0500

    added FMT_SPLIT_UNIFY_CASE

--- src/dynamic_compiler_fmt_plug.c
+++ src/dynamic_compiler_fmt_plug.c
@@ -145,7 +145,7 @@ struct fmt_main fmt_CompiledDynamic =
 		// setup the labeling and stuff. NOTE the max and min crypts are set to 1
 		// here, but will be reset within our init() function.
 		FORMAT_LABEL, FORMAT_NAME, ALGORITHM_NAME, BENCHMARK_COMMENT, BENCHMARK_LENGTH,
-		0, 0, 16, BINARY_ALIGN, DYNA_SALT_SIZE, SALT_ALIGN, 1, 1, FMT_CASE | FMT_8_BIT | FMT_DYNAMIC,
+		0, 0, 16, BINARY_ALIGN, DYNA_SALT_SIZE, SALT_ALIGN, 1, 1, FMT_CASE | FMT_8_BIT | FMT_DYNAMIC | FMT_SPLIT_UNIFIES_CASE,
 		{ NULL },
 		tests
 	},
