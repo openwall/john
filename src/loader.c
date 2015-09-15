439ecc9798303ce899074dfec91a149915ad66e3
Solar <solar@openwall.com>
2015-09-15 21:49:46 +0200

    Avoid ldr_remove_marked() when there's nothing marked for removal.

--- src/loader.c
+++ src/loader.c
@@ -1050,6 +1050,7 @@ static void ldr_load_pot_line(struct db_main *db, char *line)
 	char *ciphertext;
 	void *binary;
 	int hash;
+	int need_removal;
 	struct db_password *current;
 
 	ciphertext = ldr_get_field(&line, db->options->field_sep_char);
@@ -1057,6 +1058,7 @@ static void ldr_load_pot_line(struct db_main *db, char *line)
 	ciphertext = format->methods.split(ciphertext, 0, format);
 	binary = format->methods.binary(ciphertext);
 	hash = db->password_hash_func(binary);
+	need_removal = 0;
 
 	if ((current = db->password_hash[hash]))
 	do {
@@ -1071,7 +1073,11 @@ static void ldr_load_pot_line(struct db_main *db, char *line)
 		    format->methods.source(current->source, current->binary)))
 			continue;
 		current->binary = NULL; /* mark for removal */
+		need_removal = 1;
 	} while ((current = current->next_hash));
+
+	if (need_removal)
+		db->options->flags |= DB_NEED_REMOVAL;
 }
 
 void ldr_load_pot_file(struct db_main *db, char *name)
@@ -1322,6 +1328,9 @@ static void ldr_remove_marked(struct db_main *db)
 	struct db_salt *current_salt, *last_salt;
 	struct db_password *current_pw, *last_pw;
 
+	if (!(db->options->flags & DB_NEED_REMOVAL))
+		return;
+
 	last_salt = NULL;
 	if ((current_salt = db->salts))
 	do {
@@ -1353,6 +1362,8 @@ static void ldr_remove_marked(struct db_main *db)
 		} else
 			last_salt = current_salt;
 	} while ((current_salt = current_salt->next));
+
+	db->options->flags &= ~DB_NEED_REMOVAL;
 }
 
 /*
