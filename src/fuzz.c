openwall/john

commit aeff56220a8afcc6996ada2e17fd7c0a0f4e781f

Add a SingleSkipLogin config variable
Useful if you  already used --single mode with just the login
field, and later want to use --soingle mode together with other
information (added into the GECOS field), but still want the login
field to be reported for cracks.

src/fuzz.c
@@ -53,6 +53,7 @@ static struct FuzzDic *rfd;
 static FILE *s_file; // Status file which is ./fuzz_status/'format->params.label'
 
 extern int pristine_gecos;
+extern int single_skip_login;
 
 static char *file_pos, *file_end;
 
@@ -611,6 +612,8 @@ int fuzz(struct db_main *db)
 
 	pristine_gecos = cfg_get_bool(SECTION_OPTIONS, NULL,
 	        "PristineGecos", 0);
+	single_skip_login = cfg_get_bool(SECTION_OPTIONS, NULL,
+		"PristineGecos", 0);
 
 	if (options.flags & FLG_FUZZ_DUMP_CHK) {
 		from = -1;

run/john.conf
@@ -82,6 +82,14 @@ WordlistMemoryMap = Y
 # you might want to bump SingleWordsPairMax too, below, to 10 or more.
 PristineGecos = N
 
+# For single mode, ignore the login field.
+# Normal behavior is to use the login field for single mode.
+# Skipping the login field should only be enabled if previous single mode
+# sessions did already make use of the login field, but no other information,
+# and now you want to use other information, skip the login field, but still
+# want the login field to be reported on successful cracks or with --show.
+SingleSkipLogin = N
+
 # Over-ride SINGLE_WORDS_PAIR_MAX in params.h. This may slow down Single mode
 # but it may also help cracking a few more candidates. Default in core John
 # is 4 while the Jumbo default is 6.

src/loader.c
@@ -70,8 +70,10 @@ int ldr_in_pot = 0;
 static char *no_username = "?";
 #ifdef HAVE_FUZZ
 int pristine_gecos;
+int single_skip_login;
 #else
 static int pristine_gecos;
+static int single_skip_login;
 #endif
 
 /* There should be legislation against adding a BOM to UTF-8 */
@@ -775,10 +777,10 @@ static struct list_main *ldr_init_words(char *login, char *gecos, char *home)
 
 	list_init(&words);
 
-	if (*login && login != no_username)
+	if (*login && login != no_username && !single_skip_login)
 		list_add(words, ldr_conv(login));
 	ldr_split_string(words, ldr_conv(gecos));
-	if (login != no_username)
+	if (login != no_username && !single_skip_login)
 		ldr_split_string(words, ldr_conv(login));
 	if (pristine_gecos && *gecos)
 		list_add_unique(words, ldr_conv(gecos));
@@ -1007,6 +1009,8 @@ void ldr_load_pw_file(struct db_main *db, char *name)
 {
 	pristine_gecos = cfg_get_bool(SECTION_OPTIONS, NULL,
 	        "PristineGecos", 0);
+	single_skip_login = cfg_get_bool(SECTION_OPTIONS, NULL,
+	        "SingleSkipLogin", 0);
 
 	read_file(db, name, RF_ALLOW_DIR, ldr_load_pw_line);
 }

commit eaca7ff61fc25e0f18f89d3a79bd7641ef229c8b

fuzz.c: fix a copy&paste error introduced in aeff562

src/fuzz.c
@@ -613,7 +613,7 @@ int fuzz(struct db_main *db)
 	pristine_gecos = cfg_get_bool(SECTION_OPTIONS, NULL,
 	        "PristineGecos", 0);
 	single_skip_login = cfg_get_bool(SECTION_OPTIONS, NULL,
-		"PristineGecos", 0);
+		"SingleSkipLogin", 0);
 
 	if (options.flags & FLG_FUZZ_DUMP_CHK) {
 		from = -1;