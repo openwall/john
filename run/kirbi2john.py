560f8220c4904b2e52bb95eedf400d6db4fa45ff
magnum <john.magnum@hushmail.com>
2015-09-30 22:59:03 +0200

    Cosmetic fixes in kirbi2john.py.

--- run/kirbi2john.py
+++ run/kirbi2john.py
@@ -1,4 +1,5 @@
-# Part of the Kerberoast Scrypt from Tim Medin to extract the Kerberos tickets from a kirbi file. 
+# Based on the Kerberoast script from Tim Medin to extract the Kerberos tickets
+# from a kirbi file.
 # Modification to parse them into the JTR-format by Michael Kramer (SySS GmbH)
 # Copyright [2015] [Tim Medin, Michael Kramer]
 #
@@ -12,7 +13,7 @@
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
-# limitations under the License 
+# limitations under the License
 
 from pyasn1.codec.ber import encoder, decoder
 from multiprocessing import JoinableQueue, Manager
@@ -24,7 +25,7 @@ if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description='Read Mimikatz kerberos ticket then modify it and save it in crack_file')
 	parser.add_argument('files', nargs='+', metavar='file.kirbi',
 					help='File name to crack. Use asterisk \'*\' for many files.\n Files are exported with mimikatz or from extracttgsrepfrompcap.py')
-	
+
 	args = parser.parse_args()
 
 	manager = Manager()
@@ -38,7 +39,7 @@ if __name__ == '__main__':
 			#data = open('f.read()
 
 			if data[0] == '\x76':
-				# rem dump 
+				# rem dump
 				enctickets.append((str(decoder.decode(data)[0][2][0][3][2]), i, f))
 				i += 1
 			elif data[:2] == '6d':
@@ -46,8 +47,7 @@ if __name__ == '__main__':
 					enctickets.append((str(decoder.decode(ticket.decode('hex'))[0][4][3][2]), i, f))
 					i += 1
 
-	out=open("crack_file","wb")	
+	out=open("crack_file","wb")
 	for et in enctickets:
 		out.write("$krb5tgs$unkown:"+et[0][:16].encode("hex")+"$"+et[0][16:].encode("hex")+"\n")
 	out.close
-		
