5402c156b3a1b4c25b35071b4d9c9e658e41f8aa
jfoug <jfoug@openwall.net>
2015-10-05 20:14:10 -0500

    jumbo.h.  Recent proper header wrapping was causing problems with VC builds

--- src/jumbo.h
+++ src/jumbo.h
@@ -15,7 +15,12 @@
  * ./configure)
  */
 #ifndef _JTR_JUMBO_H
+#ifndef _MSC_VER
+// not sure why I need to multiply include in VC, but the SIMD builds will NOT work without it.
+// problems with the define of inline->_inline   I just do not understand why this has to be included
+// multiple times, BUT it does??
 #define _JTR_JUMBO_H
+#endif
 
 // use this define in some core (master) code, to be able to more cleanly insert code
 // leaving the master code more intact for easier merging of changes Solar gives us.
