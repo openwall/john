8eddde3e098efc64789c540a50344715cd1c3942
magnum <john.magnum@hushmail.com>
2015-10-07 22:46:02 +0200

    Use Makefile logic to copy/update OpenCL stuff to run/kernels.

--- src/plugin_deps.pl
+++ src/plugin_deps.pl
@@ -1,21 +1,46 @@
 #!/usr/bin/perl -w
 use strict;
 
-foreach my $format (@ARGV) {
-	my $object = $format;
+sub find_deps {
+	my ($src_file, $uniq_ref) = @_;
 	my $deps = "";
-	$object =~ s/\.c$/.o/;
-	open FILE, "<", $format or die $!;
-	while (<FILE>) {
+
+	open my $fh, "<", $src_file or die $!;
+	while (<$fh>) {
 		if (/^\s*#\s*include\s+"([^"]+)"/) {
 			if ($1 eq "arch.h" || $1 eq "autoconfig.h" || -f $1) {
-				$deps .= " " . $1;
+				if (!($uniq_ref->{$1}++)) {
+					$deps .= " " . $1;
+					# Recurse!
+					$deps .= find_deps($1, $uniq_ref);
+				}
 			} else {
-				print STDERR "Warning: " . $format . " includes \"" . $1 . "\" but that file is not found.\n";
+				print STDERR "Warning: " . $src_file . " includes \"" . $1 . "\" but that file is not found.\n";
 			}
 		}
 	}
-	close(FILE);
-	print $object . ":" . "\t" . $format . $deps . "\n\n";
-	#print "\t" . '$(CC) $(CFLAGS) ' . $format . " -o " . $object . "\n\n";
+	close($fh);
+
+	return $deps;
+}
+
+foreach my $src_file (@ARGV) {
+	my $object = $src_file;
+	my $type = "c";
+	my %uniqdeps;
+
+	if ($object =~ /\.c$/) {
+		$object =~ s/\.c$/.o/;
+	} elsif ($object =~ /\.cl$/) {
+		$object =~ s/^opencl\//..\/run\/kernels\//;
+		$type = "cl";
+	}
+	my $deps = find_deps($src_file, \%uniqdeps);
+	print $object . ":" . "\t" . $src_file . $deps . "\n";
+	if ($type eq "c") {
+		#print "\t" . '$(CC) $(CFLAGS) ' . $src_file . " -o " . $object . "\n";
+	} elsif ($type eq "cl") {
+		#print "\t" . '$(CP) $? ' . "../run/kernels\n";
+	}
+	print "\n";
 }
