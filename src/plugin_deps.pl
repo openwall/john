ecd4d1187b260ba971169aa230df968564c22560
magnum <john.magnum@hushmail.com>
2015-10-08 01:34:03 +0200

    Revert recent Makefile changes. They are problematic.

--- src/plugin_deps.pl
+++ src/plugin_deps.pl
@@ -1,46 +1,21 @@
 #!/usr/bin/perl -w
 use strict;
 
-sub find_deps {
-	my ($src_file, $uniq_ref) = @_;
+foreach my $format (@ARGV) {
+	my $object = $format;
 	my $deps = "";
-
-	open my $fh, "<", $src_file or die $!;
-	while (<$fh>) {
+	$object =~ s/\.c$/.o/;
+	open FILE, "<", $format or die $!;
+	while (<FILE>) {
 		if (/^\s*#\s*include\s+"([^"]+)"/) {
 			if ($1 eq "arch.h" || $1 eq "autoconfig.h" || -f $1) {
-				if (!($uniq_ref->{$1}++)) {
-					$deps .= " " . $1;
-					# Recurse!
-					$deps .= find_deps($1, $uniq_ref);
-				}
+				$deps .= " " . $1;
 			} else {
-				print STDERR "Warning: " . $src_file . " includes \"" . $1 . "\" but that file is not found.\n";
+				print STDERR "Warning: " . $format . " includes \"" . $1 . "\" but that file is not found.\n";
 			}
 		}
 	}
-	close($fh);
-
-	return $deps;
-}
-
-foreach my $src_file (@ARGV) {
-	my $object = $src_file;
-	my $type = "c";
-	my %uniqdeps;
-
-	if ($object =~ /\.c$/) {
-		$object =~ s/\.c$/.o/;
-	} elsif ($object =~ /\.cl$/) {
-		$object =~ s/^opencl\//..\/run\/kernels\//;
-		$type = "cl";
-	}
-	my $deps = find_deps($src_file, \%uniqdeps);
-	print $object . ":" . "\t" . $src_file . $deps . "\n";
-	if ($type eq "c") {
-		#print "\t" . '$(CC) $(CFLAGS) ' . $src_file . " -o " . $object . "\n";
-	} elsif ($type eq "cl") {
-		#print "\t" . '$(CP) $? ' . "../run/kernels\n";
-	}
-	print "\n";
+	close(FILE);
+	print $object . ":" . "\t" . $format . $deps . "\n\n";
+	#print "\t" . '$(CC) $(CFLAGS) ' . $format . " -o " . $object . "\n\n";
 }
