548058af3b7fb08609baabb0b92ab3231fac8e25
Dhiru Kholia <kholia@kth.se>
2015-11-20 23:25:59 +0100

    Add support for iWork '09 files

--- src/iwork_fmt_plug.c
+++ src/iwork_fmt_plug.c
@@ -74,6 +74,8 @@ john_register_one(&fmt_iwork);
 static struct fmt_tests iwork_tests[] = {
 	{"$iwork$1$2$1$100000$d77ce46a68697e08b76ac91de9117541$e7b72b2848dc27efed883963b00b1ac7$e794144cd2f04bd50e23957b30affb2898554a99a3accb7506c17132654e09c04bbeff45dc4f8a8a1db5fd1592f699eeff2f9a8c31b503e9631a25a344b517f7" ,"12345678"},
 	{FORMAT_TAG "1$2$1$100000$c773f06bcd580e4afa35618a7d0bee39$8b241504af92416f226d0eea4bf26443$18358e736a0401061f2dca103fceb29e88606d3ec80d09841360cbb8b9dc1d2908c270d3ff4c05cf7a46591e02ff3c9d75f4582f631721a3257dc087f98f523e", "password"},
+	// iWork '09 Keynote file
+	{"$iwork$2$1$1$4000$736f6d6553616c74$a9d975f8b3e1bf0c388944b457127df4$09eb5d093584376001d4c94e9d0a41eb8a2993132849c5aed8e56e7bd0e8ed50ba38aced793e3480675990c828c01d25fe245cc6aa603c6cb1a0425988f1d3dc", "openwall"},
 	{NULL}
 };
 
@@ -116,6 +118,8 @@ static int valid(char *ciphertext, struct fmt_main *self)
 {
 	char *ctcopy, *keeptr, *p;
 	int value;
+	int file_version;
+	int salt_length; // this is "someSalt" for iWork '09 files
 
 	if (strncmp(ciphertext, FORMAT_TAG, TAG_LENGTH) != 0)
 		return 0;
@@ -129,14 +133,14 @@ static int valid(char *ciphertext, struct fmt_main *self)
 	if (!isdec(p))
 		goto err;
 	value = atoi(p);
-	if (value != 1)
+	if (value != 1 && value !=2)
 		goto err;
 	if ((p = strtokm(NULL, "$")) == NULL)   // file version
 		goto err;
 	if (!isdec(p))
 		goto err;
-	value = atoi(p);
-	if (value != 2)
+	file_version = atoi(p);
+	if (file_version != 1 && file_version != 2) // iWork '09, iWork 2013
 		goto err;
 	if ((p = strtokm(NULL, "$")) == NULL)   // format
 		goto err;
@@ -151,7 +155,11 @@ static int valid(char *ciphertext, struct fmt_main *self)
 		goto err;
 	if ((p = strtokm(NULL, "$")) == NULL)   // salt
 		goto err;
-	if(hexlenl(p) != SALTLEN * 2)
+	if (file_version == 1)
+		salt_length = 8;
+	else
+		salt_length = SALTLEN;
+	if(hexlenl(p) != salt_length * 2)
 		goto err;
 	if ((p = strtokm(NULL, "$")) == NULL)   // iv
 		goto err;
@@ -178,12 +186,18 @@ static void *get_salt(char *ciphertext)
 	char *keeptr = ctcopy;
 	int i;
 	char *p;
+	int file_version;
 
 	fctx = mem_calloc_tiny(sizeof(struct format_context), MEM_ALIGN_WORD);
 
 	ctcopy += TAG_LENGTH;
 	p = strtokm(ctcopy, "$"); // internal version
 	p = strtokm(NULL, "$"); // version
+	file_version = atoi(p);
+	if (file_version == 1)
+		fctx->salt_length = 8;
+	else
+		fctx->salt_length = SALTLEN;
 	p = strtokm(NULL, "$"); // fmt
 	p = strtokm(NULL, "$"); // iterations
 	fctx->iterations = atoi(p);
@@ -265,9 +279,9 @@ static int crypt_all(int *pcount, struct db_salt *salt)
 			pin[i] = (unsigned char*)saved_key[index+i];
 			pout[i] = master[i];
 		}
-		pbkdf2_sha1_sse((const unsigned char**)pin, lens, fctx->salt, SALTLEN, fctx->iterations, pout, 16, 0);
+		pbkdf2_sha1_sse((const unsigned char**)pin, lens, fctx->salt, fctx->salt_length, fctx->iterations, pout, 16, 0);
 #else
-		pbkdf2_sha1((unsigned char *)saved_key[index], strlen(saved_key[index]), fctx->salt, SALTLEN, fctx->iterations, master[0], 16, 0);
+		pbkdf2_sha1((unsigned char *)saved_key[index], strlen(saved_key[index]), fctx->salt, fctx->salt_length, fctx->iterations, master[0], 16, 0);
 #if !ARCH_LITTLE_ENDIAN
 		{
 			int i;
