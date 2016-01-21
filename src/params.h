a83b1eb5f216c6670679811c82521e91e0d83438
Solar Designer <solar@openwall.com>
2016-01-21 08:28:44 +0000

    Reduced CRACKED_HASH_LOG from jumbo's 25 to 21 because of performance regression seen on smaller systems when --show'ing not so large files.

--- src/params.h
+++ src/params.h
@@ -224,7 +224,7 @@ extern unsigned int password_hash_thresholds[PASSWORD_HASH_SIZES];
 /*
  * Cracked password hash size, used while loading.
  */
-#define CRACKED_HASH_LOG		25
+#define CRACKED_HASH_LOG		21
 #define CRACKED_HASH_SIZE		(1 << CRACKED_HASH_LOG)
 
 /*
