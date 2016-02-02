24bdc7c52ff056adf6cc2718d5b63c716326f16c
Dhiru Kholia <kholia@kth.se>
2016-02-02 21:40:02 +0530

    Advertise npdf2john a bit

--- run/pdf2john.py
+++ run/pdf2john.py
@@ -19,6 +19,8 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
+#
+# In case of problems, give https://github.com/kholia/npdf2john a try.
 
 import re
 import sys
