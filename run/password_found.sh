8c86612dc568c34f37eeb281c8635d23b203f5c3
magnum <john.magnum@hushmail.com>
2016-02-16 00:51:53 +0100

    Revert "Some tweaks for 1be1841: Support $JOHN path, set execute bit on"
    This reverts commit 4fb8c31c0697f8fb28e6431deb662b9faff5d2fb.
    
    Revert "Merge pull request #2054 from akos-sereg/bleeding-jumbo"
    
    This reverts commit 00150ba88f926c18e28be14c46d8487d2c931ace, reversing
    changes made to a20f4772033ab6e5993399a6069bdbf36a4bb286.

--- run/password_found.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# This is a sample shell script which is triggered when a password is found. You can configure john to 
-# execute it by uncommenting "ExecOnCrackedPassword" key in john.conf
-
-echo "Password found for $1: $2"
