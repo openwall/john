7b63341e651893306b83051d0149a76fa6b80af4
magnum <john.magnum@hushmail.com>
2016-03-18 11:30:35 +0100

    Bugfix for --skip-self-tests. See #1967 and closes #2090.

--- src/formats.c
+++ src/formats.c
@@ -378,7 +378,8 @@ static char *fmt_self_test_body(struct fmt_main *format,
 	if (options.flags & FLG_NOTESTS) {
 		fmt_init(format);
 		dyna_salt_init(format);
-		format->methods.reset(db);
+		if (db)
+			format->methods.reset(db);
 		format->private.initialized = 2;
 		format->methods.clear_keys();
 		return NULL;
