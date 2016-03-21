40bd22f931ee5283b88e356ef1533e11d768202a
jfoug <jfoug@cox.net>
2016-03-21 12:15:27 -0500

    oldoffice: removed redundant calls to dyna_salt_init within salt_compare

--- src/oldoffice_fmt_plug.c
+++ src/oldoffice_fmt_plug.c
@@ -92,7 +92,6 @@ static unsigned char (*rc4_key)[16];
 static int any_cracked, *cracked;
 static size_t cracked_size;
 static int new_keys;
-extern struct fmt_main fmt_oldoffice;
 
 typedef struct {
 	dyna_salt dsalt;
@@ -380,14 +379,11 @@ static void set_salt(void *salt)
 static int salt_compare(const void *x, const void *y)
 {
 	int c;
-	struct fmt_main *pold;
 
 	c = memcmp((*(custom_salt**)x)->salt, (*(custom_salt**)y)->salt, 16);
 	if (c)
 		return c;
-	pold = dyna_salt_init(&fmt_oldoffice);
 	c = dyna_salt_cmp((void*)x, (void*)y, SALT_SIZE);
-	dyna_salt_init(pold);
 	return c;
 }
 
