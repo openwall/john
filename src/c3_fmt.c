e4a65c7b478f7d7a394ba58aa45938c37ca23da6
magnum <john.magnum@hushmail.com>
2016-06-17 21:55:00 +0200

    Revert "crypt: problems with #defines and strdup"
    This reverts commit 70d2140adda533680aec3c0cb0e828a868fafa68.

--- src/c3_fmt.c
+++ src/c3_fmt.c
@@ -17,28 +17,6 @@
 
 #if HAVE_CRYPT
 
-/* if this comes after the #define crap below, there are often
- * problems with strdup or other things not being defined. We
- * move this block of includes to above the _XOPEN_* defines
- */
-#if STRING_WITH_STRINGS
-#include <string.h>
-#include <strings.h>
-#elif HAVE_STRING_H
-#include <string.h>
-#elif HAVE_STRINGS_H
-#include <strings.h>
-#endif
-#if !AC_BUILT
-#include <string.h>
-#ifndef _MSC_VER
-#include <strings.h>
-#endif
-#undef _XOPEN_VERSION
-#undef _XOPEN_SOURCE
-#undef _XOPEN_SOURCE_EXTENDED
-#undef _GNU_SOURCE
-
 #define _XOPEN_SOURCE 4 /* for crypt(3) */
 #define _XOPEN_SOURCE_EXTENDED 1 /* for OpenBSD */
 #define _XOPEN_VERSION 4
@@ -46,6 +24,11 @@
 #define _GNU_SOURCE 1 /* for crypt_r(3) */
 #include <stdio.h>
 
+#if !AC_BUILT
+#include <string.h>
+#ifndef _MSC_VER
+#include <strings.h>
+#endif
 #ifdef __CYGWIN__
 #include <crypt.h>
 #endif
@@ -56,6 +39,15 @@
 #include <unistd.h>
 #endif
 #endif
+#endif
+
+#if STRING_WITH_STRINGS
+#include <string.h>
+#include <strings.h>
+#elif HAVE_STRING_H
+#include <string.h>
+#elif HAVE_STRINGS_H
+#include <strings.h>
 #endif
 
 #if (!AC_BUILT && defined(HAVE_CRYPT))
