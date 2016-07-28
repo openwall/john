3b9ccdefd21d1d7e883a214edf1e886b14522248
jfoug <jfoug@openwall.net>
2016-07-28 01:16:43 -0500

    hmac-sha:  added hmac-sha224 and hmac-sha384 to the easy jtr interface

--- src/hmac_sha_plug.c
+++ src/hmac_sha_plug.c
@@ -110,6 +110,46 @@ void JTR_hmac_sha256(const unsigned char *key, int key_len, const unsigned char 
 	}
 }
 
+void JTR_hmac_sha224(const unsigned char *key, int key_len, const unsigned char *data, int data_len, unsigned char *digest, int digest_len) {
+	JTR_ALIGN(8) unsigned char buf[64];
+	unsigned char local_digest[28];
+	ARCH_WORD *pW = (ARCH_WORD *)buf;
+	unsigned i;
+	SHA256_CTX ctx;
+
+	if (key_len > 64) {
+		SHA224_Init(&ctx);
+		SHA224_Update(&ctx, key, key_len);
+		SHA224_Final(buf, &ctx);
+		memset(&buf[28], 0, 4);
+		for (i = 0; i < HMAC_SHA32_COUNT/2; ++i)
+			pW[i] ^= HMAC_SHA_IPAD_XOR;
+		for (; i < HMAC_SHA32_COUNT; ++i)
+			pW[i] = HMAC_SHA_IPAD_XOR;
+	} else {
+		memcpy(buf, key, key_len);
+		memset(&buf[key_len], 0, 64-key_len);
+		for (i = 0; i < HMAC_SHA32_COUNT; ++i)
+			pW[i] ^= HMAC_SHA_IPAD_XOR;
+	}
+	SHA224_Init(&ctx);
+	SHA224_Update(&ctx, buf, 64);
+	if (data_len)
+		SHA224_Update(&ctx, data, data_len);
+	SHA224_Final(local_digest, &ctx);
+	for (i = 0; i < HMAC_SHA32_COUNT; ++i)
+		pW[i] ^= HMAC_SHA_OPAD_XOR;
+	SHA224_Init(&ctx);
+	SHA224_Update(&ctx, buf, 64);
+	SHA224_Update(&ctx, local_digest, 28);
+	if (digest_len >= 28)
+		SHA224_Final(digest, &ctx);
+	else {
+		SHA224_Final(local_digest, &ctx);
+		memcpy(digest, local_digest, digest_len);
+	}
+}
+
 void JTR_hmac_sha512(const unsigned char *key, int key_len, const unsigned char *data, int data_len, unsigned char *digest, int digest_len) {
 	JTR_ALIGN(8) unsigned char buf[128];
 	unsigned char local_digest[64];
@@ -145,6 +185,46 @@ void JTR_hmac_sha512(const unsigned char *key, int key_len, const unsigned char 
 		SHA512_Final(digest, &ctx);
 	else {
 		SHA512_Final(local_digest, &ctx);
+		memcpy(digest, local_digest, digest_len);
+	}
+}
+
+void JTR_hmac_sha384(const unsigned char *key, int key_len, const unsigned char *data, int data_len, unsigned char *digest, int digest_len) {
+	JTR_ALIGN(8) unsigned char buf[128];
+	unsigned char local_digest[48];
+	ARCH_WORD *pW = (ARCH_WORD *)buf;
+	unsigned i;
+	SHA512_CTX ctx;
+
+	if (key_len > 128) {
+		SHA384_Init(&ctx);
+		SHA384_Update(&ctx, key, key_len);
+		SHA384_Final(buf, &ctx);
+		memset(&buf[48], 0, 16);
+		for (i = 0; i < HMAC_SHA64_COUNT/2; ++i)
+			pW[i] ^= HMAC_SHA_IPAD_XOR;
+		for (; i < HMAC_SHA64_COUNT; ++i)
+			pW[i] = HMAC_SHA_IPAD_XOR;
+	} else {
+		memcpy(buf, key, key_len);
+		memset(&buf[key_len], 0, 128-key_len);
+		for (i = 0; i < HMAC_SHA64_COUNT; ++i)
+			pW[i] ^= HMAC_SHA_IPAD_XOR;
+	}
+	SHA384_Init(&ctx);
+	SHA384_Update(&ctx, buf, 128);
+	if (data_len)
+		SHA384_Update(&ctx, data, data_len);
+	SHA384_Final(local_digest, &ctx);
+	for (i = 0; i < HMAC_SHA64_COUNT; ++i)
+		pW[i] ^= HMAC_SHA_OPAD_XOR;
+	SHA384_Init(&ctx);
+	SHA384_Update(&ctx, buf, 128);
+	SHA384_Update(&ctx, local_digest, 48);
+	if (digest_len >= 48)
+		SHA384_Final(digest, &ctx);
+	else {
+		SHA384_Final(local_digest, &ctx);
 		memcpy(digest, local_digest, digest_len);
 	}
 }
