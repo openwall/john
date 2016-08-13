openwall/john

commit a1bac03e412c4f7a4e0218f91298a5b7e8958241

Additional normalization of FORMAT_TAG defines.  #2196

src/opencl_rawsha512_ng_fmt_plug.c
@@ -346,8 +346,7 @@ static void *get_salt(char *ciphertext)
 	} out;
 	char *p;
 	int i;
-
-	ciphertext += 6;
+	ciphertext += TAG_LENGTH;
 	p = ciphertext;
 	for (i = 0; i < sizeof(out.c); i++) {
 		out.c[i] = (atoi16[ARCH_INDEX(*p)] << 4) | atoi16[ARCH_INDEX(p[1])];
@@ -927,7 +926,11 @@ struct fmt_main fmt_opencl_ng_xsha512 = {
 		MAX_KEYS_PER_CRYPT,
 		FMT_CASE | FMT_8_BIT | FMT_SPLIT_UNIFIES_CASE,
 		{NULL},
-		{ NULL },
+		{
+			FORMAT_TAG,
+			XSHA512_FORMAT_TAG,
+			NSLDAP_FORMAT_TAG
+		},
 		sha512_common_tests_xsha512
 	}, {
 		init_x,

src/AzureAD_common.h
@@ -16,7 +16,7 @@
 #include "formats.h"
 
 #define FORMAT_TAG                      "v1;PPH1_MD4,"
-#define TAG_LENGTH                      12
+#define TAG_LENGTH                      (sizeof(FORMAT_TAG)-1)
 
 #define HASH_LENGTH                     64
 #define SALT_HASH_LEN                   20

src/AzureAD_fmt_plug.c
@@ -237,7 +237,7 @@ struct fmt_main fmt_AzureAD = {
 #endif
 		FMT_CASE | FMT_8_BIT | FMT_SPLIT_UNIFIES_CASE | FMT_OMP | FMT_UNICODE | FMT_UTF8,
 		{ NULL },
-		{ NULL },
+		{ FORMAT_TAG },
 		AzureAD_common_tests
 	}, {
 		init,

src/MD5_fmt.c
@@ -402,7 +402,11 @@ struct fmt_main fmt_MD5 = {
 #endif
 		FMT_CASE | FMT_8_BIT,
 		{ NULL },
-		{ NULL },
+		{
+			md5_salt_prefix,
+			apr1_salt_prefix,
+			smd5_salt_prefix
+		},
 		tests
 	}, {
 		init,

src/MD5_std.c
@@ -17,6 +17,7 @@
 #include "arch.h"
 #include "common.h"
 #include "MD5_std.h"
+#include "cryptmd5_common.h"
 
 #if MD5_std_mt
 #include <omp.h>
@@ -417,11 +418,11 @@ void MD5_std_set_salt(char *salt)
 #endif
 
 	if (salt[8] == MD5_TYPE_STD) {
-		prefix = "$1$";
-		prelen = 3;
+		prefix = md5_salt_prefix;
+		prelen = md5_salt_prefix_len;
 	} else if (salt[8] == MD5_TYPE_APACHE) {
-		prefix = "$apr1$";
-		prelen = 6;
+		prefix = apr1_salt_prefix;
+		prelen = apr1_salt_prefix_len;
 	} else if (salt[8] == MD5_TYPE_AIX) {
 		prefix = "";
 		prelen = 0;
@@ -992,16 +993,16 @@ char *MD5_std_get_salt(char *ciphertext)
 	char *p, *q;
 	int i;
 
-	if (!strncmp(ciphertext, "$apr1$", 6)) {
+	if (!strncmp(ciphertext, apr1_salt_prefix, apr1_salt_prefix_len)) {
 		out[8] = MD5_TYPE_APACHE;
-		p = ciphertext + 6;
+		p = ciphertext + apr1_salt_prefix_len;
 	} else
-	if (!strncmp(ciphertext, "{smd5}", 6)) {
+	if (!strncmp(ciphertext, smd5_salt_prefix, smd5_salt_prefix_len)) {
 		out[8] = MD5_TYPE_AIX;
-		p = ciphertext + 6;
+		p = ciphertext + smd5_salt_prefix_len;
 	} else {
 		out[8] = MD5_TYPE_STD;
-		p = ciphertext + 3;
+		p = ciphertext + md5_salt_prefix_len;
 	}
 	q = out;
 	for (i = 0; *p != '$' && i < 8; i++)
@@ -1032,10 +1033,10 @@ MD5_word *MD5_std_get_binary(char *ciphertext)
 	char *pos;
 	MD5_word value;
 
-	pos = ciphertext + 3;
-	if (!strncmp(ciphertext, "$apr1$", 6) ||
-	    !strncmp(ciphertext, "{smd5}", 6))
-		pos = ciphertext + 6;
+	pos = ciphertext + md5_salt_prefix_len;
+	if (!strncmp(ciphertext, apr1_salt_prefix, apr1_salt_prefix_len) ||
+	    !strncmp(ciphertext, smd5_salt_prefix, smd5_salt_prefix_len))
+		pos = ciphertext + apr1_salt_prefix_len;
 
 	while (*pos++ != '$');
 

src/crypt-sha1_fmt_plug.c
@@ -80,7 +80,7 @@ static ARCH_WORD_32 (*crypt_out)[BINARY_SIZE / sizeof(ARCH_WORD_32)];
 static struct saltstruct {
 	unsigned int length;
 	unsigned int rounds;
-	unsigned char salt[SALT_BUFFER_LENGTH+sizeof(SHA1_MAGIC)+7+1]; // allows up to 9999999 sized rounds with 64 byte salt.
+	unsigned char salt[SALT_BUFFER_LENGTH+SHA1_MAGIC_LEN+7]; // allows up to 9999999 sized rounds with 64 byte salt.
 } *cur_salt;
 
 static void init(struct fmt_main *self)
@@ -179,7 +179,7 @@ static void *get_salt(char *ciphertext)
 	memset(&out, 0, sizeof(out));
 	p = strrchr(ciphertext, '$') + 1;
 	strnzcpy(tmp, ciphertext, p - ciphertext);
-	out.rounds = strtoul(&ciphertext[sizeof(SHA1_MAGIC)-1], NULL, 10);
+	out.rounds = strtoul(&ciphertext[SHA1_MAGIC_LEN], NULL, 10);
 	// point p to the salt value, BUT we have to decorate the salt for this hash.
 	p = strrchr(tmp, '$') + 1;
 	// real salt used is: <salt><magic><iterations>
@@ -247,7 +247,7 @@ struct fmt_main fmt_cryptsha1 = {
 		{
 			"iteration count",
 		},
-		{ NULL },
+		{ SHA1_MAGIC },
 		sha1crypt_common_tests
 	}, {
 		init,

src/cryptmd5_common.c
@@ -14,15 +14,17 @@ int cryptmd5_common_valid(char *ciphertext, struct fmt_main *self)
 {
 	char *pos, *start;
 
-	if (strncmp(ciphertext, md5_salt_prefix, sizeof(md5_salt_prefix)-1)) {
-		if (strncmp(ciphertext, apr1_salt_prefix, sizeof(apr1_salt_prefix)-1) &&
-		    strncmp(ciphertext, smd5_salt_prefix, sizeof(smd5_salt_prefix)-1))
-			return 0;
-		ciphertext += 3;
-	}
+	if (!strncmp(ciphertext, md5_salt_prefix, md5_salt_prefix_len))
+		ciphertext += md5_salt_prefix_len;
+	else if (!strncmp(ciphertext, apr1_salt_prefix, apr1_salt_prefix_len))
+		ciphertext += apr1_salt_prefix_len;
+	else if(!strncmp(ciphertext, smd5_salt_prefix, smd5_salt_prefix_len))
+		ciphertext += smd5_salt_prefix_len;
+	else
+		return 0;
 
-	for (pos = &ciphertext[3]; *pos && *pos != '$'; pos++);
-	if (!*pos || pos < &ciphertext[3] || pos > &ciphertext[11]) return 0;
+	for (pos = ciphertext; *pos && *pos != '$'; pos++);
+	if (!*pos || pos < ciphertext || pos > &ciphertext[11]) return 0;
 
 	start = ++pos;
 	while (atoi64[ARCH_INDEX(*pos)] != 0x7F) pos++;

src/cryptmd5_common.h
@@ -4,9 +4,13 @@
  * and the GPU formats, and places it into one common location
  */
 
-static const char md5_salt_prefix[] = "$1$";
-static const char apr1_salt_prefix[] = "$apr1$";
-static const char smd5_salt_prefix[] = "{smd5}";
+#define md5_salt_prefix "$1$"
+#define apr1_salt_prefix "$apr1$"
+#define smd5_salt_prefix "{smd5}"
+
+#define md5_salt_prefix_len  (sizeof(md5_salt_prefix)-1)
+#define apr1_salt_prefix_len (sizeof(apr1_salt_prefix)-1)
+#define smd5_salt_prefix_len (sizeof(smd5_salt_prefix)-1)
 
 int cryptmd5_common_valid(char *ciphertext, struct fmt_main *self);
 

commit 43cf0a4ace3d3c3f276d2a0ffd24eaafa41f5814

Fix XSHA512-ng-opencl (see #2208).
Commit a1bac03e412c4f7a4e0218f91298a5b7e8958241 introduced the typo.

.travis.yml
@@ -24,7 +24,7 @@ before_install:
   - sudo apt-get install fglrx-dev opencl-headers || true
   - export OMP_NUM_THREADS=4
 script:
-  - cd src && ./configure && make -sj4 && echo -e '[Disabled:Formats]\nRaw-SHA512-opencl = Y\nXSHA512-opencl = Y\nmscash2-opencl = Y\nXSHA512-ng-opencl = Y\ngpg-opencl = Y' > john-local.conf && ../run/john -test-full=0
+  - cd src && ./configure && make -sj4 && echo -e '[Disabled:Formats]\nRaw-SHA512-opencl = Y\nXSHA512-opencl = Y\nmscash2-opencl = Y\ngpg-opencl = Y' > john-local.conf && ../run/john -test-full=0
 
 addons:
   coverity_scan:

src/opencl_rawsha512_ng_fmt_plug.c
@@ -346,7 +346,7 @@ static void *get_salt(char *ciphertext)
 	} out;
 	char *p;
 	int i;
-	ciphertext += TAG_LENGTH;
+	ciphertext += XSHA512_TAG_LENGTH;
 	p = ciphertext;
 	for (i = 0; i < sizeof(out.c); i++) {
 		out.c[i] = (atoi16[ARCH_INDEX(*p)] << 4) | atoi16[ARCH_INDEX(p[1])];
@@ -927,9 +927,7 @@ struct fmt_main fmt_opencl_ng_xsha512 = {
 		FMT_CASE | FMT_8_BIT | FMT_SPLIT_UNIFIES_CASE,
 		{NULL},
 		{
-			FORMAT_TAG,
-			XSHA512_FORMAT_TAG,
-			NSLDAP_FORMAT_TAG
+			XSHA512_FORMAT_TAG
 		},
 		sha512_common_tests_xsha512
 	}, {