openwall/john

commit 8c488775e617e9debee7a6956523612fb2f5d1b3

Additional normalization of FORMAT_TAG defines.  #2196

src/opencl_ntlmv2_fmt_plug.c
@@ -41,6 +41,8 @@ john_register_one(&fmt_opencl_NTLMv2);
 
 #define FORMAT_LABEL            "ntlmv2-opencl"
 #define FORMAT_NAME             "NTLMv2 C/R"
+#define FORMAT_TAG           "$NETNTLMv2$"
+#define FORMAT_TAG_LEN       (sizeof(FORMAT_TAG)-1)
 #define ALGORITHM_NAME          "MD4 HMAC-MD5 OpenCL"
 #define BENCHMARK_COMMENT       ""
 #define BENCHMARK_LENGTH        0
@@ -217,12 +219,12 @@ static void *get_salt(char *ciphertext)
 	memset(binary_salt, 0, SALT_SIZE_MAX);
 
 	/* Calculate identity length */
-	for (pos = ciphertext + 11; *pos != '$'; pos++);
+	for (pos = ciphertext + FORMAT_TAG_LEN; *pos != '$'; pos++);
 
 	/* Convert identity (username + domain) string to NT unicode */
 	identity_length = enc_to_utf16((UTF16*)binary_salt, SALT_MAX_LENGTH,
-	                               (unsigned char *)ciphertext + 11,
-	                               pos - (ciphertext + 11)) * sizeof(UTF16);
+	                               (unsigned char *)ciphertext + FORMAT_TAG_LEN,
+	                               pos - (ciphertext + FORMAT_TAG_LEN)) * sizeof(UTF16);
 	binary_salt[identity_length] = 0x80;
 
 	/* Set length of last MD5 block */
@@ -317,7 +319,7 @@ static void reset(struct db_main *db)
 		                       2 * ocl_v_width * max_len, gws_limit, db);
 
 		//Auto tune execution from shared/included code.
-		autotune_run(self, 11, gws_limit, 500);
+		autotune_run(self, FORMAT_TAG_LEN, gws_limit, 500);
 	}
 }
 
@@ -329,12 +331,12 @@ static int valid(char *ciphertext, struct fmt_main *self)
 	int saltlen;
 
 	if (ciphertext == NULL) return 0;
-	else if (strncmp(ciphertext, "$NETNTLMv2$", 11)!=0) return 0;
+	else if (strncmp(ciphertext, FORMAT_TAG, FORMAT_TAG_LEN)!=0) return 0;
 
 	if (strlen(ciphertext) > TOTAL_LENGTH)
 		return 0;
 
-	pos = &ciphertext[11];
+	pos = &ciphertext[FORMAT_TAG_LEN];
 
 	/* Validate Username and Domain Length */
 	for (pos2 = pos; *pos2 != '$'; pos2++)
@@ -399,7 +401,7 @@ static char *prepare(char *split_fields[10], struct fmt_main *self)
 	char *uid = split_fields[2];
 	char *identity = NULL, *tmp;
 
-	if (!strncmp(split_fields[1], "$NETNTLMv2$", 11))
+	if (!strncmp(split_fields[1], FORMAT_TAG, FORMAT_TAG_LEN))
 		return split_fields[1];
 	if (!split_fields[0]||!split_fields[2]||!split_fields[3]||!split_fields[4]||!split_fields[5])
 		return split_fields[1];
@@ -423,7 +425,7 @@ static char *prepare(char *split_fields[10], struct fmt_main *self)
 		strcat(identity, uid);
 	}
 	tmp = (char *) mem_alloc(11 + strlen(identity) + 1 + strlen(srv_challenge) + 1 + strlen(nethashv2) + 1 + strlen(cli_challenge) + 1);
-	sprintf(tmp, "$NETNTLMv2$%s$%s$%s$%s", identity, srv_challenge, nethashv2, cli_challenge);
+	sprintf(tmp, "%s%s$%s$%s$%s", FORMAT_TAG, identity, srv_challenge, nethashv2, cli_challenge);
 	MEM_FREE(identity);
 
 	if (valid(tmp, self)) {
@@ -442,12 +444,12 @@ static char *split(char *ciphertext, int index, struct fmt_main *pFmt)
 	int identity_length = 0;
 
 	/* Calculate identity length */
-	for (pos = ciphertext + 11; *pos != '$'; pos++);
-	identity_length = pos - (ciphertext + 11);
+	for (pos = ciphertext + FORMAT_TAG_LEN; *pos != '$'; pos++);
+	identity_length = pos - (ciphertext + FORMAT_TAG_LEN);
 
 	memset(out, 0, sizeof(out));
 	memcpy(out, ciphertext, strlen(ciphertext));
-	strlwr(&out[12 + identity_length]); /* Exclude: $NETNTLMv2$USERDOMAIN$ */
+	strlwr(&out[FORMAT_TAG_LEN + identity_length + 1]); /* Exclude: $NETNTLMv2$USERDOMAIN$ */
 
 	return out;
 }
@@ -460,10 +462,10 @@ static void *get_binary(char *ciphertext)
 
 	if (!binary) binary = mem_alloc_tiny(DIGEST_SIZE, MEM_ALIGN_WORD);
 
-	for (pos = ciphertext + 11; *pos != '$'; pos++);
-	identity_length = pos - (ciphertext + 11);
+	for (pos = ciphertext + FORMAT_TAG_LEN; *pos != '$'; pos++);
+	identity_length = pos - (ciphertext + FORMAT_TAG_LEN);
 
-	ciphertext += 11 + identity_length + 1 + SERVER_CHALL_LENGTH + 1;
+	ciphertext += FORMAT_TAG_LEN + identity_length + 1 + SERVER_CHALL_LENGTH + 1;
 	for (i=0; i<DIGEST_SIZE; i++)
 	{
 		binary[i] = (atoi16[ARCH_INDEX(ciphertext[i*2])])<<4;
@@ -613,7 +615,7 @@ struct fmt_main fmt_opencl_NTLMv2 = {
 		MAX_KEYS_PER_CRYPT,
 		FMT_CASE | FMT_8_BIT | FMT_SPLIT_UNIFIES_CASE | FMT_UNICODE | FMT_UTF8,
 		{ NULL },
-		{ NULL },
+		{ FORMAT_TAG },
 		tests
 	}, {
 		init,

src/loader.c
@@ -1144,7 +1144,8 @@ static void ldr_load_pot_line(struct db_main *db, char *line)
 		for (;i < FMT_SIGNATURES && format->params.signature[i] && !valid; ++i) {
 			if (!strncmp(ciphertext, format->params.signature[i],
 			             strlen(format->params.signature[i])) &&
-				     ldr_isa_pot_source(ciphertext)) {
+			             strnlen(ciphertext, MAX_CIPHERTEXT_SIZE + 2) > MAX_CIPHERTEXT_SIZE &&
+			             ldr_isa_pot_source(ciphertext)) {
 					     valid = 1;
 			}
 		}

src/odf_fmt_plug.c
@@ -38,7 +38,7 @@ john_register_one(&fmt_odf);
 
 #define FORMAT_LABEL		"ODF"
 #define FORMAT_TAG		"$odf$*"
-#define FORAMT_TAG_LEN	(sizeof(FORMAT_TAG)-1)
+#define FORMAT_TAG_LEN	(sizeof(FORMAT_TAG)-1)
 #define FORMAT_NAME		""
 #ifdef SIMD_COEF_32
 #define ALGORITHM_NAME		"SHA1/SHA256 " SHA1_ALGORITHM_NAME " BF/AES"
@@ -116,11 +116,11 @@ static int valid(char *ciphertext, struct fmt_main *self)
 	char *keeptr;
 	char *p;
 	int res;
-	if (strncmp(ciphertext, FORMAT_TAG, FORAMT_TAG_LEN))
+	if (strncmp(ciphertext, FORMAT_TAG, FORMAT_TAG_LEN))
 		return 0;
 	ctcopy = strdup(ciphertext);
 	keeptr = ctcopy;
-	ctcopy += FORAMT_TAG_LEN;
+	ctcopy += FORMAT_TAG_LEN;
 	if ((p = strtokm(ctcopy, "*")) == NULL)	/* cipher type */
 		goto err;
 	if (strlen(p) != 1)
@@ -195,7 +195,7 @@ static void *get_salt(char *ciphertext)
 	char *p;
 	static struct custom_salt cs;
 	memset(&cs, 0, sizeof(cs));
-	ctcopy += FORAMT_TAG_LEN;	/* skip over "$odf$*" */
+	ctcopy += FORMAT_TAG_LEN;	/* skip over "$odf$*" */
 	p = strtokm(ctcopy, "*");
 	cs.cipher_type = atoi(p);
 	p = strtokm(NULL, "*");
@@ -241,7 +241,7 @@ static void *get_binary(char *ciphertext)
 	char *ctcopy = strdup(ciphertext);
 	char *keeptr = ctcopy;
 
-	ctcopy += FORAMT_TAG_LEN;	/* skip over "$odf$*" */
+	ctcopy += FORMAT_TAG_LEN;	/* skip over "$odf$*" */
 	strtokm(ctcopy, "*");
 	strtokm(NULL, "*");
 	strtokm(NULL, "*");

src/opencl_7z_fmt_plug.c
@@ -33,7 +33,7 @@ john_register_one(&fmt_opencl_sevenzip);
 #define FORMAT_LABEL		"7z-opencl"
 #define FORMAT_NAME		"7-Zip"
 #define FORMAT_TAG		"$7z$"
-#define TAG_LENGTH		4
+#define TAG_LENGTH		(sizeof(FORMAT_TAG)-1)
 #define ALGORITHM_NAME		"SHA256 OPENCL AES"
 #define BENCHMARK_COMMENT	" (512K iterations)"
 #define BENCHMARK_LENGTH	0
@@ -384,7 +384,7 @@ static void *get_salt(char *ciphertext)
 
 	memset(cs, 0, SALT_SIZE);
 
-	ctcopy += 4;
+	ctcopy += TAG_LENGTH;
 	p = strtokm(ctcopy, "$");
 	cs->type = atoi(p);
 	p = strtokm(NULL, "$");
@@ -652,7 +652,7 @@ struct fmt_main fmt_opencl_sevenzip = {
 		{
 			"iteration count",
 		},
-		{ "$7z$" },
+		{ FORMAT_TAG },
 		sevenzip_tests
 	}, {
 		init,

src/opencl_krb5pa-md5_fmt_plug.c
@@ -48,6 +48,10 @@ john_register_one(&fmt_opencl_krb5pa_md5);
 
 #define FORMAT_LABEL       "krb5pa-md5-opencl"
 #define FORMAT_NAME        "Kerberos 5 AS-REQ Pre-Auth etype 23" /* md4, rc4-hmac-md5 */
+#define FORMAT_TAG         "$krb5pa$23$"
+#define FORMAT_TAG_LEN     (sizeof(FORMAT_TAG)-1)
+#define FORMAT_TAG2        "$mskrb5$"
+#define FORMAT_TAG2_LEN    (sizeof(FORMAT_TAG2)-1)
 #define ALGORITHM_NAME     "MD4 HMAC-MD5 RC4 OpenCL"
 #define BENCHMARK_COMMENT  ""
 #define BENCHMARK_LENGTH   -1000
@@ -408,7 +412,7 @@ static char *split(char *ciphertext, int index, struct fmt_main *self)
 	static char out[TOTAL_LENGTH + 1];
 	char *data;
 
-	if (!strncmp(ciphertext, "$mskrb5$", 8)) {
+	if (!strncmp(ciphertext, FORMAT_TAG2, FORMAT_TAG2_LEN)) {
 		char in[TOTAL_LENGTH + 1];
 		char *c, *t;
 
@@ -417,13 +421,13 @@ static char *split(char *ciphertext, int index, struct fmt_main *self)
 		t = strrchr(in, '$'); *t++ = 0;
 		c = strrchr(in, '$'); *c++ = 0;
 
-		snprintf(out, sizeof(out), "$krb5pa$23$$$$%s%s", t, c);
+		snprintf(out, sizeof(out), "%s$$$%s%s", FORMAT_TAG, t, c);
 	} else {
 		char *tc;
 
 		tc = strrchr(ciphertext, '$');
 
-		snprintf(out, sizeof(out), "$krb5pa$23$$$$%s", ++tc);
+		snprintf(out, sizeof(out), "%s$$$%s", FORMAT_TAG, ++tc);
 	}
 
 	data = out + strlen(out) - 2 * (CHECKSUM_SIZE + TIMESTAMP_SIZE) - 1;
@@ -456,8 +460,8 @@ static int valid(char *ciphertext, struct fmt_main *self)
 {
 	char *data = ciphertext, *p;
 
-	if (!strncmp(ciphertext, "$mskrb5$", 8)) {
-		data += 8;
+	if (!strncmp(ciphertext, FORMAT_TAG2, FORMAT_TAG2_LEN)) {
+		data += FORMAT_TAG2_LEN;
 
 		// user field
 		p = strchr(data, '$');
@@ -485,8 +489,8 @@ static int valid(char *ciphertext, struct fmt_main *self)
 			return 0;
 
 		return 1;
-	} else if (!strncmp(ciphertext, "$krb5pa$23$", 11)) {
-		data += 11;
+	} else if (!strncmp(ciphertext, FORMAT_TAG, FORMAT_TAG_LEN)) {
+		data += FORMAT_TAG_LEN;
 
 		// user field
 		p = strchr(data, '$');
@@ -567,7 +571,7 @@ struct fmt_main fmt_opencl_krb5pa_md5 = {
 		MAX_KEYS_PER_CRYPT,
 		FMT_CASE | FMT_8_BIT | FMT_SPLIT_UNIFIES_CASE | FMT_UNICODE | FMT_UTF8,
 		{ NULL },
-		{ NULL },
+		{ FORMAT_TAG, FORMAT_TAG2 },
 		tests
 	}, {
 		init,

src/opencl_krb5pa-sha1_fmt_plug.c
@@ -61,6 +61,8 @@ john_register_one(&fmt_opencl_krb5pa_sha1);
 
 #define FORMAT_LABEL		"krb5pa-sha1-opencl"
 #define FORMAT_NAME		"Kerberos 5 AS-REQ Pre-Auth etype 17/18" /* aes-cts-hmac-sha1-96 */
+#define FORMAT_TAG           "$krb5pa$"
+#define FORMAT_TAG_LEN       (sizeof(FORMAT_TAG)-1)
 #define ALGORITHM_NAME		"PBKDF2-SHA1 OpenCL"
 #define BENCHMARK_COMMENT	""
 #define BENCHMARK_LENGTH	-1001
@@ -379,9 +381,9 @@ static int valid(char *ciphertext, struct fmt_main *self)
 	int type, saltlen = 0;
 
 	// tag is mandatory
-	if (strncmp(ciphertext, "$krb5pa$", 8) != 0)
+	if (strncmp(ciphertext, FORMAT_TAG, FORMAT_TAG_LEN) != 0)
 		return 0;
-	data += 8;
+	data += FORMAT_TAG_LEN;
 
 	// etype field, 17 or 18
 	p = strchr(data, '$');
@@ -445,7 +447,7 @@ static void *get_salt(char *ciphertext)
 	static struct custom_salt cs;
 
 	memset(&cs, 0, sizeof(cs));
-	ctcopy += 8;
+	ctcopy += FORMAT_TAG_LEN;
 	p = strtokm(ctcopy, "$");
 	cs.etype = atoi(p);
 	p = strtokm(NULL, "$");
@@ -524,7 +526,7 @@ static char *split(char *ciphertext, int index, struct fmt_main *pFmt)
 		snprintf(salt, sizeof(salt), "%s%s", r, u);
 		s = salt;
 	}
-	snprintf(out, sizeof(out), "$krb5pa$%s$%s$%s$%s$%s", e, u, r, s, tc);
+	snprintf(out, sizeof(out), "%s%s$%s$%s$%s$%s", FORMAT_TAG, e, u, r, s, tc);
 
 	data = out + strlen(out) - 2 * (CHECKSUM_SIZE + TIMESTAMP_SIZE) - 1;
 	strlwr(data);
@@ -796,7 +798,7 @@ struct fmt_main fmt_opencl_krb5pa_sha1 = {
 		MAX_KEYS_PER_CRYPT,
 		FMT_CASE | FMT_8_BIT | FMT_SPLIT_UNIFIES_CASE | FMT_OMP,
 		{ NULL },
-		{ NULL },
+		{ FORMAT_TAG },
 		tests
 	}, {
 		init,

src/opencl_lm_fmt_plug.c
@@ -24,6 +24,8 @@ john_register_one(&fmt_opencl_lm);
 #include "memdbg.h"
 
 #define FORMAT_NAME			""
+#define FORMAT_TAG           "$LM$"
+#define FORMAT_TAG_LEN       (sizeof(FORMAT_TAG)-1)
 
 #define BENCHMARK_COMMENT		""
 #define BENCHMARK_LENGTH		-1
@@ -84,9 +86,9 @@ static int valid(char *ciphertext, struct fmt_main *self)
 			return 1;
 	}
 
-	if (strncmp(ciphertext, "$LM$", 4)) return 0;
+	if (strncmp(ciphertext, FORMAT_TAG, FORMAT_TAG_LEN)) return 0;
 
-	for (pos = &ciphertext[4]; atoi16[ARCH_INDEX(*pos)] != 0x7F; pos++);
+	for (pos = &ciphertext[FORMAT_TAG_LEN]; atoi16[ARCH_INDEX(*pos)] != 0x7F; pos++);
 	if (*pos || pos - ciphertext != 20) return 0;
 
 	return 1;
@@ -94,26 +96,21 @@ static int valid(char *ciphertext, struct fmt_main *self)
 
 static char *split(char *ciphertext, int index, struct fmt_main *self)
 {
-	static char out[21];
+	static char out[FORMAT_TAG_LEN + 16 + 1] = "$LM$";
 
 /* We don't just "return ciphertext" for already split hashes since we may
  * need to convert hashes stored by older versions of John to all-lowercase. */
-	if (!strncmp(ciphertext, "$LM$", 4))
-		ciphertext += 4;
-
-	out[0] = '$';
-	out[1] = 'L';
-	out[2] = 'M';
-	out[3] = '$';
+	if (!strncmp(ciphertext, FORMAT_TAG, FORMAT_TAG_LEN))
+		ciphertext += FORMAT_TAG_LEN;
 
 	if (index)
-		memcpy(&out[4], &ciphertext[16], 16);
+		memcpy(&out[FORMAT_TAG_LEN], &ciphertext[16], 16);
 	else
-		memcpy(&out[4], ciphertext, 16);
+		memcpy(&out[FORMAT_TAG_LEN], ciphertext, 16);
 
 	out[20] = 0;
 
-	strlwr(&out[4]);
+	strlwr(&out[FORMAT_TAG_LEN]);
 
 	return out;
 }
@@ -193,7 +190,7 @@ struct fmt_main fmt_opencl_lm = {
 		MAX_KEYS_PER_CRYPT,
 		FMT_8_BIT | FMT_BS | FMT_TRUNC | FMT_SPLIT_UNIFIES_CASE | FMT_REMOVE,
 		{ NULL },
-		{ NULL },
+		{ FORMAT_TAG },
 		tests
 	}, {
 		init,

src/opencl_nt_fmt_plug.c
@@ -45,6 +45,8 @@ john_register_one(&fmt_opencl_NT);
 
 #define FORMAT_LABEL        "nt-opencl"
 #define FORMAT_NAME         "NT"
+#define FORMAT_TAG          "$NT$"
+#define FORMAT_TAG_LEN      (sizeof(FORMAT_TAG)-1)
 #define ALGORITHM_NAME      "MD4 OpenCL"
 #define BENCHMARK_COMMENT   ""
 #define BENCHMARK_LENGTH    -1
@@ -344,20 +346,20 @@ static void init(struct fmt_main *_self)
 
 static char *split(char *ciphertext, int index, struct fmt_main *self)
 {
-	static char out[CIPHERTEXT_LENGTH + 4 + 1];
+	static char out[CIPHERTEXT_LENGTH + FORMAT_TAG_LEN + 1];
 
-	if (!strncmp(ciphertext, "$NT$", 4))
-		ciphertext += 4;
+	if (!strncmp(ciphertext, FORMAT_TAG, FORMAT_TAG_LEN))
+		ciphertext += FORMAT_TAG_LEN;
 
 	out[0] = '$';
 	out[1] = 'N';
 	out[2] = 'T';
 	out[3] = '$';
 
-	memcpy(&out[4], ciphertext, 32);
+	memcpy(&out[FORMAT_TAG_LEN], ciphertext, 32);
 	out[36] = 0;
 
-	strlwr(&out[4]);
+	strlwr(&out[FORMAT_TAG_LEN]);
 
 	return out;
 }
@@ -366,8 +368,8 @@ static int valid(char *ciphertext, struct fmt_main *self)
 {
         char *pos;
 
-	if (!strncmp(ciphertext, "$NT$", 4))
-		ciphertext += 4;
+	if (!strncmp(ciphertext, FORMAT_TAG, FORMAT_TAG_LEN))
+		ciphertext += FORMAT_TAG_LEN;
 
         for (pos = ciphertext; atoi16[ARCH_INDEX(*pos)] != 0x7F; pos++);
 
@@ -382,11 +384,11 @@ static int valid(char *ciphertext, struct fmt_main *self)
 // Note, we address the user id inside loader.
 static char *prepare(char *split_fields[10], struct fmt_main *self)
 {
-	static char out[33+5];
+	static char out[33+FORMAT_TAG_LEN+1];
 
 	if (!valid(split_fields[1], self)) {
 		if (split_fields[3] && strlen(split_fields[3]) == 32) {
-			sprintf(out, "$NT$%s", split_fields[3]);
+			sprintf(out, "%s%s", FORMAT_TAG, split_fields[3]);
 			if (valid(out,self))
 				return out;
 		}
@@ -607,7 +609,7 @@ struct fmt_main fmt_opencl_NT = {
 		MAX_KEYS_PER_CRYPT,
 		FMT_CASE | FMT_8_BIT | FMT_SPLIT_UNIFIES_CASE | FMT_UNICODE | FMT_UTF8 | FMT_REMOVE,
 		{ NULL },
-		{ NULL },
+		{ FORMAT_TAG },
 		tests
 	}, {
 		init,

commit 40e5323cf5b413f847a7b00fcab243baf09edc22

Fix for another bug introduced in 8c48877.

src/opencl_ntlmv2_fmt_plug.c
@@ -319,7 +319,7 @@ static void reset(struct db_main *db)
 		                       2 * ocl_v_width * max_len, gws_limit, db);
 
 		//Auto tune execution from shared/included code.
-		autotune_run(self, FORMAT_TAG_LEN, gws_limit, 500);
+		autotune_run(self, 11, gws_limit, 500);
 	}
 }
 