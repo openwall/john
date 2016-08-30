openwall/john

commit 744cdb91dded9a4292ac2e79210bc1ea94867668

zip2john: Change default to never use file magic. Closes #1569.

src/zip2john.c
@@ -114,7 +114,7 @@
 
 #define LARGE_ENOUGH 8192
 
-static int checksum_only=0, use_magic=1;
+static int checksum_only=0, use_magic=0;
 static int force_2_byte_checksum = 0;
 static char *ascii_fname, *only_fname;
 static int inline_thr = MAX_INLINE_SIZE;
@@ -609,9 +609,9 @@ int zip2john(int argc, char **argv)
 			checksum_only = 1;
 			fprintf(stderr, "Outputing hashes that are 'checksum ONLY' hashes\n");
 			break;
-		case 'n':
-			use_magic = 0;
-			fprintf(stderr, "Ignoring any checking of file 'magic' signatures\n");
+		case 'm':
+			use_magic = 1;
+			fprintf(stderr, "Using file 'magic' signatures if applicable (not 100%% safe)\n");
 			break;
 		case '2':
 			force_2_byte_checksum = 1;

commit 7b103c7abfbaaa0a6ef9cf21d8e47b5e47157517

zip2john: Fix a bug introduced in 744cdb91 by yours truly: File magic could not be used.

src/zip2john.c
@@ -614,7 +614,7 @@ int zip2john(int argc, char **argv)
 	int c;
 
 	/* Parse command line */
-	while ((c = getopt(argc, argv, "a:o:cn2")) != -1) {
+	while ((c = getopt(argc, argv, "a:o:cm2")) != -1) {
 		switch (c) {
 		case 'a':
 			ascii_fname = optarg;