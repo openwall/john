openwall/john

commit 1a666cb797e18745ae98f426c02f8180eb24512f

Ensure reset(realdb) is called with --skip-self-tests. Closes #2256. See also #2090 and #1967.

src/formats.c
@@ -378,8 +378,7 @@ static char *fmt_self_test_body(struct fmt_main *format,
 	if (options.flags & FLG_NOTESTS) {
 		fmt_init(format);
 		dyna_salt_init(format);
-		if (db)
-			format->methods.reset(db);
+		format->methods.reset(db->real);
 		format->private.initialized = 2;
 		format->methods.clear_keys();
 		return NULL;

src/john.c
@@ -1608,9 +1608,8 @@ static void john_run(void)
 			struct db_main *test_db = 0;
 			char *where;
 
-			if (!(options.flags & FLG_NOTESTS))
-				test_db = ldr_init_test_db(database.format,
-				                           &database);
+			test_db = ldr_init_test_db(database.format,
+			                           &database);
 			where = fmt_self_test(database.format, test_db);
 			ldr_free_test_db(test_db);
 			if (where) {

commit 9e4e287c27fac0b44a4ebc5056619d3621244cc9

formats.c: Fix a problem with gpu-side mask formats, introduced in 1a666cb. Tricky business.

src/formats.c
@@ -378,7 +378,10 @@ static char *fmt_self_test_body(struct fmt_main *format,
 	if (options.flags & FLG_NOTESTS) {
 		fmt_init(format);
 		dyna_salt_init(format);
-		format->methods.reset(db->real);
+		if (db->real)
+			format->methods.reset(db->real);
+		else
+			format->methods.reset(db);
 		format->private.initialized = 2;
 		format->methods.clear_keys();
 		return NULL;