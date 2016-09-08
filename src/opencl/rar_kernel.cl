openwall/john

commit c5c1fd3a6f1c803ae9b51d48fba685c984ca0461

RAR-opencl: Use shared SHA-1 code.

src/opencl/rar_kernel.cl
@@ -19,374 +19,32 @@
 
 #include "opencl_device_info.h"
 #include "opencl_misc.h"
+#include "opencl_sha1.h"
 
 #define UNICODE_LENGTH		(2 * PLAINTEXT_LENGTH)
 #define ROUNDS			0x40000
 
-/* SHA1 constants and IVs */
-#define H1	0x67452301
-#define H2	0xEFCDAB89
-#define H3	0x98BADCFE
-#define H4	0x10325476
-#define H5	0xC3D2E1F0
-
 /*
- * Raw'n'lean sha1, state kept in output buffer.
- * This version does several blocks at a time and
- * does not thrash the input buffer.
+ * This version does several blocks at a time
  */
-inline void sha1_mblock(uint *Win, uint *out, uint blocks)
+inline void sha1_mblock(uint *W, uint *out, uint blocks)
 {
-	uint W[16], output[5];
-	uint A, B, C, D, E, temp;
+	uint output[5];
+	uint A, B, C, D, E, temp, r[16];
 
 	for (temp = 0; temp < 5; temp++)
 		output[temp] = out[temp];
 
 	while (blocks--) {
-		A = output[0];
-		B = output[1];
-		C = output[2];
-		D = output[3];
-		E = output[4];
-
-		for (temp = 0; temp < 16; temp++)
-			W[temp] = Win[temp];
-#undef R
-#define R(t)	  \
-		( \
-			temp = W[(t -  3) & 0x0F] ^ W[(t - 8) & 0x0F] ^ \
-			W[(t - 14) & 0x0F] ^ W[ t      & 0x0F], \
-			( W[t & 0x0F] = rotate(temp, 1U) ) \
-			)
-
-#undef P
-#define P(a,b,c,d,e,x)	  \
-		{ \
-			e += rotate(a, 5U) + F(b,c,d) + K + x; \
-			b = rotate(b, 30U); \
-		}
-
-#undef F // F1
-#if HAVE_LUT3
-#define F(x, y, z) lut3(x, y, z, 0xca)
-#elif USE_BITSELECT
-#define F(x, y, z) bitselect(z, y, x)
-#elif HAVE_ANDNOT
-#define F(x, y, z) ((x & y) ^ ((~x) & z))
-#else
-#define F(x, y, z) (z ^ (x & (y ^ z)))
-#endif
-
-#define K		0x5A827999
-
-		P( A, B, C, D, E, W[0]  );
-		P( E, A, B, C, D, W[1]  );
-		P( D, E, A, B, C, W[2]  );
-		P( C, D, E, A, B, W[3]  );
-		P( B, C, D, E, A, W[4]  );
-		P( A, B, C, D, E, W[5]  );
-		P( E, A, B, C, D, W[6]  );
-		P( D, E, A, B, C, W[7]  );
-		P( C, D, E, A, B, W[8]  );
-		P( B, C, D, E, A, W[9]  );
-		P( A, B, C, D, E, W[10] );
-		P( E, A, B, C, D, W[11] );
-		P( D, E, A, B, C, W[12] );
-		P( C, D, E, A, B, W[13] );
-		P( B, C, D, E, A, W[14] );
-		P( A, B, C, D, E, W[15] );
-		P( E, A, B, C, D, R(16) );
-		P( D, E, A, B, C, R(17) );
-		P( C, D, E, A, B, R(18) );
-		P( B, C, D, E, A, R(19) );
-
-#undef F // F2
-#if HAVE_LUT3
-#define F(x, y, z) lut3(x, y, z, 0x96)
-#else
-#define F(x, y, z) (x ^ y ^ z)
-#endif
-
-#undef K
-#define K		0x6ED9EBA1
-
-		P( A, B, C, D, E, R(20) );
-		P( E, A, B, C, D, R(21) );
-		P( D, E, A, B, C, R(22) );
-		P( C, D, E, A, B, R(23) );
-		P( B, C, D, E, A, R(24) );
-		P( A, B, C, D, E, R(25) );
-		P( E, A, B, C, D, R(26) );
-		P( D, E, A, B, C, R(27) );
-		P( C, D, E, A, B, R(28) );
-		P( B, C, D, E, A, R(29) );
-		P( A, B, C, D, E, R(30) );
-		P( E, A, B, C, D, R(31) );
-		P( D, E, A, B, C, R(32) );
-		P( C, D, E, A, B, R(33) );
-		P( B, C, D, E, A, R(34) );
-		P( A, B, C, D, E, R(35) );
-		P( E, A, B, C, D, R(36) );
-		P( D, E, A, B, C, R(37) );
-		P( C, D, E, A, B, R(38) );
-		P( B, C, D, E, A, R(39) );
-
-#undef F // F3
-#if HAVE_LUT3
-#define F(x, y, z) lut3(x, y, z, 0xe8)
-#elif USE_BITSELECT
-#define F(x, y, z) bitselect(x, y, (z) ^ (x))
-#else
-#define F(x, y, z) ((x & y) | (z & (x | y)))
-#endif
-
-#undef K
-#define K		0x8F1BBCDC
-
-		P( A, B, C, D, E, R(40) );
-		P( E, A, B, C, D, R(41) );
-		P( D, E, A, B, C, R(42) );
-		P( C, D, E, A, B, R(43) );
-		P( B, C, D, E, A, R(44) );
-		P( A, B, C, D, E, R(45) );
-		P( E, A, B, C, D, R(46) );
-		P( D, E, A, B, C, R(47) );
-		P( C, D, E, A, B, R(48) );
-		P( B, C, D, E, A, R(49) );
-		P( A, B, C, D, E, R(50) );
-		P( E, A, B, C, D, R(51) );
-		P( D, E, A, B, C, R(52) );
-		P( C, D, E, A, B, R(53) );
-		P( B, C, D, E, A, R(54) );
-		P( A, B, C, D, E, R(55) );
-		P( E, A, B, C, D, R(56) );
-		P( D, E, A, B, C, R(57) );
-		P( C, D, E, A, B, R(58) );
-		P( B, C, D, E, A, R(59) );
-
-#undef F // F4
-#if HAVE_LUT3
-#define F(x, y, z) lut3(x, y, z, 0x96)
-#else
-#define F(x, y, z) (x ^ y ^ z)
-#endif
-
-#undef K
-#define K		0xCA62C1D6
-
-		P( A, B, C, D, E, R(60) );
-		P( E, A, B, C, D, R(61) );
-		P( D, E, A, B, C, R(62) );
-		P( C, D, E, A, B, R(63) );
-		P( B, C, D, E, A, R(64) );
-		P( A, B, C, D, E, R(65) );
-		P( E, A, B, C, D, R(66) );
-		P( D, E, A, B, C, R(67) );
-		P( C, D, E, A, B, R(68) );
-		P( B, C, D, E, A, R(69) );
-		P( A, B, C, D, E, R(70) );
-		P( E, A, B, C, D, R(71) );
-		P( D, E, A, B, C, R(72) );
-		P( C, D, E, A, B, R(73) );
-		P( B, C, D, E, A, R(74) );
-		P( A, B, C, D, E, R(75) );
-		P( E, A, B, C, D, R(76) );
-		P( D, E, A, B, C, R(77) );
-		P( C, D, E, A, B, R(78) );
-		P( B, C, D, E, A, R(79) );
-
-#undef K
-#undef F
-
-		output[0] += A;
-		output[1] += B;
-		output[2] += C;
-		output[3] += D;
-		output[4] += E;
-
-		Win += 16;
+		sha1_block(uint, W, output);
+		W += 16;
 	}
 
 	for (temp = 0; temp < 5; temp++)
 		out[temp] = output[temp];
 }
 
-/* This version has less overhead but destroys input */
-inline void sha1_block(MAYBE_VECTOR_UINT *W, MAYBE_VECTOR_UINT *output) {
-	MAYBE_VECTOR_UINT A, B, C, D, E, temp;
-
-	A = output[0];
-	B = output[1];
-	C = output[2];
-	D = output[3];
-	E = output[4];
-
-#undef R
-#define R(t)	  \
-	( \
-		temp = W[(t -  3) & 0x0F] ^ W[(t - 8) & 0x0F] ^ \
-		W[(t - 14) & 0x0F] ^ W[ t      & 0x0F], \
-		( W[t & 0x0F] = rotate(temp, 1U) ) \
-		)
-
-#undef P
-#define P(a,b,c,d,e,x)	\
-	{ \
-		e += rotate(a, 5U) + F(b,c,d) + K + x; \
-		b = rotate(b, 30U); \
-	}
-
-#undef F // F1
-#if HAVE_LUT3
-#define F(x, y, z) lut3(x, y, z, 0xca)
-#elif USE_BITSELECT
-#define F(x, y, z) bitselect(z, y, x)
-#else
-#if HAVE_ANDNOT
-#define F(x, y, z) ((x & y) ^ ((~x) & z))
-#else
-#define F(x, y, z) (z ^ (x & (y ^ z)))
-#endif
-#endif
-
-#define K		0x5A827999
-
-	P( A, B, C, D, E, W[0]  );
-	P( E, A, B, C, D, W[1]  );
-	P( D, E, A, B, C, W[2]  );
-	P( C, D, E, A, B, W[3]  );
-	P( B, C, D, E, A, W[4]  );
-	P( A, B, C, D, E, W[5]  );
-	P( E, A, B, C, D, W[6]  );
-	P( D, E, A, B, C, W[7]  );
-	P( C, D, E, A, B, W[8]  );
-	P( B, C, D, E, A, W[9]  );
-	P( A, B, C, D, E, W[10] );
-	P( E, A, B, C, D, W[11] );
-	P( D, E, A, B, C, W[12] );
-	P( C, D, E, A, B, W[13] );
-	P( B, C, D, E, A, W[14] );
-	P( A, B, C, D, E, W[15] );
-	P( E, A, B, C, D, R(16) );
-	P( D, E, A, B, C, R(17) );
-	P( C, D, E, A, B, R(18) );
-	P( B, C, D, E, A, R(19) );
-
-#undef F // F2
-#if HAVE_LUT3
-#define F(x, y, z) lut3(x, y, z, 0x96)
-#else
-#define F(x, y, z) (x ^ y ^ z)
-#endif
-
-#undef K
-#define K		0x6ED9EBA1
-
-	P( A, B, C, D, E, R(20) );
-	P( E, A, B, C, D, R(21) );
-	P( D, E, A, B, C, R(22) );
-	P( C, D, E, A, B, R(23) );
-	P( B, C, D, E, A, R(24) );
-	P( A, B, C, D, E, R(25) );
-	P( E, A, B, C, D, R(26) );
-	P( D, E, A, B, C, R(27) );
-	P( C, D, E, A, B, R(28) );
-	P( B, C, D, E, A, R(29) );
-	P( A, B, C, D, E, R(30) );
-	P( E, A, B, C, D, R(31) );
-	P( D, E, A, B, C, R(32) );
-	P( C, D, E, A, B, R(33) );
-	P( B, C, D, E, A, R(34) );
-	P( A, B, C, D, E, R(35) );
-	P( E, A, B, C, D, R(36) );
-	P( D, E, A, B, C, R(37) );
-	P( C, D, E, A, B, R(38) );
-	P( B, C, D, E, A, R(39) );
-
-#undef F // F3
-#if HAVE_LUT3
-#define F(x, y, z) lut3(x, y, z, 0xe8)
-#elif USE_BITSELECT
-#define F(x, y, z) bitselect(x, y, (z) ^ (x))
-#else
-#define F(x, y, z) ((x & y) | (z & (x | y)))
-#endif
-
-#undef K
-#define K		0x8F1BBCDC
-
-	P( A, B, C, D, E, R(40) );
-	P( E, A, B, C, D, R(41) );
-	P( D, E, A, B, C, R(42) );
-	P( C, D, E, A, B, R(43) );
-	P( B, C, D, E, A, R(44) );
-	P( A, B, C, D, E, R(45) );
-	P( E, A, B, C, D, R(46) );
-	P( D, E, A, B, C, R(47) );
-	P( C, D, E, A, B, R(48) );
-	P( B, C, D, E, A, R(49) );
-	P( A, B, C, D, E, R(50) );
-	P( E, A, B, C, D, R(51) );
-	P( D, E, A, B, C, R(52) );
-	P( C, D, E, A, B, R(53) );
-	P( B, C, D, E, A, R(54) );
-	P( A, B, C, D, E, R(55) );
-	P( E, A, B, C, D, R(56) );
-	P( D, E, A, B, C, R(57) );
-	P( C, D, E, A, B, R(58) );
-	P( B, C, D, E, A, R(59) );
-
-#undef F // F4
-#if HAVE_LUT3
-#define F(x, y, z)     lut3(x, y, z, 0x96)
-#else
-#define F(x, y, z)		(x ^ y ^ z)
-#endif
-
-#undef K
-#define K		0xCA62C1D6
-
-	P( A, B, C, D, E, R(60) );
-	P( E, A, B, C, D, R(61) );
-	P( D, E, A, B, C, R(62) );
-	P( C, D, E, A, B, R(63) );
-	P( B, C, D, E, A, R(64) );
-	P( A, B, C, D, E, R(65) );
-	P( E, A, B, C, D, R(66) );
-	P( D, E, A, B, C, R(67) );
-	P( C, D, E, A, B, R(68) );
-	P( B, C, D, E, A, R(69) );
-	P( A, B, C, D, E, R(70) );
-	P( E, A, B, C, D, R(71) );
-	P( D, E, A, B, C, R(72) );
-	P( C, D, E, A, B, R(73) );
-	P( B, C, D, E, A, R(74) );
-	P( A, B, C, D, E, R(75) );
-	P( E, A, B, C, D, R(76) );
-	P( D, E, A, B, C, R(77) );
-	P( C, D, E, A, B, R(78) );
-	P( B, C, D, E, A, R(79) );
-
-#undef K
-#undef F
-
-	output[0] += A;
-	output[1] += B;
-	output[2] += C;
-	output[3] += D;
-	output[4] += E;
-}
-
-#define sha1_init(output) {	  \
-		output[0] = H1; \
-		output[1] = H2; \
-		output[2] = H3; \
-		output[3] = H4; \
-		output[4] = H5; \
-	}
-
-inline void sha1_final(uint *W, uint *output, const uint tot_len)
+inline void sha1_empty_final(uint *W, uint *output, const uint tot_len)
 {
 	uint len = ((tot_len & 63) >> 2) + 1;
 
@@ -395,7 +53,7 @@ inline void sha1_final(uint *W, uint *output, const uint tot_len)
 	while (len < 15)
 		W[len++] = 0;
 	W[15] = tot_len << 3;
-	sha1_block(W, output);
+	sha1_block(uint, W, output);
 }
 
 __kernel void RarInit(__global uint *OutputBuf, __global uint *round)
@@ -457,16 +115,16 @@ __kernel void RarHashLoop(
 		PUTCHAR_BE(tempin, pwlen + 9, (round >> 8) & 255);
 
 #ifdef __OS_X__
-		/* This is the weirdest workaround. Using sha1_final()
+		/* This is the weirdest workaround. Using sha1_empty_final()
 		   works perfectly fine in the RarFinal() subkernel below. */
 		PUTCHAR_BE(tempin, pwlen + 11, 0x80);
 		for (i = pwlen + 12; i < 56; i++)
 			PUTCHAR_BE(tempin, i, 0);
 		tempin[14] = 0;
 		tempin[15] = (blocklen * (round + 1)) << 3;
-		sha1_block(tempin, tempout);
+		sha1_block(uint, tempin, tempout);
 #else
-		sha1_final(tempin, tempout, blocklen * (round + 1));
+		sha1_empty_final(tempin, tempout, blocklen * (round + 1));
 #endif
 		PUTCHAR_G(aes_iv, gid * 16 + (round >> 14), GETCHAR(tempout, 16));
 	}
@@ -504,7 +162,7 @@ __kernel void RarFinal(
 		output[i] = OutputBuf[i * gws + gid];
 
 	/* This is always an empty block (except length) */
-	sha1_final(block, output, (pw_len[gid] + 8 + 3) * ROUNDS);
+	sha1_empty_final(block, output, (pw_len[gid] + 8 + 3) * ROUNDS);
 
 	/* No endian-swap and we only use first 128 bits */
 	for (i = 0; i < 4; i++)

commit 2922e9ba0c1c58d1664f0a46e467953aea837542

c5c1fd3a broke RAR-opencl for OSX.

src/opencl/rar_kernel.cl
@@ -29,19 +29,19 @@
  */
 inline void sha1_mblock(uint *W, uint *out, uint blocks)
 {
+	uint i;
 	uint output[5];
-	uint A, B, C, D, E, temp, r[16];
 
-	for (temp = 0; temp < 5; temp++)
-		output[temp] = out[temp];
+	for (i = 0; i < 5; i++)
+		output[i] = out[i];
 
 	while (blocks--) {
 		sha1_block(uint, W, output);
 		W += 16;
 	}
 
-	for (temp = 0; temp < 5; temp++)
-		out[temp] = output[temp];
+	for (i = 0; i < 5; i++)
+		out[i] = output[i];
 }
 
 inline void sha1_empty_final(uint *W, uint *output, const uint tot_len)
@@ -104,27 +104,27 @@ __kernel void RarHashLoop(
 	if ((round % (ROUNDS / 16)) == 0)
 #endif
 	{
-		uint tempin[16], tempout[5];
+		uint W[16], tempout[5];
 
 		for (i = 0; i < 5; i++)
 			tempout[i] = output[i];
 		for (i = 0; i < (UNICODE_LENGTH + 8) / 4; i++)
-			tempin[i] = block[i];
+			W[i] = block[i];
 
-		PUTCHAR_BE(tempin, pwlen + 8, round & 255);
-		PUTCHAR_BE(tempin, pwlen + 9, (round >> 8) & 255);
+		PUTCHAR_BE(W, pwlen + 8, round & 255);
+		PUTCHAR_BE(W, pwlen + 9, (round >> 8) & 255);
 
 #ifdef __OS_X__
 		/* This is the weirdest workaround. Using sha1_empty_final()
 		   works perfectly fine in the RarFinal() subkernel below. */
-		PUTCHAR_BE(tempin, pwlen + 11, 0x80);
+		PUTCHAR_BE(W, pwlen + 11, 0x80);
 		for (i = pwlen + 12; i < 56; i++)
-			PUTCHAR_BE(tempin, i, 0);
-		tempin[14] = 0;
-		tempin[15] = (blocklen * (round + 1)) << 3;
-		sha1_block(uint, tempin, tempout);
+			PUTCHAR_BE(W, i, 0);
+		W[14] = 0;
+		W[15] = (blocklen * (round + 1)) << 3;
+		sha1_block(uint, W, tempout);
 #else
-		sha1_empty_final(tempin, tempout, blocklen * (round + 1));
+		sha1_empty_final(W, tempout, blocklen * (round + 1));
 #endif
 		PUTCHAR_G(aes_iv, gid * 16 + (round >> 14), GETCHAR(tempout, 16));
 	}