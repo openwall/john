0390610afee8bffae581edb01a35e122c9f1b271
magnum <john.magnum@hushmail.com>
2016-09-21 11:21:59 +0200

    misc.c: Fix a long-standing #ifdef-logic bug with MPI vs. fork causing pexit() to segfault due to a dangling 'else'.

--- src/misc.c
+++ src/misc.c
@@ -69,13 +69,18 @@ void real_pexit(char *file, int line, char *format, ...)
 {
 	va_list args;
 
-#if defined(HAVE_MPI) && !defined(_JOHN_MISC_NO_LOG)
+#if !defined(_JOHN_MISC_NO_LOG)
+#if HAVE_MPI
 	if (mpi_p > 1)
 		fprintf(stderr, "%u@%s: ", mpi_id + 1, mpi_name);
+#endif
+#if HAVE_MPI && OS_FORK
 	else
-#elif OS_FORK && !defined(_JOHN_MISC_NO_LOG)
+#endif
+#if OS_FORK
 	if (options.fork)
 		fprintf(stderr, "%u: ", options.node_min);
+#endif
 #endif
 
 	va_start(args, format);
