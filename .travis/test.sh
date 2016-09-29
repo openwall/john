fdd43b30620198a9ea689ca0d19d1673f79853ee
Claudio André <claudioandre.br@gmail.com>
2016-09-29 14:35:20 -0300

    TravisCI: add ASAN into gcc build step
    Odd design options due to Travis eccentricity.

---
+++ .travis/test.sh
@@ -0,0 +1,11 @@
+#!/bin/sh -e
+
+# There is a bug in echo -e in Travis
+echo '[Disabled:Formats]' > john-local.conf
+echo 'Raw-SHA512-free-opencl = Y' >> john-local.conf
+echo 'XSHA512-free-opencl = Y' >> john-local.conf
+echo 'gpg-opencl = Y' >> john-local.conf
+
+# Proper testing. Trusty AMD GPU drivers on Travis are fragile
+../run/john -test-full=0 --format=cpu
+../run/john -test-full=0 --format=opencl
