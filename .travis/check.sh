0f128a3ff0154297b46d43332cb7bf4bcd84366f
Claudio André <claudioandre.br@gmail.com>
2016-10-19 17:32:17 -0200

    CI: Add a TS check

--- .travis/check.sh
+++ .travis/check.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-if [[ "$FRESH" != "yes" ]]; then
+if [[ -z "$TEST" ]]; then
     cd src
+
+    # Prepare environment
+    sudo apt-get update -qq
+    sudo apt-get install libssl-dev yasm libgmp-dev libpcap-dev pkg-config debhelper libnet1-dev
+    sudo apt-get install fglrx-dev opencl-headers || true
 
     # Configure and build
     ./configure $ASAN
@@ -9,24 +14,53 @@ if [[ "$FRESH" != "yes" ]]; then
 
     ../.travis/test.sh
 
-else
-   docker run -v $HOME:/root -v $(pwd):/cwd ubuntu:latest sh -c ' \
+elif [[ "$TEST" == "fresh test" ]]; then
+    # ASAN using a 'recent' compiler
+    docker run -v $HOME:/root -v $(pwd):/cwd ubuntu:latest sh -c " \
       cd /cwd/src; \
       apt-get update -qq; \
       apt-get install -y build-essential libssl-dev yasm libgmp-dev libpcap-dev pkg-config debhelper libnet1-dev libbz2-dev; \
       ./configure --enable-asan; \
       make -sj4; \
-      ASAN="fresh" ../.travis/test.sh
-  '
+      export OPENCL="""$OPENCL"""; \
+      PROBLEM='slow' ../.travis/test.sh
+   "
 
-#   docker run -v $HOME:/root -v $(pwd):/cwd centos:latest sh -c ' \
-#      cd /cwd/src; \
-#      yum update -y; \
-#      yum groupinstall -y 'Development Tools'; \
-#      yum install -y openssl-devel gmp-devel libpcap-devel pkgconfig libnet-devel bzip2-devel; \
-#      ./configure --enable-asan; \
-#      make -sj4; \
-#      ../.travis/test.sh
-#  '
+elif [[ "$TEST" == "TS --restore" ]]; then
+    # Test Suite run
+    cd src
+
+    # Prepare environment
+    sudo apt-get update -qq
+    sudo apt-get install libssl-dev yasm libgmp-dev libpcap-dev pkg-config debhelper libnet1-dev
+
+    # Configure and build
+    ./configure
+    make -sj4
+
+    cd ..
+    git clone --depth 1 https://github.com/magnumripper/jtrTestSuite.git tests
+    cd tests
+    #export PERL_MM_USE_DEFAULT=1
+    (echo y;echo o conf prerequisites_policy follow;echo o conf commit)|cpan
+    cpan install Digest::MD5
+    ./jtrts.pl --restore
+
+elif [[ "$TEST" == "TS docker" ]]; then
+    # Test Suite run
+    docker run -v $HOME:/root -v $(pwd):/cwd ubuntu:xenial sh -c ' \
+      cd /cwd/src; \
+      apt-get update -qq; \
+      apt-get install -y build-essential libssl-dev yasm libgmp-dev libpcap-dev pkg-config debhelper libnet1-dev libbz2-dev git; \
+      ./configure; \
+      make -sj4; \
+      cd ..; \
+      git clone --depth 1 https://github.com/magnumripper/jtrTestSuite.git tests; \
+      cd tests; \
+      cpan install Digest::MD5; \
+      ./jtrts.pl --restore
+    '
+else
+    echo  "Nothing to do!!"
 fi
 
