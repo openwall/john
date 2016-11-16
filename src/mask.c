openwall/john

commit c19f75fb968495773dad1f153109ed915384ad25

Mask mode: Add ?h and ?H for hex digits (hashcat compatibility). Closes #2324.

src/mask.c
@@ -641,14 +641,16 @@ static char* plhdr2string(char p, int fmt_case)
 
 	case 'B': /* All high-bit */
 
-	case 'h': /* deprecated alias for B */
-		add_range(0x80, 0xff);
-		break;
-
 	case 'b': /* All (except NULL which we can't handle) */
 
-	case 'H': /* deprecated alias for b */
-		add_range(0x01, 0xff);
+	case 'h': /* Lower-case hex */
+		add_range('0', '9');
+		add_range('a', 'f');
+		break;
+
+	case 'H': /* Upper-case hex */
+		add_range('0', '9');
+		add_range('A', 'F');
 		break;
 
 	case 'a': /* Printable ASCII */

doc/MASK
@@ -17,6 +17,8 @@ A mask may consist of:
      reduction of keyspace for the win.
 - ?B all 8-bit (0x80-0xff)
 - ?b all (0x01-0xff) (the NULL character is currently not supported by core).
+- ?h lower-case HEX digits (0-9, a-f)
+- ?H upper-case HEX digits (0-9, A-F)
 - ?L lower-case non-ASCII letters
 - ?U upper-case non-ASCII letters
 - ?D non-ASCII "digits"

commit 8b3234478b60ee2dbadcda6849e64cec110c222b

mask.c: Fix a silly bug introduced by c19f75fb9

src/mask.c
@@ -640,8 +640,12 @@ static char* plhdr2string(char p, int fmt_case)
 		break;
 
 	case 'B': /* All high-bit */
+		add_range(0x80, 0xff);
+		break;
 
 	case 'b': /* All (except NULL which we can't handle) */
+		add_range(0x01, 0xff);
+		break;
 
 	case 'h': /* Lower-case hex */
 		add_range('0', '9');