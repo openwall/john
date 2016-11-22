openwall/john

commit c79fce1bdba82b3fa715b10bff92dc4b5f63a19a

cracker.c: multi-salt restore records were often being written for non-salted hashes

src/cracker.c
@@ -899,7 +899,7 @@ static int crk_salt_loop(void)
 		if ((done = crk_password_loop(salt)))
 			break;
 	} while ((salt = salt->next));
-	if (!salt)
+	if (!salt || salt->count < 2)
 		status.resume_salt_md5 = 0;
 
 	if (done >= 0) {

commit 271fdadb7f4322ab5b47aaf91073249ddeb789b5

Fix weird bugs introduced in c79fce1, messing salt resume up. You can't look at salt->count, that's the number of hashes for *this* salt.

src/cracker.c
@@ -950,12 +950,13 @@ static int crk_salt_loop(void)
 	}
 	do {
 		crk_methods.set_salt(salt->salt);
-		status.resume_salt_md5 = salt->salt_md5;
+		status.resume_salt_md5 = (crk_db->salt_count > 1) ?
+			salt->salt_md5 : NULL;
 		if ((done = crk_password_loop(salt)))
 			break;
 	} while ((salt = salt->next));
-	if (!salt || salt->count < 2)
-		status.resume_salt_md5 = 0;
+	if (!salt || crk_db->salt_count < 2)
+		status.resume_salt_md5 = NULL;
 
 	if (done >= 0) {
 #if !HAVE_OPENCL