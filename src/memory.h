openwall/john

commit b701bc0eb89ddfc6331a9f7f032efbb0878621b4

minGW: always use aligned allocs

src/memory.h
@@ -156,6 +156,24 @@ char *strdup_MSVC(const char *str);
 }
 #endif
 
+#elif HAVE___MINGW_ALIGNED_MALLOC
+#if !defined (MEMDBG_ON)
+#define malloc(a) __mingw_aligned_malloc(a,(sizeof(long long)))
+#define realloc(a,b) __mingw_aligned_realloc(a,b,(sizeof(long long)))
+#define calloc(a,b) memset(__mingw_aligned_malloc(a*b,(sizeof(long long))),0,a*b)
+#define free(a) __mingw_aligned_free(a)
+#define strdup(a) strdup_MSVC(a)
+char *strdup_MSVC(const char *str);
+
+#define MEM_FREE(ptr) \
+{ \
+	if ((ptr)) { \
+		__mingw_aligned_free((ptr)); \
+		(ptr) = NULL; \
+	} \
+}
+#endif
+
 #else
 #define MEM_FREE(ptr) \
 { \

src/memory.c
@@ -25,7 +25,7 @@
 #include "johnswap.h"
 #include "memdbg.h"
 
-#if defined (_MSC_VER) && !defined (MEMDBG_ON)
+#if (defined (_MSC_VER) || HAVE___MINGW_ALIGNED_MALLOC) && !defined (MEMDBG_ON)
 char *strdup_MSVC(const char *str)
 {
 	char * s;

commit 284b51c658cdc1d096bb5a730d1478c277afc0f7

minGW: fix for a memdbg bug introduced in b701bc0

src/memory.h
@@ -172,6 +172,14 @@ char *strdup_MSVC(const char *str);
 		(ptr) = NULL; \
 	} \
 }
+#else
+#define MEM_FREE(ptr) \
+{ \
+	if ((ptr)) { \
+		MEMDBG_free(((const void*)ptr),__FILE__,__LINE__); \
+		(ptr) = NULL; \
+	} \
+}
 #endif
 
 #else