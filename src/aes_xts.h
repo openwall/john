5f85f87ce8d6fdcc97c2594802c0f9002ec6646f
Dhiru Kholia <dhiru.kholia@gmail.com>
2017-03-10 19:38:26 +0530

    Initial support for Jetico BestCrypt 8.x / 9.x
    + Only AES-256 with CBC / XTS mode supported
    
    + Only SHA-256 / Whirlpool / SHA-512 supported
    
    + Tested with BestCrypt 3.0.3 on Linux and BestCrypt 9.03 on Windows

--- src/aes_xts.h
+++ src/aes_xts.h
@@ -3,3 +3,6 @@
 void AES_XTS_decrypt(const unsigned char *double_key, unsigned char *out,
                      const unsigned char *data, unsigned len, int bits);
 
+void AES_XTS_decrypt_custom_tweak(const unsigned char *double_key, unsigned
+		char *tweak, unsigned char *out, const unsigned char *data,
+		unsigned len, int bits);
