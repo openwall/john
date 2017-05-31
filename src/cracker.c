openwall/john

commit 827df44161b5fdb26c12cc5d287c1d3f707ea818

Pot sync: Emit a warning if pot file shrunk. Merely seeking past end does not detect that.

src/cracker.c
@@ -600,10 +600,27 @@ int crk_reload_pot(void)
 	if (!(pot_file = fopen(path_expand(options.activepot), "rb")))
 		pexit("fopen: %s", path_expand(options.activepot));
 
-	if (crk_pot_pos && (jtr_fseek64(pot_file, crk_pot_pos, SEEK_SET) == -1)) {
-		perror("fseek");
-		rewind(pot_file);
-		crk_pot_pos = 0;
+	if (crk_pot_pos) {
+		if (jtr_fseek64(pot_file, 0, SEEK_END) == -1)
+			pexit("fseek to end of pot file");
+		if (crk_pot_pos == jtr_ftell64(pot_file))
+			return 0;
+		if (crk_pot_pos > jtr_ftell64(pot_file)) {
+			if (john_main_process) {
+				fprintf(stderr,
+				        "Note: pot file shrunk. Recovering.\n");
+			}
+			log_event("Note: pot file shrunk. Recovering.");
+			rewind(pot_file);
+			crk_pot_pos = 0;
+		}
+		if (jtr_fseek64(pot_file, crk_pot_pos, SEEK_SET) == -1) {
+			perror("fseek to sync pos. of pot file");
+			log_event("fseek to sync pos. of pot file: %s",
+			          strerror(errno));
+			crk_pot_pos = 0;
+			return 0;
+		}
 	}
 
 	ldr_in_pot = 1; /* Mutes some warnings from valid() et al */

commit 3f83ca3a31b1951db89b8af6d2159b100f618474

Fix a bug introduced in 827df441.

src/cracker.c
@@ -603,8 +603,11 @@ int crk_reload_pot(void)
 	if (crk_pot_pos) {
 		if (jtr_fseek64(pot_file, 0, SEEK_END) == -1)
 			pexit("fseek to end of pot file");
-		if (crk_pot_pos == jtr_ftell64(pot_file))
+		if (crk_pot_pos == jtr_ftell64(pot_file)) {
+			if (fclose(pot_file))
+				pexit("fclose");
 			return 0;
+		}
 		if (crk_pot_pos > jtr_ftell64(pot_file)) {
 			if (john_main_process) {
 				fprintf(stderr,
@@ -619,6 +622,8 @@ int crk_reload_pot(void)
 			log_event("fseek to sync pos. of pot file: %s",
 			          strerror(errno));
 			crk_pot_pos = 0;
+			if (fclose(pot_file))
+				pexit("fclose");
 			return 0;
 		}
 	}