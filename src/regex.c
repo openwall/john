8a2b9b982d896ef98023817cb6b3327df6c74e5d
teeshop <teeshop@users.noreply.github.com>
2017-07-21 11:25:58 +0200

    Using the new multibyte character aware API from rexgen (#2571)
    * initial adoption of rexgen-1.5.0
    
    * renamed string export function
    
    * string length limitation with rexgen works
    
    * do not allow to compile against <rexgen-2.0.0
    
    * require at least version 2.0.2 of rexgen
    
    * corrected indentation

--- src/regex.c
+++ src/regex.c
@@ -137,13 +137,13 @@ static void rexgen_setlocale()
 }
 
 // Would be nice to have SOME way to be thread safe!!!
-static wchar_t BaseWord[LINE_BUFFER_SIZE];
+static char BaseWord[LINE_BUFFER_SIZE];
 
-size_t callback(wchar_t* dst, const size_t buffer_size)
+size_t callback(char* dst, const size_t buffer_size)
 {
-	size_t len = wcslen(BaseWord);
+	size_t len = strlen(BaseWord);
 
-	memcpy(dst, BaseWord, (len + 1) * SIZEOF_WCHAR_T);
+	memcpy(dst, BaseWord, (len + 1) * sizeof(BaseWord[0]));
 	*BaseWord = 0;
 	return len;
 }
@@ -190,7 +190,7 @@ int do_regex_hybrid_crack(struct db_main *db, const char *regex,
                           const char *regex_alpha)
 {
 	c_simplestring_ptr buffer = c_simplestring_new();
-	char word[PLAINTEXT_BUFFER_SIZE];
+	const char* word;
 	static int bFirst = 1;
 	static int bALPHA = 0;
 	int max_len = db->format->params.plaintext_length;
@@ -203,10 +203,11 @@ int do_regex_hybrid_crack(struct db_main *db, const char *regex,
 	if (bFirst)
 		rexgen_setlocale();
 
-	if (options.internal_cp != UTF_8)
-		cp_to_wcs(BaseWord, sizeof(BaseWord), base_word);
-	else
-		enc_to_wcs(BaseWord, sizeof(BaseWord), base_word);
+	//if (options.internal_cp != UTF_8)
+	//	cp_to_wcs(BaseWord, sizeof(BaseWord), base_word);
+	//else /* options.internal_cp == UTF_8 */
+	//	enc_to_wcs(BaseWord, sizeof(BaseWord), base_word);
+	strcpy(BaseWord, base_word);
 
 	if (bFirst) {
 		bFirst = 0;
@@ -218,7 +219,7 @@ int do_regex_hybrid_crack(struct db_main *db, const char *regex,
 		rec_init_hybrid(save_state_hybrid);
 		crk_set_hybrid_fix_state_func_ptr(rex_hybrid_fix_state);
 
-		regex_ptr = c_regex_cb(regex, callback);
+		regex_ptr = c_regex_cb_mb(regex, callback);
 		if (!regex_ptr) {
 			c_simplestring_delete(buffer);
 			fprintf(stderr,
@@ -286,11 +287,14 @@ int do_regex_hybrid_crack(struct db_main *db, const char *regex,
 	}
 
 	while (c_iterator_next(iter)) {
-		c_iterator_value(iter, buffer);
-		c_simplestring_to_utf8_string(buffer, &word[0], sizeof(word));
 		c_simplestring_clear(buffer);
-		if (options.internal_cp != UTF_8)
-			utf8_to_cp_r(word, word, sizeof(word));
+		c_iterator_value(iter, buffer);
+		word = c_simplestring_to_string(buffer);
+		/**
+		  * rexgen already creates the correct encoding
+		  */
+		//if (options.internal_cp != UTF_8)
+		//	utf8_to_cp_r(word, word, sizeof(word));
 		if (options.mask) {
 			if (do_mask_crack(word)) {
 				retval = 1;
@@ -298,7 +302,7 @@ int do_regex_hybrid_crack(struct db_main *db, const char *regex,
 			}
 		} else
 		if (ext_filter((char *)word)) {
-			word[max_len] = 0;
+			c_simplestring_truncate_bytes(buffer, max_len);
 			if (crk_process_key((char *)word)) {
 				retval = 1;
 				goto out;
@@ -316,7 +320,7 @@ out:
 void do_regex_crack(struct db_main *db, const char *regex)
 {
 	c_simplestring_ptr buffer = c_simplestring_new();
-	char word[PLAINTEXT_BUFFER_SIZE];
+	const char* word;
 	int max_len = db->format->params.plaintext_length;
 
 	if (options.req_maxlength)
@@ -330,7 +334,7 @@ void do_regex_crack(struct db_main *db, const char *regex)
 	crk_init(db, fix_state, NULL);
 	rec_init_hybrid(save_state_hybrid);
 
-	regex_ptr = c_regex_cb(regex, callback);
+	regex_ptr = c_regex_cb_mb(regex, callback);
 	if (!regex_ptr) {
 		fprintf(stderr,
 		        "Error, invalid regex expression.  John exiting now\n");
@@ -346,15 +350,15 @@ void do_regex_crack(struct db_main *db, const char *regex)
 		restore_str = 0;
 	}
 	while (c_iterator_next(iter)) {
-		c_iterator_value(iter, buffer);
-		c_simplestring_to_utf8_string(buffer, &word[0], sizeof(word));
 		c_simplestring_clear(buffer);
+		c_iterator_value(iter, buffer);
+		word = c_simplestring_to_string(buffer);
 		if (options.mask) {
 			if (do_mask_crack(word))
 				break;
 		} else
 		if (ext_filter((char *)word)) {
-			word[max_len] = 0;
+			c_simplestring_truncate_bytes(buffer, max_len);
 			if (crk_process_key((char *)word))
 				break;
 		}
