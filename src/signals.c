openwall/john

commit 0168a45a145d3c3ff714dd9f6bf73128f5408bac

Don't start "the clock" until all loading, auto-tune etc. are done. This ensures stable speed readings from start, and things like --progress-every and --max-run-time will work better.

src/signals.c
@@ -549,11 +549,7 @@ void sig_init(void)
 		abort_grace_time =
 			cfg_get_int(SECTION_OPTIONS, NULL, "AbortGraceTime");
 	}
-#if OS_TIMER
-	timer_save_value = timer_save_interval;
-#elif !defined(BENCH_BUILD)
-	timer_save_value = status_get_time() + timer_save_interval;
-#endif
+
 	timer_ticksafety_interval = (clock_t)1 << (sizeof(clock_t) * 8 - 4);
 	timer_ticksafety_interval /= clk_tck;
 	if ((timer_ticksafety_interval /= TIMER_INTERVAL) <= 0)
@@ -562,9 +558,28 @@ void sig_init(void)
 
 	atexit(sig_done);
 
-	sig_install(sig_handle_update, SIGHUP);
 	sig_install_abort();
+}
+
+void sig_init_late(void)
+{
+	unsigned int time;
+
+#if OS_TIMER
+	timer_save_value = timer_save_interval;
+	time = 0;
+#elif !defined(BENCH_BUILD)
+	timer_save_value = status_get_time() + timer_save_interval;
+	time = status_get_time();
+#endif
+
+	sig_install(sig_handle_update, SIGHUP);
 	sig_install_timer();
+
+	if (options.max_run_time)
+		timer_abort = time + abs(options.max_run_time);
+	if (options.status_interval)
+		timer_status = time + options.status_interval;
 }
 
 void sig_init_child(void)

src/john.c
@@ -1713,6 +1713,9 @@ static void john_run(void)
 		if (options.flags & FLG_MASK_CHK)
 			mask_crk_init(&database);
 
+		/* Placed here to disregard load time. */
+		sig_init_late();
+
 		if (options.flags & FLG_SINGLE_CHK)
 			do_single_crack(&database);
 		else
@@ -1849,7 +1852,6 @@ static void john_done(void)
 int main(int argc, char **argv)
 {
 	char *name;
-	unsigned int time;
 
 #ifdef TEST_MEMDBG_LOGIC
 	int i,j;
@@ -1988,16 +1990,6 @@ int main(int argc, char **argv)
 	}
 	john_init(name, argc, argv);
 
-	/* Placed here to disregard load time. */
-#if OS_TIMER
-	time = 0;
-#else
-	time = status_get_time();
-#endif
-	if (options.max_run_time)
-		timer_abort = time + abs(options.max_run_time);
-	if (options.status_interval)
-		timer_status = time + options.status_interval;
 	if (options.max_cands) {
 		if (options.node_count) {
 			long long orig_max_cands = options.max_cands;

src/signals.h
@@ -73,6 +73,11 @@ void sig_preinit(void);
 extern void sig_init(void);
 
 /*
+ * Starts "the clock". Called after loading, auto-tuning, etc.
+ */
+extern void sig_init_late(void);
+
+/*
  * Performs additional (re-)initialization after fork().  Assumes that
  * sig_init() has already been called.
  */

commit f3dd1a6148399d42596b0a68664b0b0c87de62b3

Fix a bug introduced in 0168a45a when using fork (not MPI). Closes #2664. This might also affect #2547 somehow.

src/signals.c
@@ -590,9 +590,6 @@ void sig_init_child(void)
 #ifdef SIGUSR2
 	sig_remove_reload();
 #endif
-#if OS_TIMER
-	sig_init_timer();
-#endif
 }
 
 static void sig_done(void)