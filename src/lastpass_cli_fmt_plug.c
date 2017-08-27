0496c0c7328a02781e6539a2885de1d9b71eac14
Dhiru Kholia <dhiru.kholia@gmail.com>
2017-08-27 16:08:16 +0530

    OpenCL support for LastPass formats

--- src/lastpass_cli_fmt_plug.c
+++ src/lastpass_cli_fmt_plug.c
@@ -15,8 +15,6 @@ john_register_one(&fmt_lastpass_cli);
 #else
 
 #include <string.h>
-#include <assert.h>
-#include <errno.h>
 #ifdef _OPENMP
 #include <omp.h>
 #ifndef OMP_SCALE
@@ -34,10 +32,10 @@ john_register_one(&fmt_lastpass_cli);
 #include "aes.h"
 #include "sha2.h"
 #include "pbkdf2_hmac_sha256.h"
+#include "lastpass_cli_common.h"
 #include "memdbg.h"
 
 #define FORMAT_LABEL            "lpcli"
-#define FORMAT_NAME             "LastPass CLI"
 #define FORMAT_TAG              "$lpcli$"
 #define FORMAT_TAG_LEN          (sizeof(FORMAT_TAG)-1)
 #ifdef SIMD_COEF_32
@@ -60,28 +58,13 @@ john_register_one(&fmt_lastpass_cli);
 #define MAX_KEYS_PER_CRYPT      1
 #endif
 
-static struct fmt_tests lastpass_cli_tests[] = {
-	/* LastPass CLI v1.2.1 */
-	{"$lpcli$0$lulu@mailinator.com$1234$3fec6cd2d8c049cbafe9fa6a9343f42f$f21d8e60ad22db53033e431700fb5e0c", "Badpassword098765"},
-	{"$lpcli$0$lulu@mailinator.com$1234$fbd97e7e14713363c5567bdc106bb1f4$75ebb9460f9852ccb2382029fe333867", "Password12345"},
-	// Special case where iterations == 1
-	{"$lpcli$0$lulu@mailinator.com$1$9611651d6cbe6ab1dfb035d3874bd803$68bbe1480410c03cc053662658884f2b", "Password12345"},
-	{NULL}
-};
-
 #if defined (_OPENMP)
 static int omp_t = 1;
 #endif
 static char (*saved_key)[PLAINTEXT_LENGTH + 1];
 static uint32_t (*crypt_out)[32 / sizeof(uint32_t)];
 
-static struct custom_salt {
-	int iterations;
-	int salt_length;
-	int type;
-	unsigned char iv[32];
-	unsigned char salt[32];
-} *cur_salt;
+static struct custom_salt *cur_salt;
 
 static void init(struct fmt_main *self)
 {
@@ -99,100 +82,6 @@ static void done(void)
 {
 	MEM_FREE(crypt_out);
 	MEM_FREE(saved_key);
-}
-
-static int valid(char *ciphertext, struct fmt_main *self)
-{
-	char *ctcopy;
-	char *keeptr;
-	char *p;
-	int extra;
-	int type = 0;
-
-	if (strncmp(ciphertext, FORMAT_TAG, FORMAT_TAG_LEN))
-		return 0;
-	ctcopy = strdup(ciphertext);
-	keeptr = ctcopy;
-	ctcopy += FORMAT_TAG_LEN;
-	if ((p = strtokm(ctcopy, "$")) == NULL)	/* type */
-		goto err;
-	if (!isdec(p))
-		goto err;
-	type = atoi(p);
-	if (type != 0)
-		goto err;
-	if (strlen(p) > 32)
-		goto err;
-	if ((p = strtokm(NULL, "$")) == NULL) /* email */
-		goto err;
-	if (strlen(p) > 32)
-		goto err;
-	if ((p = strtokm(NULL, "$")) == NULL) /* iterations */
-		goto err;
-	if (!isdec(p))
-		goto err;
-	if ((p = strtokm(NULL, "$")) == NULL) /* iv */
-		goto err;
-	if (hexlenl(p, &extra) != 32 || extra)
-		goto err;
-	if ((p = strtokm(NULL, "$")) == NULL) /* hash */
-		goto err;
-	if (hexlenl(p, &extra) != 32 || extra)
-		goto err;
-
-	MEM_FREE(keeptr);
-	return 1;
-
-err:
-	MEM_FREE(keeptr);
-	return 0;
-}
-
-static void *get_salt(char *ciphertext)
-{
-	char *ctcopy = strdup(ciphertext);
-	char *keeptr = ctcopy;
-	char *p;
-	static struct custom_salt cs;
-	int i;
-
-	memset(&cs, 0, sizeof(cs));
-	ctcopy += FORMAT_TAG_LEN;
-
-	p = strtokm(ctcopy, "$");
-	p = strtokm(NULL, "$");
-	strncpy((char*)cs.salt, p, 32);
-	cs.salt_length = strlen((char*)p);
-	p = strtokm(NULL, "$");
-	cs.iterations = atoi(p);
-	if (cs.iterations < 1)
-		cs.iterations = 1;
-	p = strtokm(NULL, "$");
-	for (i = 0; i < 16; i++)
-		cs.iv[i] = (atoi16[ARCH_INDEX(p[2 * i])] << 4) | atoi16[ARCH_INDEX(p[2 * i + 1])];
-
-	MEM_FREE(keeptr);
-	return (void *)&cs;
-}
-
-static void *get_binary(char *ciphertext)
-{
-	static union {
-		unsigned char c[BINARY_SIZE+1];
-		uint32_t dummy;
-	} buf;
-	unsigned char *out = buf.c;
-	char *p;
-	int i;
-
-	p = strrchr(ciphertext, '$') + 1;
-	for (i = 0; i < BINARY_SIZE; i++) {
-		out[i] = (atoi16[ARCH_INDEX(*p)] << 4) |
-			atoi16[ARCH_INDEX(p[1])];
-		p += 2;
-	}
-
-	return out;
 }
 
 static void set_salt(void *salt)
@@ -285,11 +174,6 @@ static char *get_key(int index)
 	return saved_key[index];
 }
 
-static unsigned int lastpass_iteration_count(void *salt)
-{
-        return ((struct custom_salt*)salt)->iterations;
-}
-
 struct fmt_main fmt_lastpass_cli = {
 	{
 		FORMAT_LABEL,
@@ -316,12 +200,12 @@ struct fmt_main fmt_lastpass_cli = {
 		done,
 		fmt_default_reset,
 		fmt_default_prepare,
-		valid,
+		lastpass_cli_common_valid,
 		fmt_default_split,
-		get_binary,
-		get_salt,
+		lastpass_cli_common_get_binary,
+		lastpass_cli_common_get_salt,
 		{
-			lastpass_iteration_count,
+			lastpass_cli_common_iteration_count,
 		},
 		fmt_default_source,
 		{
--- src/lastpass_fmt_plug.c
+++ src/lastpass_fmt_plug.c
@@ -1,12 +1,14 @@
-/* LastPass offline cracker patch for JtR. Hacked together during January of 2013 by
- * Dhiru Kholia <dhiru.kholia at gmail.com>.
+/*
+ * LastPass offline cracker patch for JtR. Hacked together during January of
+ * 2013 by Dhiru Kholia <dhiru.kholia at gmail.com>.
  *
- * All the hard work was done by Milen (author of hashkill).
+ * All the hard work was done by Milen Rangelov (author of hashkill).
  *
  * This software is Copyright (c) 2012, Dhiru Kholia <dhiru.kholia at gmail.com>,
  * and it is hereby released to the general public under the following terms:
  * Redistribution and use in source and binary forms, with or without modification,
- * are permitted.  */
+ * are permitted.
+ */
 
 #if FMT_EXTERNS_H
 extern struct fmt_main fmt_lastpass;
@@ -15,8 +17,6 @@ john_register_one(&fmt_lastpass);
 #else
 
 #include <string.h>
-#include <assert.h>
-#include <errno.h>
 #ifdef _OPENMP
 #include <omp.h>
 #ifndef OMP_SCALE
@@ -32,11 +32,11 @@ john_register_one(&fmt_lastpass);
 #include "params.h"
 #include "options.h"
 #include "aes.h"
+#include "lastpass_common.h"
 #include "pbkdf2_hmac_sha256.h"
 #include "memdbg.h"
 
 #define FORMAT_LABEL            "lp"
-#define FORMAT_NAME             "LastPass offline"
 #define FORMAT_TAG              "$lp$"
 #define FORMAT_TAG_LEN          (sizeof(FORMAT_TAG)-1)
 #ifdef SIMD_COEF_32
@@ -47,7 +47,6 @@ john_register_one(&fmt_lastpass);
 #define BENCHMARK_COMMENT       ""
 #define BENCHMARK_LENGTH        -1
 #define PLAINTEXT_LENGTH        125
-#define BINARY_SIZE             16
 #define SALT_SIZE               sizeof(struct custom_salt)
 #define BINARY_ALIGN            sizeof(uint32_t)
 #define SALT_ALIGN              sizeof(int)
@@ -59,29 +58,13 @@ john_register_one(&fmt_lastpass);
 #define MAX_KEYS_PER_CRYPT      1
 #endif
 
-static struct fmt_tests lastpass_tests[] = {
-	{"$lp$hackme@mailinator.com$6f5d8cec3615fc9ac7ba2e0569bce4f5", "strongpassword"},
-	{"$lp$3$27c8641d7f5ab5985569d9d0b499b467", "123"},
-	{"$lp$ninechars$d09153108a89347da5c97a4a18f91345", "PassWord"},
-	{"$lp$anicocls$764b0f54528eb4a4c93aab1b18af28a5", ""},
-	/* Three hashes from LastPass v3.3.4 for Firefox on Linux */
-	{"$lp$lulu@mailinator.com$5000$d8d1e25680b3d9f73489d5769ac3a9c1", "Openwall123"},
-	{"$lp$lulu@mailinator.com$5000$2edc5742660ddd3e26ce52aeca993531", "Password123"},
-	{"$lp$lulu@mailinator.com$1234$6e5cea4fbde80072ffc736bfa8c88730", "Password123"},
-	{NULL}
-};
-
 #if defined (_OPENMP)
 static int omp_t = 1;
 #endif
 static char (*saved_key)[PLAINTEXT_LENGTH + 1];
 static uint32_t (*crypt_out)[32 / sizeof(uint32_t)];
 
-static struct custom_salt {
-	int iterations;
-	int salt_length;
-	unsigned char salt[32];
-} *cur_salt;
+static struct custom_salt *cur_salt;
 
 static void init(struct fmt_main *self)
 {
@@ -99,85 +82,6 @@ static void done(void)
 {
 	MEM_FREE(crypt_out);
 	MEM_FREE(saved_key);
-}
-
-static int valid(char *ciphertext, struct fmt_main *self)
-{
-	char *ctcopy;
-	char *keeptr;
-	char *p;
-	int extra;
-	int have_iterations = 0;
-
-	if (strncmp(ciphertext, FORMAT_TAG, FORMAT_TAG_LEN))
-		return 0;
-	ctcopy = strdup(ciphertext);
-	keeptr = ctcopy;
-	ctcopy += FORMAT_TAG_LEN;
-	if ((p = strtokm(ctcopy, "$")) == NULL)	/* email */
-		goto err;
-	if (strlen(p) > 32)
-		goto err;
-	/* hack to detect if iterations is present in hash */
-	if ((p = strtokm(NULL, "$")) == NULL)	/* iterations or hash */
-		goto err;
-	if (strlen(p) < 24) {
-		have_iterations = 1;
-	}
-	if (have_iterations) {
-		if ((p = strtokm(NULL, "$")) == NULL)	/* hash */
-			goto err;
-	}
-	if (hexlenl(p, &extra) != 32 || extra)
-		goto err;
-
-	MEM_FREE(keeptr);
-	return 1;
-
-err:
-	MEM_FREE(keeptr);
-	return 0;
-}
-
-static void *get_salt(char *ciphertext)
-{
-	char *ctcopy = strdup(ciphertext);
-	char *keeptr = ctcopy;
-	char *p;
-	static struct custom_salt cs;
-
-	memset(&cs, 0, sizeof(cs));
-	ctcopy += FORMAT_TAG_LEN;
-	p = strtokm(ctcopy, "$");
-	strncpy((char*)cs.salt, p, 32);
-	cs.salt_length = strlen((char*)p);
-	p = strtokm(NULL, "$");
-	if (strlen(p) < 24) { // new hash format
-		cs.iterations = atoi(p);
-	} else {
-		cs.iterations = 500; // default iterations value
-	}
-
-	MEM_FREE(keeptr);
-	return (void *)&cs;
-}
-
-static void *get_binary(char *ciphertext)
-{
-	static union {
-		unsigned char c[BINARY_SIZE+1];
-		uint32_t dummy;
-	} buf;
-	unsigned char *out = buf.c;
-	char *p;
-	int i;
-	p = strrchr(ciphertext, '$') + 1;
-	for (i = 0; i < BINARY_SIZE; i++) {
-		out[i] = (atoi16[ARCH_INDEX(*p)] << 4) |
-			atoi16[ARCH_INDEX(p[1])];
-		p += 2;
-	}
-	return out;
 }
 
 static void set_salt(void *salt)
@@ -254,11 +158,6 @@ static char *get_key(int index)
 	return saved_key[index];
 }
 
-static unsigned int lastpass_iteration_count(void *salt)
-{
-        return ((struct custom_salt*)salt)->iterations;
-}
-
 struct fmt_main fmt_lastpass = {
 	{
 		FORMAT_LABEL,
@@ -285,12 +184,12 @@ struct fmt_main fmt_lastpass = {
 		done,
 		fmt_default_reset,
 		fmt_default_prepare,
-		valid,
+		lastpass_common_valid,
 		fmt_default_split,
-		get_binary,
-		get_salt,
+		lastpass_common_get_binary,
+		lastpass_common_get_salt,
 		{
-			lastpass_iteration_count,
+			lastpass_common_iteration_count,
 		},
 		fmt_default_source,
 		{
