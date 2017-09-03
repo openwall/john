c35c3420d71f0b546469a44f1b8e72440044f595
Dhiru Kholia <dhiru.kholia@gmail.com>
2017-09-03 19:52:54 +0530

    Add support for cracking PGP SDAs (self-decrypting archives)

---
+++ src/pgpsda_common.h
@@ -0,0 +1,26 @@
+/*
+ * Common code for the PGP SDA format.
+ */
+
+#include <string.h>
+#include <openssl/cast.h>
+
+#include "formats.h"
+#include "sha.h"
+
+#define FORMAT_NAME             ""
+#define FORMAT_TAG              "$pgpsda$"
+#define FORMAT_TAG_LENGTH       (sizeof(FORMAT_TAG) - 1)
+
+struct custom_salt {
+	int version;
+	int iterations;
+	int salt_size;
+	unsigned char salt[8];
+};
+
+extern struct fmt_tests pgpsda_tests[];
+
+// exported 'common' functions
+int pgpsda_common_valid(char *ciphertext, struct fmt_main *self);
+void *pgpsda_common_get_salt(char *ciphertext);
