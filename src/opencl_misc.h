openwall/john

commit 599a16749b7f465792268c29e3849246b19e3ec8

OpenCL: Add a shared SHA-512 "CTX" (init/update/final style) kernel. See #2801.

src/opencl_misc.h
@@ -169,10 +169,10 @@ inline uint SWAP32(uint x)
 }
 
 #define SWAP64(n)	bitselect( \
-		bitselect(rotate(n, 24UL), \
-		          rotate(n, 8UL), 0x000000FF000000FFUL), \
-		bitselect(rotate(n, 56UL), \
-		          rotate(n, 40UL), 0x00FF000000FF0000UL), \
+		bitselect(rotate((ulong)n, 24UL), \
+		          rotate((ulong)n, 8UL), 0x000000FF000000FFUL), \
+		bitselect(rotate((ulong)n, 56UL), \
+		          rotate((ulong)n, 40UL), 0x00FF000000FF0000UL), \
 		0xFFFF0000FFFF0000UL)
 #else
 inline uint SWAP32(uint x)
@@ -218,6 +218,30 @@ inline MAYBE_VECTOR_UINT VSWAP32(MAYBE_VECTOR_UINT x)
 		(b)[(i) + 3] = (uchar) ((n)      ); \
 	}
 
+#define GET_UINT64BE_UNALIGNED(n, b, i)	  \
+	{ \
+		(n) = ((ulong) (b)[(i)] << 56) \
+			| ((ulong) (b)[(i) + 1] << 48) \
+			| ((ulong) (b)[(i) + 2] << 40) \
+			| ((ulong) (b)[(i) + 3] << 32) \
+			| ((ulong) (b)[(i) + 4] << 24) \
+			| ((ulong) (b)[(i) + 5] << 16) \
+			| ((ulong) (b)[(i) + 6] <<  8) \
+			| ((ulong) (b)[(i) + 7]      ); \
+	}
+
+#define PUT_UINT64BE_UNALIGNED(n, b, i)	  \
+	{ \
+		(b)[(i)    ] = (uchar) ((n) >> 56); \
+		(b)[(i) + 1] = (uchar) ((n) >> 48); \
+		(b)[(i) + 2] = (uchar) ((n) >> 40); \
+		(b)[(i) + 3] = (uchar) ((n) >> 32); \
+		(b)[(i) + 4] = (uchar) ((n) >> 24); \
+		(b)[(i) + 5] = (uchar) ((n) >> 16); \
+		(b)[(i) + 6] = (uchar) ((n) >>  8); \
+		(b)[(i) + 7] = (uchar) ((n)      ); \
+	}
+
 /*
  * Faster on nvidia, no difference on AMD
  * NOTE: This version needs b to be aligned as int!
@@ -225,9 +249,13 @@ inline MAYBE_VECTOR_UINT VSWAP32(MAYBE_VECTOR_UINT x)
 #if __ENDIAN_LITTLE__
 #define GET_UINT32BE(n, b, i)	(n) = SWAP32(((uint*)(b))[(i) >> 2])
 #define PUT_UINT32BE(n, b, i)	((uint*)(b))[(i) >> 2] = SWAP32(n)
+#define GET_UINT64BE(n, b, i)	(n) = SWAP64(((ulong*)(b))[(i) >> 3])
+#define PUT_UINT64BE(n, b, i)	((ulong*)(b))[(i) >> 3] = SWAP64(n)
 #else
 #define GET_UINT32BE(n, b, i)	(n) = ((uint*)(b))[(i) >> 2]
 #define PUT_UINT32BE(n, b, i)	((uint*)(b))[(i) >> 2] = (n)
+#define GET_UINT64BE(n, b, i)	(n) = ((ulong*)(b))[(i) >> 3]
+#define PUT_UINT64BE(n, b, i)	((ulong*)(b))[(i) >> 3] = (n)
 #endif
 
 #else /* Safe code for any arch */
@@ -246,6 +274,30 @@ inline MAYBE_VECTOR_UINT VSWAP32(MAYBE_VECTOR_UINT x)
 		(b)[(i) + 1] = (uchar) ((n) >> 16); \
 		(b)[(i) + 2] = (uchar) ((n) >>  8); \
 		(b)[(i) + 3] = (uchar) ((n)      ); \
+	}
+
+#define GET_UINT64BE(n, b, i)	  \
+	{ \
+		(n) = ((ulong) (b)[(i)] << 56) \
+			| ((ulong) (b)[(i) + 1] << 48) \
+			| ((ulong) (b)[(i) + 2] << 40) \
+			| ((ulong) (b)[(i) + 3] << 32) \
+			| ((ulong) (b)[(i) + 4] << 24) \
+			| ((ulong) (b)[(i) + 5] << 16) \
+			| ((ulong) (b)[(i) + 6] <<  8) \
+			| ((ulong) (b)[(i) + 7]      ); \
+	}
+
+#define PUT_UINT64BE(n, b, i)	  \
+	{ \
+		(b)[(i)    ] = (uchar) ((n) >> 56); \
+		(b)[(i) + 1] = (uchar) ((n) >> 48); \
+		(b)[(i) + 2] = (uchar) ((n) >> 40); \
+		(b)[(i) + 3] = (uchar) ((n) >> 32); \
+		(b)[(i) + 4] = (uchar) ((n) >> 24); \
+		(b)[(i) + 5] = (uchar) ((n) >> 16); \
+		(b)[(i) + 6] = (uchar) ((n) >>  8); \
+		(b)[(i) + 7] = (uchar) ((n)      ); \
 	}
 #endif
 

src/opencl_sha2_ctx.h
@@ -179,4 +179,174 @@ void SHA256_Final(uchar output[32], SHA256_CTX *ctx) {
 	}
 }
 
+/*
+ * SHA-512 context setup
+ */
+
+typedef struct {
+	uint total;        /* number of bytes processed  */
+	ulong state[8];    /* intermediate digest state  */
+	uchar buffer[128]; /* data block being processed */
+} SHA512_CTX;
+
+inline
+void SHA512_Init(SHA512_CTX *ctx) {
+	ctx->total = 0;
+	ctx->state[0] = SHA2_INIT_A;
+	ctx->state[1] = SHA2_INIT_B;
+	ctx->state[2] = SHA2_INIT_C;
+	ctx->state[3] = SHA2_INIT_D;
+	ctx->state[4] = SHA2_INIT_E;
+	ctx->state[5] = SHA2_INIT_F;
+	ctx->state[6] = SHA2_INIT_G;
+	ctx->state[7] = SHA2_INIT_H;
+}
+
+inline
+void _sha512_process(SHA512_CTX *ctx, const uchar data[128]) {
+	ulong t, W[16], A, B, C, D, E, F, G, H;
+
+#if gpu_nvidia(DEVICE_INFO)
+	if ((ulong)data & 0x07) {
+		GET_UINT64BE_UNALIGNED(W[ 0], data,   0);
+		GET_UINT64BE_UNALIGNED(W[ 1], data,   8);
+		GET_UINT64BE_UNALIGNED(W[ 2], data,  16);
+		GET_UINT64BE_UNALIGNED(W[ 3], data,  24);
+		GET_UINT64BE_UNALIGNED(W[ 4], data,  32);
+		GET_UINT64BE_UNALIGNED(W[ 5], data,  40);
+		GET_UINT64BE_UNALIGNED(W[ 6], data,  48);
+		GET_UINT64BE_UNALIGNED(W[ 7], data,  56);
+		GET_UINT64BE_UNALIGNED(W[ 8], data,  64);
+		GET_UINT64BE_UNALIGNED(W[ 9], data,  72);
+		GET_UINT64BE_UNALIGNED(W[10], data,  80);
+		GET_UINT64BE_UNALIGNED(W[11], data,  88);
+		GET_UINT64BE_UNALIGNED(W[12], data,  96);
+		GET_UINT64BE_UNALIGNED(W[13], data, 104);
+		GET_UINT64BE_UNALIGNED(W[14], data, 112);
+		GET_UINT64BE_UNALIGNED(W[15], data, 120);
+	} else
+#endif
+	{
+		GET_UINT64BE(W[ 0], data,   0);
+		GET_UINT64BE(W[ 1], data,   8);
+		GET_UINT64BE(W[ 2], data,  16);
+		GET_UINT64BE(W[ 3], data,  24);
+		GET_UINT64BE(W[ 4], data,  32);
+		GET_UINT64BE(W[ 5], data,  40);
+		GET_UINT64BE(W[ 6], data,  48);
+		GET_UINT64BE(W[ 7], data,  56);
+		GET_UINT64BE(W[ 8], data,  64);
+		GET_UINT64BE(W[ 9], data,  72);
+		GET_UINT64BE(W[10], data,  80);
+		GET_UINT64BE(W[11], data,  88);
+		GET_UINT64BE(W[12], data,  96);
+		GET_UINT64BE(W[13], data, 104);
+		GET_UINT64BE(W[14], data, 112);
+		GET_UINT64BE(W[15], data, 120);
+	}
+
+	A = ctx->state[0];
+	B = ctx->state[1];
+	C = ctx->state[2];
+	D = ctx->state[3];
+	E = ctx->state[4];
+	F = ctx->state[5];
+	G = ctx->state[6];
+	H = ctx->state[7];
+
+	SHA512(A, B, C, D, E, F, G, H, W);
+
+	ctx->state[0] += A;
+	ctx->state[1] += B;
+	ctx->state[2] += C;
+	ctx->state[3] += D;
+	ctx->state[4] += E;
+	ctx->state[5] += F;
+	ctx->state[6] += G;
+	ctx->state[7] += H;
+}
+
+/*
+ * SHA-512 process buffer
+ */
+inline
+void SHA512_Update(SHA512_CTX *ctx, const uchar *input, uint ilen) {
+	uint fill;
+	uint left;
+
+	if (ilen <= 0)
+		return;
+
+	left = ctx->total & 0x7F;
+	fill = 128 - left;
+
+	ctx->total += ilen;
+
+	if (left && ilen >= fill)
+	{
+		memcpy_pp(ctx->buffer + left, input, fill);
+		_sha512_process(ctx, ctx->buffer);
+		input += fill;
+		ilen  -= fill;
+		left = 0;
+	}
+
+	while(ilen >= 128)
+	{
+		_sha512_process(ctx, input);
+		input += 128;
+		ilen  -= 128;
+	}
+
+	if (ilen > 0)
+	{
+		memcpy_pp(ctx->buffer + left, input, ilen);
+	}
+}
+
+/*
+ * SHA-512 final digest
+ */
+inline
+void SHA512_Final(uchar output[64], SHA512_CTX *ctx) {
+	uint last, padn;
+	ulong bits;
+	uchar msglen[16];
+	uchar sha512_padding[128] = { 0x80 /* , 0, 0 ... */ };
+
+	bits = ctx->total << 3;
+
+	PUT_UINT64BE(0, msglen, 0);
+	PUT_UINT64BE(bits, msglen, 8);
+
+	last = ctx->total & 0x7F;
+	padn = (last < 112) ? (112 - last) : (240 - last);
+
+	SHA512_Update(ctx, sha512_padding, padn);
+	SHA512_Update(ctx, msglen, 16);
+
+#if gpu_nvidia(DEVICE_INFO)
+	if ((ulong)output & 0x07) {
+		PUT_UINT64BE_UNALIGNED(ctx->state[0], output,  0);
+		PUT_UINT64BE_UNALIGNED(ctx->state[1], output,  8);
+		PUT_UINT64BE_UNALIGNED(ctx->state[2], output, 16);
+		PUT_UINT64BE_UNALIGNED(ctx->state[3], output, 24);
+		PUT_UINT64BE_UNALIGNED(ctx->state[4], output, 32);
+		PUT_UINT64BE_UNALIGNED(ctx->state[5], output, 40);
+		PUT_UINT64BE_UNALIGNED(ctx->state[6], output, 48);
+		PUT_UINT64BE_UNALIGNED(ctx->state[7], output, 56);
+	} else
+#endif
+	{
+		PUT_UINT64BE(ctx->state[0], output,  0);
+		PUT_UINT64BE(ctx->state[1], output,  8);
+		PUT_UINT64BE(ctx->state[2], output, 16);
+		PUT_UINT64BE(ctx->state[3], output, 24);
+		PUT_UINT64BE(ctx->state[4], output, 32);
+		PUT_UINT64BE(ctx->state[5], output, 40);
+		PUT_UINT64BE(ctx->state[6], output, 48);
+		PUT_UINT64BE(ctx->state[7], output, 56);
+	}
+}
+
 #endif // #ifndef _OPENCL_SHA2_CTX_H

commit 05ae208cee068eec984744e8a66f72996dd6b124

Fix a regression caused by 599a1674: The shared SWAP64 macro need to be type agnostic (type may be vector).

src/opencl_misc.h
@@ -169,10 +169,10 @@ inline uint SWAP32(uint x)
 }
 
 #define SWAP64(n)	bitselect( \
-		bitselect(rotate((ulong)n, 24UL), \
-		          rotate((ulong)n, 8UL), 0x000000FF000000FFUL), \
-		bitselect(rotate((ulong)n, 56UL), \
-		          rotate((ulong)n, 40UL), 0x00FF000000FF0000UL), \
+		bitselect(rotate(n, 24UL), \
+		          rotate(n, 8UL), 0x000000FF000000FFUL), \
+		bitselect(rotate(n, 56UL), \
+		          rotate(n, 40UL), 0x00FF000000FF0000UL), \
 		0xFFFF0000FFFF0000UL)
 #else
 inline uint SWAP32(uint x)

src/opencl_sha2_ctx.h
@@ -316,7 +316,7 @@ void SHA512_Final(uchar output[64], SHA512_CTX *ctx) {
 
 	bits = ctx->total << 3;
 
-	PUT_UINT64BE(0, msglen, 0);
+	PUT_UINT64BE(0UL, msglen, 0);
 	PUT_UINT64BE(bits, msglen, 8);
 
 	last = ctx->total & 0x7F;