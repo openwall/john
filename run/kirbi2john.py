2acf4f510bb73f57abd892862d29895d51eab5fb
Dhiru Kholia <dhiru.kholia@gmail.com>
2017-10-21 16:01:59 +0530

    Minor updates for KRB5 TGS stuff

--- run/kirbi2john.py
+++ run/kirbi2john.py
@@ -2,6 +2,7 @@
 
 # Based on the Kerberoast script from Tim Medin to extract the Kerberos tickets
 # from a kirbi file (https://github.com/nidem/kerberoast).
+
 # Modification to parse them into the JtR-format by Michael Kramer (SySS GmbH)
 # Copyright [2015] [Tim Medin, Michael Kramer]
 #
@@ -30,13 +31,19 @@ if __name__ == '__main__':
     for f in sys.argv[1:]:
         with open(f, 'rb') as fd:
             data = fd.read()
-            if data[0] == '\x76':
+            if data[0] == '\x76':  # process .kirbi
                 # rem dump
+                etype = str(decoder.decode(data)[0][2][0][3][0])
+                if etype != "23":
+                    sys.stderr.write("Unsupported etype %s seen! Please report this to us.\n" % etype)
                 et = str(decoder.decode(data)[0][2][0][3][2])
-                sys.stdout.write("$krb5tgs$unkown:" + et[:16].encode("hex") +
+                sys.stdout.write("$krb5tgs$unknown:$krb5tgs$%s$" % etype + et[:16].encode("hex") +
                                  "$" + et[16:].encode("hex") + "\n")
-            elif data[:2] == '6d':
+            elif data[:2] == '6d':  # extracttgsrepfrompcap.py output
                 for ticket in data.strip().split('\n'):
+                    etype = str(decoder.decode(ticket.decode('hex'))[0][4][3][0])
+                    if etype != "23":
+                        sys.stderr.write("Unsupported etype %s seen! Please report this to us.\n" % etype)
                     et = str(decoder.decode(ticket.decode('hex'))[0][4][3][2])
-                    sys.stdout.write("$krb5tgs$unkown:" + et[:16].encode("hex")
-                                     + "$" + et[16:].encode("hex") + "\n")
+                    sys.stdout.write("$krb5tgs$unknown:$krb5tgs$%s$" % etype + et[:16].encode("hex") +
+                                     "$" + et[16:].encode("hex") + "\n")
