952a34ba003b4cdadcea7da98f3eee2132bf029f
Solar Designer <solar@openwall.com>
2017-11-08 20:33:20 +0000

    1.8.0.11: - DokuWiki" external mode sample has been added to the default john.conf. - Increased SALT_HASH_LOG from 12 to 20, just like it had been done in jumbo. This should be affordable, even though it is wasteful for some formats (some have smaller salts, but currently have no way to declare that). - Dropped the smallest bitmap size (4-bit index, which was normally unused anyway) and introduced a new largest one (30-bit index).

--- src/params.h
+++ src/params.h
@@ -22,7 +22,7 @@
 /*
  * John's version number.
  */
-#define JOHN_VERSION			"1.8.0.10"
+#define JOHN_VERSION			"1.8.0.11"
 
 /*
  * Notes to packagers of John for *BSD "ports", Linux distributions, etc.:
