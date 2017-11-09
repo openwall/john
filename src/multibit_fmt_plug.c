openwall/john

commit 00655ad6336659fefda94ec0be65cf586a1fba50

Fix several issues with OpenMP in multibit format. See #2846.

src/multibit_fmt_plug.c
@@ -20,7 +20,7 @@ john_register_one(&fmt_multibit);
 #ifdef _OPENMP
 #include <omp.h>
 #ifndef OMP_SCALE
-#define OMP_SCALE               128 // just 8 is better for v2 salts
+#define OMP_SCALE               2
 #endif
 #endif
 
@@ -50,7 +50,7 @@ john_register_one(&fmt_multibit);
 #define SALT_ALIGN              sizeof(uint32_t)
 #define PLAINTEXT_LENGTH        125
 #define MIN_KEYS_PER_CRYPT      1
-#define MAX_KEYS_PER_CRYPT      1
+#define MAX_KEYS_PER_CRYPT      64 // just 4 is better for v2 salts
 
 static struct fmt_tests multibit_tests[] = {
 	// Wallets created by MultiBit Classic 0.5.18
@@ -75,11 +75,13 @@ static struct custom_salt {
 static void init(struct fmt_main *self)
 {
 #ifdef _OPENMP
-	int omp_t = omp_get_num_threads();
+	int omp_t = omp_get_max_threads();
 
-	self->params.min_keys_per_crypt *= omp_t;
-	omp_t *= OMP_SCALE;
-	self->params.max_keys_per_crypt *= omp_t;
+	if (omp_t > 1) {
+		self->params.min_keys_per_crypt *= omp_t;
+		omp_t *= OMP_SCALE;
+		self->params.max_keys_per_crypt *= omp_t;
+	}
 #endif
 	saved_key = mem_calloc(sizeof(*saved_key), self->params.max_keys_per_crypt);
 	cracked = mem_calloc(sizeof(*cracked), self->params.max_keys_per_crypt);
@@ -245,7 +247,10 @@ static int crypt_all(int *pcount, struct db_salt *salt)
 		AES_KEY aes_decrypt_key;
 		int len = strlen(saved_key[index]);
 
-		cracked[index] = 0;
+#ifdef _OPENMP
+		if (cracked[index]) /* avoid false sharing of nearby elements */
+#endif
+			cracked[index] = 0;
 
 		if (cur_salt->type == 1) {
 			unsigned char c;

commit 790d23b24b78b07c26c299ac7adbc100a108d9ef

Fix a bug with non-OMP introduced by 00655ad63

src/multibit_fmt_plug.c
@@ -234,13 +234,12 @@ static const unsigned char *iv_hardcoded = (unsigned char*)"\xa3\x44\x39\x1f\x53
 static int crypt_all(int *pcount, struct db_salt *salt)
 {
 	const int count = *pcount;
-	int index = 0;
+	int index;
 
 #ifdef _OPENMP
 #pragma omp parallel for
-	for (index = 0; index < count; index++)
 #endif
-	{
+	for (index = 0; index < count; index++) {
 		unsigned char iv[16];
 		unsigned char key[32];
 		unsigned char outbuf[16];