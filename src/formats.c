555a03fccdb378b2a7216cf0a02a20175b1726a0
Claudio André <claudioandre.br@gmail.com>
2017-11-17 17:44:18 -0200

    core: fix a bug introduced in 38d20bb
    OpenCL formats were crashing.

--- src/formats.c
+++ src/formats.c
@@ -965,7 +965,7 @@ static char *fmt_self_test_body(struct fmt_main *format,
 				for (i = index + 1; i < max - 1; i++)
 				    fmt_set_key(longcand(format, i, sl), i);
 				index = max - 1;
-			} else
+			}
 #endif
 			current = format->params.tests;
 			done |= 2;
