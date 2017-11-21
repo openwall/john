aa1be1765c78f32287ae46f94b47a2b2c8f1cb80
jfoug <jfoug@openwall.net>
2017-11-20 19:04:54 -0600

    android FDE: salt alignment problem

--- src/androidfde_fmt_plug.c
+++ src/androidfde_fmt_plug.c
@@ -72,7 +72,7 @@ static int omp_t = 1;
 #define BENCHMARK_LENGTH    -1
 #define BINARY_SIZE         0
 #define BINARY_ALIGN		1
-#define SALT_ALIGN			sizeof(int)
+#define SALT_ALIGN			8
 #define SALT_SIZE           sizeof(struct custom_salt)
 #ifdef SIMD_COEF_32
 #define MIN_KEYS_PER_CRYPT  SSE_GROUP_SZ_SHA1
