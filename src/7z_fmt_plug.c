a1b6e172b5ff3e121a267c594b9061c54ead5e19
jfoug <jfoug@openwall.net>
2017-11-24 01:34:38 -0600

    7zip:  BE porting.  Disable SIMD building on BE.  #2888

--- src/7z_fmt_plug.c
+++ src/7z_fmt_plug.c
@@ -30,6 +30,12 @@ john_register_one(&fmt_sevenzip);
 #endif
 
 #include "arch.h"
+
+#if !ARCH_LITTLE_ENDIAN
+#undef SIMD_COEF_32
+#undef SIMD_PARA_SHA256
+#endif
+
 #include "johnswap.h"
 #include "misc.h"
 #include "common.h"
