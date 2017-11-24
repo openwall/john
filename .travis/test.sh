18a0097499cb87f7473d07fa205c2df65e563882
jfoug <jfoug@openwall.net>
2017-11-24 12:57:42 -0600

    added encoding and utf8 tests to Travis CI

--- .travis/test.sh
+++ .travis/test.sh
@@ -9,11 +9,17 @@ echo 'gpg-opencl = Y' >> john-local.conf
 # Proper testing. Trusty AMD GPU drivers on Travis are fragile
 if test "$PROBLEM" = "slow" ; then
     ../run/john -test=0 --format=cpu
+    ../run/john -test=0 --format=cpu --encoding=utf8
+    ../run/john -test=0 --format=cpu --encoding=cp737
 else
     ../run/john -test-full=0 --format=cpu
+    ../run/john -test-full=0 --format=cpu --encoding=utf8
+    ../run/john -test-full=0 --format=cpu --encoding=cp737
 fi
 
 if test "$OPENCL" = "yes" ; then
     ../run/john -test-full=0 --format=opencl
+    ../run/john -test-full=0 --format=opencl --encoding=utf8
+    ../run/john -test-full=0 --format=opencl --encoding=cp737
 fi
 
