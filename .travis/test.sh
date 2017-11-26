f3c0621ebe80afa30df5667c1e02c1f3bb53e4af
Claudio André <claudioandre.br@gmail.com>
2017-11-26 16:58:26 -0200

    CI: run the encoding tests on its own job

--- .travis/test.sh
+++ .travis/test.sh
@@ -6,20 +6,27 @@ echo 'Raw-SHA512-free-opencl = Y' >> john-local.conf
 echo 'XSHA512-free-opencl = Y' >> john-local.conf
 echo 'gpg-opencl = Y' >> john-local.conf
 
-# Proper testing. Trusty AMD GPU drivers on Travis are fragile
-if test "$PROBLEM" = "slow" ; then
-    ../run/john -test=0 --format=cpu
-    ../run/john -test=0 --format=cpu --encoding=utf8
-    ../run/john -test=0 --format=cpu --encoding=cp737
-else
-    ../run/john -test-full=0 --format=cpu
+if test "$1" = "encoding" ; then
     ../run/john -test-full=0 --format=cpu --encoding=utf8
+    echo
     ../run/john -test-full=0 --format=cpu --encoding=cp737
-fi
 
-if test "$OPENCL" = "yes" ; then
-    ../run/john -test-full=0 --format=opencl
-    ../run/john -test-full=0 --format=opencl --encoding=utf8
-    ../run/john -test-full=0 --format=opencl --encoding=cp737
+    if test "$OPENCL" = "yes" ; then
+        ../run/john -test-full=0 --format=opencl --encoding=utf8
+        echo
+        ../run/john -test-full=0 --format=opencl --encoding=cp737
+    fi
+else
+
+    # Proper testing. Trusty AMD GPU drivers on Travis are fragile
+    if test "$PROBLEM" = "slow" ; then
+        ../run/john -test=0 --format=cpu
+    else
+        ../run/john -test-full=0 --format=cpu
+    fi
+
+    if test "$OPENCL" = "yes" ; then
+        ../run/john -test-full=0 --format=opencl
+    fi
 fi
 
