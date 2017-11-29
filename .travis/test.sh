55786e8c5b74e435773adabce5ee8062706010fb
Claudio André <claudioandre.br@gmail.com>
2017-11-28 23:54:28 -0200

    CI: test encoding inside OSX job

--- .travis/test.sh
+++ .travis/test.sh
@@ -1,5 +1,13 @@
 #!/bin/sh -e
 
+do_test_encoding() {
+    echo "Testing $1"
+    ../run/john -test-full=0 --format="$1" --encoding=utf8
+    echo
+    ../run/john -test-full=0 --format="$1" --encoding=cp737
+    echo "Done $1"
+}
+
 # There is a bug in echo -e in Travis
 echo '[Local:Disabled:Formats]' > john-local.conf
 echo 'Raw-SHA512-free-opencl = Y' >> john-local.conf
@@ -7,14 +15,10 @@ echo 'XSHA512-free-opencl = Y' >> john-local.conf
 echo 'gpg-opencl = Y' >> john-local.conf
 
 if test "$1" = "encoding" ; then
-    ../run/john -test-full=0 --format=cpu --encoding=utf8
-    echo
-    ../run/john -test-full=0 --format=cpu --encoding=cp737
+    do_test_encoding cpu
 
     if test "$OPENCL" = "yes" ; then
-        ../run/john -test-full=0 --format=opencl --encoding=utf8
-        echo
-        ../run/john -test-full=0 --format=opencl --encoding=cp737
+        do_test_encoding opencl
     fi
 else
 
@@ -25,8 +29,16 @@ else
         ../run/john -test-full=0 --format=cpu
     fi
 
+    if test "$TRAVIS_OS_NAME" = "osx" ; then
+        do_test_encoding cpu
+    fi
+
     if test "$OPENCL" = "yes" ; then
         ../run/john -test-full=0 --format=opencl
+
+        if test "$TRAVIS_OS_NAME" = "osx" ; then
+            do_test_encoding opencl
+        fi
     fi
 fi
 
