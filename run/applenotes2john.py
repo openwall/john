d1e56df919965918d13dc9b53bab53de3ce4b354
Dhiru Kholia <dhiru@openwall.com>
2017-12-14 11:24:55 +0530

    Add support for cracking Apple Notes databases

---
+++ run/applenotes2john.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+# Script to extract "hashes" from password protected Apple Notes databases.
+#
+# All credit goes to hashcat folks for doing the original hard work.
+#
+# ~/Library/Group Containers/group.com.apple.notes/NoteStore.sqlite <- typical
+# database location.
+#
+# This software is Copyright (c) 2017, Dhiru Kholia <kholia at kth.se> and it is
+# hereby released to the general public under the following terms:
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted.
+
+import os
+import sys
+import sqlite3
+import binascii
+
+PY3 = sys.version_info[0] == 3
+
+if not PY3:
+    reload(sys)
+    sys.setdefaultencoding('utf8')
+
+
+def process_file(filename):
+    db = sqlite3.connect(filename)
+    cursor = db.cursor()
+    rows = cursor.execute("SELECT Z_PK, ZCRYPTOITERATIONCOUNT, ZCRYPTOSALT, ZCRYPTOWRAPPEDKEY, ZPASSWORDHINT, ZCRYPTOVERIFIER, ZISPASSWORDPROTECTED FROM ZICCLOUDSYNCINGOBJECT")
+    for row in rows:
+        iden, iterations, salt, fhash, hint, shash, is_protected = row
+        if fhash is None:
+            phash = shash
+        else:
+            phash = fhash
+        if hint is None:
+            hint = "None"
+        # NOTE: is_protected can be zero even if iterations value is non-zero!
+        # This was tested on macOS 10.13.2 with cloud syncing turned off.
+        if iterations == 0:  # is this a safer check than checking is_protected?
+            continue
+        phash = binascii.hexlify(phash)
+        salt = binascii.hexlify(salt)
+        if PY3:
+            phash = str(phash, 'ascii')
+            salt = str(salt, 'ascii')
+        fname = os.path.basename(filename)
+        sys.stdout.write("%s:$ASN$*%d*%d*%s*%s:::::%s\n" % (fname, iden,
+                                               iterations, salt, phash, hint))
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        sys.stderr.write("Usage: %s [Apple Notes .sqlite files]\n" % sys.argv[0])
+        sys.exit(-1)
+
+    for i in range(1, len(sys.argv)):
+        process_file(sys.argv[i])
---
+++ src/notes_common_plug.c
@@ -0,0 +1,95 @@
+/*
+ * Common code for Apple Notes format.
+ */
+
+#include "arch.h"
+#include "misc.h"
+#include "common.h"
+#include "notes_common.h"
+#include "johnswap.h"
+#include "memdbg.h"
+
+struct fmt_tests notes_tests[] = {
+	// macOS 10.13.2 with cloud syncing turned off
+	{"$ASN$*4*20000*caff9d98b629cad13d54f5f3cbae2b85*79270514692c7a9d971a1ab6f6d22ba42c0514c29408c998", "openwall"},
+	{"$ASN$*4*20000*f5cf417cdec96291463fb0e19868307d*3ed7c0a3df2ecf2baaa0d4593acce83a04bea708446e0556", "åbc"},
+	// from hashcat project
+	{"$ASN$*1*20000*35317234050571840136152316737833*fdcdf88f2b65f95d5b9225db2c85496e7ebb12e655314460", "hashcat"},
+	{NULL}
+};
+
+int notes_common_valid(char *ciphertext, struct fmt_main *self)
+{
+	char *ctcopy, *keeptr, *p;
+	int extra;
+
+	if (strncmp(ciphertext, FORMAT_TAG, TAG_LENGTH) != 0)
+		return 0;
+
+	ctcopy = strdup(ciphertext);
+	keeptr = ctcopy;
+
+	ctcopy += TAG_LENGTH;
+	if ((p = strtokm(ctcopy, "*")) == NULL) // iden (unused)
+		goto err;
+	if (!isdec(p))
+		goto err;
+	if ((p = strtokm(NULL, "*")) == NULL)   // iterations
+		goto err;
+	if (!isdec(p))
+		goto err;
+	if ((p = strtokm(NULL, "*")) == NULL)   // salt
+		goto err;
+	if (hexlenl(p, &extra) != SALTLEN * 2 || extra)
+		goto err;
+	if ((p = strtokm(NULL, "*")) == NULL)   // wrapped kek, "blob"
+		goto err;
+	if (hexlenl(p, &extra) != BLOBLEN * 2 || extra)
+		goto err;
+
+	MEM_FREE(keeptr);
+	return 1;
+
+err:
+	MEM_FREE(keeptr);
+	return 0;
+}
+
+void *notes_common_get_salt(char *ciphertext)
+{
+	char *ctcopy = strdup(ciphertext);
+	char *keeptr = ctcopy;
+	int i;
+	char *p;
+	static struct custom_salt *cs;
+
+	cs = mem_calloc_tiny(sizeof(struct custom_salt), sizeof(uint64_t));
+
+	ctcopy += TAG_LENGTH;
+	p = strtokm(ctcopy, "*");
+	p = strtokm(NULL, "*");
+	cs->iterations = atoi(p);
+	cs->salt_length = SALTLEN;
+	p = strtokm(NULL, "*");
+	for (i = 0; i < cs->salt_length; i++)
+		cs->salt[i] = atoi16[ARCH_INDEX(p[i * 2])] * 16
+			+ atoi16[ARCH_INDEX(p[i * 2 + 1])];
+	p = strtokm(NULL, "*");
+	for (i = 0; i < BLOBLEN; i++)
+		cs->blob.chr[i] = atoi16[ARCH_INDEX(p[i * 2])] * 16
+			+ atoi16[ARCH_INDEX(p[i * 2 + 1])];
+#if ARCH_LITTLE_ENDIAN
+	for (i = 0; i < BLOBLEN / 8; i++)
+		 cs->blob.qword[i] = JOHNSWAP64(cs->blob.qword[i]);
+#endif
+	MEM_FREE(keeptr);
+
+	return (void *)cs;
+}
+
+unsigned int notes_common_iteration_count(void *salt)
+{
+	struct custom_salt *cs = salt;
+
+	return (unsigned int) cs->iterations;
+}
