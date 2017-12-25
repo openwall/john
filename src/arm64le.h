1df172e4af7bbefb5b4297a5330cdffde73fc21c
magnum <john.magnum@hushmail.com>
2017-12-25 12:35:37 +0100

    Autoconf changes for using NEON on ARM64.

---
+++ src/arm64le.h
@@ -0,0 +1,133 @@
+/*
+ * This file is part of John the Ripper password cracker,
+ * Copyright (c) 2017 by Solar Designer
+ * And others
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted.
+ *
+ * There's ABSOLUTELY NO WARRANTY, express or implied.
+ */
+
+/*
+ * Architecture specific parameters for 64-bit ARM in little-endian mode.
+ */
+
+#ifndef _JOHN_ARCH_H
+#define _JOHN_ARCH_H
+
+#if AC_BUILT
+#include "autoconfig.h"
+#else
+#define ARCH_WORD			long
+#define ARCH_SIZE			8
+#define ARCH_BITS			64
+#define ARCH_BITS_LOG			6
+#define ARCH_BITS_STR			"64"
+#define ARCH_LITTLE_ENDIAN		1
+#define ARCH_INT_GT_32			0
+#endif
+
+#define ARCH_ALLOWS_UNALIGNED		0
+#define ARCH_INDEX(x)			((unsigned int)(unsigned char)(x))
+
+#define CPU_DETECT			0
+
+#define DES_ASM				0
+#define DES_128K			0
+#define DES_X2				0
+#define DES_MASK			0
+#define DES_SCALE			1
+#define DES_EXTB			0
+#define DES_COPY			1
+#define DES_BS_ASM			0
+#define DES_BS				1
+#ifdef __ARM_NEON
+#if 0
+#define DES_BS_VECTOR			2
+#define DES_BS_ALGORITHM_NAME		"DES 64/64 NEON"
+#elif 0
+#define DES_BS_VECTOR			3
+#define DES_BS_VECTOR_SIZE		4
+#define DES_BS_ALGORITHM_NAME		"DES 64/64 NEON + 32/32"
+#elif 0
+#define DES_BS_2X64
+#define DES_BS_VECTOR			4
+#define DES_BS_ALGORITHM_NAME		"DES 64/64 X2 NEON"
+#elif 1
+#define DES_BS_VECTOR			4
+#define DES_BS_ALGORITHM_NAME		"DES 128/128 NEON"
+#elif 0
+#define DES_BS_VECTOR			5
+#define DES_BS_VECTOR_SIZE		8
+#define DES_BS_ALGORITHM_NAME		"DES 128/128 NEON + 32/32"
+#else
+#define DES_BS_VECTOR			8
+#define DES_BS_ALGORITHM_NAME		"DES 128/128 X2 NEON"
+#endif
+#else
+#define DES_BS_VECTOR			0
+#endif
+#define DES_BS_EXPAND			1
+
+#define MD5_ASM				0
+#define MD5_X2				1
+#define MD5_IMM				0
+
+#define BF_ASM				0
+#define BF_SCALE			1
+#define BF_X2				0
+
+#ifdef __ARM_NEON
+#define SIMD_COEF_32		4
+#define SIMD_COEF_64		2
+#ifndef SIMD_PARA_MD4
+#define SIMD_PARA_MD4		2
+#endif
+#ifndef SIMD_PARA_MD5
+#define SIMD_PARA_MD5		2
+#endif
+#ifndef SIMD_PARA_SHA1
+#define SIMD_PARA_SHA1		1
+#endif
+#ifndef SIMD_PARA_SHA256
+#define SIMD_PARA_SHA256	1
+#endif
+#ifndef SIMD_PARA_SHA512
+#define SIMD_PARA_SHA512	1
+#endif
+#endif
+
+#define STR_VALUE(arg)			#arg
+#define PARA_TO_N(n)			STR_VALUE(n) "x"
+#define PARA_TO_MxN(m, n)		STR_VALUE(m) "x" STR_VALUE(n)
+
+#if SIMD_PARA_MD4 > 1
+#define MD4_N_STR			PARA_TO_MxN(SIMD_COEF_32, SIMD_PARA_MD4)
+#else
+#define MD4_N_STR			PARA_TO_N(SIMD_COEF_32)
+#endif
+#if SIMD_PARA_MD5 > 1
+#define MD5_N_STR			PARA_TO_MxN(SIMD_COEF_32, SIMD_PARA_MD5)
+#else
+#define MD5_N_STR			PARA_TO_N(SIMD_COEF_32)
+#endif
+#if SIMD_PARA_SHA1 > 1
+#define SHA1_N_STR			PARA_TO_MxN(SIMD_COEF_32, SIMD_PARA_SHA1)
+#else
+#define SHA1_N_STR			PARA_TO_N(SIMD_COEF_32)
+#endif
+#if SIMD_PARA_SHA256 > 1
+#define SHA256_N_STR		PARA_TO_MxN(SIMD_COEF_32, SIMD_PARA_SHA256)
+#else
+#define SHA256_N_STR		PARA_TO_N(SIMD_COEF_32)
+#endif
+#if SIMD_PARA_SHA512 > 1
+#define SHA512_N_STR		PARA_TO_MxN(SIMD_COEF_64, SIMD_PARA_SHA512)
+#else
+#define SHA512_N_STR		PARA_TO_N(SIMD_COEF_64)
+#endif
+
+#define SHA_BUF_SIZ			16
+
+#endif
