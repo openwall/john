fbd1e90ddbe957fe2e06d56502f6dbc03aef6a42
Dhiru Kholia <dhiru.kholia@gmail.com>
2017-12-27 19:41:26 +0530

    Cleanup putty2john a bit

--- src/putty2john.c
+++ src/putty2john.c
@@ -1,10 +1,12 @@
-/* Modified in July 2012 by Dhiru Kholia <dhiru at openwall.com> to be
+/*
+ * Modified in July 2012 by Dhiru Kholia <dhiru at openwall.com> to be
  * standalone and compilable.
  *
  * p-ppk-crack v0.5 made by michu@neophob.com -- PuTTY private key cracker
  *
- * Source code based on putty svn version, check
- * http://www.chiark.greenend.org.uk/~sgtatham/putty/licence.html
+ * Source code based on putty svn version, see [1] for licensing information.
+ *
+ * [1] http://www.chiark.greenend.org.uk/~sgtatham/putty/licence.html
  */
 
 #ifndef PUTTY_COMMON_H
@@ -14,8 +16,8 @@
 #endif
 
 #include <stdint.h>
-#include <stddef.h>		       /* for size_t */
-#include <string.h>		       /* for memcpy() */
+#include <stddef.h>  /* for size_t */
+#include <string.h>  /* for memcpy() */
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -41,25 +43,24 @@
 #endif
 
 struct ssh2_userkey {
-    const struct ssh_signkey *alg;     /* the key algorithm */
-    void *data;			       /* the key data */
-    char *comment;		       /* the key comment */
+	const struct ssh_signkey *alg;  /* the key algorithm */
+	void *data;  /* the key data */
+	char *comment;  /* the key comment */
 };
 
 enum {
-    SSH_KEYTYPE_UNOPENABLE,
-    SSH_KEYTYPE_UNKNOWN,
-    SSH_KEYTYPE_SSH1, SSH_KEYTYPE_SSH2,
-    SSH_KEYTYPE_OPENSSH, SSH_KEYTYPE_SSHCOM
+	SSH_KEYTYPE_UNOPENABLE,
+	SSH_KEYTYPE_UNKNOWN,
+	SSH_KEYTYPE_SSH1, SSH_KEYTYPE_SSH2,
+	SSH_KEYTYPE_OPENSSH, SSH_KEYTYPE_SSHCOM
 };
 
 static int base64_decode_atom(char *atom, unsigned char *out);
 #endif
 
 typedef struct Filename {
-    char path[4096];
+	char path[4096];
 } Filename;
-
 
 static char header[40], *b, *encryption, *comment, *mac;
 static const char *putty_error = NULL;
@@ -157,20 +158,20 @@ static int read_header(FILE * fp, char *header)
 	while (len > 0) {
 		c = fgetc(fp);
 		if (c == '\n' || c == '\r' || c == EOF)
-			return 0;		       /* failure */
+			return 0;  /* failure */
 		if (c == ':') {
 			c = fgetc(fp);
 			if (c != ' ')
 				return 0;
 			*header = '\0';
-			return 1;		       /* success! */
+			return 1;  /* success! */
 		}
 		if (len == 0)
-			return 0;		       /* failure */
+			return 0;  /* failure */
 		*header++ = c;
 		len--;
 	}
-	return 0;			       /* failure */
+	return 0;  /* failure */
 }
 
 
@@ -329,18 +330,16 @@ error:
 
 static FILE *f_open(const Filename *filename, char const *mode, int is_private)
 {
-    if (!is_private) {
-        return fopen(filename->path, mode);
-    } else {
-        int fd;
-        fd = open(filename->path, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-        if (fd < 0)
-            return NULL;
-        return fdopen(fd, mode);
-    }
+	if (!is_private) {
+		return fopen(filename->path, mode);
+	} else {
+		int fd;
+		fd = open(filename->path, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+		if (fd < 0)
+			return NULL;
+		return fdopen(fd, mode);
+	}
 }
-
-
 
 /* ----------------------------------------------------------------------
  * A function to determine the type of a private key file. Returns
@@ -350,136 +349,136 @@ static FILE *f_open(const Filename *filename, char const *mode, int is_private)
 
 static int key_type(const Filename *filename)
 {
-    FILE *fp;
-    char buf[32];
-    const char putty2_sig[] = "PuTTY-User-Key-File-";
-    const char sshcom_sig[] = "---- BEGIN SSH2 ENCRYPTED PRIVAT";
-    const char openssh_sig[] = "-----BEGIN ";
-    int i;
+	FILE *fp;
+	char buf[32];
+	const char putty2_sig[] = "PuTTY-User-Key-File-";
+	const char sshcom_sig[] = "---- BEGIN SSH2 ENCRYPTED PRIVAT";
+	const char openssh_sig[] = "-----BEGIN ";
+	int i;
 
-    fp = f_open(filename, "r", FALSE);
-    if (!fp)
-	return SSH_KEYTYPE_UNOPENABLE;
-    i = fread(buf, 1, sizeof(buf), fp);
-    fclose(fp);
-    if (i < 0)
-	return SSH_KEYTYPE_UNOPENABLE;
-    if (i < 32)
-	return SSH_KEYTYPE_UNKNOWN;
-    if (!memcmp(buf, rsa_signature, sizeof(rsa_signature)-1))
-	return SSH_KEYTYPE_SSH1;
-    if (!memcmp(buf, putty2_sig, sizeof(putty2_sig)-1))
-	return SSH_KEYTYPE_SSH2;
-    if (!memcmp(buf, openssh_sig, sizeof(openssh_sig)-1))
-	return SSH_KEYTYPE_OPENSSH;
-    if (!memcmp(buf, sshcom_sig, sizeof(sshcom_sig)-1))
-	return SSH_KEYTYPE_SSHCOM;
-    return SSH_KEYTYPE_UNKNOWN;	       /* unrecognised or EOF */
+	fp = f_open(filename, "r", FALSE);
+	if (!fp)
+		return SSH_KEYTYPE_UNOPENABLE;
+	i = fread(buf, 1, sizeof(buf), fp);
+	fclose(fp);
+	if (i < 0)
+		return SSH_KEYTYPE_UNOPENABLE;
+	if (i < 32)
+		return SSH_KEYTYPE_UNKNOWN;
+	if (!memcmp(buf, rsa_signature, sizeof(rsa_signature)-1))
+		return SSH_KEYTYPE_SSH1;
+	if (!memcmp(buf, putty2_sig, sizeof(putty2_sig)-1))
+		return SSH_KEYTYPE_SSH2;
+	if (!memcmp(buf, openssh_sig, sizeof(openssh_sig)-1))
+		return SSH_KEYTYPE_OPENSSH;
+	if (!memcmp(buf, sshcom_sig, sizeof(sshcom_sig)-1))
+		return SSH_KEYTYPE_SSHCOM;
+	return SSH_KEYTYPE_UNKNOWN;	       /* unrecognised or EOF */
 }
 
 static int ssh2_userkey_encrypted(const Filename *filename, char **commentptr)
 {
-    FILE *fp;
-    char header[40], *b, *comment;
-    int ret;
+	FILE *fp;
+	char header[40], *b, *comment;
+	int ret;
 
-    if (commentptr)
-	*commentptr = NULL;
+	if (commentptr)
+		*commentptr = NULL;
 
-    fp = f_open(filename, "rb", FALSE);
-    if (!fp)
-	return 0;
-    if (!read_header(fp, header)
-	|| (0 != strcmp(header, "PuTTY-User-Key-File-2") &&
-	    0 != strcmp(header, "PuTTY-User-Key-File-1"))) {
-	fclose(fp);
-	return 0;
-    }
-    if ((b = read_body(fp)) == NULL) {
-	fclose(fp);
-	return 0;
-    }
-    MEM_FREE(b);			       /* we don't care about key type here */
-    /* Read the Encryption header line. */
-    if (!read_header(fp, header) || 0 != strcmp(header, "Encryption")) {
-	fclose(fp);
-	return 0;
-    }
-    if ((b = read_body(fp)) == NULL) {
-	fclose(fp);
-	return 0;
-    }
+	fp = f_open(filename, "rb", FALSE);
+	if (!fp)
+		return 0;
+	if (!read_header(fp, header)
+			|| (0 != strcmp(header, "PuTTY-User-Key-File-2") &&
+				0 != strcmp(header, "PuTTY-User-Key-File-1"))) {
+		fclose(fp);
+		return 0;
+	}
+	if ((b = read_body(fp)) == NULL) {
+		fclose(fp);
+		return 0;
+	}
+	MEM_FREE(b);  /* we don't care about key type here */
+	/* Read the Encryption header line. */
+	if (!read_header(fp, header) || 0 != strcmp(header, "Encryption")) {
+		fclose(fp);
+		return 0;
+	}
+	if ((b = read_body(fp)) == NULL) {
+		fclose(fp);
+		return 0;
+	}
 
-    /* Read the Comment header line. */
-    if (!read_header(fp, header) || 0 != strcmp(header, "Comment")) {
+	/* Read the Comment header line. */
+	if (!read_header(fp, header) || 0 != strcmp(header, "Comment")) {
+		fclose(fp);
+		MEM_FREE(b);
+		return 1;
+	}
+	if ((comment = read_body(fp)) == NULL) {
+		fclose(fp);
+		MEM_FREE(b);
+		return 1;
+	}
+
+	if (commentptr)
+		*commentptr = comment;
+
 	fclose(fp);
+	if (!strcmp(b, "aes256-cbc"))
+		ret = 1;
+	else
+		ret = 0;
 	MEM_FREE(b);
-	return 1;
-    }
-    if ((comment = read_body(fp)) == NULL) {
-	fclose(fp);
-	MEM_FREE(b);
-	return 1;
-    }
-
-    if (commentptr)
-	*commentptr = comment;
-
-    fclose(fp);
-    if (!strcmp(b, "aes256-cbc"))
-	ret = 1;
-    else
-	ret = 0;
-    MEM_FREE(b);
-    return ret;
+	return ret;
 }
 
 static int base64_decode_atom(char *atom, unsigned char *out)
 {
-    int vals[4];
-    int i, v, len;
-    unsigned word;
-    char c;
+	int vals[4];
+	int i, v, len;
+	unsigned word;
+	char c;
 
-    for (i = 0; i < 4; i++) {
-        c = atom[i];
-        if (c >= 'A' && c <= 'Z')
-            v = c - 'A';
-        else if (c >= 'a' && c <= 'z')
-            v = c - 'a' + 26;
-        else if (c >= '0' && c <= '9')
-            v = c - '0' + 52;
-        else if (c == '+')
-            v = 62;
-        else if (c == '/')
-            v = 63;
-        else if (c == '=')
-            v = -1;
-        else
-            return 0;                  /* invalid atom */
-        vals[i] = v;
-    }
+	for (i = 0; i < 4; i++) {
+		c = atom[i];
+		if (c >= 'A' && c <= 'Z')
+			v = c - 'A';
+		else if (c >= 'a' && c <= 'z')
+			v = c - 'a' + 26;
+		else if (c >= '0' && c <= '9')
+			v = c - '0' + 52;
+		else if (c == '+')
+			v = 62;
+		else if (c == '/')
+			v = 63;
+		else if (c == '=')
+			v = -1;
+		else
+			return 0;  /* invalid atom */
+		vals[i] = v;
+	}
 
-    if (vals[0] == -1 || vals[1] == -1)
-        return 0;
-    if (vals[2] == -1 && vals[3] != -1)
-        return 0;
+	if (vals[0] == -1 || vals[1] == -1)
+		return 0;
+	if (vals[2] == -1 && vals[3] != -1)
+		return 0;
 
-    if (vals[3] != -1)
-        len = 3;
-    else if (vals[2] != -1)
-        len = 2;
-    else
-        len = 1;
+	if (vals[3] != -1)
+		len = 3;
+	else if (vals[2] != -1)
+		len = 2;
+	else
+		len = 1;
 
-    word = ((vals[0] << 18) |
-            (vals[1] << 12) | ((vals[2] & 0x3F) << 6) | (vals[3] & 0x3F));
-    out[0] = (word >> 16) & 0xFF;
-    if (len > 1)
-        out[1] = (word >> 8) & 0xFF;
-    if (len > 2)
-        out[2] = word & 0xFF;
-    return len;
+	word = ((vals[0] << 18) |
+			(vals[1] << 12) | ((vals[2] & 0x3F) << 6) | (vals[3] & 0x3F));
+	out[0] = (word >> 16) & 0xFF;
+	if (len > 1)
+		out[1] = (word >> 8) & 0xFF;
+	if (len > 2)
+		out[2] = word & 0xFF;
+	return len;
 }
 
 static void process_file(const char *fname)
@@ -576,7 +575,8 @@ int main(int argc, char **argv)
 	}
 
 	for (i = 1; i < argc; i++)
-	    process_file(argv[i]);
+		process_file(argv[i]);
+
 	return 0;
 }
 #endif
