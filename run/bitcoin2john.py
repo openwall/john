3b24d056aef757ff6ed86365e4af21a90353732a
Dhiru Kholia <dhiru@openwall.com>
2018-01-04 12:24:18 +0530

    Fix license text in bitcoin2john.py utility

--- run/bitcoin2john.py
+++ run/bitcoin2john.py
@@ -1,8 +1,32 @@
 #!/usr/bin/env python
 
-# jackjack's pywallet.py
-# https://github.com/jackjack-jj/pywallet
-# forked from Joric's pywallet.py
+# This software is Copyright (c) 2012, Dhiru Kholia <dhiru at openwall.com> and
+# it is hereby placed in the public domain.
+#
+# This utility (bitcoin2john.py) is based on jackjack's pywallet.py [1] which
+# is forked from Joric's pywallet.py whose licensing information follows,
+#
+# [1] https://github.com/jackjack-jj/pywallet
+#
+# PyWallet 1.2.1 (Public Domain)
+# http://github.com/joric/pywallet
+# Most of the actual PyWallet code placed in the public domain.
+# PyWallet includes portions of free software, listed below.
+#
+# BitcoinTools (wallet.dat handling code, MIT License)
+# https://github.com/gavinandresen/bitcointools
+# Copyright (c) 2010 Gavin Andresen
+#
+# python-ecdsa (EC_KEY implementation, MIT License)
+# http://github.com/warner/python-ecdsa
+# "python-ecdsa" Copyright (c) 2010 Brian Warner
+# Portions written in 2005 by Peter Pearson and placed in the public domain.
+#
+# SlowAES (aes.py code, Apache 2 License)
+# http://code.google.com/p/slowaes/
+# Copyright (c) 2008, Josh Davis (http://www.josh-davis.org),
+# Alex Martelli (http://www.aleax.it)
+# Ported from C code written by Laurent Haan (http://www.progressive-coding.com)
 
 missing_dep = []
 
