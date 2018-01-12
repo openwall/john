dd954c29a3d0f82ae105b0807fc0a42219e4aa8d
magnum <john.magnum@hushmail.com>
2018-01-12 15:45:06 +0100

    Mysqlna format: Tune MKPC and OMP

--- src/mysql_netauth_fmt_plug.c
+++ src/mysql_netauth_fmt_plug.c
@@ -18,9 +18,6 @@ john_register_one(&fmt_mysqlna);
 
 #ifdef _OPENMP
 #include <omp.h>
-#ifndef OMP_SCALE
-#define OMP_SCALE               1024 // tuned K8-dual HT
-#endif
 #endif
 
 #include "arch.h"
@@ -47,7 +44,11 @@ john_register_one(&fmt_mysqlna);
 #define SALT_SIZE               sizeof(struct custom_salt)
 #define SALT_ALIGN              MEM_ALIGN_NONE
 #define MIN_KEYS_PER_CRYPT      1
-#define MAX_KEYS_PER_CRYPT      1
+#define MAX_KEYS_PER_CRYPT      256
+
+#ifndef OMP_SCALE
+#define OMP_SCALE               2 // Tuned w/ MKPC for core i7
+#endif
 
 static struct fmt_tests mysqlna_tests[] = {
 	{"$mysqlna$2D52396369653E4626293B2F75244D3871507A39*7D63098BEE381A51AA6DF11E307E46BD4F8B6E0C", "openwall"},
@@ -65,9 +66,8 @@ static struct custom_salt {
 
 static void init(struct fmt_main *self)
 {
-#ifdef _OPENMP
 	omp_autotune(self, OMP_SCALE);
-#endif
+
 	saved_key = mem_calloc(self->params.max_keys_per_crypt,
 	                       sizeof(*saved_key));
 	crypt_out = mem_calloc(self->params.max_keys_per_crypt,
