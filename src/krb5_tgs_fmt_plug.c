4f28d791d19d57bad756c4e54838227a7426527d
magnum <john.magnum@hushmail.com>
2018-01-12 15:24:39 +0100

    krb5tgs: Tune MKPC and OMP

--- src/krb5_tgs_fmt_plug.c
+++ src/krb5_tgs_fmt_plug.c
@@ -25,6 +25,7 @@ john_register_one(&fmt_krb5tgs);
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
+
 #ifdef _OPENMP
 #include <omp.h>
 #endif
@@ -39,10 +40,6 @@ john_register_one(&fmt_krb5tgs);
 #include "unicode.h"
 #include "memdbg.h"
 
-#ifndef OMP_SCALE
-#define OMP_SCALE            256
-#endif
-
 #define FORMAT_LABEL         "krb5tgs"
 #define FORMAT_NAME          "Kerberos 5 TGS etype 23"
 #define FORMAT_TAG           "$krb5tgs$23$"
@@ -56,7 +53,11 @@ john_register_one(&fmt_krb5tgs);
 #define SALT_SIZE            sizeof(struct custom_salt *)
 #define SALT_ALIGN           sizeof(struct custom_salt *)
 #define MIN_KEYS_PER_CRYPT   1
-#define MAX_KEYS_PER_CRYPT   1
+#define MAX_KEYS_PER_CRYPT   64
+
+#ifndef OMP_SCALE
+#define OMP_SCALE            4 // Tuned w/ MKPC for core i7
+#endif
 
 /*
   assuming checksum == edata1
@@ -169,9 +170,8 @@ err:
 
 static void init(struct fmt_main *self)
 {
-#ifdef _OPENMP
 	omp_autotune(self, OMP_SCALE);
-#endif
+
 	saved_key = mem_alloc_align(sizeof(*saved_key) *
 			self->params.max_keys_per_crypt,
 			MEM_ALIGN_CACHE);
@@ -281,10 +281,10 @@ static int crypt_all(int *pcount, struct db_salt *salt)
 		memset(cracked, 0, cracked_size);
 		any_cracked = 0;
 	}
+
 #ifdef _OPENMP
 #pragma omp parallel for
 #endif
-
 	for (index = 0; index < count; index++) {
 		unsigned char K3[16];
 #ifdef _MSC_VER
