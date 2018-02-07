5cd2ea23a9337068b9b994527250cf9bd7da62cb
magnum <john.magnum@hushmail.com>
2018-02-07 10:46:29 +0100

    truth.pl: Add some tweaks and usage blurb bits. Even I did not remember how to use it.

--- src/truth.pl
+++ src/truth.pl
@@ -15,13 +15,16 @@ use strict;
 
 my $f;
 
-if (defined($ARGV[0]) && $#ARGV == 0) {
+if (defined($ARGV[0]) && $#ARGV == 0 && !($ARGV[0] =~ m/a-wA-Z/) &&
+	$ARGV[0] =~ m/x/ && $ARGV[0] =~ m/y/ && $ARGV[0] =~ m/z/) {
 	$f = $ARGV[0];
 } else {
-	die "Usage: $0 <formula>\n\nExample:\n$0 '(x & y ^ z)'\n";
+	print "\nEvaluate truth table for eg. _mm512_ternarylogic_epi32 or LOP3.LUT\n\n";
+	die "Usage: $0 <formula written with x, y and z>\n\nExample:\n$0 '(x & y ^ z)'\n";
 }
 
 my $orig_f = $f;
+$f = "(" . $f . ")";
 $f =~ s/ //g;
 $f =~ s/([^\$])([xyz])/$1\$$2/g;
 $f =~ s/~(\([^\)]*\))/(~$1 & 1)/g;
@@ -37,8 +40,8 @@ foreach my $i (0..7) {
 }
 printf("lut3(x, y, z, 0x%02x)  ==  %s\n", $r, $orig_f);
 
-if (0) {
-	print "\nAlternatives:\n";
+if (1) {
+	print "\nAlternatives (different order of operands):\n";
 	$r = 0;
 	foreach my $i (0..7) {
 		my $x = ($i >> 2) & 0x1;
