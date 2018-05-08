15d23334df4650d008a981144f21fcc2917638b1
magnum <john.magnum@hushmail.com>
2018-05-08 14:25:09 +0200

    PGPSDA OpenCL format: Post-process (CAST) on GPU.  See 3242.
    Incidentally, a memory leak was fixed. The now dropped crypt_out buffer
    was never freed.

--- src/opencl/pgpsda_kernel.cl
+++ src/opencl/pgpsda_kernel.cl
@@ -1,15 +1,15 @@
 /*
- * This software is Copyright (c) 2017 Dhiru Kholia <dhiru at openwall.com> and
- * Copyright (c) 2017 magnum, and it is hereby released to the general public
- * under the following terms:
- *
+ * This software is
+ * Copyright (c) 2017 Dhiru Kholia <dhiru at openwall.com>
+ * Copyright (c) 2017-2018 magnum
+ * and it is hereby released to the general public under the following terms:
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted.
  */
 
-#include "opencl_device_info.h"
 #include "opencl_misc.h"
 #include "opencl_sha1_ctx.h"
+#include "opencl_cast.h"
 
 #ifndef PLAINTEXT_LENGTH
 #error PLAINTEXT_LENGTH must be defined
@@ -25,7 +25,7 @@ typedef struct {
 } pgpsda_password;
 
 typedef struct {
-	uchar v[16];
+	uchar v[8];
 } pgpsda_hash;
 
 typedef struct {
@@ -38,13 +38,13 @@ inline
 #endif
 void pgpsda_kdf(__global const uchar *ipassword, const uint plen,
                  __constant uchar *isalt, const uint iterations,
-				 __global uchar *okey)
+				uchar *key)
 {
 	SHA_CTX ctx;
 	uint32_t j = 0;
-	unsigned char password[PLAINTEXT_LENGTH];
-	unsigned char salt[8];
-	unsigned char key[SHA1_DIGEST_LENGTH];
+	uchar password[PLAINTEXT_LENGTH];
+	uchar salt[8];
+
 	memcpy_cp(salt, isalt, 8);
 	memcpy_gp(password, ipassword, plen);
 
@@ -59,16 +59,21 @@ void pgpsda_kdf(__global const uchar *ipassword, const uint plen,
 #endif
 	}
 	SHA1_Final(key, &ctx);
-
-	memcpy_pg(okey, key, 16);
 }
 
 __kernel void pgpsda(__global const pgpsda_password *inbuffer,
-                  __global pgpsda_hash *outbuffer,
-                  __constant pgpsda_salt *salt)
+                     __global pgpsda_hash *outbuffer,
+                     __constant pgpsda_salt *salt)
 {
 	uint idx = get_global_id(0);
+	uchar key[SHA1_DIGEST_LENGTH];
+	CAST_KEY ck;
 
 	pgpsda_kdf(inbuffer[idx].v, inbuffer[idx].length, salt->salt,
-			salt->iterations, outbuffer[idx].v);
+	           salt->iterations, key);
+
+	CAST_set_key(&ck, 16, key);
+	CAST_ecb_encrypt(key, key, &ck);
+
+	memcpy_pg(outbuffer[idx].v, key, 8);
 }
--- src/opencl_pgpsda_fmt_plug.c
+++ src/opencl_pgpsda_fmt_plug.c
@@ -1,9 +1,10 @@
 /*
  * Format for brute-forcing PGP SDAs (self-decrypting archives).
  *
- * This software is Copyright (c) 2017 Dhiru Kholia <dhiru at openwall.net> and
- * it is hereby released to the general public under the following terms:
- *
+ * This software is
+ * Copyright (c) 2017 Dhiru Kholia <dhiru at openwall.com>
+ * Copyright (c) 2017-2018 magnum
+ * and it is hereby released to the general public under the following terms:
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted.
  */
@@ -18,23 +19,18 @@ john_register_one(&fmt_opencl_pgpsda);
 
 #include <stdint.h>
 #include <string.h>
-#include <openssl/cast.h>
-#ifdef _OPENMP
-#include <omp.h>
-#endif
 
 #include "arch.h"
 #include "params.h"
 #include "common.h"
 #include "formats.h"
 #include "misc.h"
-#include "sha.h"
 #include "opencl_common.h"
 #include "options.h"
 #include "pgpsda_common.h"
 
 #define FORMAT_LABEL            "pgpsda-opencl"
-#define ALGORITHM_NAME          "SHA1 OpenCL"
+#define ALGORITHM_NAME          "SHA1 CAST OpenCL"
 #define BINARY_SIZE             8
 #define BINARY_ALIGN            sizeof(uint32_t)
 #define SALT_SIZE               sizeof(struct custom_salt)
@@ -51,7 +47,7 @@ typedef struct {
 } pgpsda_password;
 
 typedef struct {
-	uint8_t v[16];
+	uint8_t v[BINARY_SIZE];
 } pgpsda_hash;
 
 typedef struct {
@@ -59,7 +55,6 @@ typedef struct {
 	uint8_t salt[8];
 } pgpsda_salt;
 
-static uint32_t (*crypt_out)[BINARY_SIZE * 2 / sizeof(uint32_t)];
 static struct custom_salt *cur_salt;
 
 static cl_int cl_error;
@@ -89,7 +84,6 @@ static void create_clobj(size_t gws, struct fmt_main *self)
 	insize = sizeof(pgpsda_password) * gws;
 	outsize = sizeof(pgpsda_hash) * gws;
 	settingsize = sizeof(pgpsda_salt);
-	crypt_out = mem_calloc(gws, sizeof(*crypt_out));
 
 	inbuffer = mem_calloc(1, insize);
 	outbuffer = mem_alloc(outsize);
@@ -225,7 +219,6 @@ static char *get_key(int index)
 static int crypt_all(int *pcount, struct db_salt *salt)
 {
 	const int count = *pcount;
-	int index = 0;
 	size_t *lws = local_work_size ? &local_work_size : NULL;
 
 	global_work_size = GET_MULTIPLE_OR_BIGGER(count, local_work_size);
@@ -246,22 +239,6 @@ static int crypt_all(int *pcount, struct db_salt *salt)
 		outsize, outbuffer, 0, NULL, multi_profilingEvent[2]),
 		"Copy result back");
 
-	if (ocl_autotune_running)
-		return count;
-
-#ifdef _OPENMP
-#pragma omp parallel for
-#endif
-	for (index = 0; index < count; index++) {
-		unsigned char  *key;
-		CAST_KEY ck;
-
-		key =  outbuffer[index].v;
-		CAST_set_key(&ck, 16, key);
-		memset((unsigned char*)crypt_out[index], 0, BINARY_SIZE);
-		CAST_ecb_encrypt(key, (unsigned char*)crypt_out[index], &ck, CAST_ENCRYPT);
-	}
-
 	return count;
 }
 
@@ -270,14 +247,14 @@ static int cmp_all(void *binary, int count)
 	int index;
 
 	for (index = 0; index < count; index++)
-		if (!memcmp(binary, crypt_out[index], ARCH_SIZE))
+		if (!memcmp(binary, outbuffer[index].v, ARCH_SIZE))
 			return 1;
 	return 0;
 }
 
 static int cmp_one(void *binary, int index)
 {
-	return !memcmp(binary, crypt_out[index], BINARY_SIZE);
+	return !memcmp(binary, outbuffer[index].v, BINARY_SIZE);
 }
 
 static int cmp_exact(char *source, int index)
@@ -300,7 +277,7 @@ struct fmt_main fmt_opencl_pgpsda = {
 		SALT_ALIGN,
 		MIN_KEYS_PER_CRYPT,
 		MAX_KEYS_PER_CRYPT,
-		FMT_CASE | FMT_8_BIT | FMT_OMP,
+		FMT_CASE | FMT_8_BIT,
 		{
 			"iteration count",
 		},
