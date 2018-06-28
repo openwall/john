openwall/john

commit e5b031ed609c9f444a867dc6f70255cdc93a90ce

Revert Jumbo's old BENCHMARK_LENGTH -1000 hack (it predates Git history), and instead *always* smash passwords when benchmarking - and in a much better way for OpenCL formats in particular.  They may otherwise show an inflated benchmark speed due to way less thread divergence than in real use.
This affects benchmark speed for a handfull of formats, some actually
positive (because they didn't use the previous hack although they should
have, so they are no longer executing slow code for correct passwords)
and some negative.  In both cases, the new figure is more correct.

src/bench.c
@@ -239,6 +239,9 @@ static void bench_set_keys(struct fmt_main *format,
 
 	length = format->params.benchmark_length;
 	for (index = 0; index < format->params.max_keys_per_crypt; index++) {
+		char random_pass[PLAINTEXT_BUFFER_SIZE];
+		int i;
+
 		do {
 			if (!current->ciphertext)
 				current = format->params.tests;
@@ -254,14 +257,17 @@ static void bench_set_keys(struct fmt_main *format,
 				break;
 		} while (1);
 
+		strnzcpy(random_pass, plaintext, sizeof(random_pass));
+		for (i = 0; i < strlen(plaintext); i++)
+			random_pass[i] ^= (index & 127);
 #ifndef BENCH_BUILD
 		if (options.flags & FLG_MASK_CHK) {
-			plaintext[len] = 0;
-			if (do_mask_crack(len ? plaintext : NULL))
+			random_pass[len] = 0;
+			if (do_mask_crack(len ? random_pass : NULL))
 				return;
 		} else
 #endif
-			format->methods.set_key(plaintext, index);
+			format->methods.set_key(random_pass, index);
 	}
 }
 
@@ -292,8 +298,6 @@ char *benchmark_format(struct fmt_main *format, int salts,
 	static int binary_size = 0;
 	static char s_error[128];
 	static int wait_salts = 0;
-	char *TmpPW[1024];
-	int pw_mangled = 0;
 	char *where;
 	struct fmt_tests *current;
 	int cond;
@@ -434,30 +438,6 @@ char *benchmark_format(struct fmt_main *format, int salts,
 	}
 #endif
 
-/* Smashed passwords: -1001 turns into -1 and -1000 turns into 0, and
-   -999 turns into 1 for benchmark length. */
-	if (format->params.benchmark_length < -950) {
-		pw_mangled = 1;
-		format->params.benchmark_length += 1000;
-	}
-
-/* Ensure we use a buffer that can be read past end of word
-   (eg. SIMD optimizations). */
-	i = 0;
-	current = format->params.tests;
-	while (current->ciphertext && i < 1024) {
-		TmpPW[i] = current->plaintext;
-		current->plaintext =
-			strnzcpy(mem_alloc(PLAINTEXT_BUFFER_SIZE),
-			         TmpPW[i++], PLAINTEXT_BUFFER_SIZE);
-
-		/* Smash passwords! */
-		if (current->plaintext[0] && pw_mangled == 1)
-			current->plaintext[0] ^= 5;
-
-		++current;
-	}
-
 	if (format->params.benchmark_length > 0) {
 		cond = (salts == 1) ? 1 : -1;
 		salts = 1;
@@ -568,18 +548,6 @@ char *benchmark_format(struct fmt_main *format, int salts,
 			dyna_salt_remove(two_salts[index]);
 		MEM_FREE(two_salts[index]);
 	}
-
-	/* Unsmash/unbuffer the passwords. */
-	i = 0;
-	current = format->params.tests;
-	while (current->ciphertext && i < 1024) {
-		MEM_FREE(current->plaintext);
-		current->plaintext = TmpPW[i++];
-		++current;
-	}
-
-	if (pw_mangled)
-		format->params.benchmark_length -= 1000;
 
 	return event_abort ? "" : NULL;
 }
@@ -781,7 +749,6 @@ AGAIN:
 
 		switch (format->params.benchmark_length) {
 		case 0:
-		case -1000:
 			if (format->params.tests[1].ciphertext) {
 				msg_m = "Many salts";
 				msg_1 = "Only one salt";
@@ -790,7 +757,6 @@ AGAIN:
 			/* fall through */
 
 		case -1:
-		case -1001:
 			msg_m = "Raw";
 			msg_1 = NULL;
 			break;

src/bitcoin_fmt_plug.c
@@ -64,7 +64,7 @@ john_register_one(&fmt_bitcoin);
 #endif
 
 #define BENCHMARK_COMMENT       ""
-#define BENCHMARK_LENGTH        -1000
+#define BENCHMARK_LENGTH        0
 #define PLAINTEXT_LENGTH        125
 #define BINARY_SIZE             0
 #define BINARY_ALIGN            1

src/bitshares_fmt_plug.c
@@ -37,7 +37,7 @@ john_register_one(&fmt_bitshares);
 #define TAG_LENGTH              (sizeof(FORMAT_TAG) - 1)
 #define ALGORITHM_NAME          "SHA-512 64/" ARCH_BITS_STR
 #define BENCHMARK_COMMENT       ""
-#define BENCHMARK_LENGTH        -1000
+#define BENCHMARK_LENGTH        0
 #define PLAINTEXT_LENGTH        125
 #define BINARY_SIZE             0
 #define BINARY_ALIGN            1

src/bitwarden_fmt_plug.c
@@ -47,7 +47,7 @@ john_register_one(&fmt_bitwarden);
 #endif
 #endif
 #define BENCHMARK_COMMENT       ""
-#define BENCHMARK_LENGTH        -1000
+#define BENCHMARK_LENGTH        0
 #define BINARY_SIZE             0
 #define PLAINTEXT_LENGTH        125
 #define SALT_SIZE               sizeof(struct custom_salt)

src/dmg_fmt_plug.c
@@ -101,7 +101,7 @@ extern volatile int bench_running;
 #define ALGORITHM_NAME      "PBKDF2-SHA1 3DES/AES 32/" ARCH_BITS_STR
 #endif
 #define BENCHMARK_COMMENT   ""
-#define BENCHMARK_LENGTH    -1001
+#define BENCHMARK_LENGTH    -1
 #define BINARY_SIZE         0
 #define PLAINTEXT_LENGTH	125
 #define SALT_SIZE           sizeof(struct custom_salt)

src/encfs_fmt_plug.c
@@ -40,7 +40,7 @@ john_register_one(&fmt_encfs);
 #define ALGORITHM_NAME      "PBKDF2-SHA1 AES/Blowfish 32/" ARCH_BITS_STR
 #endif
 #define BENCHMARK_COMMENT   ""
-#define BENCHMARK_LENGTH    -1000
+#define BENCHMARK_LENGTH    0
 #define BINARY_SIZE         0
 #define PLAINTEXT_LENGTH    125
 #define BINARY_ALIGN        MEM_ALIGN_NONE

src/gpg_common.h
@@ -14,7 +14,7 @@
 #include "dyna_salt.h"
 
 #define BENCHMARK_COMMENT    ""
-#define BENCHMARK_LENGTH     -1000
+#define BENCHMARK_LENGTH     0
 #define FORMAT_TAG           "$gpg$*"
 #define FORMAT_TAG_LEN       (sizeof(FORMAT_TAG)-1)
 #define BINARY_SIZE          0

src/krb5_asrep_fmt_plug.c
@@ -89,7 +89,7 @@ john_register_one(&fmt_krb5asrep);
 #define ALGORITHM_NAME          "MD4 HMAC-MD5 RC4 / PBKDF2 HMAC-SHA1 AES 32/" ARCH_BITS_STR
 #endif
 #define BENCHMARK_COMMENT       ""
-#define BENCHMARK_LENGTH        -1000
+#define BENCHMARK_LENGTH        0
 #define MIN_PLAINTEXT_LENGTH    0
 #define PLAINTEXT_LENGTH        125
 #define BINARY_SIZE             0

commit 2241040eb66a537761dbf3ca14edb7395c8439aa

Mitigate a benchmark regression introduced in e5b031e. See #3289.

src/bench.c
@@ -52,6 +52,7 @@
 #include "config.h"
 #include "common-gpu.h"
 #include "mask.h"
+#include "aligned.h"
 
 #ifndef BENCH_BUILD
 #include "options.h"
@@ -223,6 +224,18 @@ static void bench_install_handler(void)
 #endif
 }
 
+static char *strnzcpy_mangle(char *dst, const char *src, int size, int mangle)
+{
+	char *dptr = dst;
+
+	if (size)
+		while (--size && *src)
+			*dptr++ = *src++ ^ mangle;
+	*dptr = 0;
+
+	return dst;
+}
+
 static void bench_set_keys(struct fmt_main *format,
 	struct fmt_tests *current, int cond)
 {
@@ -239,8 +252,8 @@ static void bench_set_keys(struct fmt_main *format,
 
 	length = format->params.benchmark_length;
 	for (index = 0; index < format->params.max_keys_per_crypt; index++) {
-		char random_pass[PLAINTEXT_BUFFER_SIZE];
-		int i;
+		JTR_ALIGN(MEM_ALIGN_WORD)
+			char random_pass[PLAINTEXT_BUFFER_SIZE];
 
 		do {
 			if (!current->ciphertext)
@@ -257,9 +270,8 @@ static void bench_set_keys(struct fmt_main *format,
 				break;
 		} while (1);
 
-		strnzcpy(random_pass, plaintext, sizeof(random_pass));
-		for (i = 0; i < strlen(plaintext); i++)
-			random_pass[i] ^= (index & 127);
+		strnzcpy_mangle(random_pass, plaintext,
+		                sizeof(random_pass), index & 127);
 #ifndef BENCH_BUILD
 		if (options.flags & FLG_MASK_CHK) {
 			random_pass[len] = 0;