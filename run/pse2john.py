61d7e0a67253b1fd0bba233a4372d5982c79e23a
Dhiru Kholia <dhiru.kholia@gmail.com>
2018-07-12 15:11:11 +0530

    Add support for cracking SAP's PSE files

---
+++ run/pse2john.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+# ===========
+# pysap - Python library for crafting SAP's network protocols packets
+#
+# Copyright (C) 2012-2018 by Martin Gallo, Core Security
+#
+# The library was designed and developed by Martin Gallo from
+# Core Security's CoreLabs team.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# ==============
+
+# Standard imports
+from sys import stdout
+from binascii import hexlify
+from optparse import OptionParser
+# Custom imports
+import pysap
+from pysap.SAPPSE import (SAPPSEFile, PKCS12_ALGORITHM_PBE1_SHA_3DES_CBC)
+
+
+# Command line options parser
+def parse_options():
+
+    description = "This script can be used to parse PSE files and extract encrypted material and data in a format that" \
+                  "John the Ripper or other cracking tools can use to look for the decryption PIN."
+
+    epilog = "pysap %(version)s - %(url)s - %(repo)s" % {"version": pysap.__version__,
+                                                         "url": pysap.__url__,
+                                                         "repo": pysap.__repo__}
+
+    usage = "Usage: %prog <input_file>"
+
+    parser = OptionParser(usage=usage, description=description, epilog=epilog)
+    parser.add_option("-o", "--output", help="Filename to write the output to [stdout]")
+
+    options, args = parser.parse_args()
+
+    return options, args
+
+
+def parse_pse(filename):
+    """Parses a PSE file and produces """
+    with open(filename, "rb") as f:
+        data = f.read()
+
+    pse_file = SAPPSEFile(data)
+
+    if pse_file.enc_cont.algorithm_identifier.alg_id == PKCS12_ALGORITHM_PBE1_SHA_3DES_CBC:
+        pbe_algo = 1
+        salt = hexlify(pse_file.enc_cont.algorithm_identifier.parameters.salt.val)
+        salt_size = len(pse_file.enc_cont.algorithm_identifier.parameters.salt.val)
+        iterations = pse_file.enc_cont.algorithm_identifier.parameters.iterations.val
+        iv = ""
+        iv_size = len(iv)
+    else:
+        raise Exception("Unsupported encryption algorithm")
+
+    encrypted_pin = hexlify(pse_file.enc_cont.encrypted_pin.val)
+    encrypted_pin_length = len(pse_file.enc_cont.encrypted_pin.val)
+
+    return "{}:$pse${}${}${}${}${}${}${}${}:::::\n".format(
+        filename, pbe_algo, iterations, salt_size, salt, iv_size, iv, encrypted_pin_length, encrypted_pin,
+    )
+
+
+if __name__ == "__main__":
+    options, args = parse_options()
+
+    # Select the output file to write
+    if options.output:
+        f = open(options.output, "w")
+    else:
+        f = stdout
+
+    # Parse all the files and write output
+    for i in range(0, len(args)):
+        line = parse_pse(args[i])
+        f.write(line)
+
+    # Close the file descriptor
+    if options.output:
+        f.close()
