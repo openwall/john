f85af7d4815b96efda75dae87317b45656b602d0
Dhiru Kholia <dhiru.kholia@gmail.com>
2018-08-04 10:46:16 +0530

    Cleanup and enhance axcrypt2john.py utility

--- run/axcrypt2john.py
+++ run/axcrypt2john.py
@@ -1,102 +1,137 @@
 #!/usr/bin/python
 
-# AxCrypt 1.x encrypted file parser for JtR
-# 2016 by Fist0urs <eddy.maaalou at gmail.com>.
-
+# AxCrypt 1.x encrypted file parser for JtR.
+# Written in 2016 by Fist0urs <eddy.maaalou at gmail.com>.
+#
 # This software is Copyright (c) 2016, Fist0urs <eddy.maaalou at gmail.com>,
 # and it is hereby released to the general public under the following terms:
+#
 # Redistribution and use in source and binary forms, with or without modification,
 # are permitted.
+#
+# PEP 8 fixes + Python 3.x support: Dhiru Kholia (August, 2018)
+#
+# See AxCryptVersion1AlgorithmsandFileFormat.pdf for details on the .axx file
+# format and the algorithms involved.
 
-import sys, struct
+import sys
+import struct
+import binascii
 
-# file is beginning with 16bytes constant header
-GUID='\xc0\xb9\x07\x2e\x4f\x93\xf1\x46\xa0\x15\x79\x2c\xa1\xd9\xe8\x21'
-OFFSET_TYPE=4
-SIZE_KEYDATA=24  # size of constant in keywrap (0xA6*8) + size of DEK (16)
-SIZE_SALT=16
-SIZE_ITERATION=4
+# file begins with a 16 bytes constant header
+GUID = b'\xc0\xb9\x07\x2e\x4f\x93\xf1\x46\xa0\x15\x79\x2c\xa1\xd9\xe8\x21'
+OFFSET_TYPE = 4
+SIZE_KEYDATA = 24  # size of constant in keywrap (0xA6 * 8) + size of DEK (16)
+SIZE_SALT = 16
+SIZE_ITERATION = 4
 
-StructKeys=[]
+StructKeys = []
+
+PY3 = sys.version_info[0] == 3
+
+if not PY3:
+    reload(sys)
+    sys.setdefaultencoding("utf8")
+
 
 def usage():
-	print >> sys.stderr, 'usage: %s <axxfile> [KEY-FILE]\n' % sys.argv[0]
-	print >> sys.stderr, 'Script to extract hash from AxCrypt encrypted file or self-decrypting binary\n'
-	print >> sys.stderr, 'optional arguments:\n  KEY-FILE			 path to optional key-file provided'
-	sys.exit(1)
+    sys.stderr.write('Usage: %s <axxfile> [KEY-FILE]\n\n' % sys.argv[0])
+    sys.stderr.write('Script to extract hash from AxCrypt encrypted file or self-decrypting binary\n\n')
+    sys.stderr.write('Optional arguments:\n  KEY-FILE			 path to optional key-file provided\n')
+    sys.exit(1)
+
 
 def DWORD_to_int(string_dword):
-	string_dword_reversed = string_dword[::-1]
-	return int('0x'+str(string_dword_reversed.encode('hex')), 16)
+    return struct.unpack("<I", string_dword)[0]
+
 
 def parse_PE(axxdata):
-	i = 0
-	while(axxdata[i:i+16] != GUID):
-		i += 1
-	return axxdata[i:]
+    i = 0
+
+    while axxdata[i:i+16] != GUID:
+        i += 1
+
+    return axxdata[i:]
+
 
 def parse_axxfile(axxfile):
-	stream=open(axxfile, 'rb')
-	axxdata=stream.read()
-	stream.close()
+    stream = open(axxfile, 'rb')
+    axxdata = stream.read()
+    stream.close()
 
-	# if header is 'MZ'
-	if axxdata[:2] == '\x4D\x5a':
-		offset_PE_magic = struct.unpack('<L', axxdata[60:64])[0]
-		# if 'PE' assume PE
-		if axxdata[offset_PE_magic:offset_PE_magic+2] == '\x50\x45':
-			axxdata = parse_PE(axxdata)
+    # if header is 'MZ'
+    if axxdata[:2] == b'\x4D\x5a':
+        offset_PE_magic = struct.unpack('<L', axxdata[60:64])[0]
+        # if 'PE' assume PE
+        if axxdata[offset_PE_magic:offset_PE_magic+2] == '\x50\x45':
+            axxdata = parse_PE(axxdata)
 
-	sizeof_file=len(axxdata)
+    sizeof_file = len(axxdata)
 
-	if (axxdata[:16] != GUID):
-		print "Be Careful, GUID is different from axcrypt's one..." 
+    if axxdata[:16] != GUID:
+        print("Be careful, GUID is different from AxCrypt's one...")
 
-	header_datalen_offset = 16
-	headertype = '\x02' # first type encountered
+    header_datalen_offset = 16
+    headertype = b'\x02' # first type encountered
 
-	# headertype of dataencrypted section is 0x3f
-	while(headertype != 63):
-		header_datalen = ord(axxdata[header_datalen_offset])
-		headertype = ord(axxdata[header_datalen_offset + OFFSET_TYPE])
-		
-		# probably a StructKey
-		if (header_datalen == 49 and headertype == 04):
-			offset_to_keydata = header_datalen_offset + OFFSET_TYPE + 1
-			offset_to_salt = offset_to_keydata + SIZE_KEYDATA
-			offset_to_iteration = offset_to_salt + SIZE_SALT
-			
-			dword_str = axxdata[offset_to_iteration:offset_to_iteration + SIZE_ITERATION]
-			
-			StructKeys.append({'KeyData' : axxdata[offset_to_keydata:offset_to_salt]
-						, 'Salt' : axxdata[offset_to_salt:offset_to_iteration]
-						,'Iteration' : DWORD_to_int(dword_str)})
+    # headertype of dataencrypted section is 0x3f
+    while headertype != 63:
+        if not PY3:
+            header_datalen = ord(axxdata[header_datalen_offset])
+            headertype = ord(axxdata[header_datalen_offset + OFFSET_TYPE])
+        else:
+            header_datalen = axxdata[header_datalen_offset]
+            headertype = axxdata[header_datalen_offset + OFFSET_TYPE]
 
-		header_datalen_offset += header_datalen
-		
-		if (header_datalen_offset >= sizeof_file):
-			print "Could not parse file, exiting"
-			sys.exit(0)
-	return StructKeys[0]['KeyData'],StructKeys[0]['Salt'],StructKeys[0]['Iteration']
+        # probably a StructKey
+        if (header_datalen == 49 and headertype == 4):
+            offset_to_keydata = header_datalen_offset + OFFSET_TYPE + 1
+            offset_to_salt = offset_to_keydata + SIZE_KEYDATA
+            offset_to_iteration = offset_to_salt + SIZE_SALT
 
-if __name__=="__main__":
-	if (len(sys.argv) != 2 and len(sys.argv) != 3):
-		usage()
-	
-	# A_DEK == wrappedKey
-	wrappedKey, Salt, nb_iteration = parse_axxfile(sys.argv[1])
+            dword_str = axxdata[offset_to_iteration:offset_to_iteration + SIZE_ITERATION]
 
-	version = 1
-	
-	keyfile_content = ''
-	key_file_name = ''
-	# dummy strip to relative path
-	axxfile = sys.argv[1][sys.argv[1].rfind("/")+1:]
+            StructKeys.append({'KeyData': axxdata[offset_to_keydata:offset_to_salt],
+                'salt': axxdata[offset_to_salt:offset_to_iteration]
+                ,'Iteration': DWORD_to_int(dword_str)})
 
-	if (len(sys.argv) == 3):
-		keyfile = open(sys.argv[2], 'r')
-		keyfile_content = '*' + keyfile.read().encode("hex")
-		key_file_name = '*' + sys.argv[2][sys.argv[2].rfind("/")+1:]
-		keyfile.close()
+        header_datalen_offset += header_datalen
 
-	print axxfile + key_file_name + ":$axcrypt$" + "*" + str(version) + "*" + str(nb_iteration) + "*" + Salt.encode("hex") + "*" + wrappedKey.encode("hex") + keyfile_content
+        if header_datalen_offset >= sizeof_file:
+            print("Could not parse file, exiting...")
+            sys.exit(0)
+
+    return StructKeys[0]['KeyData'], StructKeys[0]['salt'], StructKeys[0]['Iteration']
+
+
+if __name__ == "__main__":
+    if (len(sys.argv) != 2 and len(sys.argv) != 3):
+        usage()
+
+    # A_DEK == wrapped_key
+    wrapped_key, salt, nb_iteration = parse_axxfile(sys.argv[1])
+
+    version = 1
+
+    keyfile_content = ''
+    key_file_name = ''
+    # dummy strip to relative path
+    axxfile = sys.argv[1][sys.argv[1].rfind("/")+1:]
+
+    if len(sys.argv) == 3:
+        keyfile = open(sys.argv[2], 'rb')
+        data = binascii.hexlify(keyfile.read())
+        if PY3:
+            data = data.decode("ascii")
+        keyfile_content = '*' + data
+        key_file_name = '*' + sys.argv[2][sys.argv[2].rfind("/")+1:]
+        keyfile.close()
+
+    salt = binascii.hexlify(salt)
+    wrapped_key = binascii.hexlify(wrapped_key)
+
+    if PY3:
+        salt = salt.decode("ascii")
+        wrapped_key = wrapped_key.decode("ascii")
+
+    print(axxfile + key_file_name + ":$axcrypt$" + "*" + str(version) + "*" + str(nb_iteration) + "*" + salt + "*" + wrapped_key + keyfile_content)
