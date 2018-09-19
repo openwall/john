openwall/john

commit 05272574c5862258fbb63273ad0ffc1fe3e6206d

Change our vswapxx pseudo-intrinsics so they just return the result, without changing input. This sometimes allow optimizations. Neither our OpenCL macros nor the johnswap.h CPU macros modify the input.

src/pseudo_intrinsics.h
@@ -87,12 +87,13 @@ typedef union {
 inline static int vanyeq_epi32(vtype x, vtype y)
 {
 	vtype z = (vtype)vceqq_u32(x.v32, y.v32);
+
 	return vgetq_lane_u32(z.v32, 0) || vgetq_lane_u32(z.v32, 1) ||
 	       vgetq_lane_u32(z.v32, 2) || vgetq_lane_u32(z.v32, 3);
 }
 
-#define vswap32(x)              (x = (vtype)vrev32q_u8(x.v8))
-#define vswap64(x)              (x = (vtype)vrev64q_u8(x.v8))
+#define vswap32(x)              ((vtype)vrev32q_u8(x.v8))
+#define vswap64(x)              ((vtype)vrev64q_u8(x.v8))
 
 #define GATHER64(x,y,z)     { x = vset_epi64 (y[1][z], y[0][z]); }
 
@@ -139,12 +140,12 @@ typedef union {
 #define vunpacklo_epi64(x, y)   (vtype)(vtype64)vec_mergeh((vector long)(x).v64, (vector long)(y).v64)
 #define vxor(x, y)              (vtype)vec_xor((x).v32, (y).v32)
 
-#define vanyeq_epi32(x, y)     vec_any_eq((x).v32, (y).v32)
+#define vanyeq_epi32(x, y)      vec_any_eq((x).v32, (y).v32)
 
 #define vswap32                 vswap32_emu
 #define vswap64                 vswap64_emu
 
-#define GATHER64(x,y,z)     { x = vset_epi64 (y[1][z], y[0][z]); }
+#define GATHER64(x,y,z)         { x = vset_epi64 (y[1][z], y[0][z]); }
 
 /*************************** AVX512 and MIC ***************************/
 #elif __AVX512F__ || __MIC__
@@ -242,7 +243,7 @@ typedef __m512i vtype;
 #define vsrli_epi16             _mm512_srli_epi16
 
 #define vswap32(n)                                              \
-    n = vshuffle_epi8(n, vset_epi32(0x3c3d3e3f, 0x38393a3b,     \
+        vshuffle_epi8(n, vset_epi32(0x3c3d3e3f, 0x38393a3b,     \
                                     0x34353637, 0x30313233,     \
                                     0x2c2d2e2f, 0x28292a2b,     \
                                     0x24252627, 0x20212223,     \
@@ -251,7 +252,7 @@ typedef __m512i vtype;
                                     0x0c0d0e0f, 0x08090a0b,     \
                                     0x04050607, 0x00010203))
 #define vswap64(n) \
-    n = vshuffle_epi8(n, vset_epi64(0x38393a3b3c3d3e3fULL, \
+        vshuffle_epi8(n, vset_epi64(0x38393a3b3c3d3e3fULL, \
                                     0x3031323334353637ULL, \
                                     0x28292a2b2c2d2e2fULL, \
                                     0x2021222324252627ULL, \
@@ -260,9 +261,8 @@ typedef __m512i vtype;
                                     0x08090a0b0c0d0e0fULL, \
                                     0x0001020304050607ULL))
 #else /* workarounds without AVX512BW */
-#define vswap32 vswap32_emu
-#define vswap64(x) \
-	(x = vshuffle_epi32(x, _MM_SHUFFLE(2, 3, 0, 1)), vswap32(x))
+#define vswap32             vswap32_emu
+#define vswap64(x)          vswap32(vshuffle_epi32(x, _MM_SHUFFLE(2, 3, 0, 1)))
 
 #endif /* __AVX512BW__ */
 
@@ -273,6 +273,7 @@ typedef __m512i vtype;
 inline static __m512i _mm512_loadu_si512(void const *addr)
 {
 	__m512i indices = _mm512_set_epi64(7, 6, 5, 4, 3, 2, 1, 0);
+
 	return is_aligned(addr, 64) ? _mm512_load_si512(addr) :
 	                              _mm512_i64gather_epi64(indices, addr, 8);
 }
@@ -349,11 +350,8 @@ typedef __m256i vtype;
     vset_epi64(0x18191a1b1c1d1e1fULL, 0x1011121314151617ULL, \
                0x08090a0b0c0d0e0fULL, 0x0001020304050607ULL)
 
-#define vswap32(n)                              \
-    (n = vshuffle_epi8(n, swap_endian_mask))
-
-#define vswap64(n)                              \
-    (n = vshuffle_epi8(n, swap_endian64_mask))
+#define vswap32(n)              vshuffle_epi8(n, swap_endian_mask)
+#define vswap64(n)              vshuffle_epi8(n, swap_endian64_mask)
 
 #define GATHER_4x(x, y, z)                           \
 {                                                    \
@@ -473,26 +471,21 @@ typedef __m128i vtype;
 
 #define swap_endian_mask    \
     vset_epi32(0x0c0d0e0f, 0x08090a0b, 0x04050607, 0x00010203)
-#define vswap32(n)              (n = vshuffle_epi8(n, swap_endian_mask))
+#define vswap32(n)              vshuffle_epi8(n, swap_endian_mask)
 
 #define swap_endian64_mask  \
     vset_epi64(0x08090a0b0c0d0e0fULL, 0x0001020304050607ULL)
-#define vswap64(n)              (n = vshuffle_epi8(n, swap_endian64_mask))
+#define vswap64(n)              vshuffle_epi8(n, swap_endian64_mask)
 
 #else /* Just basic SSE2 */
 
 #define vswap32(n)                                  \
-    (n = vxor(                                      \
-        vsrli_epi16(                                \
+	vxor(vsrli_epi16(                               \
             vroti16_epi32(n,16), 8),                \
-            vslli_epi16(vroti16_epi32(n,16), 8)))
+            vslli_epi16(vroti16_epi32(n,16), 8))
 
-#define vswap64(n)                                              \
-    (                                                           \
-        n = vshufflehi_epi16(vshufflelo_epi16(n, 0xb1), 0xb1),  \
-        n = vxor(vslli_epi16(n, 8), vsrli_epi16(n, 8)),         \
-        n = vshuffle_epi32(n, 0xb1)                             \
-    )
+#define vswap64(n)                                  \
+	vshuffle_epi32(vshufflehi_epi16(vshufflelo_epi16(vxor(vslli_epi16(n, 8), vsrli_epi16(n, 8)), 0xb1), 0xb1), 0xb1)
 
 #endif /* __SSSE3__ */
 
@@ -563,7 +556,7 @@ typedef __m64i vtype;
 /************************* COMMON STUFF BELOW *************************/
 
 #ifdef _MSC_VER
-#define MEM_ALIGN_SIMD			16
+#define MEM_ALIGN_SIMD          16
 #define INLINE _inline
 #else
 #define MEM_ALIGN_SIMD          (SIMD_COEF_32 * 4)
@@ -598,12 +591,13 @@ static INLINE void vstoreu_emu(void *addr, vtype v)
 #endif
 
 #define vswap32_emu(x) \
-	x = vxor(vsrli_epi32(x, 24),                                            \
+	(vxor(vsrli_epi32(x, 24), \
 	         vxor(vslli_epi32(vsrli_epi32(vslli_epi32(x, 8), 24), 8),       \
 	              vxor(vsrli_epi32(vslli_epi32(vsrli_epi32(x, 8), 24), 8),  \
-                       vslli_epi32(x, 24))))
+	                   vslli_epi32(x, 24)))))
+
 #define vswap64_emu(x) \
-	(x = vxor(vsrli_epi64(x, 32), vslli_epi64(x, 32)), vswap32_emu(x))
+	(vxor(vsrli_epi64(x, 32), vslli_epi64(x, 32)), vswap32_emu(x))
 
 #if VCMOV_EMULATED
 #if VANDNOT_EMULATED /* currently never */

src/rawSHA1_ng_fmt_plug.c
@@ -457,10 +457,8 @@ static void sha1_fmt_set_key(char *key, int index)
 	// we do here.
 	//  X = 40 41 42 44 45 80 00 00 00 00 00 00 00 00 00    // What we have.
 	//  X = 44 42 41 40 00 00 80 45 00 00 00 00 00 00 00    // What we want.
-	vswap32(X);
-
 	// Store the result into the message buffer.
-	vstore(&M[index], X);
+	vstore(&M[index], vswap32(X));
 
 	return;
 }

src/rawSHA256_ng_fmt_plug.c
@@ -250,7 +250,7 @@ static int crypt_all(int *pcount, struct db_salt *salt)
 
 #if __SSE4_1__ && !__AVX2__
         for (i=0; i < 16; i++) GATHER(w[i], saved_key, i);
-        for (i=0; i < 15; i++) vswap32(w[i]);
+        for (i=0; i < 15; i++) w[i] = vswap32(w[i]);
 #else
         JTR_ALIGN(VWIDTH * 4) uint32_t __w[16][VWIDTH];
         int j;
@@ -260,10 +260,7 @@ static int crypt_all(int *pcount, struct db_salt *salt)
 		        __w[j][i] = saved_key[index + i][j];
 
         for (i=0; i < 15; i++)
-        {
-	        w[i] = vload((vtype*) __w[i]);
-	        vswap32(w[i]);
-        }
+	        w[i] = vswap32(vload((vtype*) __w[i]));
 
         w[15] = vload((vtype*) __w[15]);
 #endif

src/rawSHA512_ng_fmt_plug.c
@@ -305,14 +305,11 @@ static int crypt_all(int *pcount, struct db_salt *salt)
         for (i = 0; i < 14; i += 2) {
             GATHER(tmp1, saved_key, i);
             GATHER(tmp2, saved_key, i + 1);
-            vswap64(tmp1);
-            vswap64(tmp2);
-            w[i] = tmp1;
-            w[i + 1] = tmp2;
+            w[i] = vswap64(tmp1);
+            w[i + 1] = vswap64(tmp2);
         }
         GATHER(tmp1, saved_key, 14);
-        vswap64(tmp1);
-        w[14] = tmp1;
+        w[14] = vswap64(tmp1);
         GATHER(w[15], saved_key, 15);
         for (i = 16; i < 80; i++) R(i);
 

src/simd-intrinsics.c
@@ -181,12 +181,12 @@ void SIMDmd5body(vtype* _data, unsigned int *out,
 			}
 #if !ARCH_LITTLE_ENDIAN
 			for (i=0; i < 14; i++)
-				vswap32(W[i]);
+				W[i] = vswap32(W[i]);
 			if (((SSEi_flags & SSEi_2BUF_INPUT_FIRST_BLK) == SSEi_2BUF_INPUT_FIRST_BLK) ||
 			    ((SSEi_flags & SSEi_4BUF_INPUT_FIRST_BLK) == SSEi_4BUF_INPUT_FIRST_BLK) ||
 			    (SSEi_flags & SSEi_FLAT_RELOAD_SWAPLAST)) {
-				vswap32(W[14]);
-				vswap32(W[15]);
+				W[14] = vswap32(W[14]);
+				W[15] = vswap32(W[15]);
 			}
 #endif
 			W += 16;
@@ -866,12 +866,12 @@ void SIMDmd4body(vtype* _data, unsigned int *out, uint32_t *reload_state,
 			}
 #if !ARCH_LITTLE_ENDIAN
 			for (i=0; i < 14; i++)
-				vswap32(W[i]);
+				W[i] = vswap32(W[i]);
 			if (((SSEi_flags & SSEi_2BUF_INPUT_FIRST_BLK) == SSEi_2BUF_INPUT_FIRST_BLK) ||
 			    ((SSEi_flags & SSEi_4BUF_INPUT_FIRST_BLK) == SSEi_4BUF_INPUT_FIRST_BLK) ||
 			    (SSEi_flags & SSEi_FLAT_RELOAD_SWAPLAST)) {
-				vswap32(W[14]);
-				vswap32(W[15]);
+				W[14] = vswap32(W[14]);
+				W[15] = vswap32(W[15]);
 			}
 #endif
 			W += 16;
@@ -1281,7 +1281,7 @@ void SIMDSHA1body(vtype* _data, uint32_t *out, uint32_t *reload_state,
 			if (SSEi_flags & SSEi_4BUF_INPUT) {
 				for (i=0; i < 14; ++i) {
 					GATHER_4x(W[i], saved_key, i);
-					vswap32(W[i]);
+					W[i] = vswap32(W[i]);
 				}
 				GATHER_4x(W[14], saved_key, 14);
 				GATHER_4x(W[15], saved_key, 15);
@@ -1289,7 +1289,7 @@ void SIMDSHA1body(vtype* _data, uint32_t *out, uint32_t *reload_state,
 			} else if (SSEi_flags & SSEi_2BUF_INPUT) {
 				for (i=0; i < 14; ++i) {
 					GATHER_2x(W[i], saved_key, i);
-					vswap32(W[i]);
+					W[i] = vswap32(W[i]);
 				}
 				GATHER_2x(W[14], saved_key, 14);
 				GATHER_2x(W[15], saved_key, 15);
@@ -1297,7 +1297,7 @@ void SIMDSHA1body(vtype* _data, uint32_t *out, uint32_t *reload_state,
 			} else {
 				for (i=0; i < 14; ++i) {
 					GATHER(W[i], saved_key, i);
-					vswap32(W[i]);
+					W[i] = vswap32(W[i]);
 				}
 				GATHER(W[14], saved_key, 14);
 				GATHER(W[15], saved_key, 15);
@@ -1306,8 +1306,8 @@ void SIMDSHA1body(vtype* _data, uint32_t *out, uint32_t *reload_state,
 			if (((SSEi_flags & SSEi_2BUF_INPUT_FIRST_BLK) == SSEi_2BUF_INPUT_FIRST_BLK) ||
 			    ((SSEi_flags & SSEi_4BUF_INPUT_FIRST_BLK) == SSEi_4BUF_INPUT_FIRST_BLK) ||
 			    (SSEi_flags & SSEi_FLAT_RELOAD_SWAPLAST)) {
-				vswap32(W[14]);
-				vswap32(W[15]);
+				W[14] = vswap32(W[14]);
+				W[15] = vswap32(W[15]);
 			}
 			W += 16;
 		}
@@ -1336,12 +1336,12 @@ void SIMDSHA1body(vtype* _data, uint32_t *out, uint32_t *reload_state,
 			}
 #if ARCH_LITTLE_ENDIAN
 			for (i=0; i < 14; i++)
-				vswap32(W[i]);
+				W[i] = vswap32(W[i]);
 			if (((SSEi_flags & SSEi_2BUF_INPUT_FIRST_BLK) == SSEi_2BUF_INPUT_FIRST_BLK) ||
 			    ((SSEi_flags & SSEi_4BUF_INPUT_FIRST_BLK) == SSEi_4BUF_INPUT_FIRST_BLK) ||
 			    (SSEi_flags & SSEi_FLAT_RELOAD_SWAPLAST)) {
-				vswap32(W[14]);
-				vswap32(W[15]);
+				W[14] = vswap32(W[14]);
+				W[15] = vswap32(W[15]);
 			}
 #endif
 			W += 16;
@@ -1826,7 +1826,7 @@ void SIMDSHA256body(vtype *data, uint32_t *out, uint32_t *reload_state, unsigned
 			if (SSEi_flags & SSEi_4BUF_INPUT) {
 				for (i=0; i < 14; ++i) {
 					GATHER_4x(w[i], saved_key, i);
-					vswap32(w[i]);
+					w[i] = vswap32(w[i]);
 				}
 				GATHER_4x(w[14], saved_key, 14);
 				GATHER_4x(w[15], saved_key, 15);
@@ -1834,7 +1834,7 @@ void SIMDSHA256body(vtype *data, uint32_t *out, uint32_t *reload_state, unsigned
 			} else if (SSEi_flags & SSEi_2BUF_INPUT) {
 				for (i=0; i < 14; ++i) {
 					GATHER_2x(w[i], saved_key, i);
-					vswap32(w[i]);
+					w[i] = vswap32(w[i]);
 				}
 				GATHER_2x(w[14], saved_key, 14);
 				GATHER_2x(w[15], saved_key, 15);
@@ -1842,7 +1842,7 @@ void SIMDSHA256body(vtype *data, uint32_t *out, uint32_t *reload_state, unsigned
 			} else {
 				for (i=0; i < 14; ++i) {
 					GATHER(w[i], saved_key, i);
-					vswap32(w[i]);
+					w[i] = vswap32(w[i]);
 				}
 				GATHER(w[14], saved_key, 14);
 				GATHER(w[15], saved_key, 15);
@@ -1851,8 +1851,8 @@ void SIMDSHA256body(vtype *data, uint32_t *out, uint32_t *reload_state, unsigned
 			if (((SSEi_flags & SSEi_2BUF_INPUT_FIRST_BLK) == SSEi_2BUF_INPUT_FIRST_BLK) ||
 			    ((SSEi_flags & SSEi_4BUF_INPUT_FIRST_BLK) == SSEi_4BUF_INPUT_FIRST_BLK) ||
 			    (SSEi_flags & SSEi_FLAT_RELOAD_SWAPLAST)) {
-				vswap32(w[14]);
-				vswap32(w[15]);
+				w[14] = vswap32(w[14]);
+				w[15] = vswap32(w[15]);
 			}
 		}
 #else
@@ -1880,12 +1880,12 @@ void SIMDSHA256body(vtype *data, uint32_t *out, uint32_t *reload_state, unsigned
 			}
 #if ARCH_LITTLE_ENDIAN
 			for (i=0; i < 14; i++)
-				vswap32(w[i]);
+				w[i] = vswap32(w[i]);
 			if (((SSEi_flags & SSEi_2BUF_INPUT_FIRST_BLK) == SSEi_2BUF_INPUT_FIRST_BLK) ||
 			    ((SSEi_flags & SSEi_4BUF_INPUT_FIRST_BLK) == SSEi_4BUF_INPUT_FIRST_BLK) ||
 			    (SSEi_flags & SSEi_FLAT_RELOAD_SWAPLAST)) {
-				vswap32(w[14]);
-				vswap32(w[15]);
+				w[14] = vswap32(w[14]);
+				w[15] = vswap32(w[15]);
 			}
 #endif
 		}
@@ -2418,11 +2418,12 @@ void SIMDSHA512body(vtype* data, uint64_t *out, uint64_t *reload_state,
 					GATHER64(tmp1[k], saved_key, i);
 					GATHER64(tmp2[k], saved_key, i + 1);
 #if ARCH_LITTLE_ENDIAN
-					vswap64(tmp1[k]);
-					vswap64(tmp2[k]);
-#endif
+					w[k][i] = vswap64(tmp1[k]);
+					w[k][i + 1] = vswap64(tmp2[k]);
+#else
 					w[k][i] = tmp1[k];
 					w[k][i + 1] = tmp2[k];
+#endif
 				}
 				GATHER64(tmp1[k], saved_key, 14);
 				GATHER64(tmp2[k], saved_key, 15);
@@ -2433,11 +2434,12 @@ void SIMDSHA512body(vtype* data, uint64_t *out, uint64_t *reload_state,
 					GATHER64(tmp1[k], saved_key, i);
 					GATHER64(tmp2[k], saved_key, i + 1);
 #if ARCH_LITTLE_ENDIAN
-					vswap64(tmp1[k]);
-					vswap64(tmp2[k]);
-#endif
+					w[k][i] = vswap64(tmp1[k]);
+					w[k][i + 1] = vswap64(tmp2[k]);
+#else
 					w[k][i] = tmp1[k];
 					w[k][i + 1] = tmp2[k];
+#endif
 				}
 				GATHER64(tmp1[k], saved_key, 14);
 				GATHER64(tmp2[k], saved_key, 15);
@@ -2446,8 +2448,8 @@ void SIMDSHA512body(vtype* data, uint64_t *out, uint64_t *reload_state,
 #if ARCH_LITTLE_ENDIAN
 			if (((SSEi_flags & SSEi_2BUF_INPUT_FIRST_BLK) == SSEi_2BUF_INPUT_FIRST_BLK) ||
 			    (SSEi_flags & SSEi_FLAT_RELOAD_SWAPLAST)) {
-				vswap64(tmp1[k]);
-				vswap64(tmp2[k]);
+				tmp1[k] = vswap64(tmp1[k]);
+				tmp2[k] = vswap64(tmp2[k]);
 			}
 #endif
 			w[k][14] = tmp1[k];

commit 3413f9af2c76cdaf0ff6f49655709415298e9552

05272574c broke AMD NEON, this fixes it (and more).

src/pseudo_intrinsics.h
@@ -86,14 +86,14 @@ typedef union {
 
 inline static int vanyeq_epi32(vtype x, vtype y)
 {
-	vtype z = (vtype)vceqq_u32(x.v32, y.v32);
+	vtype z = (vtype)vceqq_u32((x).v32, (y).v32);
 
-	return vgetq_lane_u32(z.v32, 0) || vgetq_lane_u32(z.v32, 1) ||
-	       vgetq_lane_u32(z.v32, 2) || vgetq_lane_u32(z.v32, 3);
+	return vgetq_lane_u32((z).v32, 0) || vgetq_lane_u32((z).v32, 1) ||
+	       vgetq_lane_u32((z).v32, 2) || vgetq_lane_u32((z).v32, 3);
 }
 
-#define vswap32(x)              ((vtype)vrev32q_u8(x.v8))
-#define vswap64(x)              ((vtype)vrev64q_u8(x.v8))
+#define vswap32(x)              ((vtype)vrev32q_u8((x).v8))
+#define vswap64(x)              ((vtype)vrev64q_u8((x).v8))
 
 #define GATHER64(x,y,z)         { x = vset_epi64(y[1][z], y[0][z]); }
 