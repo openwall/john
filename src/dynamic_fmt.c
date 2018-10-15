d26cc086ebeb7fc2da4e79e12a0ab5f742151367
magnum <john.magnum@hushmail.com>
2018-10-15 09:04:23 +0200

    Dynamic: Change an erratic unsigned long to uintptr_t. See #3122.

--- src/dynamic_fmt.c
+++ src/dynamic_fmt.c
@@ -2455,16 +2455,17 @@ static void *get_salt(char *ciphertext)
  *********************************************************************************/
 static int salt_hash(void *salt)
 {
-	unsigned long H;
+	uintptr_t H;
+
 	if (!salt) return 0;
 	if ( (curdat.pSetup->flags&MGF_SALTED) == 0)
 		return 0;
 
 	// salt is now a pointer, but WORD aligned.  We remove that word alingment, and simply use the next bits
 #if ARCH_ALLOWS_UNALIGNED
-	H = *((unsigned long*)salt);
+	H = *((uintptr_t*)salt);
 #else
-	memcpy(&H, salt, 8);
+	memcpy(&H, salt, sizeof(H));
 #endif
 
 	// Mix up the pointer value (H^(H>>9)) so that if we have a fixed sized allocation
