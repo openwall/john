aaae6f577f025732da7a9c1bea343547c3ef9f6c
Claudio André <claudioandre.br@gmail.com>
2018-12-09 14:38:00 -0200

    OpenCL: rawSHA256 and rawSHA512 set GWS to LWS multiple
    For less-than-full batches set GWS to next LWS multiple larger than count.
    
    This reverts commit 97ed253cac3ea83fb506eb15676e8588e1a76e9f. See #3489.

--- src/opencl_rawsha256_fmt_plug.c
+++ src/opencl_rawsha256_fmt_plug.c
@@ -607,9 +607,10 @@ static int crypt_all(int *pcount, struct db_salt *_salt)
 {
 	const int count = *pcount;
 	const struct db_salt *salt = _salt;
-	size_t gws = count;
-	size_t *lws = (local_work_size && !(gws % local_work_size)) ?
-		&local_work_size : NULL;
+	size_t gws, initial = 128;
+	size_t *lws = local_work_size ? &local_work_size : &initial;
+
+	gws = GET_MULTIPLE_OR_BIGGER(count, local_work_size);
 
 	//Check if any password was cracked and reload (if necessary)
 	if (num_loaded_hashes != salt->count)
