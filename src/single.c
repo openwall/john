openwall/john

commit 5be3ed49083f8c609d4e2858696fe77fa597ffa1

Stacked rules. New option --rules-stack=(SECTION[,..]|:rule[;..]) adds a second pass of rules, applied after normal processing.
Example: --single --rules-stack=ShiftToggle will run single mode's
normal rules fully through the first (no-op) rule in the toggle ruleset.
Only then will the latter set move on to its second rule, the main rules
are rewinded, and so on.

In all other cases, eg. --wordlist --rules --rules-stack=ShiftToggle,
the stacked rules (ShiftToggle in this case) will do a full pass before
the base rule moves to its second rule.

This option also works fine with ALL modes that normally don't support
rules at all, eg. "--mask=password?d?d?d --rules-stack=shifttoggle"
although that will disable any GPU-side mask accleration.

You can even use --wordlist --rules-stack=SomeRule without using any
base rule. The difference is in how it iterates: Normal --rules will
run all words through the first rule before moving the second rule,
while --rules-stack will apply all rules to the first word before
moving on to the second word from the list.

Closes #745, closes #2122. See #998, #2479, #3467, #3468, #3490.

src/single.c
@@ -46,15 +46,15 @@ static struct rpp_context *rule_ctx;
 static int words_pair_max;
 static int retest_guessed;
 static int orig_max_len, orig_min_kpc;
-#if HAVE_OPENCL
-static int ocl_fmt;
-#endif
+static int stacked_rule_count = 1;
+static rule_stack single_rule_stack;
+
 #if HAVE_OPENCL || HAVE_ZTEX
 static int acc_fmt, prio_resume;
-#endif
-
-extern int rpp_real_run; /* set to 1 when we really get into single mode */
-
+#if HAVE_OPENCL
+static int ocl_fmt;
+#endif /* HAVE_OPENCL */
+#endif /* HAVE_OPENCL || HAVE_ZTEX */
 
 static void save_state(FILE *file)
 {
@@ -79,10 +79,14 @@ static int restore_state(FILE *file)
 
 static double get_progress(void)
 {
+	uint64_t tot_rules = rule_count * stacked_rule_count;
+	uint64_t tot_rule_number =
+		(rules_stacked_number - 1) * rule_count + rule_number;
+
 	emms();
 
 	return progress ? progress :
-		(double)rule_number / (rule_count + 1) * 100.0;
+		(double)tot_rule_number / (tot_rules + 1) * 100.0;
 }
 
 static uint64_t calc_buf_size(int length, int min_kpc)
@@ -111,6 +115,8 @@ static void single_alloc_keys(struct db_keys **keys)
 	(*keys)->ptr = (*keys)->buffer;
 	(*keys)->have_words = 1; /* assume yes; we'll see for real later */
 	(*keys)->rule = rule_number;
+	//if (rules_stacked_after)
+	//	(*keys)->rule2 = rules_stacked_number;
 	(*keys)->lock = 0;
 	memset((*keys)->hash, -1, hash_size);
 }
@@ -147,10 +153,14 @@ static void single_init(void)
 	log_event("Proceeding with \"single crack\" mode");
 
 	if ((options.flags & FLG_BATCH_CHK || rec_restored) && john_main_process) {
-		fprintf(stderr, "Proceeding with single, rules:%s",
-		        options.activesinglerules);
+		fprintf(stderr, "Proceeding with single, rules:");
+		if (options.rule_stack)
+			fprintf(stderr, "(%s x %s)",
+			        options.activewordlistrules, options.rule_stack);
+		else
+			fprintf(stderr, "%s", options.activewordlistrules);
 		if (options.req_minlength >= 0 || options.req_maxlength)
-			fprintf(stderr, ", lengths %d-%d", options.eff_minlength,
+			fprintf(stderr, ", lengths:%d-%d", options.eff_minlength,
 			        options.eff_maxlength);
 		fprintf(stderr, "\n");
 	}
@@ -366,7 +376,12 @@ static void single_init(void)
 	rec_rule = rule_number = 0;
 	rule_count = rules_count(rule_ctx, 0);
 
-	log_event("- %d preprocessed word mangling rules", rule_count);
+	if (rules_stacked_after)
+		log_event("- Total %u (%d x %u) preprocessed word mangling rules",
+		          rule_count * stacked_rule_count,
+		          rule_count, stacked_rule_count);
+	else
+		log_event("- %d preprocessed word mangling rules", rule_count);
 
 	status_init(get_progress, 0);
 
@@ -391,6 +406,14 @@ static void single_init(void)
 	single_alloc_keys(&guessed_keys);
 
 	crk_init(single_db, NULL, guessed_keys);
+
+	stacked_rule_count = rules_init_stack(options.rule_stack,
+	                                      &single_rule_stack, single_db);
+
+	rules_stacked_after = (stacked_rule_count > 0);
+
+	if (!stacked_rule_count)
+		stacked_rule_count = 1;
 }
 
 static MAYBE_INLINE int single_key_hash(char *key)
@@ -441,6 +464,10 @@ static int single_add_key(struct db_salt *salt, char *key, int is_from_guesses)
 	struct db_keys *keys = salt->keys;
 	int index, new_hash, reuse_hash;
 	struct db_keys_hash_entry *entry;
+
+	if (rules_stacked_after)
+		if (!(key = rules_process_stack(key, &single_rule_stack)))
+			return 0;
 
 /* Check if this is a known duplicate, and reject it if so */
 	if ((index = keys->hash->hash[new_hash = single_key_hash(key)]) >= 0)
@@ -534,8 +561,10 @@ static int single_process_buffer(struct db_salt *salt)
 
 	keys = salt->keys;
 	keys->lock--;
-	if (!keys->count && !keys->lock)
+	if (!keys->count && !keys->lock) {
 		keys->rule = rule_number;
+		//keys->rule2 = rules_stacked_number;
+	}
 
 	return 0;
 }
@@ -658,8 +687,10 @@ next:
 		if (single_process_buffer(salt))
 			return 1;
 
-	if (!keys->count)
+	if (!keys->count) {
 		keys->rule = rule_number;
+		//keys->rule2 = rules_stacked_number;
+	}
 
 	if (!have_words) {
 		keys->have_words = 0;
@@ -680,70 +711,92 @@ static void single_run(void)
 
 	saved_min = rec_rule;
 	rpp_real_run = 1;
-	while ((prerule = rpp_next(rule_ctx))) {
-		if (options.node_count) {
-			int for_node = rule_number % options.node_count + 1;
-			if (for_node < options.node_min ||
-			    for_node > options.node_max) {
+
+	do {
+		while ((prerule = rpp_next(rule_ctx))) {
+			if (options.node_count) {
+				int for_node = rule_number % options.node_count + 1;
+				if (for_node < options.node_min ||
+				    for_node > options.node_max) {
+					rule_number++;
+					continue;
+				}
+			}
+
+			if (!(rule = rules_reject(prerule, 0, NULL, single_db))) {
 				rule_number++;
+				if (options.verbosity >= VERB_DEFAULT &&
+				    !rules_mute &&
+				    strncmp(prerule, "!!", 2))
+					log_event("- Rule #%d: '%.100s' rejected",
+					          rule_number, prerule);
 				continue;
+			}
+
+			if (!rules_mute) {
+				if (strcmp(prerule, rule)) {
+					log_event("- Rule #%d: '%.100s' accepted as '%.100s')",
+					          rule_number + 1, prerule, rule);
+				} else {
+					log_event("- Rule #%d: '%.100s' accepted",
+					          rule_number + 1, prerule);
+				}
+			}
+
+			if (saved_min != rec_rule) {
+				if (!rules_mute)
+					log_event("- Oldest still in use is now rule #%d",
+					          rec_rule + 1);
+				saved_min = rec_rule;
+			}
+
+			have_words = 0;
+
+			min = rule_number;
+
+			/* pot reload might have removed the salt */
+			if (!(salt = single_db->salts))
+				return;
+			do {
+				if (!salt->list)
+					continue;
+				if (single_process_salt(salt, rule))
+					return;
+				if (!salt->keys->have_words)
+					continue;
+				have_words = 1;
+				if (salt->keys->rule < min)
+					min = salt->keys->rule;
+			} while ((salt = salt->next));
+
+			if (event_reload && single_db->salts)
+				crk_reload_pot();
+
+			rec_rule = min;
+			rule_number++;
+
+			if (have_words)
+				continue;
+
+			log_event("- No information to base%s candidate passwords on",
+			          rule_number > 1 ? " further" : "");
+			return;
+		}
+
+		if (rules_stacked_after) {
+			rule_number = 0;
+			rpp_init(rule_ctx, options.activesinglerules);
+			if (!rules_mute && options.verbosity <= VERB_DEFAULT) {
+				rules_mute = 1;
+				if (john_main_process) {
+					log_event(
+"- Some rule logging suppressed. Re-enable with --verbosity=%d or greater",
+					          VERB_LEGACY);
+				}
 			}
 		}
 
-		if (!(rule = rules_reject(prerule, 0, NULL, single_db))) {
-			rule_number++;
-			if (options.verbosity >= VERB_DEFAULT && strncmp(prerule, "!!", 2))
-				log_event("- Rule #%d: '%.100s' rejected",
-				          rule_number, prerule);
-			continue;
-		}
-
-		if (strcmp(prerule, rule)) {
-			log_event("- Rule #%d: '%.100s' accepted as '%.100s'",
-				rule_number + 1, prerule, rule);
-		} else {
-			log_event("- Rule #%d: '%.100s' accepted",
-				rule_number + 1, prerule);
-		}
-
-		if (saved_min != rec_rule) {
-			log_event("- Oldest still in use is now rule #%d",
-				rec_rule + 1);
-			saved_min = rec_rule;
-		}
-
-		have_words = 0;
-
-		min = rule_number;
-
-		/* pot reload might have removed the salt */
-		if (!(salt = single_db->salts))
-			return;
-		do {
-			if (!salt->list)
-				continue;
-			if (single_process_salt(salt, rule))
-				return;
-			if (!salt->keys->have_words)
-				continue;
-			have_words = 1;
-			if (salt->keys->rule < min)
-				min = salt->keys->rule;
-		} while ((salt = salt->next));
-
-		if (event_reload && single_db->salts)
-			crk_reload_pot();
-
-		rec_rule = min;
-		rule_number++;
-
-		if (have_words)
-			continue;
-
-		log_event("- No information to base%s candidate passwords on",
-			rule_number > 1 ? " further" : "");
-		return;
-	}
+	} while (rules_stacked_after && rules_advance_stack(&single_rule_stack));
 }
 
 static void single_done(void)

.editorconfig
@@ -42,6 +42,9 @@ indent_size = 4
 [loader.c]
 indent_size = 4
 
+[rules.c]
+indent_size = 4
+
 [single.c]
 indent_size = 4
 
@@ -215,7 +218,7 @@ indent_size = 8
 [rpp.[ch]]
 indent_size = 8
 
-[rules.[ch]]
+[rules.h]
 indent_size = 8
 
 [sboxes-s.[ch]]

doc/OPTIONS
@@ -72,8 +72,7 @@ attacked.  Note that by default, some rules are applied too (see john.conf
 Input data in a character encoding other than the default.  See also
 doc/ENCODINGS.  --list=encodings gives a list of supported encodings.
 
---rules[=(SECTION[,SECTION2,....,SECTIONn]|:rule)]  enable word mangling rules
-   (available in wordlist and prince modes)
+--rules[=(SECTION[,..]|:rule[;..])]
 
 Enables word mangling rules that are read from the specified section, which
 defaults to [List.Rules:Wordlist] if not given.
@@ -90,6 +89,28 @@ a rule, then it must be preceded by a \ character, to escape it.
 So --rules::'$\;$[0-9]' is just one rule, while --rules=:'$\;$[0-9];Az"tst"'
 is two rules, and --rules=:'$;$[0-9]' would 'try' to generate 2 rules, BUT
 the first will be invalid since it is just the $ character.
+
+--rules-stack=(SECTION[,..]|:rule[;..])
+
+Stacked rules. Adds a second pass of rules, applied after normal processing.
+Example: "--single --rules-stack=ShiftToggle" will run single mode's normal
+rules fully through the first (no-op) rule in the toggle ruleset.  Only then
+will the latter set move on to its second rule, the main rules are rewinded,
+and so on.  In other modes, eg. "--wordlist --rules --rules-stack=best64",
+the stacked rules (best64 in this case) will do a full pass before the
+base rule moves to its second rule.  This option also works fine with ALL
+modes that normally don't support rules at all, eg. "--mask=password?d?d?d
+--rules-stack=KoreLogic" (although that will disable any GPU-side mask
+accleration).  You can even use --wordlist --rules-stack=SomeRule without
+using any base rule.  The difference is in how it iterates: Normal --rules
+will run all words through the first rule before moving the second rule,
+while --rules-stack will apply all rules to the first word before moving on
+to the second word from the list.  Please note that when using (rule x rule)
+you very quickly end up with huge numbers of rules:  Running the "all"
+ruleset twice as "--rules=all --rules-stack=all" will produce a whopping
+53 trillion rules.  This feature is best used with very carefully picked
+rulesets that are made to work together.
+
 
 --incremental[=MODE]		"incremental" mode [using section MODE]
 

src/cracker.c
@@ -65,6 +65,7 @@
 #if HAVE_LIBDL && defined(HAVE_OPENCL)
 #include "gpu_common.h"
 #endif
+#include "rules.h"
 #include "memdbg.h"
 
 #ifdef index
@@ -84,14 +85,23 @@ static unsigned int crk_prefetch;
 #endif
 static int crk_key_index, crk_last_key;
 static void *crk_last_salt;
-void (*crk_fix_state)(void);
 static struct db_keys *crk_guesses;
 static uint64_t *crk_timestamps;
 static char crk_stdout_key[PLAINTEXT_BUFFER_SIZE];
-int64_t crk_pot_pos;
 static int kpc_warn, kpc_warn_limit, single_running;
 
-/* expose max_keys_per_crypt to the world (needed in recovery.c) */
+int crk_stacked_rule_count = 1;
+rule_stack crk_rule_stack;
+
+int64_t crk_pot_pos;
+
+void (*crk_fix_state)(void);
+int (*crk_process_key)(char *key);
+
+static int process_key(char *key);
+static int process_key_stack_rules(char *key);
+
+/* Expose max_keys_per_crypt to the world (needed in recovery.c) */
 int crk_max_keys_per_crypt(void)
 {
 	return  crk_params->max_keys_per_crypt;
@@ -225,6 +235,20 @@ void crk_init(struct db_main *db, void (*fix_state)(void),
 	if ((kpc_warn_limit =
 	     cfg_get_int(SECTION_OPTIONS, NULL, "MaxKPCWarnings")) == -1)
 		kpc_warn_limit = CRK_KPC_WARN;
+
+	if (!(options.flags & FLG_SINGLE_CHK))
+		crk_stacked_rule_count =
+			rules_init_stack(options.rule_stack, &crk_rule_stack, db);
+	else
+		crk_stacked_rule_count = 0;
+
+	if (!(rules_stacked_after = (crk_stacked_rule_count > 0)))
+		crk_stacked_rule_count = 1;
+
+	if (rules_stacked_after)
+		crk_process_key = process_key_stack_rules;
+	else
+		crk_process_key = process_key;
 }
 
 /*
@@ -1051,7 +1075,7 @@ int crk_process_buffer(void)
 	return ext_abort;
 }
 
-int crk_process_key(char *key)
+static int process_key(char *key)
 {
 	if (crk_db->loaded) {
 		int max_keys = crk_params->max_keys_per_crypt;
@@ -1107,6 +1131,18 @@ int crk_process_key(char *key)
 	}
 
 	return ext_abort;
+}
+
+static int process_key_stack_rules(char *key)
+{
+	int ret = 0;
+	char *word;
+
+	while ((word = rules_process_stack_all(key, &crk_rule_stack)))
+		if ((ret = process_key(word)))
+			break;
+
+	return ret;
 }
 
 /* This function is used by single.c only */

src/cracker.h
@@ -11,7 +11,9 @@
 #define _JOHN_CRACKER_H
 
 #include <stdint.h>
+
 #include "loader.h"
+#include "rules.h"
 
 /* Our last read position in pot file (during crack) */
 extern int64_t crk_pot_pos;
@@ -27,11 +29,18 @@ extern void crk_init(struct db_main *db, void (*fix_state)(void),
 	struct db_keys *guesses);
 
 /*
+ * How many stacked rules will be run, or 1 for none.
+ *
+ * Note: To tell IF stack rules will be run, use rules_stacked_after flag.
+ */
+extern int crk_stacked_rule_count;
+
+/*
  * Tries the key against all passwords in the database (should not be empty).
  * The return value is non-zero if aborted or everything got cracked (the
  * event_abort flag can be used to find out which of these has happened).
  */
-extern int crk_process_key(char *key);
+extern int (*crk_process_key)(char *key);
 
 /*
  * Process all/any keys already loaded with crk_process_key, regardless of

src/external.c
@@ -474,8 +474,10 @@ void do_external_crack(struct db_main *db)
 
 	if (rec_restored && john_main_process) {
 		fprintf(stderr, "Proceeding with external:%s", ext_mode);
+		if (options.rule_stack)
+			fprintf(stderr, ", rules-stack:%s", options.rule_stack);
 		if (options.req_minlength >= 0 || options.req_maxlength)
-			fprintf(stderr, ", lengths %d-%d",
+			fprintf(stderr, ", lengths:%d-%d",
 			        options.eff_minlength, options.eff_maxlength);
 		fprintf(stderr, "\n");
 	}

src/inc.c
@@ -38,14 +38,17 @@ static double cand;
 
 static double get_progress(void)
 {
-	unsigned long long mask_mult = mask_tot_cand ? mask_tot_cand : 1;
+	uint64_t mask_mult = mask_tot_cand ? mask_tot_cand : 1;
+	uint64_t factors = crk_stacked_rule_count * mask_mult;
+	uint64_t keyspace = cand * factors;
+	uint64_t pos = status.cands;
 
 	emms();
 
 	if (!cand)
 		return -1;
 
-	return 100.0 * status.cands / (cand * mask_mult);
+	return 100.0 * pos / keyspace;
 }
 
 typedef char (*char2_table)
@@ -493,9 +496,11 @@ void do_incremental_crack(struct db_main *db, char *mode)
 	if ((options.flags & FLG_BATCH_CHK || rec_restored) && john_main_process) {
 		fprintf(stderr, "Proceeding with incremental:%s", mode);
 		if (options.mask)
-			fprintf(stderr, ", mask:%s", options.mask);
+			fprintf(stderr, ", hybrid mask:%s", options.mask);
+		if (options.rule_stack)
+			fprintf(stderr, ", rules-stack:%s", options.rule_stack);
 		if (options.req_minlength >= 0 || options.req_maxlength)
-			fprintf(stderr, ", lengths %d-%d", options.eff_minlength,
+			fprintf(stderr, ", lengths:%d-%d", options.eff_minlength,
 			        options.eff_maxlength);
 		fprintf(stderr, "\n");
 	}

src/loader.h
@@ -97,6 +97,9 @@ struct db_keys {
 /* Number of last processed rule */
 	int rule;
 
+/* Number of last processed stacked rule */
+	//int rule2;
+
 /* Number of recursive calls for this salt */
 	int lock;
 

commit 8c39fa88264bd603d429c54097ce5aaa9e91be2a

single.c: Fix a hard-to-see typo introduced in 5be3ed4908

src/single.c
@@ -755,7 +755,7 @@ static void single_run(void)
 
 			if (!rules_mute) {
 				if (strcmp(prerule, rule)) {
-					log_event("- Rule #%d: '%.100s' accepted as '%.100s')",
+					log_event("- Rule #%d: '%.100s' accepted as '%.100s'",
 					          rule_number + 1, prerule, rule);
 				} else {
 					log_event("- Rule #%d: '%.100s' accepted",