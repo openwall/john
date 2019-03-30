9c437ee09872b29d4bb62845568cc7fb5d75bdd2
Claudio André <claudioandre.br@gmail.com>
2019-03-30 11:38:37 -0300

    mask: free resources only after use
    Release the resources used by '--mask' when they are no longer needed.

--- src/john.c
+++ src/john.c
@@ -1878,10 +1878,10 @@ static void john_run(void)
 		if (options.flags & FLG_BATCH_CHK)
 			do_batch_crack(&database);
 
+		status_print();
+
 		if (options.flags & FLG_MASK_CHK)
 			mask_done();
-
-		status_print();
 
 #if OS_FORK
 		if (options.fork && john_main_process)
