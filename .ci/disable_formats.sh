615ec7611210871ffe359cd3e5d0082932eebc84
Claudio André <claudioandre.br@gmail.com>
2019-04-16 09:31:26 -0300

    CI: refactor and small tweaks
    - the full test is now a manual test;
    - document problematic formats.

---
+++ .ci/disable_formats.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -e
+
+# There was a bug in echo -e in Travis
+# TODO: we know these formats must be fixed (or removed)
+echo '[Local:Disabled:Formats]' > john-local.conf
+echo 'Raw-SHA512-free-opencl = Y' >> john-local.conf
+echo 'XSHA512-free-opencl = Y' >> john-local.conf
+echo 'gpg-opencl = Y' >> john-local.conf
+echo 'KeePass-opencl = Y' >> john-local.conf
+echo 'scrypt = Y' >> john-local.conf
+echo 'django-scrypt = Y' >> john-local.conf
+echo 'multibit = Y' >> john-local.conf
+
+# These formats fails OpenCL CPU runtime
+echo 'lotus5-opencl = Y' >> john-local.conf
+echo 'pgpdisk-opencl = Y' >> john-local.conf
