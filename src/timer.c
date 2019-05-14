838d1ed7688a662708f308b02e54004cc67e8aeb
magnum <john.magnum@hushmail.com>
2019-05-14 14:40:46 +0200

    Improve our "portable HR timer" resolution to 1 ns under Linux/POSIC and under macOS (two different code paths).

--- src/timer.c
+++ src/timer.c
@@ -1,27 +1,26 @@
 /*
- * This software was written by Jim Fougeron jfoug AT cox dot net
- * in 2009. No copyright is claimed, and the software is hereby
- * placed in the public domain. In case this attempt to disclaim
- * copyright and place the software in the public domain is deemed
- * null and void, then the software is Copyright (c) 2009 Jim Fougeron
- * and it is hereby released to the general public under the following
- * terms:
+ * This file is
+ * Copyright (c) 2009 by Jim Fougeron jfoug AT cox dot net
+ * Copyright (c) 2019 by magnum
  *
- * This software may be modified, redistributed, and used for any
- * purpose, in source and binary forms, with or without modification.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted.
  *
- * Portable hi-res timer.  Was a nice C++ class.
- * Downgraded to C for project john
+ * Portable hi-res timer.  Was a nice C++ class. Downgraded to C for john.
  */
 
 #include <stdio.h>
 
 #include "timer.h"
 
-double sm_HRTicksPerSec = 0.0;  // HR Ticks per second
-int sm_fGotHRTicksPerSec = 0;   // Set if we have got the above
-double sm_hrPrecision = 0.0;
-double sm_cPrecision = 0.0;
+#if __MACH__
+static double sm_timebase;
+#endif
+static int sm_fGotHRTicksPerSec = 0;   // Set if we have got the above
+
+double sm_HRTicksPerSec;  // HR ticks per second (claimed)
+double sm_hrPrecision;    // HR ticks per second (observed, guess)
+double sm_cPrecision;     // clocks (ticks) per second (observed, guess)
 
 void sTimer_Init(sTimer *t)
 {
@@ -36,17 +35,18 @@ void sTimer_Init(sTimer *t)
 		// That's a fair indication what the precision is likely to be.
 		// Note - this isn't actually used
 		int i;
+
 		sm_fGotHRTicksPerSec = 1;
 		sm_cPrecision = 0;
 		for (i = 0; i < 10; ++i) {
 			clock_t heuristicTimeTest = clock();
+
 			if (heuristicTimeTest % 10) {
 				sm_cPrecision = 1.0 / CLOCKS_PER_SEC;
 				break;
 			}
 			else if (heuristicTimeTest % 100) {
-				if (sm_cPrecision == 0 ||
-				        sm_cPrecision > 10.0 / CLOCKS_PER_SEC)
+				if (sm_cPrecision == 0 || sm_cPrecision > 10.0 / CLOCKS_PER_SEC)
 					sm_cPrecision = 10.0 / CLOCKS_PER_SEC;
 			}
 			else if (heuristicTimeTest % 1000) {
@@ -78,14 +78,14 @@ void sTimer_Init(sTimer *t)
 
 		if (sm_HRTicksPerSec != 0.0) {
 			hr_timer start, end;
+
 			HRSETCURRENT(start);
 			do {
 				HRSETCURRENT(end);
 			} while (HRGETTICKS(end) == HRGETTICKS(start));
 
-			sm_hrPrecision =
-			    (HRGETTICKS(end) -
-			     HRGETTICKS(start)) / sm_HRTicksPerSec;
+			sm_hrPrecision = (HRGETTICKS(end) -
+			                  HRGETTICKS(start)) / sm_HRTicksPerSec;
 		}
 	}
 }
@@ -94,15 +94,13 @@ void sTimer_Init(sTimer *t)
 void sTimer_Stop(sTimer *t)
 {
 	if (t->m_fRunning) {
-		if (sm_HRTicksPerSec != 0.0) {
+		if (sm_HRTicksPerSec != 0.0)
 			HRSETCURRENT(t->m_hrEndTime);
-		}
-		else {
+		else
 			t->m_cEndTime = clock();
-		}
-	}
-	else
+	} else
 		HRZERO(t->m_hrEndTime);
+
 	t->m_fRunning = 0;
 }
 
@@ -110,12 +108,12 @@ void sTimer_Start(sTimer *t, int bClear)
 {
 	if (bClear)
 		sTimer_ClearTime(t);
-	if (sm_HRTicksPerSec != 0.0) {
+
+	if (sm_HRTicksPerSec != 0.0)
 		HRSETCURRENT(t->m_hrStartTime);
-	}
-	else {
+	else
 		t->m_cStartTime = clock();
-	}
+
 	t->m_fRunning = 1;
 }
 
@@ -124,23 +122,24 @@ void sTimer_ClearTime(sTimer *t)
 	t->m_dAccumSeconds = 0;
 	HRZERO(t->m_hrStartTime);
 	HRZERO(t->m_hrEndTime);
+
 	t->m_fRunning = 0;
 }
 
 double sTimer_GetSecs(sTimer *t)
 {
 	double retval;
+
 	if (t->m_fRunning) {
-		if (sm_HRTicksPerSec != 0.0) {
+		if (sm_HRTicksPerSec != 0.0)
 			HRSETCURRENT(t->m_hrEndTime);
-		}
-		else {
+		else
 			t->m_cEndTime = clock();
-		}
 	}
 	if (sm_HRTicksPerSec == 0.0) {
 		// This is process time
 		double d = (t->m_cEndTime - t->m_cStartTime) * 1.0;
+
 		if (d > 0)
 			retval = d / CLOCKS_PER_SEC;
 		else
@@ -148,14 +147,13 @@ double sTimer_GetSecs(sTimer *t)
 	}
 	else {
 		// This is wall-clock time
-		double d =
-		    (HRGETTICKS(t->m_hrEndTime) -
-		     HRGETTICKS(t->m_hrStartTime));
-		retval = 0;
+		double d = (HRGETTICKS(t->m_hrEndTime) - HRGETTICKS(t->m_hrStartTime));
+
 		if (d > 0)
 			retval = d / sm_HRTicksPerSec;
 		else
 			retval = 0;
 	}
+
 	return retval + t->m_dAccumSeconds;
 }
--- src/timer.h
+++ src/timer.h
@@ -1,17 +1,12 @@
 /*
- * This software was written by Jim Fougeron jfoug AT cox dot net
- * in 2009. No copyright is claimed, and the software is hereby
- * placed in the public domain. In case this attempt to disclaim
- * copyright and place the software in the public domain is deemed
- * null and void, then the software is Copyright (c) 2009 Jim Fougeron
- * and it is hereby released to the general public under the following
- * terms:
+ * This file is
+ * Copyright (c) 2009 by Jim Fougeron jfoug AT cox dot net
+ * Copyright (c) 2019 by magnum
  *
- * This software may be modified, redistributed, and used for any
- * purpose, in source and binary forms, with or without modification.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted.
  *
- * Portable hi-res timer.  Was a nice C++ class.
- * Downgraded to C for project john
+ * Portable hi-res timer.  Was a nice C++ class. Downgraded to C for john.
  */
 
 #ifndef _HAVE_TIMER_H
@@ -19,6 +14,7 @@
 
 #include <time.h>
 
+/* Windows - use provided hi-res timer */
 #if defined (_MSC_VER) || defined (__MINGW32__) || defined (__CYGWIN32__)
 
 #include <sys/timeb.h>      /* for ftime(), which is not used */
@@ -28,7 +24,7 @@
 typedef LARGE_INTEGER hr_timer;
 
 #define HRZERO(X)             (X).HighPart = (X).LowPart = 0
-#define HRSETCURRENT(X)       QueryPerformanceCounter (&(X));
+#define HRSETCURRENT(X)       QueryPerformanceCounter(&(X))
 #define HRGETTICKS(X)         ((double)(X).HighPart * 4294967296.0 + \
                                (double)(X).LowPart)
 #define HRGETTICKS_PER_SEC(X) {	  \
@@ -39,17 +35,43 @@ typedef LARGE_INTEGER hr_timer;
 			(X) = 0.0; \
 	}
 
-#else
+#elif __MACH__ /* macOS */
+
+#include <mach/mach_time.h>
+
+typedef struct timespec hr_timer;
+#define HRZERO(X)             (X).tv_sec = (X).tv_nsec = 0
+#define HRSETCURRENT(X)       do { int64_t tmp = mach_absolute_time() * sm_timebase; (X).tv_sec = tmp * 1E-9; (X).tv_nsec = tmp - (X).tv_sec * 1E9; } while (0)
+#define HRGETTICKS(X)         ((double)(X).tv_sec * 1E9 + (double)(X).tv_nsec)
+#define HRGETTICKS_PER_SEC(X) { mach_timebase_info_data_t tb; mach_timebase_info(&tb); sm_timebase = tb.numer; sm_timebase /= tb.denom; (X) = sm_timebase * 1E9; }
+
+#else /* Linux, POSIX */
+
+#include <unistd.h>
+
+/* Do we seem to have a (hopefully) nanosecond monotonic clock? */
+#if _POSIX_TIMERS && defined(_POSIX_MONOTONIC_CLOCK)
+
+typedef struct timespec hr_timer;
+#define HRZERO(X)             (X).tv_sec = (X).tv_nsec = 0
+#define HRSETCURRENT(X)       clock_gettime(CLOCK_MONOTONIC, &(X))
+#define HRGETTICKS(X)         ((double)(X).tv_sec * 1E9 + (double)(X).tv_nsec)
+#define HRGETTICKS_PER_SEC(X) { hr_timer r; clock_getres(CLOCK_MONOTONIC, &r); (X) = r.tv_sec + 1E9 / r.tv_nsec; }
+
+#else /* Fallback to microsecond non-monotonic clock that should be available */
 
 #include <sys/time.h>
+
 typedef struct timeval hr_timer;
 #define HRZERO(X)             (X).tv_sec = (X).tv_usec = 0
-#define HRSETCURRENT(X)       { gettimeofday (&(X), NULL); }
+#define HRSETCURRENT(X)       gettimeofday(&(X), NULL)
 #define HRGETTICKS(X)         ((double)(X).tv_sec * 1000000.0 +	\
                                (double)(X).tv_usec)
 #define HRGETTICKS_PER_SEC(X) (X) = 1000000.0
 
 #endif
+
+#endif /* Windows or Unix */
 
 typedef struct sTimer_s {
 	int m_fRunning;     // true if we are running
@@ -68,9 +90,8 @@ double sTimer_GetSecs(sTimer *t); // If timer is running returns elapsed;
                                   // if stopped returns timed interval;
                                   // if not started returns 0.0.
 
-extern double sm_HRTicksPerSec;  // HR Ticks per second (claimed)
-extern int sm_fGotHRTicksPerSec; // Set if we have got the above
-extern double sm_hrPrecision;    // HR Ticks per second (observed, guess)
+extern double sm_HRTicksPerSec;  // HR ticks per second (claimed)
+extern double sm_hrPrecision;    // HR ticks per second (observed, guess)
 extern double sm_cPrecision;     // clocks (ticks) per second (observed, guess)
 
 #define sTimer_Start_noclear(t) \
