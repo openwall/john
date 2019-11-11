f012b29610a438a0d36be815e50363c5da7155a0
Solar Designer <solar@openwall.com>
2019-11-11 13:29:51 +0100

    bitcoin2john.py: Drop no longer relevant python-ecdsa and SlowAES credits

--- run/bitcoin2john.py
+++ run/bitcoin2john.py
@@ -16,17 +16,6 @@
 # BitcoinTools (wallet.dat handling code, MIT License)
 # https://github.com/gavinandresen/bitcointools
 # Copyright (c) 2010 Gavin Andresen
-#
-# python-ecdsa (EC_KEY implementation, MIT License)
-# http://github.com/warner/python-ecdsa
-# "python-ecdsa" Copyright (c) 2010 Brian Warner
-# Portions written in 2005 by Peter Pearson and placed in the public domain.
-#
-# SlowAES (aes.py code, Apache 2 License)
-# http://code.google.com/p/slowaes/
-# Copyright (c) 2008, Josh Davis (http://www.josh-davis.org),
-# Alex Martelli (http://www.aleax.it)
-# Ported from C code written by Laurent Haan (http://www.progressive-coding.com)
 
 try:
         from bsddb.db import *
