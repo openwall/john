0acc326bd420c156343bc71474d5dcda16ff9bdb
Claudio André <claudioandre.br@gmail.com>
2019-12-17 17:23:30 -0300

    CI: save john-local.conf in ../run folder
    JtR is no longer able to read the config file from the current
    directory to disable a format.

--- .ci/disable_formats.sh
+++ .ci/disable_formats.sh
@@ -2,12 +2,12 @@
 
 # There was a bug in echo -e in Travis
 # TODO: we know these formats must be fixed (or removed)
-echo '[Local:Disabled:Formats]' > john-local.conf
-echo 'Raw-SHA512-free-opencl = Y' >> john-local.conf
-echo 'XSHA512-free-opencl = Y' >> john-local.conf
-echo 'gpg-opencl = Y' >> john-local.conf
-echo 'KeePass-opencl = Y' >> john-local.conf
+echo '[Local:Disabled:Formats]' > ../run/john-local.conf
+echo 'Raw-SHA512-free-opencl = Y' >> ../run/john-local.conf
+echo 'XSHA512-free-opencl = Y' >> ../run/john-local.conf
+echo 'gpg-opencl = Y' >> ../run/john-local.conf
+echo 'KeePass-opencl = Y' >> ../run/john-local.conf
 
 # These formats fails OpenCL CPU runtime
-echo 'lotus5-opencl = Y' >> john-local.conf
-echo 'pgpdisk-opencl = Y' >> john-local.conf
+echo 'lotus5-opencl = Y' >> ../run/john-local.conf
+echo 'pgpdisk-opencl = Y' >> ../run/john-local.conf
