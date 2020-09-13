fa8c70c64860cc135bcdb8a2f8a8bd68936f3fec
Solar Designer <solar@openwall.com>
2020-09-13 23:18:51 +0200

    7z formats: benchmark for only one set of costs, report only Raw
    Fixes #4320

--- src/7z_common.h
+++ src/7z_common.h
@@ -20,7 +20,7 @@
 #define FORMAT_TAG              "$7z$"
 #define TAG_LENGTH              (sizeof(FORMAT_TAG)-1)
 #define BENCHMARK_COMMENT       " (512K iterations)"
-#define BENCHMARK_LENGTH        7
+#define BENCHMARK_LENGTH        0x507
 #define BINARY_SIZE             0
 #define BINARY_ALIGN            1
 #define SALT_SIZE               sizeof(sevenzip_salt_t*)
