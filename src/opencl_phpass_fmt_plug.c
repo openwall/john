2606c8f51a08645e09c0846349b04474b1ec86a6
Solar Designer <solar@openwall.com>
2020-09-13 17:05:56 +0200

    phpass-opencl: Transfer keys only when changed

--- src/opencl_phpass_fmt_plug.c
+++ src/opencl_phpass_fmt_plug.c
@@ -55,6 +55,7 @@ static phpass_hash *outbuffer;			/** calculated hashes **/
 static cl_int cl_error;
 static cl_mem mem_in, mem_out, mem_salt;
 static size_t insize, outsize, saltsize;
+static int new_keys;
 static struct fmt_main *self;
 
 #define STEP			0
@@ -134,6 +135,7 @@ static void set_key(char *key, int index)
 {
 	inbuffer[index].length = strlen(key);
 	strncpy((char*)inbuffer[index].v, key, sizeof(inbuffer[index].v));
+	new_keys = 1;
 }
 
 static char *get_key(int index)
@@ -217,9 +219,12 @@ static int crypt_all(int *pcount, struct db_salt *salt)
 	global_work_size = GET_NEXT_MULTIPLE(count, local_work_size);
 
 	// Copy data to gpu
-	BENCH_CLERROR(clEnqueueWriteBuffer(queue[gpu_id], mem_in, CL_FALSE, 0,
-		insize, inbuffer, 0, NULL, multi_profilingEvent[0]),
-		"Copy data to gpu");
+	if (new_keys) {
+		BENCH_CLERROR(clEnqueueWriteBuffer(queue[gpu_id], mem_in, CL_FALSE, 0,
+			insize, inbuffer, 0, NULL, multi_profilingEvent[0]),
+			"Copy data to gpu");
+		new_keys = 0;
+	}
 
 	// Run kernel
 	BENCH_CLERROR(clEnqueueNDRangeKernel(queue[gpu_id], crypt_kernel, 1,
