c0e0f92ed7ff002caba41c18a155a4d5d8da6291
magnum <john.magnum@hushmail.com>
2020-09-23 15:42:59 +0200

    zip2john and pkzip format: Bump MAX_PKZ_FILES from 3 to 8
    Some code did not honor that macro, this was fixed.  Closes #3978

--- src/zip2john.c
+++ src/zip2john.c
@@ -43,7 +43,7 @@
  * filename:$pkzip2$C*B*[DT*MT{CL*UL*CR*OF*OX}*CT*DL*CS*TC*DA]*$/pkzip2$   (new format, with 2 checksums)
  * All numeric and 'binary data' fields are stored in hex.
  *
- * C   is the count of hashes present (the array of items, inside the []  C can be 1 to 3.).
+ * C   is the count of hashes present (the array of items, inside the []  C can be 1 to 8.).
  * B   is number of valid bytes in the checksum (1 or 2).  Unix zip is 2 bytes, all others are 1 (NOTE, some can be 0)
  * ARRAY of data starts here
  *   DT  is a "Data Type enum".  This will be 1 2 or 3.  1 is 'partial'. 2 and 3 are full file data (2 is inline, 3 is load from file).
@@ -132,6 +132,10 @@
 #include "missing_getopt.h"
 #endif
 #include "johnswap.h"
+
+#define _STR_VALUE(arg) #arg
+#define STR_MACRO(n)    _STR_VALUE(n)
+#define MAX_FILES STR_MACRO(MAX_PKZ_FILES)
 
 #define MAX_BLOB_INLINE_SIZE 0x400000000ULL // 16 GB
 
@@ -232,7 +236,7 @@ typedef struct _zip_file
 
 // Wrapper struct so we don't have to pass three pointers all the time
 typedef struct _zip_context {
-	zip_ptr best_files[3];  // Three candidated for handling old encryption type archives
+	zip_ptr best_files[MAX_PKZ_FILES]; // Up to 8 candidates for handling old encryption type archives
 	zip_ptr curzip;         // Meta data of file in archive we're currently processing
 	zip_file archive;       // The zip file being processed
 	int num_candidates;     // Number of candidates in best_files array
@@ -863,73 +867,19 @@ static void handle_file_entry(zip_context *ctx)
 	}
 
 	// Suitable file with legacy encryption
-	if (ctx->num_candidates == 0) {
-		move_entry(&(ctx->best_files[ctx->num_candidates++]), &ctx->curzip);
-	} else if (ctx->num_candidates == 1) {
-		if (ctx->curzip.cmp_len < ctx->best_files[0].cmp_len) {
-			move_entry(&(ctx->best_files[ctx->num_candidates++]), &(ctx->best_files[0]));
-			move_entry(&(ctx->best_files[0]), &ctx->curzip);
-		} else {
-			move_entry(&(ctx->best_files[ctx->num_candidates++]), &ctx->curzip);
-		}
-	} else if (ctx->num_candidates == 2) {
-		if (ctx->curzip.cmp_len < ctx->best_files[0].cmp_len) {
-			move_entry(&(ctx->best_files[ctx->num_candidates++]), &(ctx->best_files[1]));
-			move_entry(&(ctx->best_files[1]), &(ctx->best_files[0]));
-			move_entry(&(ctx->best_files[0]), &ctx->curzip);
-		} else if (ctx->curzip.cmp_len < ctx->best_files[1].cmp_len) {
-			move_entry(&(ctx->best_files[ctx->num_candidates++]), &(ctx->best_files[1]));
-			move_entry(&(ctx->best_files[1]), &ctx->curzip);
-		} else {
-			move_entry(&(ctx->best_files[ctx->num_candidates++]), &ctx->curzip);
-		}
-	} else {
-		int done = 0;
-		if (ctx->curzip.magic_type && ctx->curzip.cmp_len > ctx->best_files[0].cmp_len) {
-			// if we have a magic type, we will replace any NON magic type, for the 2nd and 3rd largest, without caring about
-			// the size.
-			if (ctx->best_files[1].magic_type == 0) {
-				if (ctx->best_files[2].cmp_len < ctx->curzip.cmp_len) {
-					move_entry(&(ctx->best_files[1]), &(ctx->best_files[2]));
-					move_entry(&(ctx->best_files[2]), &ctx->curzip);
-					done = 1;
-				} else {
-					move_entry(&(ctx->best_files[1]), &ctx->curzip);
-					done = 1;
-				}
-			} else if (ctx->best_files[2].magic_type == 0) {
-				if (ctx->best_files[1].cmp_len < ctx->curzip.cmp_len) {
-					move_entry(&(ctx->best_files[2]), &ctx->curzip);
-					done = 1;
-				} else {
-					move_entry(&(ctx->best_files[2]), &(ctx->best_files[1]));
-					move_entry(&(ctx->best_files[1]), &ctx->curzip);
-					done = 1;
-				}
-			}
-		}
-		if (!done && ctx->curzip.cmp_len < ctx->best_files[0].cmp_len) {
-			// we 'only' replace the smallest zip, and always keep as many any other magic as possible.
-			if (ctx->best_files[0].magic_type == 0) {
-				move_entry(&(ctx->best_files[0]), &ctx->curzip);
-			} else {
-				// Ok, the 1st is a magic, we WILL keep it.
-				if (ctx->best_files[1].magic_type) {  // Ok, we found our 2
-					move_entry(&(ctx->best_files[2]), &(ctx->best_files[1]));
-					move_entry(&(ctx->best_files[1]), &(ctx->best_files[0]));
-					move_entry(&(ctx->best_files[0]), &ctx->curzip);
-				} else if (ctx->best_files[2].magic_type) {  // Ok, we found our 2
-					move_entry(&(ctx->best_files[1]), &(ctx->best_files[0]));
-					move_entry(&(ctx->best_files[0]), &ctx->curzip);
-				} else {
-					// found none.  So we will simply roll them down (like when #1 was a magic also).
-					move_entry(&(ctx->best_files[2]), &(ctx->best_files[1]));
-					move_entry(&(ctx->best_files[1]), &(ctx->best_files[0]));
-					move_entry(&(ctx->best_files[0]), &ctx->curzip);
-				}
-			}
+	int i, j;
+
+	for (i = 0; i < ctx->num_candidates; i++) {
+		if (ctx->curzip.cmp_len < ctx->best_files[i].cmp_len) {
+			for (j = ctx->num_candidates; j > i; j--)
+				if (j < MAX_PKZ_FILES)
+					move_entry(&(ctx->best_files[j]), &(ctx->best_files[j - 1]));
+			break;
 		}
 	}
+	move_entry(&(ctx->best_files[i]), &ctx->curzip);
+	if (ctx->num_candidates < MAX_PKZ_FILES)
+		ctx->num_candidates++;
 }
 
 static void print_and_cleanup(zip_context *ctx);
@@ -1220,10 +1170,8 @@ static int usage(char *name)
 	fprintf(stderr, " -o <filename>   Only use this file from the .zip file.\n");
 	fprintf(stderr, " -c This will create a 'checksum only' hash.  If there are many encrypted\n");
 	fprintf(stderr, "    files in the .zip file, then this may be an option, and there will be\n");
-	fprintf(stderr, "    enough data that false positives will not be seen.  If the .zip is 2\n");
-	fprintf(stderr, "    byte checksums, and there are 3 or more of them, then we have 48 bits\n");
-	fprintf(stderr, "    knowledge, which 'may' be enough to crack the password, without having\n");
-	fprintf(stderr, "    to force the user to have the .zip file present.\n");
+	fprintf(stderr, "    enough data that false positives will not be seen.  Up to " MAX_FILES " files are\n");
+	fprintf(stderr, "    supported. These hashes do not reveal actual file data.\n");
 	fprintf(stderr, " -m Use \"file magic\" as known-plain if applicable. This can be faster but\n");
 	fprintf(stderr, "    not 100%% safe in all situations.\n");
 	fprintf(stderr, " -2 Force 2 byte checksum computation.\n");
