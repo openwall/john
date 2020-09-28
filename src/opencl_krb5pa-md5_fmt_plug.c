openwall/john

commit 8bbd90f49ca6b18179112b3fa923192e198b4894

OpenCL formats (many); Fix memory leaks
Fix a bug in o5logon introduced recently. MEM_FREE nulls the pointer,
OpenCL functions don't.
Fix RAW formats invalid pointer
Handle release_clobj call even if not initialized
Ensure buffers are always destroyed before re-created

See #4295

src/opencl_krb5pa-md5_fmt_plug.c
@@ -158,8 +158,12 @@ static void set_kernel_args()
 	HANDLE_CLERROR(clSetKernelArg(crypt_kernel, 10, sizeof(buffer_bitmap_dupe), (void *) &buffer_bitmap_dupe), "Error setting argument 11.");
 }
 
+static void release_clobj(void);
+
 static void create_clobj(size_t kpc, struct fmt_main *self)
 {
+	release_clobj();
+
 	pinned_saved_keys = clCreateBuffer(context[gpu_id], CL_MEM_READ_ONLY | CL_MEM_ALLOC_HOST_PTR, BUFSIZE * kpc, NULL, &ret_code);
 	if (ret_code != CL_SUCCESS) {
 		saved_plain = (cl_uint *) mem_alloc(BUFSIZE * kpc);
@@ -366,9 +370,12 @@ static void init_kernel(void)
 #endif
 	);
 
-	opencl_build_kernel("$JOHN/opencl/krb5pa-md5_kernel.cl", gpu_id, build_opts, 0);
-	crypt_kernel = clCreateKernel(program[gpu_id], "krb5pa_md5", &ret_code);
-	HANDLE_CLERROR(ret_code, "Error creating kernel. Double-check kernel name?");
+	if (!program[gpu_id])
+		opencl_build_kernel("$JOHN/opencl/krb5pa-md5_kernel.cl", gpu_id, build_opts, 0);
+	if (!crypt_kernel) {
+		crypt_kernel = clCreateKernel(program[gpu_id], "krb5pa_md5", &ret_code);
+		HANDLE_CLERROR(ret_code, "Error creating kernel. Double-check kernel name?");
+	}
 }
 
 static void set_key(char *_key, int index);

commit 4ccf33865d3cae3f86e875b661e1c6d450fca566

krb5pa-md5-opencl: Fix segfault introduced with 8bbd90f49ca6b18179112b3fa923192e198b4894
Fixes #4375

src/opencl_krb5pa-md5_fmt_plug.c
@@ -330,6 +330,7 @@ static void init_kernel(void)
 	cl_ulong const_cache_size;
 
 	clReleaseKernel(crypt_kernel);
+	crypt_kernel = NULL;
 
 	for (i = 0; i < MASK_FMT_INT_PLHDR; i++)
 		if (mask_skip_ranges && mask_skip_ranges[i] != -1)