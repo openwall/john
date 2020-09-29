127ec6f37d36eb2edafbb92db9eda94f3bcc628c
Claudio André <claudioandre.br@gmail.com>
2020-09-29 07:23:49 -0300

    mask: initialize properly for benchmarking
    Properly initialize mask. Any format benchmarked right after LM-opencl
    crashes: super's --dev=1 makes the crash reproducible.

--- src/mask.c
+++ src/mask.c
@@ -2119,6 +2119,7 @@ void mask_init(struct db_main *db, char *unprocessed_mask)
 
 	mask_db = db;
 	mask_fmt = db->format;
+	mask_bench_index = 0;
 
 	/* These formats are too wierd for magnum to get working */
 	if (!strcasecmp(mask_fmt->params.label, "descrypt-opencl") ||
