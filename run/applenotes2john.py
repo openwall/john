2882dfd057dd3abfa5a6ebcec5a93beec05037f5
Banaanhangwagen <banaanhangwagen@protonmail.com>
2020-11-05 16:36:48 +0100

    Update applenotes2john.py

--- run/applenotes2john.py
+++ run/applenotes2john.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 # Script to extract "hashes" from password protected Apple Notes databases.
 #
@@ -18,37 +18,28 @@ import sys
 import sqlite3
 import binascii
 
-PY3 = sys.version_info[0] == 3
-
-if not PY3:
-    reload(sys)
-    sys.setdefaultencoding('utf8')
-
-
 def process_file(filename):
     db = sqlite3.connect(filename)
     cursor = db.cursor()
-    rows = cursor.execute("SELECT Z_PK, ZCRYPTOITERATIONCOUNT, ZCRYPTOSALT, ZCRYPTOWRAPPEDKEY, ZPASSWORDHINT, ZCRYPTOVERIFIER, ZISPASSWORDPROTECTED FROM ZICCLOUDSYNCINGOBJECT")
-    for row in rows:
-        iden, iterations, salt, fhash, hint, shash, is_protected = row
-        if fhash is None:
-            phash = shash
-        else:
-            phash = fhash
-        if hint is None:
-            hint = "None"
+    rows = cursor.execute("SELECT Z_PK, ZCRYPTOITERATIONCOUNT, ZCRYPTOSALT, ZCRYPTOWRAPPEDKEY, ZPASSWORDHINT, "
+                          "ZCRYPTOVERIFIER, ZISPASSWORDPROTECTED FROM ZICCLOUDSYNCINGOBJECT")
+    for iden, iterations, salt, fhash, hint, shash, is_protected in rows:
+        phash = fhash if fhash is not None else shash
+        hint = hint or ""
+
         # NOTE: is_protected can be zero even if iterations value is non-zero!
         # This was tested on macOS 10.13.2 with cloud syncing turned off.
         if iterations == 0:  # is this a safer check than checking is_protected?
             continue
-        phash = binascii.hexlify(phash)
-        salt = binascii.hexlify(salt)
-        if PY3:
-            phash = str(phash, 'ascii')
-            salt = str(salt, 'ascii')
-        fname = os.path.basename(filename)
-        sys.stdout.write("%s:$ASN$*%d*%d*%s*%s:::::%s\n" % (fname, iden,
-                                               iterations, salt, phash, hint))
+
+        if phash is None:
+            salt = binascii.hexlify(salt).decode("utf8")
+            print(f'$ASN$*{iden}*{iterations}*{salt}*ERROR:::::{hint}')
+            continue
+        salt = binascii.hexlify(salt).decode("utf8")
+        phash = binascii.hexlify(phash).decode("utf8")
+
+        print(f'$ASN$*{iden}*{iterations}*{salt}*{phash}:::::{hint}')
 
 
 if __name__ == "__main__":
