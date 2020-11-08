05b84ff4c7a6571114ab2361bea3468648b56a8b
Udi Schneider <udisch@gmail.com>
2020-11-08 09:24:37 +0200

    fix indentation

--- src/blockchain_common_plug.c
+++ src/blockchain_common_plug.c
@@ -105,11 +105,11 @@ int blockchain_decrypt(unsigned char *derived_key, unsigned char *data)
 	/* various tests */
 	if (out[0] != '{') // fast test
 		return -1;
-	
+
 	// check for android format. See https://github.com/gurnec/btcrecover/issues/203
-        if (memmem(out, 16, "\"address_book", 13)) {
-                return 0;
-        }
+	if (memmem(out, 16, "\"address_book", 13)) {
+		return 0;
+	}
 
 	// "guid" will be found in the first block
 	if (memmem(out, 16, "\"guid\"", 6)) {
