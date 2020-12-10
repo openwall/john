92ff3387a0ff81d121bfeef6770fb3afea1e6cfb
y <yvain29@gmail.com>
2020-12-10 15:18:54 +0100

    enhanced

--- src/rain.c
+++ src/rain.c
@@ -437,12 +437,12 @@ int do_rain_crack(struct db_main *db, char *req_charset)
 			        int tmpc = charset_utf32[(charset_idx[loop][i] + rotate) % charcount];
 			        if( (word[i] = tmpc) > cp_max )
 				        quick_conversion = 0;
-				    rotate -= rotate / charcount;
+				    rotate /= (i + mpl % 2) / (mpl / 2 + mpl % 2 + 1) + 1;
+					rotate -= rotate / charcount;
 				}
-				    submit(word);
-		    }
+			    submit(word);
+			}
 		    rain[loop]++;
-		    
 		    int pos = 0;
 	        while(pos < mpl && ++charset_idx[loop][pos] >= charcount) {
 			    charset_idx[loop][pos] = 0;
