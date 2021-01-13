9c27012f86454fd52ca8620eaf2a77f87249883a
Juergen Hoetzel <juergen@hoetzel.info>
2021-01-13 07:09:10 +0100

    poly1305-donna: Prevent name clashing with existing implementation
    Closes #4546

--- src/poly1305-donna/poly1305-donna.h
+++ src/poly1305-donna/poly1305-donna.h
@@ -3,6 +3,14 @@
 
 #include <stddef.h>
 
+/* prevent name clashing with existing implementations */
+#define poly1305_init john_poly1305_init
+#define poly1305_update john_poly1305_update
+#define poly1305_finish john_poly1305_finish
+#define poly1305_auth john_poly1305_auth
+#define poly1305_verify john_poly1305_verify
+#define poly1305_power_on_self_test john_poly1305_power_on_self_test
+
 typedef struct poly1305_context {
 	size_t aligner;
 	unsigned char opaque[136];
