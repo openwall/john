4abf22b5e91322bfe6530907cdd628aecd243844
exploide <me@exploide.net>
2021-03-20 16:08:24 +0100

    made ps_token2john.py compatible with both, Python2 and Python3

--- run/ps_token2john.py
+++ run/ps_token2john.py
@@ -1,20 +1,25 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
-import base64
-import hashlib
 import argparse
-import zlib
+import base64
+import binascii
+import hashlib
 import sys
+import zlib
 
-# Use "generate.py from https://erpscan.com/wp-content/uploads/tools/ERPScan-tockenchpoken.zip
+# Use "generate.py from https://erpscan.io/wp-content/uploads/tools/ERPScan-tockenchpoken.zip
 # to generate sample PS_TOKEN cookies.
 
-print "Based on tokenchpoken v0.5 beta's parse.py file"
-print 'Oracle PS_TOKEN cracker. Token parser'
-print
-print 'Alexey Tyurin - a.tyurin at erpscan.com'
-print 'ERPScan Research Group - http://www.erpscan.com'
-print
+print("""Based on tokenchpoken v0.5 beta's parse.py file
+Oracle PS_TOKEN cracker. Token parser
+
+Alexey Tyurin - a.tyurin at erpscan.io
+ERPScan Research Group - https://www.erpscan.io
+""")
+
+def hexstr(bytestr):
+    return binascii.hexlify(bytestr).decode('ascii')
+
 parser = argparse.ArgumentParser()
 parser.add_argument('-c', action='store', dest='cookie', required=True,
                     help='Set a victim\'s PS_TOKEN cookie for parsing')
@@ -24,7 +29,7 @@ args = parser.parse_args()
 input = args.cookie
 
 full_str = base64.b64decode(input)
-sha_mac = full_str[44:64].encode('hex')
+sha_mac = hexstr(full_str[44:64])
 inflate_data = full_str[76:]
 data = zlib.decompress(inflate_data)
 
@@ -32,13 +37,14 @@ data = zlib.decompress(inflate_data)
 data_hash = hashlib.sha1(data).hexdigest()
 
 user_length = data[20]
+if isinstance(user_length, int):
+    user_length = user_length.to_bytes(1, sys.byteorder)
 loc = 21
-user = data[loc:loc + int(user_length.encode('hex'), 16)].replace("\x00", "")
+user = data[loc:loc + int(hexstr(user_length), 16)].replace(b"\x00", b"").decode('utf-8')
 
 # python generate.py -e 0 -u PS -l ENG -p "" -n PSFT_HR -d 2015-07-01-08.06.46
 if data_hash == sha_mac:
-    print "%s: there is no password for the attacking node!" % user
+    print("%s: there is no password for the attacking node!" % user)
 else:
     # print hash
-    sys.stdout.write("%s:$dynamic_1600$%s$HEX$%s\n" % (user, sha_mac,
-                                                       data.encode("hex")))
+    sys.stdout.write("%s:$dynamic_1600$%s$HEX$%s\n" % (user, sha_mac, hexstr(data)))
