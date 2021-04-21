openwall/john

commit 82d0825a8aed3c15cd13be709125b6a37319cce6

Let --node numbers range be any multiple of --fork process count
Previously, the range needed to exactly match the process count.  Other
multiples help balance the workload across machine with very different
per-process performance.

src/john.c
@@ -534,6 +534,9 @@ static void john_fork(void)
 	pids = mem_alloc_tiny((options.fork - 1) * sizeof(*pids),
 	    sizeof(*pids));
 
+	unsigned int range = options.node_max - options.node_min + 1;
+	unsigned int npf = range / options.fork;
+
 	for (i = 1; i < options.fork; i++) {
 		switch ((pid = fork())) {
 		case -1:
@@ -541,8 +544,6 @@ static void john_fork(void)
 
 		case 0:
 			sig_preinit();
-			options.node_min += i;
-			options.node_max = options.node_min;
 #if HAVE_OPENCL
 			/* Poor man's multi-device support */
 			if (options.acc_devices->count &&
@@ -560,17 +561,26 @@ static void john_fork(void)
 				usleep(i * 100000);
 			}
 #endif
-			if (rec_restoring_now) {
-				unsigned int node_id = options.node_min;
+ 			if (rec_restoring_now) {
+				unsigned int save_min = options.node_min;
+				unsigned int save_max = options.node_max;
+				unsigned int save_count = options.node_count;
+				unsigned int save_fork = options.fork;
+				options.node_min += i * npf;
+				options.node_max = options.node_min + npf - 1;
 				rec_done(-2);
 				rec_restore_args(1);
 				john_set_tristates();
-				if (node_id != options.node_min + i)
+				if (options.node_min != save_min ||
+				    options.node_max != save_max ||
+				    options.node_count != save_count ||
+				    options.fork != save_fork)
 					fprintf(stderr,
 					    "Inconsistent crash recovery file:"
 					    " %s\n", rec_name);
-				options.node_min = options.node_max = node_id;
 			}
+			options.node_min += i * npf;
+			options.node_max = options.node_min + npf - 1;
 			sig_init_child();
 			return;
 

doc/OPTIONS
@@ -402,8 +402,14 @@ with similar per-core performance, you could run an OpenMP-enabled build
 on both of them and use "--node=1/9" (without "--fork") on the first
 machine (8 threads in 1 process) and "--fork=8 --node=2-9/9" on the
 64-core machine (8 threads in 8 processes, for 64 threads total on this
-machine).  With the current implementation, the node numbers range
-assigned to each John invocation must match the "--fork" process count.
+machine).  The node numbers range assigned to each John invocation must
+be a multiple of the "--fork" process count.  You'd usually have the
+number of nodes in the range exactly match the "--fork" process count,
+but you can use other multiples to compensate for major differences in
+per-process performance between different machines.  For example, on a
+machine where each process is twice faster than on another, you'd
+reasonably allocate two virtual node numbers per each "--fork" process,
+e.g. with "--fork=8 --node=9-24/24".
 
 When running with "--fork", multiple ".rec" files are created, which are
 then read back by "--status" and "--restore" if you use those options.

src/options.c
@@ -607,8 +607,11 @@ void opt_init(char *name, int argc, char **argv)
 		status_print();
 #if OS_FORK
 		if (options.fork) {
-			unsigned int i, node_max = options.node_max;
-			for (i = options.node_min + 1; i <= node_max; i++) {
+			unsigned int node_max = options.node_max;
+			unsigned int range = node_max - options.node_min + 1;
+			unsigned int npf = range / options.fork;
+			unsigned int i = options.node_min;
+			while ((i += npf) <= node_max) {
 				rec_name = rec_name_orig;
 				rec_name_completed = 0;
 				rec_restoring_now = 0;
@@ -844,6 +847,7 @@ void opt_init(char *name, int argc, char **argv)
 				options.node_max += mpi_p - 1;
 #endif
 		}
+		unsigned int range = options.node_max - options.node_min + 1;
 		if (n < 2)
 			msg = "valid syntax is MIN-MAX/TOTAL or N/TOTAL";
 		else if (!options.node_min)
@@ -855,9 +859,8 @@ void opt_init(char *name, int argc, char **argv)
 		else if (options.node_max > options.node_count)
 			msg = "node numbers can't exceed node count";
 #if OS_FORK
-		else if (options.fork &&
-		    options.node_max - options.node_min + 1 != options.fork)
-			msg = "range must be consistent with --fork number";
+		else if (options.fork && range % options.fork)
+			msg = "node range must be divisible by fork count";
 #endif
 #ifdef HAVE_MPI
 		if (mpi_p > 1 &&
@@ -866,10 +869,9 @@ void opt_init(char *name, int argc, char **argv)
 #endif
 		else if (!options.fork &&
 #ifdef HAVE_MPI
-		         mpi_p == 1 &&
+		    mpi_p == 1 &&
 #endif
-		    options.node_max - options.node_min + 1 ==
-		    options.node_count)
+		    range == options.node_count)
 			msg = "node numbers can't span the whole range";
 		if (msg) {
 			if (john_main_process)

commit 0bbd56c3b4fc96a595406d5ab8c6df48f207a883

Fix multi-device support
The combination of "--devices" with "--fork" was broken for OpenCL devices
since 82d0825a8aed3c15cd13be709125b6a37319cce6.

Fixes #4679

src/john.c
@@ -544,23 +544,6 @@ static void john_fork(void)
 
 		case 0:
 			sig_preinit();
-#if HAVE_OPENCL
-			/* Poor man's multi-device support */
-			if (options.acc_devices->count &&
-			    strstr(database.format->params.label, "-opencl")) {
-				/* Postponed format init in forked process */
-				fmt_init(database.format);
-			}
-#endif
-#if HAVE_ZTEX
-			if (strstr(database.format->params.label, "-ztex")) {
-				list_init(&ztex_use_list);
-				list_extract_list(ztex_use_list, ztex_detected_list,
-					i * ztex_devices_per_fork, ztex_devices_per_fork);
-				ztex_fork_num = i;
-				usleep(i * 100000);
-			}
-#endif
 			if (rec_restoring_now) {
 				unsigned int save_min = options.node_min;
 				unsigned int save_max = options.node_max;
@@ -579,6 +562,23 @@ static void john_fork(void)
 			}
 			options.node_min += i * npf;
 			options.node_max = options.node_min + npf - 1;
+#if HAVE_OPENCL
+			/* Poor man's multi-device support */
+			if (options.acc_devices->count &&
+			    strstr(database.format->params.label, "-opencl")) {
+				/* Postponed format init in forked process */
+				fmt_init(database.format);
+			}
+#endif
+#if HAVE_ZTEX
+			if (strstr(database.format->params.label, "-ztex")) {
+				list_init(&ztex_use_list);
+				list_extract_list(ztex_use_list, ztex_detected_list,
+					i * ztex_devices_per_fork, ztex_devices_per_fork);
+				ztex_fork_num = i;
+				usleep(i * 100000);
+			}
+#endif
 			sig_init_child();
 			return;
 