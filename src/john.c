e70ccb5400fae5963f590042af80390d84c3a311
owein <yvain29@gmail.com>
2021-08-26 00:04:08 +0200

    correct count with chains. TODO: chaining 3 (or 4 ?) characters. Splitting the sets with randomness.

--- src/john.c
+++ src/john.c
@@ -104,7 +104,7 @@ static int john_omp_threads_new;
 #include "mask.h"
 #include "mkv.h"
 #include "subsets.h"
-#include "posfreq.h"
+#include "inc2.h"
 #include "external.h"
 #include "batch.h"
 #include "dynamic.h"
@@ -1802,8 +1802,8 @@ static void john_run(void)
 		if (options.flags & FLG_SUBSETS_CHK)
 			do_subsets_crack(&database, options.subset_full);
 		else
-		if (options.flags & FLG_POSFREQ_CHK)
-			do_posfreq_crack(&database);
+		if (options.flags & FLG_INC2_CHK)
+			do_inc2_crack(&database);
 		else
 #if HAVE_REXGEN
 		if ((options.flags & FLG_REGEX_CHK) &&
