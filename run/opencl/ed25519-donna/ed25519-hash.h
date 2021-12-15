e3b210eb524ddcdc237923064567c3d4d71d3e24
magnum <magnumripper@hushmail.com>
2021-12-15 12:47:29 +0100

    Drop dead code in OpenCL's ed25519

--- run/opencl/ed25519-donna/ed25519-hash.h
+++ run/opencl/ed25519-donna/ed25519-hash.h
@@ -1,24 +1,5 @@
 #include "../opencl_sha2_ctx.h"
 
-typedef SHA512_CTX ed25519_hash_context;
-
-#if 0
-static void
-ed25519_hash_init(ed25519_hash_context *ctx) {
-	SHA512_Init(ctx);
-}
-
-static void
-ed25519_hash_update(ed25519_hash_context *ctx, const uint8_t *in, size_t inlen) {
-	SHA512_Update(ctx, in, inlen);
-}
-
-static void
-ed25519_hash_final(ed25519_hash_context *ctx, uint8_t *hash) {
-	SHA512_Final(hash, ctx);
-}
-#endif
-
 static void
 ed25519_hash(uint8_t *hash, const uint8_t *in, size_t inlen) {
 	SHA512_CTX ctx;
