79c47de0f8ed09cb07517f516140fd9ff32c9bc3
owein <yvain29@gmail.com>
2022-04-09 09:50:05 +0200

    option --full to specify if we complete the characters list with ascii table we also output a global frequency list, not dependent on position.

--- src/parse_wordlist.c
+++ src/parse_wordlist.c
@@ -6,10 +6,18 @@
 struct freq {
     char c;
     int *posfreq;
+    int freq;
 };
 
 int main(int argc, char *argv[]) {
-    FILE *file = fopen(argv[1], "r");
+    int full = 0;
+    int ind = 1;
+    if(strcmp(argv[1], "--full") == 0) {
+        ind = 2;
+        full = 1;
+    }
+    
+    FILE *file = fopen(argv[ind], "r");
     int buff_size = 0;
     char c;
     while(!feof(file)) {
@@ -47,9 +55,8 @@ int main(int argc, char *argv[]) {
             }
         }
         if(set)
-            max_len = words_size[i];
+            max_len = words_size[i] - 1;
     }
-
     char **words;
     words = (char **) malloc(wordcount * sizeof(char*));
     for(i=0; i<wordcount; i++)
@@ -65,6 +72,7 @@ int main(int argc, char *argv[]) {
     struct freq chars[95];
     for(i=0; i<95; i++) {
         chars[i].c = ' ' + i;
+        chars[i].freq = 0;
         chars[i].posfreq = (int *) malloc(sizeof(int) * max_len);
         for(j=0; j<max_len; j++)
             chars[i].posfreq[j] = 0;
@@ -75,50 +83,72 @@ int main(int argc, char *argv[]) {
             for(j=0; j<words_size[i]; j++) {
                 if(words[i][j] == chars[x].c) {
                     chars[x].posfreq[j]++;
+                    chars[x].freq++;
                 }
             }
         }
     }
     char freqs[max_len][96];
     int y;  
-    for(i=0; i<95; i++) {
-        //printf("%c:\t", chars[i].c);
-        for(j=0; j<max_len; j++) {
-            //printf("%d\t", chars[i].posfreq[j]);
-            freqs[j][i] = 'a';
-        }
-        //printf("\n");
-    }
+
     for(i=0; i<max_len; i++)
         freqs[i][95] = 0;
     
     int used[max_len][95];
-
-    int p,q;
+    int end;
+    int t,p,q;
     for(j=0; j<max_len; j++) {
-        int t;
         for(p=0; p<max_len; p++)
             for(q=0; q<95; q++)
                 used[p][q] = 0;
-
+        end = 0;
         for(t=0; t<95; t++) {
             for(x=0; x<95; x++) {
                 int set = 1;
                 for(i=0; i<95; i++) {
-                    if(chars[i].posfreq[j] > chars[x].posfreq[j] && !used[j][i]) {
+                    if(chars[i].posfreq[j] > chars[x].posfreq[j] && !used[j][i] || (!chars[x].posfreq[j] && !full)) {
                         set = 0;
                         break;
-                    }    
+                    }
                 }
                 if(set && !used[j][x]) { //test loop doesn't overpass original char
                     freqs[j][t] = chars[x].c;
                     used[j][x] = 1;
+                    end++;
                     break;
                 }
             }
         }
-        freqs[j][95] = 0;
+        freqs[j][end] = 0;
     }
+    char glob_freq[95];//we will need this one for very long length for which we won't have datas
+    
+    int used2[95];
+    for(q=0; q<95; q++)
+        used2[q] = 0;
+        
+    end = 0;
+    for(t=0; t<95; t++) {
+        for(x=0; x<95; x++) {
+            int set = 1;
+            for(i=0; i<95; i++) {
+                if(chars[i].freq > chars[x].freq && !used2[i] || (!chars[x].freq && !full)) {
+                    set = 0;
+                    break;
+                }
+            }
+            if(set && !used2[x]) { //test loop doesn't overpass original char
+                glob_freq[t] = chars[x].c;
+                used2[x] = 1;
+                end++;
+                break;
+            }
+        }
+    }
+    glob_freq[end] = 0;
+
     for(i=0; i<max_len; i++)
-        printf("%s\n", freqs[i]);
+        if(strlen(freqs[i]))
+            printf("%s\n", freqs[i]);
+    printf("\n%s\n", glob_freq);
 }
