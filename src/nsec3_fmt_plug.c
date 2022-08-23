e2e2dbf5bb6c4ee3306b286f89884a7dcbecdede
Ralf Sager <nsec3map@3fnc.org>
2022-08-23 13:59:06 +0200

    NSEC3: remove FMT_HUGE_INPUT, add hashes back
    I am not sure why FMT_HUGE_INPUT was added as ciphertexts are <600 in
    length.
    This change should significantly speed things up again.

--- src/nsec3_fmt_plug.c
+++ src/nsec3_fmt_plug.c
@@ -387,6 +387,14 @@ static int cmp_exact(char *source, int index)
 	return 1;
 }
 
+static int get_hash_0(int index) { return crypt_out[index][0] & PH_MASK_0; }
+static int get_hash_1(int index) { return crypt_out[index][0] & PH_MASK_1; }
+static int get_hash_2(int index) { return crypt_out[index][0] & PH_MASK_2; }
+static int get_hash_3(int index) { return crypt_out[index][0] & PH_MASK_3; }
+static int get_hash_4(int index) { return crypt_out[index][0] & PH_MASK_4; }
+static int get_hash_5(int index) { return crypt_out[index][0] & PH_MASK_5; }
+static int get_hash_6(int index) { return crypt_out[index][0] & PH_MASK_6; }
+
 struct fmt_main fmt_nsec3 = {
 	{
 		FORMAT_LABEL,
@@ -402,7 +410,7 @@ struct fmt_main fmt_nsec3 = {
 		SALT_ALIGN,
 		MIN_KEYS_PER_CRYPT,
 		MAX_KEYS_PER_CRYPT,
-		FMT_8_BIT | FMT_HUGE_INPUT | FMT_OMP,
+		FMT_8_BIT | FMT_OMP,
 #if FMT_MAIN_VERSION > 11
 		{ NULL },
 #endif
@@ -422,7 +430,13 @@ struct fmt_main fmt_nsec3 = {
 #endif
 		fmt_default_source,
 		{
-			fmt_default_binary_hash
+			fmt_default_binary_hash_0,
+			fmt_default_binary_hash_1,
+			fmt_default_binary_hash_2,
+			fmt_default_binary_hash_3,
+			fmt_default_binary_hash_4,
+			fmt_default_binary_hash_5,
+			fmt_default_binary_hash_6
 		},
 		salt_hash,
 		NULL,
@@ -432,7 +446,13 @@ struct fmt_main fmt_nsec3 = {
 		fmt_default_clear_keys,
 		crypt_all,
 		{
-			fmt_default_get_hash
+			get_hash_0,
+			get_hash_1,
+			get_hash_2,
+			get_hash_3,
+			get_hash_4,
+			get_hash_5,
+			get_hash_6
 		},
 		cmp_all,
 		cmp_one,
