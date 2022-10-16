84a4aeb200d000cea78a36ffa9f99407c6f7417e
Sylvain Pelissier <sylvain.pelissier@gmail.com>
2022-10-16 09:19:39 +0200

    ENCDataVault: Increase signature length to reduce false positive

--- src/encdatavault_pbkdf2_fmt_plug.c
+++ src/encdatavault_pbkdf2_fmt_plug.c
@@ -50,13 +50,13 @@ john_register_one(&fmt_encdadatavault_pbkdf2);
 
 static struct fmt_tests encdatavault_pbkdf2_tests[] = {
 	// PrivateAccess
-	{ "$encdv-pbkdf2$1$1$eb012112b3561e6e$01c7dca5$32$9b1c17f7a467cf4d13b73dfd3ce12b3dd51cb980cc1f0ccfad4e47e9c9f3d774$100000", "123456Aa!"},
+	{ "$encdv-pbkdf2$1$1$eb012112b3561e6e$01c7dca58660d6ae$32$9b1c17f7a467cf4d13b73dfd3ce12b3dd51cb980cc1f0ccfad4e47e9c9f3d774$100000", "123456Aa!"},
 	// ENCDataVault v7.2.1 user password
 	//{ "$encdv-pbkdf2$1$3$e84dddc75bc68e3c$789ad76e$32$e89708fb33780d45b975117445c861ed6dfe30142dfdea9afc36ae1a28c552a7$100000", "openwallopenwall"},
 	// ENCDataVault v7.2.1 128 bits vault
- 	//{ "$encdv-pbkdf2$3$1$ae47f1c80b611a1c$bbbcc413$32$da910b0244a6704868b27b72fb0c4558f89b4343ac9716816d63d6ce95cdde6f$100000$f9ce89ee98ed1668cdd25881e0921a30f0a016f3d34055544b082422334f446db6361bbbfa1493ab6bca6c1255b9de6bcd0d1e7fac970fd8d8d21ccf289d224dcb1b5a89bde0c1c04c449155dfe58fa9c383b6856a28ba18d8c5d4efc208a79f05f68491ea98930f5bc7d4dacf56eda4b6277b6b7e88784fa466afba2f8c628a", "123456789ABCDEf"},
+ 	//{ "$encdv-pbkdf2$3$1$ae47f1c80b611a1c$bbbcc4131de92af6$32$da910b0244a6704868b27b72fb0c4558f89b4343ac9716816d63d6ce95cdde6f$100000$f9ce89ee98ed1668cdd25881e0921a30f0a016f3d34055544b082422334f446db6361bbbfa1493ab6bca6c1255b9de6bcd0d1e7fac970fd8d8d21ccf289d224dcb1b5a89bde0c1c04c449155dfe58fa9c383b6856a28ba18d8c5d4efc208a79f05f68491ea98930f5bc7d4dacf56eda4b6277b6b7e88784fa466afba2f8c628a", "123456789ABCDEf"},
 	// ENCDataVault v7.2.1 1024 bits vault
-	{ "$encdv-pbkdf2$3$4$4f1d3889c629968c$e8b68bb8$32$fa79c83e85a41973799522f525e1316ca07ab663def47b816da76205dc1e3e80$100000$0443baa02c7e00870a24f0c16ff0454c56ad079e334bb6fb52bf39de56485dc07f4827d8d83b6f3bf135062e2869d58b8ade04db0716c75d2519797cdbb1d9d75ffe3fc156edfd3bb95747fc0286c149c3c90da8a4b4cdf11838b680b6e9b26da3a0c93f29f8bff6e70f07e6077cda620f7b808793dc127395a11bfc068f1895", "123456789ABCDEf"},
+	{ "$encdv-pbkdf2$3$4$4f1d3889c629968c$e8b68bb804b94a23$32$fa79c83e85a41973799522f525e1316ca07ab663def47b816da76205dc1e3e80$100000$0443baa02c7e00870a24f0c16ff0454c56ad079e334bb6fb52bf39de56485dc07f4827d8d83b6f3bf135062e2869d58b8ade04db0716c75d2519797cdbb1d9d75ffe3fc156edfd3bb95747fc0286c149c3c90da8a4b4cdf11838b680b6e9b26da3a0c93f29f8bff6e70f07e6077cda620f7b808793dc127395a11bfc068f1895", "123456789ABCDEf"},
 	{ NULL }
 };
 
@@ -163,7 +163,7 @@ static int crypt_all_pbkdf2(int *pcount, struct db_salt *salt)
 				// result buffer is used here to hold the decrypted data.
 				enc_aes_ctr_iterated(cur_salt->encrypted_data, result, kdf_out[i][0].u8, ivs, AES_BLOCK_SIZE,
 				                     nb_keys, 1);
-				if (!memcmp(result + 4, "\xd2\xc3\xb4\xa1", ENC_SIG_SIZE)) {
+				if (!memcmp(result + 4, "\xd2\xc3\xb4\xa1\x00\x00", MIN(cur_salt->encrypted_data_length, ENC_SIG_SIZE - 2))) {
 					cracked[index + i] = 1;
 #ifdef _OPENMP
 #pragma omp atomic
@@ -189,7 +189,7 @@ static int crypt_all_pbkdf2(int *pcount, struct db_salt *salt)
 				}
 				// result buffer is reused here to hold the decrypted data.
 				enc_aes_ctr_iterated(cur_salt->encrypted_data, result, result, ivs, AES_BLOCK_SIZE, nb_keys, 1);
-				if (!memcmp(result + 4, "\xd2\xc3\xb4\xa1", ENC_SIG_SIZE)) {
+				if (!memcmp(result + 4, "\xd2\xc3\xb4\xa1\x00\x00", MIN(cur_salt->encrypted_data_length, ENC_SIG_SIZE - 2))) {
 					cracked[index + i] = 1;
 #ifdef _OPENMP
 #pragma omp atomic
