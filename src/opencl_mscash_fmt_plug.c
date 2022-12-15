openwall/john

commit bd7b8267764bfda5bce9df40cf1a6dfcf89e0329

Unify the reset() functions in all OpenCL formats that use shared auto-tune, moving any heuristics to shared code.  Closes #4012

src/opencl_mscash_fmt_plug.c
@@ -719,43 +719,29 @@ static int cmp_exact(char *source, int index)
 
 static void reset(struct db_main *db)
 {
-	static size_t o_lws, o_gws;
-	static int initialized;
-	size_t gws_limit;
+	static int last_int_cand;
 
-	//fprintf(stderr, "%s(%p), i=%d\n", __FUNCTION__, db, initialized);
-	gws_limit = MIN((0xf << 21) * 4 / BUFSIZE,
-	                get_max_mem_alloc_size(gpu_id) / BUFSIZE);
+	if (!crypt_kernel || last_int_cand != mask_int_cand.num_int_cand) {
+		release_base_clobj();
+		release_clobj();
+
+		prepare_table(db);
+		init_kernel();
+
+		create_base_clobj();
+
+		current_salt = 0;
+		hash_ids[0] = 0;
+
+		last_int_cand = mask_int_cand.num_int_cand;
+	}
+
+	size_t gws_limit = MIN((0xf << 21) * 4 / BUFSIZE,
+	                       get_max_mem_alloc_size(gpu_id) / BUFSIZE);
 	get_power_of_two(gws_limit);
 	if (gws_limit > MIN((0xf << 21) * 4 / BUFSIZE,
 	                    get_max_mem_alloc_size(gpu_id) / BUFSIZE))
 		gws_limit >>= 1;
-
-
-	if (initialized) {
-		// Forget the previous auto-tune
-		local_work_size = o_lws;
-		global_work_size = o_gws;
-
-		release_base_clobj();
-		release_clobj();
-	} else {
-		o_lws = local_work_size;
-		o_gws = global_work_size;
-		initialized = 1;
-	}
-
-	prepare_table(db);
-	init_kernel();
-
-	create_base_clobj();
-
-	current_salt = 0;
-	hash_ids[0] = 0;
-
-	// If real crack run, don't auto-tune for self-tests
-	if (db->real && db != db->real)
-		opencl_get_sane_lws_gws_values();
 
 	// Initialize openCL tuning (library) for this format.
 	opencl_init_auto_setup(SEED, 1, NULL, warn, 2, self,

src/opencl_ntlmv2_fmt_plug.c
@@ -940,43 +940,29 @@ static int cmp_exact(char *source, int index)
 
 static void reset(struct db_main *db)
 {
-	static size_t o_lws, o_gws;
-	static int initialized;
-	size_t gws_limit;
+	static int last_int_cand;
 
-	//fprintf(stderr, "%s(%p), i=%d\n", __FUNCTION__, db, initialized);
-	gws_limit = MIN((0xf << 21) * 4 / BUFSIZE,
-	                get_max_mem_alloc_size(gpu_id) / BUFSIZE);
+	if (!crypt_kernel || last_int_cand != mask_int_cand.num_int_cand) {
+		release_base_clobj();
+		release_clobj();
+
+		prepare_table(db);
+		init_kernel();
+
+		create_base_clobj();
+
+		current_salt = 0;
+		hash_ids[0] = 0;
+
+		last_int_cand = mask_int_cand.num_int_cand;
+	}
+
+	size_t gws_limit = MIN((0xf << 21) * 4 / BUFSIZE,
+	                       get_max_mem_alloc_size(gpu_id) / BUFSIZE);
 	get_power_of_two(gws_limit);
 	if (gws_limit > MIN((0xf << 21) * 4 / BUFSIZE,
 	                    get_max_mem_alloc_size(gpu_id) / BUFSIZE))
 		gws_limit >>= 1;
-
-
-	if (initialized) {
-		// Forget the previous auto-tune
-		local_work_size = o_lws;
-		global_work_size = o_gws;
-
-		release_base_clobj();
-		release_clobj();
-	} else {
-		o_lws = local_work_size;
-		o_gws = global_work_size;
-		initialized = 1;
-	}
-
-	prepare_table(db);
-	init_kernel();
-
-	create_base_clobj();
-
-	current_salt = 0;
-	hash_ids[0] = 0;
-
-	// If real crack run, don't auto-tune for self-tests
-	if (db->real && db != db->real)
-		opencl_get_sane_lws_gws_values();
 
 	// Initialize openCL tuning (library) for this format.
 	opencl_init_auto_setup(SEED, 1, NULL, warn, 2, self,

commit 2751ac5c2add57435356ef0a67ad82715f4bce07

OpenCL NTLMv2 and MSCASH formats: Bug fix for reset()
When running without mask, format would not reinitialize hash tables
between self-tests and the real run, causing a hang at best and false
negatives at worst.

Problem introduced with bd7b8267 (#4012).

src/opencl_mscash_fmt_plug.c
@@ -730,22 +730,16 @@ static int cmp_exact(char *source, int index)
 
 static void reset(struct db_main *db)
 {
-	static int last_int_cand;
+	release_base_clobj();
+	release_clobj();
 
-	if (!crypt_kernel || last_int_cand != mask_int_cand.num_int_cand) {
-		release_base_clobj();
-		release_clobj();
+	prepare_table(db);
+	init_kernel();
 
-		prepare_table(db);
-		init_kernel();
+	create_base_clobj();
 
-		create_base_clobj();
-
-		current_salt = 0;
-		hash_ids[0] = 0;
-
-		last_int_cand = mask_int_cand.num_int_cand;
-	}
+	current_salt = 0;
+	hash_ids[0] = 0;
 
 	size_t gws_limit = MIN((0xf << 21) * 4 / BUFSIZE,
 	                       get_max_mem_alloc_size(gpu_id) / BUFSIZE);

src/opencl_ntlmv2_fmt_plug.c
@@ -952,22 +952,16 @@ static int cmp_exact(char *source, int index)
 
 static void reset(struct db_main *db)
 {
-	static int last_int_cand;
+	release_base_clobj();
+	release_clobj();
 
-	if (!crypt_kernel || last_int_cand != mask_int_cand.num_int_cand) {
-		release_base_clobj();
-		release_clobj();
+	prepare_table(db);
+	init_kernel();
 
-		prepare_table(db);
-		init_kernel();
+	create_base_clobj();
 
-		create_base_clobj();
-
-		current_salt = 0;
-		hash_ids[0] = 0;
-
-		last_int_cand = mask_int_cand.num_int_cand;
-	}
+	current_salt = 0;
+	hash_ids[0] = 0;
 
 	size_t gws_limit = MIN((0xf << 21) * 4 / BUFSIZE,
 	                       get_max_mem_alloc_size(gpu_id) / BUFSIZE);