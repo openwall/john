b4ed6628758a54629d64e560ede999108db19e0b
Tobias Svenblad <59305921+tsvenbla@users.noreply.github.com>
2023-06-16 20:42:09 +0300

    Refactor password parser script for improved readability and maintainability
    The specific changes include:
    
    1. Introduced the `use strict` pragma to catch potential issues such as misspelled variable names or the use of uninitialized variables.
    
    2. Replaced print with the `say` feature for better readability and to avoid the need to explicitly include a newline character.
    
    3. Made use of the English module to make some Perl special variables more readable.
    
    4. Introduced a subroutine `output_user` to handle user output, improving code organization and maintainability. This also ensures we don't repeat ourselves when we need to output a user, which follows the DRY (Don't Repeat Yourself) principle.
    
    5. Altered the control flow of the script to only output the user when a new user line is encountered, improving the efficiency of the script. 
    
    6. Added a condition to output `NoPassword` for users without a password or with a placeholder password `*`, to make the output more meaningful.
    
    These changes make the script more readable, maintainable, and efficient, and they also follow modern Perl best practices.

--- run/aix2john.pl
+++ run/aix2john.pl
@@ -1,37 +1,57 @@
 #!/usr/bin/env perl
-#/
-#  This software is Copyright (c) 2013 Konrads Smelkovs <konrads.smelkovs@kpmg.co.uk>,
-#  and it is hereby released to the general public under the following terms:
-#  Redistribution and use in source and binary forms, with or without
-#  modification, are permitted.
-#
-# This script converts AIX /etc/security/passw
-# cat /etc/security/passwd
-# root:
-#         password = mrXXXXXXXXXX
-#         lastupdate = 1343960660
-#         flags =
-#
-# admin:
-#         password = oiXXXXXXXXXX
-#         lastupdate = 1339748349
-#         flags =
-# ...
-# Usage: aixpasswd2john.pl <inputfile>
-# If no input files are given, aixpasswd2john.pl will read from stdin
 
+# aixpasswd2john.pl
+#
+# This script parses AIX password data and transforms it into a format
+# suitable for use with the John the Ripper password cracking tool.
+# It expects input in the form of AIX /etc/security/passwd file data.
+#
+# The script reads input either from a file passed as a command-line
+# argument or from stdin if no file is provided.
+# Each line of the input is expected to follow one of these formats:
+# 
+#   username:
+#   password = password_hash
+#
+# The script produces output in the format:
+#   username:password_hash
+# If a user does not have a password or the password is "*", the output will be:
+#   username:NoPassword
+#
+# It's important to note that password hashes are sensitive data. Ensure this 
+# script is run in a secure environment and that the output is stored securely.
+#
+# Usage:
+#   aixpasswd2john.pl <inputfile>
+# If no input files are given, it will read from stdin.
+#
+# Example:
+#   cat /etc/security/passwd | aixpasswd2john.pl
+#
+# This will output the usernames and password hashes in a format suitable for 
+# John the Ripper, one per line.
+
+use strict;
 use warnings;
+use feature 'say';
+use English '-no_match_vars';
 
-$currentuser="";
-while(<>){
-	chomp;
-	if (m/^\s*([^:]+):\s*$/){
-		$currentuser=$1;
-		next;
-	}
-	if (m/^\s*password\s+=\s*(\S+)\s*$/ and $1 ne "*"){
-		print "$currentuser:$1\n";
-		$currentuser="";
-		next
-	}
+my ($current_user, $current_password);
+
+while (<>) {
+    chomp;
+    if (/^\s*([^:]+):\s*$/) {
+        output_user($current_user, $current_password) if defined $current_user;
+        ($current_user, $current_password) = ($1, undef);
+    } elsif (/^\s*password\s+=\s*(\S+)\s*$/) {
+        $current_password = $1;
+    }
+}
+
+output_user($current_user, $current_password) if defined $current_user;
+
+sub output_user {
+    my ($user, $password) = @_;
+    $password = 'NoPassword' if !defined $password || $password eq '*';
+    say "$user:$password";
 }
