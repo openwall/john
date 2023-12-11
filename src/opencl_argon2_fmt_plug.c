bd752f7df8210a1af8a91449572a8fa53028e110
Alain Espinosa <alainesp@gmail.com>
2023-12-11 11:23:49 +0100

    argon2-opencl: Define 2 kernels using the same file

--- src/opencl_argon2_fmt_plug.c
+++ src/opencl_argon2_fmt_plug.c
@@ -577,9 +577,8 @@ static void reset(struct db_main *db)
 	{
 		// Create and build OpenCL kernels
                 char build_opts[64];
-                snprintf(build_opts, sizeof(build_opts), "-DUSE_WARP_SHUFFLE=%i", !DEVICE_USE_LOCAL_MEMORY);
-                // TODO: When any file in '#include <file_name>' is changed => the binary cache should be not used
-		opencl_init("$JOHN/opencl/argon2_kernels_include.cl", gpu_id, build_opts);// Develop Nvidia: "-cl-nv-verbose -nv-line-info"
+                snprintf(build_opts, sizeof(build_opts), "-DUSE_WARP_SHUFFLE=%i", !DEVICE_USE_LOCAL_MEMORY);// Develop Nvidia: "-cl-nv-verbose -nv-line-info"
+		opencl_init("$JOHN/opencl/argon2_kernel.cl", gpu_id, build_opts);
 
 		// Select OpenCL kernel
 		char kernel_name[32];
