61b2795c3972f17367c3b906567a59ca81de193d
Solar Designer <solar@openwall.com>
2023-12-30 04:05:48 +0100

    CI: Add (revised) GitHub Actions from passwdqc

---
+++ .ci/install-dependencies.sh
@@ -0,0 +1,86 @@
+#!/bin/sh -ex
+#
+# Copyright (c) 2018-2020 The strace developers.
+# All rights reserved.
+#
+# SPDX-License-Identifier: GPL-2.0-or-later
+
+j=-j`nproc` || j=
+type sudo >/dev/null 2>&1 && sudo=sudo || sudo=
+common_packages='make libssl-dev'
+
+retry_if_failed()
+{
+	for i in `seq 0 99`; do
+		"$@" && i= && break || sleep 1
+	done
+	[ -z "$i" ]
+}
+
+updated=
+apt_get_install()
+{
+	[ -n "$updated" ] || {
+		retry_if_failed $sudo apt-get -qq update
+		updated=1
+	}
+	retry_if_failed $sudo \
+		apt-get -qq --no-install-suggests --no-install-recommends \
+		install -y "$@"
+}
+
+git_installed=
+clone_repo()
+{
+	local src dst branch
+	src="$1"; shift
+	dst="$1"; shift
+	branch="${1-}"
+
+	[ -n "$git_installed" ] || {
+		apt_get_install git ca-certificates
+		git_installed=1
+	}
+
+	case "$src" in
+		*://*)	;;
+		*)	local url path
+			url="$(git config remote.origin.url)"
+			path="${url#*://*/}"
+			src="${url%$path}$src"
+			;;
+	esac
+
+	retry_if_failed \
+		git clone --depth=1 ${branch:+--branch $branch} "$src" "$dst"
+}
+
+case "$TARGET" in
+	x32|x86)
+		packages="$common_packages gcc-multilib"
+		;;
+	*)
+		packages="$common_packages gcc"
+		;;
+esac
+
+case "$CC" in
+	gcc-*)
+		retry_if_failed \
+			$sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+		case "$TARGET" in
+			x32|x86)
+				apt_get_install $packages "$CC"-multilib "$CC"
+				;;
+			*)
+				apt_get_install $packages "$CC"
+				;;
+		esac
+		;;
+	clang*)
+		apt_get_install $packages "$CC"
+		;;
+	*)
+		apt_get_install $packages
+		;;
+esac
---
+++ .ci/run-build-and-tests.sh
@@ -0,0 +1,40 @@
+#!/bin/sh -ex
+#
+# Copyright (c) 2018-2020 The strace developers.
+# All rights reserved.
+#
+# SPDX-License-Identifier: GPL-2.0-or-later
+
+case "${TARGET-}" in
+	x32)
+		CC="$CC -mx32"
+		;;
+	x86)
+		CC="$CC -m32"
+		;;
+esac
+
+echo 'BEGIN OF BUILD ENVIRONMENT INFORMATION'
+uname -a |head -1
+libc="$(ldd /bin/sh |sed -n 's|^[^/]*\(/[^ ]*/libc\.so[^ ]*\).*|\1|p' |head -1)"
+$libc |head -1
+file -L /bin/sh
+$CC --version |head -1
+$CC -print-multi-lib ||:
+make --version |head -1
+kver="$(printf '%s\n%s\n' '#include <linux/version.h>' 'LINUX_VERSION_CODE' | $CC $CPPFLAGS -E -P -)"
+printf 'kernel-headers %s.%s.%s\n' $(($kver/65536)) $(($kver/256%256)) $(($kver%256))
+echo 'END OF BUILD ENVIRONMENT INFORMATION'
+
+nproc="$(nproc)" || nproc=1
+j="-j$nproc"
+
+cd src
+time ./configure
+time make $j
+time make $j check
+
+if git status --porcelain |grep ^.; then
+	echo >&2 'git status reported uncleanness'
+	exit 1
+fi
