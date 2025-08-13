0f37050f54f8f14c040173522c4453f083b62270
magnum <john.magnum@hushmail.com>
2025-08-13 17:36:55 +0200

    OpenCL AES: Keep only the unrolled code paths

--- run/opencl/opencl_aes_plain.h
+++ run/opencl/opencl_aes_plain.h
@@ -54,14 +54,6 @@
 #endif
 
 /*
- * Unroll key schedule, encryption and decryption. This has no adverse effects
- * on any device I've tested.
- */
-#define AES_FULL_UNROLL_SCHEDULE 1
-#define AES_FULL_UNROLL_ENCRYPT  1
-#define AES_FULL_UNROLL_DECRYPT  1
-
-/*
  * Declare Te4 table and use it instead of Te0..Te3 for last round encryption.
  * There is no equivalent opt-out for Td4.
  */
@@ -208,8 +200,6 @@ INLINE void AES_set_encrypt_key(AES_KEY_TYPE void *_userKey,
 	rk[1] = GETU32(userKey +  4);
 	rk[2] = GETU32(userKey +  8);
 	rk[3] = GETU32(userKey + 12);
-
-#if AES_FULL_UNROLL_SCHEDULE
 
 	if (bits == 128) {
 		key->rounds = 10;
@@ -523,88 +513,6 @@ INLINE void AES_set_encrypt_key(AES_KEY_TYPE void *_userKey,
 
 		return;
 	}
-
-#else	/* !AES_FULL_UNROLL_SCHEDULE */
-
-	int i = 0;
-	u32 temp;
-
-	if (bits == 128) {
-		key->rounds = 10;
-		for (i = 0; i < 10; i++) {
-			temp  = rk[3];
-			rk[4] = rk[0] ^
-				(TE2((temp >> 16) & 0xff) & 0xff000000) ^
-				(TE3((temp >>  8) & 0xff) & 0x00ff0000) ^
-				(TE0((temp      ) & 0xff) & 0x0000ff00) ^
-				(TE1( temp >> 24)         & 0x000000ff) ^
-				rcon[i];
-			rk[5] = rk[1] ^ rk[4];
-			rk[6] = rk[2] ^ rk[5];
-			rk[7] = rk[3] ^ rk[6];
-
-			rk += 4;
-		}
-		return;
-	}
-
-	rk[4] = GETU32(userKey + 16);
-	rk[5] = GETU32(userKey + 20);
-	if (bits == 192) {
-		key->rounds = 12;
-		for (i = 0; i < 8; i++) {
-			temp = rk[5];
-			rk[6] = rk[0] ^
-				(TE2((temp >> 16) & 0xff) & 0xff000000) ^
-				(TE3((temp >>  8) & 0xff) & 0x00ff0000) ^
-				(TE0((temp      ) & 0xff) & 0x0000ff00) ^
-				(TE1( temp >> 24)         & 0x000000ff) ^
-				rcon[i];
-			rk[7] = rk[1] ^ rk[6];
-			rk[8] = rk[2] ^ rk[7];
-			rk[9] = rk[3] ^ rk[8];
-			if (i < 7) {
-				rk[10] = rk[4] ^ rk[9];
-				rk[11] = rk[5] ^ rk[10];
-
-				rk += 6;
-			}
-		}
-		return;
-	}
-
-	rk[6] = GETU32(userKey + 24);
-	rk[7] = GETU32(userKey + 28);
-	if (bits == 256) {
-		key->rounds = 14;
-		for (i = 0; i < 7; i++) {
-			temp = rk[7];
-			rk[8] = rk[0] ^
-				(TE2((temp >> 16) & 0xff) & 0xff000000) ^
-				(TE3((temp >>  8) & 0xff) & 0x00ff0000) ^
-				(TE0((temp      ) & 0xff) & 0x0000ff00) ^
-				(TE1((temp >> 24)       ) & 0x000000ff) ^
-				rcon[i];
-			rk[9] = rk[1] ^ rk[8];
-			rk[10] = rk[2] ^ rk[9];
-			rk[11] = rk[3] ^ rk[10];
-			if (i < 6) {
-				temp = rk[11];
-				rk[12] = rk[4] ^
-					(TE2((temp >> 24)       ) & 0xff000000) ^
-					(TE3((temp >> 16) & 0xff) & 0x00ff0000) ^
-					(TE0((temp >>  8) & 0xff) & 0x0000ff00) ^
-					(TE1((temp      ) & 0xff) & 0x000000ff);
-				rk[13] = rk[5] ^ rk[12];
-				rk[14] = rk[6] ^ rk[13];
-				rk[15] = rk[7] ^ rk[14];
-
-				rk += 8;
-			}
-		}
-		return;
-	}
-#endif	/* ?AES_FULL_UNROLL_SCHEDULE */
 }
 
 /**
@@ -729,8 +637,6 @@ INLINE void AES_encrypt(const uchar *in, uchar *out, const AES_KEY *key)
 	s2 = GETU32(in +  8) ^ rk[2];
 	s3 = GETU32(in + 12) ^ rk[3];
 
-#if AES_FULL_UNROLL_ENCRYPT
-
 	/* round 1: */
 	t0 = TE0(s0 >> 24) ^ TE1((s1 >> 16) & 0xff) ^ TE2((s2 >> 8) & 0xff) ^ TE3(s3 & 0xff) ^ rk[4];
 	t1 = TE0(s1 >> 24) ^ TE1((s2 >> 16) & 0xff) ^ TE2((s3 >> 8) & 0xff) ^ TE3(s0 & 0xff) ^ rk[5];
@@ -801,70 +707,6 @@ INLINE void AES_encrypt(const uchar *in, uchar *out, const AES_KEY *key)
 		}
 	}
 	rk += key->rounds << 2;
-
-#else  /* !AES_FULL_UNROLL_ENCRYPT */
-
-	/*
-	 * Nr - 1 full rounds:
-	 */
-	int r = key->rounds >> 1;
-	for (;;) {
-		t0 =
-			TE0((s0 >> 24)       ) ^
-			TE1((s1 >> 16) & 0xff) ^
-			TE2((s2 >>  8) & 0xff) ^
-			TE3((s3      ) & 0xff) ^
-			rk[4];
-		t1 =
-			TE0((s1 >> 24)       ) ^
-			TE1((s2 >> 16) & 0xff) ^
-			TE2((s3 >>  8) & 0xff) ^
-			TE3((s0      ) & 0xff) ^
-			rk[5];
-		t2 =
-			TE0((s2 >> 24)       ) ^
-			TE1((s3 >> 16) & 0xff) ^
-			TE2((s0 >>  8) & 0xff) ^
-			TE3((s1      ) & 0xff) ^
-			rk[6];
-		t3 =
-			TE0((s3 >> 24)       ) ^
-			TE1((s0 >> 16) & 0xff) ^
-			TE2((s1 >>  8) & 0xff) ^
-			TE3((s2      ) & 0xff) ^
-			rk[7];
-
-		rk += 8;
-		if (--r == 0) {
-			break;
-		}
-
-		s0 =
-			TE0((t0 >> 24)       ) ^
-			TE1((t1 >> 16) & 0xff) ^
-			TE2((t2 >>  8) & 0xff) ^
-			TE3((t3      ) & 0xff) ^
-			rk[0];
-		s1 =
-			TE0((t1 >> 24)       ) ^
-			TE1((t2 >> 16) & 0xff) ^
-			TE2((t3 >>  8) & 0xff) ^
-			TE3((t0      ) & 0xff) ^
-			rk[1];
-		s2 =
-			TE0((t2 >> 24)       ) ^
-			TE1((t3 >> 16) & 0xff) ^
-			TE2((t0 >>  8) & 0xff) ^
-			TE3((t1      ) & 0xff) ^
-			rk[2];
-		s3 =
-			TE0((t3 >> 24)       ) ^
-			TE1((t0 >> 16) & 0xff) ^
-			TE2((t1 >>  8) & 0xff) ^
-			TE3((t2      ) & 0xff) ^
-			rk[3];
-	}
-#endif /* ?AES_FULL_UNROLL_ENCRYPT */
 
 	/*
 	 * apply last round and
@@ -980,8 +822,6 @@ INLINE void AES_decrypt(const uchar *in, uchar *out, const AES_KEY *key)
 	s2 = GETU32(in +  8) ^ rk[2];
 	s3 = GETU32(in + 12) ^ rk[3];
 
-#if AES_FULL_UNROLL_DECRYPT
-
 	/* round 1: */
 	t0 = TD0(s0 >> 24) ^ TD1((s3 >> 16) & 0xff) ^ TD2((s2 >> 8) & 0xff) ^ TD3(s1 & 0xff) ^ rk[4];
 	t1 = TD0(s1 >> 24) ^ TD1((s0 >> 16) & 0xff) ^ TD2((s3 >> 8) & 0xff) ^ TD3(s2 & 0xff) ^ rk[5];
@@ -1052,70 +892,6 @@ INLINE void AES_decrypt(const uchar *in, uchar *out, const AES_KEY *key)
 		}
 	}
 	rk += key->rounds << 2;
-
-#else  /* !AES_FULL_UNROLL_DECRYPT */
-
-	/*
-	 * Nr - 1 full rounds:
-	 */
-	int r = key->rounds >> 1;
-	for (;;) {
-		t0 =
-			TD0((s0 >> 24)       ) ^
-			TD1((s3 >> 16) & 0xff) ^
-			TD2((s2 >>  8) & 0xff) ^
-			TD3((s1      ) & 0xff) ^
-			rk[4];
-		t1 =
-			TD0((s1 >> 24)       ) ^
-			TD1((s0 >> 16) & 0xff) ^
-			TD2((s3 >>  8) & 0xff) ^
-			TD3((s2      ) & 0xff) ^
-			rk[5];
-		t2 =
-			TD0((s2 >> 24)       ) ^
-			TD1((s1 >> 16) & 0xff) ^
-			TD2((s0 >>  8) & 0xff) ^
-			TD3((s3      ) & 0xff) ^
-			rk[6];
-		t3 =
-			TD0((s3 >> 24)       ) ^
-			TD1((s2 >> 16) & 0xff) ^
-			TD2((s1 >>  8) & 0xff) ^
-			TD3((s0      ) & 0xff) ^
-			rk[7];
-
-		rk += 8;
-		if (--r == 0) {
-			break;
-		}
-
-		s0 =
-			TD0((t0 >> 24)       ) ^
-			TD1((t3 >> 16) & 0xff) ^
-			TD2((t2 >>  8) & 0xff) ^
-			TD3((t1      ) & 0xff) ^
-			rk[0];
-		s1 =
-			TD0((t1 >> 24)       ) ^
-			TD1((t0 >> 16) & 0xff) ^
-			TD2((t3 >>  8) & 0xff) ^
-			TD3((t2      ) & 0xff) ^
-			rk[1];
-		s2 =
-			TD0((t2 >> 24)       ) ^
-			TD1((t1 >> 16) & 0xff) ^
-			TD2((t0 >>  8) & 0xff) ^
-			TD3((t3      ) & 0xff) ^
-			rk[2];
-		s3 =
-			TD0((t3 >> 24)       ) ^
-			TD1((t2 >> 16) & 0xff) ^
-			TD2((t1 >>  8) & 0xff) ^
-			TD3((t0      ) & 0xff) ^
-			rk[3];
-	}
-#endif /* ?AES_FULL_UNROLL_DECRYPT */
 
 	/*
 	 * apply last round and
