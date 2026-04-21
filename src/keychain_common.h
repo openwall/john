59a7569c9d88e35228447f44c98a2c887268f291
Dhiru Kholia <dhiru@openwall.com>
2026-04-21 17:05:23 +0530

    Make Apple Keychain format more robust
    Closes https://github.com/openwall/john/issues/3408

--- src/keychain_common.h
+++ src/keychain_common.h
@@ -10,10 +10,13 @@
 #define FORMAT_NAME             "Mac OS X Keychain"
 #define FORMAT_TAG              "$keychain$*"
 #define FORMAT_TAG_LEN          (sizeof(FORMAT_TAG)-1)
+#define FORMAT_TAG_V2           "$keychainv2$*"
+#define FORMAT_TAG_V2_LEN       (sizeof(FORMAT_TAG_V2)-1)
 
 #define SALTLEN                 20
 #define IVLEN                   8
 #define CTLEN                   48
+#define SYMKEY_MAX_CTLEN        256
 
 extern struct fmt_tests keychain_tests[];
 
@@ -21,6 +24,9 @@ struct custom_salt {
 	unsigned char salt[SALTLEN];
 	unsigned char iv[IVLEN];
 	unsigned char ct[CTLEN];
+	int has_symkey;
+	unsigned char symkey_ct[SYMKEY_MAX_CTLEN];
+	int symkey_ct_len;
 };
 
 int keychain_valid(char *ciphertext, struct fmt_main *self);
